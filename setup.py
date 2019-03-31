35ab6d13f2a3235f20affd29800abc70bf0464ae
William Falcon <waf2107@columbia.edu>
2019-03-31 16:31:36 -0400

    release v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pytorch-lightning',
-      version='0.0.1',
+      version='0.0.2',
       description='Rapid research framework',
       author='',
       author_email='',
