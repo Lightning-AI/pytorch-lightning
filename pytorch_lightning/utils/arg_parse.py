91e90d8f91280d4c83de9773b8be1dcf8ac062f7
William Falcon <waf2107@columbia.edu>
2019-05-16 15:47:21 -0400

    added amp level option

--- pytorch_lightning/utils/arg_parse.py
+++ pytorch_lightning/utils/arg_parse.py
@@ -51,7 +51,7 @@ def add_default_args(parser, root_dir, rand_seed=None, possible_model_names=None
     parser.add_argument('--disable_cuda', dest='disable_cuda', action='store_true')
     parser.add_argument('--default_tensor_type', default='torch.cuda.FloatTensor', type=str)
     parser.add_argument('--use_amp', dest='use_amp', action='store_true')
-    parser.add_argument('--amp_level', dest='O2', action='store_true')
+    parser.add_argument('--amp_level', default='O2',type=str)
 
 
     # run on hpc
