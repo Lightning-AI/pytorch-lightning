23137ea08a8ccec074c4e68294902464c1f36ca6
William Falcon <falc@fb.com>
2019-07-03 16:38:03 -0400

    added single node distdataparallel

--- pytorch_lightning/pt_overrides/override_data_parallel.py
+++ pytorch_lightning/pt_overrides/override_data_parallel.py
@@ -28,6 +28,7 @@ class LightningDataParallel(DataParallel):
     """
 
     def parallel_apply(self, replicas, inputs, kwargs):
+        print('parallel_apply called')
         return parallel_apply(replicas, inputs, kwargs, self.device_ids[:len(replicas)])
 
 
