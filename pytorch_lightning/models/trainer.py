ad3d1374012e3c00b7843741caaba3cb747b24ac
William Falcon <falc@fb.com>
2019-07-03 15:23:47 -0400

    added single node distdataparallel

--- pytorch_lightning/models/trainer.py
+++ pytorch_lightning/models/trainer.py
@@ -284,7 +284,7 @@ class Trainer(TrainerIO):
         # when GPU is called, spawn off a single worker for each gpu
         if self.on_gpu:
             rank = 0
-            self.model = model
+            # self.model = model
             mp.spawn(dummy, nprocs=len(self.data_parallel_device_ids), args=(self, ))
         else:
             self.__run_pretrain_routine(model)
