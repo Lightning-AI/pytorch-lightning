8042a44f0983e2fff00c3f0561a0863e84575dbb
William Falcon <falc@fb.com>
2019-07-08 13:25:02 -0400

    added multi-node locked ip search

--- examples/new_project_templates/trainer_gpu_cluster_template.py
+++ examples/new_project_templates/trainer_gpu_cluster_template.py
@@ -42,6 +42,9 @@ def main(hparams, cluster, results_dict):
     :param hparams:
     :return:
     """
+    path = 'emv_' + os.environ('SLURM_CLUSTER_NAME')
+    os.makedirs(os.path.join(hparams.test_tube_save_path, path), exist_ok=True)
+
     # ------------------------
     # 1 INIT LIGHTNING MODEL
     # ------------------------
