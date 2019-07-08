d4fc07185675bcf647b3b0ebc2f69185c120272d
William Falcon <falc@fb.com>
2019-07-08 19:53:24 -0400

    scaled batch size

--- examples/new_project_templates/lightning_module_template.py
+++ examples/new_project_templates/lightning_module_template.py
@@ -164,7 +164,7 @@ class LightningTemplateModel(LightningModule):
 
         # try:
         #     if self.on_gpu:
-        train_sampler = DistributedSampler(dataset, num_replicas=self.trainer.world_size, rank=self.trainer.proc_rank)
+        train_sampler = DistributedSampler(dataset, rank=self.trainer.proc_rank)
         batch_size = batch_size // self.trainer.world_size  # scale batch size
 
         # except Exception as e:
