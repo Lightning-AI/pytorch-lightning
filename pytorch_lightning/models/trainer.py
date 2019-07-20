d881460e8bd8a80e6b478072c11b19cac41f73ed
William Falcon <falc@fb.com>
2019-07-20 09:17:20 -0400

    removed logging

--- pytorch_lightning/models/trainer.py
+++ pytorch_lightning/models/trainer.py
@@ -380,6 +380,9 @@ class Trainer(TrainerIO):
             # otherwise we launch the required number of processes
             try:
                 nb_slurm_tasks = int(os.environ['SLURM_NTASKS'])
+                print('x'*100)
+                print(f'NB_TASKS: {nb_slurm_tasks}')
+                print('x'*100)
                 nb_requested_gpus = len(self.data_parallel_device_ids)
                 is_slurm_managing_tasks = nb_slurm_tasks == nb_requested_gpus
             except Exception as e:
