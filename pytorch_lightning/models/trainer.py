9580a05e0954148a6c2a3752dd8a0ef3c159c673
William Falcon <falc@fb.com>
2019-07-24 17:18:58 -0400

    added multiple outputs to LightningTestModel

--- pytorch_lightning/models/trainer.py
+++ pytorch_lightning/models/trainer.py
@@ -225,7 +225,7 @@ class Trainer(TrainerIO):
         if self.use_amp:
             print('using 16bit precision')
 
-        if use_amp and not APEX_AVAILABLE:
+        if use_amp and not APEX_AVAILABLE:  # pragma: no cover
             msg = '''
             You set use_amp=True but do not have apex installed.
             Install apex first using this guide and rerun with use_amp=True: 
