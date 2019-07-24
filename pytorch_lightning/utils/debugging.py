c67eeb58c7ffe9fc16104556217c1625dc8270b5
William Falcon <falc@fb.com>
2019-07-24 10:34:21 -0400

    added debugging util

---
+++ pytorch_lightning/utils/debugging.py
@@ -0,0 +1,15 @@
+import pdb
+import sys
+
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
