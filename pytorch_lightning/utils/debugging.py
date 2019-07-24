ebc120a3c3d9096476eda137ff276a5694a2eabf
William Falcon <falc@fb.com>
2019-07-24 15:27:59 -0400

    removed forkedpdb

--- pytorch_lightning/utils/debugging.py
+++ pytorch_lightning/utils/debugging.py
@@ -1,19 +1,5 @@
 import pdb
 import sys
 
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
-
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
-
-
 class IncompatibleArgumentsException(Exception):
     pass
