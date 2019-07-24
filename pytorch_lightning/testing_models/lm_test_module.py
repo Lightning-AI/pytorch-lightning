f0eedb44ec7f00d4fd604c947c9cf0fefe880ec4
William Falcon <falc@fb.com>
2019-07-24 17:23:19 -0400

    added multiple outputs to LightningTestModel

--- pytorch_lightning/testing_models/lm_test_module.py
+++ pytorch_lightning/testing_models/lm_test_module.py
@@ -127,16 +127,16 @@ class LightningTestModel(LightningModule):
             val_acc = val_acc.unsqueeze(0)
 
         # alternate possible outputs to test
-        if self.trainer.batch_nb % 0 == 0:
+        if self.trainer.batch_nb % 1 == 0:
             output = OrderedDict({
                 'val_loss': loss_val,
                 'val_acc': val_acc,
             })
             return output
-        if self.trainer.batch_nb % 1 == 0:
+        if self.trainer.batch_nb % 2 == 0:
             return val_acc
 
-        if self.trainer.batch_nb % 2 == 0:
+        if self.trainer.batch_nb % 3 == 0:
             output = OrderedDict({
                 'val_loss': loss_val,
                 'val_acc': val_acc,
