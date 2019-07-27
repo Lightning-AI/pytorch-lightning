60e60fcd8be90c3e906fe4bbdd2b372c7948315d
William Falcon <falc@fb.com>
2019-07-26 23:09:27 -0400

    added clean slurm save load test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 setup(
     name="pytorch-lightning",
-    version='0.3.6.1',
+    version='0.3.6.2',
     description="The Keras for ML researchers using PyTorch",
     author="William Falcon",
     author_email="waf2107@columbia.edu",
