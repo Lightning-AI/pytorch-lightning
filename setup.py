79f0856bc8e386e9acd7e0b6912ad767c778b7f1
Jiri BOROVEC <jirka.borovec@seznam.cz>
2019-08-05 22:19:55 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+import pytorch_lightning
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 setup(
     name="pytorch-lightning",
-    version='0.3.6.9',
-    description="The Keras for ML researchers using PyTorch",
-    author="William Falcon",
-    author_email="waf2107@columbia.edu",
-    url="https://github.com/williamFalcon/pytorch-lightning",
-    download_url="https://github.com/williamFalcon/pytorch-lightning",
-    license="MIT",
+    version=pytorch_lightning.__version__,
+    description=pytorch_lightning.__doc__,
+    author=pytorch_lightning.__author__,
+    author_email=pytorch_lightning.__author_email__,
+    url=pytorch_lightning.__homepage__,
+    license=pytorch_lightning.__license__,
+    packages=['pytorch_lightning'],
+
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+
+    include_package_data=True,
+    zip_safe=False,
+
     keywords=["deep learning", "pytorch", "AI"],
     python_requires=">=3.5",
     install_requires=[
@@ -21,9 +30,26 @@ setup(
         "tqdm",
         "test-tube>=0.6.7.6",
     ],
-    packages=find_packages(),
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type='text/markdown',
-    include_package_data=True,
-    zip_safe=False,
+
+    classifiers=[
+        'Environment :: Console',
+        'Natural Language :: English',
+        # How mature is this project? Common values are
+        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        # Pick your license as you wish
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
