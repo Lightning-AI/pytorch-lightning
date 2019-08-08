d00c0586457d29c9f5c26b0543a893b4055a0b77
William Falcon <waf2107@columbia.edu>
2019-08-08 04:28:38 -0400

    Update __init__.py

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -1,19 +1,6 @@
-try:
-    from .models.trainer import Trainer
-    from .root_module.root_module import LightningModule
-    from .root_module.decorators import data_loader
-except ImportError as e:
-    print('Failing to load some internal modules.')
-
-__version__ = '0.3.6.9'
-__author__ = "William Falcon",
-__author_email__ = "waf2107@columbia.edu"
-__license__ = 'Apache-2'
-__homepage__ = 'https://github.com/williamFalcon/pytorch-lightning',
-__copyright__ = 'Copyright (c) 2018-2019, %s.' % __author__
-__doc__ = """
-The Keras for ML researchers using PyTorch
-"""
+from .models.trainer import Trainer
+from .root_module.root_module import LightningModule
+from .root_module.decorators import data_loader
 
 __all__ = [
     'Trainer',
