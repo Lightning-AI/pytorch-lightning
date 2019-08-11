be4f46e4bdb5851ccec8ea7747b14163ac9a84fd
sidhanthholalkere <sidhanth.holalkere@gmail.com>
2019-08-11 13:23:32 -0400

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,7 +10,7 @@ from test_tube import Experiment, SlurmCluster
 
 # sys.path += [os.path.abspath('..'), os.path.abspath('../..')]
 from pytorch_lightning import Trainer
-from pytorch_lightning.testing import LightningTestModel, NoValEndTestModel, NoValModel, MultiValModel
+from pytorch_lightning.testing import LightningTestModel, NoValEndTestModel, NoValModel
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.utilities.debugging import MisconfigurationException
 from pytorch_lightning.root_module import memory
