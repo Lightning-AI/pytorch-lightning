c4e92c4f6cd7d994e8b8435b307d747a6cc61d79
William Falcon <falc@fb.com>
2019-08-11 08:08:41 -0400

    remove class

--- pytorch_lightning/models/trainer.py
+++ pytorch_lightning/models/trainer.py
@@ -314,8 +314,6 @@ class Trainer(TrainerIO):
         return callable(f_op)
 
     def __is_overriden(self, f_name):
-        import pdb
-        pdb.set_trace()
         model = self.__get_model()
         return f_name in model.__dict__
 
