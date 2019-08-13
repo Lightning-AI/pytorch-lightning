2fa0a72e60666f4bcd1ac1489548564461162925
William Falcon <waf2107@columbia.edu>
2019-08-13 11:37:37 -0400

    Val idx optional in validation_step (#108)
    * made dataset_i only available with multiple datasets
    
    * updated interface signature
    
    * updated tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -767,7 +767,7 @@ def test_multiple_val_dataloader():
     :return:
     """
     hparams = get_hparams()
-    model = LightningTemplateModel(hparams)
+    model = LightningTestModel(hparams)
 
     save_dir = init_save_dir()
 
