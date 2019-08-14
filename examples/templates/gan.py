13f2d1ab1ce5239ae3f6ad41d9a7b244dd78289a
William Falcon <waf2107@columbia.edu>
2019-08-14 08:41:32 -0400

    Update gan.py

--- examples/templates/gan.py
+++ examples/templates/gan.py
@@ -1,3 +1,11 @@
+"""
+To run this template just do:  
+python gan.py   
+
+After a few epochs, launch tensorboard to see the images being generated at every batch.   
+
+tensorboard --logdir default
+"""
 from argparse import ArgumentParser
 import os
 import numpy as np
