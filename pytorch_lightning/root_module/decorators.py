e2d3793567a26f758f7b912163187ab4276fbd0c
William Falcon <falc@fb.com>
2019-08-23 19:55:20 -0400

    updated base files

--- pytorch_lightning/root_module/decorators.py
+++ pytorch_lightning/root_module/decorators.py
@@ -17,7 +17,7 @@ def data_loader(fn):
                 value = fn(self)  # Lazy evaluation, done only once.
             except AttributeError as e:
                 # Guard against AttributeError suppression. (Issue #142)
-                raise RuntimeError(f'{attr_name}: An AttributeError was encountered: ' + str(e)) from e
+                raise RuntimeError(f'{fn.__name__}: An AttributeError was encountered: ' + str(e)) from e
             setattr(self, attr_name, value)  # Memoize evaluation.
         return value
 
