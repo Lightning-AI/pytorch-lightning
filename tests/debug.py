2504da0231179f2214387a578d4f20ecdafaed25
William Falcon <falc@fb.com>
2019-08-23 20:09:50 -0400

    updated base files

--- tests/debug.py
+++ tests/debug.py
@@ -181,7 +181,6 @@ def main():
     os.environ['MASTER_PORT'] = str(np.random.randint(12000, 19000, 1)[0])
     model, hparams = get_model()
     trainer_options = dict(
-        show_progress_bar=False,
         max_nb_epochs=1,
         train_percent_check=0.4,
         val_percent_check=0.2,
