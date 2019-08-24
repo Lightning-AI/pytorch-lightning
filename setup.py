257f52e59f17beea8e2b70fab77ff98c8e4423ea
William Falcon <falc@fb.com>
2019-08-24 08:13:54 -0400

    release v

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 # engineer specific practices
 setup(
     name='pytorch-lightning',
-    version='0.4.6',
+    version='0.4.7',
     description='The Keras for ML researchers using PyTorch',
     author='William Falcon',
     author_email='waf2107@columbia.edu',
