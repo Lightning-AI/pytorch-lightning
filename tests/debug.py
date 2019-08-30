ff12e39cba262c76169e9a483e702b95c6ed810d
William Falcon <falc@fb.com>
2019-08-30 12:38:17 -0400

    debug

--- tests/debug.py
+++ tests/debug.py
@@ -1,7 +1,5 @@
 import sys
-import os.path
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+sys.path.append("..")
 
 from pytorch_lightning import Trainer
 from  .. import examples
