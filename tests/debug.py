e4bb7981ac131eaba8f82678cea018b1d872465e
William Falcon <falc@fb.com>
2019-08-30 13:22:20 -0400

    debug

--- tests/debug.py
+++ tests/debug.py
@@ -205,7 +205,7 @@ def main():
     os.environ['MASTER_PORT'] = str(np.random.randint(12000, 19000, 1)[0])
     model, hparams = get_model()
     trainer_options = dict(
-        max_nb_epochs=2,
+        max_nb_epochs=1,
         train_percent_check=0.4,
         val_percent_check=0.2,
         gpus=[0],
