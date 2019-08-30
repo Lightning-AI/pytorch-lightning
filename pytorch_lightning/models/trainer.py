b79991eab3fbe8199bcfe226528cd07b8de2b61e
William Falcon <falc@fb.com>
2019-08-30 16:17:08 -0400

    debug

--- pytorch_lightning/models/trainer.py
+++ pytorch_lightning/models/trainer.py
@@ -1217,18 +1217,3 @@ class Trainer(TrainerIO):
             print('save callback...')
             self.checkpoint_callback.on_epoch_end(epoch=self.current_epoch,
                                                   logs=self.__tng_tqdm_dic)
-
-
-import sys
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
-
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
