887303e13e4d082aca75d0dbd549afe9ca04c688
William Falcon <falc@fb.com>
2019-09-05 09:01:19 -0400

    pl handles signal now not test-tube

--- tests/debug.py
+++ tests/debug.py
@@ -232,7 +232,7 @@ def main():
         show_progress_bar=False,
         max_nb_epochs=1,
         train_percent_check=1.0,
-        val_percent_check=0.2,
+        val_percent_check=0.8,
         checkpoint_callback=checkpoint,
         experiment=exp
     )
