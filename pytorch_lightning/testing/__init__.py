e9c4bfe358394d37dc486e961951a393e5cffe05
William Falcon <falc@fb.com>
2019-09-05 13:50:13 -0400

    pl handles signal now not test-tube

--- pytorch_lightning/testing/__init__.py
+++ pytorch_lightning/testing/__init__.py
@@ -1,12 +1,3 @@
 from .lm_test_module import LightningTestModel
-from .lm_test_module_base import LightningTestModelBase
-from .lm_test_module_mixins import (
-    LightningValidationStepMixin,
-    LightningValidationMixin,
-    LightningValidationStepMultipleDataloadersMixin,
-    LightningValidationMultipleDataloadersMixin,
-    LightningTestStepMixin,
-    LightningTestMixin,
-    LightningTestStepMultipleDataloadersMixin,
-    LightningTestMultipleDataloadersMixin,
-)
+from .no_val_end_module import NoValEndTestModel
+from .no_val_module import NoValModel
