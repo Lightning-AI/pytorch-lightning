6c3ce1de9ae14a03af1ae71601291ccbb2466adb
William Falcon <falc@fb.com>
2019-09-05 15:43:46 -0400

    refactored amp

--- tests/test_models.py
+++ tests/test_models.py
@@ -93,6 +93,7 @@ def test_cpu_restore_training():
 
     # by calling fit again, we trigger training, loading weights from the cluster
     # and our hook to predict using current model before any more weight updates
+    pdb.set_trace()
     trainer.fit(model)
 
     clear_save_dir()
