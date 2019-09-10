390295aa80d3717b030aa0ac90eccfdab17e7453
William Falcon <falc@fb.com>
2019-09-10 08:31:14 -0400

    added load on CPU first

--- tests/test_models.py
+++ tests/test_models.py
@@ -52,7 +52,7 @@ def test_amp_dp_resume():
 
     trainer_options = dict(
         show_progress_bar=True,
-        max_nb_epochs=4,
+        max_nb_epochs=1,
         gpus=2,
         distributed_backend='dp',
     )
