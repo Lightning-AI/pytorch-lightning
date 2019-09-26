cf04ff73e98330d8c901a2512ef7810f15f18f4c
William Falcon <waf2107@columbia.edu>
2019-09-26 16:10:51 -0400

    undo test changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -339,7 +339,7 @@ def test_running_test_pretrained_model_dp():
         val_percent_check=0.2,
         checkpoint_callback=checkpoint,
         experiment=exp,
-        gpus=[0, 1],
+        gpus=2,
         distributed_backend='dp'
     )
 
@@ -1247,7 +1247,7 @@ def test_amp_gpu_ddp_slurm_managed():
     trainer_options = dict(
         show_progress_bar=True,
         max_nb_epochs=1,
-        gpus=1,
+        gpus=2,
         distributed_backend='ddp',
         use_amp=True
     )
