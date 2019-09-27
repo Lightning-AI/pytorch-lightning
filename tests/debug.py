c1fdfe52cdc7904103012493a74b1c1be59f3611
William Falcon <waf2107@columbia.edu>
2019-09-27 13:27:30 -0400

    debug

--- tests/debug.py
+++ tests/debug.py
@@ -59,18 +59,14 @@ class CoolModel(pl.LightningModule):
         return DataLoader(MNIST('path/to/save', train=False), batch_size=32)
 
 
-def get_model():
+def get_model(use_test_model=False):
     # set up model with these hyperparams
-    root_dir = os.path.dirname(os.path.realpath(__file__))
-    hparams = Namespace(**{'drop_prob': 0.2,
-                           'batch_size': 32,
-                           'in_features': 28 * 28,
-                           'learning_rate': 0.001 * 8,
-                           'optimizer_name': 'adam',
-                           'data_root': os.path.join(root_dir, 'mnist'),
-                           'out_features': 10,
-                           'hidden_dim': 1000})
-    model = LightningTemplateModel(hparams)
+    hparams = get_hparams()
+
+    if use_test_model:
+        model = LightningTestModel(hparams)
+    else:
+        model = LightningTemplateModel(hparams)
 
     return model, hparams
 
