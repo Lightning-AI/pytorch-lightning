8b0af19abee0b296c08e1e726b9eaca64ba5d2ca
Anton Bakhtin <7470747+akhti@users.noreply.github.com>
2019-10-01 09:25:47 -0700

    Log warning

--- pytorch_lightning/logging/mlflow_logger.py
+++ pytorch_lightning/logging/mlflow_logger.py
@@ -34,8 +34,12 @@ class MLFlowLogger(LightningLoggerBase):
     def log_metrics(self, metrics, step_num=None):
         timestamp_ms = int(time() * 1000)
         for k, v in metrics.items():
-            if not isinstance(v, str):
-                self.client.log_metric(self.run_id, k, v, timestamp_ms, step_num)
+            if isinstance(v, str):
+                logger.warning(
+                    f"Discarding metric with string value {k}={v}"
+                )
+                continue
+            self.client.log_metric(self.run_id, k, v, timestamp_ms, step_num)
 
     def save(self):
         pass
