6918bcba54f826e7ef49a906a550ed2101a48556
William Falcon <waf2107@columbia.edu>
2019-10-04 15:07:28 -0400

    cheesecake

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -51,21 +51,6 @@ def reduce_distributed_output(output, nb_gpus):
             output[k] = reduced
     return output
 
-import sys
-import pdb
-
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
-
 
 class Trainer(TrainerIO):
 
