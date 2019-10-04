22bb2a87366fc58ba51a709d847ae7a8c2b0c47c
William Falcon <waf2107@columbia.edu>
2019-10-04 15:58:54 -0400

    Update .run_local_tests.sh

--- .run_local_tests.sh
+++ .run_local_tests.sh
@@ -1,3 +1,3 @@
 rm -rf tests/save_dir*
-rm -rf tests/mlruns_9964541/mlruns/
+rm -rf tests/mlruns_*
 coverage run --source pytorch_lightning -m py.test pytorch_lightning tests examples -v --doctest-modules
