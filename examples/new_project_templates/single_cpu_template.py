d03d7a24406435d8b90d2a85d58663fefdd4349e
William Falcon <waf2107@columbia.edu>
2019-10-05 13:52:25 -0400

    Update single_cpu_template.py

--- examples/new_project_templates/single_cpu_template.py
+++ examples/new_project_templates/single_cpu_template.py
@@ -28,56 +28,15 @@ def main(hparams):
     model = LightningTemplateModel(hparams)
 
     # ------------------------
-    # 2 INIT EXP
+    # 2 INIT TRAINER
     # ------------------------
-    # init experiment
-    exp = Experiment(
-        name=hyperparams.experiment_name,
-        save_dir=hyperparams.test_tube_save_path,
-        autosave=False,
-        description='test demo'
-    )
-
-    exp.argparse(hparams)
-    exp.save()
+    trainer = Trainer()
 
     # ------------------------
-    # 3 INIT TRAINER
-    # ------------------------
-    trainer = Trainer(experiment=exp)
-
-    # ------------------------
-    # 4 START TRAINING
+    # 3 START TRAINING
     # ------------------------
     trainer.fit(model)
 
 
 if __name__ == '__main__':
-
-    # dirs
-    root_dir = os.path.dirname(os.path.realpath(__file__))
-    demo_log_dir = os.path.join(root_dir, 'pt_lightning_demo_logs')
-    checkpoint_dir = os.path.join(demo_log_dir, 'model_weights')
-    test_tube_dir = os.path.join(demo_log_dir, 'test_tube_data')
-
-    # although we user hyperOptParser, we are using it only as argparse right now
-    parent_parser = HyperOptArgumentParser(strategy='grid_search', add_help=False)
-
-    # gpu args
-    parent_parser.add_argument('--test_tube_save_path', type=str,
-                               default=test_tube_dir, help='where to save logs')
-    parent_parser.add_argument('--model_save_path', type=str,
-                               default=checkpoint_dir, help='where to save model')
-    parent_parser.add_argument('--experiment_name', type=str,
-                               default='pt_lightning_exp_a', help='test tube exp name')
-
-    # allow model to overwrite or extend args
-    parser = LightningTemplateModel.add_model_specific_args(parent_parser, root_dir)
-    hyperparams = parser.parse_args()
-
-    # ---------------------
-    # RUN TRAINING
-    # ---------------------
-    # run on HPC cluster
-    print('RUNNING ON CPU')
     main(hyperparams)
