9d79534b8f1df49436a6033c6e5bdc62c1597645
William Falcon <falc@fb.com>
2019-10-15 21:08:30 -0400

    fixed tests

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -271,7 +271,7 @@ class Trainer(TrainerIOMixin):
         if self.checkpoint_callback is True:
             # init a default one
             if isinstance(self.logger, TestTubeLogger):
-                ckpt_path = '{}/{}/{}/{}'.format(
+                ckpt_path = '{}/{}/version_{}/{}'.format(
                     self.default_save_path,
                     self.logger.experiment.name,
                     self.logger.experiment.version,
