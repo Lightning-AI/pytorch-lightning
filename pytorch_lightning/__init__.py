d8ea1cc231e0fc2f453471f47243f7ab44943ab7
William Falcon <waf2107@columbia.edu>
2019-10-18 10:00:28 +0200

    Update __init__.py

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -8,8 +8,8 @@ __all__ = [
     'data_loader',
 ]
 
-__version__ = '0.5.2'
-__author__ = ' William Falcon at al.'
+__version__ = '0.5.2.1'
+__author__ = ' William Falcon et al.'
 __author_email__ = 'waf2107@columbia.edu'
 __license__ = 'Apache-2.0'
 __homepage__ = 'https://github.com/williamFalcon/pytorch-lightning',
