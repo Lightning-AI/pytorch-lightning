a7f26a67ac1b28f2ddd47eaec96ca7e92befbfc8
Jirka Borovec <Borda@users.noreply.github.com>
2019-10-18 11:01:32 +0300

    add package info (#359)
    * add package info #358
    
    * Update __init__.py
    
    * Update __init__.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ from setuptools import setup, find_packages
 
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 
+import pytorch_lightning
+
 # https://packaging.python.org/discussions/install-requires-vs-requirements /
 # keep the meta-data here for simplicity in reading this file... it's not obvious
 # what happens and to non-engineers they won't know to look in init ...
@@ -14,13 +16,13 @@ from setuptools import setup, find_packages
 # engineer specific practices
 setup(
     name='pytorch-lightning',
-    version='0.5.2.1',
-    description='The Keras for ML researchers using PyTorch',
-    author='William Falcon',
-    author_email='waf2107@columbia.edu',
-    url='https://github.com/williamFalcon/pytorch-lightning',
+    version=pytorch_lightning.__version__,
+    description=pytorch_lightning.__doc__,
+    author=pytorch_lightning.__author__,
+    author_email=pytorch_lightning.__author_email__,
+    url=pytorch_lightning.__homepage__,
     download_url='https://github.com/williamFalcon/pytorch-lightning',
-    license='Apache-2',
+    license=pytorch_lightning.__license__,
     packages=find_packages(),
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
