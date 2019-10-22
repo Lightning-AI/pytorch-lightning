6bc2b22feaff1882d6db9c7db706d3f4ed007bdd
Cristobal Eyzaguirre <ceyzaguirre4@uc.cl>
2019-10-21 22:51:17 -0300

    Logger consistency (#397)
    * added comet logger
    
    * bug fix in cases where comet was not imported before torch
    
    * fixed mlflow logger to be consistent with docs, updated cometLogger and cometLoggers docs + flake 8 compliance

---
+++ pytorch_lightning/logging/comet_logger.py
@@ -0,0 +1,27 @@
+from time import time
+from logging import getLogger
+from os import environ
+from comet_ml import Experiment as CometExperiment
+from .base import LightningLoggerBase, rank_zero_only
+
+# needed to prevent ImportError and duplicated logs.
+environ["COMET_DISABLE_AUTO_LOGGING"] = "1"
+
+
+class CometLogger(LightningLoggerBase):
+    def __init__(self, *args, **kwargs):
+        super(CometLogger, self).__init__()
+        self.experiment = CometExperiment(*args, **kwargs)
+
+    @rank_zero_only
+    def log_hyperparams(self, params):
+        self.experiment.log_parameters(vars(params))
+
+    @rank_zero_only
+    def log_metrics(self, metrics, step_num):
+        # self.experiment.set_epoch(self, metrics.get('epoch', 0))
+        self.experiment.log_metrics(metrics)
+
+    @rank_zero_only
+    def finalize(self, status):
+        self.experiment.end()
