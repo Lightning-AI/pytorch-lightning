4f6af4121df98bc8559847f6ea79836b26cb2c9d
Jiri BOROVEC <jirka.borovec@seznam.cz>
2019-10-24 12:49:21 +0200

    fix failing on buildins

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ import os
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
-import builtins
+try:
+    import builtins
+except ImportError:
+    import __builtin__ as builtins
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
