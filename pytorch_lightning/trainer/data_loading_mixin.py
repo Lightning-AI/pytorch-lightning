6db27d371f104e0fb66ed2244da85c159adaaa91
William Falcon <falc@fb.com>
2019-10-24 05:36:47 -0400

    refactor dataloading

--- pytorch_lightning/trainer/data_loading_mixin.py
+++ pytorch_lightning/trainer/data_loading_mixin.py
@@ -21,9 +21,8 @@ class TrainerDataLoadingMixin(object):
         :param model:
         :return:
         """
-        self.train_dataloader = model.train_dataloader
+        self.get_train_dataloader = model.train_dataloader
 
-        # determine number of training batches
         # determine number of training batches
         if isinstance(self.get_train_dataloader(), IterableDataset):
             self.nb_training_batches = float('inf')
@@ -65,7 +64,7 @@ class TrainerDataLoadingMixin(object):
         :param model:
         :return:
         """
-        self.val_dataloader = model.val_dataloader
+        self.get_val_dataloader = model.val_dataloader
 
         # enable multiple validation loaders
         have_val_loaders = self.val_dataloader is not None
@@ -109,7 +108,7 @@ class TrainerDataLoadingMixin(object):
         :return:
         """
 
-        self.test_dataloader = model.test_dataloader
+        self.get_test_dataloader = model.test_dataloader
 
         # handle returning an actual dataloader instead of a list of loaders
         have_test_loaders = self.test_dataloader is not None
