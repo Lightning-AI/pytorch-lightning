afbdf6f7f637aba74a32a7939f4e210d3cc4a67b
William Falcon <falc@fb.com>
2019-10-31 17:18:30 -0400

    added training_end

--- pytorch_lightning/trainer/train_loop_mixin.py
+++ pytorch_lightning/trainer/train_loop_mixin.py
@@ -300,4 +300,8 @@ class TrainerTrainLoopMixin(object):
 
         # format and reduce outputs accordingly
         output = self.process_output(output, train=True)
+
+        import pdb
+        pdb.set_trace()
+
         return output
