9d7b421b8f53d7505715f466db59c02d65033f1d
Yongrae Jo <dreamgonfly@gmail.com>
2019-11-03 23:10:32 +0900

    #452 Use subprocess.run

--- pytorch_lightning/root_module/memory.py
+++ pytorch_lightning/root_module/memory.py
@@ -224,13 +224,17 @@ def get_gpu_memory_map():
         Keys are device ids as integers.
         Values are memory usage as integers in MB.
     """
-    result = subprocess.check_output(
+    result = subprocess.run(
         [
-            'nvidia-smi', '--query-gpu=memory.used',
-            '--format=csv,nounits,noheader'
-        ], encoding='utf-8')
+            'nvidia-smi',
+            '--query-gpu=memory.used',
+            '--format=csv,nounits,noheader',
+        ],
+        encoding='utf-8',
+        capture_output=True,
+        check=True)
     # Convert lines into a dictionary
-    gpu_memory = [int(x) for x in result.strip().split('\n')]
+    gpu_memory = [int(x) for x in result.stdout.strip().split('\n')]
     gpu_memory_map = {}
     for k, v in zip(range(len(gpu_memory)), gpu_memory):
         k = f'gpu_{k}'
