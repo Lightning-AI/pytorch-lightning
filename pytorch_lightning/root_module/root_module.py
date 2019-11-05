74fb41783ff109a402013931667ab48ddb072af4
Ir1dXD <sirius.caffrey@gmail.com>
2019-11-05 21:43:21 +0800

    change print to logging (#457)
    * change print to logging
    
    * always use logging.info
    
    * use f-strings
    
    * update code style
    
    * set logging configs
    
    * remove unused code

--- pytorch_lightning/root_module/root_module.py
+++ pytorch_lightning/root_module/root_module.py
@@ -10,6 +10,7 @@ from pytorch_lightning.root_module.hooks import ModelHooks
 from pytorch_lightning.root_module.memory import ModelSummary
 from pytorch_lightning.root_module.model_saving import ModelIO
 from pytorch_lightning.trainer.trainer_io import load_hparams_from_tags_csv
+import logging
 
 
 class LightningModule(GradInformation, ModelIO, ModelHooks):
@@ -240,7 +241,7 @@ class LightningModule(GradInformation, ModelIO, ModelHooks):
 
     def summarize(self, mode):
         model_summary = ModelSummary(self, mode=mode)
-        print(model_summary)
+        logging.info(model_summary)
 
     def freeze(self):
         for param in self.parameters():
