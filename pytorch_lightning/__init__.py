978519fc33fd70125ff9f2e083e293099908b0fd
William Falcon <waf2107@columbia.edu>
2019-11-06 14:56:51 -0500

    release v0.5.3

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.5.3'
 __author__ = ' William Falcon et al.'
 __author_email__ = 'waf2107@columbia.edu'
 __license__ = 'Apache-2.0'
-__homepage__ = 'https://github.com/williamFalcon/pytorch-lightning',
+__homepage__ = 'https://github.com/williamFalcon/pytorch-lightning'
 __docs__ = """# PyTorch Lightning
 
 The lightweight PyTorch wrapper for ML researchers. Scale your models. Write less boilerplate.
