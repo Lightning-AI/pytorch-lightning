5142a9610e2db4b957851271aa6931042aa362bc
ctlaltdefeat <jgradstein@gmail.com>
2019-11-26 02:08:31 +0200

    Update logging_mixin.py

--- pytorch_lightning/trainer/logging_mixin.py
+++ pytorch_lightning/trainer/logging_mixin.py
@@ -72,7 +72,8 @@ class TrainerLoggingMixin(object):
             callback_metrics = self.reduce_distributed_output(callback_metrics, nb_gpus)
 
         for k, v in callback_metrics.items():
-            callback_metrics[k] = v.item()
+            if isinstance(v, torch.Tensor):
+                callback_metrics[k] = v.item()
 
         # ---------------
         # EXTRACT PROGRESS BAR KEYS
