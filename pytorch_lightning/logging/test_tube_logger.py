1d949c1bab4d825a7a4a5ab0065256f4b1a83a24
Jiri Borovec <jirka.borovec@seznam.cz>
2020-01-09 22:34:50 +0100

    update version in deprecated msg

--- pytorch_lightning/logging/test_tube_logger.py
+++ pytorch_lightning/logging/test_tube_logger.py
@@ -1,10 +1,10 @@
 """
-.. warning:: `test_tube_logger` module has been renamed to `test_tube` since v0.5.3 and will be removed in v0.8.0
+.. warning:: `test_tube_logger` module has been renamed to `test_tube` since v0.6.0 and will be removed in v0.8.0
 """
 
 import warnings
 
-warnings.warn("`test_tube_logger` module has been renamed to `test_tube` since v0.5.3"
+warnings.warn("`test_tube_logger` module has been renamed to `test_tube` since v0.6.0"
               " and will be removed in v0.8.0", DeprecationWarning)
 
 from pytorch_lightning.logging.test_tube import TestTubeLogger  # noqa: E402
