ec7fc97857f8c100d8e9ff341ec1e64612ff53a0
Boris Dayma <boris.dayma@gmail.com>
2020-01-13 21:25:27 -0600

    Feature: wandb logger (#627)
    * Basic wandb support
    
    * refactor(wandb): remove unused variables and document logger
    
    * docs(wandb): explain how to use WandbLogger
    
    * test(wandb): add tests for WandbLogger
    
    * feat(wandb): add save_dir
    
    * fix(wandb): allow pickle of logger
    
    * fix(wandb): save logs in custom directory
    
    * test(wandb): test import
    
    * docs(wandb): simplify docstring and use doctest
    
    * test: increase number of epochs for satisfactory accuracy
    
    * test(test_load_model_from_checkpoint): ensure we load last checkpoint
    
    Co-authored-by: Chris Van Pelt <vanpelt@wandb.com>
    Co-authored-by: William Falcon <waf2107@columbia.edu>

--- pytorch_lightning/logging/__init__.py
+++ pytorch_lightning/logging/__init__.py
@@ -181,6 +181,10 @@ except ImportError:
     pass
 
 try:
+    from .wandb import WandbLogger
+except ImportError:
+    pass
+try:
     # needed to prevent ImportError and duplicated logs.
     environ["COMET_DISABLE_AUTO_LOGGING"] = "1"
 
