3168d493ab26787eb6011112a96bdea47688691a
MartinPernus <36865527+MartinPernus@users.noreply.github.com>
2020-01-14 04:54:06 +0100

    log named parameters (#660)

--- pytorch_lightning/core/grads.py
+++ pytorch_lightning/core/grads.py
@@ -10,7 +10,7 @@ class GradInformation(nn.Module):
     def grad_norm(self, norm_type):
         results = {}
         total_norm = 0
-        for i, p in enumerate(self.parameters()):
+        for name, p in self.named_parameters():
             if p.requires_grad:
                 try:
                     param_norm = p.grad.data.norm(norm_type)
@@ -18,7 +18,7 @@ class GradInformation(nn.Module):
                     norm = param_norm ** (1 / norm_type)
 
                     grad = round(norm.data.cpu().numpy().flatten()[0], 3)
-                    results['grad_{}_norm_{}'.format(norm_type, i)] = grad
+                    results['grad_{}_norm_{}'.format(norm_type, name)] = grad
                 except Exception:
                     # this param had no grad
                     pass
