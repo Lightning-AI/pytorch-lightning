db6b4047488a01a38d0ffad3b70cf644f28c3ef4
Jirka Borovec <Borda@users.noreply.github.com>
2020-01-14 04:09:47 +0100

    CI pass (#671)
    * fix pillow in test
    
    * test acc
    
    * update version in deprecated msg

--- pytorch_lightning/root_module/__init__.py
+++ pytorch_lightning/root_module/__init__.py
@@ -1,10 +1,10 @@
 """
-.. warning:: `root_module` package has been renamed to `core` since v0.5.3 and will be removed in v0.8.0
+.. warning:: `root_module` package has been renamed to `core` since v0.6.0 and will be removed in v0.8.0
 """
 
 import warnings
 
-warnings.warn("`root_module` package has been renamed to `core` since v0.5.3"
+warnings.warn("`root_module` package has been renamed to `core` since v0.6.0"
               " and will be removed in v0.8.0", DeprecationWarning)
 
 from pytorch_lightning.core import (  # noqa: E402
