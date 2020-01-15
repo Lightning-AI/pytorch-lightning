e16bd11001b9799462c8ef854098e97724ddf980
Jiri Borovec <jirka.borovec@seznam.cz>
2020-01-15 10:22:48 +0100

    fix typo

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -135,7 +135,7 @@ class LightningModule(ABC, GradInformation, ModelIO, ModelHooks):
 
                 logger_logs = {'training_loss': loss} # optional (MUST ALL BE TENSORS)
 
-                # if using TestTubeLogger or TensorboardLogger you can nest scalars
+                # if using TestTubeLogger or TensorBoardLogger you can nest scalars
                 logger_logs = {'losses': logger_logs} # optional (MUST ALL BE TENSORS)
 
                 output = {
