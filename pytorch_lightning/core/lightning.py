6a414195fd1f99f285cd355afee6314a20c3f762
William Falcon <waf2107@columbia.edu>
2020-01-16 13:36:48 -0500

    working on trainer docs

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -1,13 +1,4 @@
-"""
-The LightningModule is the "system recipe." It groups the following in one file:
-    - computational system definition
-    - computations done on forward
-    - training loop
-    - validation loop
-    - testing loop
-    - train, val, test dataloaders
-    - optimizers
-"""
+
 
 import os
 import warnings
@@ -18,69 +9,69 @@ from argparse import Namespace
 
 import torch
 import torch.distributed as dist
-
+# 
 from pytorch_lightning.core.decorators import data_loader
 from pytorch_lightning.core.grads import GradInformation
 from pytorch_lightning.core.hooks import ModelHooks
-from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.core.saving import ModelIO
+from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.trainer.training_io import load_hparams_from_tags_csv
 from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
 
 
 class LightningModule(ABC, GradInformation, ModelIO, ModelHooks):
-    """
-    A LightningModule has the following properties which you can access at any time
-
-    **logger**
-    A reference to the logger you passed into trainer.
-    Passing a logger is optional. If you don't pass one in, Lightning will create one
-     for you automatically. This logger saves logs to `/os.getcwd()/lightning_logs`::
-
-        Trainer(logger=your_logger)
-
-
-    Call it from anywhere in your LightningModule to add metrics, images, etc...
-     whatever your logger supports.
-
-    Here is an example using the TestTubeLogger (which is a wrapper
-     on 'PyTorch SummaryWriter <https://pytorch.org/docs/stable/tensorboard.html>`_
-     with versioned folder structure).
-
-    .. code-block:: python
-
-        # if logger is a tensorboard logger or TestTubeLogger
-        self.logger.experiment.add_embedding(...)
-        self.logger.experiment.log({'val_loss': 0.9})
-        self.logger.experiment.add_scalars(...)
-
-
-    **trainer**
-    Last resort access to any state the trainer has.
-     Changing certain properties here could affect your training run.
-
-    .. code-block:: python
-
-        self.trainer.optimizers
-        self.trainer.current_epoch
-        ...
-
-    Debugging
-    ---------
-
-    The LightningModule also offers these tricks to help debug.
-
-    **example_input_array**
-
-    In the LightningModule init, you can set a dummy tensor for this property
-    to get a print out of sizes coming into and out of every layer.
-
-    .. code-block:: python
-
-        def __init__(self):
-            # put the dimensions of the first input to your system
-            self.example_input_array = torch.rand(5, 28 * 28)
-    """
+    # """
+    # A LightningModule has the following properties which you can access at any time
+    # 
+    # **logger**
+    # A reference to the logger you passed into trainer.
+    # Passing a logger is optional. If you don't pass one in, Lightning will create one
+    #  for you automatically. This logger saves logs to `/os.getcwd()/lightning_logs`::
+    # 
+    #     Trainer(logger=your_logger)
+    # 
+    # 
+    # Call it from anywhere in your LightningModule to add metrics, images, etc...
+    #  whatever your logger supports.
+    # 
+    # Here is an example using the TestTubeLogger (which is a wrapper
+    #  on 'PyTorch SummaryWriter <https://pytorch.org/docs/stable/tensorboard.html>`_
+    #  with versioned folder structure).
+    # 
+    # .. code-block:: python
+    # 
+    #     # if logger is a tensorboard logger or TestTubeLogger
+    #     self.logger.experiment.add_embedding(...)
+    #     self.logger.experiment.log({'val_loss': 0.9})
+    #     self.logger.experiment.add_scalars(...)
+    # 
+    # 
+    # **trainer**
+    # Last resort access to any state the trainer has.
+    #  Changing certain properties here could affect your training run.
+    # 
+    # .. code-block:: python
+    # 
+    #     self.trainer.optimizers
+    #     self.trainer.current_epoch
+    #     ...
+    # 
+    # Debugging
+    # ---------
+    # 
+    # The LightningModule also offers these tricks to help debug.
+    # 
+    # **example_input_array**
+    # 
+    # In the LightningModule init, you can set a dummy tensor for this property
+    # to get a print out of sizes coming into and out of every layer.
+    # 
+    # .. code-block:: python
+    # 
+    #     def __init__(self):
+    #         # put the dimensions of the first input to your system
+    #         self.example_input_array = torch.rand(5, 28 * 28)
+    # """
 
     def __init__(self, *args, **kwargs):
         super(LightningModule, self).__init__(*args, **kwargs)
