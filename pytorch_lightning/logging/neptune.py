f82def41f24cf76baa604d3a91beba9fe17d63ee
William Falcon <waf2107@columbia.edu>
2020-01-16 17:29:13 -0500

    added loggers

--- pytorch_lightning/logging/neptune.py
+++ pytorch_lightning/logging/neptune.py
@@ -57,34 +57,67 @@ class NeptuneLogger(LightningLoggerBase):
     def __init__(self, api_key=None, project_name=None, offline_mode=False,
                  experiment_name=None, upload_source_files=None,
                  params=None, properties=None, tags=None, **kwargs):
-        """Initialize a neptune.ml logger.
-        Requires either an API Key (online mode) or a local directory path (offline mode)
+        r"""
 
-        :param str|None api_key: Required in online mode. Neputne API token, found on https://neptune.ml.
-           Read how to get your API key https://docs.neptune.ml/python-api/tutorials/get-started.html#copy-api-token.
-        :param str project_name: Required in online mode. Qualified name of a project in a form of
-           "namespace/project_name" for example "tom/minst-classification".
-           If None, the value of NEPTUNE_PROJECT environment variable will be taken.
-           You need to create the project in https://neptune.ml first.
-        :param bool offline_mode: Optional default False. If offline_mode=True no logs will be send to neptune.
-           Usually used for debug purposes.
-        :param str|None experiment_name: Optional. Editable name of the experiment.
-           Name is displayed in the experiment’s Details (Metadata section) and in experiments view as a column.
-        :param list|None upload_source_files: Optional. List of source files to be uploaded.
-           Must be list of str or single str. Uploaded sources are displayed in the experiment’s Source code tab.
-           If None is passed, Python file from which experiment was created will be uploaded.
-           Pass empty list ([]) to upload no files. Unix style pathname pattern expansion is supported.
-           For example, you can pass '*.py' to upload all python source files from the current directory.
-           For recursion lookup use '**/*.py' (for Python 3.5 and later). For more information see glob library.
-        :param dict|None params: Optional. Parameters of the experiment. After experiment creation params are read-only.
-           Parameters are displayed in the experiment’s Parameters section and each key-value pair can be
-           viewed in experiments view as a column.
-        :param dict|None properties: Optional default is {}. Properties of the experiment.
-           They are editable after experiment is created. Properties are displayed in the experiment’s Details and
-           each key-value pair can be viewed in experiments view as a column.
-        :param list|None tags: Optional default []. Must be list of str. Tags of the experiment.
-           They are editable after experiment is created (see: append_tag() and remove_tag()).
-           Tags are displayed in the experiment’s Details and can be viewed in experiments view as a column.
+        Initialize a neptune.ml logger.
+
+        .. note:: Requires either an API Key (online mode) or a local directory path (offline mode)
+
+        .. code-block:: python
+
+            # ONLINE MODE
+            from pytorch_lightning.logging import NeptuneLogger
+            # arguments made to NeptuneLogger are passed on to the neptune.experiments.Experiment class
+
+            neptune_logger = NeptuneLogger(
+                api_key=os.environ["NEPTUNE_API_TOKEN"],
+                project_name="USER_NAME/PROJECT_NAME",
+                experiment_name="default", # Optional,
+                params={"max_epochs": 10}, # Optional,
+                tags=["pytorch-lightning","mlp"] # Optional,
+            )
+            trainer = Trainer(max_epochs=10, logger=neptune_logger)
+
+        .. code-block:: python
+
+            # OFFLINE MODE
+            from pytorch_lightning.logging import NeptuneLogger
+            # arguments made to NeptuneLogger are passed on to the neptune.experiments.Experiment class
+
+            neptune_logger = NeptuneLogger(
+                project_name="USER_NAME/PROJECT_NAME",
+                experiment_name="default", # Optional,
+                params={"max_epochs": 10}, # Optional,
+                tags=["pytorch-lightning","mlp"] # Optional,
+            )
+            trainer = Trainer(max_epochs=10, logger=neptune_logger)
+
+        Args:
+            api_key (str|None): Required in online mode. Neputne API token, found on https://neptune.ml.
+                Read how to get your API key https://docs.neptune.ml/python-api/tutorials/get-started.html#copy-api-token.
+            project_name (str): Required in online mode. Qualified name of a project in a form of
+               "namespace/project_name" for example "tom/minst-classification".
+               If None, the value of NEPTUNE_PROJECT environment variable will be taken.
+               You need to create the project in https://neptune.ml first.
+            offline_mode (bool): Optional default False. If offline_mode=True no logs will be send to neptune.
+               Usually used for debug purposes.
+            experiment_name (str|None): Optional. Editable name of the experiment.
+               Name is displayed in the experiment’s Details (Metadata section) and in experiments view as a column.
+            upload_source_files (list|None): Optional. List of source files to be uploaded.
+               Must be list of str or single str. Uploaded sources are displayed in the experiment’s Source code tab.
+               If None is passed, Python file from which experiment was created will be uploaded.
+               Pass empty list ([]) to upload no files. Unix style pathname pattern expansion is supported.
+               For example, you can pass '*.py' to upload all python source files from the current directory.
+               For recursion lookup use '**/*.py' (for Python 3.5 and later). For more information see glob library.
+            params (dict|None): Optional. Parameters of the experiment. After experiment creation params are read-only.
+               Parameters are displayed in the experiment’s Parameters section and each key-value pair can be
+               viewed in experiments view as a column.
+            properties (dict|None): Optional default is {}. Properties of the experiment.
+               They are editable after experiment is created. Properties are displayed in the experiment’s Details and
+               each key-value pair can be viewed in experiments view as a column.
+            tags (list|None): Optional default []. Must be list of str. Tags of the experiment.
+               They are editable after experiment is created (see: append_tag() and remove_tag()).
+               Tags are displayed in the experiment’s Details and can be viewed in experiments view as a column.
         """
         super().__init__()
         self.api_key = api_key
