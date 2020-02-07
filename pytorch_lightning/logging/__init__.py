5218bd67d07bc0947680f376dbae7d2527dc88c9
Tullie Murrell <tulliemurrell@gmail.com>
2020-02-07 10:56:44 -0800

    Fix backwards compatibility for logging

--- pytorch_lightning/logging/__init__.py
+++ pytorch_lightning/logging/__init__.py
@@ -8,6 +8,3 @@ warnings.warn("`logging` package has been renamed to `loggers` since v0.6.1"
               " and will be removed in v0.8.0", DeprecationWarning)
 
 from pytorch_lightning.loggers import *  # noqa: F403
-from pytorch_lightning.loggers import (  # noqa: E402
-    base, comet, mlflow, neptune, tensorboard, test_tube, wandb
-)
