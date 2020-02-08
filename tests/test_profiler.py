01ce73cef9c2c62c18796e14740d048b7e0fdbfc
Jiri Borovec <jirka.borovec@seznam.cz>
2020-02-08 19:45:49 +0100

    user relative tol

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -22,9 +22,9 @@ def test_simple_profiler():
 
     # different environments have different precision when it comes to time.sleep()
     # see: https://github.com/PyTorchLightning/pytorch-lightning/issues/796
-    np.testing.assert_allclose(p.recorded_durations["a"], [3, 1], atol=0.1)
-    np.testing.assert_allclose(p.recorded_durations["b"], [2], atol=0.1)
-    np.testing.assert_allclose(p.recorded_durations["c"], [1], atol=0.1)
+    np.testing.assert_allclose(p.recorded_durations["a"], [3, 1], rtol=0.2)
+    np.testing.assert_allclose(p.recorded_durations["b"], [2], rtol=0.2)
+    np.testing.assert_allclose(p.recorded_durations["c"], [1], rtol=0.2)
 
 
 def test_advanced_profiler():
@@ -48,8 +48,8 @@ def test_advanced_profiler():
     # different environments have different precision when it comes to time.sleep()
     # see: https://github.com/PyTorchLightning/pytorch-lightning/issues/796
     a_duration = _get_duration(p.profiled_actions["a"])
-    np.testing.assert_allclose(a_duration, [4], atol=0.5)
+    np.testing.assert_allclose(a_duration, [4], rtol=0.2)
     b_duration = _get_duration(p.profiled_actions["b"])
-    np.testing.assert_allclose(b_duration, [2], atol=0.5)
+    np.testing.assert_allclose(b_duration, [2], rtol=0.2)
     c_duration = _get_duration(p.profiled_actions["c"])
-    np.testing.assert_allclose(c_duration, [1], atol=0.5)
+    np.testing.assert_allclose(c_duration, [1], rtol=0.2)
