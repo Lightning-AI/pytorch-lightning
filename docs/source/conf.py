467192159b14ea001738b87b870476948c230330
Hanbyul Kim <hahanbyul@gmail.com>
2020-02-12 00:39:50 +0900

    refer explicit LightningModule

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,7 +123,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['*.test_*']
+exclude_patterns = ['*.test_*py']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
