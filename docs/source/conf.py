3479c595d600fcbece5f35b7c26eea3abd93a988
Hanbyul Kim <hahanbyul@gmail.com>
2020-02-12 00:48:45 +0900

    restore exclude patterns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,7 +125,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['*.test_*py']
+exclude_patterns = ['*.test_*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
