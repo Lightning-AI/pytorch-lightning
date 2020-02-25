b70952d6525a2b25a200d88a9a8565cf37e66680
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-02-25 16:36:44 +0100

    relax hparams (#919)
    relax model loading hparams
    
    
    test wip
    
    
    wip
    
    
    fix warning
    
    
    finish test
    
    
    remove unused import

--- tests/models/__init__.py
+++ tests/models/__init__.py
@@ -26,3 +26,21 @@ class LightningTestModel(LightningValidationMixin, LightningTestMixin, Lightning
 
     def on_training_metrics(self, logs):
         logs['some_tensor_to_test'] = torch.rand(1)
+
+
+class LightningTestModelWithoutHyperparametersArg(LightningTestModel):
+    """ without hparams argument in constructor """
+
+    def __init__(self):
+        import tests.models.utils as tutils
+
+        # the user loads the hparams in some other way
+        hparams = tutils.get_hparams()
+        super().__init__(hparams)
+
+
+class LightningTestModelWithUnusedHyperparametersArg(LightningTestModelWithoutHyperparametersArg):
+    """ has hparams argument in constructor but is not used """
+
+    def __init__(self, hparams):
+        super().__init__()
