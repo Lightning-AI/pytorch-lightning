cf20be9e652f68cbe9ad3bdc9c0e4b66df9ba71b
Sasha <arush@g.harvard.edu>
2020-02-25 22:28:28 -0500

    .

--- pytorch_lightning/trainer/data_loading.py
+++ pytorch_lightning/trainer/data_loading.py
@@ -176,8 +176,8 @@ class TrainerDataLoadingMixin(ABC):
         # support IterableDataset for train data
         self.is_iterable_train_dataloader = (
             EXIST_ITER_DATASET
-            and hasattr(self.train_dataloader, "dataset")
-            and isinstance(self.train_dataloader.dataset, IterableDataset)
+            and (not hasattr(self.train_dataloader, "dataset") or
+                 isinstance(self.train_dataloader.dataset, IterableDataset))
         )
         if self.is_iterable_dataloader(self.train_dataloader) and not isinstance(self.val_check_interval, int):
             m = '''
@@ -190,8 +190,8 @@ class TrainerDataLoadingMixin(ABC):
     def is_iterable_dataloader(self, dataloader):
         return (
             EXIST_ITER_DATASET
-            and hasattr(self.train_dataloader, "dataset")
-            and isinstance(dataloader.dataset, IterableDataset)
+            and (not hasattr(self.train_dataloader, "dataset")
+            or isinstance(dataloader.dataset, IterableDataset))
         )
 
     def reset_val_dataloader(self, model):
