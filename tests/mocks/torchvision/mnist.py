5ef53a7905dbbf760a20315318d3a246f5076d25
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-02-29 18:56:17 +0100

    fix line too long

--- tests/mocks/torchvision/mnist.py
+++ tests/mocks/torchvision/mnist.py
@@ -38,10 +38,22 @@ class MNIST(Dataset):
     """
 
     resources = [
-        ("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
-        ("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz", "d53e105ee54ea40749a09fcbcd1e9432"),
-        ("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz", "9fb629c4189551a2d022fa330f9573f3"),
-        ("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz", "ec29112dd5afa0611ce80d1b7f02629c")
+        (
+            "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz",
+            "f68b3c2dcbeaaa9fbdd348bbdeb94873"
+        ),
+        (
+            "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
+            "d53e105ee54ea40749a09fcbcd1e9432"
+        ),
+        (
+            "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
+            "9fb629c4189551a2d022fa330f9573f3"
+        ),
+        (
+            "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz",
+            "ec29112dd5afa0611ce80d1b7f02629c"
+        )
     ]
 
     training_file = 'training.pt'
