59000f065e38eb4996995a672f99d3d128698ced
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-03-01 00:09:58 +0100

    simplify MNIST a lot and download directly the .pt files

--- tests/mocks/torchvision/mnist.py
+++ tests/mocks/torchvision/mnist.py
@@ -2,22 +2,15 @@
 Customized MNIST Dataset for testing Pytorch Lightning without the torchvision
 dependency.
 
-Copied from
+Part of the code was copied from
 https://github.com/pytorch/vision/blob/build/v0.5.0/torchvision/datasets/mnist.py
 """
-
-from __future__ import print_function
-
-
-from PIL import Image
 import os
-import os.path
-import numpy as np
-import torch
-import codecs
-from torch.utils.data import Dataset
+import urllib.request
 
-from tests.mocks.torchvision.utils import download_and_extract_archive
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
 
 
 class MNIST(Dataset):
@@ -38,33 +31,15 @@ class MNIST(Dataset):
     """
 
     resources = [
-        (
-            "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz",
-            "f68b3c2dcbeaaa9fbdd348bbdeb94873"
-        ),
-        (
-            "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
-            "d53e105ee54ea40749a09fcbcd1e9432"
-        ),
-        (
-            "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
-            "9fb629c4189551a2d022fa330f9573f3"
-        ),
-        (
-            "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz",
-            "ec29112dd5afa0611ce80d1b7f02629c"
-        )
+        "https://pl-public-data.s3.amazonaws.com/MNIST/processed/training.pt",
+        "https://pl-public-data.s3.amazonaws.com/MNIST/processed/test.pt",
     ]
 
     training_file = 'training.pt'
     test_file = 'test.pt'
-    classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
-               '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine']
 
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
         super(MNIST, self).__init__()
-        if isinstance(root, torch._six.string_classes):
-            root = os.path.expanduser(root)
         self.root = root
         self.train = train  # training set or test set
         self.transform = transform
@@ -74,8 +49,7 @@ class MNIST(Dataset):
             self.download()
 
         if not self._check_exists():
-            raise RuntimeError('Dataset not found.' +
-                               ' You can use download=True to download it')
+            raise RuntimeError('Dataset not found.')
 
         if self.train:
             data_file = self.training_file
@@ -84,18 +58,7 @@ class MNIST(Dataset):
         self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
         img, target = self.data[index], int(self.targets[index])
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img.numpy(), mode='L')
 
         if self.transform is not None:
             img = self.transform(img)
@@ -109,10 +72,6 @@ class MNIST(Dataset):
         return len(self.data)
 
     @property
-    def raw_folder(self):
-        return os.path.join(self.root, self.__class__.__name__, 'raw')
-
-    @property
     def processed_folder(self):
         return os.path.join(self.root, self.__class__.__name__, 'processed')
 
@@ -121,10 +80,9 @@ class MNIST(Dataset):
         return {_class: i for i, _class in enumerate(self.classes)}
 
     def _check_exists(self):
-        return (os.path.exists(os.path.join(self.processed_folder,
-                                            self.training_file)) and
-                os.path.exists(os.path.join(self.processed_folder,
-                                            self.test_file)))
+        train_file = os.path.join(self.processed_folder, self.training_file)
+        test_file = os.path.join(self.processed_folder, self.test_file)
+        return os.path.exists(train_file) and os.path.exists(test_file)
 
     def download(self):
         """Download the MNIST data if it doesn't exist in processed_folder already."""
@@ -132,97 +90,10 @@ class MNIST(Dataset):
         if self._check_exists():
             return
 
-        os.makedirs(self.raw_folder, exist_ok=True)
         os.makedirs(self.processed_folder, exist_ok=True)
 
-        # download files
-        for url, md5 in self.resources:
-            filename = url.rpartition('/')[2]
-            download_and_extract_archive(
-                url, download_root=self.raw_folder, filename=filename, md5=md5
-            )
-
-        # process and save as torch files
-        print('Processing...')
-
-        training_set = (
-            read_image_file(os.path.join(self.raw_folder, 'train-images-idx3-ubyte')),
-            read_label_file(os.path.join(self.raw_folder, 'train-labels-idx1-ubyte'))
-        )
-        test_set = (
-            read_image_file(os.path.join(self.raw_folder, 't10k-images-idx3-ubyte')),
-            read_label_file(os.path.join(self.raw_folder, 't10k-labels-idx1-ubyte'))
-        )
-        with open(os.path.join(self.processed_folder, self.training_file), 'wb') as f:
-            torch.save(training_set, f)
-        with open(os.path.join(self.processed_folder, self.test_file), 'wb') as f:
-            torch.save(test_set, f)
-
-        print('Done!')
-
-    def extra_repr(self):
-        return "Split: {}".format("Train" if self.train is True else "Test")
-
-
-def get_int(b):
-    return int(codecs.encode(b, 'hex'), 16)
-
-
-def open_maybe_compressed_file(path):
-    """Return a file object that possibly decompresses 'path' on the fly.
-       Decompression occurs when argument `path` is a string and ends with '.gz' or '.xz'.
-    """
-    if not isinstance(path, torch._six.string_classes):
-        return path
-    if path.endswith('.gz'):
-        import gzip
-        return gzip.open(path, 'rb')
-    if path.endswith('.xz'):
-        import lzma
-        return lzma.open(path, 'rb')
-    return open(path, 'rb')
-
-
-def read_sn3_pascalvincent_tensor(path, strict=True):
-    """Read a SN3 file in "Pascal Vincent" format (Lush file 'libidx/idx-io.lsh').
-       Argument may be a filename, compressed filename, or file object.
-    """
-    # typemap
-    if not hasattr(read_sn3_pascalvincent_tensor, 'typemap'):
-        read_sn3_pascalvincent_tensor.typemap = {
-            8: (torch.uint8, np.uint8, np.uint8),
-            9: (torch.int8, np.int8, np.int8),
-            11: (torch.int16, np.dtype('>i2'), 'i2'),
-            12: (torch.int32, np.dtype('>i4'), 'i4'),
-            13: (torch.float32, np.dtype('>f4'), 'f4'),
-            14: (torch.float64, np.dtype('>f8'), 'f8')}
-    # read
-    with open_maybe_compressed_file(path) as f:
-        data = f.read()
-    # parse
-    magic = get_int(data[0:4])
-    nd = magic % 256
-    ty = magic // 256
-    assert nd >= 1 and nd <= 3
-    assert ty >= 8 and ty <= 14
-    m = read_sn3_pascalvincent_tensor.typemap[ty]
-    s = [get_int(data[4 * (i + 1): 4 * (i + 2)]) for i in range(nd)]
-    parsed = np.frombuffer(data, dtype=m[1], offset=(4 * (nd + 1)))
-    assert parsed.shape[0] == np.prod(s) or not strict
-    return torch.from_numpy(parsed.astype(m[2], copy=False)).view(*s)
-
-
-def read_label_file(path):
-    with open(path, 'rb') as f:
-        x = read_sn3_pascalvincent_tensor(f, strict=False)
-    assert(x.dtype == torch.uint8)
-    assert(x.ndimension() == 1)
-    return x.long()
-
-
-def read_image_file(path):
-    with open(path, 'rb') as f:
-        x = read_sn3_pascalvincent_tensor(f, strict=False)
-    assert(x.dtype == torch.uint8)
-    assert(x.ndimension() == 3)
-    return x
+        for url in self.resources:
+            # TODO: use logging
+            print('Downloading ' + url)
+            fpath = os.path.join(self.processed_folder, os.path.basename(url))
+            urllib.request.urlretrieve(url, fpath)
