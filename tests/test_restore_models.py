dfe2be2fef4d78f7ef84ca7f4401b29982e74666
William Falcon <waf2107@columbia.edu>
2020-03-02 15:53:22 -0500

    docs

--- tests/test_restore_models.py
+++ tests/test_restore_models.py
@@ -142,7 +142,6 @@ def test_load_model_from_checkpoint(tmpdir):
         assert getattr(pretrained_model.hparams, k) == v
 
     # assert weights are the same
-    import pdb; pdb.set_trace()
     for (old_name, old_p), (new_name, new_p) in zip(model.named_parameters(), pretrained_model.named_parameters()):
         assert torch.all(torch.eq(old_p, new_p)), 'loaded weights are not the same as the saved weights'
 
