58d046f2267b0a3d2fa99feef109f19befc66dd1
J. Borovec <jirka.borovec@seznam.cz>
2020-03-05 00:26:03 +0100

    pylint

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -22,7 +22,7 @@ class ModelCheckpoint(Callback):
                 # save any arbitrary metrics like and val_loss, etc in name
                 ModelCheckpoint(filepath='/my/path/{epoch}-{val_loss:.2f}-{other_metric:.2f}')
                 # saves file like: /my/path/epoch=2-val_loss=0.2_other_metric=0.3.ckpt
-                
+
         monitor (str): quantity to monitor.
         verbose (bool): verbosity mode, False or True.
         save_top_k (int): if `save_top_k == k`,
