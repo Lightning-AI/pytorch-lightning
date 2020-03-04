22a57bd8f28b54e68b6885f7b0651299f0129890
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-03-04 13:38:41 +0100

    refactor if train
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>

--- tests/datasets/mnist.py
+++ tests/datasets/mnist.py
@@ -44,10 +44,7 @@ class MNIST(Dataset):
         if not self._check_exists():
             raise RuntimeError('Dataset not found.')
 
-        if self.train:
-            data_file = self.training_file
-        else:
-            data_file = self.test_file
+        data_file = self.training_file if self.train else self.test_file
         self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
 
     def __getitem__(self, index):
