192874918afcaef80937b3fb0a597c66bb529680
J. Borovec <jirka.borovec@seznam.cz>
2020-03-05 14:05:33 +0100

    fix deprecated warnings

--- pytorch_lightning/logging/__init__.py
+++ pytorch_lightning/logging/__init__.py
@@ -1,12 +1,12 @@
 """
-.. warning:: `logging` package has been renamed to `loggers` since v0.6.1.
- The deprecated package name will be removed in v0.8.0.
+.. warning:: `logging` package has been renamed to `loggers` since v0.7.0.
+ The deprecated package name will be removed in v0.9.0.
 """
 
 import warnings
 
-warnings.warn("`logging` package has been renamed to `loggers` since v0.6.1"
-              " The deprecated package name will be removed in v0.8.0.", DeprecationWarning)
+warnings.warn("`logging` package has been renamed to `loggers` since v0.7.0"
+              " The deprecated package name will be removed in v0.9.0.", DeprecationWarning)
 
 from pytorch_lightning.loggers import *  # noqa: F403
 from pytorch_lightning.loggers import base, tensorboard  # noqa: F403
