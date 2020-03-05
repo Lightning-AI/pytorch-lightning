890364ee484917c6e1808d1104138a0fd680e558
William Falcon <waf2107@columbia.edu>
2020-03-05 07:30:13 -0500

    training_end to training_step_end

--- pytorch_lightning/trainer/evaluation_loop.py
+++ pytorch_lightning/trainer/evaluation_loop.py
@@ -261,6 +261,18 @@ class TrainerEvaluationLoopMixin(ABC):
                 # -----------------
                 output = self.evaluation_forward(model, batch, batch_idx, dataloader_idx, test_mode)
 
+                # on dp / ddp2 might still want to do something with the batch parts
+                if test_mode:
+                    if self.is_overriden('test_step_end'):
+                        model_ref = self.get_model()
+                        with self.profiler.profile('test_step_end'):
+                            output = model_ref.test_step_end(output)
+                else:
+                    if self.is_overriden('validation_step_end'):
+                        model_ref = self.get_model()
+                        with self.profiler.profile('validation_step_end'):
+                            output = model_ref.validation_step_end(output)
+
                 # track outputs for collation
                 dl_outputs.append(output)
 
@@ -283,9 +295,9 @@ class TrainerEvaluationLoopMixin(ABC):
         model = self.get_model()
 
         if test_mode and self.is_overriden('test_epoch_end'):
-            eval_results = model.test_end(outputs)
+            eval_results = model.test_epoch_end(outputs)
         elif self.is_overriden('validation_epoch_end'):
-            eval_results = model.validation_end(outputs)
+            eval_results = model.validation_epoch_end(outputs)
 
         # TODO: remove in v 1.0.0
         if test_mode and self.is_overriden('test_end'):
@@ -406,7 +418,7 @@ class TrainerEvaluationLoopMixin(ABC):
             output = model(*args)
             return output
 
-        # single GPU
+        # single GPU data transfer
         if self.single_gpu:
             # for single GPU put inputs on gpu manually
             root_gpu = 0
@@ -415,12 +427,12 @@ class TrainerEvaluationLoopMixin(ABC):
             batch = self.transfer_batch_to_gpu(batch, root_gpu)
             args[0] = batch
 
-        # TPU
+        # TPU data  transfer
         if self.use_tpu:
             batch = self.transfer_batch_to_tpu(batch)
             args[0] = batch
 
-        # CPU
+        # CPU, TPU or gpu step
         if test_mode:
             output = model.test_step(*args)
         else:
