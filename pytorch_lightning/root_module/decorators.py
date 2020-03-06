0174289d5adbd1fd2a3208d50e07fae38540e391
Jirka Borovec <Borda@users.noreply.github.com>
2020-03-06 16:35:58 +0100

    Apply suggestions from code review

--- pytorch_lightning/root_module/decorators.py
+++ pytorch_lightning/root_module/decorators.py
@@ -8,4 +8,4 @@ import warnings
 warnings.warn("`root_module.decorators` module has been renamed to `core.decorators` since v0.6.0."
               " The deprecated module name will be removed in v0.8.0.", DeprecationWarning)
 
-from pytorch_lightning.core.decorators import *  # noqa: E403
+from pytorch_lightning.core.decorators import *  # noqa: F403
