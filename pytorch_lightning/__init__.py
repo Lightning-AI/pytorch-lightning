22bfb8cb1fd54f1f58ea797d7838fd312cd7e617
William Falcon <waf2107@columbia.edu>
2020-03-06 15:32:40 -0500

    release v

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -1,6 +1,6 @@
 """Root package info."""
 
-__version__ = '0.7.0.dev'
+__version__ = '0.6.2.dev'
 __author__ = 'William Falcon et al.'
 __author_email__ = 'waf2107@columbia.edu'
 __license__ = 'Apache-2.0'
