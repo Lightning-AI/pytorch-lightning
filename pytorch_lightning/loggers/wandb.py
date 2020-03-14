79f553eecf88a0896beed5b6405ff9507fc19711
Chris Van Pelt <vanpelt@gmail.com>
2020-03-14 15:30:45 -0700

    Allow reinits in sub procs

--- pytorch_lightning/loggers/wandb.py
+++ pytorch_lightning/loggers/wandb.py
@@ -87,7 +87,7 @@ class WandbLogger(LightningLoggerBase):
                 os.environ['WANDB_MODE'] = 'dryrun'
             self._experiment = wandb.init(
                 name=self._name, dir=self._save_dir, project=self._project, anonymous=self._anonymous,
-                id=self._id, resume='allow', tags=self._tags, entity=self._entity)
+                reinit=True, id=self._id, resume='allow', tags=self._tags, entity=self._entity)
             # save checkpoints in wandb dir to upload on W&B servers
             if self._log_model:
                 self.save_dir = self._experiment.dir
