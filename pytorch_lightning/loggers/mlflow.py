7774ad980a85a0eb087f8a8fdb5ab676dbfc0899
J. Borovec <jirka.borovec@seznam.cz>
2020-03-17 22:25:20 +0100

    doctest trains

--- pytorch_lightning/loggers/mlflow.py
+++ pytorch_lightning/loggers/mlflow.py
@@ -30,6 +30,7 @@ from typing import Optional, Dict, Any, Union
 
 try:
     import mlflow
+    from mlflow.tracking import MlflowClient
 except ImportError:  # pragma: no-cover
     raise ImportError('You want to use `mlflow` logger which is not installed yet,'  # pragma: no-cover
                       ' install it with `pip install mlflow`.')
@@ -50,13 +51,13 @@ class MLFlowLogger(LightningLoggerBase):
             tags (dict): todo this param
         """
         super().__init__()
-        self._mlflow_client = mlflow.tracking.MlflowClient(tracking_uri)
+        self._mlflow_client = MlflowClient(tracking_uri)
         self.experiment_name = experiment_name
         self._run_id = None
         self.tags = tags
 
     @property
-    def experiment(self) -> mlflow.tracking.MlflowClient:
+    def experiment(self) -> MlflowClient:
         r"""
 
         Actual mlflow object. To use mlflow features do the following.
