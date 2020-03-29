12909e0a12f54eb6d5ce6486a9495328bb4b666f
Nicki Skafte <nugginea@gmail.com>
2020-03-29 14:25:14 +0200

    error_on_zero_length

--- pytorch_lightning/trainer/data_loading.py
+++ pytorch_lightning/trainer/data_loading.py
@@ -26,9 +26,14 @@ else:
 
 
 def _has_len(dataloader: DataLoader) -> bool:
+    """ Checks if a given Dataloader has __len__ method implemented i.e. if
+    it is a finite dataloader or infinite dataloader """
     try:
         # try getting the length
-        _ = len(dataloader)
+        val = len(dataloader)
+        if val == 0:
+            raise ValueError('Dataloader returned 0 length. Please make sure'
+                             ' that your Dataloader atleast returns 1 batch')
         return True
     except TypeError:
         return False
