e48a711742630fb90325817476e2257852856b06
akarnachev <a.karnachev@just-ai.com>
2020-03-30 03:43:51 +0300

    test_metrics_agg.py removed (all tested in doctrings), agg metrics refactored

--- pytorch_lightning/loggers/metrics_agg.py
+++ pytorch_lightning/loggers/metrics_agg.py
@@ -2,22 +2,17 @@ import functools
 import operator
 from typing import Dict, Callable, Mapping, Sequence
 
-MetricsT = Dict[str, float]
-MetricsAggFnT = Callable[[Sequence[MetricsT]], MetricsT]
 
-
-def merge_two_dicts(d1: Mapping, d2: Mapping, fn: Callable[[float, float], float], default_value: float = 0) -> Dict:
+def merge_dicts(dicts: Sequence[Mapping], fn: Callable[[Sequence[float]], float]) -> Dict:
     """Merges two dictionaries values with the given function.
 
     Args:
-        d1 (dict):
-            First dictionary
-        d2 (dict):
-            Second dictionary
+        dicts (list of dicts):
+            Sequence of positional dict arguments to be merged.
         fn:
-            Function which will be applied to two values from the same key of both dicts.
-        default_value (float):
-            If value is presented only in one dict, it will be aggregated with `default_value`.
+            Function which will be applied to the sequence of values from the same key of all dicts.
+            If some dict has no required key, the value from this dict-key will not
+            appear in the sequence to be aggregated.
 
     Returns (dict):
         Dictionary with merged values.
@@ -26,72 +21,55 @@ def merge_two_dicts(d1: Mapping, d2: Mapping, fn: Callable[[float, float], float
         >>> import pprint
         >>> d1 = {'a': 1.7, 'b': 2.0, 'c': 1}
         >>> d2 = {'a': 1.1, 'b': 2.2}
+        >>> d3 = {'a': 1.1, 'v': 2.3}
         >>> fn = max
-        >>> pprint.pprint(merge_two_dicts(d1, d2, fn))
-        {'a': 1.7, 'b': 2.2, 'c': 1}
+        >>> pprint.pprint(merge_dicts([d1, d2, d3], fn))
+        {'a': 1.7, 'b': 2.2, 'c': 1, 'v': 2.3}
     """
 
-    keys = set(list(d1.keys()) + list(d2.keys()))
-    dx = {k: fn([v for v in (d1.get(k), d2.get(k)) if v]) for k in keys}
+    keys = list(functools.reduce(operator.or_, [set(d.keys()) for d in dicts]))
+    dx = {k: fn([v for v in [d.get(k) for d in dicts] if v is not None]) for k in keys}
     return dx
 
 
-def metrics_agg_simple(metrics_to_agg: Sequence[MetricsT], fn: Callable[[float, float], float]) -> MetricsT:
-    """Aggregates metrics dictionaries with the given function.
+def metrics_agg_sum(metrics_to_agg: Sequence[Dict[str, float]]) -> Dict[str, float]:
+    """Aggregates metric dictionaries sequences with sum function.
 
     Args:
         metrics_to_agg (Sequence[MetricsT]):
             Sequence with metrics dictionaries to be aggregated.
-        fn: Values reduction function (check `merge_two_dicts`).
-
-    Returns (MetricsT):
-        Aggregated metrics dictionary.
 
     Examples:
         >>> import pprint
-        >>> import operator
-        >>> metrics_to_agg = [{'a': 1.7, 'b': 2.0}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1}]
-        >>> pprint.pprint(metrics_agg_simple(metrics_to_agg, operator.add))
-        {'a': 2.8, 'b': 4.3}
-    """
-    return functools.reduce(lambda d1, d2: merge_two_dicts(d1, d2, fn), metrics_to_agg)
-
-
-def metrics_agg_sum(metrics_to_agg: Sequence[MetricsT]) -> MetricsT:
-    """Aggregates metric dictionaries sequences with sum function.
-
-    Args:
-        Check `metrics_agg_simple` function for args and return description.
-
-    Examples:
-        >>> import pprint
-        >>> metrics_to_agg = [{'a': 1.7, 'b': 2.0}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1}]
+        >>> metrics_to_agg = [{'a': 1.7, 'b': 2.0}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1, 'c': 0.2}]
         >>> pprint.pprint(metrics_agg_sum(metrics_to_agg))
-        {'a': 2.8, 'b': 4.3}
+        {'a': 2.8, 'b': 4.3, 'c': 0.2}
     """
-    return metrics_agg_simple(metrics_to_agg, operator.add)
+    return merge_dicts(metrics_to_agg, sum)
 
 
-def metrics_agg_max(metrics_to_agg: Sequence[MetricsT]) -> MetricsT:
+def metrics_agg_max(metrics_to_agg: Sequence[Dict[str, float]]) -> Dict[str, float]:
     """Aggregates metric dictionaries sequences with max function.
 
     Args:
-        Check `metrics_agg_simple` function for args and return description.
+        metrics_to_agg (Sequence[MetricsT]):
+            Sequence with metrics dictionaries to be aggregated.
 
     Examples:
         >>> import pprint
-        >>> metrics_to_agg = [{'a': 1.7, 'b': 2.0}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1}]
+        >>> metrics_to_agg = [{'a': 1.7, 'b': 2.0}, {'a': 1.1, 'd': 2.2}, {'b': 0.0, 'c': 0.1}]
         >>> pprint.pprint(metrics_agg_max(metrics_to_agg))
-        {'a': 1.7, 'b': 2.2}
+        {'a': 1.7, 'b': 2.0, 'c': 0.1, 'd': 2.2}
     """
-    return metrics_agg_simple(metrics_to_agg, max)
+    return merge_dicts(metrics_to_agg, max)
 
 
-def metrics_agg_min(metrics_to_agg: Sequence[MetricsT]) -> MetricsT:
+def metrics_agg_min(metrics_to_agg: Sequence[Dict[str, float]]) -> Dict[str, float]:
     """Aggregates metric dictionaries sequences with min function.
 
     Args:
-        Check `metrics_agg_simple` function for args and return description.
+        metrics_to_agg (Sequence[MetricsT]):
+            Sequence with metrics dictionaries to be aggregated.
 
     Examples:
         >>> import pprint
@@ -99,20 +77,21 @@ def metrics_agg_min(metrics_to_agg: Sequence[MetricsT]) -> MetricsT:
         >>> pprint.pprint(metrics_agg_min(metrics_to_agg))
         {'a': 0.0, 'b': 0.1}
     """
-    return metrics_agg_simple(metrics_to_agg, min)
+    return merge_dicts(metrics_to_agg, min)
 
 
-def metrics_agg_avg(metrics_to_agg: Sequence[MetricsT]):
+def metrics_agg_avg(metrics_to_agg: Sequence[Dict[str, float]]):
     """Aggregates metric dictionaries sequences with average function.
 
     Args:
-        Check `metrics_agg_simple` function for args and return description.
+        metrics_to_agg (Sequence[MetricsT]):
+            Sequence with metrics dictionaries to be aggregated.
 
     Examples:
         >>> import pprint
-        >>> metrics_to_agg = [{'a': 1.9, 'b': 2.2}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1}]
+        >>> metrics_to_agg = [{'a': 1.9, 'b': 2.2}, {'a': 1.1, 'b': 2.2}, {'a': 0.0, 'b': 0.1, 'c': 3}]
         >>> pprint.pprint(metrics_agg_avg(metrics_to_agg))
-        {'a': 1.0, 'b': 1.5}
+        {'a': 1.0, 'b': 1.5, 'c': 1.0}
     """
     agg_mets = metrics_agg_sum(metrics_to_agg)
     agg_mets = {k: v / len(metrics_to_agg) for k, v in agg_mets.items()}
