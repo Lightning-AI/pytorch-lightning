49403a0db5f1af18d289717b4d057a0f9904af38
William Falcon <waf2107@columbia.edu>
2020-03-31 10:04:58 -0400

    removes need to unsqueeze from dp

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -163,6 +163,9 @@ def parallel_apply(modules, inputs, kwargs_tup=None, devices=None):  # pragma: n
 
                 else:
                     output = module.validation_step(*input, **kwargs)
+
+                if module.use_dp or module.use_ddp2:
+                    output['loss'] = output['loss'].unsqueeze()
                 # ---------------
 
             with lock:
