7e7de87f47420dc91726622a484ff895728be973
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-31 16:53:09 +0200

    hook into device and dtype changes

--- pytorch_lightning/metrics/metric.py
+++ pytorch_lightning/metrics/metric.py
@@ -1,12 +1,11 @@
 import numbers
 from abc import ABC, abstractmethod
-from collections import Mapping, Sequence
-from functools import partial
 from typing import Union, Any, Optional
 
 import torch
 import torch.distributed
-from torch.utils.data._utils.collate import np_str_obj_array_pattern
+
+from pytorch_lightning.utilities.apply_to_collection import apply_to_collection
 
 __all__ = ['BaseMetric']
 
@@ -53,9 +52,12 @@ class BaseMetric(torch.nn.Module, ABC):
         raise NotImplementedError
 
     def __call__(self, *args, **kwargs) -> torch.Tensor:
-        return _sync_collections(super().__call__(*args, **kwargs),
-                                 group=self.reduce_group,
-                                 reduce_op=self.reduce_op)
+        return apply_to_collection(
+            super().__call__(*args, **kwargs),
+            torch.Tensor, _sync_ddp_to_device_type,
+            device=self.device, dtype=self.dtype,
+            group=self.reduce_group,
+            reduce_op=self.reduce_op)
 
     def to(self, *args, **kwargs):
         """Moves and/or casts the parameters and buffers.
@@ -198,14 +200,19 @@ class BaseMetric(torch.nn.Module, ABC):
         return super().half()
 
 
-def _sync_ddp(result: Union[torch.Tensor, numbers.Number],
-              group: Any = torch.distributed.group.WORLD,
-              reduce_op: torch.distributed.ReduceOp = torch.distributed.ReduceOp.SUM) -> torch.Tensor:
+def _sync_ddp_to_device_type(result: Union[torch.Tensor, numbers.Number],
+                             device: Union[str, torch.device],
+                             dtype: Union[str, torch.dtype],
+                             group: Any = torch.distributed.group.WORLD,
+                             reduce_op: torch.distributed.ReduceOp = torch.distributed.ReduceOp.SUM,
+                             ) -> torch.Tensor:
     """
     Function to reduce the tensors from several ddp processes to one master process
 
     Args:
         result: the value to sync and reduce (typically tensor or number)
+        device: the device to put the synced and reduced value to
+        dtype: the datatype to convert the synced and reduced value to
         group: the process group to gather results from. Defaults to all processes (world)
         reduce_op: the reduction operation. Defaults to sum
 
@@ -224,49 +231,4 @@ def _sync_ddp(result: Union[torch.Tensor, numbers.Number],
         torch.distributed.all_reduce(result, op=reduce_op, group=group,
                                      async_op=False)
 
-    return result
-
-
-def _sync_collections(result: Union[torch.Tensor, numbers.Number,
-                                    Mapping, Sequence],
-                      group: Any = torch.distributed.group.WORLD,
-                      reduce_op: torch.distributed.ReduceOp = torch.distributed.ReduceOp.SUM
-                      ) -> Union[torch.Tensor, numbers.Number,
-                                 Mapping, Sequence]:
-    """
-    Recursively applies sync_ddp to collections
-
-    Args:
-        result: Tensor or Number or Mapping or Sequence holding the values to be reduced
-        group: the process group to gather results from. Defaults to all processes (world)
-        reduce_op: the reduction operation. Defaults to sum
-
-    Returns:
-        the reduced collection
-
-    """
-    # function adapted from torch.utils.data._utils.collate
-    elem_type = type(result)
-
-    func = partial(_sync_collections, group=group, reduce_op=reduce_op)
-
-    # convert numpy to tensor if possible
-    if elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        # array not of string classes and object
-        if elem_type.__name__ != 'ndarray' \
-                or np_str_obj_array_pattern.search(result.dtype.str) is None:
-            result = torch.as_tensor(result)
-
-    if isinstance(result, (torch.Tensor, numbers.Number)):
-        return _sync_ddp(result, group=group, reduce_op=reduce_op)
-
-    elif isinstance(result, Mapping):
-        return elem_type({key: func(result[key]) for key in result})
-    elif isinstance(result, tuple) and hasattr(result, '_fields'):  # namedtuple
-        return elem_type(*(func(r) for r in result))
-    elif isinstance(result, Sequence) and not isinstance(result, str):
-        return elem_type([func(r) for r in result])
-    # not possible to reduce this type
-    else:
-        return result
+    return result.to(device=device, dtype=dtype)
