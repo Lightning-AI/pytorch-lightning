aecff01f3e7d25d72c812d19b5addbd8f969ef2c
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-04-03 09:50:38 +0200

    Apply suggestions from code review
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>

--- tests/metrics/convertors.py
+++ tests/metrics/convertors.py
@@ -4,9 +4,9 @@ import torch
 import torch.distributed as dist
 
 import tests.base.utils as tutils
-from pytorch_lightning.metrics.convertors import _apply_to_inputs, _apply_to_outputs, \
-    _convert_to_tensor, _convert_to_numpy, _numpy_metric_conversion, \
-    _tensor_metric_conversion, _sync_ddp, tensor_metric, numpy_metric
+from pytorch_lightning.metrics.convertors import (
+    _apply_to_inputs, _apply_to_outputs, _convert_to_tensor, _convert_to_numpy,
+    _numpy_metric_conversion, tensor_metric_conversion, _sync_ddp, tensor_metric, numpy_metric)
 
 
 def test_apply_to_inputs():
