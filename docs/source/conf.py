72cdf3a755ad98e6fddcc90b468b125eb4bda510
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-04-04 04:50:54 +0200

    remove comment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -251,7 +251,6 @@ epub_exclude_files = ['search.html']
 
 # -- Options for intersphinx extension ---------------------------------------
 
-# Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'torch': ('https://pytorch.org/docs/stable/', None),
