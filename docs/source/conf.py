c45541695b519a24f0d61f052567eb31ea6f5d32
Jirka Borovec <Borda@users.noreply.github.com>
2020-04-05 20:27:36 +0200

    Update docs/source/conf.py
    Co-Authored-By: Adrian Wälchli <aedu.waelchli@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ release = pytorch_lightning.__version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '2.4'
+needs_sphinx = '2.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
