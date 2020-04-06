4ed3027309fe1882554e9b7ffe33f1aa92c88106
Roshan Rao <roshan_rao@berkeley.edu>
2020-04-06 05:13:24 -0700

    Set precision=16 when use_amp is passed as True (#1145)
    * Set precision=16 when use_amp is passed as True
    
    * Update CHANGELOG.md
    
    * add use_amp to deprecated API
    
    * Update trainer.py
    
    * Update trainer.py
    
    * move the use_amp attribute to deprecated API
    
    * move use_amp deprecation back to Trainer's __init__
    
    * drop unsed
    
    * drop deprecated
    
    * reorder imports
    
    * typing
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: William Falcon <waf2107@columbia.edu>
    Co-authored-by: J. Borovec <jirka.borovec@seznam.cz>

--- pytorch_lightning/trainer/auto_mix_precision.py
+++ pytorch_lightning/trainer/auto_mix_precision.py
@@ -14,19 +14,21 @@ class TrainerAMPMixin(ABC):
 
     # this is just a summary on variables used in this abstract class,
     #  the proper values/initialisation should be done in child class
-    use_amp: bool
+    precision: int
 
     def init_amp(self, use_amp):
-        self.use_amp = use_amp and APEX_AVAILABLE
-        if self.use_amp:
-            log.info('Using 16bit precision.')
-
         if use_amp and not APEX_AVAILABLE:  # pragma: no-cover
-            msg = """
+            raise ModuleNotFoundError("""
             You set `use_amp=True` but do not have apex installed.
             Install apex first using this guide and rerun with use_amp=True:
             https://github.com/NVIDIA/apex#linux
 
             this run will NOT use 16 bit precision
-            """
-            raise ModuleNotFoundError(msg)
+            """)
+
+        if self.use_amp:
+            log.info('Using 16bit precision.')
+
+    @property
+    def use_amp(self) -> bool:
+        return self.precision == 16 and APEX_AVAILABLE
