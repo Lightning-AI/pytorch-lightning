014b6dd85a2cd795adfd4454160a670c7ead4e64
J. Borovec <jirka.borovec@seznam.cz>
2020-04-07 14:27:10 +0200

    refactor

--- pytorch_lightning/loggers/base.py
+++ pytorch_lightning/loggers/base.py
@@ -25,78 +25,54 @@ def rank_zero_only(fn: Callable):
     return wrapped_fn
 
 
-def merge_dicts(
-        dicts: Sequence[Mapping],
-        agg_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None,
-        dflt_func: Callable[[Sequence[float]], float] = np.mean
-) -> Dict:
-    """Merge a sequence with dictionaries into one dictionary by aggregating the
-    same keys with some given function.
-
-    Args:
-        dicts:
-            Sequence of dictionaries to be merged.
-        agg_funcs:
-            Mapping from key name to function. This function will aggregate a
-            list of values, obtained from the same key of all dictionaries.
-            If some key has no specified aggregation function, the default one
-            will be used. Default is: None (all keys will be aggregated by the
-            default function).
-        dflt_func:
-            Default function to aggregate keys, which are not presented in the
-            `agg_funcs` map.
-
-    Returns:
-        Dictionary with merged values.
-
-    Examples:
-        >>> import pprint
-        >>> d1 = {'a': 1.7, 'b': 2.0, 'c': 1}
-        >>> d2 = {'a': 1.1, 'b': 2.2}
-        >>> d3 = {'a': 1.1, 'v': 2.3}
-        >>> dflt_func = min
-        >>> agg_funcs = {'a': np.mean, 'v': max}
-        >>> pprint.pprint(merge_dicts([d1, d2, d3], agg_funcs, dflt_func))
-        {'a': 1.3, 'b': 2.0, 'c': 1, 'v': 2.3}
-    """
-
-    keys = list(functools.reduce(operator.or_, [set(d.keys()) for d in dicts]))
-    dx = {}
-    for k in keys:
-        fn = agg_funcs.get(k, dflt_func) if agg_funcs else dflt_func
-        agg_val = fn([v for v in [d.get(k) for d in dicts] if v is not None])
-        dx[k] = agg_val
-
-    return dx
-
-
 class LightningLoggerBase(ABC):
     """Base class for experiment loggers."""
 
     def __init__(
             self,
-            agg_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None,
-            dflt_func: Callable[[Sequence[float]], float] = np.mean
+            agg_key_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None,
+            agg_default_func: Callable[[Sequence[float]], float] = np.mean
     ):
         """
         Args:
-            agg_funcs:
+            agg_key_funcs:
                 Dictionary which maps a metric name to a function, which will
                 aggregate the metric values for the same steps.
-            dflt_func:
+            agg_default_func:
                 Default function to aggregate metric values. If some metric name
-                is not presented in the `agg_funcs` dictionary, then the
-                `dflt_func` will be used for aggregation.
+                is not presented in the `agg_key_funcs` dictionary, then the
+                `agg_default_func` will be used for aggregation.
 
         Notes:
-            `agg_func` and `dflt_func` are used only when one logs metrics with
+            `agg_key_funcs` and `agg_default_func` are used only when one logs metrics with
             `LightningLoggerBase.agg_and_log_metrics` method.
         """
         self._rank = 0
         self._prev_step = -1
         self._metrics_to_agg: List[Dict[str, float]] = []
-        self._agg_funcs = agg_funcs
-        self._dflt_func = dflt_func
+        self._agg_key_funcs = agg_key_funcs if agg_key_funcs else {}
+        self._agg_default_func = agg_default_func
+
+    def update_agg_funcs(
+            self,
+            agg_key_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None,
+            agg_default_func: Callable[[Sequence[float]], float] = np.mean
+    ):
+        """Update aggregation methods.
+
+        Args:
+            agg_key_funcs:
+                Dictionary which maps a metric name to a function, which will
+                aggregate the metric values for the same steps.
+            agg_default_func:
+                Default function to aggregate metric values. If some metric name
+                is not presented in the `agg_key_funcs` dictionary, then the
+                `agg_default_func` will be used for aggregation.
+        """
+        if agg_key_funcs:
+            self._agg_key_funcs.update(agg_key_funcs)
+        if agg_default_func:
+            self._agg_default_func = agg_default_func
 
     @property
     @abstractmethod
@@ -116,16 +92,20 @@ class LightningLoggerBase(ABC):
             Aggregated metrics. The return value could be None. In such case, metrics
             are added to the aggregation list, but not aggregated yet.
         """
+        # if you still receiving metric from the same step, just accumulate it
         if step == self._prev_step:
             self._metrics_to_agg.append(metrics)
             return None
 
-        if len(self._metrics_to_agg) > 0:
-            agg_mets = merge_dicts(
-                self._metrics_to_agg, self._agg_funcs, self._dflt_func)
-        else:
+        # compute the metrics
+        if not self._metrics_to_agg:
             agg_mets = None
+        elif len(self._metrics_to_agg) == 1:
+            agg_mets = self._metrics_to_agg[0]
+        else:
+            agg_mets = merge_dicts(self._metrics_to_agg, self._agg_key_funcs, self._agg_default_func)
 
+        # as new step received reset accumulator
         self._metrics_to_agg = [metrics]
         self._prev_step = step
         return agg_mets
@@ -314,3 +294,48 @@ class LoggerCollection(LightningLoggerBase):
     @property
     def version(self) -> str:
         return '_'.join([str(logger.version) for logger in self._logger_iterable])
+
+
+def merge_dicts(
+        dicts: Sequence[Mapping],
+        agg_key_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None,
+        default_func: Callable[[Sequence[float]], float] = np.mean
+) -> Dict:
+    """Merge a sequence with dictionaries into one dictionary by aggregating the
+    same keys with some given function.
+
+    Args:
+        dicts:
+            Sequence of dictionaries to be merged.
+        agg_key_funcs:
+            Mapping from key name to function. This function will aggregate a
+            list of values, obtained from the same key of all dictionaries.
+            If some key has no specified aggregation function, the default one
+            will be used. Default is: None (all keys will be aggregated by the
+            default function).
+        default_func:
+            Default function to aggregate keys, which are not presented in the
+            `agg_key_funcs` map.
+
+    Returns:
+        Dictionary with merged values.
+
+    Examples:
+        >>> import pprint
+        >>> d1 = {'a': 1.7, 'b': 2.0, 'c': 1}
+        >>> d2 = {'a': 1.1, 'b': 2.2, 'v': 1}
+        >>> d3 = {'a': 1.1, 'v': 2.3}
+        >>> dflt_func = min
+        >>> agg_funcs = {'a': np.mean, 'v': max}
+        >>> pprint.pprint(merge_dicts([d1, d2, d3], agg_funcs, dflt_func))
+        {'a': 1.3, 'b': 2.0, 'c': 1, 'v': 2.3}
+    """
+
+    keys = list(functools.reduce(operator.or_, [set(d.keys()) for d in dicts]))
+    d_out = {}
+    for k in keys:
+        fn = agg_key_funcs.get(k, default_func) if agg_key_funcs else default_func
+        agg_val = fn([v for v in [d_in.get(k) for d_in in dicts] if v is not None])
+        d_out[k] = agg_val
+
+    return d_out
