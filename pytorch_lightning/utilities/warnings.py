3543e5586ecd9a0de58305fee906b49b473ad0fe
J. Borovec <jirka.borovec@seznam.cz>
2020-04-09 18:57:05 +0200

    docs

--- pytorch_lightning/utilities/warnings.py
+++ pytorch_lightning/utilities/warnings.py
@@ -6,11 +6,13 @@ _proc_rank = 0
 
 
 def set_proc_rank(value: int) -> None:
+    """Set the (sub)process rank."""
     global _proc_rank
     _proc_rank = value
 
 
-def rank_zero_warn(*args, **kwargs):
+def rank_zero_warn(*args, **kwargs) -> None:
+    """Warning only if (sub)process has rank 0."""
     global _proc_rank
     if _proc_rank:
-        rank_zero_warn(*args, **kwargs)
+        warnings.warn(*args, **kwargs)
