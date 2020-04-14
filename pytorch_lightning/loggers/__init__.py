7708f819e637ee5b488f01ff43b99e59f8a60c62
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-04-14 05:06:48 +0200

    improve __init__

--- pytorch_lightning/loggers/__init__.py
+++ pytorch_lightning/loggers/__init__.py
@@ -1,62 +1,59 @@
 """
-Lightning supports most popular logging frameworks (Tensorboard, comet, weights and biases, etc...).
-To use a logger, simply pass it into the trainer. To use multiple loggers, simply pass in a ``list``
-or ``tuple`` of loggers.
+Lightning supports the most popular logging frameworks (TensorBoard, Comet, Weights and Biases, etc...).
+To use a logger, simply pass it into the :class:`~pytorch_lightning.trainer.trainer.Trainer`.
+Lightning uses TensorBoard by default.
 
-.. code-block:: python
+>>> from pytorch_lightning import Trainer
+>>> from pytorch_lightning import loggers
+>>> tb_logger = loggers.TensorBoardLogger('logs/')
+>>> trainer = Trainer(logger=tb_logger)
 
-    from pytorch_lightning import loggers
+Choose from any of the others such as MLflow, Comet, Neptune, WandB, ...
 
-    # lightning uses tensorboard by default
-    tb_logger = loggers.TensorBoardLogger()
-    trainer = Trainer(logger=tb_logger)
+>>> comet_logger = loggers.CometLogger(save_dir='logs/')
+>>> trainer = Trainer(logger=comet_logger)
 
-    # or choose from any of the others such as MLFlow, Comet, Neptune, Wandb
-    comet_logger = loggers.CometLogger()
-    trainer = Trainer(logger=comet_logger)
+To use multiple loggers, simply pass in a ``list`` or ``tuple`` of loggers ...
 
-    # or pass a list
-    tb_logger = loggers.TensorBoardLogger()
-    comet_logger = loggers.CometLogger()
-    trainer = Trainer(logger=[tb_logger, comet_logger])
+>>> tb_logger = loggers.TensorBoardLogger('logs/')
+>>> comet_logger = loggers.CometLogger(save_dir='logs/')
+>>> trainer = Trainer(logger=[tb_logger, comet_logger])
 
-.. note:: All loggers log by default to ``os.getcwd()``. To change the path without creating a logger set
+Note:
+    All loggers log by default to ``os.getcwd()``. To change the path without creating a logger set
     ``Trainer(default_root_dir='/your/path/to/save/checkpoints')``
 
-Custom logger
+Custom Logger
 -------------
 
 You can implement your own logger by writing a class that inherits from
-``LightningLoggerBase``. Use the ``rank_zero_only`` decorator to make sure that
-only the first process in DDP training logs data.
+:class:`LightningLoggerBase`. Use the :func:`~pytorch_lightning.loggers.base.rank_zero_only`
+decorator to make sure that only the first process in DDP training logs data.
 
-.. code-block:: python
-
-    from pytorch_lightning.loggers import LightningLoggerBase, rank_zero_only
-
-    class MyLogger(LightningLoggerBase):
-
-        @rank_zero_only
-        def log_hyperparams(self, params):
-            # params is an argparse.Namespace
-            # your code to record hyperparameters goes here
-            pass
-
-        @rank_zero_only
-        def log_metrics(self, metrics, step):
-            # metrics is a dictionary of metric names and values
-            # your code to record metrics goes here
-            pass
-
-        def save(self):
-            # Optional. Any code necessary to save logger data goes here
-            pass
-
-        @rank_zero_only
-        def finalize(self, status):
-            # Optional. Any code that needs to be run after training
-            # finishes goes here
-
+>>> from pytorch_lightning.loggers import LightningLoggerBase, rank_zero_only
+>>> class MyLogger(LightningLoggerBase):
+...
+...     @rank_zero_only
+...     def log_hyperparams(self, params):
+...         # params is an argparse.Namespace
+...         # your code to record hyperparameters goes here
+...         pass
+...
+...     @rank_zero_only
+...     def log_metrics(self, metrics, step):
+...         # metrics is a dictionary of metric names and values
+...         # your code to record metrics goes here
+...         pass
+...
+...     def save(self):
+...         # Optional. Any code necessary to save logger data goes here
+...         pass
+...
+...     @rank_zero_only
+...     def finalize(self, status):
+...         # Optional. Any code that needs to be run after training
+...         # finishes goes here
+...         pass
 
 If you write a logger that may be useful to others, please send
 a pull request to add it to Lighting!
@@ -64,16 +61,17 @@ a pull request to add it to Lighting!
 Using loggers
 -------------
 
-Call the logger anywhere except ``__init__`` in your LightningModule by doing:
+Call the logger anywhere except ``__init__`` in your
+:class:`~pytorch_lightning.core.lightning.LightningModule` by doing:
 
-.. code-block:: python
-
-    def train_step(...):
-        # example
-        self.logger.experiment.whatever_method_summary_writer_supports(...)
-
-    def any_lightning_module_function_or_hook(...):
-        self.logger.experiment.add_histogram(...)
+>>> from pytorch_lightning import LightningModule
+>>> class LitModel(LightningModule):
+...     def training_step(self, batch, batch_idx):
+...         # example
+...         self.logger.experiment.whatever_method_summary_writer_supports(...)
+...
+...     def any_lightning_module_function_or_hook(self):
+...         self.logger.experiment.add_histogram()
 
 Read more in the `Experiment Logging use case <./experiment_logging.html>`_.
 
@@ -85,7 +83,11 @@ from os import environ
 from pytorch_lightning.loggers.base import LightningLoggerBase, LoggerCollection, rank_zero_only
 from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 
-__all__ = ['TensorBoardLogger']
+__all__ = [
+    'LightningLoggerBase',
+    'LoggerCollection',
+    'TensorBoardLogger',
+]
 
 try:
     # needed to prevent ImportError and duplicated logs.
