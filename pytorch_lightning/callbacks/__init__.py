619a2df4af3ab00a2a9a2ee3399d3ba91e21a704
Nicki Skafte <nugginea@gmail.com>
2020-04-15 14:10:37 +0200

    renaming

--- pytorch_lightning/callbacks/__init__.py
+++ pytorch_lightning/callbacks/__init__.py
@@ -2,12 +2,12 @@ from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks.gradient_accumulation_scheduler import GradientAccumulationScheduler
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from pytorch_lightning.callbacks.lr_logger import LRLogger
+from pytorch_lightning.callbacks.lr_logger import LearningRateLogger
 
 __all__ = [
     'Callback',
     'EarlyStopping',
     'ModelCheckpoint',
     'GradientAccumulationScheduler',
-    'LRLogger'
+    'LearningRateLogger'
 ]
