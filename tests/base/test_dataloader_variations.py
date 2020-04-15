f51523ea0fb5345438d86d1c31b3667248f8cdba
William Falcon <waf2107@columbia.edu>
2020-04-15 12:02:32 -0400

    added tests for warning

--- tests/base/test_dataloader_variations.py
+++ tests/base/test_dataloader_variations.py
@@ -1,10 +1,7 @@
-import tests.base.model_utils as mutils
-
-
 class TestDataloaderVariationsMixin:
 
     def test_dataloader(self):
-        return mutils.dataloader(
+        return self.dataloader(
             train=False,
             data_root=self.hparams.data_root,
             batch_size=self.hparams.batch_size,
--- tests/base/val_dataloader_variations.py
+++ tests/base/val_dataloader_variations.py
@@ -1,10 +1,7 @@
-import tests.base.model_utils as mutils
-
-
 class ValDataloaderVariationsMixin:
 
     def val_dataloader(self):
-        return mutils.dataloader(
+        return self.dataloader(
             train=False,
             data_root=self.hparams.data_root,
             batch_size=self.hparams.batch_size,
