db7cb775827c623b140f90c9ac495f1196c7981e
Nicki Skafte <nugginea@gmail.com>
2020-04-15 14:34:09 +0200

    add tests

--- tests/callbacks/test_callbacks.py
+++ tests/callbacks/test_callbacks.py
@@ -1,11 +1,12 @@
 import tests.base.utils as tutils
 from pytorch_lightning import Callback
 from pytorch_lightning import Trainer, LightningModule
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateLogger, ModelCheckpoint
 from tests.base import (
     LightTrainDataloader,
     LightTestMixin,
     LightValidationMixin,
+    LightTestOptimizersWithMixedSchedulingMixin,
     TestModelBase
 )
 
@@ -262,3 +263,59 @@ def test_model_checkpoint_with_non_string_input(tmpdir):
 
     # These should be different if the dirpath has be overridden
     assert trainer.ckpt_path != trainer.default_root_dir
+
+
+def test_lr_logger_single_lr(tmpdir):
+    """ Test that learning rates are extracted and logged for single lr scheduler"""
+    tutils.reset_seed()
+
+    class CurrentTestModel(LightTrainDataloader, TestModelBase):
+        pass
+
+    hparams = tutils.get_default_hparams()
+    model = CurrentTestModel(hparams)
+
+    lr_logger = LearningRateLogger()
+    trainer_options = dict(
+        default_root_dir=tmpdir,
+        max_epochs=5,
+        val_percent_check=0.1,
+        train_percent_check=0.5,
+        callbacks=[lr_logger]
+    )
+
+    trainer = Trainer(**trainer_options)
+    results = trainer.fit(model)
+
+    assert lr_logger.lrs != {}, 'No learning rates logged'
+    assert len(lr_logger.lrs.keys()) == len(trainer.lr_schedulers), \
+        'Number of learning rates logged does not match number of lr schedulers'
+
+
+def test_lr_logger_multi_lrs(tmpdir):
+    """ Test that learning rates are extracted and logged for multi lr schedulers """
+    tutils.reset_seed()
+
+    class CurrentTestModel(LightTestOptimizersWithMixedSchedulingMixin,
+                           LightTrainDataloader,
+                           TestModelBase):
+        pass
+
+    hparams = tutils.get_default_hparams()
+    model = CurrentTestModel(hparams)
+
+    lr_logger = LearningRateLogger()
+    trainer_options = dict(
+        default_root_dir=tmpdir,
+        max_epochs=1,
+        val_percent_check=0.1,
+        train_percent_check=0.5,
+        callbacks=[lr_logger]
+    )
+
+    trainer = Trainer(**trainer_options)
+    results = trainer.fit(model)
+
+    assert lr_logger.lrs != {}, 'No learning rates logged'
+    assert len(lr_logger.lrs.keys()) == len(trainer.lr_schedulers), \
+        'Number of learning rates logged does not match number of lr schedulers'
