e3001a092913514d65547d4e912382525cfedad2
Nic Eggert <nic@eggert.io>
2020-04-15 22:17:31 -0500

    Add ddp_cpu backend for testing ddp without GPUs (#1158)
    * Add tests for distributed backend config
    
    * Refactor set_distributed_mode
    
    * Use gloo backend on cpu
    
    * Use 127.0.0.1 instead of 127.0.0.2
    
    Not totally clear on why this is necessary, but it seemt to work
    
    * Update LightningDDP so that it works with CPU
    
    * Add ddp_cpu backend and num_processes Trainer arg
    
    * PEP8
    
    * Fix test skipping. Inequalities are hard :/
    
    * Skip ddp_cpu test on Windows
    
    * Make a few more cases fall back to ddp_cpu
    
    * New function name
    
    * Flake8
    
    * Don't test distributed on MacOS with torch < 1.3
    
    Support for distributed in MacOS was added in Torch 1.3.0
    
    * Add ddp_cpu and num_processes to docs
    
    * Parametrize trainer config tests
    
    * Tweak warning
    
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>
    
    * Remove redundant test
    
    * Replace pass branches with comments
    
    * Add missing warnings import
    
    * save_path -> root_dir
    
    * Use new rank_zero_warn
    
    * Whitespace
    
    * Apply suggestions from code review
    
    * formatting
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: J. Borovec <jirka.borovec@seznam.cz>

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -944,11 +944,12 @@ class LightningModule(ABC, GradInformation, ModelIO, ModelHooks):
         try:
             root_node = os.environ['SLURM_NODELIST'].split(' ')[0]
         except Exception:
-            root_node = '127.0.0.2'
+            root_node = '127.0.0.1'
 
         root_node = self.trainer.resolve_root_node_address(root_node)
         os.environ['MASTER_ADDR'] = root_node
-        torch_distrib.init_process_group('nccl', rank=proc_rank, world_size=world_size)
+        torch_backend = "nccl" if self.trainer.on_gpu else "gloo"
+        torch_distrib.init_process_group(torch_backend, rank=proc_rank, world_size=world_size)
 
     def configure_apex(
             self,
