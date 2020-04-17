d0c0380de87739dd9ed8236a4ab2ff05a59ef332
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-04-17 10:08:54 +0200

    pep8 fixes

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -282,8 +282,8 @@ class Trainer(
                 trying to optimize initial learning for faster convergence. Sets learning
                 rate in self.hparams.lr | self.hparams.learning_rate in the lightning module.
                 To use a different key, set a string instead of True with the key name.
-                
-            replace_sampler_ddp: Explicitly enables or disables sampler replacement. 
+ 
+            replace_sampler_ddp: Explicitly enables or disables sampler replacement.
                 If not specified this will toggled automatically ddp is used
 
             benchmark: If true enables cudnn.benchmark.
