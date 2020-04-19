d7e64f4e3552b385ea859bbfacbaab419b5d0bce
Hengjian Jia <henryjia18@gmail.com>
2020-04-19 05:48:26 +0100

    Make sure we stick with pep8 formatting

--- tests/models/test_gpu.py
+++ tests/models/test_gpu.py
@@ -114,7 +114,8 @@ def test_auto_move_data(tmpdir):
     for x, y in loader:
         x = x.view(x.size(0), -1)
         assert model(x).device == torch.device('cuda:0'), "Automoving data to same device as model failed"
-        assert model(x.cuda(0)).device == torch.device('cuda:0'), "Automoving data to same device as model failed"
+        x = x.cuda(0)
+        assert model().device == torch.device('cuda:0'), "Automoving data to same device as model failed"
 
 
 @pytest.fixture
