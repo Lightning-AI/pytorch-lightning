d0c9472cb3b6361f0cb4c84a2e3457ffc3076994
areshytko <alex4am4i@gmail.com>
2020-04-20 00:08:19 +0300

    Add SLURM check in ddp_train() and init_ddp_connection() (#1387)
    * slurm check in ddp_train and init_ddp_connection
    
    * Remove code example in init_ddp_connection
    
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>
    
    * remove blank line
    
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>
    
    * improve for test coverage
    
    Co-Authored-By: Jirka Borovec <Borda@users.noreply.github.com>
    
    * update changelog
    
    * Default values and warnings for DDP env variables
    
    * fix merge artifacts
    
    * update localhost value
    
    * change to NODE_RANK
    
    Co-authored-by: Alexander Reshytko <areshytko@Alexanders-MacBook-Pro.local>
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: William Falcon <waf2107@columbia.edu>

--- pytorch_lightning/trainer/distrib_data_parallel.py
+++ pytorch_lightning/trainer/distrib_data_parallel.py
@@ -285,12 +285,13 @@ class TrainerDDPMixin(ABC):
         :param cluster_obj:
         :return:
         """
-        # node rank using relative slurm id
-        # otherwise default to node rank 0
+        # node rank using relative slurm id if under slurm management
+        # otherwise use given node rank or default to node rank 0
         try:
-            node_id = os.environ['SLURM_NODEID']
+            node_id = os.environ['SLURM_NODEID'] if self.is_slurm_managing_tasks else os.environ['NODE_RANK']
             self.node_rank = int(node_id)
-        except Exception:
+        except KeyError:
+            log.warning("SLURM_NODEID or NODE_RANK environment variable is not defined. Set as 0.")
             self.node_rank = 0
 
         # show progressbar only on progress_rank 0
@@ -317,7 +318,7 @@ class TrainerDDPMixin(ABC):
         # try to init for 20 times at max in case ports are taken
         # where to store ip_table
         model.trainer = self
-        model.init_ddp_connection(self.proc_rank, self.world_size)
+        model.init_ddp_connection(self.proc_rank, self.world_size, self.is_slurm_managing_tasks)
 
         # CHOOSE OPTIMIZER
         # allow for lr schedulers as well
