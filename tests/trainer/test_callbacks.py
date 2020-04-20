3968eb03cd8d5dc30ad1322b52da190fe1b19e0c
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-04-20 05:09:46 +0200

    more thorough testing

--- tests/trainer/test_callbacks.py
+++ tests/trainer/test_callbacks.py
@@ -286,7 +286,7 @@ def test_progress_bar_off(callbacks, refresh_rate):
 
 def test_progress_bar_misconfiguration():
     """Test that Trainer doesn't accept multiple progress bars."""
-    callbacks = [ProgressBar(), ProgressBar()]
+    callbacks = [ProgressBar(), ProgressBar(), ModelCheckpoint('.')]
     with pytest.raises(MisconfigurationException, match=r'^You added multiple progress bar callbacks'):
         Trainer(callbacks=callbacks)
 
@@ -311,20 +311,78 @@ def test_progress_bar_totals():
         val_percent_check=1.0,
         max_epochs=1,
     )
-    progress_bar = trainer.progress_bar_callback
-    assert 0 == progress_bar.total_train_batches
-    assert 0 == progress_bar.total_val_batches
-    assert 0 == progress_bar.total_test_batches
+    bar = trainer.progress_bar_callback
+    assert 0 == bar.total_train_batches
+    assert 0 == bar.total_val_batches
+    assert 0 == bar.total_test_batches
 
     trainer.fit(model)
-    assert progress_bar.total_train_batches == len(trainer.train_dataloader)
-    assert progress_bar.total_val_batches == progress_bar.val_progress_bar.total
-    assert progress_bar.total_val_batches == sum(len(loader) for loader in trainer.val_dataloaders)
-    assert 0 == progress_bar.total_test_batches
+
+    # check main progress bar total
+    n = bar.total_train_batches
+    m = bar.total_val_batches
+    assert len(trainer.train_dataloader) == n
+    assert bar.main_progress_bar.total == n + m
+
+    # check val progress bar total
+    assert sum(len(loader) for loader in trainer.val_dataloaders) == m
+    assert bar.val_progress_bar.total == m
+
+    # main progress bar should have reached the end (train batches + val batches)
+    assert bar.main_progress_bar.n == n + m
+    assert bar.train_batch_idx == n
+
+    # val progress bar should have reached the end
+    assert bar.val_progress_bar.n == m
+    assert bar.val_batch_idx == m
+
+    # check that the test progress bar is off
+    assert 0 == bar.total_test_batches
+    assert bar.test_progress_bar is None
 
     trainer.test(model)
-    assert progress_bar.total_test_batches == progress_bar.test_progress_bar.total
-    assert progress_bar.total_test_batches == sum(len(loader) for loader in trainer.test_dataloaders)
+
+    # check test progress bar total
+    k = bar.total_test_batches
+    assert sum(len(loader) for loader in trainer.test_dataloaders) == k
+    assert bar.test_progress_bar.total == k
+
+    # test progress bar should have reached the end
+    assert bar.test_progress_bar.n == k
+    assert bar.test_batch_idx == k
+
+
+def test_progress_bar_fast_dev_run():
+    class CurrentTestModel(
+        LightTrainDataloader,
+        LightTestMixin,
+        LightValidationMixin,
+        TestModelBase,
+    ):
+        pass
+
+    hparams = tutils.get_default_hparams()
+    model = CurrentTestModel(hparams)
+
+    trainer = Trainer(
+        progress_bar_callback=True,
+        fast_dev_run=True,
+    )
+
+    progress_bar = trainer.progress_bar_callback
+    assert 1 == progress_bar.total_train_batches
+    # total val batches are known only after val dataloaders have reloaded
+
+    trainer.fit(model)
+
+    assert 1 == progress_bar.total_val_batches
+    assert 1 == progress_bar.train_batch_idx
+    assert 1 == progress_bar.val_batch_idx
+    assert 0 == progress_bar.test_batch_idx
+
+    # the main progress bar should display 2 batches (1 train, 1 val)
+    assert 2 == progress_bar.main_progress_bar.total
+    assert 2 == progress_bar.main_progress_bar.n
 
 
 @pytest.mark.parametrize('refresh_rate', [0, 1, 50])
