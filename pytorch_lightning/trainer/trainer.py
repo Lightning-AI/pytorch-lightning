2063042691b78a6d5745d23924f0a3235852c84f
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-04-20 06:52:19 +0200

    fix horror bug

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -87,7 +87,7 @@ class Trainer(
             logger: Union[LightningLoggerBase, Iterable[LightningLoggerBase], bool] = True,
             checkpoint_callback: Union[ModelCheckpoint, bool] = True,
             early_stop_callback: Optional[Union[EarlyStopping, bool]] = False,
-            callbacks: List[Callback] = [],
+            callbacks: Optional[List[Callback]] = None,
             default_root_dir: Optional[str] = None,
             gradient_clip_val: float = 0,
             process_position: int = 0,
@@ -293,7 +293,7 @@ class Trainer(
         """
 
         # Init callbacks
-        self.callbacks = callbacks
+        self.callbacks = callbacks or []
         self.on_init_start()
 
         # benchmarking
