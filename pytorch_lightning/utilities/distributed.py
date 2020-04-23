d0352aede3029673c68ac979a2f3635e96eeec17
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2020-04-24 00:56:38 +0200

    improve proposal (fix problems with method call self)

--- pytorch_lightning/utilities/distributed.py
+++ pytorch_lightning/utilities/distributed.py
@@ -3,28 +3,45 @@ from typing import Callable
 import warnings
 
 
-class rank_zero_only(object):
-    """ This is a decorator (in form of a class instead of function). """
+# class rank_zero_only(object):
+#     """ This is a decorator (in form of a class instead of function). """
+#
+#     # this is a class attribute, it is constant across all instances of "rank_zero_only"
+#     # it's like the old rank global, but now it is limited to the scope of this class/decorator
+#     _rank = 0
+#
+#     def __init__(self, fn):
+#         # this is the function we try to decorate.
+#         self._function = fn
+#
+#     def __call__(self, *args, **kwargs):
+#         if self._rank == 0:
+#             return self._function(*args, **kwargs)
+#
+#     @property
+#     def rank(self):
+#         return self._rank
+#
+#     @rank.setter
+#     def rank(self, rank):
+#         self._rank = rank
+#
 
-    # this is a class attribute, it is constant across all instances of "rank_zero_only"
-    # it's like the old rank global, but now it is limited to the scope of this class/decorator
-    _rank = 0
+def rank_zero_only(fn):
 
-    def __init__(self, fn):
-        # this is the function we try to decorate.
-        self._function = fn
+    @wraps(fn)
+    def wrapped_fn(*args, **kwargs):
+        if rank_zero_only.rank == 0:
+            return fn(*args, **kwargs)
 
-    def __call__(self, *args, **kwargs):
-        if self._rank == 0:
-            return self._function(*args, **kwargs)
+    return wrapped_fn
 
-    @property
-    def rank(self):
-        return self._rank
 
-    @rank.setter
-    def rank(self, rank):
-        self._rank = rank
+try:
+    # add the attribute to the function but don't overwrite in case Trainer has already set it
+    getattr(rank_zero_only, 'rank')
+except AttributeError:
+    rank_zero_only.rank = 0
 
 
 def _warn(*args, **kwargs):
