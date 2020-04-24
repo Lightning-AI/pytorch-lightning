6a5b6a25b39bcc6130f54b763b324f19100fbb6d
J. Borovec <jirka.borovec@seznam.cz>
2020-04-24 15:25:57 +0200

    cleaning

--- pytorch_lightning/utilities/distributed.py
+++ pytorch_lightning/utilities/distributed.py
@@ -1,31 +1,6 @@
 from functools import wraps
-from typing import Callable
 import warnings
 
-
-# class rank_zero_only(object):
-#     """ This is a decorator (in form of a class instead of function). """
-#
-#     # this is a class attribute, it is constant across all instances of "rank_zero_only"
-#     # it's like the old rank global, but now it is limited to the scope of this class/decorator
-#     _rank = 0
-#
-#     def __init__(self, fn):
-#         # this is the function we try to decorate.
-#         self._function = fn
-#
-#     def __call__(self, *args, **kwargs):
-#         if self._rank == 0:
-#             return self._function(*args, **kwargs)
-#
-#     @property
-#     def rank(self):
-#         return self._rank
-#
-#     @rank.setter
-#     def rank(self, rank):
-#         self._rank = rank
-#
 
 def rank_zero_only(fn):
 
