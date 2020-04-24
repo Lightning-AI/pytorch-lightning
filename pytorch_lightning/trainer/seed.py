fd73061058444e344518703e47a8a4cf275f9687
kumuji <alexey.nekrasov@rwth-aachen.de>
2020-04-24 14:04:18 +0200

    cuda manual_seed is not necessary

--- pytorch_lightning/trainer/seed.py
+++ pytorch_lightning/trainer/seed.py
@@ -27,5 +27,3 @@ def seed_everything(seed: int = None):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
