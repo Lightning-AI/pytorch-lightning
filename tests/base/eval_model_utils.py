455a6fb0bdd337553cebde647e6c7a42ae4b71a6
J. Borovec <jirka.borovec@seznam.cz>
2020-04-25 22:26:22 +0200

    refactor

--- tests/base/eval_model_utils.py
+++ tests/base/eval_model_utils.py
@@ -3,7 +3,8 @@ from torch.utils.data import DataLoader
 from tests.base.datasets import TrialMNIST
 
 
-class ModelTemplateUtils:
+class ModelTemplateData:
+    hparams: ...
 
     def dataloader(self, train):
         dataset = TrialMNIST(root=self.hparams.data_root, train=train, download=True)
@@ -11,9 +12,13 @@ class ModelTemplateUtils:
         loader = DataLoader(
             dataset=dataset,
             batch_size=self.hparams.batch_size,
-            shuffle=train
+            # test and valid shall not be shuffled
+            shuffle=train,
         )
         return loader
+
+
+class ModelTemplateUtils:
 
     def get_output_metric(self, output, name):
         if isinstance(output, dict):
