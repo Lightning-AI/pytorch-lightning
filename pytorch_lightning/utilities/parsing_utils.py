fe68a3b63244fa862f746f03dbcb2fa3ec3b4159
William Falcon <waf2107@columbia.edu>
2020-04-26 06:55:34 -0400

    fixed distutil parsing

---
+++ pytorch_lightning/utilities/parsing_utils.py
@@ -0,0 +1,14 @@
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
