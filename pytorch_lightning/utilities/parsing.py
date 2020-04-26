9bb44f0d8a28a31ce8de4a5b7d64223735390f34
J. Borovec <jirka.borovec@seznam.cz>
2020-04-26 13:35:01 +0200

    doctest

--- pytorch_lightning/utilities/parsing.py
+++ pytorch_lightning/utilities/parsing.py
@@ -5,6 +5,11 @@ def strtobool(val):
     True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
     'val' is anything else.
+
+    >>> strtobool('YES')
+    1
+    >>> strtobool('FALSE')
+    0
     """
     val = val.lower()
     if val in ('y', 'yes', 't', 'true', 'on', '1'):
