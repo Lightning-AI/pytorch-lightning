bd0d1a73b65d239a71bf0f59e45ee3f08b07299c
Justus Schock <justus.schock@rwth-aachen.de>
2020-04-27 09:01:36 +0200

    return x and y only for curves

--- pytorch_lightning/metrics/sklearn.py
+++ pytorch_lightning/metrics/sklearn.py
@@ -542,13 +542,13 @@ class PrecisionRecallCurve(SklearnMetric):
     """
 
     def __init__(self,
-                 pos_labels: Union[str, int] = 1,
+                 pos_label: Union[str, int] = 1,
                  reduce_group: Any = torch.distributed.group.WORLD,
                  reduce_op: Any = torch.distributed.ReduceOp.SUM):
         """
 
         Args:
-            pos_labels: The class to report if ``average='binary'``.
+            pos_label: The class to report if ``average='binary'``.
             reduce_group: the process group for DDP reduces (only needed for DDP training).
                 Defaults to all processes (world)
             reduce_op: the operation to perform during reduction within DDP (only needed for DDP training).
@@ -557,7 +557,7 @@ class PrecisionRecallCurve(SklearnMetric):
         super().__init__('precision_recall_curve',
                          reduce_group=reduce_group,
                          reduce_op=reduce_op,
-                         pos_labels=pos_labels)
+                         pos_label=pos_label)
 
     def forward(self, probas_pred: np.ndarray, y_true: np.ndarray,
                 sample_weight: Optional[np.ndarray] = None) -> Union[np.ndarray, float]:
@@ -581,7 +581,10 @@ class PrecisionRecallCurve(SklearnMetric):
                 precision and recall.
 
         """
-        return super().forward(probas_pred=probas_pred, y_true=y_true, sample_weight=sample_weight)
+        # only return x and y here, since for now we cannot auto-convert elements of multiple length.
+        # Will be fixed in native implementation
+        return np.array(
+            super().forward(probas_pred=probas_pred, y_true=y_true, sample_weight=sample_weight)[:2])
 
 
 class ROC(SklearnMetric):
@@ -638,7 +641,7 @@ class ROC(SklearnMetric):
                 and is arbitrarily set to `max(y_score) + 1`.
 
         """
-        return super().forward(y_score=y_score, y_true=y_true, sample_weight=sample_weight)
+        return np.array(super().forward(y_score=y_score, y_true=y_true, sample_weight=sample_weight)[:2])
 
 
 class AUROC(SklearnMetric):
