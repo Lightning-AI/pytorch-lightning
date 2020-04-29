55d3dfef6f6ffb31a8dc518295d4e8ccdd6ffd6b
J. Borovec <jirka.borovec@seznam.cz>
2020-04-29 16:16:00 +0200

    formatting

--- tests/models/data/horovod/train_default_model.py
+++ tests/models/data/horovod/train_default_model.py
@@ -29,9 +29,8 @@ sys.path.insert(0, os.path.abspath(PATH_ROOT))
 
 from pytorch_lightning import Trainer  # noqa: E402
 from pytorch_lightning.callbacks import ModelCheckpoint  # noqa: E402
-from tests.base import EvalModelTemplate
-import tests.base.utils as tutils  # noqa: E402
-from tests.base import EvalModelTemplate
+from tests.base import EvalModelTemplate  # noqa: E402
+from tests.base.utils import set_random_master_port, get_default_hparams, run_model_test  # noqa: E402
 
 
 parser = argparse.ArgumentParser()
@@ -41,13 +40,13 @@ parser.add_argument('--on-gpu', action='store_true', default=False)
 
 def run_test_from_config(trainer_options):
     """Trains the default model with the given config."""
-    tutils.set_random_master_port()
+    set_random_master_port()
 
     ckpt_path = trainer_options['default_root_dir']
     trainer_options.update(checkpoint_callback=ModelCheckpoint(ckpt_path))
 
-    model = EvalModelTemplate(tutils.get_default_hparams())
-    tutils.run_model_test(trainer_options, model, on_gpu=args.on_gpu, version=0, with_hpc=False)
+    model = EvalModelTemplate(get_default_hparams())
+    run_model_test(trainer_options, model, on_gpu=args.on_gpu, version=0, with_hpc=False)
 
     # Horovod should be initialized following training. If not, this will raise an exception.
     assert hvd.size() == 2
