42d5cfc3b056b4c82a77a7cdcb8eafc63a812b67
Peter Yu <2057325+yukw777@users.noreply.github.com>
2020-04-29 12:36:28 -0400

    None check for filepath in ModelCheckpoint (#1654)
    Check if the optional filepath is None before checking if it exists
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -86,7 +86,7 @@ class ModelCheckpoint(Callback):
                  save_top_k: int = 1, save_weights_only: bool = False,
                  mode: str = 'auto', period: int = 1, prefix: str = ''):
         super().__init__()
-        if save_top_k > 0 and os.path.isdir(filepath) and len(os.listdir(filepath)) > 0:
+        if save_top_k > 0 and filepath is not None and os.path.isdir(filepath) and len(os.listdir(filepath)) > 0:
             rank_zero_warn(
                 f"Checkpoint directory {filepath} exists and is not empty with save_top_k != 0."
                 "All files in this directory will be deleted when a checkpoint is saved!"
--- tests/callbacks/test_callbacks.py
+++ tests/callbacks/test_callbacks.py
@@ -1,3 +1,4 @@
+import pytest
 import tests.base.utils as tutils
 from pytorch_lightning import Callback
 from pytorch_lightning import Trainer, LightningModule
@@ -249,7 +250,8 @@ def test_pickling(tmpdir):
     pickle.dumps(early_stopping)
 
 
-def test_model_checkpoint_with_non_string_input(tmpdir):
+@pytest.mark.parametrize('save_top_k', [-1, 0, 1, 2])
+def test_model_checkpoint_with_non_string_input(tmpdir, save_top_k):
     """ Test that None in checkpoint callback is valid and that chkp_path is
         set correctly """
     tutils.reset_seed()
@@ -260,7 +262,7 @@ def test_model_checkpoint_with_non_string_input(tmpdir):
     hparams = tutils.get_default_hparams()
     model = CurrentTestModel(hparams)
 
-    checkpoint = ModelCheckpoint(filepath=None, save_top_k=-1)
+    checkpoint = ModelCheckpoint(filepath=None, save_top_k=save_top_k)
 
     trainer = Trainer(default_root_dir=tmpdir,
                       checkpoint_callback=checkpoint,
