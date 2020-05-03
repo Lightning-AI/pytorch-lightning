1daadfa4d0f749a1e1ded3f92c503a0b66b708a7
lezwon <lezwon@synup.com>
2020-05-03 21:41:55 +0530

    parallel loader if tpu_id is None

--- pytorch_lightning/trainer/evaluation_loop.py
+++ pytorch_lightning/trainer/evaluation_loop.py
@@ -251,7 +251,7 @@ class TrainerEvaluationLoopMixin(ABC):
             dl_outputs = []
 
             # on TPU we have to wrap it under the ParallelLoader
-            if self.use_tpu:
+            if self.use_tpu and self.tpu_id is None:
                 device = xm.xla_device(self.tpu_id)
                 dataloader = xla_pl.ParallelLoader(dataloader, [device])
                 dataloader = dataloader.per_device_loader(device)
--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -394,7 +394,7 @@ class TrainerTrainLoopMixin(ABC):
         train_dataloader = self.train_dataloader
 
         # on TPU we have to wrap it under the ParallelLoader
-        if self.use_tpu:
+        if self.use_tpu and self.tpu_id is None:
             device = xm.xla_device(self.tpu_id)
             train_dataloader = xla_pl.ParallelLoader(train_dataloader, [device])
             train_dataloader = train_dataloader.per_device_loader(device)
