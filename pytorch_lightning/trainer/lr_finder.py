78f7efbcf75c3b2a75769e8f347623b4644a1241
Jeremy Jordan <jtjordan@ncsu.edu>
2020-05-04 21:56:19 -0400

    fix merge conflict

--- pytorch_lightning/trainer/lr_finder.py
+++ pytorch_lightning/trainer/lr_finder.py
@@ -225,16 +225,18 @@ class TrainerLRFinderMixin(ABC):
             'configure_optimizers': model.configure_optimizers,
         }
 
-    def __lr_finder_restore_params(self, model):
-        self.auto_lr_find = self.__dumped_params['auto_lr_find']
-        self.logger = self.__dumped_params['logger']
-        self.callbacks = self.__dumped_params['callbacks']
-        self.max_steps = self.__dumped_params['max_steps']
-        self.checkpoint_callback = self.__dumped_params['checkpoint_callback']
-        self.early_stop_callback = self.__dumped_params['early_stop_callback']
-        self.enable_early_stop = self.__dumped_params['enable_early_stop']
-        model.configure_optimizers = self.__dumped_params['configure_optimizers']
-        del self.__dumped_params
+    def _lr_finder_restore_params(self, model):
+        self.auto_lr_find = self._params['auto_lr_find']
+        self.logger = self._params['logger']
+        self.callbacks = self._params['callbacks']
+        self.max_steps = self._params['max_steps']
+        self.progress_bar_refresh_rate = self._params['progress_bar_refresh_rate']
+        self.accumulate_grad_batches = self._params['accumulate_grad_batches']
+        self.checkpoint_callback = self._params['checkpoint_callback']
+        self.early_stop_callback = self._params['early_stop_callback']
+        self.enable_early_stop = self._params['enable_early_stop']
+        self.progress_bar_callback = self._params['progress_bar_callback']
+        model.configure_optimizers = self._params['configure_optimizers']
 
 
 class _LRFinder(object):
