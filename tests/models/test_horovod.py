bac5b63b1ccf2c978c47ca252b6922960958351e
kumuji <alexey.nekrasov@rwth-aachen.de>
2020-05-07 18:49:29 +0200

    static seeds for horovod tests

--- tests/models/test_horovod.py
+++ tests/models/test_horovod.py
@@ -39,6 +39,7 @@ def _nccl_available():
 
 def _run_horovod(trainer_options, on_gpu=False):
     """Execute the training script across multiple workers in parallel."""
+    seed_everything(42)
     cmdline = [
         'horovodrun',
         '-np', '2',
