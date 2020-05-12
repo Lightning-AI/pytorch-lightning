940fb79721ccfbccda80f9e831f45f0f4b915e62
Jirka <jirka.borovec@seznam.cz>
2020-05-12 10:25:04 +0200

    flake8

--- pytorch_lightning/logging/__init__.py
+++ pytorch_lightning/logging/__init__.py
@@ -4,7 +4,8 @@
 """
 
 from pytorch_lightning.utilities import rank_zero_warn
-from pytorch_lightning.loggers import *  # noqa: F403
 
 rank_zero_warn("`logging` package has been renamed to `loggers` since v0.7.0"
                " The deprecated package name will be removed in v0.9.0.", DeprecationWarning)
+
+from pytorch_lightning.loggers import *  # noqa: F403 E402
