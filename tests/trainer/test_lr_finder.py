3d01679d5bb4da3fc140d1a6f0391f329081c1b0
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-05-16 23:37:02 +0200

    whitespace

--- tests/trainer/test_lr_finder.py
+++ tests/trainer/test_lr_finder.py
@@ -57,7 +57,7 @@ def test_trainer_reset_correctly(tmpdir):
     )
 
     changed_attributes = ['callbacks', 'logger', 'max_steps', 'auto_lr_find',
-                          'early_stop_callback', 'accumulate_grad_batches', 
+                          'early_stop_callback', 'accumulate_grad_batches',
                           'enable_early_stop', 'checkpoint_callback']
     attributes_before = {}
     for ca in changed_attributes:
