4c3ba83cda558ec8917a1920d53c911bba5fe1b1
William Falcon <waf2107@columbia.edu>
2020-05-17 08:25:36 -0400

    Update trainer.py

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -805,8 +805,7 @@ class Trainer(
         self.__attach_dataloaders(model, train_dataloader, val_dataloaders)
 
         # check that model is configured correctly
-        if not self.testing:
-            self.check_model_configuration(model)
+        self.check_model_configuration(model)
 
         # download the data and do whatever transforms we need
         # do before any spawn calls so that the model can assign properties
