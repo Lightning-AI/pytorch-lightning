7acface93cc8e283a39ca39a258bb81250bced45
Ananya Harsh Jha <ahj265@nyu.edu>
2020-05-19 13:34:14 -0400

    wip commit for state maintenance in DataParallel

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -6,6 +6,7 @@ import torch
 from torch.cuda._utils import _get_device_index
 from torch.nn import DataParallel
 from torch.nn.parallel import DistributedDataParallel
+from torch.nn.parallel.replicate import replicate
 
 
 def _find_tensors(obj):  # pragma: no-cover
@@ -36,12 +37,45 @@ def get_a_var(obj):  # pragma: no-cover
     return None
 
 
+class DistributedState():
+    def merge_state(self, parameter_list):
+        pass
+
+# TODO: define state container class
+# TODO: maintain n separate copies in data parallel
+# TODO: at end of data parallel, create a method to merge/ default opt: save from replica[0]
 class LightningDataParallel(DataParallel):
     """
-    Override the forward call in lightning so it goes to training and validation step respectively
+    CHANGE:
+        add init and call super() to allow for instance var
+        self.module_copies, which maintains state
     """
+    def __init__(self, module, device_ids=None, output_device=None, dim=0):
+        super(LightningDataParallel, self).__init__(
+            module=module,
+            device_ids=device_ids,
+            output_device=output_device,
+            dim=dim
+        )
+
+        #setattr(self.module, 'distributed_state', StateContainer())
+
+        # make initial copy 
+        # this gives an idea about the keys that will be replicated 
+        # from the network at each forward
+        self.module_copies = self.replicate(self.module, self.device_ids)
+
+        # assign None to all key value
+        # initially self.module_copies will have same keys as replicas
+        for module in self.module_copies:
+            for key in module.__dict__.keys():
+                module.__dict__[key] = None
 
     def forward(self, *inputs, **kwargs):
+        """
+            Override the forward call in lightning so it goes to 
+            training and validation step respectively
+        """
         if not self.device_ids:
             return self.module(*inputs, **kwargs)
 
@@ -62,7 +96,36 @@ class LightningDataParallel(DataParallel):
             return self.module.validation_step(*inputs[0], **kwargs[0])
 
         replicas = self.replicate(self.module, self.device_ids[:len(inputs)])
-        outputs = self.parallel_apply(replicas, inputs, kwargs)
+
+        # ---------------
+        # CHANGE
+
+        # combine previous state and replicas here
+        # 
+        # replica count is based on len(input) (<= self.module_copies)
+        # replica made from initial network,
+        # prev state should contain >= keys depending on prev forward
+        for idx, replica in enumerate(replicas):
+            for key in replica.__dict__.keys():
+                # TODO: assert keys from replica are None in state?
+                # add references from the replicated modules, buffers, params etc.
+                self.module_copies[idx].__dict__[key] = replica.__dict__[key]
+
+        # send in self.module_copies
+        # checked backprop, works
+        outputs = self.parallel_apply(self.module_copies[:len(inputs)], inputs, kwargs)
+
+        # flush keys that were replicated from init. network
+        # additional keys in self.module_copies will be retained
+        # 
+        # replica count is according to input (<= self.module_copies)
+        for idx, replica in enumerate(replicas):
+            for key in replica.__dict__.keys():
+                self.module_copies[idx].__dict__[key] = None
+
+        # CHANGE END
+        # ---------------
+
         return self.gather(outputs, self.output_device)
 
     def parallel_apply(self, replicas, inputs, kwargs):
@@ -224,3 +287,100 @@ def auto_squeeze_dim_zeros(output):
         is_scalar = v.dim() == 0
         if is_scalar:
             output[k] = output[k].unsqueeze(0)
+
+# TODO: remove this, did not need to break replicate in the end,
+# solution while maintaing atomicity of replicate will scale across 1.3.0-1.5.0
+"""
+def replicate(network, devices, detach=False):
+    if not _replicatable_module(network):
+        raise RuntimeError("Cannot replicate network where python modules are "
+                           "childrens of ScriptModule")
+
+    devices = list(map(lambda x: _get_device_index(x, True), devices))
+    num_replicas = len(devices)
+
+    params = list(network.parameters())
+    param_indices = {param: idx for idx, param in enumerate(params)}
+    param_copies = _broadcast_coalesced_reshape(params, devices, detach)
+
+    buffers = list(network.buffers())
+    buffers_rg = []
+    buffers_not_rg = []
+    for buf in buffers:
+        if buf.requires_grad and not detach:
+            buffers_rg.append(buf)
+        else:
+            buffers_not_rg.append(buf)
+
+    buffer_indices_rg = {buf: idx for idx, buf in enumerate(buffers_rg)}
+    buffer_indices_not_rg = {buf: idx for idx, buf in enumerate(buffers_not_rg)}
+
+    buffer_copies_rg = _broadcast_coalesced_reshape(buffers_rg, devices, detach=detach)
+    buffer_copies_not_rg = _broadcast_coalesced_reshape(buffers_not_rg, devices, detach=True)
+
+    modules = list(network.modules())
+    module_copies = [[] for device in devices]
+    module_indices = {}
+    scriptmodule_skip_attr = {"_parameters", "_buffers", "_modules", "forward", "_c"}
+
+    for i, module in enumerate(modules):
+        module_indices[module] = i
+        for j in range(num_replicas):
+            if _is_script_module(module):
+                # we have to initialize ScriptModule properly so that
+                # it works with pybind11
+                def init_fn(script_module):
+                    # Don't do anything here, we'll initialize the ScriptModule below
+                    return
+                replica = torch.jit.RecursiveScriptModule._construct(module._c._replicate_for_data_parallel(), init_fn)
+            else:
+                replica = module._replicate_for_data_parallel()
+
+            module_copies[j].append(replica)
+
+    for i, module in enumerate(modules):
+        for key, child in module._modules.items():
+            if child is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._modules[key] = None
+            else:
+                module_idx = module_indices[child]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    setattr(replica, key, module_copies[j][module_idx])
+        for key, param in module._parameters.items():
+            if param is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._parameters[key] = None
+            else:
+                param_idx = param_indices[param]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    param = param_copies[j][param_idx]
+                    # parameters in replicas are no longer leaves, so remove them from _parameters
+                    # and setattr them as non-parameter attributes
+                    # scripted modules don't allow deleting parameters, but also don't complain
+                    # on assigning non-Parameter type
+                    if (not _is_script_module(replica)):
+                        del replica._parameters[key]
+                    setattr(replica, key, param)
+        for key, buf in module._buffers.items():
+            if buf is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._buffers[key] = None
+            else:
+                if buf.requires_grad and not detach:
+                    buffer_copies = buffer_copies_rg
+                    buffer_idx = buffer_indices_rg[buf]
+                else:
+                    buffer_copies = buffer_copies_not_rg
+                    buffer_idx = buffer_indices_not_rg[buf]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    setattr(replica, key, buffer_copies[j][buffer_idx])
+
+    return [module_copies[j][0] for j in range(num_replicas)]
+"""
