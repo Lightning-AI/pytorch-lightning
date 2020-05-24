2892e5a03a6ef952dde8943d5eca68e940cfc663
Jirka <jirka@pytorchlightning.ai>
2020-05-24 19:00:31 +0200

    flake8

--- tests/base/model_template.py
+++ tests/base/model_template.py
@@ -66,7 +66,7 @@ class EvalModelTemplate(
 
         # if you specify an example input, the summary will show input/output for each layer
         # TODO: to be fixed in #1773
-        #self.example_input_array = torch.rand(5, 28 * 28)
+        # self.example_input_array = torch.rand(5, 28 * 28)
 
         # build model
         self.__build_model()
