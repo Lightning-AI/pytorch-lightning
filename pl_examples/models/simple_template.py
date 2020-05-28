df5f2c0c047fa541b936befb6421649cd8b0f63a
William Falcon <waf2107@columbia.edu>
2020-05-28 14:58:01 -0400

    added basic template with new syntax and result object

--- pl_examples/models/simple_template.py
+++ pl_examples/models/simple_template.py
@@ -72,7 +72,7 @@ class SuperLitModel(LightningModule):
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
 
-        step_result = {'loss': loss}
+        # step_result = {'loss': loss}
         # structure the return from the training loop
         step_result.minimize = loss
         # step_result.checkpoint_on = loss
