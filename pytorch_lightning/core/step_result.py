519300450d1c9a734710035d9b86ae3d430ee17f
William Falcon <waf2107@columbia.edu>
2020-05-28 08:23:45 -0400

    added result

---
+++ pytorch_lightning/core/step_result.py
@@ -0,0 +1,55 @@
+from typing import Optional, Dict
+from torch import Tensor
+
+
+class Result:
+
+    def __init__(self, loss: Tensor,
+                 logs: Optional[Dict] = None,
+                 progress_bar_logs: Optional[Dict] = None):
+        self.loss = loss
+        self.logs = logs
+        self.progress_bar_logs = progress_bar_logs
+
+    def log(self, key, value):
+        self.logs[key] = value
+
+    def display_in_progress_bar(self, key, value):
+        self.progress_bar_logs[key] = value
+
+
+class TrainStepResult(Result):
+    """
+    Return this in the training step
+    """
+
+    def __init__(self, loss: Tensor,
+                 logs: Optional[Dict] = None,
+                 progress_bar_logs: Optional[Dict] = None):
+        """
+
+        Args:
+            loss: the loss to minimize (Tensor)
+            logs: a dictionary to pass to the logger
+            progress_bar_logs: a dictionary to pass to the progress bar
+        """
+        super().__init__(loss, logs, progress_bar_logs)
+
+
+class EvalStepResult(Result):
+    """
+    Return this in the validation step
+    """
+
+    def __init__(self, monitor_metric: Tensor,
+                 logs: Optional[Dict] = None,
+                 progress_bar_logs: Optional[Dict] = None):
+        """
+
+        Args:
+            monitor_metric: the metric to monitor for callbacks (Tensor)
+            logs: a dictionary to pass to the logger
+            progress_bar_logs: a dictionary to pass to the progress bar
+        """
+
+        super().__init__(monitor_metric, logs, progress_bar_logs)
