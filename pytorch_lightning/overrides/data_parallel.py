a362b3b096d4f338a5c0e5c643d2a7bb8d58892e
William Falcon <waf2107@columbia.edu>
2020-05-29 18:38:44 -0400

    docs

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -67,6 +67,7 @@ class LightningDataParallel(DataParallel):
 
         # structured responses break gather when not dict
         if isinstance(outputs[0], Result):
+            import pdb; pdb.set_trace()
             original_class = outputs[0].__class__
             outputs = [dict(x) for x in outputs]
             outputs = self.gather(outputs, self.output_device)
