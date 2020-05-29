a8698e0a666446fb727578f73e3265e922ead55f
Nicki Skafte <nugginea@gmail.com>
2020-05-29 14:06:06 +0200

    test for call order

--- tests/tuner/test_tuner.py
+++ tests/tuner/test_tuner.py
@@ -1,18 +1,24 @@
 import pytest
-import torch
 
 import tests.base.utils as tutils
-from pytorch_lightning import Trainer
-from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from pytorch_lightning import Trainer, Tuner
 from tests.base import EvalModelTemplate
 
 def test_call_order(tmpdir):
     """ Check that an warning occurs if the methods are called in a different
         order than expected """
-    
-    tuner = pl.Tuner()
+    tutils.reset_seed()
+
+    model = EvalModelTemplate()
+
+    # logger file to get meta
+    trainer = Trainer(
+        default_save_path=tmpdir,
+        max_epochs=1
+    )
+    tuner = Tuner(trainer)
     
     # Wrong call order, should give warning
-    with pytest.warning():
-        _ = tuner.lr_find()
-        _ = tuner.scale_batch_size()
+    with pytest.warns(UserWarning):
+        _ = tuner.lr_find(model)
+        _ = tuner.scale_batch_size(model)
