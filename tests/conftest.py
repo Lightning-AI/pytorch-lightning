b71f69abd52b0f10fd204330f2174b18abd91cee
Peter Yu <2057325+yukw777@users.noreply.github.com>
2020-05-31 13:26:01 -0400

    ThreadingHTTPServer support in 3.6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from functools import wraps, partial
-from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from http.server import SimpleHTTPRequestHandler
 
 import sys
 import pytest
@@ -26,6 +26,7 @@ def pytest_pyfunc_call(pyfuncitem):
 def tmpdir_server(tmpdir):
     if sys.version_info >= (3, 7):
         Handler = partial(SimpleHTTPRequestHandler, directory=str(tmpdir))
+        from http.server import ThreadingHTTPServer
     else:
         # unfortunately SimpleHTTPRequestHandler doesn't accept the directory arg in python3.6
         # so we have to hack it like this
@@ -39,6 +40,13 @@ def tmpdir_server(tmpdir):
                 relpath = os.path.relpath(path, os.getcwd())
                 # return the full path from root_dir
                 return os.path.join(str(tmpdir), relpath)
+
+        # ThreadingHTTPServer was added in 3.7, so we need to define it ourselves
+        from socketserver import ThreadingMixIn
+        from http.server import HTTPServer
+
+        class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+            daemon_threads = True
 
     with ThreadingHTTPServer(('', 0), Handler) as server:
         server_thread = threading.Thread(target=server.serve_forever)
