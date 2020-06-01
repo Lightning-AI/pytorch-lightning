799927f3c892296370e062ed1b183c8500dc0fa8
Jirka <jirka@pytorchlightning.ai>
2020-06-01 19:59:19 +0200

    @pytest

--- tests/trainer/test_dataloaders.py
+++ tests/trainer/test_dataloaders.py
@@ -248,9 +248,8 @@ def test_mixing_of_dataloader_options(tmpdir):
         f'`test_dataloaders` not initiated properly, got {trainer.test_dataloaders}'
 
 
+@pytest.skip('TODO: speed up this test')
 def test_train_inf_dataloader_error(tmpdir):
-    pytest.skip('TODO: fix speed of this test')
-
     """Test inf train data loader (e.g. IterableDataset)"""
     model = EvalModelTemplate()
     model.train_dataloader = model.train_dataloader__infinite
@@ -261,9 +260,8 @@ def test_train_inf_dataloader_error(tmpdir):
         trainer.fit(model)
 
 
+@pytest.skip('TODO: speed up this test')
 def test_val_inf_dataloader_error(tmpdir):
-    pytest.skip('TODO: fix speed of this test')
-
     """Test inf train data loader (e.g. IterableDataset)"""
     model = EvalModelTemplate()
     model.val_dataloader = model.val_dataloader__infinite
@@ -274,9 +272,8 @@ def test_val_inf_dataloader_error(tmpdir):
         trainer.fit(model)
 
 
+@pytest.skip('TODO: speed up this test')
 def test_test_inf_dataloader_error(tmpdir):
-    pytest.skip('TODO: fix speed of this test')
-
     """Test inf train data loader (e.g. IterableDataset)"""
     model = EvalModelTemplate()
     model.test_dataloader = model.test_dataloader__infinite
@@ -288,9 +285,8 @@ def test_test_inf_dataloader_error(tmpdir):
 
 
 @pytest.mark.parametrize('check_interval', [50, 1.0])
+@pytest.skip('TODO: speed up this test')
 def test_inf_train_dataloader(tmpdir, check_interval):
-    pytest.skip('TODO: fix speed of this test')
-
     """Test inf train data loader (e.g. IterableDataset)"""
 
     model = EvalModelTemplate()
@@ -307,9 +303,8 @@ def test_inf_train_dataloader(tmpdir, check_interval):
 
 
 @pytest.mark.parametrize('check_interval', [1.0])
+@pytest.skip('TODO: speed up this test')
 def test_inf_val_dataloader(tmpdir, check_interval):
-    pytest.skip('TODO: fix speed of this test')
-
     """Test inf val data loader (e.g. IterableDataset)"""
 
     model = EvalModelTemplate()
