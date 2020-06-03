e2820a85f5f609dcaa355cc63a609945607c84d4
Jirka <jirka@pytorchlightning.ai>
2020-06-03 16:32:33 +0200

    speed

--- benchmarks/test_parity.py
+++ benchmarks/test_parity.py
@@ -11,7 +11,7 @@ from pytorch_lightning import Trainer, seed_everything
 
 @pytest.mark.parametrize('cls_model,max_diff', [
     (ParityModuleRNN, 0.01),
-    (ParityModuleMNIST, 0.1)
+    (ParityModuleMNIST, 0.5)
 ])
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
 def test_pytorch_parity(tmpdir, cls_model, max_diff):
