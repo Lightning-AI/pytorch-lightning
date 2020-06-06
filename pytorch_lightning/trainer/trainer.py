243237e0dc8279dfb9a7e5104b835ed850dc2a31
lezwon <lezwon@synup.com>
2020-06-06 17:13:06 +0530

    fix pep 8 issues

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -363,9 +363,9 @@ class Trainer(
         self.on_tpu = tpu_cores is not None
         self.tpu_cores = tpu_cores
         assert self.tpu_cores in (1, 8, None) or (
-            isinstance(self.tpu_cores, (list, tuple, set))
-            and len(self.tpu_cores) == 1
-            and self.tpu_cores[0] in range(1, 9)
+            isinstance(self.tpu_cores, (list, tuple, set)) and
+            len(self.tpu_cores) == 1 and
+            self.tpu_cores[0] in range(1, 9)
         ), '`tpu_cores` can only be 1, 8 or [<1-8>]'
 
         self.tpu_id = tpu_cores[0] if isinstance(tpu_cores, list) else None
