99685ea4792dcdaa453ea55ec501473e9b4beddd
William Falcon <waf2107@columbia.edu>
2020-06-07 08:39:12 -0400

    Update test_hparams.py

--- tests/models/test_hparams.py
+++ tests/models/test_hparams.py
@@ -9,7 +9,8 @@ from omegaconf import OmegaConf, Container
 from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.utilities import AttributeDict
 from tests.base import EvalModelTemplate
-import pickle, cloudpickle
+import pickle
+import cloudpickle
 
 
 # -------------------------
