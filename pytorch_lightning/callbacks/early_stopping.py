479ab49d03585d22f586e46f4334670a312f549b
William Falcon <waf2107@columbia.edu>
2020-06-08 19:28:26 -0400

    temporarily fixes early stopping bug (#2119)
    * fixes early stopping bug
    
    * fixes early stopping bug
    
    * fixes early stopping bug
    
    * fixes early stopping bug
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * fixe docs
    
    * added test

--- pytorch_lightning/callbacks/early_stopping.py
+++ pytorch_lightning/callbacks/early_stopping.py
@@ -110,7 +110,7 @@ class EarlyStopping(Callback):
         self.best = torch_inf if self.monitor_op == torch.lt else -torch_inf
 
     def on_validation_end(self, trainer, pl_module):
-        self._run_early_stopping_check(trainer, pl_module)
+        return self._run_early_stopping_check(trainer, pl_module)
 
     def _run_early_stopping_check(self, trainer, pl_module):
         logs = trainer.callback_metrics
