1eebf54535c0cc069799b86b725af8dfa230c675
William Falcon <waf2107@columbia.edu>
2020-06-08 16:03:32 -0400

    training batch clean up

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -345,6 +345,7 @@ class Trainer(
                 "track_grad_norm can be an int, a float or 'inf' (infinity norm).")
         self.track_grad_norm = float(track_grad_norm)
 
+        import pdb; pdb.set_trace()
         self.on_gpu = True if (gpus and torch.cuda.is_available()) else False
 
         # tpu config
