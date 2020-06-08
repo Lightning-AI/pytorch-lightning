55295a91dd092c17c61de8079f75501995b78152
William Falcon <waf2107@columbia.edu>
2020-06-08 18:13:46 -0400

    fixe docs

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -826,8 +826,6 @@ class Trainer(
         model.logger = self.logger
         self.copy_trainer_model_properties(model)
 
-        import pdb; pdb.set_trace()
-
         # clean hparams
         if hasattr(model, 'hparams'):
             parsing.clean_namespace(model.hparams)
