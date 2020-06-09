e11cf461145c3093e3cd1097f10a4d5ce95a8010
Jirka Borovec <Borda@users.noreply.github.com>
2020-06-09 22:50:59 +0200

    setup py 3.8 (#2135)

--- setup.py
+++ setup.py
@@ -94,5 +94,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
