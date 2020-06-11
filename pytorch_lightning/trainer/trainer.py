e51124edc0da6f9b9bb3a22ea4313e7e706603c8
Jirka <jirka@pytorchlightning.ai>
2020-06-11 20:08:28 +0200

    amp_level

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -123,7 +123,7 @@ class Trainer(
         replace_sampler_ddp: bool = True,
         terminate_on_nan: bool = False,
         auto_scale_batch_size: Union[str, bool] = False,
-        amp_level: str = 'O1',  # backward compatible, todo: remove in v0.9.0
+        amp_level: str = 'O1',  # backward compatible, todo: remove in v1.0.0
         num_tpu_cores: Optional[int] = None,  # backward compatible, todo: remove in v0.9.0
         use_amp=None,  # backward compatible, todo: remove in v0.9.0
         show_progress_bar=None,  # backward compatible, todo: remove in v0.9.0
