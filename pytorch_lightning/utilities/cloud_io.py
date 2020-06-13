60848c40e2840f84febd4908a53acab4c6780e33
Jirka <jirka@pytorchlightning.ai>
2020-06-13 19:30:03 +0200

    flake8

--- pytorch_lightning/utilities/cloud_io.py
+++ pytorch_lightning/utilities/cloud_io.py
@@ -1,7 +1,7 @@
-import torch
-
-from pathlib import Path
+import pickle
 from urllib.parse import urlparse
+
+import torch
 
 
 def load(path_or_url: str, map_location=None):
@@ -14,18 +14,15 @@ def load(path_or_url: str, map_location=None):
     return torch.hub.load_state_dict_from_url(path_or_url, map_location=map_location)
 
 
-def load_s3_checkpoint(checkpoint_path, map_location, **pickle_load_args):
+def load_s3_checkpoint(checkpoint_path: str, map_location, **pickle_load_args):
     from torch.serialization import _legacy_load
-    import pickle
 
     # Attempt s3fs import
     try:
         import s3fs
     except ImportError:
-        raise ImportError(
-            f"Tried to import `s3fs` for AWS S3 i/o, but `s3fs` is not installed. "
-            f"Please `pip install s3fs` and try again."
-        )
+        raise ImportError("Tried to import `s3fs` for AWS S3 i/o, but `s3fs` is not installed."
+                          " Please `pip install s3fs` and try again.")
 
     if 'encoding' not in pickle_load_args.keys():
         pickle_load_args['encoding'] = 'utf-8'
@@ -37,16 +34,13 @@ def load_s3_checkpoint(checkpoint_path, map_location, **pickle_load_args):
 
 def save_s3_checkpoint(checkpoint, checkpoint_path):
     from torch.serialization import _legacy_save
-    import pickle
 
     # Attempt s3fs import
     try:
         import s3fs
     except ImportError:
-        raise ImportError(
-            f'Tried to import `s3fs` for AWS S3 i/o, but `s3fs` is not installed. '
-            f'Please `pip install s3fs` and try again.'
-        )
+        raise ImportError("Tried to import `s3fs` for AWS S3 i/o, but `s3fs` is not installed."
+                          " Please `pip install s3fs` and try again.")
 
     DEFAULT_PROTOCOL = 2  # from torch.serialization.py line 19
     fs = s3fs.S3FileSystem()
