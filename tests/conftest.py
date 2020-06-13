d27d7becb5ddd094c466ae1a5dd8c318387eda8f
Laksh Aithani <laithani@ip-192-168-1-147.eu-west-1.compute.internal>
2020-06-13 23:36:31 +0100

    Made start on moto testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from http.server import SimpleHTTPRequestHandler
 
 import pytest
 import torch.multiprocessing as mp
+import boto3
+from moto import mock_s3
+import os
 
 
 def pytest_configure(config):
@@ -55,3 +58,20 @@ def tmpdir_server(tmpdir):
         server_thread.start()
         yield server.server_address
         server.shutdown()
+
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with mock_s3():
+        s3 = boto3.client('s3')
+        s3.create_bucket(Bucket='testing')
+        yield s3
