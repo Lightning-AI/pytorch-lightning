d2bb8cdeb9a75ad5d63b3c00c1fc74c0529e0f64
Jirka <jirka@pytorchlightning.ai>
2020-06-16 09:01:08 +0200

    fix tests

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -451,7 +451,7 @@ def test_trainer_max_steps_and_epochs(tmpdir):
 
     # check training stopped at max_epochs
     assert trainer.global_step == num_train_samples * trainer.max_epochs
-    assert trainer.current_epoch == trainer.max_epochs - 1, "Model did not stop at max_epochs"
+    assert trainer.current_epoch == trainer.max_epochs, "Model did not stop at max_epochs"
 
 
 def test_trainer_min_steps_and_epochs(tmpdir):
@@ -619,7 +619,7 @@ def test_disabled_validation():
 
     # check that val_percent_check=0 turns off validation
     assert result == 1, 'training failed to complete'
-    assert trainer.current_epoch == 1
+    assert trainer.current_epoch == 2
     assert not model.validation_step_invoked, \
         '`validation_step` should not run when `val_percent_check=0`'
     assert not model.validation_epoch_end_invoked, \
