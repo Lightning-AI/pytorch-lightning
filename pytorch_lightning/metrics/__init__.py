5a1d9d09a97019add2672d18504b0aaeff43831f
William Falcon <waf2107@columbia.edu>
2020-06-17 13:48:36 -0400

    allow regression metrics to import

--- pytorch_lightning/metrics/__init__.py
+++ pytorch_lightning/metrics/__init__.py
@@ -1,5 +1,11 @@
 from pytorch_lightning.metrics.converters import numpy_metric, tensor_metric
 from pytorch_lightning.metrics.metric import Metric, TensorMetric, NumpyMetric
+from pytorch_lightning.metrics.regression import (
+    MSE,
+    RMSE,
+    MAE,
+    RMSLE
+)
 from pytorch_lightning.metrics.classification import (
     Accuracy,
     AveragePrecision,
