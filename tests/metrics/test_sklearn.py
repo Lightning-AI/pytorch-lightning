f1c732a77b9f15b3aa0cdf3f6644162a97f2bccd
Nicki Skafte <skaftenicki@gmail.com>
2020-06-17 13:34:39 +0200

    Metric docs fix (#2209)
    * fix docs
    
    * Update docs/source/metrics.rst
    
    * Update docs/source/metrics.rst
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    
    * Update docs/source/metrics.rst
    
    * Update docs/source/metrics.rst
    
    * Update metrics.rst
    
    * title
    
    * fix
    
    * fix for num_classes
    
    * chlog
    
    * nb classes
    
    * hints
    
    * zero division
    
    * add tests
    
    * Update metrics.rst
    
    * Update classification.py
    
    * Update classification.py
    
    * prune doctests
    
    * docs
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * flake8
    
    * doctests
    
    * formatting
    
    * cleaning
    
    * formatting
    
    * formatting
    
    * doctests
    
    * flake8
    
    * docs
    
    * rename
    
    * rename
    
    * typo
    
    Co-authored-by: Nicki Skafte <nugginea@gmail.com>
    Co-authored-by: William Falcon <waf2107@columbia.edu>
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: Jirka <jirka@pytorchlightning.ai>
    Co-authored-by: edenlightning <66261195+edenlightning@users.noreply.github.com>

--- tests/metrics/test_sklearn.py
+++ tests/metrics/test_sklearn.py
@@ -6,75 +6,84 @@ import numpy as np
 import pytest
 import torch
 from sklearn.metrics import (
-    accuracy_score,
-    average_precision_score,
-    auc,
-    confusion_matrix,
-    f1_score,
-    fbeta_score,
-    precision_score,
-    recall_score,
-    precision_recall_curve,
-    roc_curve,
-    roc_auc_score
+    accuracy_score as sk_accuracy,
+    precision_score as sk_precision,
+    recall_score as sk_recall,
+    f1_score as sk_f1_score,
+    fbeta_score as sk_fbeta_score,
+    confusion_matrix as sk_confusion_matrix,
+    average_precision_score as sk_average_precision,
+    auc as sk_auc,
+    precision_recall_curve as sk_precision_recall_curve,
+    roc_curve as sk_roc_curve,
+    roc_auc_score as sk_roc_auc_score,
 )
 
 from pytorch_lightning.metrics.converters import _convert_to_numpy
-from pytorch_lightning.metrics.sklearn import (
-    Accuracy, AveragePrecision, AUC, ConfusionMatrix, F1, FBeta,
-    Precision, Recall, PrecisionRecallCurve, ROC, AUROC)
+from pytorch_lightning.metrics.sklearns import (
+    Accuracy,
+    AveragePrecision,
+    AUC,
+    ConfusionMatrix,
+    F1,
+    FBeta,
+    Precision,
+    Recall,
+    PrecisionRecallCurve,
+    ROC,
+    AUROC
+)
 from pytorch_lightning.utilities.apply_func import apply_to_collection
 
 
-def xy_only(func):
+def _xy_only(func):
     def new_func(*args, **kwargs):
         return np.array(func(*args, **kwargs)[:2])
-
     return new_func
 
 
 @pytest.mark.parametrize(['metric_class', 'sklearn_func', 'inputs'], [
-    pytest.param(Accuracy(), accuracy_score,
-                 {'y_pred': torch.randint(low=0, high=10, size=(128,)),
-                  'y_true': torch.randint(low=0, high=10, size=(128,))},
+    pytest.param(Accuracy(), sk_accuracy,
+                 {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='Accuracy'),
-    pytest.param(AUC(), auc, {'x': torch.arange(10, dtype=torch.float) / 10,
-                              'y': torch.tensor([0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.5, 0.6, 0.7])},
+    pytest.param(AUC(), sk_auc,
+                 {'x': torch.arange(10, dtype=torch.float) / 10,
+                  'y': torch.tensor([0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.5, 0.6, 0.7])},
                  id='AUC'),
-    pytest.param(AveragePrecision(), average_precision_score,
+    pytest.param(AveragePrecision(), sk_average_precision,
                  {'y_score': torch.randint(2, size=(128,)), 'y_true': torch.randint(2, size=(128,))},
                  id='AveragePrecision'),
-    pytest.param(ConfusionMatrix(), confusion_matrix,
+    pytest.param(ConfusionMatrix(), sk_confusion_matrix,
                  {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='ConfusionMatrix'),
-    pytest.param(F1(average='macro'), partial(f1_score, average='macro'),
+    pytest.param(F1(average='macro'), partial(sk_f1_score, average='macro'),
                  {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='F1'),
-    pytest.param(FBeta(beta=0.5, average='macro'), partial(fbeta_score, beta=0.5, average='macro'),
+    pytest.param(FBeta(beta=0.5, average='macro'), partial(sk_fbeta_score, beta=0.5, average='macro'),
                  {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='FBeta'),
-    pytest.param(Precision(average='macro'), partial(precision_score, average='macro'),
+    pytest.param(Precision(average='macro'), partial(sk_precision, average='macro'),
                  {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='Precision'),
-    pytest.param(Recall(average='macro'), partial(recall_score, average='macro'),
+    pytest.param(Recall(average='macro'), partial(sk_recall, average='macro'),
                  {'y_pred': torch.randint(10, size=(128,)), 'y_true': torch.randint(10, size=(128,))},
                  id='Recall'),
-    pytest.param(PrecisionRecallCurve(), xy_only(precision_recall_curve),
+    pytest.param(PrecisionRecallCurve(), _xy_only(sk_precision_recall_curve),
                  {'probas_pred': torch.rand(size=(128,)), 'y_true': torch.randint(2, size=(128,))},
                  id='PrecisionRecallCurve'),
-    pytest.param(ROC(), xy_only(roc_curve),
+    pytest.param(ROC(), _xy_only(sk_roc_curve),
                  {'y_score': torch.rand(size=(128,)), 'y_true': torch.randint(2, size=(128,))},
                  id='ROC'),
-    pytest.param(AUROC(), roc_auc_score,
+    pytest.param(AUROC(), sk_roc_auc_score,
                  {'y_score': torch.rand(size=(128,)), 'y_true': torch.randint(2, size=(128,))},
                  id='AUROC'),
 ])
-def test_sklearn_metric(metric_class, sklearn_func, inputs: dict):
-    numpy_inputs = apply_to_collection(
-        inputs, (torch.Tensor, np.ndarray, numbers.Number), _convert_to_numpy)
+def test_sklearn_metric(metric_class, sklearn_func, inputs):
+    numpy_inputs = apply_to_collection(inputs, (torch.Tensor, np.ndarray, numbers.Number), _convert_to_numpy)
 
     sklearn_result = sklearn_func(**numpy_inputs)
     lightning_result = metric_class(**inputs)
+    assert np.allclose(sklearn_result, lightning_result, atol=1e-5)
 
     sklearn_result = apply_to_collection(
         sklearn_result, (torch.Tensor, np.ndarray, numbers.Number), _convert_to_numpy)
@@ -82,17 +91,5 @@ def test_sklearn_metric(metric_class, sklearn_func, inputs: dict):
     lightning_result = apply_to_collection(
         lightning_result, (torch.Tensor, np.ndarray, numbers.Number), _convert_to_numpy)
 
+    assert np.allclose(sklearn_result, lightning_result, atol=1e-5)
     assert isinstance(lightning_result, type(sklearn_result))
-
-    if isinstance(lightning_result, np.ndarray):
-        assert np.allclose(lightning_result, sklearn_result)
-    elif isinstance(lightning_result, Mapping):
-        for key in lightning_result.keys():
-            assert np.allclose(lightning_result[key], sklearn_result[key])
-
-    elif isinstance(lightning_result, Sequence):
-        for val_lightning, val_sklearn in zip(lightning_result, sklearn_result):
-            assert np.allclose(val_lightning, val_sklearn)
-
-    else:
-        raise TypeError
