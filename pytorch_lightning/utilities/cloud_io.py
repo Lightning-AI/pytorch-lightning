de959c6be384d18423c2f737bface2a2a198a2d3
Vincent Thibault <vinthi96@gmail.com>
2020-06-18 16:28:43 -0400

    Fixed the load_from_checkpoint path detected as URL bug

--- pytorch_lightning/utilities/cloud_io.py
+++ pytorch_lightning/utilities/cloud_io.py
@@ -1,10 +1,12 @@
 import torch
 
-from pathlib import Path
+from Pathlib import Path
+from urllib.parse import urlparse
 
 
 def load(path_or_url: str, map_location=None):
-    if Path(path_or_url).is_file():
-        # local file
+    parsed = urlparse(path_or_url)
+    if parsed.scheme == '' or Path(path_or_url).is_file():
+        # no scheme or local file
         return torch.load(path_or_url, map_location=map_location)
     return torch.hub.load_state_dict_from_url(path_or_url, map_location=map_location)
