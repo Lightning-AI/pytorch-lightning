6efb65a6fd33ff72594ba5587424b083673f65cf
William Falcon <waf2107@columbia.edu>
2020-06-18 22:46:09 -0400

    decrease some training times

--- tests/models/test_hooks.py
+++ tests/models/test_hooks.py
@@ -57,7 +57,7 @@ def test_training_epoch_end_metrics_collection(tmpdir):
     trainer = Trainer(
         max_epochs=num_epochs,
         default_root_dir=tmpdir,
-        overfit_batches=0.1,
+        overfit_batches=2,
     )
     result = trainer.fit(model)
     assert result == 1
