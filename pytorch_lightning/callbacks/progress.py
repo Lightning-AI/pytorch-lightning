Lightning-AI/pytorch-lightning

commit 04c794ca72f45dd492e49baff773c2b8e274b9ed

[WIP] Rename overfit_pct to overfit_batches (and fix) and val_percent_check and test_percent_check (and fix) (#2213)
* fixed percent check for val/test

* fixed percent check for val/test

* fixed percent check for val/test

* fixed percent check for val/test

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

* overfit_pct now uses train loaders for val and test and does not shuffle

pytorch_lightning/callbacks/progress.py
@@ -98,6 +98,7 @@ class ProgressBarBase(Callback):
         elif not self.trainer.disable_validation:
             is_val_epoch = trainer.current_epoch % trainer.check_val_every_n_epoch == 0
             total_val_batches = trainer.num_val_batches if is_val_epoch else 0
+            total_val_batches = sum(total_val_batches)
         return total_val_batches
 
     @property
@@ -111,6 +112,7 @@ class ProgressBarBase(Callback):
             total_test_batches = len(self.trainer.test_dataloaders)
         else:
             total_test_batches = self.trainer.num_test_batches
+            total_test_batches = sum(total_test_batches)
         return total_test_batches
 
     def disable(self):

pytorch_lightning/trainer/__init__.py
@@ -433,6 +433,40 @@ Example::
     # default used by the Trainer
     trainer = Trainer(gradient_clip_val=0.0)
 
+
+limit_test_batches
+^^^^^^^^^^^^^^^^^^
+
+How much of test dataset to check.
+
+Example::
+
+    # default used by the Trainer
+    trainer = Trainer(limit_test_batches=1.0)
+
+    # run through only 25% of the test set each epoch
+    trainer = Trainer(limit_test_batches=0.25)
+
+    # run for only 10 batches
+    trainer = Trainer(limit_test_batches=10)
+
+limit_val_batches
+^^^^^^^^^^^^^^^^^
+
+How much of validation dataset to check.
+Useful when debugging or testing something that happens at the end of an epoch.
+
+Example::
+
+    # default used by the Trainer
+    trainer = Trainer(limit_val_batches=1.0)
+
+    # run through only 25% of the validation set each epoch
+    trainer = Trainer(limit_val_batches=0.25)
+
+        # run for only 10 batches
+    trainer = Trainer(limit_val_batches=10)
+
 log_gpu_memory
 ^^^^^^^^^^^^^^
 Options:
@@ -652,29 +686,28 @@ Example::
 
 overfit_pct
 ^^^^^^^^^^^
-Uses this much data of all datasets (training, validation, test).
+
+.. warning:: .. deprecated:: 0.8.0.
+
+    Use `overfit_batches`. Will remove 1.0.0.
+
+overfit_batches
+^^^^^^^^^^^^^^^
+Uses this much data of the training set. If will use the same training set for validation and testing.
+If the training Dataloaders(shuffle=True), Lightning will automatically disable it.
+
 Useful for quickly debugging or trying to overfit on purpose.
 
 Example::
 
     # default used by the Trainer
-    trainer = Trainer(overfit_pct=0.0)
+    trainer = Trainer(overfit_batches=0.0)
 
-    # use only 1% of the train, test, val datasets
-    trainer = Trainer(overfit_pct=0.01)
+    # use only 1% of the train set (and use the train set for val and test)
+    trainer = Trainer(overfit_batches=0.01)
 
-    # equivalent:
-    trainer = Trainer(
-        train_percent_check=0.01,
-        val_percent_check=0.01,
-        test_percent_check=0.01
-    )
-
-See Also:
-    - `train_percent_check`_
-    - `val_percent_check`_
-    - `test_percent_check`_
-
+    # overfit on 10 of the same batches
+    trainer = Trainer(overfit_batches=10)
 
 precision
 ^^^^^^^^^
@@ -829,39 +862,7 @@ show_progress_bar
 test_percent_check
 ^^^^^^^^^^^^^^^^^^
 
-How much of test dataset to check.
-
-Example::
-
-    # default used by the Trainer
-    trainer = Trainer(test_percent_check=1.0)
-
-    # run through only 25% of the test set each epoch
-    trainer = Trainer(test_percent_check=0.25)
-
-val_check_interval
-^^^^^^^^^^^^^^^^^^
-
-How often within one training epoch to check the validation set.
-Can specify as float or int.
-
-- use (float) to check within a training epoch
-- use (int) to check every n steps (batches)
-
-.. code-block:: python
-
-    # default used by the Trainer
-    trainer = Trainer(val_check_interval=1.0)
-
-Example::
-
-    # check validation set 4 times during a training epoch
-    trainer = Trainer(val_check_interval=0.25)
-
-    # check validation set every 1000 training batches
-    # use this when using iterableDataset and your dataset has no length
-    # (ie: production cases with streaming data)
-    trainer = Trainer(val_check_interval=1000)
+.. warning:: deprecated in v0.8.0 please use `limit_test_batches`. Will remove in 1.0.0
 
 track_grad_norm
 ^^^^^^^^^^^^^^^
@@ -955,20 +956,36 @@ override :meth:`pytorch_lightning.core.LightningModule.tbptt_split_batch`:
                 # do your own splitting on the batch
                 return splits
 
+val_check_interval
+^^^^^^^^^^^^^^^^^^
+
+How often within one training epoch to check the validation set.
+Can specify as float or int.
+
+- use (float) to check within a training epoch
+- use (int) to check every n steps (batches)
+
+.. code-block:: python
+
+    # default used by the Trainer
+    trainer = Trainer(val_check_interval=1.0)
+
+Example::
+
+    # check validation set 4 times during a training epoch
+    trainer = Trainer(val_check_interval=0.25)
+
+    # check validation set every 1000 training batches
+    # use this when using iterableDataset and your dataset has no length
+    # (ie: production cases with streaming data)
+    trainer = Trainer(val_check_interval=1000)
+
 
 val_percent_check
 ^^^^^^^^^^^^^^^^^
 
-How much of validation dataset to check.
-Useful when debugging or testing something that happens at the end of an epoch.
+.. warning:: deprecated in v0.8.0 please use `limit_val_batches`. Will remove in 1.0.0
 
-Example::
-
-    # default used by the Trainer
-    trainer = Trainer(val_percent_check=1.0)
-
-    # run through only 25% of the validation set each epoch
-    trainer = Trainer(val_percent_check=0.25)
 
 weights_save_path
 ^^^^^^^^^^^^^^^^^

pytorch_lightning/trainer/data_loading.py
@@ -70,12 +70,12 @@ class TrainerDataLoadingMixin(ABC):
     num_training_batches: Union[int, float]
     val_check_batch: ...
     val_dataloaders: List[DataLoader]
-    num_val_batches: Union[int, float]
+    num_val_batches: List[Union[int, float]]
     test_dataloaders: List[DataLoader]
-    num_test_batches: Union[int, float]
+    num_test_batches: List[Union[int, float]]
     train_percent_check: float
-    val_percent_check: float
-    test_percent_check: float
+    limit_val_batches: float
+    limit_test_batches: float
     replace_sampler_ddp: bool
     num_nodes: int
     num_processes: int
@@ -85,11 +85,16 @@ class TrainerDataLoadingMixin(ABC):
     def is_overridden(self, *args):
         """Warning: this is just empty shell for code implemented in other class."""
 
-    def _percent_range_check(self, name: str) -> None:
+    def _limit_eval_batches_check(self, name: str) -> None:
         value = getattr(self, name)
+
+        # ints are fine
+        if isinstance(value, int):
+            return
+
         msg = f'`{name}` must lie in the range [0.0, 1.0], but got {value:.3f}.'
         if name == 'val_check_interval':
-            msg += ' If you want to disable validation set `val_percent_check` to 0.0 instead.'
+            msg += ' If you want to disable validation set `limit_val_batches` to 0.0 instead.'
 
         if not 0. <= value <= 1.:
             raise ValueError(msg)
@@ -139,15 +144,21 @@ class TrainerDataLoadingMixin(ABC):
                     ' distributed training. Either remove the sampler from your DataLoader or set'
                     ' `replace_sampler_ddp`=False if you want to use your custom sampler.')
 
-            skip_keys = ['sampler', 'batch_sampler', 'dataset_kind']
+            # replace with distributed sampler
+            sampler = self._get_distributed_sampler(dataloader)
+            dataloader = self.replace_sampler(dataloader, sampler)
 
-            dl_args = {
-                k: v for k, v in dataloader.__dict__.items() if not k.startswith('_') and k not in skip_keys
-            }
+        return dataloader
 
-            dl_args['sampler'] = self._get_distributed_sampler(dataloader)
-            dataloader = type(dataloader)(**dl_args)
+    def replace_sampler(self, dataloader, sampler):
+        skip_keys = ['sampler', 'batch_sampler', 'dataset_kind']
 
+        dl_args = {
+            k: v for k, v in dataloader.__dict__.items() if not k.startswith('_') and k not in skip_keys
+        }
+
+        dl_args['sampler'] = sampler
+        dataloader = type(dataloader)(**dl_args)
         return dataloader
 
     def _get_distributed_sampler(self, dataloader):
@@ -182,14 +193,17 @@ class TrainerDataLoadingMixin(ABC):
         self.train_dataloader = self.auto_add_sampler(self.train_dataloader, train=True)
 
         self._worker_check(self.train_dataloader, 'train dataloader')
-        self._percent_range_check('train_percent_check')
+        self._limit_eval_batches_check('train_percent_check')
 
         if not _has_len(self.train_dataloader):
             self.num_training_batches = float('inf')
         else:
             # try getting the length
-            self.num_training_batches = len(self.train_dataloader)
-            self.num_training_batches = int(self.num_training_batches * self.train_percent_check)
+            if isinstance(self.train_percent_check, float):
+                self.num_training_batches = len(self.train_dataloader)
+                self.num_training_batches = int(self.num_training_batches * self.train_percent_check)
+            else:
+                self.num_training_batches = self.train_percent_check
 
         # determine when to check validation
         # if int passed in, val checks that often
@@ -200,7 +214,7 @@ class TrainerDataLoadingMixin(ABC):
                 raise ValueError(
                     f'`val_check_interval` ({self.val_check_interval}) must be less than or equal '
                     f'to the number of the training batches ({self.num_training_batches}). '
-                    'If you want to disable validation set `val_percent_check` to 0.0 instead.')
+                    'If you want to disable validation set `limit_val_batches` to 0.0 instead.')
         else:
             if not _has_len(self.train_dataloader):
                 if self.val_check_interval == 1.0:
@@ -212,12 +226,14 @@ class TrainerDataLoadingMixin(ABC):
                         ' `Trainer(val_check_interval)` must be `1.0` or an int. An int k specifies'
                         ' checking validation every k training batches.')
             else:
-                self._percent_range_check('val_check_interval')
+                self._limit_eval_batches_check('val_check_interval')
 
                 self.val_check_batch = int(self.num_training_batches * self.val_check_interval)
                 self.val_check_batch = max(1, self.val_check_batch)
 
-    def _reset_eval_dataloader(self, model: LightningModule, mode: str) -> Tuple[Union[int, float], List[DataLoader]]:
+    def _reset_eval_dataloader(self,
+                               model: LightningModule,
+                               mode: str) -> Tuple[List[Union[int, float]], List[DataLoader]]:
         """Generic method to reset a dataloader for evaluation.
 
         Args:
@@ -227,17 +243,32 @@ class TrainerDataLoadingMixin(ABC):
         Returns:
             Tuple (num_batches, dataloaders)
         """
-        dataloaders = self.request_dataloader(getattr(model, f'{mode}_dataloader'))
+        # use the training loader as val and test when overfitting
+        if self.overfit_batches > 0:
+            dataloaders = self.request_dataloader(getattr(model, 'train_dataloader'))
+        else:
+            dataloaders = self.request_dataloader(getattr(model, f'{mode}_dataloader'))
 
         if not isinstance(dataloaders, list):
             dataloaders = [dataloaders]
 
-        # shuffling in val and test set is bad practice
-        for loader in dataloaders:
+        for loader_i in range(len(dataloaders)):
+            loader = dataloaders[loader_i]
+
+            # shuffling in val and test set is bad practice
             if mode in ('val', 'test') and hasattr(loader, 'sampler') and isinstance(loader.sampler, RandomSampler):
-                rank_zero_warn(
-                    f'Your {mode}_dataloader has shuffle=True, it is best practice to turn'
-                    ' this off for validation and test dataloaders.')
+
+                # when overfitting, the dataloader should not have sampler
+                if self.overfit_batches > 0:
+                    m = 'You requested to overfit but enabled training Dataloader shuffling. ' \
+                        'we are turning it off for you'
+                    rank_zero_warn(m)
+                    dataloaders[loader_i] = self.replace_sampler(loader, SequentialSampler(loader.dataset))
+
+                else:
+                    rank_zero_warn(
+                        f'Your {mode}_dataloader has shuffle=True, it is best practice to turn'
+                        ' this off for validation and test dataloaders.')
 
         if any([dl is None for dl in dataloaders]):
             rank_zero_warn("One of given dataloaders is None and it will be skipped.")
@@ -245,29 +276,47 @@ class TrainerDataLoadingMixin(ABC):
         # add samplers
         dataloaders = [self.auto_add_sampler(dl, train=False) for dl in dataloaders if dl is not None]
 
-        num_batches = 0
+        loader_num_batches = []
 
         # determine number of batches
         # datasets could be none, 1 or 2+
         if len(dataloaders) != 0:
             for i, dataloader in enumerate(dataloaders):
+                num_batches = 0
                 self._worker_check(dataloader, f'{mode} dataloader {i}')
                 if not _has_len(dataloader):
                     num_batches = float('inf')
 
-            percent_check = getattr(self, f'{mode}_percent_check')
+                # percent or num_steps
+                limit_eval_batches = getattr(self, f'limit_{mode}_batches')
 
-            if num_batches != float('inf'):
-                self._percent_range_check(f'{mode}_percent_check')
+                if num_batches != float('inf'):
+                    self._limit_eval_batches_check(f'limit_{mode}_batches')
 
-                num_batches = sum(len(dataloader) for dataloader in dataloaders)
-                num_batches = int(num_batches * percent_check)
-            elif percent_check not in (0.0, 1.0):
-                raise MisconfigurationException(
-                    'When using an infinite DataLoader (e.g. with an IterableDataset'
-                    f' or when DataLoader does not implement `__len__`) for `{mode}_dataloader`,'
-                    f' `Trainer({mode}_percent_check)` must be `0.0` or `1.0`.')
-        return num_batches, dataloaders
+                    num_batches = len(dataloader)
+
+                    # limit num batches either as a percent or num steps
+                    if isinstance(limit_eval_batches, float):
+                        num_batches = int(num_batches * limit_eval_batches)
+                    else:
+                        num_batches = limit_eval_batches
+
+                elif limit_eval_batches not in (0.0, 1.0):
+                    raise MisconfigurationException(
+                        'When using an infinite DataLoader (e.g. with an IterableDataset'
+                        f' or when DataLoader does not implement `__len__`) for `limit_{mode}_batches`,'
+                        f' `Trainer(limit_{mode}_batches)` must be `0.0` or `1.0`.')
+
+                if num_batches == 0 and limit_eval_batches > 0.0 and isinstance(limit_eval_batches, float):
+                    min_pct = 1.0 / len(dataloader)
+                    m = f'you requested to check {limit_eval_batches} of the {mode} dataloader but ' \
+                        f'{limit_eval_batches}*{num_batches} = 0. Please increase the limit_{mode}_batches.' \
+                        f'Try at least limit_{mode}_batches={min_pct}'
+                    raise MisconfigurationException(m)
+
+                loader_num_batches.append(num_batches)
+
+        return loader_num_batches, dataloaders
 
     def reset_val_dataloader(self, model: LightningModule) -> None:
         """Resets the validation dataloader and determines the number of batches.
@@ -316,18 +365,19 @@ class TrainerDataLoadingMixin(ABC):
 
         return dataloader
 
-    def determine_data_use_amount(self, train_percent_check: float, val_percent_check: float,
-                                  test_percent_check: float, overfit_pct: float) -> None:
+    def determine_data_use_amount(self, train_percent_check: float, limit_val_batches: Union[int, float],
+                                  limit_test_batches: Union[int, float], overfit_batches: float) -> None:
         """Use less data for debugging purposes
         """
         self.train_percent_check = train_percent_check
-        self.val_percent_check = val_percent_check
-        self.test_percent_check = test_percent_check
-        if overfit_pct > 0:
-            if overfit_pct > 1:
+        self.limit_val_batches = limit_val_batches
+        self.limit_test_batches = limit_test_batches
+        if overfit_batches > 0:
+            if isinstance(overfit_batches, float) and overfit_batches > 1:
                 raise ValueError(
-                    f'`overfit_pct` must be not greater than 1.0, but got {overfit_pct:.3f}.')
+                    f'`overfit_batches` when used as a percentage must '
+                    f'be not 0.0 < x < 1.0 but got {overfit_batches:.3f}.')
 
-            self.train_percent_check = overfit_pct
-            self.val_percent_check = overfit_pct
-            self.test_percent_check = overfit_pct
+            self.train_percent_check = overfit_batches
+            self.limit_val_batches = overfit_batches
+            self.limit_test_batches = overfit_batches

pytorch_lightning/trainer/evaluation_loop.py
@@ -24,30 +24,30 @@ Set how much of the validation set to check
 
 If you don't want to check 100% of the validation set (for debugging or if it's huge), set this flag
 
-val_percent_check will be overwritten by overfit_pct if `overfit_pct > 0`
+limit_val_batches will be overwritten by overfit_batches if `overfit_batches > 0`
 
 .. code-block:: python
 
     # DEFAULT
-    trainer = Trainer(val_percent_check=1.0)
+    trainer = Trainer(limit_val_batches=1.0)
 
     # check 10% only
-    trainer = Trainer(val_percent_check=0.1)
+    trainer = Trainer(limit_val_batches=0.1)
 
 Set how much of the test set to check
 -------------------------------------
 
 If you don't want to check 100% of the test set (for debugging or if it's huge), set this flag
 
-test_percent_check will be overwritten by overfit_pct if `overfit_pct > 0`
+limit_test_batches will be overwritten by overfit_batches if `overfit_batches > 0`
 
 .. code-block:: python
 
     # DEFAULT
-    trainer = Trainer(test_percent_check=1.0)
+    trainer = Trainer(limit_test_batches=1.0)
 
     # check 10% only
-    trainer = Trainer(test_percent_check=0.1)
+    trainer = Trainer(limit_test_batches=0.1)
 
 Set validation check frequency within 1 training epoch
 ------------------------------------------------------
@@ -124,7 +124,7 @@ In this second case, the options you pass to trainer will be used when running
 
 from abc import ABC, abstractmethod
 from pprint import pprint
-from typing import Callable, Optional
+from typing import Callable, Optional, List
 
 import torch
 from torch.utils.data import DataLoader
@@ -162,7 +162,7 @@ class TrainerEvaluationLoopMixin(ABC):
     single_gpu: bool
     data_parallel_device_ids: ...
     model: LightningModule
-    num_test_batches: int
+    num_test_batches: List[int]
     num_val_batches: int
     fast_dev_run: ...
     process_output: ...
@@ -222,13 +222,13 @@ class TrainerEvaluationLoopMixin(ABC):
     def reset_val_dataloader(self, *args):
         """Warning: this is just empty shell for code implemented in other class."""
 
-    def _evaluate(self, model: LightningModule, dataloaders, max_batches: int, test_mode: bool = False):
+    def _evaluate(self, model: LightningModule, dataloaders, max_batches: List[int], test_mode: bool = False):
         """Run evaluation code.
 
         Args:
             model: PT model
             dataloaders: list of PT dataloaders
-            max_batches: Scalar
+            max_batches: List of scalars
             test_mode:
         """
         # enable eval mode
@@ -254,12 +254,15 @@ class TrainerEvaluationLoopMixin(ABC):
                 dataloader = xla_pl.ParallelLoader(dataloader, [device])
                 dataloader = dataloader.per_device_loader(device)
 
+            # each dataloader has a max num batches
+            dl_max_batches = max_batches[dataloader_idx]
+
             for batch_idx, batch in enumerate(dataloader):
                 if batch is None:
                     continue
 
                 # stop short when on fast_dev_run (sets max_batch=1)
-                if batch_idx >= max_batches:
+                if batch_idx >= dl_max_batches:
                     break
 
                 # callbacks
@@ -359,13 +362,18 @@ class TrainerEvaluationLoopMixin(ABC):
 
         # cap max batches to 1 when using fast_dev_run
         if self.fast_dev_run:
-            max_batches = 1
+            max_batches = [1]
 
         # Validation/Test begin callbacks
         if test_mode:
             self.on_test_start()
         else:
             self.on_validation_start()
+
+        # enable disabling validation step with limit_val_batches = 0
+        should_skip = sum(max_batches) == 0
+        if should_skip:
+            return
 
         # run evaluation
         eval_results = self._evaluate(self.model, dataloaders, max_batches, test_mode)

commit ca7a4b00456bb99259b08af28d9f32634ee3fca2

Update progress.py
Fixes a minor bug introduced in #2213

pytorch_lightning/callbacks/progress.py
@@ -97,8 +97,7 @@ class ProgressBarBase(Callback):
             total_val_batches = len(trainer.val_dataloaders)
         elif not self.trainer.disable_validation:
             is_val_epoch = trainer.current_epoch % trainer.check_val_every_n_epoch == 0
-            total_val_batches = trainer.num_val_batches if is_val_epoch else 0
-            total_val_batches = sum(total_val_batches)
+            total_val_batches = sum(trainer.num_val_batches) if is_val_epoch else 0
         return total_val_batches
 
     @property