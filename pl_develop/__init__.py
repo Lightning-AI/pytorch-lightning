d9faea42e749d320531195f4416e6ac08a450d73
Jirka <jirka@pytorchlightning.ai>
2020-06-23 09:45:01 +0200

    flake8

--- pl_develop/__init__.py
+++ pl_develop/__init__.py
@@ -1,5 +1,20 @@
-"""Models for testing."""
+"""
+Models and datasets for PL testing
+==================================
+
+This is a supplementary package serving for setting common test cases and simplified versions of standard datasets.
+
+"""
 
 from pl_develop.model_template import EvalModelTemplate
 from pl_develop.datasets import TrialMNIST, TrialCIFAR10
 from pl_develop.models import EvalModelGAN, ParityModuleMNIST, ParityModuleRNN
+
+__all__ = [
+    'EvalModelTemplate',
+    'TrialMNIST',
+    'TrialCIFAR10',
+    'EvalModelGAN',
+    'ParityModuleMNIST',
+    'ParityModuleRNN',
+]
