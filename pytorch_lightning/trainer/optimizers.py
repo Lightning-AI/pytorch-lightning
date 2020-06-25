4748b0c969db0301b988efd16484ea32664a3bba
William Falcon <waf2107@columbia.edu>
2020-06-24 22:41:25 -0400

    swaps lr sched order

--- pytorch_lightning/trainer/optimizers.py
+++ pytorch_lightning/trainer/optimizers.py
@@ -111,14 +111,19 @@ class TrainerOptimizersMixin(ABC):
     def reinit_scheduler_properties(self, optimizers: list, schedulers: list):
         # Reinitialize optimizer.step properties added by schedulers
         for scheduler in schedulers:
+            scheduler = scheduler['scheduler']
+
             for optimizer in optimizers:
-                scheduler = scheduler['scheduler']
                 # check that we dont mix users optimizers and schedulers
                 if scheduler.optimizer == optimizer:
+
                     # Find the mro belonging to the base lr scheduler class
                     for i, mro in enumerate(scheduler.__class__.__mro__):
                         if mro == optim.lr_scheduler._LRScheduler:
                             idx = i
+                        elif mro == optim.lr_scheduler.ReduceLROnPlateau:
+                            idx = i
+
                     scheduler.__class__.__mro__[idx].__init__(scheduler, optimizer)
 
 
