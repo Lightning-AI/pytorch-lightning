d64b19a44ae18b16da9d627701f6f7ed90aff8c5
Jirka <jirka@pytorchlightning.ai>
2020-06-26 12:36:18 +0200

    spawn

--- tests/base/develop_utils.py
+++ tests/base/develop_utils.py
@@ -14,7 +14,7 @@ from tests.base.model_template import EvalModelTemplate
 def default_trainer_options():
     opt = {}
     if platform.system() == "Windows":
-        opt.update(distributed_backend='ddp_spawn')
+        opt.update(distributed_backend='ddp_cpu')
     return opt
 
 
