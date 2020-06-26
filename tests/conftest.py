431f7d4e3f090c3d508e00262a6427b8ee031363
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-06-27 00:03:56 +0200

    fix httpserver address

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,7 +48,7 @@ def tmpdir_server(tmpdir):
         class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
             daemon_threads = True
 
-    with ThreadingHTTPServer(('', 0), Handler) as server:
+    with ThreadingHTTPServer(('localhost', 0), Handler) as server:
         server_thread = threading.Thread(target=server.serve_forever)
         # Exit the server thread when the main thread terminates
         server_thread.daemon = True
