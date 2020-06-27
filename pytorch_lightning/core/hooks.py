a2df408f57a93671fe8e293e9cc3d9b049e3fda8
Mateusz Pieniak <mateusz.pieniak@gmail.com>
2020-06-27 13:18:06 +0200

    Correct the docs

--- pytorch_lightning/core/hooks.py
+++ pytorch_lightning/core/hooks.py
@@ -208,7 +208,7 @@ class ModelHooks(Module):
         - :class:`list`
         - :class:`dict`
         - :class:`tuple`
-        - :class:`torchtext.data.Batch`
+        - :class:`torchtext.data.batch.Batch`
 
         For anything else, you need to define how the data is moved to the target device (CPU, GPU, TPU, ...).
 
