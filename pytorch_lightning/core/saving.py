c37dba1c46bf3c6f1465f3f38210caa28dfc7d1e
Jirka <jirka@pytorchlightning.ai>
2020-06-28 21:41:46 +0200

    extra case

--- pytorch_lightning/core/saving.py
+++ pytorch_lightning/core/saving.py
@@ -172,6 +172,7 @@ class ModelIO(object):
     @classmethod
     def _load_model_state(cls, checkpoint: Dict[str, Any], *cls_args, **cls_kwargs):
         cls_spec = inspect.getfullargspec(cls.__init__)
+        cls_init_args_name = inspect.signature(cls).parameters.keys()
         # pass in the values we saved automatically
         if cls.CHECKPOINT_HYPER_PARAMS_KEY in checkpoint:
             model_args = {}
@@ -184,19 +185,19 @@ class ModelIO(object):
             model_args = _convert_loaded_hparams(model_args, checkpoint.get(cls.CHECKPOINT_HYPER_PARAMS_TYPE))
 
             args_name = checkpoint.get(cls.CHECKPOINT_HYPER_PARAMS_NAME)
-            kwargs_identifier = cls_spec.varkw
-            cls_init_args_name = inspect.signature(cls).parameters.keys()
 
             if args_name == 'kwargs':
                 # in case the class cannot take any extra argument filter only the possible
-                if not kwargs_identifier:
-                    model_args = {k: v for k, v in model_args.items() if k in cls_init_args_name}
                 cls_kwargs.update(**model_args)
             elif args_name:
                 if args_name in cls_init_args_name:
                     cls_kwargs.update({args_name: model_args})
             else:
                 cls_args = (model_args,) + cls_args
+
+        if not cls_spec.varkw:
+            # filter kwargs according to class init unless it allows any argument via kwargs
+            cls_kwargs = {k: v for k, v in cls_kwargs.items() if k in cls_init_args_name}
 
         # prevent passing positional arguments if class does not accept any
         if len(cls_spec.args) <= 1 and not cls_spec.kwonlyargs:
