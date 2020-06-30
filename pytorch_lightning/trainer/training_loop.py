1c442563832806d7d448941d5880e02f684bcc36
William Falcon <waf2107@columbia.edu>
2020-06-30 14:02:11 -0400

    fix outputs

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -802,9 +802,9 @@ class TrainerTrainLoopMixin(ABC):
             if self.precision == 16 and not self.on_tpu:
                 closure_loss = model_ref.amp_scale_loss(closure_loss, optimizer, opt_idx)
 
-            # apply amp context
-            if self.precision == 16 and not NATIVE_AMP_AVALAIBLE:
-                closure_loss = closure_loss.exit()
+                # apply amp context
+                if not NATIVE_AMP_AVALAIBLE:
+                    closure_loss = closure_loss.exit()
 
             # do backward pass
             model_ref.backward(self, closure_loss, optimizer, opt_idx)
