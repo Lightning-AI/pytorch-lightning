a4e44cf33f33242b482c0f9b1d682c61cdf5433a
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-07-01 07:34:10 +0200

    try version parse

--- tests/trainer/test_dataloaders.py
+++ tests/trainer/test_dataloaders.py
@@ -2,6 +2,7 @@ import platform
 
 import pytest
 import torch
+from packaging.version import parse
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Subset, IterableDataset
 
@@ -488,6 +489,11 @@ def test_warning_with_few_workers(tmpdir, ckpt_path):
         trainer.test(**test_options)
 
 
+@pytest.mark.xfail(
+    parse(torch.__version__) < parse("1.4.0"),
+    reason="IterableDataset with __len__ before 1.4 raises",
+    raises=TypeError
+)
 def test_warning_with_iterable_dataset_and_len(tmpdir):
     """ Tests that a warning messages is shown when an IterableDataset defines `__len__`. """
     model = EvalModelTemplate()
