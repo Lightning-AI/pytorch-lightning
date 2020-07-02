cca0481b385cf3dd89cb60d335b9772967f8b96f
Anthony Bisulco <bisulco.a@husky.neu.edu>
2020-07-01 21:50:53 -0400

    wandb logging fix

--- pytorch_lightning/loggers/wandb.py
+++ pytorch_lightning/loggers/wandb.py
@@ -125,6 +125,7 @@ class WandbLogger(LightningLoggerBase):
     @rank_zero_only
     def log_hyperparams(self, params: Union[Dict[str, Any], Namespace]) -> None:
         params = self._convert_params(params)
+        params = self._flatten_dict(params)
         self.experiment.config.update(params, allow_val_change=True)
 
     @rank_zero_only
