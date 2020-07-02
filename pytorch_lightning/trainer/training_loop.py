57e54acfca90939a8f804fb0110ac4bc75989b1f
atee <atee@evidscience.com>
2020-07-02 16:25:45 -0700

    Debugging misstep

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -532,7 +532,6 @@ class TrainerTrainLoopMixin(ABC):
     def run_training_epoch_end(self, epoch_output):
         model = self.get_model()
         if self.is_overridden('training_epoch_end', model=model):
-            self.global_step += 1
             epoch_output = model.training_epoch_end(epoch_output)
             _processed_outputs = self.process_output(epoch_output)
             log_epoch_metrics = _processed_outputs[2]
