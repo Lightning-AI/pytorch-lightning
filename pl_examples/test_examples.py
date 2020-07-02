e3ba4e15a1a9d5a9bc56ff18cab101ed448fad5a
Jirka <jirka@pytorchlightning.ai>
2020-07-02 18:13:46 +0200

    gpu

--- pl_examples/test_examples.py
+++ pl_examples/test_examples.py
@@ -28,7 +28,7 @@ def test_gpu_template(cli_args):
         run_cli()
 
 
-@pytest.mark.parametrize('cli_args', ['--epochs 1'])
+@pytest.mark.parametrize('cli_args', ['--epochs 1 --gpus 1'])
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
 def test_imagenet(tmpdir, cli_args):
     """Test running CLI for an example with default params."""
