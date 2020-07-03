43fa46307fd8e8d447561d5a64511c1f3d69ee26
William Falcon <waf2107@columbia.edu>
2020-07-03 00:15:14 -0400

    added early stop tpu test

--- pytorch_lightning/callbacks/early_stopping.py
+++ pytorch_lightning/callbacks/early_stopping.py
@@ -172,12 +172,12 @@ class EarlyStopping(Callback):
             dist.barrier()
             trainer.should_stop = stop == trainer.world_size
 
-        if trainer.use_tpu:
-            stop = torch.tensor(int(trainer.should_stop), device=pl_module.device)
-            xm.all_reduce('sum', [stop])
-            print(type(stop))
-            torch_xla.core.xla_model.rendezvous("pl.EarlyStoppingCallback.stop_distributed_training_check")
-            trainer.should_stop = stop.item() == trainer.world_size
+        # if trainer.use_tpu:
+        #     stop = torch.tensor(int(trainer.should_stop), device=pl_module.device)
+        #     xm.all_reduce('sum', [stop])
+        #     print(type(stop))
+        #     torch_xla.core.xla_model.rendezvous("pl.EarlyStoppingCallback.stop_distributed_training_check")
+        #     trainer.should_stop = stop.item() == trainer.world_size
 
     def on_train_end(self, trainer, pl_module):
         if self.stopped_epoch > 0 and self.verbose > 0:
