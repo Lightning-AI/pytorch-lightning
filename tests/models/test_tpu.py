d13f680b3e01711d739b370bb916fedf0b0e3757
William Falcon <waf2107@columbia.edu>
2020-07-05 07:44:46 -0400

    added base tests for tpu

--- tests/models/test_tpu.py
+++ tests/models/test_tpu.py
@@ -38,6 +38,7 @@ def test_base_tpu_model(tmpdir, tpu_cores, expected_device):
     )
 
     model = EvalModelTemplate()
+    assert False == True, 'debug'
 
     tpipes.run_model_test(trainer_options, model, on_gpu=False, with_hpc=False)
 
