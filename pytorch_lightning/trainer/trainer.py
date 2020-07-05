d11bb1a6f4e90fb4b7bad362aa0ea0c1db99bd56
William Falcon <waf2107@columbia.edu>
2020-07-05 13:02:16 -0400

    added base tests for tpu

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -960,7 +960,6 @@ class Trainer(
                 self.set_random_port()
                 model.share_memory()
 
-                import pdb; pdb.set_trace()
                 # spin up peers
                 mp.spawn(self.ddp_train, nprocs=self.num_processes, args=(model, ))
 
