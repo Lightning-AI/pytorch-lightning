625fc11f88ec4a41c3d685faee8f04d42770ed71
William Falcon <waf2107@columbia.edu>
2020-07-05 10:25:52 -0400

    added base tests for tpu

--- tests/models/test_gpu.py
+++ tests/models/test_gpu.py
@@ -46,8 +46,8 @@ def test_multi_gpu_model(tmpdir, backend):
             memory.get_memory_profile('min_max')
             queue.put(1)
         except Exception as e:
-            if backend == 'ddp':
-                dist.destroy_process_group()
+            # if backend == 'ddp':
+            #     dist.destroy_process_group()
 
             queue.put(-1)
             queue.put(e)
