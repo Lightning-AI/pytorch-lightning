fa3f67282fe39f7f315cf2dbfa11489e97c33a48
William Falcon <waf2107@columbia.edu>
2020-07-04 21:20:52 -0400

    fix tpu hang

--- tests/trainer/test_trainer_steps.py
+++ tests/trainer/test_trainer_steps.py
@@ -13,6 +13,7 @@ def test_training_step_dict(tmpdir):
     trainer = Trainer(
         default_root_dir=tmpdir,
         fast_dev_run=True,
+        precision=16,
         weights_summary=None,
     )
     trainer.fit(model)
@@ -33,7 +34,7 @@ def test_training_step_dict(tmpdir):
 
     train_step_out = out.training_step_output_for_epoch_end
     pbar_metrics = train_step_out['progress_bar']
-    assert 'loss' in train_step_out
+    assert train_step_out['loss'] == (42.0 * 3) + (15.0 * 3)
     assert 'log' in train_step_out
     assert 'progress_bar' in train_step_out
     assert train_step_out['train_step_test'] == 549
