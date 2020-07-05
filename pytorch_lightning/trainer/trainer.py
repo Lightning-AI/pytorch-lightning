69ac053826cb64211f91342b3328cdf9e8af6ca7
William Falcon <waf2107@columbia.edu>
2020-07-05 18:17:49 -0400

    added base tests for tpu

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -961,7 +961,7 @@ class Trainer(
                 model.share_memory()
 
                 # spin up peers
-                mp.spawn(self.ddp_train, nprocs=self.num_processes - 1, args=(model, False, 1), join=False)
+                mp.spawn(self.ddp_train, nprocs=self.num_processes - 1, args=(model, False, 1), daemon=True)
                 print('in PROC 0')
                 self.ddp_train(0, model, is_master=True)
 
