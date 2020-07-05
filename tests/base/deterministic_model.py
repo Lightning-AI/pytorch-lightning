9bfff902d4c797bd5f8ddf720cf6fbaa5da94777
William Falcon <waf2107@columbia.edu>
2020-07-04 21:38:33 -0400

    fix tpu hang

--- tests/base/deterministic_model.py
+++ tests/base/deterministic_model.py
@@ -134,7 +134,8 @@ class DeterministicModel(LightningModule):
         return torch.optim.Adam(self.parameters(), lr=0)
 
     def backward(self, trainer, loss, optimizer, optimizer_idx):
-        if loss.dtype == torch.int16:
+        print(loss.dtype)
+        if self.trainer.precision == 16:
             assert loss == 11206656
         else:
             assert loss == 171.0
