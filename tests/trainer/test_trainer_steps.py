a8260db72d0636e39b034691ee153af5eef14e89
William Falcon <waf2107@columbia.edu>
2020-07-04 21:30:41 -0400

    fix tpu hang

--- tests/trainer/test_trainer_steps.py
+++ tests/trainer/test_trainer_steps.py
@@ -4,7 +4,6 @@ import pytest
 import torch
 
 
-@pytest.mark.spawn
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 def test_training_step_dict(tmpdir):
     """
