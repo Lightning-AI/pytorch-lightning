6cd9d9cef7ef49d6b5c47f1a44e9c4c84947c90e
William Falcon <waf2107@columbia.edu>
2020-07-06 08:52:40 -0400

    added base tests for tpu

---
+++ tests/models/test_test_loop.py
@@ -0,0 +1,21 @@
+import os
+from pytorch_lightning import Trainer
+import pytorch_lightning as pl
+from tests.base import EvalModelTemplate
+
+
+def test_ddp_test(tmpdir):
+
+    model = EvalModelTemplate()
+    trainer = pl.Trainer(
+        default_root_dir=os.getcwd(),
+        max_epochs=10,
+        limit_train_batches=10,
+        limit_val_batches=10,
+        gpus=[0, 1],
+        distributed_backend='ddp_spawn',
+    )
+    trainer.fit(model)
+    assert 'ckpt' in trainer.checkpoint_callback.best_model_path
+    results = trainer.test()
+    assert 'test_acc' in results
