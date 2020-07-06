e4a7de46d5d140395c5bd35b13ea73fefdf3efd8
William Falcon <waf2107@columbia.edu>
2020-07-06 09:47:57 -0400

    added base tests for tpu

--- tests/models/test_test_loop.py
+++ tests/models/test_test_loop.py
@@ -4,25 +4,6 @@ from tests.base import EvalModelTemplate
 import tests.base.develop_utils as tutils
 
 
-def test_ddp(tmpdir):
-    tutils.set_random_master_port()
-
-    model = EvalModelTemplate()
-    trainer = pl.Trainer(
-        default_root_dir=os.getcwd(),
-        max_epochs=2,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        gpus=[0, 1],
-        distributed_backend='ddp',
-    )
-    trainer.fit(model)
-    assert 'ckpt' in trainer.checkpoint_callback.best_model_path
-    results = trainer.test()
-    assert 'test_acc' in results
-    assert 1 == 2
-
-
 def test_ddp_spawn_test(tmpdir):
     tutils.set_random_master_port()
 
