8ff2dec4d285dde75b4e3552c18d18b277aa8823
William Falcon <waf2107@columbia.edu>
2020-07-07 08:29:58 -0400

    added base tests for tpu

--- pytorch_lightning/trainer/data_loading.py
+++ pytorch_lightning/trainer/data_loading.py
@@ -176,6 +176,7 @@ class TrainerDataLoadingMixin(ABC):
         }
 
         dl_args['sampler'] = sampler
+        import pdb; pdb.set_trace()
         dataloader = type(dataloader)(**dl_args)
         return dataloader
 
