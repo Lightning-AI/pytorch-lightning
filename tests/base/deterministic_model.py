13ba1a5ab68d3a4b0e1f9d72aa42a2ba8722dbee
William Falcon <waf2107@columbia.edu>
2020-07-07 09:52:30 -0400

    added base tests for tpu

--- tests/base/deterministic_model.py
+++ tests/base/deterministic_model.py
@@ -31,9 +31,8 @@ class DeterministicModel(LightningModule):
         x = batch
         y_hat = self(x)
 
-        print(y_hat)
         assert torch.all(y_hat[:, 0] == 15.0)
-        assert torch.all(y_hat[:, 0] == 42.0)
+        assert torch.all(y_hat[:, 1] == 42.0)
         out = y_hat.sum()
         assert out == (42.0 * 3) + (15.0 * 3)
 
