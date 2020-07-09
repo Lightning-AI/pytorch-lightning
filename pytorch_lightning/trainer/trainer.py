23cd95591ec9b81fe94e4b9feb13281f5fb6fa98
William Falcon <waf2107@columbia.edu>
2020-07-09 16:33:36 -0400

    enable none checkpoint

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -1019,7 +1019,7 @@ class Trainer(
             # if self.tpu_id is not None:
             #     results = self.tpu_train(self.tpu_id, q, model)
             # else:
-            xmp.spawn(self.tpu_train, args=(q, model), nprocs=self.tpu_cores, start_method=start_method)
+            xmp.spawn(self.tpu_train, args=(q, model), nprocs=self.tpu_cores, start_method='spawn')
 
             best_path = q.get()
             results = q.get()
