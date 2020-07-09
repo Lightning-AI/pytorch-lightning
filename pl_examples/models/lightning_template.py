4686c22a7c3e6523085d06c574b9d7b6588ac37e
William Falcon <waf2107@columbia.edu>
2020-07-09 14:26:08 -0400

    enable none checkpoint

--- pl_examples/models/lightning_template.py
+++ pl_examples/models/lightning_template.py
@@ -125,14 +125,14 @@ class LightningTemplateModel(LightningModule):
         """
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         val_acc = sum([x['n_correct_pred'] for x in outputs]) // sum(x['n_pred'] for x in outputs)
-        val_acc = torch.tensor(val_acc, device=self.device)
+        val_acc = torch.tensor(val_acc, device=self.device).mean()
         tensorboard_logs = {'val_loss': avg_loss, 'val_acc': val_acc}
         return {'val_loss': avg_loss, 'log': tensorboard_logs}
 
     def test_epoch_end(self, outputs):
         avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
         test_acc = sum([x['n_correct_pred'] for x in outputs]) // sum(x['n_pred'] for x in outputs)
-        test_acc = torch.tensor(test_acc, device=self.device)
+        test_acc = torch.tensor(test_acc, device=self.device).mean()
         tensorboard_logs = {'test_loss': avg_loss, 'test_acc': test_acc}
         return {'test_loss': avg_loss, 'log': tensorboard_logs}
 
