1f82457a412fb4a2b5b4c5464ec6c3a4f93e96e2
Jirka Borovec <Borda@users.noreply.github.com>
2020-07-09 09:50:18 +0200

    Update message

--- pytorch_lightning/trainer/ignored_warnings.py
+++ pytorch_lightning/trainer/ignored_warnings.py
@@ -3,12 +3,9 @@ import warnings
 
 def ignore_scalar_return_in_dp():
     # Users get confused by this warning so we silence it
-    m_1 = (
-        'Was asked to gather along dimension 0, but all '
-        'input tensors were scalars; will instead unsqueeze '
-        'and return a vector.'
-    )
-    warnings.filterwarnings('ignore', message=m_1)
+    warnings.filterwarnings('ignore', message='Was asked to gather along dimension 0, but all '
+                                              'input tensors were scalars; will instead unsqueeze '
+                                              'and return a vector.')
 
 
 ignore_scalar_return_in_dp()
