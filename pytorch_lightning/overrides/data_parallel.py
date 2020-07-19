e891e4bec28294ed7049c26a53993c66b610371f
William Falcon <waf2107@columbia.edu>
2020-07-19 10:16:38 -0400

    finished tests for structured results on train epoch

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -187,7 +187,7 @@ def parallel_apply(modules, inputs, kwargs_tup=None, devices=None):  # pragma: n
                 # this also avoids accidental slicing of `input` if it is a Tensor
                 if not isinstance(input, (list, tuple)):
                     input = (input,)
-
+                module._device = device
                 print(input, module.device, module.l1.weight.device)
 
                 # ---------------
