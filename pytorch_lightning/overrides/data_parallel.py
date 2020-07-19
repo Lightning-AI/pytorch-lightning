947dc70df0e82fea4138974a1dae73016a1c8ff2
William Falcon <waf2107@columbia.edu>
2020-07-19 10:13:11 -0400

    finished tests for structured results on train epoch

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -183,29 +183,28 @@ def parallel_apply(modules, inputs, kwargs_tup=None, devices=None):  # pragma: n
         if device is None:
             device = get_a_var(input).get_device()
         try:
-            print('old device', module.device)
-            module = module.to(device)
-            print('new device', module.device)
-            print(input)
+            with torch.cuda.device(device):
+                # this also avoids accidental slicing of `input` if it is a Tensor
+                if not isinstance(input, (list, tuple)):
+                    input = (input,)
 
-            # this also avoids accidental slicing of `input` if it is a Tensor
-            if not isinstance(input, (list, tuple)):
-                input = (input,)
+                print(input, module.device)
 
-            # ---------------
-            # CHANGE
-            if module.training:
-                output = module.training_step(*input, **kwargs)
+                # ---------------
+                # CHANGE
+                print(module.device)
+                if module.training:
+                    output = module.training_step(*input, **kwargs)
 
-            elif module.testing:
-                output = module.test_step(*input, **kwargs)
+                elif module.testing:
+                    output = module.test_step(*input, **kwargs)
 
-            else:
-                output = module.validation_step(*input, **kwargs)
+                else:
+                    output = module.validation_step(*input, **kwargs)
 
-            if module.use_dp or module.use_ddp2:
-                auto_squeeze_dim_zeros(output)
-            # ---------------
+                if module.use_dp or module.use_ddp2:
+                    auto_squeeze_dim_zeros(output)
+                # ---------------
 
             with lock:
                 results[i] = output
