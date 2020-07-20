704d2019375b42588f105eea8ea32948cfeddc37
William Falcon <waf2107@columbia.edu>
2020-07-20 18:42:36 -0400

    Update pytorch_lightning/overrides/data_parallel.py
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- pytorch_lightning/overrides/data_parallel.py
+++ pytorch_lightning/overrides/data_parallel.py
@@ -188,8 +188,6 @@ def parallel_apply(modules, inputs, kwargs_tup=None, devices=None):  # pragma: n
                 if not isinstance(input, (list, tuple)):
                     input = (input,)
 
-                if hasattr(module, '_device'):
-                    module._device = device
                 module = module.to(device)
 
                 # ---------------
