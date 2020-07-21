6450d7e16991ff3e6fee2bdefff84cc0fc807547
William Falcon <waf2107@columbia.edu>
2020-07-21 18:53:10 -0400

    logging metrics

--- tests/base/model_train_steps.py
+++ tests/base/model_train_steps.py
@@ -92,6 +92,7 @@ class TrainingStepVariations(ABC):
         """
         Full loop flow train step (result obj + dp)
         """
+        import pdb; pdb.set_trace()
         eval_name = 'validation' if not self.trainer.testing else 'test'
         reduced = getattr(result, f'{eval_name}_step_metric').mean()
         result.train_step_metric = reduced
@@ -105,6 +106,7 @@ class TrainingStepVariations(ABC):
         """
         Full loop flow train step (result obj + dp)
         """
+        import pdb; pdb.set_trace()
         eval_name = 'validation' if not self.trainer.testing else 'test'
         result.log(f'{eval_name}_epoch_end_metric', 1, on_epoch=True)
         result.checkpoint_on = result.checkpoint_on.mean()
