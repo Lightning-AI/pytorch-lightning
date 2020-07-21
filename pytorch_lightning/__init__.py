2d5988e763ed500787e58e205f859f2c029a2dac
nateraw <nxr9266@g.rit.edu>
2020-07-21 21:30:19 +0000

    :sparkles: add datamodule to necessary init files

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -50,7 +50,7 @@ if __LIGHTNING_SETUP__:
     sys.stdout.write(f'Partial import of `{__name__}` during the build process.\n')  # pragma: no-cover
     # We are not importing the rest of the lightning during the build process, as it may not be compiled yet
 else:
-    from pytorch_lightning.core import LightningModule, data_loader
+    from pytorch_lightning.core import LightningDataModule, LightningModule, data_loader
     from pytorch_lightning.callbacks import Callback
     from pytorch_lightning.trainer import Trainer
     from pytorch_lightning.utilities.seed import seed_everything
@@ -59,6 +59,7 @@ else:
 
     __all__ = [
         'Trainer',
+        'LightningDataModule',
         'LightningModule',
         'Callback',
         'data_loader',
