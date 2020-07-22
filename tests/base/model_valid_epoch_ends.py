e14f6d405e647c10965f10126b3100dbc3a72164
William Falcon <waf2107@columbia.edu>
2020-07-22 13:13:30 -0400

    logging metrics

--- tests/base/model_valid_epoch_ends.py
+++ tests/base/model_valid_epoch_ends.py
@@ -21,7 +21,7 @@ class ValidationEpochEndVariations(ABC):
             # recursive mean for multilevel dicts
             return torch.stack([x[key] if isinstance(x, dict) else _mean(x, key) for x in res]).mean()
 
-        import pdb; pdb.set_trace()
+        print('in validation epoch end')
         val_loss_mean = _mean(outputs, 'val_loss')
         val_acc_mean = _mean(outputs, 'val_acc')
 
