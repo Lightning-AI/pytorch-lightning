e72779475f34c81fb620dd2e28fa431bd8c12de8
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-07-24 01:22:11 +0200

    make backward compatible

--- pytorch_lightning/trainer/deprecated_api.py
+++ pytorch_lightning/trainer/deprecated_api.py
@@ -46,6 +46,7 @@ class TrainerDeprecatedAPITillVer0_10(ABC):
     limit_train_batches: Union[int, float]
     overfit_batches: Union[int, float]
     weights_save_path: str
+    is_global_zero: bool
 
     def __init__(self):
         super().__init__()  # mixin calls super too
@@ -125,7 +126,7 @@ class TrainerDeprecatedAPITillVer0_10(ABC):
         """Back compatibility, will be removed in v0.10.0"""
         rank_zero_warn("Attribute `ckpt_path` is now set by `weights_save_path` since v0.9.0"
                        " and this method will be removed in v0.10.0", DeprecationWarning)
-        return self.weights_save_path
+        return self.weights_save_path if self.is_global_zero else None
 
     @ckpt_path.setter
     def ckpt_path(self, path: str):
