a6b96b07ca0f9d98309d9d6e2ea00b467b8bfb79
Thomas Schaaf <tschaaf@mmm.com>
2020-07-27 11:05:39 -0400

    removed tests using RNN models to be moved into a separate PR

--- tests/utilities/test_apply_func_torchtext.py
+++ tests/utilities/test_apply_func_torchtext.py
@@ -2,7 +2,6 @@ import torch
 import torchtext
 from torchtext.data.example import Example
 
-import pytorch_lightning as pl
 from pytorch_lightning.utilities.apply_func import move_data_to_device
 
 
@@ -28,157 +27,6 @@ def _get_torchtext_data_iterator(include_lengths=False):
     return iterator, text_field
 
 
-def test_torchtext_field_include_length_true():
-    """Test if batches created by torchtext with include_lengths=True raise an exception."""
-
-    class DebugModel(pl.LightningModule):
-
-        def __init__(self):
-            super(DebugModel, self).__init__()
-
-            # setup data loader generating batches with fields consisting of tuples of tensors
-            self.debug_data_loader, self.text_field = _get_torchtext_data_iterator(include_lengths=True)
-
-            self.learning_rate = 0.001
-
-            pad_idx = self.text_field.vocab.stoi['<pad>']
-            self.criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
-
-            self.INPUT_DIM = len(self.text_field.vocab)
-            self.ENC_EMB_DIM = 4  # keep it small for debugging
-            self.embedding = torch.nn.Embedding(self.INPUT_DIM, self.ENC_EMB_DIM)
-
-            self.hid_dim = 4
-            self.rnn = torch.nn.GRU(self.ENC_EMB_DIM, self.hid_dim, 1, bidirectional=False)
-            self.out = torch.nn.Linear(self.hid_dim, self.embedding.num_embeddings)
-
-        def configure_optimizers(self):
-            return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-
-        def forward(self, input_seq, length):
-            embedded = self.embedding(input_seq)
-            packed_embedded = torch.nn.utils.rnn.pack_padded_sequence(embedded,
-                                                                      length,
-                                                                      batch_first=False,
-                                                                      enforce_sorted=False)
-            packed_outputs, hidden = self.rnn(packed_embedded)
-            outputs, length = torch.nn.utils.rnn.pad_packed_sequence(packed_outputs)
-
-            output = outputs.squeeze(0)
-            prediction = self.out(output)
-
-            return prediction
-
-        @staticmethod
-        def _parse_batch(batch):
-            source = batch.text[0]
-            source_length = batch.text[1]
-
-            return source, source_length
-
-        def training_step(self, batch, batch_nb):
-            """ Needed for testing data transfer. """
-            x = self._parse_batch(batch)
-            target, target_length = x
-
-            output = self.forward(target, target_length)
-            loss = self.criterion(output[:-1].view(-1, output.shape[2]), target[1:].view(-1))
-
-            prefix = 'train'
-            tensorboard_logs = {f'{prefix}_loss': loss.item()}
-
-            result = {'loss': loss, 'log': tensorboard_logs}
-            return result
-
-        def train_dataloader(self):
-            return self.debug_data_loader
-
-    model = DebugModel()
-
-    cuda_device_cnt = torch.cuda.device_count()
-    if cuda_device_cnt > 0:
-        use_num_cuda_devices = 1
-    else:
-        use_num_cuda_devices = None
-
-    trainer = pl.Trainer(fast_dev_run=True, max_steps=None,
-                         gradient_clip_val=10,
-                         weights_summary=None, gpus=use_num_cuda_devices,
-                         show_progress_bar=True)
-
-    result = trainer.fit(model)
-    # verify training completed
-    assert result == 1
-
-
-def test_move_data_to_device_torchtext_include_length_false():
-    """Test if batches created by torchtext with include_lengths=False raise an exception."""
-
-    class DebugModel(pl.LightningModule):
-
-        def __init__(self):
-            super(DebugModel, self).__init__()
-
-            # setup data loader generating batches with fields consisting of tensors
-            self.debug_data_loader, self.text_field = _get_torchtext_data_iterator(include_lengths=False)
-
-            self.learning_rate = 0.001
-
-            pad_idx = self.text_field.vocab.stoi['<pad>']
-            self.criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
-
-            self.INPUT_DIM = len(self.text_field.vocab)
-            self.ENC_EMB_DIM = 4  # keep it small for debugging
-            self.embedding = torch.nn.Embedding(self.INPUT_DIM, self.ENC_EMB_DIM)
-
-            self.hid_dim = 4
-            self.rnn = torch.nn.GRU(self.ENC_EMB_DIM, self.hid_dim, 1, bidirectional=False)
-            self.out = torch.nn.Linear(self.hid_dim, self.embedding.num_embeddings)
-
-        def configure_optimizers(self):
-            return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-
-        def forward(self, input_seq):
-            embedded = self.embedding(input_seq)
-            outputs, hidden = self.rnn(embedded)
-            output = outputs.squeeze(0)
-            prediction = self.out(output)
-            return prediction
-
-        def training_step(self, batch, batch_nb):
-            """ Needed for testing data transfer. """
-
-            target = batch.text
-            output = self.forward(target)
-            loss = self.criterion(output[:-1].view(-1, output.shape[2]), target[1:].view(-1))
-
-            prefix = 'train'
-            tensorboard_logs = {f'{prefix}_loss': loss.item()}
-
-            result = {'loss': loss, 'log': tensorboard_logs}
-            return result
-
-        def train_dataloader(self):
-            return self.debug_data_loader
-
-    model = DebugModel()
-
-    cuda_device_cnt = torch.cuda.device_count()
-    if cuda_device_cnt > 0:
-        use_num_cuda_devices = 1
-    else:
-        use_num_cuda_devices = None
-
-    trainer = pl.Trainer(fast_dev_run=True, max_steps=None,
-                         gradient_clip_val=10,
-                         weights_summary=None, gpus=use_num_cuda_devices,
-                         show_progress_bar=True)
-
-    result = trainer.fit(model)
-    # verify training completed
-    assert result == 1
-
-
 def test_batch_move_data_to_device_torchtext_include_lengths_false():
     cuda_device_cnt = torch.cuda.device_count()
     if cuda_device_cnt > 0:
@@ -193,9 +41,9 @@ def test_batch_move_data_to_device_torchtext_include_lengths_false():
     # this call should not throw an error
     batch_on_device = move_data_to_device(batch, device)
     # tensor with data
-    assert(batch_on_device.text[0].device == device)
+    assert (batch_on_device.text[0].device == device)
     # tensor with length of data
-    assert(batch_on_device.text[1].device == device)
+    assert (batch_on_device.text[1].device == device)
 
 
 def test_batch_move_data_to_device_torchtext_include_lengths_true():
@@ -212,4 +60,4 @@ def test_batch_move_data_to_device_torchtext_include_lengths_true():
     # this call should not throw an error
     batch_on_device = move_data_to_device(batch, device)
     # tensor with data
-    assert(batch_on_device.text[0].device == device)
+    assert (batch_on_device.text[0].device == device)
