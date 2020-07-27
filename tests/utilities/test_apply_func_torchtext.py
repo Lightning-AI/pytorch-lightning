c9fdf50a7c468c69522c1627a81b9472e679208d
Thomas Schaaf <tschaaf@cs.cmu.edu>
2020-07-26 20:22:01 -0400

    adding tests more specific to batch_move_data_to_device with tochtext Batch

--- tests/utilities/test_apply_func_torchtext.py
+++ tests/utilities/test_apply_func_torchtext.py
@@ -1,23 +1,30 @@
 import torch
 import torchtext
+from torchtext.data.example import Example
 
 import pytorch_lightning as pl
+from pytorch_lightning.utilities.apply_func import move_data_to_device
 
 
 def _get_torchtext_data_iterator(include_lengths=False):
     text_field = torchtext.data.Field(sequential=True, pad_first=False,  # nosec
-                                      init_token="<s>", eos_token="</s>", include_lengths=include_lengths)  # nosec
+                                      init_token="<s>", eos_token="</s>",  # nosec
+                                      include_lengths=include_lengths)  # nosec
 
-    example1 = torchtext.data.example.Example.fromdict({"text": "a b c a c"}, {"text": ("text", text_field)})
-    example2 = torchtext.data.example.Example.fromdict({"text": "b c a a"}, {"text": ("text", text_field)})
-    example3 = torchtext.data.example.Example.fromdict({"text": "c b a"}, {"text": ("text", text_field)})
+    example1 = Example.fromdict({"text": "a b c a c"}, {"text": ("text", text_field)})
+    example2 = Example.fromdict({"text": "b c a a"}, {"text": ("text", text_field)})
+    example3 = Example.fromdict({"text": "c b a"}, {"text": ("text", text_field)})
 
-    dataset = torchtext.data.Dataset([example1, example2, example3], {"text": text_field})
+    dataset = torchtext.data.Dataset([example1, example2, example3],
+                                     {"text": text_field}
+                                     )
     text_field.build_vocab(dataset)
 
     iterator = torchtext.data.Iterator(dataset, batch_size=3,
-                                       sort_key=None, device=None, batch_size_fn=None,
-                                       train=True, repeat=False, shuffle=None, sort=None, sort_within_batch=None)
+                                       sort_key=None, device=None,
+                                       batch_size_fn=None,
+                                       train=True, repeat=False, shuffle=None,
+                                       sort=None, sort_within_batch=None)
     return iterator, text_field
 
 
@@ -50,7 +57,9 @@ def test_move_data_to_device_torchtext_include_length_true():
 
         def forward(self, input_seq, length):
             embedded = self.embedding(input_seq)
-            packed_embedded = torch.nn.utils.rnn.pack_padded_sequence(embedded, length, batch_first=False,
+            packed_embedded = torch.nn.utils.rnn.pack_padded_sequence(embedded,
+                                                                      length,
+                                                                      batch_first=False,
                                                                       enforce_sorted=False)
             packed_outputs, hidden = self.rnn(packed_embedded)
             outputs, length = torch.nn.utils.rnn.pad_packed_sequence(packed_outputs)
@@ -168,3 +177,33 @@ def test_move_data_to_device_torchtext_include_length_false():
     result = trainer.fit(model)
     # verify training completed
     assert result == 1
+
+
+def test_torchtext_include_lengths_false_batch_move_data_to_device():
+    cuda_device_cnt = torch.cuda.device_count()
+    if cuda_device_cnt > 0:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    data_iterator, _ = _get_torchtext_data_iterator(include_lengths=True)
+    data_iter = iter(data_iterator)
+    batch = next(data_iter)
+
+    # this call should not throw an error
+    batch_on_device = move_data_to_device(batch, device)
+
+
+def test_torchtext_include_lengths_true_batch_move_data_to_device():
+    cuda_device_cnt = torch.cuda.device_count()
+    if cuda_device_cnt > 0:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    data_iterator, _ = _get_torchtext_data_iterator(include_lengths=False)
+    data_iter = iter(data_iterator)
+    batch = next(data_iter)
+
+    # this call should not throw an error
+    batch_on_device = move_data_to_device(batch, device)
