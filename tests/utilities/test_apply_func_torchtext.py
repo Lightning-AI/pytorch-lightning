61e692f08b360e9066899becbf8bca47f3ab67b1
Thomas Schaaf <tschaaf@mmm.com>
2020-07-27 20:50:32 -0400

    Added check only if length tensor exist. Removed left over comments.

--- tests/utilities/test_apply_func_torchtext.py
+++ tests/utilities/test_apply_func_torchtext.py
@@ -28,23 +28,19 @@ def _get_torchtext_data_iterator(include_lengths=False):
     return iterator, text_field
 
 
-# @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
-# @pytest.mark.parametrize('device', [torch.device('cpu'), torch.device('cuda', 0)])
-@pytest.mark.parametrize('device', [torch.device('cpu')] if not torch.cuda.is_available() else [torch.device('cpu'), torch.device('cuda', 0)])
+@pytest.mark.parametrize('device', [torch.device('cpu')] if not torch.cuda.is_available() else [torch.device('cpu'),
+                                                                                                torch.device('cuda',
+                                                                                                             0)])
 @pytest.mark.parametrize('include_lengths', [False, True])
 def test_batch_move_data_to_device_torchtext_include_lengths(include_lengths, device):
-    # cuda_device_cnt = torch.cuda.device_count()
-    # if cuda_device_cnt > 0:
-    #     device = torch.device('cuda')
-    # else:
-    #     device = torch.device('cpu')
-
     data_iterator, _ = _get_torchtext_data_iterator(include_lengths=include_lengths)
     data_iter = iter(data_iterator)
     batch = next(data_iter)
-
     batch_on_device = move_data_to_device(batch, device)
+
     # tensor with data
     assert (batch_on_device.text[0].device == device)
-    # tensor with length of data
-    assert (batch_on_device.text[1].device == device)
+
+    if include_lengths:
+        # tensor with length of data
+        assert (batch_on_device.text[1].device == device)
