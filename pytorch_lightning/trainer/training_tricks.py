4f6b9d02f968e0a009ddf1e5d7b741081c4c0791
Philipp Jund <ijund.phil@gmail.com>
2020-07-30 15:56:23 +0200

    Speed up gradient clipping and allow parameters on multiple devices.
    The speed up is achieved by:
    - Moving the "where" out of the loop (and replacing with min for simplicity).
    - Replacing manual sum and pow with torch.norm. Even though this results
      in unnessecary computation (computing pow(root)) this is still a lot
      faster.
    - Preallocating the output gives a slight speed up.
    
    Note that calling .to for all parameters results in a small speed
    penalty (~4 ms in my case) but allows parameters on different devices.
    
    Overall this reduces the time used for gradient clipping from 206ms to
    74 ms for my model (Resnet50 + few additional vars, all vars on GPU).

--- pytorch_lightning/trainer/training_tricks.py
+++ pytorch_lightning/trainer/training_tricks.py
@@ -64,27 +64,29 @@ class TrainerTrainingTricksMixin(ABC):
 
         # this code is a modification of torch.nn.utils.clip_grad_norm_
         # with TPU support based on https://github.com/pytorch/xla/blob/master/TROUBLESHOOTING.md
-        if self.gradient_clip_val > 0:
-            model = self.get_model()
-            parameters = model.parameters()
-            max_norm = float(self.gradient_clip_val)
-            norm_type = float(2.0)
-            if isinstance(parameters, torch.Tensor):
-                parameters = [parameters]
-            parameters = list(filter(lambda p: p.grad is not None, parameters))
-            if norm_type == math.inf:
-                total_norm = max(p.grad.data.abs().max() for p in parameters)
-            else:
-                device = parameters[0].device
-                total_norm = torch.zeros([], device=device if parameters else None)
-                for p in parameters:
-                    param_norm = p.grad.data.pow(norm_type).sum()
-                    total_norm.add_(param_norm)
-                total_norm = (total_norm ** (1. / norm_type))
-            eps = EPSILON_FP16 if self.precision == 16 else EPSILON
-            clip_coef = torch.tensor(max_norm, device=device) / (total_norm + eps)
-            for p in parameters:
-                p.grad.data.mul_(torch.where(clip_coef < 1, clip_coef, torch.tensor(1., device=device)))
+        if self.gradient_clip_val <= 0:
+            return
+        model = self.get_model()
+        parameters = model.parameters()
+        max_norm = float(self.gradient_clip_val)
+        norm_type = float(2.0)
+        if isinstance(parameters, torch.Tensor):
+            parameters = [parameters]
+        parameters = list(filter(lambda p: p.grad is not None, parameters))
+        if norm_type == math.inf:
+            total_norm = max(p.grad.data.abs().max() for p in parameters)
+        else:
+            device = parameters[0].device
+            out = torch.empty(len(parameters), device=device)
+            for i, p in enumerate(parameters):
+                torch.norm(p.grad.data.to(device), norm_type, out=out[i])
+            total_norm = torch.norm(out, norm_type)
+
+        eps = EPSILON_FP16 if self.precision == 16 else EPSILON
+        clip_coef = torch.tensor(max_norm, device=device) / (total_norm + eps)
+        clip_coef = torch.min(clip_coef, torch.ones_like(clip_coef))
+        for p in parameters:
+            p.grad.data.mul_(clip_coef.to(p.grad.data.device))
 
     def print_nan_gradients(self) -> None:
         model = self.get_model()
