5f9f81d9ec2fddc987e8fa507661917f0462462f
Jirka Borovec <jirka@pytorchlightning.ai>
2020-08-01 12:07:10 +0200

    fix docker

--- tests/install_AMP.sh
+++ tests/install_AMP.sh
@@ -4,6 +4,6 @@ ROOT=$PWD
 git clone https://github.com/NVIDIA/apex
 cd apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-pip install -v --no-cache-dir ./
+# pip install -v --no-cache-dir ./
 cd $ROOT
 rm -rf apex
