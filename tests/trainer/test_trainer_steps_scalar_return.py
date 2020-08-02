3b89b553030e3831eff67840562150c377700af1
Jirka Borovec <jirka@pytorchlightning.ai>
2020-08-03 00:47:05 +0200

    revert

--- tests/trainer/test_trainer_steps_scalar_return.py
+++ tests/trainer/test_trainer_steps_scalar_return.py
@@ -7,24 +7,6 @@ from pytorch_lightning import Trainer
 from tests.base.deterministic_model import DeterministicModel
 
 
-def _steps_assert(model, trainer):
-    # make sure training outputs what is expected
-    batch_idx, batch = 0, next(iter(model.train_dataloader()))
-
-    out = trainer.run_training_batch(batch, batch_idx)
-    assert out.signal == 0
-    assert len(out.batch_log_metrics) == 0 and isinstance(out.batch_log_metrics, dict)
-    assert len(out.grad_norm_dic) == 0 and isinstance(out.grad_norm_dic, dict)
-
-    train_step_out = out.training_step_output_for_epoch_end
-    assert isinstance(train_step_out, torch.Tensor)
-    assert train_step_out.item() == 171
-
-    # make sure the optimizer closure returns the correct things
-    opt_closure_result = trainer.optimizer_closure(batch, batch_idx, 0, trainer.optimizers[0], trainer.hiddens)
-    assert opt_closure_result['loss'].item() == 171
-
-
 def test_training_step_scalar(tmpdir):
     """
     Tests that only training_step that returns a single scalar can be used
@@ -45,7 +27,22 @@ def test_training_step_scalar(tmpdir):
     assert not model.training_step_end_called
     assert not model.training_epoch_end_called
 
-    _steps_assert(model, trainer)
+    # make sure training outputs what is expected
+    for batch_idx, batch in enumerate(model.train_dataloader()):
+        break
+
+    out = trainer.run_training_batch(batch, batch_idx)
+    assert out.signal == 0
+    assert len(out.batch_log_metrics) == 0 and isinstance(out.batch_log_metrics, dict)
+    assert len(out.grad_norm_dic) == 0 and isinstance(out.grad_norm_dic, dict)
+
+    train_step_out = out.training_step_output_for_epoch_end
+    assert isinstance(train_step_out, torch.Tensor)
+    assert train_step_out.item() == 171
+
+    # make sure the optimizer closure returns the correct things
+    opt_closure_result = trainer.optimizer_closure(batch, batch_idx, 0, trainer.optimizers[0], trainer.hiddens)
+    assert opt_closure_result['loss'].item() == 171
 
 
 def training_step_scalar_with_step_end(tmpdir):
@@ -57,11 +54,7 @@ def training_step_scalar_with_step_end(tmpdir):
     model.training_step_end = model.training_step_end_scalar
     model.val_dataloader = None
 
-    trainer = Trainer(
-        default_root_dir=tmpdir,
-        fast_dev_run=True,
-        weights_summary=None,
-    )
+    trainer = Trainer(fast_dev_run=True, weights_summary=None)
     trainer.fit(model)
 
     # make sure correct steps were called
@@ -69,7 +62,22 @@ def training_step_scalar_with_step_end(tmpdir):
     assert model.training_step_end_called
     assert not model.training_epoch_end_called
 
-    _steps_assert(model, trainer)
+    # make sure training outputs what is expected
+    for batch_idx, batch in enumerate(model.train_dataloader()):
+        break
+
+    out = trainer.run_training_batch(batch, batch_idx)
+    assert out.signal == 0
+    assert len(out.batch_log_metrics) == 0 and isinstance(out.batch_log_metrics, dict)
+    assert len(out.grad_norm_dic) == 0 and isinstance(out.grad_norm_dic, dict)
+
+    train_step_out = out.training_step_output_for_epoch_end
+    assert isinstance(train_step_out, torch.Tensor)
+    assert train_step_out.item() == 171
+
+    # make sure the optimizer closure returns the correct things
+    opt_closure_result = trainer.optimizer_closure(batch, batch_idx, 0, trainer.optimizers[0], trainer.hiddens)
+    assert opt_closure_result['loss'].item() == 171
 
 
 def test_full_training_loop_scalar(tmpdir):
@@ -99,7 +107,22 @@ def test_full_training_loop_scalar(tmpdir):
     assert 'epoch' in trainer.callback_metrics and len(trainer.callback_metrics) == 1
     assert len(trainer.progress_bar_metrics) == 0
 
-    _steps_assert(model, trainer)
+    # make sure training outputs what is expected
+    for batch_idx, batch in enumerate(model.train_dataloader()):
+        break
+
+    out = trainer.run_training_batch(batch, batch_idx)
+    assert out.signal == 0
+    assert len(out.batch_log_metrics) == 0 and isinstance(out.batch_log_metrics, dict)
+    assert len(out.grad_norm_dic) == 0 and isinstance(out.grad_norm_dic, dict)
+
+    train_step_out = out.training_step_output_for_epoch_end
+    assert isinstance(train_step_out, torch.Tensor)
+    assert train_step_out.item() == 171
+
+    # make sure the optimizer closure returns the correct things
+    opt_closure_result = trainer.optimizer_closure(batch, batch_idx, 0, trainer.optimizers[0], trainer.hiddens)
+    assert opt_closure_result['loss'].item() == 171
 
 
 def test_train_step_epoch_end_scalar(tmpdir):
@@ -113,11 +136,7 @@ def test_train_step_epoch_end_scalar(tmpdir):
     model.training_epoch_end = model.training_epoch_end_scalar
     model.val_dataloader = None
 
-    trainer = Trainer(
-        default_root_dir=tmpdir,
-        max_epochs=1,
-        weights_summary=None,
-    )
+    trainer = Trainer(max_epochs=1, weights_summary=None)
     trainer.fit(model)
 
     # make sure correct steps were called
@@ -129,4 +148,19 @@ def test_train_step_epoch_end_scalar(tmpdir):
     assert 'epoch' in trainer.callback_metrics and len(trainer.callback_metrics) == 1
     assert len(trainer.progress_bar_metrics) == 0
 
-    _steps_assert(model, trainer)
+    # make sure training outputs what is expected
+    for batch_idx, batch in enumerate(model.train_dataloader()):
+        break
+
+    out = trainer.run_training_batch(batch, batch_idx)
+    assert out.signal == 0
+    assert len(out.batch_log_metrics) == 0 and isinstance(out.batch_log_metrics, dict)
+    assert len(out.grad_norm_dic) == 0 and isinstance(out.grad_norm_dic, dict)
+
+    train_step_out = out.training_step_output_for_epoch_end
+    assert isinstance(train_step_out, torch.Tensor)
+    assert train_step_out.item() == 171
+
+    # make sure the optimizer closure returns the correct things
+    opt_closure_result = trainer.optimizer_closure(batch, batch_idx, 0, trainer.optimizers[0], trainer.hiddens)
+    assert opt_closure_result['loss'].item() == 171
