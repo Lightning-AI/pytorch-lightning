6b9c548bab4facfb946fbc1923e4f37e835b992e
Rohit Gupta <rohitgr1998@gmail.com>
2020-08-04 01:27:21 +0530

    docs update and follow up of #2789 (#2797)
    * docs update and follow up of #2789
    
    * pep8
    
    * Update trainer.py
    
    * Update trainer.py
    
    Co-authored-by: edenlightning <66261195+edenlightning@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pytorch_lightning/trainer/__init__.py
+++ pytorch_lightning/trainer/__init__.py
@@ -801,7 +801,9 @@ Set to True to reload dataloaders every epoch.
 
 replace_sampler_ddp
 ^^^^^^^^^^^^^^^^^^^
-Enables auto adding of distributed sampler.
+Enables auto adding of distributed sampler. By default it will add ``shuffle=True``
+for train sampler and ``shuffle=False`` for val/test sampler. If you want to customize
+it, you can set ``replace_ddp_sampler=False`` and add your own distributed sampler.
 
 .. testcode::
 
