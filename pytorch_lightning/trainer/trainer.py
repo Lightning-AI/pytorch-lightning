602809f87205852e97dd97bb291b8ed58f0653b9
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-03 19:31:19 +0200

    debug

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -1338,6 +1338,8 @@ class Trainer(
             model_ref = self.get_model()
             model_ref.teardown('test')
 
+        torch_distrib.destroy_process_group()
+        
         return results
 
     def __test_given_model(self, model, test_dataloaders):
@@ -1360,6 +1362,8 @@ class Trainer(
         # teardown
         if self.is_function_implemented('teardown'):
             model.teardown('test')
+
+        torch_distrib.destroy_process_group()
 
         return results
 
