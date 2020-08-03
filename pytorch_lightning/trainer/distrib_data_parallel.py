81c5255d6fd06ed13be35fd9dca42f12dd8c9124
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-03 19:51:03 +0200

    debug

--- pytorch_lightning/trainer/distrib_data_parallel.py
+++ pytorch_lightning/trainer/distrib_data_parallel.py
@@ -409,7 +409,7 @@ class TrainerDDPMixin(ABC):
         # don't make this debug... this is good UX
         rank_zero_info(f'CUDA_VISIBLE_DEVICES: [{os.environ["CUDA_VISIBLE_DEVICES"]}]')
 
-    @rank_zero_only
+    # @rank_zero_only
     def set_random_port(self, force=False):
         """
         When running DDP NOT managed by SLURM, the ports might collide
