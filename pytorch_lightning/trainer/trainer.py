85f8929ddbbb726529ca80a6d01493cf4bab76ea
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-04 22:05:18 +0200

    debug

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -1347,7 +1347,7 @@ class Trainer(
 
         # run tests
         self.tested_ckpt_path = ckpt_path
-        self.set_random_port(force=True)
+        self.set_random_port()
         self.testing = True
         os.environ['PL_TESTING_MODE'] = '1'
         self.model = model
@@ -1370,7 +1370,7 @@ class Trainer(
 
         # run test
         # sets up testing so we short circuit to eval
-        self.set_random_port(force=True)
+        self.set_random_port()
         self.testing = True
         self.model = model
         results = self.fit(model)
