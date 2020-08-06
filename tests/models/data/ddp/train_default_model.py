924b26a4e1bdf4ae2d5c2cc163cdcec02dcad36a
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-06 22:59:11 +0200

    ddptest

--- tests/models/data/ddp/train_default_model.py
+++ tests/models/data/ddp/train_default_model.py
@@ -37,7 +37,7 @@ def main():
     seed_everything(1234)
     parser = ArgumentParser(add_help=False)
     parser = Trainer.add_argparse_args(parser)
-    parser.add_argument('variation', default=variation_fit_test.__name__, required=True)
+    parser.add_argument('--variation', default=variation_fit_test.__name__)
     parser.set_defaults(gpus=2)
     parser.set_defaults(distributed_backend="ddp")
     args = parser.parse_args()
