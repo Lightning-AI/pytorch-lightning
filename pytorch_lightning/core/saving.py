444ee6023fb2e33fe4c8e2fc6316859f90071104
romesc <rosario@cs.washington.edu>
2020-08-07 01:50:45 +0200

    Container removed. Pass local tests.

--- pytorch_lightning/core/saving.py
+++ pytorch_lightning/core/saving.py
@@ -328,23 +328,26 @@ def save_hparams_to_yaml(config_yaml, hparams: Union[dict, Namespace]) -> None:
     if not gfile.isdir(os.path.dirname(config_yaml)):
         raise RuntimeError(f"Missing folder: {os.path.dirname(config_yaml)}.")
 
-    if OmegaConf is not None and not isinstance(hparams, Namespace):
-        if OmegaConf.is_config(hparams):
-            OmegaConf.save(hparams, config_yaml, resolve=True)
-        elif (OmegaConf.is_config(v) for v in hparams.values()):
-            OmegaConf.save(OmegaConf.create(hparams), config_yaml, resolve=True)
-        return
-
-    # saving the standard way
+    # convert Namespace or AD to dict
     if isinstance(hparams, Namespace):
         hparams = vars(hparams)
     elif isinstance(hparams, AttributeDict):
         hparams = dict(hparams)
+
+    # saving with OmegaConf objects
+    if OmegaConf is not None:
+        if OmegaConf.is_config(hparams):
+            OmegaConf.save(hparams, config_yaml, resolve=True)
+            return
+        for v in hparams.values():
+            if OmegaConf.is_config(v):
+                OmegaConf.save(OmegaConf.create(hparams), config_yaml, resolve=True)
+                return
+
+    # saving the standard way
     assert isinstance(hparams, dict)
-
-    with cloud_open(config_yaml, "w", newline="") as fp:
+    with open(config_yaml, 'w', newline='') as fp:
         yaml.dump(hparams, fp)
-
 
 def convert(val: str) -> Union[int, float, bool, str]:
     try:
