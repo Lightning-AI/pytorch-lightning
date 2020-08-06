1a26952994370cbc9eb548da2f7ce741cad3e320
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-06 22:44:09 +0200

    ddptest

--- tests/models/test_gpu.py
+++ tests/models/test_gpu.py
@@ -101,9 +101,9 @@ def test_multi_gpu_model_dp(tmpdir):
 @pytest.mark.parametrize('cli_args', [
     '--max_epochs 1 --gpus 2 --distributed_backend ddp',
 ])
-#@pytest.mark.parametrize('variation', train_default_model.get_variations())
+@pytest.mark.parametrize('variation', train_default_model.get_variations())
 #@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-def test_multi_gpu_model_ddp(tmpdir, cli_args):
+def test_multi_gpu_model_ddp(tmpdir, cli_args, variation):
     assert True
     # file = Path(train_default_model.__file__).absolute()
     # cli_args = cli_args.split(' ') if cli_args else []
