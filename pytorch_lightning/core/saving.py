5a96e3015722aa93fbdb62a2f6bb7edd9a0e3bf5
Jian S <sj8716643@126.com>
2020-08-07 00:57:12 +0800

    Removed redundant code & test

--- pytorch_lightning/core/saving.py
+++ pytorch_lightning/core/saving.py
@@ -40,7 +40,6 @@ class ModelIO(object):
             map_location: Optional[Union[Dict[str, str], str, torch.device, int, Callable]] = None,
             hparams_file: Optional[str] = None,
             strict: bool = True,
-            tags_csv: Optional[str] = None,  # backward compatible, todo: remove in v0.9.0
             **kwargs
     ):
         r"""
@@ -62,8 +61,6 @@ class ModelIO(object):
                     drop_prob: 0.2
                     dataloader:
                         batch_size: 32
-
-                Or pass a dict directly.
 
                 You most likely won't need this since Lightning will always save the hyperparameters
                 to the checkpoint.
@@ -117,24 +114,14 @@ class ModelIO(object):
         else:
             checkpoint = pl_load(checkpoint_path, map_location=lambda storage, loc: storage)
 
-        # add the hparams from csv file to checkpoint
-        if tags_csv is not None:
-            hparams_file = tags_csv
-            rank_zero_warn('`tags_csv` argument is deprecated in v0.7.6. Will be removed v0.9.0', DeprecationWarning)
-
         if hparams_file is not None:
-            if isinstance(hparams_file, str):
-                extension = hparams_file.split('.')[-1]
-                if extension.lower() in ('csv'):
-                    hparams = load_hparams_from_tags_csv(hparams_file)
-                elif extension.lower() in ('yml', 'yaml'):
-                    hparams = load_hparams_from_yaml(hparams_file)
-                else:
-                    raise ValueError('.csv, .yml or .yaml is required for `hparams_file`')
-            elif isinstance(hparams_file, dict):
-                hparams = hparams_file
+            extension = hparams_file.split('.')[-1]
+            if extension.lower() in ('csv'):
+                hparams = load_hparams_from_tags_csv(hparams_file)
+            elif extension.lower() in ('yml', 'yaml'):
+                hparams = load_hparams_from_yaml(hparams_file)
             else:
-                raise ValueError('`hparams_file` must be either a dict or a path to .csv, .yml or .yaml.')
+                raise ValueError('.csv, .yml or .yaml is required for `hparams_file`')
 
             hparams['on_gpu'] = False
 
