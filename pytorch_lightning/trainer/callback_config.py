c8af476ff6b7af671323d71db948e8b573c2d8fe
Jirka Borovec <jirka@pytorchlightning.ai>
2020-08-07 23:29:32 +0200

    miss

--- pytorch_lightning/trainer/callback_config.py
+++ pytorch_lightning/trainer/callback_config.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from abc import ABC, abstractmethod
 from typing import List, Optional
 
 from pytorch_lightning.callbacks import Callback, ModelCheckpoint, EarlyStopping, ProgressBarBase, ProgressBar
