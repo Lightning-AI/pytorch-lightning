fa4474987a392d0df0ee2096022cff7f69e8c070
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-07 02:43:22 +0200

    test

--- tests/models/test_gpu.py
+++ tests/models/test_gpu.py
@@ -110,12 +110,14 @@ def test_multi_gpu_model_ddp(tmpdir, cli_args, variation):
     command = [sys.executable, file, '--variation', variation] + cli_args
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     std, err = p.communicate(timeout=60)
-    #assert std and not err
+    std = std.decode('utf-8').strip()
+    err = err.decode('utf-8').strip()
+    assert std and not err
     if p.returncode:
         print(std)
         print(err)
         print(command)
-        raise RuntimeError('error')
+        pytest.fail(err)
 
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
