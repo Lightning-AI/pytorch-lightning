7dd81f13da8f355bffbfe06267d89bde552ebd19
monney <manbirgulati@gmail.com>
2020-08-08 11:28:16 -0400

    PEP8

--- pytorch_lightning/utilities/parsing.py
+++ pytorch_lightning/utilities/parsing.py
@@ -44,7 +44,7 @@ def clean_namespace(hparams):
     if isinstance(hparams, Namespace):
         hparams_dict = hparams.__dict__
 
-    del_attrs = [k for k,v in hparams_dict.items() if not is_picklable(v)]
+    del_attrs = [k for k, v in hparams_dict.items() if not is_picklable(v)]
 
     for k in del_attrs:
         warnings.warn(f"attribute '{k}' removed from hparams because it cannot be pickled", UserWarning)
