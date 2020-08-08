a4752e74db0e93dff8380bd2ed2e94ccbd20a99a
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-08-08 14:33:59 +0200

    import without exception

--- pytorch_lightning/utilities/imports.py
+++ pytorch_lightning/utilities/imports.py
@@ -14,24 +14,13 @@ def is_apex_available():
 
 
 def is_horovod_available():
-    # TODO: use importlib
-    try:
-        import horovod.torch as hvd
-    except (ModuleNotFoundError, ImportError):
-        return False
-    else:
-        return True
+    return (importlib.util.find_spec("horovod") is not None and
+            importlib.util.find_spec("horovod.torch") is not None)
 
 
 def is_hydra_available():
-    # TODO: use importlib
-    try:
-        from hydra.utils import to_absolute_path, get_original_cwd
-        from hydra.core.hydra_config import HydraConfig
-    except ImportError:
-        return False
-    else:
-        return True
+    return (importlib.util.find_spec("hydra") is not None and
+            importlib.util.find_spec("hydra.core") is not None)
 
 
 def is_native_amp_available():
@@ -39,13 +28,7 @@ def is_native_amp_available():
 
 
 def is_omegaconf_available():
-    # TODO: use importlib
-    try:
-        from omegaconf import Container
-    except ImportError:
-        return False
-    else:
-        return True
+    return importlib.util.find_spec("omegaconf") is not None
 
 
 def is_torchtext_available():
