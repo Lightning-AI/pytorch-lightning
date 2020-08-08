ef376f5f39933af4056698551c7f6022983e92c9
Santiago Castro <bryant@montevideo.com.uy>
2020-08-08 01:09:50 -0400

    Fix a PR comment

--- pytorch_lightning/trainer/__init__.py
+++ pytorch_lightning/trainer/__init__.py
@@ -818,7 +818,7 @@ replace_sampler_ddp
 ^^^^^^^^^^^^^^^^^^^
 Enables auto adding of distributed sampler. By default it will add ``shuffle=True``
 for train sampler and ``shuffle=False`` for val/test sampler. If you want to customize
-it, you can set ``replace_ddp_sampler=False`` and add your own distributed sampler.
+it, you can set ``replace_sampler_ddp=False`` and add your own distributed sampler.
 
 .. testcode::
 
