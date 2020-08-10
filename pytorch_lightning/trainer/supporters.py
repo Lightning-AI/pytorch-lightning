7b40fc0746157b93d3ee4d674fa7891861f404ee
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-10 03:28:04 +0200

    debug

--- pytorch_lightning/trainer/supporters.py
+++ pytorch_lightning/trainer/supporters.py
@@ -119,9 +119,11 @@ class DistributedConnection:
             if trainer.global_rank == 0:
                 new_port = trainer.set_random_port(force=True, overwrite=False)
                 print('sending new port on rank=', trainer.global_rank, 'port', new_port)
-                new_port = torch.tensor(new_port, device=model.device)
+                new_port = torch.tensor([new_port], device=model.device)
+                print(new_port)
             else:
                 new_port = torch.empty(1, device=model.device)
+                print(new_port)
 
             torch.distributed.broadcast(new_port, src=0)
             new_port = int(new_port.item())
