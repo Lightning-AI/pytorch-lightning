5da491c314d6f7c0b7bd02a3260deb75263d85f8
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-11 21:24:31 +0200

    clean up tests

--- tests/trainer/test_signal_handling.py
+++ tests/trainer/test_signal_handling.py
@@ -1,32 +1,21 @@
 import os
 import platform
 import signal
-import subprocess
-import sys
-import time
-from pathlib import Path
 
 import pytest
-import torch
 import torch.distributed
 
-import pytorch_lightning
 from pytorch_lightning import Trainer, Callback
 from pytorch_lightning.trainer.states import TrainerState
 from tests.base import EvalModelTemplate
-from pl_examples.basic_examples import gpu_template
-
-from torch.multiprocessing import Process
-
-from tests.base.develop_utils import pl_multi_process_test
 
 
 class KillCallback(Callback):
+    """ A callback that simulates a terminal signal (e.g. KeyboardInterrupt). """
 
     def __init__(self, signal):
         self._signal = signal
 
-    # simulate a termination signal
     def on_batch_end(self, trainer, pl_module):
         # send the signal after the first batch
         assert trainer.global_step == 0, "did not interrupt training after first batch"
@@ -34,7 +23,6 @@ class KillCallback(Callback):
         os.kill(pid, self._signal)
 
     def on_train_end(self, trainer, pl_module):
-        print('EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEENNNNNNNNNNNNNNNNNNNNNNNNNNND')
         assert getattr(trainer, "_teardown_already_run")
 
     def on_keyboard_interrupt(self, trainer, pl_module):
@@ -70,9 +58,6 @@ def test_graceful_training_shutdown(tmpdir, signal_code):
     trainer.fit(model)
 
 
-from torch.multiprocessing import Process
-
-
 @pytest.mark.parametrize(["signal_code"], get_available_signal_codes())
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 def test_graceful_training_shutdown_gpu(tmpdir, signal_code):
@@ -85,21 +70,3 @@ def test_graceful_training_shutdown_gpu(tmpdir, signal_code):
     )
     model = EvalModelTemplate()
     trainer.fit(model)
-    # torch.multiprocessing.set_start_method("spawn", force=True)
-    # p = Process(target=trainer.fit, args=(model, ))
-    # p.start()
-    # p.join(timeout=60)
-    #assert p.exitcode == -signal_code
-
-
-# @pytest.mark.skipif(torch.cuda.device_count() < 2, reason='Test requires multiple GPUs.')
-# @pytest.mark.parametrize(['signal_code'], get_available_signal_codes())
-# def test_graceful_training_shutdown_gpu(signal_code):
-#     trainer = Trainer(
-#         max_epochs=100,
-#         distributed_backend='ddp',
-#         callbacks=[KillCallback(signal_code)],
-#         gpus=2
-#     )
-#     model = EvalModelTemplate()
-#     trainer.fit(model)
