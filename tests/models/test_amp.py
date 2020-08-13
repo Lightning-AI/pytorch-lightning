b2fc6173c3028207bc40d3fb7fa9c6c10319d106
Jirka Borovec <jirka@pytorchlightning.ai>
2020-08-13 11:27:00 +0200

    apex

--- tests/models/test_amp.py
+++ tests/models/test_amp.py
@@ -208,4 +208,4 @@ def test_amp_with_apex(tmpdir):
     assert str(trainer.amp_backend) == "AMPType.APEX"
     trainer.fit(model)
     assert trainer.state == TrainerState.FINISHED
-    assert trainer.dev_debugger.count_events('AMP') == 1
+    assert trainer.dev_debugger.count_events('AMP') >= 1
