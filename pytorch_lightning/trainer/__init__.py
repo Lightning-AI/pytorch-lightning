b7fc805dcf56728aa1239da9d7cdc2b3671573fe
William Falcon <waf2107@columbia.edu>
2020-08-13 18:56:02 -0400

    pep 8 (#2967)

--- pytorch_lightning/trainer/__init__.py
+++ pytorch_lightning/trainer/__init__.py
@@ -12,7 +12,8 @@ the Trainer automates everything else.
 
 .. raw:: html
 
-    <video width="800" controls src="https://pl-bolts-doc-images.s3.us-east-2.amazonaws.com/pl_docs/pt_trainer_mov.m4v"></video>
+    <video width="800" controls
+    src="https://pl-bolts-doc-images.s3.us-east-2.amazonaws.com/pl_docs/pt_trainer_mov.m4v"></video>
 
 |
 
