46d69f06e653bf08912d043b6c3bb7665f527ea7
William Falcon <waf2107@columbia.edu>
2020-08-15 08:10:19 -0400

    add step metrics

--- tests/trainer/test_validation_steps_result_return.py
+++ tests/trainer/test_validation_steps_result_return.py
@@ -427,11 +427,11 @@ def test_val_step_full_loop_result_dp(tmpdir):
     assert 'train_step_metric' in seen_keys
     assert 'train_step_end_metric' in seen_keys
     assert 'epoch_train_epoch_end_metric' in seen_keys
-    assert 'step_validation_step_metric' in seen_keys
+    assert 'step_validation_step_metric/epoch_0' in seen_keys
     assert 'epoch_validation_step_metric' in seen_keys
     assert 'validation_step_end_metric' in seen_keys
     assert 'validation_epoch_end_metric' in seen_keys
-    assert 'step_test_step_metric' in seen_keys
+    assert 'step_test_step_metric/epoch_0' in seen_keys
     assert 'epoch_test_step_metric' in seen_keys
     assert 'test_step_end_metric' in seen_keys
     assert 'test_epoch_end_metric' in seen_keys
