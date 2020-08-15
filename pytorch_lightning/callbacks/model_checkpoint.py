df49e5c01d9643e01de98b3f0ef3f11b02ce8aa7
Lezwon Castelino <lezwon@gmail.com>
2020-08-15 11:12:35 +0530

    removed rank_zero_only from on_train_start

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -261,7 +261,6 @@ class ModelCheckpoint(Callback):
         ckpt_name = f'{filename}.ckpt'
         return os.path.join(self.dirpath, ckpt_name) if self.dirpath else ckpt_name
 
-    @rank_zero_only
     def on_pretrain_routine_start(self, trainer, pl_module):
         """
         Determines model checkpoint save directory at runtime. References attributes from the
