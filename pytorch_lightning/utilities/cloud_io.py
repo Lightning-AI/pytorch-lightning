15eb2b857aed0ce4bef76fe459a5738e5d19e698
Lezwon Castelino <lezwon@gmail.com>
2020-08-17 17:01:54 +0530

    ayee add this too. done saving part

--- pytorch_lightning/utilities/cloud_io.py
+++ pytorch_lightning/utilities/cloud_io.py
@@ -65,7 +65,7 @@ def atomic_save(checkpoint, filepath: str, is_xla_tensor=False):
     if is_xla_tensor:
         print(inspect.currentframe().f_code.co_name)
         saved = xm.save(checkpoint['state_dict'], filepath, master_only=True, global_master=True)
-        print(inspect.currentframe().f_code.co_name)
+        print(inspect.currentframe().f_code.co_name, 'Done saving')
         return saved
 
     elif LooseVersion(torch.__version__).version[:3] == [1, 6, 0]:
