0d34ee2f5fe34bcb4f3df99a5961d52bc2d8fa1c
atee <atee@evidscience.com>
2020-08-17 17:48:21 -0700

    Getting rid decrement_global_step function

--- tests/loggers/test_all.py
+++ tests/loggers/test_all.py
@@ -79,6 +79,8 @@ def test_loggers_fit_test(wandb, tmpdir, monkeypatch, logger_class):
     trainer.test()
 
     log_metric_names = [(s, sorted(m.keys())) for s, m in logger.history]
+    print("log_metric_names")
+    print(log_metric_names)
     assert log_metric_names == [(0, ['epoch', 'val_acc', 'val_loss']),
                                 (0, ['epoch', 'train_some_val']),
                                 (1, ['epoch', 'test_acc', 'test_loss'])]
