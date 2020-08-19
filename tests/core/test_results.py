3504a48e3da3c779903d5c8c5af31c8c2d4d75e7
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-08-19 15:54:52 +0200

    pep8

--- tests/core/test_results.py
+++ tests/core/test_results.py
@@ -236,7 +236,8 @@ def test_result_gather_mixed_types():
     expected = [1.2, ["bar", None], torch.tensor(1)]
     assert isinstance(result["foo"], list)
     assert result["foo"] == expected
-    
+
+
 def test_result_retrieve_last_logged_item():
     result = Result()
     result.log('a', 5., on_step=True, on_epoch=True)
