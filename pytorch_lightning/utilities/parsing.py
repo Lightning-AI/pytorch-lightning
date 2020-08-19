3c2c45cd50fa1ff4ff6cca873f6a533c0808991b
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-19 02:38:46 +0200

    revert refactor

--- pytorch_lightning/utilities/parsing.py
+++ pytorch_lightning/utilities/parsing.py
@@ -150,7 +150,10 @@ def lightning_hasattr(model, attribute):
         attr = True
     # Check if attribute in model.hparams, either namespace or dict
     elif hasattr(model, 'hparams'):
-        attr = attribute in model.hparams
+        if isinstance(model.hparams, dict):
+            attr = attribute in model.hparams
+        else:
+            attr = hasattr(model.hparams, attribute)
     else:
         attr = False
 
