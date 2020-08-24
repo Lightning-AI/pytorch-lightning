02da516b804f160959706deaf64516a4378c3d76
William Falcon <waf2107@columbia.edu>
2020-08-24 08:58:12 -0400

    refactored gpu backend __step

--- pytorch_lightning/utilities/seed.py
+++ pytorch_lightning/utilities/seed.py
@@ -50,6 +50,7 @@ def seed_everything(seed: Optional[int] = None) -> int:
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     return seed
 
 
