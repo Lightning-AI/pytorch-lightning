591ab944de61641c591f9d5b47710e19357ddd77
William Falcon <waf2107@columbia.edu>
2020-08-27 10:14:48 -0400

    enable dp tests

--- tests/models/test_amp.py
+++ tests/models/test_amp.py
@@ -13,7 +13,6 @@ from tests.base import EvalModelTemplate
 from pytorch_lightning.utilities import APEX_AVAILABLE
 
 
-@pytest.mark.skip(reason='dp + amp not supported currently')  # TODO
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
 def test_amp_single_gpu_dp(tmpdir):
     """Make sure DP/DDP + AMP work."""
@@ -53,7 +52,6 @@ def test_amp_single_gpu_ddp_spawn(tmpdir):
     assert result == 1
 
 
-@pytest.mark.skip(reason='dp + amp not supported currently')  # TODO
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
 def test_amp_multi_gpu_dp(tmpdir):
     """Make sure DP/DDP + AMP work."""
