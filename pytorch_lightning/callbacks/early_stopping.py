6e5aad39f3e4ebb8a79b95d4f621b33310eb1da1
William Falcon <waf2107@columbia.edu>
2020-08-26 22:19:48 -0400

    group prepare data hook

--- pytorch_lightning/callbacks/early_stopping.py
+++ pytorch_lightning/callbacks/early_stopping.py
@@ -178,7 +178,7 @@ class EarlyStopping(Callback):
 
     def __warn_deprecated_monitor_key(self):
         using_result_obj = os.environ.get('PL_USING_RESULT_OBJ', None)
-        invalid_key = self.monitor not in ['val_loss', 'early_stop_on', 'val_early_stop_on', 'loss']
+        invalid_key = self.monitor not in ['val_loss', 'early_stop_on', 'val_early_step_on', 'loss']
         if using_result_obj and not self.warned_result_obj and invalid_key:
             self.warned_result_obj = True
             m = f"""
