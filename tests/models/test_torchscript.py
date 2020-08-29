d6f6437078795f2ef87b6bd404383852f35957a6
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-08-30 00:40:59 +0200

    datamodule test

--- tests/models/test_torchscript.py
+++ tests/models/test_torchscript.py
@@ -39,7 +39,9 @@ def test_torchscript_input_output(modelclass):
 def test_torchscript_properties(modelclass):
     """ Test that scripted LightningModule has unnecessary methods removed. """
     model = modelclass()
+    model.datamodule = TrialMNISTDataModule()
     script = model.to_torchscript()
+    assert not hasattr(script, "datamodule")
     assert not hasattr(model, "batch_size") or hasattr(script, "batch_size")
     assert not hasattr(model, "learning_rate") or hasattr(script, "learning_rate")
     assert not callable(getattr(script, "training_step", None))
