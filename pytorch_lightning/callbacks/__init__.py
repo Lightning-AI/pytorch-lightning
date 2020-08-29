da91286349c5f267fb811a90e49f961ca162c7c0
rohitgr7 <rohitgr1998@gmail.com>
2020-08-30 03:57:22 +0530

    deprecated LearningRateLogger

--- pytorch_lightning/callbacks/__init__.py
+++ pytorch_lightning/callbacks/__init__.py
@@ -2,7 +2,8 @@ from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks.gpu_stats_monitor import GPUStatsMonitor
 from pytorch_lightning.callbacks.gradient_accumulation_scheduler import GradientAccumulationScheduler
-from pytorch_lightning.callbacks.lr_monitor import LearningRateLogger, LearningRateMonitor
+from pytorch_lightning.callbacks.lr_logger import LearningRateLogger
+from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks.progress import ProgressBar, ProgressBarBase
 
---
+++ pytorch_lightning/callbacks/lr_logger.py
@@ -0,0 +1,9 @@
+from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
+from pytorch_lightning.utilities import rank_zero_warn
+
+
+class LearningRateLogger(LearningRateMonitor):
+    def __init__(self, *args, **kwargs):
+        rank_zero_warn("`LearningRateLogger` is now `LearningRateMonitor`"
+                       " and this will be removed in v0.10.0", DeprecationWarning)
+        super().__init__(*args, **kwargs)
