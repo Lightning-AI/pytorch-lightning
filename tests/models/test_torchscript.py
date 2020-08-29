246e875d40d94dff9dd3d76053668dafe39383b3
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-08-29 22:02:45 +0200

    move test

--- tests/models/test_torchscript.py
+++ tests/models/test_torchscript.py
@@ -1,6 +1,7 @@
 import torch
 
 from pytorch_lightning import LightningModule
+from tests.base import EvalModelTemplate
 
 
 class SimpleModel(LightningModule):
@@ -15,13 +16,17 @@ class SimpleModel(LightningModule):
 
 def test_torchscript_save_load(tmpdir):
     """ Test that scripted LightningModule behaves like the original. """
-    model = SimpleModel()
-    example_input = torch.rand(5, 64)
+    model = EvalModelTemplate()
     script = model.to_torchscript()
     assert isinstance(script, torch.jit.ScriptModule)
     output_file = str(tmpdir / "model.jit")
     torch.jit.save(script, output_file)
     script = torch.jit.load(output_file)
-    model_output = model(example_input)
-    script_output = script(example_input)
+    # properties
+    assert script.batch_size == model.batch_size
+    assert script.learning_rate == model.learning_rate
+    assert not callable(getattr(script, "training_step", None))
+    # output matches
+    model_output = model(model.example_input_array)
+    script_output = script(model.example_input_array)
     assert torch.allclose(script_output, model_output)
