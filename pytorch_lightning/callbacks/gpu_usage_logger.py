403119674a8846f4925bc592fd5ad1ed0bf62c7f
rohitgr7 <rohitgr1998@gmail.com>
2020-08-30 05:22:55 +0530

    pep

--- pytorch_lightning/callbacks/gpu_usage_logger.py
+++ pytorch_lightning/callbacks/gpu_usage_logger.py
@@ -5,5 +5,5 @@ from pytorch_lightning.utilities import rank_zero_warn
 class GpuUsageLogger(GPUStatsMonitor):
     def __init__(self, *args, **kwargs):
         rank_zero_warn("`GpuUsageLogger is now `GPUStatsMonitor`"
-                      " and it will be removed in v0.10.0", DeprecationWarning)
+                       " and it will be removed in v0.10.0", DeprecationWarning)
         super().__init__(*args, **kwargs)
