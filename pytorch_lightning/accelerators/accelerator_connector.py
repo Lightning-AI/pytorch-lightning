34c75fcce4874ac73b6504c0ad2613ab9c0de51e
William Falcon <waf2107@columbia.edu>
2020-09-01 15:25:50 -0400

    ref: moved accelerator

--- pytorch_lightning/accelerators/accelerator_connector.py
+++ pytorch_lightning/accelerators/accelerator_connector.py
@@ -19,33 +19,33 @@ class AcceleratorConnector:
 
         # choose the appropriate accelerator backend
         if self.trainer.use_ddp2:
-            accelerator_backend = accelerators.DDP2Backend(self)
+            accelerator_backend = accelerators.DDP2Backend(self.trainer)
 
         elif use_slurm_ddp:
-            accelerator_backend = accelerators.DDPBackend(self, mode='slurm_ddp')
+            accelerator_backend = accelerators.DDPBackend(self.trainer, mode='slurm_ddp')
 
         elif use_torchelastic_ddp:
-            accelerator_backend = accelerators.DDPBackend(self, mode='torchelastic_ddp')
+            accelerator_backend = accelerators.DDPBackend(self.trainer, mode='torchelastic_ddp')
 
         elif use_ddp_spawn:
-            accelerator_backend = accelerators.DDPSpawnBackend(self, nprocs=self.trainer.num_processes)
+            accelerator_backend = accelerators.DDPSpawnBackend(self.trainer, nprocs=self.trainer.num_processes)
 
         elif self.trainer.distributed_backend == 'ddp':
-            accelerator_backend = accelerators.DDPBackend(self, mode='ddp')
+            accelerator_backend = accelerators.DDPBackend(self.trainer, mode='ddp')
 
         elif self.trainer.use_dp:
-            accelerator_backend = accelerators.DataParallelBackend(self)
+            accelerator_backend = accelerators.DataParallelBackend(self.trainer)
 
         elif self.trainer.use_horovod:
-            accelerator_backend = accelerators.HorovodBackend(self)
+            accelerator_backend = accelerators.HorovodBackend(self.trainer)
 
         elif self.trainer.use_single_gpu:
-            accelerator_backend = accelerators.GPUBackend(self)
+            accelerator_backend = accelerators.GPUBackend(self.trainer)
 
         elif self.trainer.use_tpu:
-            accelerator_backend = accelerators.TPUBackend(self)
+            accelerator_backend = accelerators.TPUBackend(self.trainer)
 
         else:
-            accelerator_backend = accelerators.CPUBackend(self)
+            accelerator_backend = accelerators.CPUBackend(self.trainer)
 
         return accelerator_backend
