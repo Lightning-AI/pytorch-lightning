ee72271d205beb2c012e34425cb16e189cc56c7d
nithin varghese <39826756+humandotlearning@users.noreply.github.com>
2020-09-02 21:27:56 +0530

    Docs/improved multigpu doc clarity (#3194)
    * + clarifying doc
    
    * improved documentation clarity
    
    * docs minor correction
    
    * remove device from type_as
    
    Co-authored-by: Rohit Gupta <rohitgr1998@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -233,7 +233,7 @@ class Trainer(
 
             num_nodes: number of GPU nodes for distributed training.
 
-            gpus: Which GPUs to train on.
+            gpus: number of gpus to train on (int) or which GPUs to train on (list or str) applied per node
 
             auto_select_gpus:
 
