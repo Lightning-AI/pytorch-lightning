9832a8e9f4dccd240c873209651f82cd8903b7d0
Nicki Skafte <skaftenicki@gmail.com>
2020-09-07 10:41:09 +0200

    fix imports

--- pytorch_lightning/metrics/__init__.py
+++ pytorch_lightning/metrics/__init__.py
@@ -1,8 +1,13 @@
 from pytorch_lightning.metrics.converters import numpy_metric, tensor_metric
+from pytorch_lightning.metrics.metric import *
 from pytorch_lightning.metrics.metric import __all__ as __base_metrics
+from pytorch_lightning.metrics.classification import *
 from pytorch_lightning.metrics.classification import __all__ as __classification_metrics
+from pytorch_lightning.metrics.nlp import *
 from pytorch_lightning.metrics.nlp import __all__ as __nlp_metrics
+from pytorch_lightning.metrics.regression import *
 from pytorch_lightning.metrics.regression import __all__ as __regression_metrics
+from pytorch_lightning.metrics.self_supervised import *
 from pytorch_lightning.metrics.self_supervised import __all__ as __selfsupervised_metrics
 
 
