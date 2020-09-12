85167e15f1a85154a88dd4ed6ebf7a8f9b14436a
William Falcon <waf2107@columbia.edu>
2020-09-12 17:47:58 -0400

    ref: ddp verify

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -388,8 +388,6 @@ class Trainer(
                 # hook
                 self.train_loop.on_train_end()
 
-            return
-
     def run_evaluation(self, test_mode: bool = False, max_batches=None):
         # bookkeeping
         self.evaluation_loop.testing = test_mode
