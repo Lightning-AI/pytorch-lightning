a552d4a2d5056705c68f2eed570a83ee3160b3bc
Cookie_thief <egorplotnikov18@gmail.com>
2020-09-14 11:05:51 +0300

    fix normalize mode at confusion matrix (replace nans with zeros) (#3465)
    * replace nans to 0 at conf. matrix & update tests
    
    * cm.isnan() -> torch.isnan(cm)
    
    * fix row-wise division while normalize
    
    * update tests
    
    * pep8 fix
    
    * Update tests/metrics/test_classification.py
    
    add comment to test
    
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>
    
    * Update tests/metrics/functional/test_classification.py
    
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>
    
    * Update pytorch_lightning/metrics/functional/classification.py
    
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>
    
    * final update
    
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>

--- tests/metrics/functional/test_classification.py
+++ tests/metrics/functional/test_classification.py
@@ -187,6 +187,17 @@ def test_confusion_matrix():
     cm = confusion_matrix(pred, target, normalize=False, num_classes=3)
     assert torch.allclose(cm, torch.tensor([[5., 0., 0.], [0., 0., 0.], [0., 0., 0.]]))
 
+    # Example taken from https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    target = torch.LongTensor([0] * 13 + [1] * 16 + [2] * 9)
+    pred = torch.LongTensor([0] * 13 + [1] * 10 + [2] * 15)
+    cm = confusion_matrix(pred, target, normalize=False, num_classes=3)
+    assert torch.allclose(cm, torch.tensor([[13., 0., 0.], [0., 10., 6.], [0., 0., 9.]]))
+    to_compare = cm / torch.tensor([[13.], [16.], [9.]])
+
+    cm = confusion_matrix(pred, target, normalize=True, num_classes=3)
+    assert torch.allclose(cm, to_compare)
+
+
 
 @pytest.mark.parametrize(['pred', 'target', 'expected_prec', 'expected_rec'], [
     pytest.param(torch.tensor([1., 0., 1., 0.]), torch.tensor([0., 1., 1., 0.]), [0.5, 0.5], [0.5, 0.5]),
