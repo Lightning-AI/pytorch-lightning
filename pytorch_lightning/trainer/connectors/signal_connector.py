d5c4185d2b5fdb0ff410e3d9bd757a0aacd4c7e1
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-09-16 23:42:04 +0200

    cleanup

--- pytorch_lightning/trainer/connectors/signal_connector.py
+++ pytorch_lightning/trainer/connectors/signal_connector.py
@@ -1,15 +1,13 @@
 import signal
 
-import pytorch_lightning as pl
-from pytorch_lightning import _logger as log
 from pytorch_lightning.trainer.states import TrainerState
 
 
 class SignalConnector:
     """
     Takes care of registering and restoring signal handlers for the
-    :class:`~pytorch_lightning.trainer.trainer.Trainer`. This includes handling
-    graceful shutdown for KeyboardInterrupt or SLURM autoresubmit signals.
+    :class:`~pytorch_lightning.trainer.trainer.Trainer`. By default, it handles
+    SIGTERM, SIGINT and SIGSEGV by raising KeyboardInterrupt and letting Trainer do graceful shutdown.
     """
 
     def __init__(self, trainer):
@@ -25,7 +23,6 @@ class SignalConnector:
         self.register_signal(signal.SIGTERM, self.default_teardown)
         self.register_signal(signal.SIGSEGV, self.default_teardown)
         self.register_signal(signal.SIGINT, self.default_teardown)
-        # atexit.register(self.trainer.run_training_teardown)
 
     def restore_signals(self):
         """ Restores the original signal handlers (e.g. the Python defaults) """
@@ -38,10 +35,8 @@ class SignalConnector:
         signal.signal(signum, handler)
 
     def default_teardown(self, signum, frame):  # pragma: no-cover
-        """ Handles teardown for certain signals that interrupt training. """
-        # trainer = self.trainer
+        """ This default teardown raises KeyboardInterrupt and lets Trainer handle the graceful shutdown. """
         if not self.trainer.interrupted:
             self.trainer.interrupted = True
             self.trainer._state = TrainerState.INTERRUPTED
-            # trainer.on_keyboard_interrupt()
             raise KeyboardInterrupt
