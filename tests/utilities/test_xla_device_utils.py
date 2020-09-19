bc24a463ee5aaad3098ac60f299df4ba206ced0d
Lezwon Castelino <lezwon@gmail.com>
2020-09-19 18:40:42 +0530

    added tests

---
+++ tests/utilities/test_xla_device_utils.py
@@ -0,0 +1,31 @@
+import pytest
+
+import pytorch_lightning.utilities.xla_device_utils as xdu
+from tests.base.develop_utils import pl_multi_process_test
+
+try:
+    import torch_xla.core.xla_model as xm
+    XLA_AVAILABLE = True
+except:
+    XLA_AVAILABLE = False
+
+
+@pytest.mark.skipif(XLA_AVAILABLE, reason="test requires torch_xla to be absent")
+def test_tpu_device_does_not_exist():
+    """Check tpu_device_exists returns None when torch_xla is not available"""
+    assert xdu.tpu_device_exists() is None
+
+
+@pytest.mark.skipif(not XLA_AVAILABLE, reason="test requires torch_xla to be installed")
+def test_tpu_device_does_not_exist():
+    """Check tpu_device_exists returns True when TPU is available"""
+    assert xdu.tpu_device_exists() is True
+
+
+@pytest.mark.skipif(not XLA_AVAILABLE, reason="test requires torch_xla to be installed")
+@pl_multi_process_test
+def test_tpu_device_does_not_exist():
+    """Check that the XLA device is a TPU"""
+    device = xm.xla_device()
+    device_type = xm.xla_device_hw(device)
+    return device_type == "TPU"
