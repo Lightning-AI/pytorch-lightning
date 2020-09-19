8d59c3aa551a6682e0135d584598e19899d983f7
ananthsub <ananth.subramaniam@gmail.com>
2020-09-19 15:23:34 -0700

    hooks formatting

--- pytorch_lightning/core/hooks.py
+++ pytorch_lightning/core/hooks.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Union, List
+from typing import Any, Dict, List, Union
 
 import torch
 from torch import Tensor
