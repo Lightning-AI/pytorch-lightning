b64225a30c15fdedd58c4ad9484a218d01226831
William Falcon <waf2107@columbia.edu>
2020-09-22 10:44:26 -0400

    enable any logged or written metric to be accessible in callbacks

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -357,7 +357,7 @@ class ModelCheckpoint(Callback):
         self.epoch_last_check = epoch
 
         # anything logged or in callbacks can be in the name
-        ckpt_name_metrics = deepcopy(trainer.logger_connector.logged_metrics)
+        ckpt_name_metrics = {k: v for k, v in trainer.logger_connector.logged_metrics.item()} #deepcopy(trainer.logger_connector.logged_metrics)
         ckpt_name_metrics.update(trainer.logger_connector.callback_metrics)
         ckpt_name_metrics.update(trainer.logger_connector.progress_bar_metrics)
 
