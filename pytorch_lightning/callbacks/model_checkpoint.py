ec57742907c75612f6ce15471907727939e81958
William Falcon <waf2107@columbia.edu>
2020-09-22 17:17:12 -0400

    clarify forward

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -357,10 +357,9 @@ class ModelCheckpoint(Callback):
         self.epoch_last_check = epoch
 
         # anything logged or in callbacks can be in the name
-        ckpt_name_metrics = trainer.logger_connector.logged_metrics
-        # ckpt_name_metrics = {k: v for k, v in trainer.logger_connector.logged_metrics.items()} #deepcopy(trainer.logger_connector.logged_metrics)
-        # ckpt_name_metrics.update(trainer.logger_connector.callback_metrics)
-        # ckpt_name_metrics.update(trainer.logger_connector.progress_bar_metrics)
+        ckpt_name_metrics = deepcopy(trainer.logger_connector.logged_metrics)
+        ckpt_name_metrics.update(trainer.logger_connector.callback_metrics)
+        ckpt_name_metrics.update(trainer.logger_connector.progress_bar_metrics)
 
         filepath = self.format_checkpoint_name(epoch, ckpt_name_metrics)
         version_cnt = 0
