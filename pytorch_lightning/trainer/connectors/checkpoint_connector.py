2d827c51878a0f02cd34f9ea592a8d9133521379
ananthsub <ananth.subramaniam@gmail.com>
2020-09-21 23:24:31 -0700

    update based on upstream/master
    checkout upstream/master

--- pytorch_lightning/trainer/connectors/checkpoint_connector.py
+++ pytorch_lightning/trainer/connectors/checkpoint_connector.py
@@ -106,9 +106,6 @@ class CheckpointConnector:
 
         # give model a chance to load something
         model.on_load_checkpoint(checkpoint)
-        # give the datamodule a chance to load something
-        if self.trainer.datamodule is not None:
-            self.trainer.datamodule.on_load_checkpoint(checkpoint)
 
         if on_gpu:
             model.cuda(self.trainer.root_gpu)
@@ -297,8 +294,6 @@ class CheckpointConnector:
 
         # give the model a chance to add a few things
         model.on_save_checkpoint(checkpoint)
-        if self.trainer.datamodule is not None:
-            self.trainer.datamodule.on_save_checkpoint(checkpoint)
 
         return checkpoint
 
