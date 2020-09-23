8f13ffad27f65fb66a0fdb77a420196a08f099e5
William Falcon <waf2107@columbia.edu>
2020-09-22 21:42:42 -0400

    fix examples

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -1,11 +1,35 @@
 from unittest import mock
-
+import torch
 import pytest
 
+dp_16_args = """
+--max_epochs 1 
+--batch_size 32 
+--limit_train_batches 2 
+--limit_val_batches 2 
+--gpus 2 
+--distributed_backend dp 
+--precision 16
+"""
 
-@pytest.mark.parametrize('cli_args', [
-    '--max_epochs 1 --batch_size 2 --limit_train_batches 2 --limit_val_batches 2',
-])
+cpu_args = """
+--max_epochs 1 
+--batch_size 32 
+--limit_train_batches 2 
+--limit_val_batches 2 
+"""
+
+ddp_args = """
+--max_epochs 1 
+--batch_size 32 
+--limit_train_batches 2 
+--limit_val_batches 2 
+--gpus 2 
+--precision 16
+"""
+
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.parametrize('cli_args', [dp_16_args, cpu_args, ddp_args])
 def test_image_classifier_example(cli_args):
     from pl_examples.basic_examples.mnist import cli_main
 
@@ -13,9 +37,8 @@ def test_image_classifier_example(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', [
-    '--max_epochs 1 --batch_size 2 --limit_train_batches 2 --limit_val_batches 2',
-])
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.parametrize('cli_args', [dp_16_args, cpu_args, ddp_args])
 def test_mnist_example(cli_args):
     from pl_examples.basic_examples.image_classifier import cli_main
 
@@ -23,9 +46,8 @@ def test_mnist_example(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', [
-    '--max_epochs 1 --batch_size 2 --limit_train_batches 2 --limit_val_batches 2',
-])
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.parametrize('cli_args', [dp_16_args, cpu_args, ddp_args])
 def test_autoencoder_example(cli_args):
     from pl_examples.basic_examples.autoencoder import cli_main
 
