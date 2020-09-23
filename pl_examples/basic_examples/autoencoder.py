3fa98721196cabc3fd5120396797ef6bffd16c1a
William Falcon <waf2107@columbia.edu>
2020-09-22 21:38:08 -0400

    fix examples

--- pl_examples/basic_examples/autoencoder.py
+++ pl_examples/basic_examples/autoencoder.py
@@ -29,14 +29,14 @@ class LitAutoEncoder(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(28*28, 64),
+            nn.Linear(28 * 28, 64),
             nn.ReLU(),
             nn.Linear(64, 3)
         )
         self.decoder = nn.Sequential(
             nn.Linear(3, 64),
             nn.ReLU(),
-            nn.Linear(64, 28*28)
+            nn.Linear(64, 28 * 28)
         )
 
     def forward(self, x):
