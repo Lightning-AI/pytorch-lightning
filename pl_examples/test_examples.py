8cd397e20b0a19792e947652f0717177b8a8eef0
William Falcon <waf2107@columbia.edu>
2020-09-22 20:51:54 -0400

    fix dp

--- pl_examples/test_examples.py
+++ pl_examples/test_examples.py
@@ -10,7 +10,7 @@ from PIL import Image
 @pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
 def test_cpu_template(cli_args):
     """Test running CLI for an example with default params."""
-    from pl_examples.basic_examples.cpu_template import run_cli
+    from pl_examples.basic_examples.mnist import run_cli
 
     cli_args = cli_args.split(' ') if cli_args else []
     with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
