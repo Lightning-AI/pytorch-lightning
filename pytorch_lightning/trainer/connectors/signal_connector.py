4c64c3eb76e46e1c616b40c76577722cd2dbba51
Jirka Borovec <Borda@users.noreply.github.com>
2020-09-25 14:33:48 +0200

    Apply suggestions from code review
    Co-authored-by: Justus Schock <12886177+justusschock@users.noreply.github.com>

--- pytorch_lightning/trainer/connectors/signal_connector.py
+++ pytorch_lightning/trainer/connectors/signal_connector.py
@@ -1,4 +1,5 @@
 import signal
+from typing import Callable, Any
 
 from pytorch_lightning.trainer.states import TrainerState
 
@@ -25,12 +26,12 @@ class SignalConnector:
         for signum, handler in self.original_handlers.items():
             signal.signal(signum, handler)
 
-    def register_signal(self, signum, handler):
+    def register_signal(self, signum: int, handler: Callable):
         """ Registers a signal handler and saves a reference to the original handler. """
         self.original_handlers.update({signum: signal.getsignal(signum)})
         signal.signal(signum, handler)
 
-    def default_teardown(self, signum, frame):  # pragma: no-cover
+    def default_teardown(self, signum: int, frame: Any):  # pragma: no-cover
         """ This default teardown raises KeyboardInterrupt and lets Trainer handle the graceful shutdown. """
         # self.trainer.interrupted = True
         # self.trainer._state = TrainerState.INTERRUPTED
