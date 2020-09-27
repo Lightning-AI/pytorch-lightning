397cbe946bc1aaf7ce26a1db041fd4081251c5b9
William Falcon <waf2107@columbia.edu>
2020-09-26 21:22:23 -0400

    enable None model checkpoint default

--- tests/trainer/test_eval_loop_dict_return.py
+++ tests/trainer/test_eval_loop_dict_return.py
@@ -211,7 +211,7 @@ def test_val_step_step_end(tmpdir):
         assert k in eval_results[1]
 
     # ensure all the keys ended up as candidates for callbacks
-    assert len(trainer.logger_connector.callback_metrics) in [9, 10]
+    assert len(trainer.logger_connector.callback_metrics) in [10, 11]
 
     # make sure correct steps were called
     assert model.validation_step_called
