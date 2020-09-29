570a1f770fafe178a0291772eff8e247759cd10c
William Falcon <waf2107@columbia.edu>
2020-09-28 20:56:34 -0400

    ref: test val epoch end

--- tests/trainer/test_eval_loop_logging_1_0.py
+++ tests/trainer/test_eval_loop_logging_1_0.py
@@ -61,3 +61,63 @@ def test__validation_step__log(tmpdir):
     expected_cb_metrics = set(expected_cb_metrics)
     callback_metrics = set(trainer.callback_metrics.keys())
     assert expected_cb_metrics == callback_metrics
+
+
+def test__validation_step__epoch_end__log(tmpdir):
+    """
+    Tests that validation_step can log
+    """
+    os.environ['PL_DEV_DEBUG'] = '1'
+
+    class TestModel(DeterministicModel):
+        def training_step(self, batch, batch_idx):
+            acc = self.step(batch, batch_idx)
+            acc = acc + batch_idx
+            self.log('train_step_acc', acc, on_step=True, on_epoch=True)
+            self.training_step_called = True
+            return acc
+
+        def validation_step(self, batch, batch_idx):
+            acc = self.step(batch, batch_idx)
+            acc = acc + batch_idx
+            self.log('val_step_acc', acc, on_step=True, on_epoch=True)
+            self.validation_step_called = True
+
+        def validation_epoch_end(self, outputs):
+            self.log('val_epoch_acc', torch.tensor(2, device=self.device), on_step=True, on_epoch=True)
+            self.validation_epoch_end_called = True
+
+        def backward(self, trainer, loss, optimizer, optimizer_idx):
+            loss.backward()
+
+    model = TestModel()
+    model.validation_step_end = None
+
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        limit_train_batches=2,
+        limit_val_batches=2,
+        max_epochs=2,
+        row_log_interval=1,
+        weights_summary=None,
+    )
+    trainer.fit(model)
+
+    # make sure all the metrics are available for callbacks
+    expected_logged_metrics = {
+        'epoch',
+        'train_step_acc', 'step_train_step_acc', 'epoch_train_step_acc',
+        'val_step_acc/epoch_0', 'val_step_acc/epoch_1',
+        'step_val_step_acc/epoch_0', 'step_val_step_acc/epoch_1',
+        'val_epoch_acc'
+    }
+    logged_metrics = set(trainer.logged_metrics.keys())
+    assert expected_logged_metrics == logged_metrics
+
+    # we don't want to enable val metrics during steps because it is not something that users should do
+    expected_cb_metrics = [
+        'train_step_acc', 'step_train_step_acc', 'epoch_train_step_acc',
+    ]
+    expected_cb_metrics = set(expected_cb_metrics)
+    callback_metrics = set(trainer.callback_metrics.keys())
+    assert expected_cb_metrics == callback_metrics
