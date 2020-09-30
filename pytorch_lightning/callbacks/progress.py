9648bfd7c137580278c2c0239a9f4bc0c6605691
rohitgr7 <rohitgr1998@gmail.com>
2020-10-01 01:21:51 +0530

    Fix val_progress_bar total with num_sanity_val_steps

--- pytorch_lightning/callbacks/progress.py
+++ pytorch_lightning/callbacks/progress.py
@@ -220,6 +220,7 @@ class ProgressBar(ProgressBarBase):
         self._refresh_rate = refresh_rate
         self._process_position = process_position
         self._enabled = True
+        self._sanity_check_completed = True
         self.main_progress_bar = None
         self.val_progress_bar = None
         self.test_progress_bar = None
@@ -305,6 +306,7 @@ class ProgressBar(ProgressBarBase):
         return bar
 
     def on_sanity_check_start(self, trainer, pl_module):
+        self._sanity_check_completed = False
         super().on_sanity_check_start(trainer, pl_module)
         self.val_progress_bar = self.init_sanity_tqdm()
         self.val_progress_bar.total = convert_inf(sum(trainer.num_sanity_val_batches))
@@ -314,7 +316,7 @@ class ProgressBar(ProgressBarBase):
         super().on_sanity_check_end(trainer, pl_module)
         self.main_progress_bar.close()
         self.val_progress_bar.close()
-        self.val_progress_bar = None
+        self._sanity_check_completed = True
 
     def on_train_start(self, trainer, pl_module):
         super().on_train_start(trainer, pl_module)
@@ -341,7 +343,7 @@ class ProgressBar(ProgressBarBase):
 
     def on_validation_start(self, trainer, pl_module):
         super().on_validation_start(trainer, pl_module)
-        if self.val_progress_bar is None:
+        if self._sanity_check_completed is True:
             self.val_progress_bar = self.init_validation_tqdm()
             self.val_progress_bar.total = convert_inf(self.total_val_batches)
 
