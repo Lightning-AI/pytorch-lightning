31fa2a9b3321f9cb225db5dc156566b1c0be368b
Indrayana Rustandi <i_rustandi@yahoo.com>
2020-09-30 09:25:33 -0400

    mark mnist_dali test as Linux and GPU only

--- pl_examples/test_examples.py
+++ pl_examples/test_examples.py
@@ -86,10 +86,19 @@ ddp_args = """
 @pytest.mark.parametrize('cli_args', [cpu_args])
 def test_examples_cpu(cli_args):
     from pl_examples.basic_examples.mnist import cli_main as mnist_cli
-    from pl_examples.basic_examples.mnist_dali import cli_main as mnist_dali_cli
     from pl_examples.basic_examples.image_classifier import cli_main as ic_cli
     from pl_examples.basic_examples.autoencoder import cli_main as ae_cli
 
-    for cli_cmd in [mnist_cli, mnist_dali_cli, ic_cli, ae_cli]:
+    for cli_cmd in [mnist_cli, ic_cli, ae_cli]:
         with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
             cli_cmd()
+
+
+@pytest.mark.skipif(torch.cuda.device_count() < 1, reason="test requires GPU machine")
+@pytest.mark.skipif(platform.system() != 'Linux', reason='Only applies to Linux platform.')
+@pytest.mark.parametrize('cli_args', [cpu_args])
+def test_examples_mnist_dali(cli_args):
+    from pl_examples.basic_examples.mnist_dali import cli_main
+
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
+        cli_main()
