581e929d3a33e913bd23404e7483d5a92f8b68fe
William Falcon <waf2107@columbia.edu>
2020-09-30 19:37:03 -0400

    ref: decoupled ddp spawn

--- tests/backends/ddp_spawn.py
+++ tests/backends/ddp_spawn.py
@@ -8,7 +8,6 @@ from pytorch_lightning.core import memory
 from pytorch_lightning.trainer import Trainer
 
 
-
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 def test_multi_gpu_early_stop_ddp_spawn(tmpdir):
     """Make sure DDP works. with early stopping"""
