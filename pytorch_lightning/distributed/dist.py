be59efe5d119be991d0b2ce0a81a5f2e82996577
William Falcon <waf2107@columbia.edu>
2020-09-30 23:02:31 -0400

    hacks

--- pytorch_lightning/distributed/dist.py
+++ pytorch_lightning/distributed/dist.py
@@ -14,39 +14,28 @@ class LightningDistributed:
 
     def broadcast(self, obj: Any):
         if self.rank == 0:
-            # Emit data
-            buffer = io.BytesIO()
-            torch.save(obj, buffer)
-            data = bytearray(buffer.getbuffer())
-            length_tensor = torch.tensor([len(data)]).long().to(self.device)
-            length_tensor = torch_distrib.broadcast(length_tensor, src=0)
-            data_tensor = torch.ByteTensor(data).to(self.device)
-            data_tensor = torch_distrib.broadcast(data_tensor, src=0)
+            print(f'sending from: {self.rank}')
+            self._emit(obj)
         else:
-            # Fetch from the source
-            length_tensor = torch.tensor([0]).long().to(self.device)
-            torch_distrib.broadcast(length_tensor, src=0)
-            data_tensor = torch.empty([length_tensor.item()], dtype=torch.uint8).to(self.device)
-            torch_distrib.broadcast(data_tensor, src=0)
-            buffer = io.BytesIO(data_tensor.cpu().numpy())
-            obj = torch.load(buffer)
-
-            print(obj)
+            obj = self._receive()
+            print(f'receiving at: {self.rank}')
+            print('obj')
         return obj
 
-    def _encode(self, obj):
-        padding = torch.zeros(100, device=self.device).long()
-        result = [str(ord(c)) for c in obj]
-        chunks = [torch.tensor(int(x)) for x in result]
-        chunks = torch.stack(chunks).long()
-        padding[:len(chunks)] = chunks
-        padding = padding.to(self.device)
-        return padding
+    def _emit(self, obj):
+        buffer = io.BytesIO()
+        torch.save(obj, buffer)
+        data = bytearray(buffer.getbuffer())
+        length_tensor = torch.tensor([len(data)]).long().to(self.device)
+        length_tensor = torch_distrib.broadcast(length_tensor, src=0)
+        data_tensor = torch.ByteTensor(data).to(self.device)
+        data_tensor = torch_distrib.broadcast(data_tensor, src=0)
 
-    def _decode(self, tensor):
-        tensor = tensor.long()
-        print(tensor)
-        chunks = [chr(x.cpu().numpy()) for x in tensor]
-        text = ''.join(chunks)
-        print(text, '|')
-        return text
+    def _receive(self):
+        length_tensor = torch.tensor([0]).long().to(self.device)
+        torch_distrib.broadcast(length_tensor, src=0)
+        data_tensor = torch.empty([length_tensor.item()], dtype=torch.uint8).to(self.device)
+        torch_distrib.broadcast(data_tensor, src=0)
+        buffer = io.BytesIO(data_tensor.cpu().numpy())
+        obj = torch.load(buffer)
+        return obj
