128f9ee9316af47a84d8e3fac25b381d85916ed3
Jeff Yang <ydcjeff@outlook.com>
2020-10-01 21:07:00 +0630

    Fix for PyTorch 1.7 CI (#3768)
    * changed to __jit_unsed_properties__

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -64,13 +64,12 @@ class LightningModule(
 ):
     # Below is for property support of JIT in PyTorch 1.7
     # since none of them is important when using JIT, we are going to ignore them.
-    # https://github.com/pytorch/pytorch/commit/e7d782e724c76bb0572023d52ee7438a40a7a262#diff-ff4f8670281cd1eb4e09329cc1dcb43b
-    __ignored_properties__ = [
+    __jit_unused_properties__ = [
         "datamodule",
         "example_input_array",
         "hparams",
         "on_gpu",
-    ] + DeviceDtypeModuleMixin.__ignored_properties__
+    ] + DeviceDtypeModuleMixin.__jit_unused_properties__
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
