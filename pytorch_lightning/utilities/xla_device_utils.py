e9b8c633f3e906f00b2c3504ad4b466929a652fc
Lezwon Castelino <lezwon@gmail.com>
2020-10-01 06:13:54 +0530

    added type hints

--- pytorch_lightning/utilities/xla_device_utils.py
+++ pytorch_lightning/utilities/xla_device_utils.py
@@ -1,5 +1,6 @@
 import functools
 import importlib
+import torch
 from multiprocessing import Process, Queue
 
 TORCHXLA_AVAILABLE = importlib.util.find_spec("torch_xla") is not None
@@ -36,22 +37,36 @@ class XLADeviceUtils:
     TPU_AVAILABLE = None
 
     @staticmethod
-    def _fetch_xla_device_type(device):
-        """ Returns XLA device type """
+    def _fetch_xla_device_type(device: torch.device) -> str:
+        """
+        Returns XLA device type
+        Args:
+            device: (:class:`~torch.device`): Accepts a torch.device type with a XLA device format i.e xla:0
+        Return:
+            Returns a str of the device hardware type. i.e TPU
+        """
         if xm is not None:
             return xm.xla_device_hw(device)
 
     @staticmethod
-    def _is_device_tpu():
-        """ Check if device is TPU """
+    def _is_device_tpu() -> bool:
+        """
+        Check if device is TPU
+        Return:
+            A boolean value indicating if the xla device is a TPU device or not
+        """
         if xm is not None:
             device = xm.xla_device()
             device_type = XLADeviceUtils._fetch_xla_device_type(device)
             return device_type == "TPU"
 
     @staticmethod
-    def tpu_device_exists():
-        """ Public method to check if TPU is available """
+    def tpu_device_exists() -> bool:
+        """
+        Public method to check if TPU is available
+        Return:
+            A boolean value indicating if a TPU device exists on the system
+        """
         if XLADeviceUtils.TPU_AVAILABLE is None and TORCHXLA_AVAILABLE:
             XLADeviceUtils.TPU_AVAILABLE = pl_multi_process(XLADeviceUtils._is_device_tpu)()
         return XLADeviceUtils.TPU_AVAILABLE
