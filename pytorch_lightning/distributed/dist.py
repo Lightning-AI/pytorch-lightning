5badf41e8661482b1d158dd460d272faf85da808
William Falcon <waf2107@columbia.edu>
2020-09-30 22:55:39 -0400

    hacks

--- pytorch_lightning/distributed/dist.py
+++ pytorch_lightning/distributed/dist.py
@@ -13,23 +13,20 @@ class LightningDistributed:
         self.device = device
 
     def broadcast(self, obj: Any):
+        torch_distrib.barrier()
         if self.rank == 0:
             # Emit data
             buffer = io.BytesIO()
             torch.save(obj, buffer)
             data = bytearray(buffer.getbuffer())
             length_tensor = torch.tensor([len(data)]).long().to(self.device)
-            print(length_tensor.size())
             length_tensor = torch_distrib.broadcast(length_tensor, src=0)
             data_tensor = torch.ByteTensor(data).to(self.device)
             data_tensor = torch_distrib.broadcast(data_tensor, src=0)
         else:
             # Fetch from the source
             length_tensor = torch.tensor([0]).long().to(self.device)
-            print(length_tensor.size())
-            torch_distrib.barrier()
             length_tensor = torch_distrib.broadcast(length_tensor, src=0)
-            print(length_tensor)
             data_tensor = torch.empty([length_tensor.item()], dtype=torch.uint8).to(self.device)
             data_tensor = torch_distrib.broadcast(data_tensor, src=0)
             buffer = io.BytesIO(data_tensor.numpy())
