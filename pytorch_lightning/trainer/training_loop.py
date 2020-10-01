ac2b0f0f066967b5896a73591ce7aa25dfb75306
William Falcon <waf2107@columbia.edu>
2020-10-01 09:25:33 -0400

    ref: continue #3733 (#3767)
    * ref: #3733 part 2
    
    * ref: #3733 part 2

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -181,13 +181,8 @@ class TrainLoop:
         if self.trainer.global_rank == 0:
             self.trainer.profiler.describe()
 
-        if self.trainer.global_rank == 0:
-            for proc in self.trainer.interactive_ddp_procs:
-                subprocess.Popen.kill(proc)
-
-        # clean up dist group
-        if self.trainer.use_ddp or self.trainer.use_ddp2:
-            torch_distrib.destroy_process_group()
+        # give accelerators a chance to finish
+        self.trainer.accelerator_backend.on_train_end()
 
         # clear mem
         if self.trainer.on_gpu:
