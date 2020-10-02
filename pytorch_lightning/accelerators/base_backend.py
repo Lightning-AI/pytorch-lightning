72068b67e54a6ca4d306b9a134c8396a62215890
Jirka Borovec <jirka@pytorchlightning.ai>
2020-10-02 11:50:22 +0200

    todo

--- pytorch_lightning/accelerators/base_backend.py
+++ pytorch_lightning/accelerators/base_backend.py
@@ -187,11 +187,13 @@ class Accelerator(object):
 
 
 # TODO: allow user to compare with string even internaly we shall use these Enum to prevent typos...
-# class BackendType(Enum):
-#     DP = 'dp'
-#     DDP = 'ddp'
-#     DDP2 = 'ddp2'
-#     DDP_SPAWN = 'ddp_spawn'
-#     DDP_CPU = 'ddp_cpu'
-#     TPU = 'tpu'
-#     HOROVOD = 'horovod'
+class BackendType(Enum):
+    DP = 'dp'
+    DDP = 'ddp'
+    DDP2 = 'ddp2'
+    DDP_SPAWN = 'ddp_spawn'
+    # decuple distrib and device
+    DDP_CPU = 'ddp_cpu'
+    HOROVOD = 'horovod'
+    # this is rather device
+    TPU = 'tpu'
