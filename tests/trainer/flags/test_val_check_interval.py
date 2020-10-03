fde3fe438fdcac0900a6c3bdcff412ac4a2ea281
Jirka Borovec <jirka@pytorchlightning.ai>
2020-10-03 22:02:16 +0200

    add test

--- tests/trainer/flags/test_val_check_interval.py
+++ tests/trainer/flags/test_val_check_interval.py
@@ -4,7 +4,8 @@ from pytorch_lightning.trainer import Trainer
 
 
 @pytest.mark.parametrize('max_epochs', [1, 2, 3])
-def test_val_check_interval_1(tmpdir, max_epochs):
+@pytest.mark.parametrize('interval', [1.0, 0.25, 0.33])
+def test_val_check_interval_(tmpdir, max_epochs, interval):
 
     class TestModel(SimpleModule):
         def __init__(self):
@@ -21,64 +22,11 @@ def test_val_check_interval_1(tmpdir, max_epochs):
 
     model = TestModel()
     trainer = Trainer(
+        default_root_dir=tmpdir,
         max_epochs=max_epochs,
-        val_check_interval=1.0,
+        val_check_interval=interval,
         logger=False,
     )
     trainer.fit(model)
 
-    assert model.val_epoch_calls == max_epochs
-
-
-@pytest.mark.parametrize('max_epochs', [1, 2, 3])
-def test_val_check_interval_quarter(tmpdir, max_epochs):
-
-    class TestModel(SimpleModule):
-        def __init__(self):
-            super().__init__()
-            self.train_epoch_calls = 0
-            self.val_epoch_calls = 0
-
-        def on_train_epoch_start(self) -> None:
-            self.train_epoch_calls += 1
-
-        def on_validation_epoch_start(self) -> None:
-            if not self.trainer.running_sanity_check:
-                self.val_epoch_calls += 1
-
-    model = TestModel()
-    trainer = Trainer(
-        max_epochs=max_epochs,
-        val_check_interval=0.25,
-        logger=False,
-    )
-    trainer.fit(model)
-
-    assert model.val_epoch_calls == max_epochs * 4
-
-
-@pytest.mark.parametrize('max_epochs', [1, 2, 3])
-def test_val_check_interval_third(tmpdir, max_epochs):
-
-    class TestModel(SimpleModule):
-        def __init__(self):
-            super().__init__()
-            self.train_epoch_calls = 0
-            self.val_epoch_calls = 0
-
-        def on_train_epoch_start(self) -> None:
-            self.train_epoch_calls += 1
-
-        def on_validation_epoch_start(self) -> None:
-            if not self.trainer.running_sanity_check:
-                self.val_epoch_calls += 1
-
-    model = TestModel()
-    trainer = Trainer(
-        max_epochs=max_epochs,
-        val_check_interval=0.33,
-        logger=False,
-    )
-    trainer.fit(model)
-
-    assert model.val_epoch_calls == max_epochs * 3
+    assert model.val_epoch_calls == max_epochs * round(1 / interval)
