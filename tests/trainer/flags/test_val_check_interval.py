d4b6a9f9ce926faaa273a9c3de48936218d459cd
William Falcon <waf2107@columbia.edu>
2020-10-03 13:57:27 -0400

    added test_val_check_interval tests

--- tests/trainer/flags/test_val_check_interval.py
+++ tests/trainer/flags/test_val_check_interval.py
@@ -1,7 +1,6 @@
 import pytest
 from tests.base import SimpleModule
 from pytorch_lightning.trainer import Trainer
-from pytorch_lightning import callbacks
 
 
 @pytest.mark.parametrize('max_epochs', [1, 2, 3])
@@ -21,12 +20,10 @@ def test_val_check_interval(tmpdir, max_epochs):
                 self.val_epoch_calls += 1
 
     model = TestModel()
-    checkpoint_callback = callbacks.ModelCheckpoint(save_top_k=1, monitor="avg_val_loss")
     trainer = Trainer(
         max_epochs=max_epochs,
         val_check_interval=1.0,
         logger=False,
-        checkpoint_callback=checkpoint_callback,
     )
     trainer.fit(model)
 
@@ -50,12 +47,10 @@ def test_val_check_interval(tmpdir, max_epochs):
                 self.val_epoch_calls += 1
 
     model = TestModel()
-    checkpoint_callback = callbacks.ModelCheckpoint(save_top_k=1, monitor="avg_val_loss")
     trainer = Trainer(
         max_epochs=max_epochs,
         val_check_interval=0.25,
         logger=False,
-        checkpoint_callback=checkpoint_callback,
     )
     trainer.fit(model)
 
@@ -79,12 +74,10 @@ def test_val_check_interval(tmpdir, max_epochs):
                 self.val_epoch_calls += 1
 
     model = TestModel()
-    checkpoint_callback = callbacks.ModelCheckpoint(save_top_k=1, monitor="avg_val_loss")
     trainer = Trainer(
         max_epochs=max_epochs,
         val_check_interval=0.33,
         logger=False,
-        checkpoint_callback=checkpoint_callback,
     )
     trainer.fit(model)
 
