8ebe46d7d86bd41f6dd7a290ade192486104cead
William Falcon <waf2107@columbia.edu>
2020-10-03 19:56:15 -0400

    verified epoch logging

--- tests/trainer/logging/test_train_loop_logging_1_0.py
+++ tests/trainer/logging/test_train_loop_logging_1_0.py
@@ -197,7 +197,7 @@ def test__training_step__step_end__epoch_end__log(tmpdir, batches, log_interval,
         def training_epoch_end(self, outputs):
             self.training_epoch_end_called = True
             self.log('c', outputs[0]['loss'], on_epoch=True, prog_bar=True, logger=True)
-            self.log('d/e/f', outputs[0]['loss'])
+            self.log('d/e/f', 2)
 
     model = TestModel()
     model.val_dataloader = None
