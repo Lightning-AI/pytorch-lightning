093535d433b025fad6fdd10c593724ddc56fedfd
William Falcon <waf2107@columbia.edu>
2020-10-04 07:31:19 -0400

    ref: adding compute environments (1/n) (#3837)
    * ref: adding compute environments (1/n)
    
    * ref: adding compute environments (1/n)
    
    * ref: adding compute environments (1/n)

---
+++ pytorch_lightning/cluster_environments/cluster_environment.py
@@ -0,0 +1,13 @@
+class ClusterEnvironment:
+
+    def __init__(self, world_size):
+        self._world_size = world_size
+
+    def master_address(self):
+        pass
+
+    def master_port(self):
+        pass
+
+    def world_size(self):
+        return self._world_size
