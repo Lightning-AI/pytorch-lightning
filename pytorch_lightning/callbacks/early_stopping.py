7a8fe83d7bc93b1a77f8c4c84471a5c32db22ad2
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-10-04 19:51:26 +0200

    fix test

--- pytorch_lightning/callbacks/early_stopping.py
+++ pytorch_lightning/callbacks/early_stopping.py
@@ -181,7 +181,7 @@ class EarlyStopping(Callback):
         current = logs.get(self.monitor)
 
         # when in dev debugging
-        trainer.dev_debugger.track_early_stopping_history(current)
+        trainer.dev_debugger.track_early_stopping_history(self, current)
 
         if not isinstance(current, torch.Tensor):
             current = torch.tensor(current, device=pl_module.device)
