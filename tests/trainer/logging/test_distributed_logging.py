03d4431110ea7cc33fd491e40a8efa613a8d2094
William Falcon <waf2107@columbia.edu>
2020-10-06 10:07:37 -0400

    fix local model

--- tests/trainer/logging/test_distributed_logging.py
+++ tests/trainer/logging/test_distributed_logging.py
@@ -3,7 +3,7 @@ import torch
 from tests.base import BoringModel
 import platform
 from distutils.version import LooseVersion
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, Callback
 from unittest import mock
 
 
@@ -16,19 +16,19 @@ def test_global_zero_only_logging_ddp_cpu(tmpdir):
     """
     Makes sure logging only happens from root zero
     """
-    class TestModel(BoringModel):
+    class CB(Callback):
 
-        def on_pretrain_routine_end(self) -> None:
+        def on_pretrain_routine_end(self, trainer, pl_module):
             with mock.patch('pytorch_lightning.loggers.base.LightningLoggerBase.agg_and_log_metrics') as m:
                 self.trainer.logger_connector.log_metrics({'a': 2}, {})
                 logged_times = m.call_count
-                expected = 1 if self.global_rank == 0 else 0
+                expected = 1 if trainer.global_rank == 0 else 0
                 assert logged_times == expected, 'actual logger called from non-global zero'
 
-    model = TestModel()
+    model = BoringModel()
     model.training_epoch_end = None
     trainer = Trainer(
-        distributed_backend='ddp_cpu',
+        callbacks=[CB()],
         num_processes=2,
         default_root_dir=tmpdir,
         limit_train_batches=1,
@@ -44,18 +44,19 @@ def test_global_zero_only_logging_ddp_spawn(tmpdir):
     """
     Makes sure logging only happens from root zero
     """
-    class TestModel(BoringModel):
+    class CB(Callback):
 
-        def on_pretrain_routine_end(self) -> None:
+        def on_pretrain_routine_end(self, trainer, pl_module):
             with mock.patch('pytorch_lightning.loggers.base.LightningLoggerBase.agg_and_log_metrics') as m:
                 self.trainer.logger_connector.log_metrics({'a': 2}, {})
                 logged_times = m.call_count
-                expected = 1 if self.global_rank == 0 else 0
+                expected = 1 if trainer.global_rank == 0 else 0
                 assert logged_times == expected, 'actual logger called from non-global zero'
 
-    model = TestModel()
+    model = BoringModel()
     model.training_epoch_end = None
     trainer = Trainer(
+        callbacks=[CB()],
         distributed_backend='ddp_spawn',
         gpus=2,
         default_root_dir=tmpdir,
