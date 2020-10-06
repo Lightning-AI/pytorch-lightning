41ddda8cae74d38d7ce3bf0f6390829e435871c8
ananyahjha93 <ananya@pytorchlightning.ai>
2020-10-06 15:45:56 -0400

    refactor dist utils, pep8

--- pytorch_lightning/metrics/metric.py
+++ pytorch_lightning/metrics/metric.py
@@ -10,8 +10,8 @@ import torch
 from torch import nn
 
 from pytorch_lightning.utilities.apply_func import apply_to_collection
-from pytorch_lightning.metrics.utils import _flatten, gather_all_tensors_if_available
-from pytorch_lightning.metrics.utils import dim_zero_cat, dim_zero_mean, dim_zero_sum
+from pytorch_lightning.utilities.distributed import gather_all_tensors_if_available
+from pytorch_lightning.metrics.utils import _flatten, dim_zero_cat, dim_zero_mean, dim_zero_sum
 
 
 class Metric(nn.Module, ABC):
@@ -181,8 +181,8 @@ class Metric(nn.Module, ABC):
 
             if (
                 self._to_sync
-                and torch.distributed.is_available()
-                and torch.distributed.is_initialized()
+                and torch.distributed.is_available()  # noqa: W503
+                and torch.distributed.is_initialized()  # noqa: W503
             ):
                 self._sync_dist()
 
--- pytorch_lightning/utilities/distributed.py
+++ pytorch_lightning/utilities/distributed.py
@@ -73,6 +73,36 @@ def find_free_network_port() -> int:
     return port
 
 
+def gather_all_tensors_if_available(result: Union[torch.Tensor], group: Optional[Any] = None):
+    """
+    Function to gather all tensors from several ddp processes onto a list that
+    is broadcasted to all processes
+
+    Args:
+        result: the value to sync
+        group: the process group to gather results from. Defaults to all processes (world)
+
+    Return:
+        gathered_result: list with size equal to the process group where
+            gathered_result[i] corresponds to result tensor from process i
+
+    """
+    if torch.distributed.is_available() and torch.distributed.is_initialized():
+        if group is None:
+            group = torch.distributed.group.WORLD
+
+        world_size = torch.distributed.get_world_size(group)
+
+        gathered_result = [torch.zeros_like(result) for _ in range(world_size)]
+
+        # sync and broadcast all
+        torch.distributed.barrier(group=group)
+        torch.distributed.all_gather(gathered_result, result, group)
+
+        result = gathered_result
+    return result
+
+
 def sync_ddp_if_available(
     result: Union[torch.Tensor], group: Optional[Any] = None, reduce_op: Optional[Union[ReduceOp, str]] = None
 ) -> torch.Tensor:
