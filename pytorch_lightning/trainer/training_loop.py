ed2cd6ed68dfd631623514ebaeed8b9cd890d29c
William Falcon <waf2107@columbia.edu>
2020-10-10 16:49:18 -0400

    ref: decouple apex second attemp part 8/n

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -758,6 +758,7 @@ class TrainLoop:
         return result
 
     def backward(self, result, optimizer, *args, **kwargs):
+        import pdb; pdb.set_trace()
         self.trainer.dev_debugger.track_event('backward_call')
 
         # backward can be called manually in the training loop.
