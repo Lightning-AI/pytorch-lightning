26592430ac04ae83b2711737f4a947f095675876
Nicki Skafte <skaftenicki@gmail.com>
2020-10-15 10:37:51 +0200

    add desc

--- pytorch_lightning/metrics/functional/explained_variance.py
+++ pytorch_lightning/metrics/functional/explained_variance.py
@@ -48,5 +48,31 @@ def _explained_variance_compute(preds: torch.Tensor,
 def explained_variance(preds: torch.Tensor,
                        target: torch.Tensor,
                        multioutput: str = 'uniform_average'):
+    """
+    Computes explained variance.
+
+    Args:
+        pred: estimated labels
+        target: ground truth labels
+        multioutput: Defines aggregation in the case of multiple output scores. Can be one
+            of the following strings (default is `'uniform_average'`.):
+
+            * `'raw_values'` returns full set of scores
+            * `'uniform_average'` scores are uniformly averaged
+            * `'variance_weighted'` scores are weighted by their individual variances
+
+    Example:
+
+        >>> from pytorch_lightning.metrics import ExplainedVariance
+        >>> target = torch.tensor([3, -0.5, 2, 7])
+        >>> preds = torch.tensor([2.5, 0.0, 2, 8])
+        >>> explained_variance(preds, target)
+        tensor(0.9572)
+
+        >>> target = torch.tensor([[0.5, 1], [-1, 1], [7, -6]])
+        >>> preds = torch.tensor([[0, 2], [-1, 2], [8, -5]])
+        >>> explained_variance(preds, target, multioutput='raw_values')
+        tensor([0.9677, 1.0000])
+    """
     preds, target = _explained_variance_update(preds, target)
     return _explained_variance_compute(preds, target, multioutput)
