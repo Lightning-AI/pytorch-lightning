62137f47fb96df571e1b5c1e5c94312e39fab0e5
Dusan Drevicky <drevicky@gmail.com>
2020-10-19 17:45:30 +0200

    Add multiclass_auroc tests

--- tests/metrics/functional/test_classification.py
+++ tests/metrics/functional/test_classification.py
@@ -32,6 +32,7 @@ from pytorch_lightning.metrics.functional.classification import (
     dice_score,
     average_precision,
     auroc,
+    multiclass_auroc,
     precision_recall_curve,
     roc,
     auc,
@@ -344,6 +345,52 @@ def test_roc_curve(pred, target, expected_tpr, expected_fpr):
 def test_auroc(pred, target, expected):
     score = auroc(torch.tensor(pred), torch.tensor(target)).item()
     assert score == expected
+
+
+def test_multiclass_auroc():
+    with pytest.raises(ValueError,
+                       match="Multiclass AUROC metric expects the target scores to be "
+                             "probabilities, i.e. they should sum up to 1.0 over classes"):
+        _ = multiclass_auroc(pred=torch.tensor([[0.9, 0.9],
+                                                [1.0, 0]]),
+                             target=torch.tensor([0, 1]))
+
+    with pytest.raises(ValueError,
+                       match=r"Number of classes found in in 'target' (.*) "
+                             r"does not equal the number of columns in 'pred' (.*). "
+                             r"Multiclass AUROC is not defined when all of the classes do not "
+                             r"occur in the target labels."):
+        _ = multiclass_auroc(pred=torch.rand((4, 3)).softmax(dim=1),
+                             target=torch.tensor([1, 0, 1, 0]))
+
+    with pytest.raises(ValueError,
+                       match=r"Number of classes deduced from 'pred' (.*) does not equal "
+                             r"the number of classes passed in 'num_classes' (.*)."):
+        _ = multiclass_auroc(pred=torch.rand((5, 4)).softmax(dim=1),
+                             target=torch.tensor([0, 1, 2, 2, 3]),
+                             num_classes=6)
+
+
+def test_multiclass_auroc_against_sklearn():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    n_samples = 300
+    for n_cls in [2, 5, 10, 50]:
+        pred = torch.rand(n_samples, n_cls, device=device).softmax(dim=1)
+        target = torch.randint(n_cls, (n_samples,), device=device)
+        # Make sure target includes all class labels so that multiclass AUROC is defined
+        target[10:10 + n_cls] = torch.arange(n_cls)
+
+        pl_score = multiclass_auroc(pred, target)
+        # For the binary case, sklearn expects an (n_samples,) array of probabilities of
+        # the positive class
+        pred = pred[:, 1] if n_cls == 2 else pred
+        sk_score = sk_roc_auc_score(target.cpu().detach().numpy(),
+                                    pred.cpu().detach().numpy(),
+                                    multi_class="ovr")
+
+        sk_score = torch.tensor(sk_score, dtype=torch.float, device=device)
+        assert torch.allclose(sk_score, pl_score)
 
 
 @pytest.mark.parametrize(['x', 'y', 'expected'], [
