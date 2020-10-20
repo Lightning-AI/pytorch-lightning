613cb49f2c5643e23ba7450fbaff6567848d6da1
Abelarm <luigi3000@gmail.com>
2020-10-20 12:40:11 +0200

    Added the function for downloading the badge locally, replacing the url

--- setup.py
+++ setup.py
@@ -48,6 +48,89 @@ def load_requirements(path_dir=PATH_ROOT, file_name='requirements.txt', comment_
     return reqs
 
 
+def _parse_README_for_badge(text):
+
+    # badge to download
+    valid_badge_names = [
+        'PyPI - Python Version',
+        'PyPI Status',
+        'PyPI Status',
+        'Conda',
+        'DockerHub',
+        'codecov',
+        'ReadTheDocs',
+        'Slack',
+        'Discourse status',
+        'license',
+        'Next Release'
+    ]
+
+    for line in text.split('\n'):
+        badge_name = re.search(r'^\[!\[(.*?)]', line)
+
+        # check for the badge name
+        if badge_name is not None:
+            badge_name = badge_name.group(1)
+
+            # check if valid name
+            if badge_name in valid_badge_names:
+
+                search_string = fr'\[\!\[{badge_name}]\((.*?)\)]'
+                badge_url = re.search(search_string, line)
+                # check for badge url
+                if badge_url is not None:
+                    badge_url = badge_url.group(1)
+
+                # download badge
+                saved_badge_name = _download_badges(badge_url, badge_name)
+
+                # replace url with local file path
+                replace_string =  f'[![{badge_name}]({saved_badge_name})]'
+                text = re.sub(search_string, replace_string, text)
+
+    return text
+
+
+def _download_badges(url_badge, badge_name):
+
+    base_path = f'docs/source/_images/badges'
+    os.makedirs(base_path, exist_ok=True)
+
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/svg,*/*;q=0.8', }
+
+    # function for saving the badge either in `.png` or `.svg`
+    def _save_file(url, save, extension):
+
+        # because there are two badge with name `PyPI Status` the second one is download
+        if 'https://pepy.tech/badge/pytorch-lightning' in url_badge:
+            save += '_downloads'
+
+        try:
+            req = Request(url=url, headers=headers)
+            resp = urlopen(req)
+        except URLError as err:
+            warnings.warn("Error while downloading the badge", UserWarning)
+        else:
+            save += extension
+            with open(save, 'wb') as download_file:
+                download_file.write(resp.read())
+
+    save_path = badge_name.replace(' - ', ' ')
+    save_path = f"{base_path}/{save_path.replace(' ', '_')}_badge"
+
+    try:
+        # always try to download the png versions (some url have an already png version available)
+        _save_file(url_badge, save_path, extension='.png')
+        return save_path + '.png'
+    except HTTPError as err:
+        if err.code == 404:
+            # save the `.svg`
+            url_badge = url_badge.replace('.png', '.svg')
+            _save_file(url_badge, save_path, extension='.svg')
+            return save_path +'.svg'
+
+
 def load_long_description():
     # https://github.com/PyTorchLightning/pytorch-lightning/raw/master/docs/source/_images/lightning_module/pt_to_pl.png
     url = os.path.join(pytorch_lightning.__homepage__, 'raw', pytorch_lightning.__version__, 'docs')
@@ -56,6 +139,8 @@ def load_long_description():
     text = text.replace('](docs', f']({url}')
     # SVG images are not readable on PyPI, so replace them  with PNG
     text = text.replace('.svg', '.png')
+    # download badge and replace url with local file
+    text = _parse_README_for_badge(text)
     return text
 
 
@@ -89,7 +174,7 @@ for ex in ('cpu', 'cpu-extra'):
 # the goal of the project is simplicity for researchers, don't want to add too much
 # engineer specific practices
 setup(
-    name='pytorch-lightning',
+    name="pytorch-lightning-nightly",
     version=pytorch_lightning.__version__,
     description=pytorch_lightning.__docs__,
     author=pytorch_lightning.__author__,
