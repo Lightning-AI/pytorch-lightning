4741258d60fa7581c5ac3e1b66c8dc4ed995b5cd
tchaton <thomas@grid.ai>
2020-10-20 16:20:18 +0100

    update for flake8

--- pytorch_lightning/trainer/connectors/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector.py
@@ -22,21 +22,6 @@ from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pprint import pprint
 from typing import Iterable
 from copy import deepcopy
-from collections import ChainMap
-
-# Copyright The PyTorch Lightning team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from collections import defaultdict, ChainMap
 
 
@@ -97,6 +82,10 @@ class LoggerConnector:
         self.progress_bar_metrics = {}
         self.eval_loop_results = []
         self.cache_internal_metrics = {"train": CacheInternalMetrics()}
+
+    def reset_cache_internal_metrics(self, stage):
+        assert stage in ["train"], f'Stage {stage} should be within ["train"]'
+        self.cache_internal_metrics[stage] = CacheInternalMetrics()
 
     def on_trainer_init(self, logger, flush_logs_every_n_steps, log_every_n_steps):
         # logging
