7f2f49188f5bc08e8c34c5e8a139c72ff18b7a12
William Falcon <waf2107@columbia.edu>
2020-10-21 08:54:36 -0400

    Update pytorch_lightning/accelerators/ddp2_accelerator.py
    Co-authored-by: Rohit Gupta <rohitgr1998@gmail.com>

--- pytorch_lightning/accelerators/ddp2_accelerator.py
+++ pytorch_lightning/accelerators/ddp2_accelerator.py
@@ -120,9 +120,9 @@ class DDP2Accelerator(Accelerator):
         Entry point for ddp
 
         Args:
-            process_idx:
+            process_idx: current process rank
             mp_queue: multiprocessing queue
-            model:
+            model: pointer to current :class:`LightningModule`
         """
         # show progressbar only on progress_rank 0
         if (self.trainer.node_rank != 0 or process_idx != 0) and self.trainer.progress_bar_callback is not None:
