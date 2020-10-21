104c9f52c5fc59d23918c05a37e52588ec0aaa62
tchaton <thomas@grid.ai>
2020-10-21 17:45:24 +0100

    remove mixin

--- pytorch_lightning/trainer/connectors/callback_connector.py
+++ pytorch_lightning/trainer/connectors/callback_connector.py
@@ -18,13 +18,86 @@ from pytorch_lightning.utilities import rank_zero_warn
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
-class CallbackConnectorLoggingMixin(ABC):
+class CallbackConnector:
 
-    # This Mixin class helps to summarize logging logic for Pytorch LightningModule using self.log functions for `pytorch_lightning.callbacks.CallBack`
+    def __init__(self, trainer):
+        self.trainer = trainer
+
+    def on_trainer_init(
+            self,
+            callbacks,
+            checkpoint_callback,
+            progress_bar_refresh_rate,
+            process_position,
+            default_root_dir,
+            weights_save_path,
+            resume_from_checkpoint
+    ):
+        self.trainer.resume_from_checkpoint = resume_from_checkpoint
+
+        # init folder paths for checkpoint + weights save callbacks
+        self.trainer._default_root_dir = default_root_dir or os.getcwd()
+        self.trainer._weights_save_path = weights_save_path or self.trainer._default_root_dir
+
+        # init callbacks
+        self.trainer.callbacks = callbacks or []
+
+        # configure checkpoint callback
+        # it is important that this is the last callback to run
+        # pass through the required args to figure out defaults
+        checkpoint_callback = self.init_default_checkpoint_callback(checkpoint_callback)
+        if checkpoint_callback:
+            self.trainer.callbacks.append(checkpoint_callback)
+
+        # TODO refactor codebase (tests) to not directly reach into these callbacks
+        self.trainer.checkpoint_callback = checkpoint_callback
+
+        # init progress bar
+        self.trainer._progress_bar_callback = self.configure_progress_bar(
+            progress_bar_refresh_rate, process_position
+        )
+
+    def init_default_checkpoint_callback(self, checkpoint_callback):
+        if checkpoint_callback is True:
+            checkpoint_callback = ModelCheckpoint(filepath=None)
+        elif checkpoint_callback is False:
+            checkpoint_callback = None
+        if checkpoint_callback:
+            checkpoint_callback.save_function = self.trainer.save_checkpoint
+
+        return checkpoint_callback
+
+    def configure_progress_bar(self, refresh_rate=1, process_position=0):
+        progress_bars = [c for c in self.trainer.callbacks if isinstance(c, ProgressBarBase)]
+        if len(progress_bars) > 1:
+            raise MisconfigurationException(
+                'You added multiple progress bar callbacks to the Trainer, but currently only one'
+                ' progress bar is supported.'
+            )
+        elif len(progress_bars) == 1:
+            progress_bar_callback = progress_bars[0]
+        elif refresh_rate > 0:
+            progress_bar_callback = ProgressBar(
+                refresh_rate=refresh_rate,
+                process_position=process_position,
+            )
+            self.trainer.callbacks.append(progress_bar_callback)
+        else:
+            progress_bar_callback = None
+
+        return progress_bar_callback
+
+    ############################################
+    #                                          #
+    #  Start  CallbackConnectorLoggingMixin    #
+    #                                          #
+    #############################################
+
+    # This part class helps to summarize logging logic for Pytorch LightningModule using self.log functions for `pytorch_lightning.callbacks.CallBack`
 
     @staticmethod
     def validate_callback_logging_arguments(current_hook_fx_name:str = None, on_step:bool = None, on_epoch: bool = None) -> None:
-        current_callback_hook_auth_args = getattr(CallbackConnectorLoggingMixin, f"_{current_hook_fx_name}_log")()
+        current_callback_hook_auth_args = getattr(self, f"_{current_hook_fx_name}_log")()
 
         if current_callback_hook_auth_args is not None:
             m = "{} function supports only {} in {}. Provided {}"
@@ -213,72 +286,8 @@ class CallbackConnectorLoggingMixin(ABC):
         """Called when loading a model checkpoint."""
         return None
 
-
-class CallbackConnector(CallbackConnectorLoggingMixin):
-
-    def __init__(self, trainer):
-        self.trainer = trainer
-
-    def on_trainer_init(
-            self,
-            callbacks,
-            checkpoint_callback,
-            progress_bar_refresh_rate,
-            process_position,
-            default_root_dir,
-            weights_save_path,
-            resume_from_checkpoint
-    ):
-        self.trainer.resume_from_checkpoint = resume_from_checkpoint
-
-        # init folder paths for checkpoint + weights save callbacks
-        self.trainer._default_root_dir = default_root_dir or os.getcwd()
-        self.trainer._weights_save_path = weights_save_path or self.trainer._default_root_dir
-
-        # init callbacks
-        self.trainer.callbacks = callbacks or []
-
-        # configure checkpoint callback
-        # it is important that this is the last callback to run
-        # pass through the required args to figure out defaults
-        checkpoint_callback = self.init_default_checkpoint_callback(checkpoint_callback)
-        if checkpoint_callback:
-            self.trainer.callbacks.append(checkpoint_callback)
-
-        # TODO refactor codebase (tests) to not directly reach into these callbacks
-        self.trainer.checkpoint_callback = checkpoint_callback
-
-        # init progress bar
-        self.trainer._progress_bar_callback = self.configure_progress_bar(
-            progress_bar_refresh_rate, process_position
-        )
-
-    def init_default_checkpoint_callback(self, checkpoint_callback):
-        if checkpoint_callback is True:
-            checkpoint_callback = ModelCheckpoint(filepath=None)
-        elif checkpoint_callback is False:
-            checkpoint_callback = None
-        if checkpoint_callback:
-            checkpoint_callback.save_function = self.trainer.save_checkpoint
-
-        return checkpoint_callback
-
-    def configure_progress_bar(self, refresh_rate=1, process_position=0):
-        progress_bars = [c for c in self.trainer.callbacks if isinstance(c, ProgressBarBase)]
-        if len(progress_bars) > 1:
-            raise MisconfigurationException(
-                'You added multiple progress bar callbacks to the Trainer, but currently only one'
-                ' progress bar is supported.'
-            )
-        elif len(progress_bars) == 1:
-            progress_bar_callback = progress_bars[0]
-        elif refresh_rate > 0:
-            progress_bar_callback = ProgressBar(
-                refresh_rate=refresh_rate,
-                process_position=process_position,
-            )
-            self.trainer.callbacks.append(progress_bar_callback)
-        else:
-            progress_bar_callback = None
-
-        return progress_bar_callback
+    ############################################
+    #                                          #
+    #    End  CallbackConnectorLoggingMixin    #
+    #                                          #
+    #############################################
