53b5a1888f4713963661e64870ce5c2b75ab5864
ananthsub <ananth.subramaniam@gmail.com>
2020-10-24 17:45:39 -0700

    types

--- pytorch_lightning/accelerators/ddp_slurm_accelerator.py
+++ pytorch_lightning/accelerators/ddp_slurm_accelerator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 import os
-from typing import List
+from typing import List, Optional
 
 import torch
 import torch.distributed as dist
