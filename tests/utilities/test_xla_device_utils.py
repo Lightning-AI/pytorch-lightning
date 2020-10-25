20c4cd6c1a019c190b462510bceb83573f725759
Lezwon Castelino <lezwon@gmail.com>
2020-10-25 09:04:18 +0530

    added tests

--- tests/utilities/test_xla_device_utils.py
+++ tests/utilities/test_xla_device_utils.py
@@ -11,13 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import math
+import time
+
 import pytest
 
-from pytorch_lightning.utilities.xla_device_utils import XLADeviceUtils as xdu
+import pytorch_lightning.utilities.xla_device_utils as utils
 from tests.base.develop_utils import pl_multi_process_test
 
 try:
     import torch_xla.core.xla_model as xm
+
     XLA_AVAILABLE = True
 except ImportError as e:
     XLA_AVAILABLE = False
@@ -26,13 +30,13 @@ except ImportError as e:
 @pytest.mark.skipif(XLA_AVAILABLE, reason="test requires torch_xla to be absent")
 def test_tpu_device_absence():
     """Check tpu_device_exists returns None when torch_xla is not available"""
-    assert xdu.tpu_device_exists() is None
+    assert utils.XLADeviceUtils.tpu_device_exists() is None
 
 
 @pytest.mark.skipif(not XLA_AVAILABLE, reason="test requires torch_xla to be installed")
 def test_tpu_device_presence():
     """Check tpu_device_exists returns True when TPU is available"""
-    assert xdu.tpu_device_exists() is True
+    assert utils.XLADeviceUtils.tpu_device_exists() is True
 
 
 @pytest.mark.skipif(not XLA_AVAILABLE, reason="test requires torch_xla to be installed")
@@ -42,3 +46,18 @@ def test_xla_device_is_a_tpu():
     device = xm.xla_device()
     device_type = xm.xla_device_hw(device)
     return device_type == "TPU"
+
+
+def test_result_returns_within_10_seconds():
+    """Check that pl_multi_process returns within 10 seconds"""
+
+    def run(n):
+        time.sleep(n)
+        return n
+
+    start = time.time()
+    result = utils.pl_multi_process(run)(n=15)
+    end = time.time()
+    elapsed_time = math.floor(end - start)
+    assert elapsed_time <= 10
+    assert result is False
