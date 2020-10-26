f07ee33db679a4b4bdcb4a2a221aa5cbb05d7b34
chaton <thomas@grid.ai>
2020-10-26 11:57:03 +0000

    BUG - Wandb: Sanitize callable. (#4320)
    * add _sanitize_callable_params
    
    * add call on _val if callable
    
    * clean code formatter
    
    * resolve pep8
    
    * default return function name
    
    * resolve pep8
    
    * Apply suggestions from code review
    
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Sean Naren <sean.narenthiran@gmail.com>
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- pytorch_lightning/loggers/wandb.py
+++ pytorch_lightning/loggers/wandb.py
@@ -135,6 +135,7 @@ class WandbLogger(LightningLoggerBase):
     def log_hyperparams(self, params: Union[Dict[str, Any], Namespace]) -> None:
         params = self._convert_params(params)
         params = self._flatten_dict(params)
+        params = self._sanitize_callable_params(params)
         self.experiment.config.update(params, allow_val_change=True)
 
     @rank_zero_only
