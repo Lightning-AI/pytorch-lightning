6f77ec20ed639260c52d5287a8d4f212d95bc759
ananthsub <ananth.subramaniam@gmail.com>
2020-10-26 14:08:51 -0700

    Update ddp_plugin.py

--- pytorch_lightning/plugins/ddp_plugin.py
+++ pytorch_lightning/plugins/ddp_plugin.py
@@ -1,11 +1,15 @@
-from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
-from pytorch_lightning.core.lightning import LightningModule
 from typing import List
+
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
 
 
 class DDPPlugin(object):
     """
     Plugin to link a custom ddp implementation to any arbitrary accelerator.
+
+    This plugin forwards all constructor arguments to `LightningDistributedDataParallel`,
+    which in turn forwards all args to `DistributedDataParallel`.
 
     Example::
 
@@ -17,11 +21,16 @@ class DDPPlugin(object):
 
         my_ddp = MyDDP()
         trainer = Trainer(accelerator='ddp_x', plugins=[my_ddp])
-
     """
 
-    def configure_ddp(self, model: LightningModule, device_ids: List[int]) -> LightningDistributedDataParallel:
+    def __init__(self, **kwargs):
+        self._kwargs: Dict[str, Any] = kwargs
+
+    def configure_ddp(
+        self, model: LightningModule, device_ids: List[int]
+    ) -> LightningDistributedDataParallel:
         """
+        Pass through all customizations from constructor to `LightningDistributedDataParallel`.
         Override to define a custom DDP implementation.
 
         .. note:: Only requirement is that your DDP implementation subclasses LightningDistributedDataParallel
@@ -43,5 +52,7 @@ class DDPPlugin(object):
             the model wrapped in LightningDistributedDataParallel
 
         """
-        model = LightningDistributedDataParallel(model, device_ids=device_ids, find_unused_parameters=True)
+        model = LightningDistributedDataParallel(
+            model, device_ids=device_ids, find_unused_parameters=True, **self._kwargs
+        )
         return model
