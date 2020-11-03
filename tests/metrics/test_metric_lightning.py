80e336ab28cf1f1f670bb56dd8fa3972de8fb930
Nicki Skafte <skaftenicki@gmail.com>
2020-11-03 14:46:20 +0100

    trying to fix ddp test

--- tests/metrics/test_metric_lightning.py
+++ tests/metrics/test_metric_lightning.py
@@ -5,7 +5,7 @@ import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.metrics import Metric
 from tests.base.boring_model import BoringModel
-
+import tests.base.develop_utils as tutils
 
 class SumMetric(Metric):
     def __init__(self):
@@ -89,38 +89,48 @@ def test_metric_lightning_log(tmpdir):
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 def test_metric_lightning_ddp(tmpdir):
+    tutils.set_random_master_port()
+
+    # Dummy dataset, where sum is known
+    data=torch.arange(10)[:,None].float()
+    dataset = torch.utils.data.TensorDataset(data)
+
     class TestModel(BoringModel):
         def __init__(self):
             super().__init__()
             self.metric = SumMetric()
-            self.sum = 0.0
+            self.p = torch.nn.Linear(1,1) # fake params
 
         def training_step(self, batch, batch_idx):
-            x = batch
-            self.metric_step(x.sum())
-            self.sum += x.sum()
-            self.log("sum", self.metric, on_epoch=True, on_step=False)
-            return self.step(x)
+            val = self.metric(batch[0])
+            self.log("sum", self.metric, on_step=False, on_epoch=True)
+            return self.p(val.view(1,1))
+
+        def train_dataloader(self):
+            return torch.utils.data.DataLoader(
+                dataset,
+                batch_size=1,
+                sampler=torch.utils.data.distributed.DistributedSampler(dataset, shuffle=False)
+            )
+
+        def configure_optimizers(self):
+            return None
 
     model = TestModel()
-    model.val_dataloader = None
-
     trainer = Trainer(
-        default_root_dir=tmpdir,
-        limit_train_batches=2,
-        limit_val_batches=2,
+        gpus=2,
         max_epochs=1,
         log_every_n_steps=1,
-        weights_summary=None,
-        gpus=2,
-        accelerator='ddp'
+        accelerator='ddp',
+        progress_bar_refresh_rate=0,
+        replace_sampler_ddp=False
     )
     trainer.fit(model)
 
-    # Manual calculate sum
-    manual_sum = 0.0
-    for batch in model.train_dataloader():
-        manual_sum += batch.sum()
-
     logged = trainer.logged_metrics
-    assert torch.allclose(torch.tensor(logged["sum"]), manual_sum)
+
+    assert torch.tensor(logged["sum"]) == dataset.tensors[0].sum(), \
+        "Metrics did not accumulate correctly in ddp mode"
+
+
+
