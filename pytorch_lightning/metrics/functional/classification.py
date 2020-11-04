c6c543b410e446fa26dd0ad081651017e03d8c93
Nicki Skafte <skaftenicki@gmail.com>
2020-11-04 20:59:39 +0100

    cleaning

--- pytorch_lightning/metrics/functional/classification.py
+++ pytorch_lightning/metrics/functional/classification.py
@@ -646,9 +646,6 @@ def multiclass_roc(
     return tuple(class_roc_vals)
 
 
-
-
-
 def auc(
         x: torch.Tensor,
         y: torch.Tensor,
@@ -818,40 +815,6 @@ def multiclass_auroc(
                                      sample_weight=sample_weight,
                                      num_classes=num_classes)
     return torch.mean(class_aurocs)
-
-
-def average_precision(
-        pred: torch.Tensor,
-        target: torch.Tensor,
-        sample_weight: Optional[Sequence] = None,
-        pos_label: int = 1.,
-) -> torch.Tensor:
-    """
-    Compute average precision from prediction scores
-
-    Args:
-        pred: estimated probabilities
-        target: ground-truth labels
-        sample_weight: sample weights
-        pos_label: the label for the positive class
-
-    Return:
-        Tensor containing average precision score
-
-    Example:
-
-        >>> x = torch.tensor([0, 1, 2, 3])
-        >>> y = torch.tensor([0, 1, 2, 2])
-        >>> average_precision(x, y)
-        tensor(0.3333)
-    """
-    precision, recall, _ = precision_recall_curve(pred=pred, target=target,
-                                                  sample_weight=sample_weight,
-                                                  pos_label=pos_label)
-    # Return the step function integral
-    # The following works because the last entry of precision is
-    # guaranteed to be 1, as returned by precision_recall_curve
-    return -torch.sum((recall[1:] - recall[:-1]) * precision[:-1])
 
 
 def dice_score(
