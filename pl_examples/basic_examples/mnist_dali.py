8d911286b270b303f99eca004e225e876f470b90
SeanNaren <sean@grid.ai>
2020-11-04 20:15:03 +0000

    Patch to reflect the mnist example module

--- pl_examples/basic_examples/mnist_dali.py
+++ pl_examples/basic_examples/mnist_dali.py
@@ -106,8 +106,8 @@ class LitClassifier(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
 
-        self.l1 = torch.nn.Linear(28 * 28, hidden_dim)
-        self.l2 = torch.nn.Linear(hidden_dim, 10)
+        self.l1 = torch.nn.Linear(28 * 28, self.hparams.hidden_dim)
+        self.l2 = torch.nn.Linear(self.hparams.hidden_dim, 10)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
@@ -122,20 +122,19 @@ class LitClassifier(pl.LightningModule):
         x, y = self.split_batch(batch)
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        self.log('loss', loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = self.split_batch(batch)
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        self.log("valid_loss", loss)
+        self.log('valid_loss', loss)
 
     def test_step(self, batch, batch_idx):
         x, y = self.split_batch(batch)
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        self.log("test_loss", loss)
+        self.log('test_loss', loss)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
@@ -143,8 +142,8 @@ class LitClassifier(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument("--hidden_dim", type=int, default=128)
-        parser.add_argument("--learning_rate", type=float, default=0.0001)
+        parser.add_argument('--hidden_dim', type=int, default=128)
+        parser.add_argument('--learning_rate', type=float, default=0.0001)
         return parser
 
 
@@ -155,7 +154,7 @@ def cli_main():
     # args
     # ------------
     parser = ArgumentParser()
-    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser = pl.Trainer.add_argparse_args(parser)
     parser = LitClassifier.add_model_specific_args(parser)
     args = parser.parse_args()
@@ -163,8 +162,8 @@ def cli_main():
     # ------------
     # data
     # ------------
-    dataset = MNIST("", train=True, download=True, transform=transforms.ToTensor())
-    mnist_test = MNIST("", train=False, download=True, transform=transforms.ToTensor())
+    dataset = MNIST('', train=True, download=True, transform=transforms.ToTensor())
+    mnist_test = MNIST('', train=False, download=True, transform=transforms.ToTensor())
     mnist_train, mnist_val = random_split(dataset, [55000, 5000])
 
     eii_train = ExternalMNISTInputIterator(mnist_train, args.batch_size)
