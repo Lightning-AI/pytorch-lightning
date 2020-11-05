7483052379dc29102f5e3ccb19e9e0a77232ea8e
tchaton <thomas@grid.ai>
2020-11-05 12:32:39 +0000

    resolve flake8

--- tests/backends/test_ddp.py
+++ tests/backends/test_ddp.py
@@ -19,6 +19,7 @@ from tests.utilities.dist import call_training_script
 
 os.environ["PL_ENABLE_DDP_COVERAGE"] = '1'
 
+
 @pytest.mark.parametrize('cli_args', [
     pytest.param('--max_epochs 1 --gpus 2 --distributed_backend ddp'),
 ])
