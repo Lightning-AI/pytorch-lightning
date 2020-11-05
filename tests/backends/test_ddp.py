2d3de7995dc511798d91fdedd8c90e3cb9c70b60
gianscarpe <gianluca@scarpellini.dev>
2020-11-05 10:55:29 +0100

    Fixed PYTHONPATH for ddp test model

--- tests/backends/test_ddp.py
+++ tests/backends/test_ddp.py
@@ -11,9 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
 import pytest
 import torch
-import os
+
 from tests.backends import ddp_model
 from tests.utilities.dist import call_training_script
 
