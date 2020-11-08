276e4ff81fbcc15b320250fd718c17a7435c11a1
Yi-Lin Sung <r06942076@ntu.edu.tw>
2020-11-08 09:02:45 +0800

    Refactor MultiIterator to be as CombinedLoaderIterator

--- pytorch_lightning/trainer/train_loader_patch.py
+++ pytorch_lightning/trainer/train_loader_patch.py
@@ -14,35 +14,77 @@
 '''
 
 import itertools
+
+from typing import Any, Union
+from collections.abc import Iterable, Iterator, Mapping, Sequence
+
+from torch.utils.data import DataLoader
+
+from pytorch_lightning.utilities.data import get_len
+from pytorch_lightning.utilities.apply_func import apply_to_collection
   
-def get_len(d):
-    if isinstance(d, dict):
-        v = max(d.items(), key=lambda x: len(x[1]))
-        return len(v[1])
-    else:
-        return len(d)
+
+# def get_len(d):
+#     if isinstance(d, dict):
+#         v = max(d.items(), key=lambda x: len(x[1]))
+#         return len(v[1])
+#     else:
+#         return len(d)
 
 class MultiIterator(object):
-    def __init__(self, data) -> None:
-        super(object, self).__init__()
-        self.d = data
-        self.l = get_len(data)
+    SUPPORTED_MODES = ('min_size', 'max_size_cycle')
+
+    def __init__(self, loaders: Any, mode: str = 'min_size') -> None:
+        self.loaders = loaders 
+        self.num_batches = self._calc_num_batches(loaders, mode)
+
+    def _calc_num_batches(self, loaders, mode: str) -> Union[int, float]:
+        all_lengths = apply_to_collection(loaders, Iterable, get_len,
+                                          wrong_dtype=(Sequence, Mapping))
+
+        if mode == 'min_size':
+            compare_func = min
+        elif mode == 'max_size_cycle':
+            compare_func = max
+        else:
+            raise ValueError(f"Invalid Mode: {mode}")
+
+        if isinstance(all_lengths, (int, float)):
+            return all_lengths
+        if isinstance(all_lengths, Mapping):
+            return compare_func(all_lengths.values())
+        elif isinstance(all_lengths, Sequence):
+            return compare_func(all_lengths)
+
+        raise TypeError(f'Got Type {type(all_lengths).__name__}, but expected one of Sequence, Mapping, int or float')
  
-    def __len__(self) -> int:
-        return get_len(self.d)
+    def __len__(self) -> Union[int, float]:
+        # Return type might be int or inf. Inf will cause type error when calling len()
+        return self.num_batches
 
     def __iter__(self):
-        if isinstance(self.d, dict):
-            gen = {}
-            for k,v in self.d.items():
-                gen[k] = itertools.cycle(v)
-            for i in range(self.l):
+        if isinstance(self.loaders, Mapping):
+            gens = {}
+            for batch_idx in range(self.num_batches):
                 rv = {}
-                for k,v in self.d.items():
-                    rv[k] = next(gen[k])
+                for loader_name, loader in self.loaders.items():
+                    # If reaching the end of the iterator, recreate one
+                    # because shuffle=True in dataloader, the iterator will have a different order
+                    if batch_idx % len(loader) == 0: 
+                        gens[loader_name] = iter(loader)
+                    rv[loader_name] = next(gens[loader_name])
                 yield rv
-        else:
-            gen = itertools.cycle(self.d)
-            for i in range(self.l):
-                batch = next(gen)
-                yield batch
+        elif isinstance(self.loaders, Sequence):
+            gens = [None] * self.num_batches
+            for batch_idx in range(self.num_batches):
+                rv = []
+                for idx, loader in enumerate(self.loaders):
+                    # If reaching the end of the iterator, recreate one
+                    # because shuffle=True in dataloader, the iterator will have a different order
+                    if batch_idx % len(loader) == 0:
+                        gens[idx] = iter(loader)
+                    rv.append(next(gens[idx]))
+                yield rv
+
+        return iter(self.loaders)
+        
