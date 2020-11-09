c1186e42bc386758d81150908aaf57ec0535eee6
Tarepan <tarepan5884@gmail.com>
2020-11-09 15:39:21 +0900

    Fix utility import pathes

--- pytorch_lightning/trainer/connectors/checkpoint_connector.py
+++ pytorch_lightning/trainer/connectors/checkpoint_connector.py
@@ -25,12 +25,11 @@ import torch.distributed as torch_distrib
 import pytorch_lightning
 from pytorch_lightning import _logger as log
 from pytorch_lightning.core.lightning import LightningModule
-from pytorch_lightning.utilities import AMPType, rank_zero_warn
+from pytorch_lightning.utilities import AMPType, rank_zero_info, rank_zero_warn
 from pytorch_lightning.utilities.cloud_io import atomic_save, get_filesystem
 from pytorch_lightning.utilities.cloud_io import load as pl_load
 from pytorch_lightning.utilities.upgrade_checkpoint import KEYS_MAPPING as DEPRECATED_CHECKPOINT_KEYS
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from pytorch_lightning.utilities.distributed import rank_zero_info, rank_zero_warn
 
 try:
     from apex import amp
