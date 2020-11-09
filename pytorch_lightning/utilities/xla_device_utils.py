1946591d6aa585d99d40e5205846d7b04874c6a8
Lezwon Castelino <lezwon@gmail.com>
2020-11-09 10:32:28 +0530

    added xla_device_utils to test

--- pytorch_lightning/utilities/xla_device_utils.py
+++ pytorch_lightning/utilities/xla_device_utils.py
@@ -22,11 +22,12 @@ XLA_AVAILABLE = importlib.util.find_spec("torch_xla") is not None
 
 if XLA_AVAILABLE:
     import torch_xla.core.xla_model as xm
+    import torch_xla.distributed.xla_multiprocessing as xmp
 
 
-def inner_f(queue, func, *args, **kwargs):  # pragma: no cover
+def inner_f(_, queue, func, *args):  # pragma: no cover
     try:
-        queue.put(func(*args, **kwargs))
+        queue.put(func(*args))
     except Exception:
         import traceback
 
@@ -38,13 +39,13 @@ def pl_multi_process(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         queue = Queue()
-        proc = Process(target=inner_f, args=(queue, func, *args), kwargs=kwargs)
-        proc.start()
-        proc.join(10)
-        try:
-            return queue.get_nowait()
-        except q.Empty:
-            return False
+        xmp.spawn(inner_f,
+          args=(queue, func, *args),
+          nprocs=1,
+          join=True,
+          daemon=False,
+          start_method='fork')
+        return queue.get()
 
     return wrapper
 
