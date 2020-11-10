be4e0f333380aeca6f266d6a39e101779dceba69
chaton <thomas@grid.ai>
2020-11-10 18:34:40 +0000

    Update pytorch_lightning/utilities/memory.py
    Co-authored-by: Justus Schock <12886177+justusschock@users.noreply.github.com>

--- pytorch_lightning/utilities/memory.py
+++ pytorch_lightning/utilities/memory.py
@@ -17,7 +17,7 @@ import gc
 import torch
 
 
-def recursive_detach(in_dict: dict, to_cpu=False) -> dict:
+def recursive_detach(in_dict: dict, to_cpu: bool = False) -> dict:
     """Detach all tensors in `in_dict`.
 
     May operate recursively if some of the values in `in_dict` are dictionaries
