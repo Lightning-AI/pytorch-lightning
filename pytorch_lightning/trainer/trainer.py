e9a8d4297c83d2456d4528a0af54f29a7ac3b187
ananthsub <ananth.subramaniam@gmail.com>
2020-11-10 09:40:58 -0800

    Fix setup callback hook

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -850,7 +850,7 @@ class Trainer(
             called = self.datamodule.has_setup_test if self.testing else self.datamodule.has_setup_fit
             if not called:
                 self.datamodule.setup(stage_name)
-        self.setup(stage_name)
+        self.setup(model, stage_name)
         model.setup(stage_name)
 
     def _reset_result_and_set_hook_fx_name(self, hook_name):
