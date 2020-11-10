07f9fd4da420d5ed7578269d3c2310c810c33614
Yi-Lin Sung <r06942076@ntu.edu.tw>
2020-11-10 17:45:17 +0800

    Update tests for supporters

--- tests/trainer/test_supporters.py
+++ tests/trainer/test_supporters.py
@@ -1,8 +1,10 @@
 from collections import Sequence
 
+import pytest
 import torch
 
-from pytorch_lightning.trainer.supporters import CycleIterator, CombinedLoaderIterator
+from torch.utils.data import TensorDataset
+from pytorch_lightning.trainer.supporters import CycleIterator, CombinedLoader, CombinedDataset, CombinedLoaderIterator
 
 
 def test_cycle_iterator():
@@ -14,11 +16,39 @@ def test_cycle_iterator():
     assert idx == len(iterator) - 1
 
 
+@pytest.mark.parametrize(['dataset_1', 'dataset_2'], [
+    ([list(range(10)), list(range(20))]),
+    ([range(10), range(20)]),
+    ([torch.randn(10, 3, 2), torch.randn(20, 5, 6)]),
+    ([TensorDataset(torch.randn(10, 3, 2)), TensorDataset(torch.randn(20, 5, 6))])
+])
+def test_combined_dataset(dataset_1, dataset_2):
+    datasets = [dataset_1, dataset_2]
+    combined_dataset = CombinedDataset(datasets)
+
+    assert combined_dataset.max_len == 20
+    assert combined_dataset.min_len == len(combined_dataset) == 10
+
+
 def test_combined_loader_iterator_dict_min_size():
     loaders = {'a': torch.utils.data.DataLoader(range(10), batch_size=4),
                'b': torch.utils.data.DataLoader(range(20), batch_size=5)}
 
-    combined_loader = CombinedLoaderIterator(loaders, 'min_size')
+    combined_iter = CombinedLoaderIterator(loaders)
+
+    for idx, item in enumerate(combined_iter):
+        assert isinstance(item, dict)
+        assert len(item) == 2
+        assert 'a' in item and 'b' in item
+
+    assert idx == min(len(loaders['a']), len(loaders['b'])) - 1
+
+
+def test_combined_loader_dict_min_size():
+    loaders = {'a': torch.utils.data.DataLoader(range(10), batch_size=4),
+               'b': torch.utils.data.DataLoader(range(20), batch_size=5)}
+
+    combined_loader = CombinedLoader(loaders, 'min_size')
 
     assert len(combined_loader) == min([len(v) for v in loaders.values()])
 
@@ -30,11 +60,11 @@ def test_combined_loader_iterator_dict_min_size():
     assert idx == len(combined_loader) - 1
 
 
-def test_combined_loader_iterator_dict_max_size_cycle():
+def test_combined_loader_dict_max_size_cycle():
     loaders = {'a': torch.utils.data.DataLoader(range(10), batch_size=4),
                'b': torch.utils.data.DataLoader(range(20), batch_size=5)}
 
-    combined_loader = CombinedLoaderIterator(loaders, 'max_size_cycle')
+    combined_loader = CombinedLoader(loaders, 'max_size_cycle')
 
     assert len(combined_loader) == max([len(v) for v in loaders.values()])
 
@@ -46,11 +76,11 @@ def test_combined_loader_iterator_dict_max_size_cycle():
     assert idx == len(combined_loader) - 1
 
 
-def test_combined_loader_iterator_sequence_min_size():
+def test_combined_loader_sequence_min_size():
     loaders = [torch.utils.data.DataLoader(range(10), batch_size=4),
                torch.utils.data.DataLoader(range(20), batch_size=5)]
 
-    combined_loader = CombinedLoaderIterator(loaders, 'min_size')
+    combined_loader = CombinedLoader(loaders, 'min_size')
 
     assert len(combined_loader) == min([len(v) for v in loaders])
 
@@ -61,11 +91,11 @@ def test_combined_loader_iterator_sequence_min_size():
     assert idx == len(combined_loader) - 1
 
 
-def test_combined_loader_iterator_sequence_max_size_cycle():
+def test_combined_loader_sequence_max_size_cycle():
     loaders = [torch.utils.data.DataLoader(range(10), batch_size=4),
                torch.utils.data.DataLoader(range(20), batch_size=5)]
 
-    combined_loader = CombinedLoaderIterator(loaders, 'max_size_cycle')
+    combined_loader = CombinedLoader(loaders, 'max_size_cycle')
 
     assert len(combined_loader) == max([len(v) for v in loaders])
 
