873e9c088d12b5103a5c4841b45f9e3da7945e43
chaton <thomas@grid.ai>
2020-11-10 19:38:57 +0000

    Update pytorch_lightning/core/step_result.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pytorch_lightning/core/step_result.py
+++ pytorch_lightning/core/step_result.py
@@ -396,6 +396,7 @@ class Result(Dict):
                 self.__setitem__(k, v.detach())
 
     def cpu(self):
+        """Move all self attributes to CPU."""
         for k, v in self.items():
             if isinstance(v, torch.Tensor):
                 self.__setitem__(k, v.cpu())
