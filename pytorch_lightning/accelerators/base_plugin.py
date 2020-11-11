b961aaf054bda242a361cba30d31ae776588b029
justusschock <justus.schock@posteo.de>
2020-11-11 16:58:23 +0100

    define base plugin api

---
+++ pytorch_lightning/accelerators/base_plugin.py
@@ -0,0 +1,31 @@
+import contextlib
+import torch
+
+class Plugin(object):
+
+    def connect(self, model: torch.nn.Module, optimizers, lr_schedulers):
+        return model, optimizers, lr_schedulers
+        
+    def pre_optimizer_step(self, optimizer, optiizer_idx):
+        pass
+
+    def post_optimizer_step(self, optimizer, optimizer_idx):
+        pass
+
+    def pre_training(self):
+        pass
+
+    def post_training(self):
+        pass
+
+    @contextlib.contextmanager
+    def train_step_context(self):
+        yield
+
+    @contextlib.contextmanager
+    def val_step_context(self):
+        yield
+
+    @contextlib.contextmanager
+    def test_step_context(self):
+        yield
