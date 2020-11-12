6a62443e0d8f3781b6fc994c1ae2212ce4efbbd3
tchaton <thomas@grid.ai>
2020-11-12 18:39:11 +0000

    i

--- tests/models/test_multi_nodes_gpu.py
+++ tests/models/test_multi_nodes_gpu.py
@@ -12,74 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pytest
-import torch
 
-import tests.base.develop_pipelines as tpipes
-import tests.base.develop_utils as tutils
-from pytorch_lightning.callbacks import EarlyStopping
-from tests.base import EvalModelTemplate
-from pytorch_lightning.core import memory
+import os
+import collections
+import pytest
+import itertools
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+
+import pytorch_lightning as pl
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning import Trainer, callbacks
+
+from tests.base.boring_model import BoringModel, RandomDictDataset, RandomDictStringDataset
+from tests.base.deterministic_model import DeterministicModel
+
 from pytorch_lightning.trainer import Trainer
 
 
-@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-def test_multi_gpu_early_stop_ddp_spawn(tmpdir):
-    """Make sure DDP works. with early stopping"""
-    tutils.set_random_master_port()
+def test_multi_node_gpu(tmpdir):
 
-    trainer_options = dict(
-        default_root_dir=tmpdir,
-        callbacks=[EarlyStopping()],
-        max_epochs=50,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        gpus=[0, 1],
-        distributed_backend='ddp_spawn',
-    )
+    class TestModel(BoringModel):
+        def on_train_epoch_start(self) -> None:
+            print("override any method to prove your bug")
 
-    model = EvalModelTemplate()
-    tpipes.run_model_test(trainer_options, model)
+        def training_step(self, batch, batch_idx):
+            output = self.layer(batch)
+            loss = self.loss(batch, output)
+            return {"loss": loss}
 
+        def validation_step(self, batch, batch_idx):
+            output = self.layer(batch)
+            loss = self.loss(batch, output)
+            self.log("val_loss", loss)
 
-@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-def test_multi_gpu_model_ddp_spawn(tmpdir):
-    tutils.set_random_master_port()
-
-    trainer_options = dict(
-        default_root_dir=tmpdir,
-        max_epochs=1,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        gpus=[0, 1],
-        distributed_backend='ddp_spawn',
-        progress_bar_refresh_rate=0
-    )
-
-    model = EvalModelTemplate()
-
-    tpipes.run_model_test(trainer_options, model)
-
-    # test memory helper functions
-    memory.get_memory_profile('min_max')
-
-
-@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-def test_ddp_all_dataloaders_passed_to_fit(tmpdir):
-    """Make sure DDP works with dataloaders passed to fit()"""
-    tutils.set_random_master_port()
-
-    model = EvalModelTemplate()
-    fit_options = dict(train_dataloader=model.train_dataloader(),
-                       val_dataloaders=model.val_dataloader())
-
+    # model
+    model = TestModel()
+    model.validation_epoch_end = None
     trainer = Trainer(
         default_root_dir=tmpdir,
-        progress_bar_refresh_rate=0,
-        max_epochs=1,
-        limit_train_batches=0.2,
-        limit_val_batches=0.2,
-        gpus=[0, 1],
-        distributed_backend='ddp_spawn'
+        limit_train_batches=8,
+        limit_val_batches=8,
+        max_epochs=2,
+        weights_summary=None,
+        num_nodes=2,
+        gpus=1,
+
     )
-    result = trainer.fit(model, **fit_options)
-    assert result == 1, "DDP doesn't work with dataloaders passed to fit()."
+    trainer.fit(model)
+
+
+
+
