3bf605d4f0f207f6e0245475c1acaf790dc8d151
tchaton <thomas@grid.ai>
2020-11-16 10:03:39 +0000

    added backward to LightningOptimizer

--- pytorch_lightning/core/optimizer.py
+++ pytorch_lightning/core/optimizer.py
@@ -11,38 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import tempfile
 import collections
 import copy
 import inspect
+import os
 import re
+import tempfile
 import types
 from abc import ABC
 from argparse import Namespace
-from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union, Mapping
-from torch.optim.optimizer import Optimizer
+from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Tuple, Union
 
 import torch
+from torch import ScriptModule, Tensor
+from torch.nn import Module
+from torch.optim.optimizer import Optimizer
+
 from pytorch_lightning import _logger as log
+from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.core.grads import GradInformation
 from pytorch_lightning.core.hooks import CheckpointHooks, DataHooks, ModelHooks
 from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.core.saving import ALLOWED_CONFIG_TYPES, PRIMITIVE_TYPES, ModelIO
 from pytorch_lightning.core.step_result import Result
-from pytorch_lightning.utilities import rank_zero_warn, AMPType
+from pytorch_lightning.utilities import AMPType, rank_zero_warn
 from pytorch_lightning.utilities.device_dtype_mixin import DeviceDtypeModuleMixin
-from pytorch_lightning.utilities.xla_device_utils import XLADeviceUtils
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from pytorch_lightning.utilities.parsing import (
-    AttributeDict,
-    collect_init_args,
-    get_init_args,
-)
-from pytorch_lightning.callbacks import Callback
-from torch import ScriptModule, Tensor
-from torch.nn import Module
-
+from pytorch_lightning.utilities.parsing import AttributeDict, collect_init_args, get_init_args
+from pytorch_lightning.utilities.xla_device_utils import XLADeviceUtils
 
 TPU_AVAILABLE = XLADeviceUtils.tpu_device_exists()
 
@@ -56,11 +52,32 @@ def do_nothing_closure():
 
 class LightningOptimizer(Optimizer):
 
-    def __init__(self, trainer, optimizer: Optimizer = None, optimizer_idx: Optional[int] = None):
+    def __init__(self,
+                 trainer,
+                 optimizer: Optimizer = None,
+                 optimizer_idx: Optional[int] = None,
+                 accumulate_grad_batches: Optional[int] = None):
+
+        if isinstance(accumulate_grad_batches, int) and accumulate_grad_batches >= 1:
+            raise MisconfigurationException(f"accumulate_grad_batches parameters "
+                                            f"{accumulate_grad_batches} should be >= 1")
+
         self._trainer = trainer
         self._optimizer = optimizer
         self._optimizer_idx = optimizer_idx
+        if accumulate_grad_batches is None:
+            self._accumulate_grad_batches = trainer.accumulate_grad_batches
+        else:
+            self._accumulate_grad_batches = accumulate_grad_batches
         self._expose_optimizer_attr()
+
+    @property
+    def accumulate_grad_batches(self):
+        return self._accumulate_grad_batches
+
+    @accumulate_grad_batches.setter
+    def accumulate_grad_batches(self, accumulate_grad_batches):
+        self._accumulate_grad_batches = accumulate_grad_batches
 
     @property
     def optimizer(self):
@@ -83,10 +100,12 @@ class LightningOptimizer(Optimizer):
             'param_groups': self._optimizer.param_groups,
             'optimizer_cls': self._optimizer.__class__,
             'optimizer_idx': self._optimizer_idx,
+            "accumulate_grad_batches": self._accumulate_grad_batches,
         }
 
     def __setstate__(self, state):
         self._optimizer_idx = state["optimizer_idx"]
+        self._accumulate_grad_batches = state["accumulate_grad_batches"]
         self._optimizer = state["optimizer_cls"](state['param_groups'], ** state['defaults'])
         self._expose_optimizer_attr()
 
@@ -104,18 +123,98 @@ class LightningOptimizer(Optimizer):
         else:
             return self.__class__.__name__
 
-    def step(self, *args, closure=do_nothing_closure, **kwargs):
-        if self._trainer.on_tpu:
-            xm.optimizer_step(self._optimizer, optimizer_args={'closure': closure, **kwargs})
-        elif self._trainer.amp_backend == AMPType.NATIVE:
-            # native amp does not yet support closures.
-            # TODO: pass the closure to the step ASAP
-            closure()
-            self._trainer.scaler.step(self._optimizer)
-        elif self._trainer.amp_backend == AMPType.APEX:
-            # apex amp does not yet support closures.
-            # TODO: pass the closure to the step ASAP
-            closure()
-            self._optimizer.step()
+    def _accumulated_batches_reached(self):
+        return (self._trainer.batch_idx + 1) % self._accumulate_grad_batches == 0
+
+    def _num_training_batches_reached(self):
+        return (self._trainer.batch_idx + 1) == self._trainer.num_training_batches
+
+    @property
+    def _should_accumulate(self):
+        # checks if backward or backward + optimizer step (via closure)
+        accumulation_done = self._accumulated_batches_reached()
+        is_final_batch = self._num_training_batches_reached()
+        return not (accumulation_done or is_final_batch)
+
+    def backward(self, loss: Tensor, *args, **kwargs) -> None:
+        """
+        Call this directly from your training_step when doing optimizations manually.
+        By using this we can ensure that all the proper scaling when using 16-bit etc has been done for you
+
+        .. tip:: In manual mode we still automatically accumulate grad over batches if
+           Trainer(accumulate_grad_batches=x) is set.
+
+        Args:
+            loss: Optimizer used to perform `.step()` call
+
+        Example::
+
+            def training_step(...):
+                (opt_a, opt_b) = self.optimizers()
+                loss_a = ...
+
+                # automatically applies scaling, etc...
+                opt_a.backward(loss_a)
+                opt_a.step()
+
+        Example::
+
+            def training_step(...):
+                (opt_a, opt_b) = self.optimizers()
+                loss_a = ...
+
+                # automatically applies scaling, etc...
+                def closure_a():
+                    loss_a = ...
+                    opt_a.backward(loss)
+
+                opt_a.step(closure=closure_a)
+
+        """
+
+        model_ref = self._trainer.get_model()
+
+        # toggle params
+        model_ref.toggle_optimizer(self, self._optimizer_idx)
+
+        # perform manual_backward
+        model_ref.manual_backward(loss, self, *args, **kwargs)
+
+    def step(self, *args, closure: Callable = do_nothing_closure, **kwargs):
+        """
+        Call this directly from your training_step when doing optimizations manually.
+        By using this we can ensure that all the proper scaling when using 16-bit etc has been done for you
+
+        .. tip:: In manual mode we still automatically accumulate grad over batches if
+           Trainer(accumulate_grad_batches=x) is set.
+
+        Args:
+            closure: Closure usually contains forward and backward step
+        """
+
+        if not self._should_accumulate:
+            if self._trainer.on_tpu:
+                xm.optimizer_step(self._optimizer, optimizer_args={'closure': closure, **kwargs})
+            elif self._trainer.amp_backend == AMPType.NATIVE:
+                # native amp does not yet support closures.
+                # TODO: pass the closure to the step ASAP
+                closure()
+                self._trainer.scaler.step(self._optimizer)
+            elif self._trainer.amp_backend == AMPType.APEX:
+                # apex amp does not yet support closures.
+                # TODO: pass the closure to the step ASAP
+                closure()
+                self._optimizer.step()
+            else:
+                self._optimizer.step(closure=closure, *args, **kwargs)
+
+            # update will be called after every optimizer_step call
+            if self._trainer.amp_backend == AMPType.NATIVE:
+                self._trainer.scaler.update()
+
+            # perform zero grad
+            self._optimizer.zero_grad()
         else:
-            self._optimizer.step(closure=closure, *args, **kwargs)
+            # make sure to call optimizer_closure when accumulating
+            if isinstance(closure, types.FunctionType):
+                closure()
--- tests/core/test_lightning_optimizer.py
+++ tests/core/test_lightning_optimizer.py
@@ -11,16 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
 import os
+from unittest.mock import patch
+
+import pytest
 import torch
 import torch.nn as nn
+
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.core.optimizer import LightningOptimizer
 from tests.base.boring_model import BoringModel, RandomDictDataset, RandomDictStringDataset
 
 
 def test_lightning_optimizer(tmpdir):
+    """
+    Test that optimizer are correctly wrapped by our LightningOptimizer
+    """
     class TestModel(BoringModel):
         def configure_optimizers(self):
             optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
@@ -49,6 +55,10 @@ def test_lightning_optimizer(tmpdir):
 
 
 def test_lightning_optimizer_from_user(tmpdir):
+    """
+    Test that the user can use our LightningOptimizer. Not recommended.
+    """
+
     class TestModel(BoringModel):
         def configure_optimizers(self):
             optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
@@ -74,3 +84,106 @@ def test_lightning_optimizer_from_user(tmpdir):
                     weight_decay: 0
                   )"""
     assert trainer.optimizers[0].__repr__().replace(" ", '') == expected.replace(" ", '')
+
+
+@patch("torch.optim.Adam.step")
+@patch("torch.optim.SGD.step")
+def test_lightning_optimizer_manual_optimization(mock_sgd_step, mock_adam_step, tmpdir):
+    """
+    Test that the user can use our LightningOptimizer. Not recommended.
+    """
+    class TestModel(BoringModel):
+
+        def training_step(self, batch, batch_idx, optimizer_idx=None):
+            (opt_1, opt_2) = self.optimizers()
+            assert isinstance(opt_1, LightningOptimizer)
+            assert isinstance(opt_2, LightningOptimizer)
+
+            output = self.layer(batch)
+            loss_1 = self.loss(batch, output)
+            opt_1.backward(loss_1)
+            opt_1.step(idx="1")
+
+            def closure():
+                output = self.layer(batch)
+                loss_2 = self.loss(batch, output)
+                opt_2.backward(loss_2)
+            opt_2.step(closure=closure, idx="2")
+
+        def configure_optimizers(self):
+            optimizer_1 = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+            optimizer_2 = torch.optim.Adam(self.layer.parameters(), lr=0.1)
+            optimizer_1 = LightningOptimizer(self.trainer, optimizer_1)
+
+            # accumulated_gradients at 4
+            optimizer_1.accumulate_grad_batches = 4
+
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_1, step_size=1)
+            return [optimizer_1, optimizer_2], [lr_scheduler]
+
+    model = TestModel()
+    model.training_step_end = None
+    model.training_epoch_end = None
+    trainer = Trainer(
+        default_root_dir=os.getcwd(),
+        limit_train_batches=8,
+        limit_val_batches=1,
+        max_epochs=1,
+        weights_summary=None,
+        automatic_optimization=False)
+    trainer.fit(model)
+
+    assert len(mock_sgd_step.mock_calls) == 2
+    assert len(mock_adam_step.mock_calls) == 8
+
+
+@patch("torch.optim.Adam.step")
+@patch("torch.optim.SGD.step")
+def test_lightning_optimizer_manual_optimization_and_accumulated_gradients(mock_sgd_step, mock_adam_step, tmpdir):
+    """
+    Test that the user can use our LightningOptimizer. Not recommended.
+    """
+    class TestModel(BoringModel):
+
+        def training_step(self, batch, batch_idx, optimizer_idx=None):
+            (opt_1, opt_2) = self.optimizers()
+            assert isinstance(opt_1, LightningOptimizer)
+            assert isinstance(opt_2, LightningOptimizer)
+
+            output = self.layer(batch)
+            loss_1 = self.loss(batch, output)
+            opt_1.backward(loss_1)
+            opt_1.step(idx="1")
+
+            def closure():
+                output = self.layer(batch)
+                loss_2 = self.loss(batch, output)
+                opt_2.backward(loss_2)
+            opt_2.step(closure=closure, idx="2")
+
+        def configure_optimizers(self):
+            optimizer_1 = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+            optimizer_2 = torch.optim.Adam(self.layer.parameters(), lr=0.1)
+            optimizer_1 = LightningOptimizer(self.trainer, optimizer_1)
+
+            # accumulated_gradients at 4
+            optimizer_1.accumulate_grad_batches = 4
+
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_1, step_size=1)
+            return [optimizer_1, optimizer_2], [lr_scheduler]
+
+    model = TestModel()
+    model.training_step_end = None
+    model.training_epoch_end = None
+    trainer = Trainer(
+        default_root_dir=os.getcwd(),
+        limit_train_batches=8,
+        limit_val_batches=1,
+        max_epochs=1,
+        weights_summary=None,
+        automatic_optimization=False,
+        accumulate_grad_batches=2)
+    trainer.fit(model)
+
+    assert len(mock_sgd_step.mock_calls) == 2
+    assert len(mock_adam_step.mock_calls) == 4
