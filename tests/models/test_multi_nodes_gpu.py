89155a8912d808ad3ac43ca02b6583f325062112
tchaton <thomas@grid.ai>
2020-11-16 18:13:46 +0000

    i

--- tests/models/test_multi_nodes_gpu.py
+++ tests/models/test_multi_nodes_gpu.py
@@ -26,7 +26,7 @@ from tests.utilities.dist import call_training_script
 
 
 @pytest.mark.parametrize('cli_args', [
-    pytest.param('--max_epochs 1 --num_nodes 2 --gpus 1 --distributed_backend ddp'),
+    pytest.param('--max_epochs 1 --num_nodes 1 --gpus 1 --distributed_backend ddp'),
 ])
 def test_multi_gpu_model_ddp_fit_only(tmpdir, cli_args):
     # call the script
@@ -38,66 +38,3 @@ def test_multi_gpu_model_ddp_fit_only(tmpdir, cli_args):
 
     # verify the file wrote the expected outputs
     assert result['status'] == 'complete'
-
-
-def test_multi_gpu_early_stop_ddp_spawn(tmpdir):
-    """Make sure DDP works. with early stopping"""
-    tutils.set_random_master_port()
-
-    trainer_options = dict(
-        default_root_dir=tmpdir,
-        callbacks=[EarlyStopping()],
-        max_epochs=50,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        gpus=1,
-        distributed_backend='ddp_spawn',
-        num_nodes=2
-    )
-
-    model = EvalModelTemplate()
-    tpipes.run_model_test(trainer_options, model)
-
-
-def test_multi_gpu_model_ddp_spawn(tmpdir):
-    tutils.set_random_master_port()
-
-    trainer_options = dict(
-        default_root_dir=tmpdir,
-        max_epochs=1,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        gpus=1,
-        distributed_backend='ddp_spawn',
-        num_nodes=2,
-        progress_bar_refresh_rate=0,
-    )
-
-    model = EvalModelTemplate()
-
-    tpipes.run_model_test(trainer_options, model)
-
-    # test memory helper functions
-    memory.get_memory_profile('min_max')
-
-
-def test_ddp_all_dataloaders_passed_to_fit(tmpdir):
-    """Make sure DDP works with dataloaders passed to fit()"""
-    tutils.set_random_master_port()
-
-    model = EvalModelTemplate()
-    fit_options = dict(train_dataloader=model.train_dataloader(),
-                       val_dataloaders=model.val_dataloader())
-
-    trainer = Trainer(
-        default_root_dir=tmpdir,
-        progress_bar_refresh_rate=0,
-        max_epochs=1,
-        limit_train_batches=0.2,
-        limit_val_batches=0.2,
-        gpus=1,
-        distributed_backend='ddp_spawn',
-        num_nodes=2
-    )
-    result = trainer.fit(model, **fit_options)
-    assert result == 1, "DDP doesn't work with dataloaders passed to fit()."
