b26be4745807f3297f59a6c0baeadf2290c2e184
deng-cy <40417707+deng-cy@users.noreply.github.com>
2020-11-16 22:15:08 -0500

    Update iou.py

--- pytorch_lightning/metrics/classification/iou.py
+++ pytorch_lightning/metrics/classification/iou.py
@@ -13,9 +13,8 @@
 # limitations under the License.
 from typing import Any, Optional
 import torch
-from pytorch_lightning.metrics.metric import ConfusionMatrix
 from pytorch_lightning.metrics.functional.reduction import reduce
-from pytorch_lightning.metrics.functional.confusion_matrix import _confusion_matrix_compute
+from pytorch_lightning.metrics.functional.confusion_matrix import _confusion_matrix_compute, ConfusionMatrix
 
 
 class Iou(ConfusionMatrix):
