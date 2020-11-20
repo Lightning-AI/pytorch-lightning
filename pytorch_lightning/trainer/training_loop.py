79b42752a8a1e9b3a0df28c2c28e0e974444445c
Swetha Mandava <smandava@nvidia.com>
2020-11-20 12:16:48 -0800

    more readable

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -276,10 +276,11 @@ class TrainLoop:
 
             # decide if we need to reduce at the end of the epoch automatically
             auto_reduce_tng_result = isinstance(sample_output, Result) and sample_output.should_reduce_on_epoch_end
+            hook_overridden = (is_overridden("training_epoch_end", model=self.trainer.get_model())
+                or is_overridden("on_train_epoch_end", model=self.trainer.get_model()))
+
             # only track when a) it needs to be autoreduced OR b) the user wants to manually reduce on epoch end
-            if not(is_overridden("training_epoch_end", model=self.trainer.get_model())
-                or not(is_overridden("on_train_epoch_end", model=self.trainer.get_model()))
-                or auto_reduce_tng_result):
+            if not(hook_not_overridden or auto_reduce_tng_result):
                 continue
 
             # with 1 step (no tbptt) don't use a sequence at epoch end
