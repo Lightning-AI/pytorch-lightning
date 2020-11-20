7745541380e25b43f702510418227fd761076ece
SeanNaren <sean@grid.ai>
2020-11-20 17:00:49 +0000

    Ensure seed does not exist before tests start

--- tests/utilities/test_seed.py
+++ tests/utilities/test_seed.py
@@ -10,6 +10,8 @@ def test_seed_stays_same_with_multiple_seed_everything_calls():
     Ensure that after the initial seed everything,
     the seed stays the same for the same run.
     """
+    if "PL_GLOBAL_SEED" in os.environ:
+        del os.environ["PL_GLOBAL_SEED"]
 
     with pytest.warns(UserWarning, match="No correct seed found"):
         seed_utils.seed_everything()
@@ -28,6 +30,8 @@ def test_correct_seed_with_environment_variable(monkeypatch):
     """
     Ensure that the PL_GLOBAL_SEED environment is read
     """
+    if "PL_GLOBAL_SEED" in os.environ:
+        del os.environ["PL_GLOBAL_SEED"]
     expected = 2020
     monkeypatch.setenv("PL_GLOBAL_SEED", str(expected))
     assert seed_utils.seed_everything() == expected
@@ -38,6 +42,8 @@ def test_invalid_seed(monkeypatch):
     """
     Ensure that we still fix the seed even if an invalid seed is given
     """
+    if "PL_GLOBAL_SEED" in os.environ:
+        del os.environ["PL_GLOBAL_SEED"]
     expected = 123
     monkeypatch.setenv("PL_GLOBAL_SEED", "invalid")
     monkeypatch.setattr(seed_utils, "_select_seed_randomly", lambda *_: expected)
@@ -52,6 +58,8 @@ def test_out_of_bounds_seed(monkeypatch, seed):
     """
     Ensure that we still fix the seed even if an out-of-bounds seed is given
     """
+    if "PL_GLOBAL_SEED" in os.environ:
+        del os.environ["PL_GLOBAL_SEED"]
     expected = 123
     monkeypatch.setattr(seed_utils, "_select_seed_randomly", lambda *_: expected)
     with pytest.warns(UserWarning, match="is not in bounds"):
