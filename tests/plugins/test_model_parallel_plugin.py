c5f56bf26b39b79d3588671e83d1f7961b8d8413
tchaton <thomas@grid.ai>
2020-11-20 12:24:33 +0000

    typo

--- tests/plugins/test_model_parallel_plugin.py
+++ tests/plugins/test_model_parallel_plugin.py
@@ -106,7 +106,7 @@ class SequentialModel(LightningModule):
 
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-@DDPLauncher.run("--distributed_backend dpp --gpus 2")
+@DDPLauncher.run("--distributed_backend ddp --gpus 2")
 def test_model_parallel_plugin(tmpdir, args=None):
 
     model = SequentialModel()
