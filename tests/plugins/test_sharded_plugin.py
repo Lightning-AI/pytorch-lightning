99326088bbd58b1fb33a0368e9ecfc8656645b73
SeanNaren <sean@grid.ai>
2020-11-25 15:38:54 +0000

    Add additional test cases

--- tests/plugins/test_sharded_plugin.py
+++ tests/plugins/test_sharded_plugin.py
@@ -154,6 +154,35 @@ def test_ddp_sharded_plugin_checkpoint_multi_gpu(tmpdir):
         assert torch.equal(ddp_param, shard_param)
 
 
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.skipif(platform.system() == "Windows",
+                    reason="Distributed training is not supported on Windows")
+@pytest.mark.skipif(not FAIRSCALE_AVAILABLE, reason="Fairscale is not available")
+def test_ddp_sharded_plugin_finetune(tmpdir):
+    """
+        Test to ensure that we can save and restart training (simulate fine-tuning)
+    """
+    model = BoringModel()
+    trainer = Trainer(
+        callbacks=[ModelCheckpoint(dirpath=tmpdir, save_last=True)],
+        gpus=2,
+        accelerator='ddp_spawn',
+        plugins=[DDPShardedPlugin()],
+        fast_dev_run=True,
+    )
+    trainer.fit(model)
+
+    checkpoint_path = glob.glob(os.path.join(tmpdir, "*.ckpt"))[0]
+    saved_model = BoringModel.load_from_checkpoint(checkpoint_path)
+
+    trainer = Trainer(
+        callbacks=[ModelCheckpoint(dirpath=tmpdir, save_last=True)],
+        fast_dev_run=True,
+    )
+    trainer.fit(saved_model)
+    return 1
+
+
 @pytest.mark.skipif(platform.system() == "Windows",
                     reason="Distributed training is not supported on Windows")
 @pytest.mark.skipif(not FAIRSCALE_AVAILABLE, reason="Fairscale is not available")
@@ -306,6 +335,42 @@ def test_ddp_sharded_plugin_correctness_amp_multi_gpu():
     run_sharded_correctness(gpus=2, precision=16, accelerator='ddp_spawn')
 
 
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.skipif(platform.system() == "Windows",
+                    reason="Distributed training is not supported on Windows")
+@pytest.mark.skipif(not FAIRSCALE_AVAILABLE,
+                    reason="Fairscale is not available")
+def test_ddp_sharded_plugin_correctness_multi_gpu_multi_optim():
+    """
+        Ensures same results using multiple optimizers across multiple GPUs
+    """
+    run_sharded_correctness(
+        gpus=2,
+        accelerator='ddp_spawn',
+        model_cls=TestMultipleOptimizersModel,
+        max_percent_speed_regression=0.5  # multiple optimizers sharded across only two GPUs is costly.
+    )
+
+
+@pytest.mark.skip(reason="Currently DDP manual optimization is broken due to no reduce within training step.")
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.skipif(platform.system() == "Windows",
+                    reason="Distributed training is not supported on Windows")
+@pytest.mark.skipif(not FAIRSCALE_AVAILABLE,
+                    reason="Fairscale is not available")
+@mock.patch.dict(os.environ, {"PL_DEV_DEBUG": "1"})
+def test_ddp_sharded_plugin_correctness_multi_gpu_multi_optim_manual(tmpdir):
+    """
+        Ensures using multiple optimizers across multiple GPUs with manual optimization
+    """
+    run_sharded_correctness(
+        gpus=2,
+        accelerator='ddp_spawn',
+        model_cls=TestManualModel,
+        max_percent_speed_regression=0.5  # multiple optimizers sharded across only two GPUs is costly.
+    )
+
+
 class TestModel(BoringModel):
     """
         Overrides training loader to ensure we enforce the same seed for all DDP processes.
@@ -314,6 +379,56 @@ class TestModel(BoringModel):
     def train_dataloader(self):
         seed_everything(42)
         return torch.utils.data.DataLoader(RandomDataset(32, 64))
+
+
+class TestManualModel(TestModel):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        # manual
+        (opt_a, opt_b) = self.optimizers()
+        loss_1 = self.step(batch)
+
+        self.manual_backward(loss_1, opt_a)
+        self.manual_optimizer_step(opt_a)
+
+        # fake discriminator
+        loss_2 = self.step(batch[0])
+
+        # ensure we forward the correct params to the optimizer
+        # without retain_graph we can't do multiple backward passes
+        self.manual_backward(loss_2, opt_b, retain_graph=True)
+        self.manual_backward(loss_2, opt_a, retain_graph=True)
+        self.manual_optimizer_step(opt_b)
+
+        assert self.layer.weight.grad is None or torch.all(self.layer.weight.grad == 0)
+
+    def training_epoch_end(self, outputs) -> None:
+        # outputs should be an array with an entry per optimizer
+        assert len(outputs) == 2
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        optimizer_2 = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        return optimizer, optimizer_2
+
+    @property
+    def automatic_optimization(self) -> bool:
+        return False
+
+
+class TestMultipleOptimizersModel(TestModel):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        output = self.layer(batch)
+        loss = self.loss(batch, output)
+        return {"loss": loss}
+
+    def training_epoch_end(self, outputs) -> None:
+        # outputs should be an array with an entry per optimizer
+        assert len(outputs) == 2
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        optimizer_2 = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        return optimizer, optimizer_2
 
 
 def record_ddp_fit_model_stats(trainer, model, gpus):
@@ -349,7 +464,8 @@ def run_sharded_correctness(
         accelerator='ddp_spawn',
         gpus=0,
         precision=32,
-        max_percent_speed_regression=0.1):
+        max_percent_speed_regression=0.2,
+        model_cls=TestModel):
     """
         Ensures that the trained model is identical to the standard DDP implementation.
         Also checks for speed/memory regressions, we should expect always less memory but performance to fluctuate.
@@ -358,15 +474,17 @@ def run_sharded_correctness(
         gpus: Number of GPUS to enable.
         precision: Whether to use AMP or normal FP32 training.
         max_percent_speed_regression: The maximum speed regression compared to normal DDP training.
-        This is more a safety net for CI which can vary in speed.
+        This is more a safety net for variability in CI which can vary in speed, not for benchmarking.
+        model_cls: Model class to use for test.
 
     """
 
     # Train normal DDP
     seed_everything(42)
-    ddp_model = TestModel()
+    ddp_model = model_cls()
 
     trainer = Trainer(
+        fast_dev_run=True,
         max_epochs=1,
         gpus=gpus,
         precision=precision,
@@ -381,9 +499,10 @@ def run_sharded_correctness(
 
     # Reset and train sharded DDP
     seed_everything(42)
-    sharded_model = TestModel()
+    sharded_model = model_cls()
 
     trainer = Trainer(
+        fast_dev_run=True,
         max_epochs=1,
         gpus=gpus,
         precision=precision,
