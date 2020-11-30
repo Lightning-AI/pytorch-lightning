856b277c959ddd8192446c785553a894ea94ef8f
Jirka Borovec <Borda@users.noreply.github.com>
2020-11-30 15:05:21 +0100

    Apply suggestions from code review

--- pl_examples/domain_templates/imagenet.py
+++ pl_examples/domain_templates/imagenet.py
@@ -72,9 +72,9 @@ class ImageNetLightningModel(LightningModule):
     def training_step(self, batch, batch_idx):
         images, target = batch
         output = self(images)
-        loss_val = F.cross_entropy(output, target)
+        loss_train= F.cross_entropy(output, target)
         acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
-        self.log('train_loss', loss_val, on_step=True, on_epoch=True, logger=True)
+        self.log('train_loss', loss_train, on_step=True, on_epoch=True, logger=True)
         self.log('train_acc1', acc1, on_step=True, prog_bar=True, on_epoch=True, logger=True)
         self.log('train_acc5', acc5, on_step=True, on_epoch=True, logger=True)
         return loss_val
