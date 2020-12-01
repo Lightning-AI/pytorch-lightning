b131d6af9948c69bb4e4ab19b7f8d9799d03ec69
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-02 00:58:31 +0100

    env

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,9 +8,10 @@ TEST_ROOT = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.dirname(TEST_ROOT)
 TEMP_PATH = os.path.join(PROJECT_ROOT, 'test_temp')
 
-ENV = os.environ.copy()
-ENV['PYTHONPATH'] = f'{pytorch_lightning.__file__}:{ENV.get("PYTHONPATH", "")}'
-os.environ = ENV
+if pytorch_lightning.__file__ not in os.getenv('PYTHONPATH'):
+    ENV = os.environ.copy()
+    ENV['PYTHONPATH'] = f'{pytorch_lightning.__file__}:{ENV.get("PYTHONPATH", "")}'
+    os.environ = ENV
 
 # generate a list of random seeds for each test
 RANDOM_PORTS = list(np.random.randint(12000, 19000, 1000))
