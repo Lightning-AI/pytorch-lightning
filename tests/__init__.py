2a2014a3ee693e6072254965b23b808759087dbd
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-02 00:44:08 +0100

    env

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,14 +2,15 @@ import os
 
 import numpy as np
 
+import pytorch_lightning
+
 TEST_ROOT = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.dirname(TEST_ROOT)
 TEMP_PATH = os.path.join(PROJECT_ROOT, 'test_temp')
 
-OS_PYTHONPATH = os.getenv('PYTHONPATH', default='')
-if PROJECT_ROOT not in OS_PYTHONPATH:
-    OS_PYTHONPATH += ';' + PROJECT_ROOT if OS_PYTHONPATH else PROJECT_ROOT
-    os.putenv('PYTHONPATH', OS_PYTHONPATH)
+ENV = os.environ.copy()
+ENV['PYTHONPATH'] = f'{pytorch_lightning.__file__}:{ENV.get("PYTHONPATH", "")}'
+os.environ = ENV
 
 # generate a list of random seeds for each test
 RANDOM_PORTS = list(np.random.randint(12000, 19000, 1000))
