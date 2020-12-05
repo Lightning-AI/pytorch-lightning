0c2b4b13edecbbaaf84f61f1db28ea6af85acbd2
Mikkel Antonsen <mikkel.antonsen@stingray.no>
2020-12-05 15:29:25 +0100

    Lifting this AverageMeter code wholesale from pytorch/tnt repo instead of adding an additional dependency to the project.

--- pytorch_lightning/trainer/supporters.py
+++ pytorch_lightning/trainer/supporters.py
@@ -19,6 +19,7 @@ import fsspec
 import torch
 from pytorch_lightning.utilities.cloud_io import get_filesystem
 from torch import Tensor
+import numpy as np
 
 
 class TensorRunningAccum(object):
@@ -177,3 +178,42 @@ class PredictionCollection(object):
             # Write predictions for current file to disk
             with fs.open(filepath, "wb") as fp:
                 torch.save(outputs, fp)
+
+
+class AverageValueMeter:
+    def __init__(self):
+        super(AverageValueMeter, self).__init__()
+        self.reset()
+        self.val = 0
+
+    def add(self, value, n=1):
+        self.val = value
+        self.sum += value * n
+        if n <= 0:
+            raise ValueError("Cannot use a non-positive weight for the running stat.")
+        elif self.n == 0:
+            self.mean = 0.0 + value  # This is to force a copy in torch/numpy
+            self.std = np.inf
+            self.mean_old = self.mean
+            self.m_s = 0.0
+        else:
+            self.mean = self.mean_old + n * (value - self.mean_old) / float(self.n + n)
+            self.m_s += n * (value - self.mean_old) * (value - self.mean)
+            self.mean_old = self.mean
+            self.std = np.sqrt(self.m_s / (self.n + n - 1.0))
+        self.var = self.std ** 2
+
+        self.n += n
+
+    def value(self):
+        return self.mean, self.std
+
+    def reset(self):
+        self.n = 0
+        self.sum = 0.0
+        self.var = 0.0
+        self.val = 0.0
+        self.mean = np.nan
+        self.mean_old = 0.0
+        self.m_s = 0.0
+        self.std = np.nan
