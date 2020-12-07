4a3686b3009b82036a8127b12ac282d8f7fccfec
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-07 17:46:30 +0100

    rls

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -1,6 +1,6 @@
 """Root package info."""
 
-__version__ = '1.1.0a5'
+__version__ = '1.1.0a6'
 __author__ = 'William Falcon et al.'
 __author_email__ = 'waf2107@columbia.edu'
 __license__ = 'Apache-2.0'
