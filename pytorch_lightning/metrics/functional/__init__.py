c3efe7a2022d2e0ad6ec3f973e144e5c99ce75e8
Teddy Koker <teddy.koker@gmail.com>
2020-12-10 11:24:01 -0500

    add back *deprecated* metric utility functions to functional

--- pytorch_lightning/metrics/functional/__init__.py
+++ pytorch_lightning/metrics/functional/__init__.py
@@ -38,3 +38,5 @@ from pytorch_lightning.metrics.functional.psnr import psnr
 from pytorch_lightning.metrics.functional.roc import roc
 from pytorch_lightning.metrics.functional.self_supervised import embedding_similarity
 from pytorch_lightning.metrics.functional.ssim import ssim
+
+from pytorch_lightning.metrics.functional.deprecated import to_categorical, to_onehot
---
+++ pytorch_lightning/metrics/functional/deprecated.py
@@ -0,0 +1,21 @@
+from pytorch_lightning.metrics.utils import to_categorical as new_to_categorical
+from pytorch_lightning.metrics.utils import to_onehot as new_to_onehot
+
+import warnings
+
+
+# TODO: remove in 1.1.1
+def to_onehot(*args, **kwargs):
+    warnings.warn(
+        "pytorch_lightning.metrics.functional.to_onehot is deprecated please use pytorch_lightning.metrics.utils.to_onehot",
+        DeprecationWarning,
+    )
+    return new_to_onehot(*args, **kwargs)
+
+# TODO: remove in 1.1.1
+def to_categorical(*args, **kwargs):
+    warnings.warn(
+        "pytorch_lightning.metrics.functional.to_categorical is deprecated please use pytorch_lightning.metrics.utils.to_categorical",
+        DeprecationWarning,
+    )
+    return new_to_categorical(*args, **kwargs)
