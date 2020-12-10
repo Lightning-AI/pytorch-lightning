1a9fa268f68a3d6e47ef6c8757f4e0d28ffc2196
Teddy Koker <teddy.koker@gmail.com>
2020-12-10 11:34:33 -0500

    pep

--- pytorch_lightning/metrics/functional/deprecated.py
+++ pytorch_lightning/metrics/functional/deprecated.py
@@ -7,7 +7,10 @@ import warnings
 # TODO: remove in 1.1.1
 def to_onehot(*args, **kwargs):
     warnings.warn(
-        "pytorch_lightning.metrics.functional.to_onehot is deprecated please use pytorch_lightning.metrics.utils.to_onehot",
+        (
+            "pytorch_lightning.metrics.functional.to_onehot is deprecated, "
+            "please use pytorch_lightning.metrics.utils.to_onehot"
+        ),
         DeprecationWarning,
     )
     return new_to_onehot(*args, **kwargs)
@@ -16,7 +19,10 @@ def to_onehot(*args, **kwargs):
 # TODO: remove in 1.1.1
 def to_categorical(*args, **kwargs):
     warnings.warn(
-        "pytorch_lightning.metrics.functional.to_categorical is deprecated please use pytorch_lightning.metrics.utils.to_categorical",
+        (
+            "pytorch_lightning.metrics.functional.to_categorical is deprecated, "
+            "please use pytorch_lightning.metrics.utils.to_categorical"
+        ),
         DeprecationWarning,
     )
     return new_to_categorical(*args, **kwargs)
