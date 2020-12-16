33e14b7adbecffe85e21eb918963e97d9fb2b8c1
tchaton <thomas@grid.ai>
2020-12-16 08:23:03 +0100

    resolve flake8

--- tests/trainer/optimization/test_multiple_optimizers.py
+++ tests/trainer/optimization/test_multiple_optimizers.py
@@ -60,4 +60,4 @@ def test_unbalanced_logging_with_multiple_optimizers(tmpdir):
     assert torch.equal(trainer.callback_metrics["loss_1_step"], model.loss_1[-1])
     # test loss are properly reduced
     assert torch.abs(trainer.callback_metrics["loss_2_epoch"] - torch.FloatTensor(model.loss_2).mean()) < 1e-6
-    assert torch.abs(trainer.callback_metrics["loss_1_epoch"] - torch.FloatTensor(model.loss_1).mean())  < 1e-6
+    assert torch.abs(trainer.callback_metrics["loss_1_epoch"] - torch.FloatTensor(model.loss_1).mean()) < 1e-6
