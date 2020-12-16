3e51408c26be172b75fd3daf9b9e097c960d07ff
Rohit Gupta <rohitgr1998@gmail.com>
2020-12-16 11:19:17 +0530

    fix rebase

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import threading
 from functools import partial, wraps
 from http.server import SimpleHTTPRequestHandler
 import sys
