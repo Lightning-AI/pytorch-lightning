61b299cb08718aa63357e149aea18855b35fcd0d
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-17 17:16:05 +0100

    ddp as extra script

--- pl_examples/test_examples.py
+++ pl_examples/test_examples.py
@@ -38,10 +38,10 @@ ARGS_DP = ARGS_DEFAULT + """
 --accelerator dp \
 """
 
-ARGS_DDP = ARGS_DEFAULT + """
---gpus 2 \
---accelerator ddp_spawn \
-"""
+# ARGS_DDP = ARGS_DEFAULT + """
+# --gpus 2 \
+# --accelerator ddp_spawn \
+# """
 
 ARGS_AMP = """
 --precision 16 \
@@ -67,21 +67,21 @@ def test_examples_dp(tmpdir, import_cli, cli_args):
         module.cli_main()
 
 
-@pytest.mark.parametrize('import_cli', [
-    'pl_examples.basic_examples.simple_image_classifier',
-    'pl_examples.basic_examples.backbone_image_classifier',
-    'pl_examples.basic_examples.autoencoder',
-])
-@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-@pytest.mark.parametrize('cli_args', [ARGS_DDP, ARGS_DDP + ARGS_AMP])
-def test_examples_ddp(tmpdir, import_cli, cli_args):
-
-    module = importlib.import_module(import_cli)
-    # update the temp dir
-    cli_args = cli_args % {'tmpdir': tmpdir}
-
-    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
-        module.cli_main()
+# @pytest.mark.parametrize('import_cli', [
+#     'pl_examples.basic_examples.simple_image_classifier',
+#     'pl_examples.basic_examples.backbone_image_classifier',
+#     'pl_examples.basic_examples.autoencoder',
+# ])
+# @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+# @pytest.mark.parametrize('cli_args', [ARGS_DDP, ARGS_DDP + ARGS_AMP])
+# def test_examples_ddp(tmpdir, import_cli, cli_args):
+#
+#     module = importlib.import_module(import_cli)
+#     # update the temp dir
+#     cli_args = cli_args % {'tmpdir': tmpdir}
+#
+#     with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
+#         module.cli_main()
 
 
 @pytest.mark.parametrize(
