b40220ca219b26bcbcbba616ca27c7ea8a63677b
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-20 10:09:23 +0100

    fix

--- tests/backends/__init__.py
+++ tests/backends/__init__.py
@@ -1,10 +1,7 @@
-from functools import wraps
-
 try:
     from dtrun.launcher import DDPLauncher
 except ImportError:
     class DDPLauncher:
-        @wraps
         def run(cmd_line, **kwargs):
             def inner(func):
                 pass
