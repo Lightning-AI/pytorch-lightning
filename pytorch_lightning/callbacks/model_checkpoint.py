bb6dfb6567fd7e661219fadfd9b1dae53a52962e
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-12-21 09:58:06 +0100

    remove unused rpc import in modelcheckpoint causing import error (#5198)
    * remove unused rpc import
    
    * isort
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -34,7 +34,6 @@ import yaml
 from pytorch_lightning import _logger as log
 from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.metrics.metric import Metric
-from pytorch_lightning.plugins.rpc_plugin import RPCPlugin
 from pytorch_lightning.utilities import rank_zero_info, rank_zero_only, rank_zero_warn
 from pytorch_lightning.utilities.cloud_io import get_filesystem
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
