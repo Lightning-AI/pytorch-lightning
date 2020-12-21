273c067e72c7a2656620397b65b9e1b360a46ee9
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-21 18:17:08 +0100

    generate

--- legacy/generate_checkpoints.sh
+++ legacy/generate_checkpoints.sh
@@ -5,8 +5,31 @@ VERSIONS=("1.0.0" "1.0.1" "1.0.2")
 LEGACY_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo $LEGACY_PATH
+# install some PT version here so it does not need to reinstalled for each env
+pip install virtualenv "torch==1.5" --quiet --no-cache-dir
+
+ENV_PATH="$LEGACY_PATH/vEnv"
 
 for ver in "${VERSIONS[@]}"
 do
-	echo "---$ver---"
+	echo "processing version: $ver"
+	# mkdir "$LEGACY_PATH/$ver"
+
+  # create local env
+  echo $ENV_PATH
+  virtualenv $ENV_PATH --system-site-packages
+  # activate and install PL version
+  source "$ENV_PATH/bin/activate"
+  pip install "pytorch_lightning==$ver" --quiet -U --no-cache-dir
+
+  python --version
+  pip --version
+  pip list | grep torch
+
+  python "$LEGACY_PATH/zero_training.py"
+
+  deactivate
+  # clear env
+  rm -rf $ENV_PATH
+
 done
