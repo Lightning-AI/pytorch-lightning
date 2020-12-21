fd5322d3e723cec0bc40cf29b7fe17f7ed632e69
Rohit Gupta <rohitgr1998@gmail.com>
2020-12-21 06:30:17 +0530

    Update warning if ckpt directory is not empty (#5209)

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -20,11 +20,11 @@ Automatically save model checkpoints during training.
 
 """
 
+from copy import deepcopy
 import numbers
 import os
-import re
-from copy import deepcopy
 from pathlib import Path
+import re
 from typing import Any, Dict, Optional, Union
 
 import numpy as np
@@ -271,8 +271,7 @@ class ModelCheckpoint(Callback):
             and len(self._fs.ls(dirpath)) > 0
         ):
             rank_zero_warn(
-                f"Checkpoint directory {dirpath} exists and is not empty. With save_top_k={save_top_k},"
-                " all files in this directory will be deleted when a checkpoint is saved!"
+                f"Checkpoint directory {dirpath} exists and is not empty."
             )
 
         if dirpath and self._fs.protocol == 'file':
