38c7cb457370cabcc84192abf4aaf630e45d4581
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-21 20:29:01 +0100

    1.4

--- pytorch_lightning/utilities/xla_device_utils.py
+++ pytorch_lightning/utilities/xla_device_utils.py
@@ -1,6 +1,6 @@
 from warnings import warn
 
-warn("`xla_device_utils` package has been renamed to `xla_device` since v1.2 and will be removed in v1.3",
+warn("`xla_device_utils` package has been renamed to `xla_device` since v1.2 and will be removed in v1.4",
      DeprecationWarning)
 
 from pytorch_lightning.utilities.xla_device import *  # noqa: F403 E402 F401
