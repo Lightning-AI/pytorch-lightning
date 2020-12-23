50732878bed3a383105049fc9c3630e9d985eb15
Jirka Borovec <Borda@users.noreply.github.com>
2020-12-23 10:55:54 +0100

    Apply suggestions from code review

--- tests/checkpointing/test_legacy_checkpoints.py
+++ tests/checkpointing/test_legacy_checkpoints.py
@@ -24,6 +24,7 @@ LEGACY_CHECKPOINTS_PATH = os.path.join(LEGACY_PATH, 'checkpoints')
 CHECKPOINT_EXTENSION = ".ckpt"
 
 
+# todo: add more legacy checkpoints :]
 @pytest.mark.parametrize("pl_version", [
     "0.10.0", "1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5", "1.0.6", "1.0.7", "1.0.8"
 ])
