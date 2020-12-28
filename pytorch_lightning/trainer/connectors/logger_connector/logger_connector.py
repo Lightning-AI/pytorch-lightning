85ea319a6a08129244f2e02154dd534912134c04
chaton <thomas@grid.ai>
2020-12-28 15:34:18 +0100

    Trainer.test should return only test metrics (#5214)
    * resolve bug
    
    * merge tests

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from copy import deepcopy
+import os
 from pprint import pprint
 from typing import Dict, Iterable, Union
 
