023dab436302b86735a4bb8effb447194125832f
lucadiliello <luca.diliello@unitn.it>
2020-12-31 17:32:41 +0100

    added double space before '# noqa: F401' flag

--- pytorch_lightning/metrics/retrieval/__init__.py
+++ pytorch_lightning/metrics/retrieval/__init__.py
@@ -11,5 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from pytorch_lightning.metrics.retrieval.retrieval_metric import RetrievalMetric # noqa: F401
-from pytorch_lightning.metrics.retrieval.mean_average_precision import RetrievalMAP # noqa: F401
+from pytorch_lightning.metrics.retrieval.retrieval_metric import RetrievalMetric  # noqa: F401
+from pytorch_lightning.metrics.retrieval.mean_average_precision import RetrievalMAP  # noqa: F401
