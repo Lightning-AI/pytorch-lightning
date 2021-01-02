73244d435d650570ebb38440c19467722936ef5b
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-02 11:38:56 +0100

    .

--- tests/base/model_train_steps.py
+++ tests/base/model_train_steps.py
@@ -34,16 +34,16 @@ class TrainingStepVariations(ABC):
         y_hat = self(x)
 
         # calculate loss
-        loss_val = self.loss(y, y_hat)
-        log_val = loss_val
+        loss_train = self.loss(y, y_hat)
+        log_train = loss_train
 
         # alternate between tensors and scalars for "log" and "progress_bar"
         if batch_idx % 2 == 0:
-            log_val = log_val.item()
+            log_train = log_train.item()
 
-        self.log('some_val', log_val * log_val, prog_bar=True, logger=False)
-        self.log('train_some_val', log_val * log_val)
-        return loss_val
+        self.log('some_val', log_train * log_train, prog_bar=True, logger=False)
+        self.log('train_some_val', log_train * log_train)
+        return loss_train
 
     def training_step__inf_loss(self, batch, batch_idx, optimizer_idx=None):
         output = self.training_step(batch, batch_idx, optimizer_idx)
@@ -54,21 +54,22 @@ class TrainingStepVariations(ABC):
                 output /= 0
         return output
 
-    def training_step_result_obj_dp(self, batch, batch_idx, optimizer_idx=None):
+    def training_step__result_obj_dp(self, batch, batch_idx, optimizer_idx=None):
+
         # forward pass
         x, y = batch
         x = x.view(x.size(0), -1)
         y_hat = self(x.to(self.device))
 
         # calculate loss
-        loss_val = self.loss(y.to(y_hat.device), y_hat)
-        log_val = loss_val
+        loss_train = self.loss(y.to(y_hat.device), y_hat)
+        log_train = loss_train
 
         # alternate between tensors and scalars for "log" and "progress_bar"
         if batch_idx % 2 == 0:
-            log_val = log_val.item()
+            log_train = log_train.item()
 
-        self.log('some_val', log_val * log_val, prog_bar=True, logger=False)
-        self.log('train_some_val', log_val * log_val)
+        self.log('some_val', log_train * log_train, prog_bar=True, logger=False)
+        self.log('train_some_val', log_train * log_train)
 
-        return loss_val
+        return loss_train
