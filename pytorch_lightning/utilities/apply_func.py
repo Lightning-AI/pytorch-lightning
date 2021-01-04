04924ae9ca0c5e551ef230509031e2672a0099e0
tchaton <thomas@grid.ai>
2021-01-04 18:59:28 +0100

    resolve pep8

--- pytorch_lightning/utilities/apply_func.py
+++ pytorch_lightning/utilities/apply_func.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import importlib
 from abc import ABC
 from collections.abc import Mapping, Sequence
 from copy import copy
