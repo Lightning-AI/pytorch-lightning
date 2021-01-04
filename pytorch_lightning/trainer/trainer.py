1f4f25585e71a5379a52858c70335efe5a3605f7
chaton <thomas@grid.ai>
2021-01-04 21:27:21 +0100

    Update pytorch_lightning/trainer/trainer.py
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -282,7 +282,7 @@ class Trainer(
             enable_pl_optimizer: If True, each optimizer will be wrapped by
                 `pytorch_lightning.core.optimizer.LightningOptimizer`. It allows Lightning to
                 handle AMP, TPU, accumulated_gradients, etc.
-                ... warning:: Currently Deprecated and it will be removed in v1.3
+                .. warning:: Currently deprecated and it will be removed in v1.3
         """
         super().__init__()
         self._device_type = DeviceType.CPU
