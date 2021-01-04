91bf148cb09e87d3672ea254ad57424cb5317913
Nicki Skafte <skaftenicki@gmail.com>
2021-01-04 16:10:44 +0100

    auc class interface

--- pytorch_lightning/metrics/classification/auc.py
+++ pytorch_lightning/metrics/classification/auc.py
@@ -11,4 +11,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Any, Callable, Optional
 
+import torch
+
+from pytorch_lightning.metrics.metric import Metric
+from pytorch_lightning.metrics.functional.auc import _auc_update, _auc_compute
+
+
+class AUC(Metric):
+    r""""""
+
+    def __init__(
+        self,
+        reorder: bool = False,
+        compute_on_step: bool = True,
+        dist_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
+        dist_sync_fn: Callable = None,
+    ):
+        super().__init__(
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+            dist_sync_fn=dist_sync_fn,
+        )
+
+        self.reorder = reorder
+
+        self.add_state("x", default=[], dist_reduce_fx=None)
+        self.add_state("y", default=[], dist_reduce_fx=None)
+
+    def update(self, x: torch.Tensor, y: torch.Tensor):
+        """
+        Update state with predictions and targets.
+
+        Args:
+            preds: Predictions from model (probabilities, or labels)
+            target: Ground truth labels
+        """
+        x, y = _auc_update(x, y)
+
+        self.x.append(x)
+        self.y.append(y)
+
+    def compute(self) -> torch.Tensor:
+        """
+        Computes AUC based on inputs passed in to ``update`` previously.
+        """
+        x = torch.cat(self.x, dim=0)
+        y = torch.cat(self.y, dim=0)
+        return _auc_compute(x, y, reorder=self.reorder)
