dc117670ce6d5aedbbdf4f0f99bdbc5c7b70f7f2
marload <rladhkstn8@gmail.com>
2021-01-05 10:51:42 +0900

    isort

--- tests/callbacks/test_lambda_cb.py
+++ tests/callbacks/test_lambda_cb.py
@@ -11,26 +11,58 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from pytorch_lightning import seed_everything, Trainer
-from pytorch_lightning.callbacks import LambdaCallback
+import inspect
+
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import Callback, LambdaCallback
 from tests.base.boring_model import BoringModel
 
 
 def test_lambda_call(tmpdir):
     seed_everything(42)
 
-    model = BoringModel()
     checker = set()
 
-    callback_dicts = {"setup": lambda *args: checker.add("setup")}
-    test_callback = LambdaCallback(**callback_dicts)
+    hooks = [
+        "setup",
+        "teardown",
+        "on_init_start",
+        "on_init_end",
+        "on_fit_start",
+        "on_fit_end",
+        "on_train_batch_start",
+        "on_train_batch_end",
+        "on_train_epoch_start",
+        "on_train_epoch_end",
+        "on_validation_epoch_start",
+        "on_validation_epoch_end",
+        "on_test_epoch_start",
+        "on_test_epoch_end",
+        "on_epoch_start",
+        "on_epoch_end",
+        "on_batch_start",
+        "on_batch_end",
+        "on_validation_batch_start",
+        "on_validation_batch_end",
+        "on_test_batch_start",
+        "on_test_batch_end",
+        "on_train_start",
+        "on_train_end",
+        "on_test_start",
+        "on_test_end",
+    ]
+    model = BoringModel()
 
-    trainer = Trainer(
-        fast_dev_run=True,
-        callbacks=[test_callback],
-    )
+    hooks_args = {h: (lambda x: lambda *args: checker.add(x))(h) for h in hooks}
+    test_callback = LambdaCallback(**hooks_args)
+
+    trainer = Trainer(default_root_dir=tmpdir, fast_dev_run=True, callbacks=[test_callback])
 
     trainer.fit(model)
+    trainer.test(model)
 
-    for name in ("setup",):
-        assert name in checker
+    print(len(hooks))
+    print(len(checker))
+
+    for h in hooks:
+        assert h in checker
