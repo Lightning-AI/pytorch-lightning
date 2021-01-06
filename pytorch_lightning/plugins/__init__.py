348a1b04efd006a1694b3415ca28d166e0862f68
justusschock <justus.schock@posteo.de>
2021-01-06 18:14:25 +0100

    move old plugins

---
+++ pytorch_lightning/plugins/__init__.py
@@ -0,0 +1,1 @@
+from pytorch_lightning.accelerators.plugins import *
--- pytorch_lightning/trainer/optimizers.py
+++ pytorch_lightning/trainer/optimizers.py
@@ -140,27 +140,6 @@ class TrainerOptimizersMixin(ABC):
                 raise ValueError(f'The provided lr scheduler "{scheduler}" is invalid')
         return lr_schedulers
 
-    def reinit_scheduler_properties(self, optimizers: list, schedulers: list):
-        # Reinitialize optimizer.step properties added by schedulers
-        for scheduler in schedulers:
-            scheduler = scheduler['scheduler']
-
-            for optimizer in optimizers:
-                # check that we dont mix users optimizers and schedulers
-                if scheduler.optimizer == optimizer:
-                    # Find the mro belonging to the base lr scheduler class
-                    for i, mro in enumerate(scheduler.__class__.__mro__):
-                        if mro in (optim.lr_scheduler._LRScheduler, optim.lr_scheduler.ReduceLROnPlateau):
-                            idx = i
-                            state = scheduler.state_dict()
-                        else:
-                            state = None
-
-                scheduler.__class__.__mro__[idx].__init__(scheduler, optimizer)
-                if state is not None:
-                    scheduler.load_state_dict(state)
-
-
 class _MockOptimizer(Optimizer):
     """The `_MockOptimizer` will be used inplace of an optimizer in the event that `None`
     is returned from `configure_optimizers`.
