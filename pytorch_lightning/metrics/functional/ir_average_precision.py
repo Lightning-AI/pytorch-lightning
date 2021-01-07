5893b816e69c22bb602295a3af7c786cd158b6d1
lucadiliello <luca.diliello@unitn.it>
2021-01-07 17:22:42 +0100

    added checks on target inputs

--- pytorch_lightning/metrics/functional/ir_average_precision.py
+++ pytorch_lightning/metrics/functional/ir_average_precision.py
@@ -23,11 +23,11 @@ def retrieval_average_precision(
     `here <https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision>`_.
 
     `preds` and `target` should be of the same shape and live on the same device. If no `target` is ``True``,
-    0 is returned.
+    0 is returned. Target must be of type `bool` or `int`, otherwise an error is raised.
 
     Args:
         preds: estimated probabilities of each document to be relevant.
-        target: ground truth about each document being relevant.
+        target: ground truth about each document being relevant or not. Requires `bool` or `int` tensor.
 
     Return:
         a single-value tensor with the average precision (AP) of the predictions `preds` wrt the labels `target`.
@@ -41,8 +41,16 @@ def retrieval_average_precision(
 
     if preds.shape != target.shape or preds.device != target.device:
         raise ValueError(
-            "`preds` and `target` must have the same shape and be on the same device"
+            "`preds` and `target` must have the same shape and live on the same device"
         )
+
+    if target.dtype not in (torch.bool, torch.int16, torch.int32, torch.int64):
+        raise ValueError(
+            "`target` must be a tensor of booleans or integers"
+        )
+
+    if target.dtype is not torch.bool:
+        target = target.bool()
 
     if target.sum() == 0:
         return torch.tensor(0).to(preds)
