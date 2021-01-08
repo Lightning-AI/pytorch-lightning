1d60a50fb175a22ae5836d96136eb2dac8491311
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-08 14:12:21 +0100

    set find unused params to False

--- pytorch_lightning/plugins/ddp_plugin.py
+++ pytorch_lightning/plugins/ddp_plugin.py
@@ -74,10 +74,6 @@ class DDPPlugin(LightningPlugin):
             the model wrapped in LightningDistributedDataParallel
 
         """
-        # if unset, default `find_unused_parameters` `True`
-        self._ddp_kwargs["find_unused_parameters"] = self._ddp_kwargs.get(
-            "find_unused_parameters", True
-        )
         model = DistributedDataParallel(
             module=LightningDistributedWrapper(model),
             device_ids=device_ids,
