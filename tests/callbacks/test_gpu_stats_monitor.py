6d83d4457d6aec341cd5cc357e7f43244a14a0be
Arnaud Gelas <arnaudgelas@gmail.com>
2021-01-14 19:15:34 +0100

    Fix pre-commit isort failure on tests/callbacks/*.py (#5428)
    * Remove tests.callbacks from skipped module in pyproject.toml
    
    * Fix pre-commit isort failure on tests/callbacks/*.py

--- tests/callbacks/test_gpu_stats_monitor.py
+++ tests/callbacks/test_gpu_stats_monitor.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+
+import numpy as np
 import pytest
 import torch
-import numpy as np
 
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import GPUStatsMonitor
