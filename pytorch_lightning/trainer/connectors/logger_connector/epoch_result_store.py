180327ad0bda02e25405b697cd50734e127e78ce
Carlos Mocholí <carlossmocholi@gmail.com>
2021-01-15 02:25:13 +0100

    Unused import

--- pytorch_lightning/trainer/connectors/logger_connector/epoch_result_store.py
+++ pytorch_lightning/trainer/connectors/logger_connector/epoch_result_store.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from collections import defaultdict
-from enum import Enum
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
