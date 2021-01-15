6c0016db2293cc01d929bf61a123bc4762dc9b68
chaton <thomas@grid.ai>
2021-01-15 11:53:52 +0000

    Update pytorch_lightning/core/lightning.py
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -360,7 +360,7 @@ class LightningModule(
                     return {"y": loss}
 
             results = trainer.test(test_dataloaders=test_dataloaders)
-            for dataloader_idx, dataloader_result in enumerate(results_:
+            for dataloader_idx, dataloader_result in enumerate(results):
                 df = pd.json_normalize(dataloader_result["predictions"], sep='_')
 
         Args:
