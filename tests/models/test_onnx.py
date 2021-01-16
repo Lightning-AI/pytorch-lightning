1b22d5be249652be27d34a510c4f077542150852
rohitgr7 <rohitgr1998@gmail.com>
2021-01-16 18:15:04 +0530

    update tests

--- tests/models/test_onnx.py
+++ tests/models/test_onnx.py
@@ -83,7 +83,9 @@ def test_model_saves_on_multi_gpu(tmpdir):
 
     trainer_options = dict(
         default_root_dir=tmpdir,
-        fast_dev_run=7,
+        max_epochs=1,
+        limit_train_batches=10,
+        limit_val_batches=10,
         gpus=[0, 1],
         accelerator='ddp_spawn',
         progress_bar_refresh_rate=0,
