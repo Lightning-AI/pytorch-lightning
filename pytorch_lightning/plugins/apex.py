2403f96b773a05c050e8275e5bb2f360a9812194
dong.hyun <artit.anthony@gmail.com>
2021-01-20 14:08:44 +0900

    pep8 formatting

--- pytorch_lightning/plugins/apex.py
+++ pytorch_lightning/plugins/apex.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 import torch
 from torch.optim.optimizer import Optimizer
--- pytorch_lightning/plugins/sharded_native_amp_plugin.py
+++ pytorch_lightning/plugins/sharded_native_amp_plugin.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Union, cast
+from typing import cast
 
 from torch.optim import Optimizer
 
