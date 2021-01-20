c724d91638e535bc318f85cafcc6e9e46d13169b
rohitgr7 <rohitgr1998@gmail.com>
2021-01-21 02:02:34 +0530

    dp

--- tests/models/test_restore.py
+++ tests/models/test_restore.py
@@ -433,7 +433,7 @@ def test_dp_resume(tmpdir):
             assert self.trainer.current_epoch == real_global_epoch and self.trainer.current_epoch > 0
 
             dataloader = self.train_dataloader()
-            tpipes.run_prediction(self, dataloader, dp=True)
+            tpipes.run_prediction(self.trainer.model, dataloader, dp=True)
             self.on_train_start_called = True
 
     # new model
