853fb095a0fd69d0c5b392e138b18902205c8320
dong.hyun <artit.anthony@gmail.com>
2021-01-20 15:57:04 +0900

    update test codes

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -970,7 +970,7 @@ def test_gradient_clipping_by_value(tmpdir):
 
     model = EvalModelTemplate()
 
-    grad_clip_val = 0.001
+    grad_clip_val = 0.0001
     trainer = Trainer(
         max_steps=10,
         max_epochs=1,
@@ -990,7 +990,8 @@ def test_gradient_clipping_by_value(tmpdir):
         parameters = model.parameters()
         grad_max_list = [torch.max(p.grad.detach().abs()) for p in parameters]
         grad_max = torch.max(torch.stack(grad_max_list))
-        assert grad_max.item() <= grad_clip_val, f"Gradient max value {grad_max} > grad_clip_val {grad_clip_val} ."
+        assert round(grad_max.item(), 6) <= grad_clip_val, \
+            f"Gradient max value {grad_max} > grad_clip_val {grad_clip_val} ."
 
         return ret_val
 
@@ -1049,7 +1050,7 @@ def test_gradient_clipping_by_value_fp16(tmpdir):
     tutils.reset_seed()
 
     model = EvalModelTemplate()
-    grad_clip_val = 0.001
+    grad_clip_val = 0.0001
     trainer = Trainer(
         max_steps=10,
         max_epochs=1,
@@ -1070,7 +1071,8 @@ def test_gradient_clipping_by_value_fp16(tmpdir):
         ret_val = trainer.train_loop.old_training_step_and_backward(split_batch, batch_idx, opt_idx, optimizer, hiddens)
         parameters = model.parameters()
         grad_max = torch.max(torch.stack([p.grad.detach() for p in parameters]))
-        assert grad_max.item() <= grad_clip_val, f"Gradient max value {grad_max} > grad_clip_val {grad_clip_val} ."
+        assert round(grad_max.item(), 6) <= grad_clip_val, \
+            f"Gradient max value {grad_max} > grad_clip_val {grad_clip_val} ."
 
         return ret_val
 
