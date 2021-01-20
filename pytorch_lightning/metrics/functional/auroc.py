1eefc6ff3734b682dee2162b10dacc2775564f9b
Nicki Skafte <skaftenicki@gmail.com>
2021-01-20 14:17:44 +0100

    fix isort

--- pytorch_lightning/metrics/functional/auroc.py
+++ pytorch_lightning/metrics/functional/auroc.py
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from distutils.version import LooseVersion
 from typing import Optional, Sequence, Tuple
 
 import torch
-from distutils.version import LooseVersion
 
 from pytorch_lightning.metrics.classification.helpers import _input_format_classification
 from pytorch_lightning.metrics.functional.auc import auc
