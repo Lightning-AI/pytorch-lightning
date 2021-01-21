cc1bcf4f42235bc60e1f0f05265252f691da4dee
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-21 10:24:43 +0100

    upgrade docs packages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ release = pytorch_lightning.__version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '2.0'
+needs_sphinx = '3.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
