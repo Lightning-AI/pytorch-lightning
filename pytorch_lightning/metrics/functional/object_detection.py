9c47afe4252974223c20dd2de2df64f62a517ed0
Brian Ko <briankosw@gmail.com>
2021-01-21 20:28:45 +0900

    Decompose pred & target inputs

--- pytorch_lightning/metrics/functional/object_detection.py
+++ pytorch_lightning/metrics/functional/object_detection.py
@@ -15,6 +15,7 @@
 from collections import Counter
 
 import torch
+from torch import Tensor
 
 try:
     from torchvision.ops import box_iou
@@ -23,44 +24,61 @@ except ModuleNotFoundError:  # pragma: no-cover
 
 
 def mean_average_precision(
-        preds: torch.Tensor, target: torch.Tensor, iou_threshold: float, ap_calculation: str
-) -> torch.Tensor:
+    pred_image_indices: Tensor,
+    pred_probs: Tensor,
+    pred_labels: Tensor,
+    pred_bboxes: Tensor,
+    target_image_indices: Tensor,
+    target_labels: Tensor,
+    target_bboxes: Tensor,
+    iou_threshold: float,
+    ap_calculation: str
+) -> Tensor:
     """
     Compute mean average precision for object detection task
 
     Args:
-        preds: an Nx7 batch of predictions with representation
-               ``[image_idx, class_pred, class_prob, x_min, y_min, x_max, y_max]``
-        target: an Nx6 batch of targets with representation
-                ``[image_idx, class_label, x_min, y_min, x_max, y_max]``
+        pred_image_indices: an (N,)-shaped Tensor of image indices of the predictions
+        pred_probs: an (N,)-shaped Tensor of probabilities of the predictions
+        pred_labels: an (N,)-shaped Tensor of predicted labels
+        pred_bboxes: an (N, 4)-shaped Tensor of predicted bounding boxes
+        target_image_indices: an (M,)-shaped Tensor of image indices of the groudn truths
+        target_labels: an (M,)-shaped Tensor of ground truth labels
+        target_bboxes: an (M, 4)-shaped Tensor of ground truth bounding boxes
         iou_threshold: threshold for IoU score for determining true positive and
                        false positive predictions.
-        ap_calculation: one of "step", "VOC2007", "VOC2010", or "COCO"
+        ap_calculation: method to calculate the average precision of the precision-recall curve
+
+            - ``'step'``: calculate the step function integral, the same way as
+            :func:`~pytorch_lightning.metrics.functional.average_precision.average_precision`
+            - ``'VOC2007'``: calculate the 11-point sampling of interpolation of the precision recall curve
+            - ``'VOC2010'``: calculate the step function integral of the interpolated precision recall curve
+            - ``'COCO'``: calculate the 101-point sampling of the interpolated precision recall curve
 
     Returns:
         mean of the average precision for each class in object detection task.
+
     """
     if box_iou is None:
         raise ImportError('`mean_average_precision` metric requires `torchvision`, which is not installed. '
                           ' install it with `pip install torchvision`.')
-    classes = torch.cat([preds[:, 1], target[:, 1]]).unique()
+    classes = torch.cat([pred_labels, target_labels]).unique()
     average_precisions = torch.zeros(len(classes))
     for class_idx, c in enumerate(classes):
-        c_preds = sorted(preds[preds[:, 1] == c], key=lambda x: x[2], reverse=True)
-        c_target = target[target[:, 1] == c]
-        targets_per_images = Counter([t[0].item() for t in c_target])
+        desc_indices = torch.argsort(pred_probs, descending=True)[pred_labels == c]
+        targets_per_images = Counter([idx.item() for idx in target_image_indices[target_labels == c]])
         targets_assigned = {
             image_idx: torch.zeros(count, dtype=torch.bool) for image_idx, count in targets_per_images.items()
         }
-        tps = torch.zeros(len(c_preds))
-        fps = torch.zeros(len(c_preds))
-        if len(c_preds) == 0:
+        if len(desc_indices) == 0:
             continue
-        for i, p in enumerate(c_preds):
-            image_idx = p[0].item()
-            ground_truths = c_target[c_target[:, 0] == image_idx]
-            ious = box_iou(p[None, 3:], ground_truths[:, 2:])
-            best_iou, best_target_idx = ious.squeeze(0).max(0) if len(ground_truths) > 0 else (0, -1)
+        tps = torch.zeros(len(desc_indices), dtype=torch.int)
+        fps = torch.zeros(len(desc_indices), dtype=torch.int)
+        for i, pred_idx in enumerate(desc_indices):
+            image_idx = pred_image_indices[pred_idx].item()
+            gt_bboxes = target_bboxes[(target_image_indices == image_idx) & (target_labels == c)]
+            ious = box_iou(torch.unsqueeze(pred_bboxes[pred_idx], dim=0), gt_bboxes)
+            best_iou, best_target_idx = ious.squeeze(0).max(0) if len(gt_bboxes) > 0 else (0, -1)
             if best_iou > iou_threshold and not targets_assigned[image_idx][best_target_idx]:
                 targets_assigned[image_idx][best_target_idx] = True
                 tps[i] = 1
@@ -68,7 +86,8 @@ def mean_average_precision(
                 fps[i] = 1
         tps_cum, fps_cum = torch.cumsum(tps, dim=0), torch.cumsum(fps, dim=0)
         precision = tps_cum / (tps_cum + fps_cum)
-        recall = tps_cum / len(c_target) if len(c_target) else tps_cum
+        num_targets = len(target_labels[target_labels == c])
+        recall = tps_cum / num_targets if num_targets else tps_cum
         precision = torch.cat([reversed(precision), torch.tensor([1.])])
         recall = torch.cat([reversed(recall), torch.tensor([0.])])
         if ap_calculation == "step":
