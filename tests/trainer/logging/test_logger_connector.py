5775ca823604dcc560fbf9d2d33d651174771e5e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-23 01:45:30 +0100

    warn about duplicate metrics

--- tests/trainer/logging/test_logger_connector.py
+++ tests/trainer/logging/test_logger_connector.py
@@ -425,3 +425,21 @@ def test_epoch_results_cache_dp(tmpdir):
     )
     trainer.fit(model)
     trainer.test(model, ckpt_path=None)
+
+
+def test_logging_to_progress_bar_with_reserved_key(tmpdir):
+    """ Test that logging a metric with a reserved name to the progress bar raises a warning. """
+    class TestModel(BoringModel):
+
+        def training_step(self, *args, **kwargs):
+            output = super().training_step(*args, **kwargs)
+            self.log("loss", output["loss"], prog_bar=True)
+            return output
+
+    model = TestModel()
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        max_steps=2,
+    )
+    with pytest.warns(UserWarning, match="The progress bar already tracks a metric with the name 'loss'"):
+        trainer.fit(model)
