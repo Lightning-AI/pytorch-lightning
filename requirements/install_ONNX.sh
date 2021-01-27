cd1bd24af01bf6b5492dcde3673361081937aef2
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-28 00:51:00 +0100

    whl

--- requirements/install_ONNX.sh
+++ requirements/install_ONNX.sh
@@ -14,28 +14,27 @@ ROOT=$PWD
 # https://github.com/microsoft/onnxruntime/blob/master/BUILD.md
 git clone --recursive https://github.com/Microsoft/onnxruntime
 cd onnxruntime
+export ONNX_ML=1
+pip install setuptools wheel
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ./build.sh --config RelWithDebInfo --build_shared_lib --parallel
+    ./build.sh --config RelWithDebInfo --build_shared_lib --build_wheel --parallel
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    ./build.sh --config RelWithDebInfo --build_shared_lib --parallel --use_xcode
+    ./build.sh --config RelWithDebInfo --build_shared_lib --build_wheel --parallel --use_xcode
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     # POSIX compatibility layer and Linux environment emulation for Windows
-    ./build.sh --config RelWithDebInfo --build_shared_lib --parallel
+    ./build.sh --config RelWithDebInfo --build_shared_lib --build_wheel --parallel
 elif [[ "$OSTYPE" == "msys" ]]; then
     # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-    .\build.bat --config RelWithDebInfo --build_shared_lib --parallel
+    .\build.bat --config RelWithDebInfo --build_shared_lib --build_wheel --parallel
 elif [[ "$OSTYPE" == "win32" ]]; then
-    .\build.bat --config RelWithDebInfo --build_shared_lib --parallel
+    .\build.bat --config RelWithDebInfo --build_shared_lib --build_wheel --parallel
 else
     echo $OSTYPE  # Unknown.
 fi
 
-export ONNX_ML=1
-pip install setuptools wheel
-python setup.py bdist_wheel
-pip install --upgrade dist/*.whl
+pip install --upgrade $(shell find . -name "*.whl")
 
 cd $ROOT
 rm -rf onnxruntime
