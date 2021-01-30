1269a47398d74b8621edad08f5c5c11790e584ae
justusschock <justus.schock@posteo.de>
2021-01-30 18:26:43 +0100

    add cpu accelerator
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

---
+++ pytorch_lightning/accelerators/cpu.py
@@ -0,0 +1,14 @@
+from pytorch_lightning.accelerators.accelerator import Accelerator
+from pytorch_lightning.plugins import MixedPrecisionPlugin
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+
+
+class CPUAccelerator(Accelerator):
+    def setup(self, trainer, model):
+        if isinstance(self.precision_plugin, MixedPrecisionPlugin):
+            MisconfigurationException("amp + cpu is not supported.  Please use a GPU option")
+
+        if "cpu" not in str(self.root_device):
+            raise MisconfigurationException(f"Device should be CPU, got {self.root_device} instead")
+
+        return super().setup(trainer, model)
