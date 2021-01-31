e11003806649184b0cd117d9bba848dad5fff718
Kaushik Bokka <kaushikbokka@gmail.com>
2021-01-31 15:15:12 +0530

    fix train dataloader condition

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -197,7 +197,7 @@ class TrainLoop:
         model = self.trainer.get_model()
 
         # reset train dataloader
-        if  epoch != 0 and self.trainer.reload_dataloaders_every_epoch:
+        if epoch != 0 and self.trainer.reload_dataloaders_every_epoch:
             self.trainer.reset_train_dataloader(model)
 
         # set seed for distributed sampler (enables shuffling for each epoch)
