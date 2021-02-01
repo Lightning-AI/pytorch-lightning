9b799247dd372488458952dfa03dc25f72ac8ce6
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-02-01 16:07:46 +0100

    mv f

--- pytorch_lightning/plugins/training_type/ddp.py
+++ pytorch_lightning/plugins/training_type/ddp.py
@@ -25,7 +25,8 @@ from torch.nn.parallel.distributed import DistributedDataParallel
 from pytorch_lightning import _logger as log
 from pytorch_lightning.distributed import LightningDistributed
 from pytorch_lightning.overrides import LightningDistributedModule
-from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel, unwrap_lightning_module
+from pytorch_lightning.overrides.base import unwrap_lightning_module
+from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
 from pytorch_lightning.plugins.environments.cluster_environment import ClusterEnvironment
 from pytorch_lightning.plugins.training_type.parallel import ParallelPlugin
 from pytorch_lightning.utilities import _HYDRA_AVAILABLE
