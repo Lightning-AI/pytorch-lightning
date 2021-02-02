457da87b47ddb05aefbb6c2bf3997debf029673e
tchaton <thomas@grid.ai>
2021-02-02 10:57:56 +0000

    resolve custom dataloader

--- tests/special_tests.sh
+++ tests/special_tests.sh
@@ -28,3 +28,5 @@ python ${DEFAULTS} tests/trainer/logging_/test_train_loop_logging_1_0.py::test_l
 python ${DEFAULTS} tests/callbacks/test_pruning.py::test_pruning_callback_ddp
 python ${DEFAULTS} tests/trainer/test_trainer.py::test_pytorch_profiler_trainer_ddp
 python ${DEFAULTS} tests/models/test_hooks.py::test_transfer_batch_hook_ddp
+python ${DEFAULTS} tests/trainer/test_trainer.py::test_prediction_collection_ddp
+python ${DEFAULTS} tests/trainer/test_trainer.py::test_misconfiguration_on_dataloader
