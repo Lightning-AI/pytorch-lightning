09565645b4ba4e4c1f146c56e5f18bf4c719903d
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-03 00:21:20 +0100

    logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -183,9 +183,9 @@ html_theme_options = {
     'logo_only': False,
 }
 
-html_logo = '_static/images/logos/logo.svg'
+html_logo = '_static/images/logo.svg'
 
-html_favicon = '_static/images/logos/icon.svg'
+html_favicon = '_static/images/icon.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
