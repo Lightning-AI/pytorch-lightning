493e29e2ee430cd012fb0cebaf0abd90a7b7b477
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-06 09:04:27 +0100

    cb

--- tests/callbacks/test_lambda_function.py
+++ tests/callbacks/test_lambda_function.py
@@ -22,6 +22,7 @@ def test_lambda_call(tmpdir):
     seed_everything(42)
 
     class CustomModel(BoringModel):
+
         def on_train_epoch_start(self):
             if self.current_epoch > 1:
                 raise KeyboardInterrupt
