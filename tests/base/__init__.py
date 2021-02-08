42812bb00300b136790f85f1c2523e3e6b3f377f
Jirka Borovec <Borda@users.noreply.github.com>
2021-02-08 09:52:54 +0100

    prune SimpleModel (#5862)

--- tests/base/__init__.py
+++ tests/base/__init__.py
@@ -3,4 +3,3 @@
 from tests.base.boring_model import BoringDataModule, BoringModel, RandomDataset  # noqa: F401
 from tests.base.datasets import TrialMNIST  # noqa: F401
 from tests.base.model_template import EvalModelTemplate, GenericEvalModelTemplate  # noqa: F401
-from tests.base.simple_model import SimpleModule  # noqa: F401
