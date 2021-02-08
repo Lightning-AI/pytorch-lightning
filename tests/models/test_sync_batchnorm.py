c3594b096680e6a695f2a15e095837e843b2db78
tchaton <thomas@grid.ai>
2021-02-08 17:07:54 +0000

    revert to ddp_spawn

--- tests/models/test_sync_batchnorm.py
+++ tests/models/test_sync_batchnorm.py
@@ -110,7 +110,7 @@ def test_sync_batchnorm_ddp(tmpdir):
     trainer = Trainer(
         gpus=2,
         num_nodes=1,
-        accelerator='ddp',
+        accelerator='ddp_spawn',
         max_epochs=1,
         max_steps=3,
         sync_batchnorm=True,
