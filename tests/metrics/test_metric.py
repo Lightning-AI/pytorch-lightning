19411f772a98119300e98570b919d960191e0dbe
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2021-02-08 17:25:52 +0300

    test_metric.py duplicate DummyList removed

--- tests/metrics/test_metric.py
+++ tests/metrics/test_metric.py
@@ -41,20 +41,6 @@ class DummyList(Metric):
         pass
 
 
-class DummyList(Metric):
-    name = "DummyList"
-
-    def __init__(self):
-        super().__init__()
-        self.add_state("x", list(), dist_reduce_fx=None)
-
-    def update(self):
-        pass
-
-    def compute(self):
-        pass
-
-
 def test_inherit():
     Dummy()
 
