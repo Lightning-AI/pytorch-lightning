82e70c91d713dee5c4ff36d284a6c0b9a3c5f797
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-09 19:08:38 +0100

    note

--- pytorch_lightning/core/memory.py
+++ pytorch_lightning/core/memory.py
@@ -227,7 +227,7 @@ class ModelSummary(object):
 
     @property
     def model_size(self) -> float:
-        # todo: seems it does not work with quantized models
+        # todo: seems it does not work with quantized models - it returns 0.0
         return self.total_parameters * self._precision_megabytes
 
     def summarize(self) -> Dict[str, LayerSummary]:
