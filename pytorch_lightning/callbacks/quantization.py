2bca86de4d16534ee11b6fab1e4bcda20086f18c
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-09 17:10:36 +0100

    .

--- pytorch_lightning/callbacks/quantization.py
+++ pytorch_lightning/callbacks/quantization.py
@@ -193,8 +193,6 @@ class QuantizationAwareTraining(Callback):
         torch.quantization.convert(pl_module, inplace=True)
         # check we shall preserve wrapper
         if self._input_compatible:
-            pl_module.forward = wrap_quantize_forward_context(
-                model=pl_module, func=self.__module_forward
-            )
+            pl_module.forward = wrap_quantize_forward_context(model=pl_module, func=self.__module_forward)
         else:
             pl_module.forward = self.__module_forward
