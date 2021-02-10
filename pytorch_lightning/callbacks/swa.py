cda86e1b856fc746c0904c5fcc2ba22a98914d82
Carlos Mocholí <carlossmocholi@gmail.com>
2021-02-10 01:38:44 +0100

    Add beta warning

--- pytorch_lightning/callbacks/swa.py
+++ pytorch_lightning/callbacks/swa.py
@@ -11,10 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
+r"""
 Stochastic Weight Averaging Callback
-====================================
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+.. warning::
+    StochasticWeightAveraging is in beta and subject to change.
 """
 from copy import deepcopy
 from typing import Callable, Optional, Union
