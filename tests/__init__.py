17d0028c5149646fb11acc6d5c31818e09a62268
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-10 14:59:17 +0100

    yapf

--- tests/__init__.py
+++ tests/__init__.py
@@ -38,7 +38,6 @@ if not os.path.isdir(_TEMP_PATH):
 _SKIPIF_ARGS_PT_LE_1_4 = dict(condition=_TORCH_LOWER_EQUAL_1_4, reason="test pytorch > 1.4")
 _SKIPIF_ARGS_NO_GPU = dict(condition=not torch.cuda.is_available(), reason="test requires single-GPU machine")
 _SKIPIF_ARGS_NO_GPUS = dict(condition=torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-_SKIPIF_ARGS_NO_PT_PRUNE = dict(condition=not _PYTORCH_PRUNE_AVAILABLE, reason="PyTorch pruning is needed for this test")
 _SKIPIF_ARGS_NO_PT_QUANT = dict(
     condition=not _TORCH_QUANTIZE_AVAILABLE, reason="PyTorch quantization is needed for this test"
 )
