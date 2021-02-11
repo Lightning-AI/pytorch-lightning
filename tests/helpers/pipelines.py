ca77fa46fdd84b1231050a8e736a8b8ba4d10a8c
Ubuntu <ubuntu@ip-172-31-88-60.ec2.internal>
2021-02-11 11:18:45 +0000

    update

--- tests/helpers/pipelines.py
+++ tests/helpers/pipelines.py
@@ -60,7 +60,7 @@ def run_model_test(
     trainer_options.update(logger=logger)
     trainer = Trainer(**trainer_options)
     initial_values = torch.tensor([torch.sum(torch.abs(x)) for x in model.parameters()])
-    trainer.fit(model)
+    trainer.fit(model, datamodule=data)
     post_train_values = torch.tensor([torch.sum(torch.abs(x)) for x in model.parameters()])
 
     assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
