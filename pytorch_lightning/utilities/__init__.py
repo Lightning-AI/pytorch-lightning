8cacef7be1e36193e5b33e71a29392f13ec9173d
Justus Schock <justus.schock@rwth-aachen.de>
2021-02-11 12:43:34 +0100

    fix imports

--- pytorch_lightning/utilities/__init__.py
+++ pytorch_lightning/utilities/__init__.py
@@ -35,7 +35,7 @@ from pytorch_lightning.utilities.imports import (  # noqa: F401
     _module_available,
     _NATIVE_AMP_AVAILABLE,
     _OMEGACONF_AVAILABLE,
-    _TORCH_GREATER_EQUAL_1_7_0,
+    _TORCH_GREATER_EQUAL_1_7,
     _PYTORCH_PRUNE_AVAILABLE,
     _RPC_AVAILABLE,
     _TORCH_GREATER_EQUAL_1_6,
