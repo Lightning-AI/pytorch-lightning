60d0b1361d72b71b78d0a898dd6ecf9bcecdd4fd
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-02-13 00:08:35 +0100

    move tests

--- tests/accelerators/test_ddp.py
+++ tests/accelerators/test_ddp.py
@@ -16,7 +16,8 @@ import os
 import pytest
 import torch
 
-from tests.accelerators.legacy import ddp_model, DDPLauncher
+from tests.accelerators import DDPLauncher
+from tests.accelerators import ddp_model
 from tests.utilities.distributed import call_training_script
 
 
