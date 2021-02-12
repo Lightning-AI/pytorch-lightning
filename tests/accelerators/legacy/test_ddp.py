415c63f79786c6f9f42f67ca38d00262b8d85b85
Nicki Skafte <skaftenicki@gmail.com>
2021-02-12 15:27:56 +0100

    fix tests

--- tests/accelerators/legacy/test_ddp.py
+++ tests/accelerators/legacy/test_ddp.py
@@ -83,20 +83,6 @@ def internal_test_cli(tmpdir, args=None):
 
 
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
-def test_cli(tmpdir):
-    DDPLauncher.run_from_cmd_line("--max_epochs 1 --gpus 2 --accelerator ddp", internal_test_cli, tmpdir)
-    # load the results of the script
-    result_path = os.path.join(tmpdir, 'ddp.result')
-    result = torch.load(result_path)
-    # verify the file wrote the expected outputs
-    assert result['status'] == 'complete'
-    assert str(result['result']) == '1'
-
-
-# END: test_cli ddp test
-
-
-@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 @DDPLauncher.run(
     "--max_epochs [max_epochs] --gpus 2 --accelerator [accelerator]",
     max_epochs=["1"],
