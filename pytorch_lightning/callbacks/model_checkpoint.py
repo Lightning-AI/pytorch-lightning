464a9b054e6171c3f2b58bf269caf5585aa0de53
thomas chaton <thomas@grid.ai>
2021-02-16 09:53:13 +0000

    resolve bugs

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -548,6 +548,12 @@ class ModelCheckpoint(Callback):
         epoch = metrics.get("epoch")
         step = metrics.get("step")
 
+        # when `val_loss` is being logged and no ModelCheckpoint is being provided
+        # `val_loss` or `checkpoint_on` will be selected for monitor and need to be reduced to 
+        # prevent processes divergence
+        if self.monitor in ("val_loss", "checkpoint_on"):
+            current = trainer.training_type_plugin.reduce(current, reduce_op="mean")
+
         if self.check_monitor_top_k(current):
             self._update_best_and_save(current, epoch, step, trainer, pl_module, metrics)
         elif self.verbose:
