580c12b027af88a727a2624a4f21020570982bc8
Carlos Mocholi <carlossmocholi@gmail.com>
2021-02-16 17:57:00 +0100

    Fix Docstring argument position

--- pytorch_lightning/utilities/apply_func.py
+++ pytorch_lightning/utilities/apply_func.py
@@ -64,9 +64,9 @@ def apply_to_collection(
 
     Args:
         data: the collection to apply the function to
-        dtype: the given function will be applied to all elements of this dtype
         function: the function to apply
         *args: positional arguments (will be forwarded to calls of ``function``)
+        dtype: If given, the function will be applied to all elements of this dtype
         wrong_dtype: the given function won't be applied if this type is specified and the given collections is of
             the :attr:`wrong_type` even if it is of type :attr`dtype`
         **kwargs: keyword arguments (will be forwarded to calls of ``function``)
