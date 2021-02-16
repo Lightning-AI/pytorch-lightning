10f0ac3168a6eb782d939a3b73f62ef3b4906b4c
JLD <javier.lordi@gmail.com>
2021-02-17 00:27:12 +0100

    see scale

--- pytorch_lightning/trainer/connectors/optimizer_connector.py
+++ pytorch_lightning/trainer/connectors/optimizer_connector.py
@@ -73,10 +73,10 @@ class OptimizerConnector:
                 old_lr = lr_scheduler['scheduler'].optimizer.param_groups[0]['lr']
                 # Check if optimizer.step is called in AMP case, inside scaler.update [#5558 solved]
                 print(self.trainer.precision_plugin.scaler.get_scale())
-                    if lr_scheduler['reduce_on_plateau']:
-                        lr_scheduler['scheduler'].step(monitor_val)
-                    else:
-                        lr_scheduler['scheduler'].step()
+                if lr_scheduler['reduce_on_plateau']:
+                    lr_scheduler['scheduler'].step(monitor_val)
+                else:
+                    lr_scheduler['scheduler'].step()
                 new_lr = lr_scheduler['scheduler'].optimizer.param_groups[0]['lr']
 
                 if self.trainer.dev_debugger.enabled:
