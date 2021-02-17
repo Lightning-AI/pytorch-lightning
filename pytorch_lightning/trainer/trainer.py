7189d673f650e887ab23424ae3688fe78312203b
Sean Naren <sean.narenthiran@gmail.com>
2021-02-17 20:23:42 +0000

    DeepSpeed Integration (#5954)
    * Add initial deepspeed changes
    
    * Address code review
    
    * Move static method outside of function
    
    * Fixes
    
    * Add missing annotation
    
    * Remove seed setting
    
    * Doc changes
    
    * Doc changes, add address reviews
    
    * Fix docs
    
    * Try fixing issue by moving to torch adam
    
    * Clean up check
    
    * Changes, better APIs!
    
    * Add wrapper, swap to git install revision
    
    * Add special test
    
    * Add warning
    
    * Address review
    
    * Add better disclaimer
    
    * Turn off ZeRO for testing due to compilation
    
    * Add description on modifying parameters via the plugin
    
    * Doc strings clear
    
    * Small doc fixes
    
    * Fix hash, reduce test
    
    * Added CI change
    
    * Move to azure pipeline
    
    * Fix test name
    
    * Add missing flag
    
    * Remove sudo...
    
    * Try conda instead
    
    * Swap to conda base
    
    * Try suggested install
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Revert "Apply suggestions from code review"
    
    This reverts commit 41cca05a
    
    * Revert "Apply suggestions from code review"
    
    This reverts commit e06ec29e
    
    * Remove setter
    
    * Address most review
    
    * Move out function, remove DeepSpeed from requirements
    
    * Install deepspeed/mpi4py within container
    
    * Use special tests, move to master commit for deepspeed
    
    * Export path
    
    * Force compile to happen first
    
    * Remove!
    
    * Debugging ninja
    
    * Fix error in optimizer step logic
    
    * Attempt to fix symbolic link
    
    * Reverse to aid debugging
    
    * Export path again
    
    * Clean up mess
    
    * var
    
    * Revert "var"
    
    This reverts commit 3450eaca
    
    * Address review, add todo
    
    * Add note about unsupported functionality
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: tchaton <thomas@grid.ai>
    Co-authored-by: Jirka Borovec <jirka.borovec@seznam.cz>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -29,6 +29,7 @@ from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.core.step_result import Result
 from pytorch_lightning.loggers import LightningLoggerBase
+from pytorch_lightning.plugins import Plugin
 from pytorch_lightning.profiler import BaseProfiler
 from pytorch_lightning.trainer.callback_hook import TrainerCallbackHookMixin
 from pytorch_lightning.trainer.configuration_validator import ConfigValidator
@@ -130,7 +131,7 @@ class Trainer(
         terminate_on_nan: bool = False,
         auto_scale_batch_size: Union[str, bool] = False,
         prepare_data_per_node: bool = True,
-        plugins: Optional[Union[str, list]] = None,
+        plugins: Optional[Union[Plugin, str, list]] = None,
         amp_backend: str = 'native',
         amp_level: str = 'O2',
         distributed_backend: Optional[str] = None,
