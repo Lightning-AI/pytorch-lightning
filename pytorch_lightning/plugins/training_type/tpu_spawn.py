23a594e958f7c686f636d0f4f6aafc1d57349ebb
tchaton <thomas@grid.ai>
2021-02-17 14:11:32 +0000

    forgot status

--- pytorch_lightning/plugins/training_type/tpu_spawn.py
+++ pytorch_lightning/plugins/training_type/tpu_spawn.py
@@ -219,7 +219,7 @@ class TPUSpawnPlugin(DDPSpawnPlugin):
 
     def _close_logger(self, trainer) -> None:
         if hasattr(trainer, "logger"):
-            trainer.logger.finalize()
+            trainer.logger.finalize("success")
 
     @property
     def xmp_spawn_kwargs(self):
