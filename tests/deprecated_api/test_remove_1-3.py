b7ab0dca13b577b436b0e690f8df8834c3610fa7
Carlos Mocholi <carlossmocholi@gmail.com>
2021-02-19 05:38:16 +0100

    Backwards compatibility

--- tests/deprecated_api/test_remove_1-3.py
+++ tests/deprecated_api/test_remove_1-3.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Test deprecated functionality which will be removed in vX.Y.Z"""
+"""Test deprecated functionality which will be removed in v1.3.0"""
 from argparse import ArgumentParser
 from unittest import mock
 
