fe4a26b0b41c04074717401b4697842124164c44
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-02-20 15:58:53 +0100

    cpu tests

--- tests/accelerators/test_cpu.py
+++ tests/accelerators/test_cpu.py
@@ -4,11 +4,12 @@ import pytest
 import torch
 
 from pytorch_lightning.accelerators import CPUAccelerator
-from pytorch_lightning.plugins import SingleDevicePlugin, PrecisionPlugin
+from pytorch_lightning.plugins import SingleDevicePlugin, PrecisionPlugin, NativeMixedPrecisionPlugin
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
 def test_invalid_root_device():
+    """ Test that CPU Accelerator has root device on CPU. """
     trainer = Mock()
     model = Mock()
     accelerator = CPUAccelerator(
@@ -16,4 +17,16 @@ def test_invalid_root_device():
         precision_plugin=PrecisionPlugin()
     )
     with pytest.raises(MisconfigurationException, match="Device should be CPU"):
+        accelerator.setup(trainer=trainer, model=model)
+
+
+def test_unsupported_precision_plugins():
+    """ Test error messages are raised for unsupported precision plugins with CPU. """
+    trainer = Mock()
+    model = Mock()
+    accelerator = CPUAccelerator(
+        training_type_plugin=SingleDevicePlugin(torch.device("cpu")),
+        precision_plugin=NativeMixedPrecisionPlugin()
+    )
+    with pytest.raises(MisconfigurationException, match=r"amp \+ cpu is not supported."):
         accelerator.setup(trainer=trainer, model=model)
