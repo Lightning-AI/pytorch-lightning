caafdf2f7d328d0cbbc28fd50c3ec4c8485cc70e
Anthony Kim <artit.anthony@gmail.com>
2021-02-23 10:20:53 +0900

    Update pytorch_lightning/plugins/precision/deepspeed_precision.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pytorch_lightning/plugins/precision/deepspeed_precision.py
+++ pytorch_lightning/plugins/precision/deepspeed_precision.py
@@ -60,7 +60,7 @@ class DeepSpeedPrecisionPlugin(PrecisionPlugin):
         optimizer: Optimizer,
         clip_val: Union[int, float],
         gradient_clip_algorithm: str = GradClipAlgorithmType.NORM,
-        norm_type: float = float(2.0),
+        norm_type: float = 2.0,
     ):
         """
         DeepSpeed handles clipping gradients via the training type plugin.
