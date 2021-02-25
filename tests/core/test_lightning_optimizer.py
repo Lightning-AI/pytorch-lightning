1845a671ef4a323ec6b1fe578a180daf83b60913
Akihiro Nitta <nitta@akihironitta.com>
2021-02-25 21:38:41 +0900

    Add back BoringModel

--- tests/core/test_lightning_optimizer.py
+++ tests/core/test_lightning_optimizer.py
@@ -19,7 +19,7 @@ from torch.optim import Adam, Optimizer
 from pytorch_lightning import Trainer
 from pytorch_lightning.core.optimizer import LightningOptimizer
 from tests.base import EvalModelTemplate
-
+from tests.helpers.boring_model import BoringModel
 
 
 def test_lightning_optimizer(tmpdir):
