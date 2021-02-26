978a71e9d5d166cc4cfe63cc27629948d879af12
Eric Cousineau <eric.cousineau@tri.global>
2021-02-26 09:47:24 -0500

    fix ci errs

--- tests/trainer/test_trainer_cli.py
+++ tests/trainer/test_trainer_cli.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import inspect
-import io
 import pickle
 from argparse import ArgumentParser, Namespace
 from unittest import mock
