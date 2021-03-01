f15f5eafe3118315dfde6c57127837a59145c1a7
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-01 13:49:50 +0100

    refactor

--- tests/helpers/skipif.py
+++ tests/helpers/skipif.py
@@ -21,50 +21,6 @@ from pkg_resources import get_distribution
 from pytorch_lightning.utilities import _TORCH_QUANTIZE_AVAILABLE
 
 
-def skipif_args(
-    min_gpus: int = 0,
-    min_torch: Optional[str] = None,
-    quant_available: bool = False,
-) -> dict:
-    """ Creating aggregated arguments for standard pytest skipif, sot the usecase is::
-
-        @pytest.mark.skipif(**skipif_args(min_torch="99"))
-        def test_any_func(...):
-            ...
-
-    >>> from pprint import pprint
-    >>> pprint(skipif_args(min_torch="99", min_gpus=0))
-    {'condition': True, 'reason': 'Required: [torch>=99]'}
-    >>> pprint(skipif_args(min_torch="0.0", min_gpus=0))  # doctest: +NORMALIZE_WHITESPACE
-    {'condition': False, 'reason': 'Conditions satisfied, going ahead with the test.'}
-    """
-    conditions = []
-    reasons = []
-
-    if min_gpus:
-        conditions.append(torch.cuda.device_count() < min_gpus)
-        reasons.append(f"GPUs>={min_gpus}")
-
-    if min_torch:
-        torch_version = LooseVersion(get_distribution("torch").version)
-        conditions.append(torch_version < LooseVersion(min_torch))
-        reasons.append(f"torch>={min_torch}")
-
-    if quant_available:
-        _miss_default = 'fbgemm' not in torch.backends.quantized.supported_engines
-        conditions.append(not _TORCH_QUANTIZE_AVAILABLE or _miss_default)
-        reasons.append("PyTorch quantization is available")
-
-    if not any(conditions):
-        return dict(condition=False, reason="Conditions satisfied, going ahead with the test.")
-
-    reasons = [rs for cond, rs in zip(conditions, reasons) if cond]
-    return dict(
-        condition=any(conditions),
-        reason=f"Required: [{' + '.join(reasons)}]",
-    )
-
-
 class LightSkipif:
     """
     Light SkipIf wrapper for simple marking specific cases, fully compatible with pytest.mark::
@@ -83,23 +39,35 @@ class LightSkipif:
         quantization_available: bool = False,
         **kwargs
     ):
-        custom = skipif_args(
-            min_gpus=min_gpus,
-            min_torch=min_torch,
-            quant_available=quantization_available,
+        conditions = []
+        reasons = []
+
+        if min_gpus:
+            conditions.append(torch.cuda.device_count() < min_gpus)
+            reasons.append(f"GPUs>={min_gpus}")
+
+        if min_torch:
+            torch_version = LooseVersion(get_distribution("torch").version)
+            conditions.append(torch_version < LooseVersion(min_torch))
+            reasons.append(f"torch>={min_torch}")
+
+        if quantization_available:
+            _miss_default = 'fbgemm' not in torch.backends.quantized.supported_engines
+            conditions.append(not _TORCH_QUANTIZE_AVAILABLE or _miss_default)
+            reasons.append("PyTorch quantization is available")
+
+        reasons = [rs for cond, rs in zip(conditions, reasons) if cond]
+        return pytest.mark.skipif(
+            *args,
+            condition=any(conditions),
+            reason=f"Requires: [{' + '.join(reasons)}]",
+            **kwargs,
         )
-        return pytest.mark.skipif(*args, **custom, **kwargs)
 
 
-@pytest.mark.skipif(**skipif_args(min_torch="99"))
+@LightSkipif(min_torch="99")
 def test_always_skip():
     exit(1)
-
-
-@pytest.mark.parametrize("arg1", [0.5, 1.0, 2.0])
-@pytest.mark.skipif(**skipif_args(min_torch="0.0"))
-def test_always_pass(arg1):
-    assert arg1 > 0.0
 
 
 @pytest.mark.parametrize("arg1", [0.5, 1.0, 2.0])
