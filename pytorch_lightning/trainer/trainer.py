8d9aa2686074d382f9a31f0a169cbac7a3cc86cc
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-03-01 20:57:29 +0100

    missing import

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Trainer to automate the training."""
+import logging
 import warnings
 from itertools import count
 from pathlib import Path
