b1afce87eec38883ae675cb3403ef84879fd276a
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-01 13:35:23 +0100

    flake8

--- tests/helpers/skipif.py
+++ tests/helpers/skipif.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from distutils.version import LooseVersion
-from functools import wraps
-from typing import Callable, Optional
+from typing import Optional
 
 import pytest
 import torch
