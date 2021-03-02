aa2485468a6044f24ae185e5aa569ce12b0673bd
tchaton <thomas@grid.ai>
2021-03-02 08:24:59 +0000

    update on comments

--- pl_examples/basic_examples/profiler_example.py
+++ pl_examples/basic_examples/profiler_example.py
@@ -1,17 +1,45 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This script will generate 2 traces: one for `training_step` and one for `validation_step`.
+
+The traces can be visualized in 2 ways:
+* With Chrome:
+    1. Open Chrome and copy/paste this url: `chrome://tracing/`.
+    2. Once tracing opens, click on `Load` at the top-right and load one of the generated traces.
+
+* With PyTorch Tensorboard Profiler (Instructions can be found at https://github.com/pytorch/kineto/tree/master/tb_plugin)
+    1. pip install tensorboard torch-tb-profiler
+    2. tensorboard --logdir={FOLDER}
+
+"""
+
+import sys
+from argparse import ArgumentParser
+
 import torch
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as T
 
+from pl_examples import cli_lightning_logo
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
-"""
-This will generate 2 traces: one for `training_step` and one for `validation_step`.
-Open Chrome and copy/paste this url: `chrome://tracing/`.
-Once tracing opens, click on `Load` at the top-right and load one of the generated traces.
-"""
+
+DEFAULT_CMD_LINE = "--max_epochs 1 --limit_train_batches 15 --limit_val_batches 15 --profiler pytorch".split(" ")
 
 
-class LitLightningModule(LightningModule):
+class ModelToProfile(LightningModule):
 
     def __init__(self, model):
         super().__init__()
@@ -37,20 +65,30 @@ class LitLightningModule(LightningModule):
 
 class CIFAR10DataModule(LightningDataModule):
 
+    transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
+
     def train_dataloader(self, *args, **kwargs):
-        transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=self.transform)
         return torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=0)
 
     def val_dataloader(self, *args, **kwargs):
-        transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
-        valset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+        valset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=self.transform)
         return torch.utils.data.DataLoader(valset, batch_size=32, shuffle=True, num_workers=0)
 
 
-if __name__ == '__main__':
+def cli_main():
 
-    model = LitLightningModule(models.resnet50(pretrained=True))
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parser)
+    cmd_line = None if len(sys.argv) != 1 else DEFAULT_CMD_LINE
+    args = parser.parse_args(args=cmd_line)
+
+    model = ModelToProfile(models.resnet50(pretrained=True))
     datamodule = CIFAR10DataModule()
-    trainer = Trainer(max_epochs=1, limit_train_batches=15, limit_val_batches=15, profiler="pytorch")
+    trainer = Trainer(**vars(args))
     trainer.fit(model, datamodule=datamodule)
+
+
+if __name__ == '__main__':
+    cli_lightning_logo()
+    cli_main()
