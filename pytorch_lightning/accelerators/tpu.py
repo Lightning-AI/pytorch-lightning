7aa594958b5519b4265172c6acbb35e9794b0a10
Kunal Mundada <mundadakunal25@gmail.com>
2021-03-03 21:21:24 +0530

    PEP8 corrections[2]

--- pytorch_lightning/accelerators/tpu.py
+++ pytorch_lightning/accelerators/tpu.py
@@ -37,7 +37,7 @@ class TPUAccelerator(Accelerator):
             raise MisconfigurationException("TPUs only support a single tpu core or tpu spawn training.")
         return super().setup(trainer, model)
 
-    def run_optimizer_step(self, optimizer: Optimizer, optimizer_idx: int, lambda_closure: Callable, **kwargs) -> None:
+    def run_optimizer_step(self, optimizer: Optimizer, optimizer_idx: int, lambda_closure: Callable, **kwargs: Any) -> None:
         xm.optimizer_step(optimizer, barrier=False, optimizer_args={'closure': lambda_closure, **kwargs})
 
     def all_gather(self, tensor: torch.Tensor, group: Optional[Any] = None, sync_grads: bool = False) -> torch.Tensor:
