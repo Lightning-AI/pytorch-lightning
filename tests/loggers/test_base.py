1be9b8947135968f268ee17a9f816b345ace1f33
hobogalaxy <zalewski.ukas@gmail.com>
2021-03-05 21:26:24 +0100

    Improve test

--- tests/loggers/test_base.py
+++ tests/loggers/test_base.py
@@ -293,9 +293,16 @@ def test_logger_collection_on_save_checkpoint(tmpdir):
     custom_logger = CustomLogger()
     tensorboard_logger = TensorBoardLogger(save_dir=tmpdir)
 
-    logger_collection = LoggerCollection(logger_iterable=[dummy_logger, custom_logger, tensorboard_logger])
+    logger_iterable = [dummy_logger, custom_logger, tensorboard_logger]
+
+    for logger in logger_iterable:
+        assert hasattr(logger, 'on_save_checkpoint')
+
+    logger_collection = LoggerCollection(logger_iterable=logger_iterable)
 
     logger_state_dict = logger_collection.on_save_checkpoint(None, None, None)
+    assert type(tensorboard_logger) in logger_state_dict
+
+    # those loggers don't have on_save_checkpoint() hook implemented, so they should be present in logger state dict
     assert type(dummy_logger) not in logger_state_dict
     assert type(custom_logger) not in logger_state_dict
-    assert type(tensorboard_logger) in logger_state_dict
