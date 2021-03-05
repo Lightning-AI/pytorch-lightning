f273538590e03f9445ea4f828fc64b41f04e09a6
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-05 11:16:46 +0100

    patch download

--- pl_examples/__init__.py
+++ pl_examples/__init__.py
@@ -34,6 +34,12 @@ LIGHTNING_LOGO = """
                      ####
 """
 
+# TorchVision hotfix https://github.com/pytorch/vision/issues/1938
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
 
 def nice_print(msg, last=False):
     print()
