440db6a42fa67060b138338bf83c4f75a1652013
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-03-06 03:41:28 +0100

    bugfix

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -19,7 +19,7 @@ from argparse import Namespace
 from copy import deepcopy
 from distutils.version import LooseVersion
 from pathlib import Path
-from unittest.mock import ANY, call, patch
+from unittest.mock import ANY, call, patch, Mock
 
 import cloudpickle
 import pytest
@@ -1785,3 +1785,33 @@ def test_train_loop_system(tmpdir):
         "training_step",
         "backward",
     ]
+
+
+def test_init_optimizers_resets_lightning_optimizers(tmpdir):
+    """ Test that the Trainer resets the `lightning_optimizers` list everytime new optimizers get initialized. """
+
+    def compare_optimizers():
+        assert trainer.lightning_optimizers[0].optimizer is trainer.optimizers[0]
+
+    class OptimizerSpy(Callback):
+        def on_fit_start(self, *args, **kwargs):
+            compare_optimizers()
+
+    model = BoringModel()
+    model.lr = 0.2
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        max_epochs=1,
+        auto_lr_find=True,
+        callbacks=[OptimizerSpy()]
+    )
+
+    trainer.tune(model)
+    compare_optimizers()
+
+    trainer.fit(model)
+    compare_optimizers()
+
+    trainer.max_epochs = 2  # simulate multiple fit calls
+    trainer.fit(model)
+    compare_optimizers()
