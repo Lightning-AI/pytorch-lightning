7bcd8e090c275d644ce988750f2014df7dc22e82
Kaushik Bokka <kaushikbokka@gmail.com>
2021-03-09 14:54:00 +0530

    add test for val_check steps

--- tests/trainer/flags/test_val_check_interval.py
+++ tests/trainer/flags/test_val_check_interval.py
@@ -17,26 +17,28 @@ from pytorch_lightning.trainer import Trainer
 from tests.helpers import BoringModel
 
 
+class TestModel(BoringModel):
+
+    def __init__(self):
+        super().__init__()
+        self.train_epoch_calls = 0
+        self.val_epoch_calls = 0
+
+    def on_train_epoch_start(self) -> None:
+        self.train_epoch_calls += 1
+
+    def on_validation_epoch_start(self) -> None:
+        if not self.trainer.sanity_checking:
+            self.val_epoch_calls += 1
+
+
 @pytest.mark.parametrize('max_epochs', [1, 2, 3])
 @pytest.mark.parametrize('denominator', [1, 3, 4])
 def test_val_check_interval(tmpdir, max_epochs, denominator):
 
-    class TestModel(BoringModel):
-
-        def __init__(self):
-            super().__init__()
-            self.train_epoch_calls = 0
-            self.val_epoch_calls = 0
-
-        def on_train_epoch_start(self) -> None:
-            self.train_epoch_calls += 1
-
-        def on_validation_epoch_start(self) -> None:
-            if not self.trainer.sanity_checking:
-                self.val_epoch_calls += 1
-
     model = TestModel()
     trainer = Trainer(
+        default_root_dir=tmpdir,
         max_epochs=max_epochs,
         val_check_interval=1 / denominator,
         logger=False,
@@ -45,3 +47,22 @@ def test_val_check_interval(tmpdir, max_epochs, denominator):
 
     assert model.train_epoch_calls == max_epochs
     assert model.val_epoch_calls == max_epochs * denominator
+
+
+@pytest.mark.parametrize('steps', [10, 100, 150])
+def test_val_check_interval_steps(tmpdir, steps):
+
+    max_epochs = 4
+    model = TestModel()
+    train_data_length = len(model.train_dataloader())
+
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        max_epochs=max_epochs,
+        val_check_interval=steps,
+        logger=False,
+    )
+    trainer.fit(model)
+
+    assert model.train_epoch_calls == max_epochs
+    assert model.val_epoch_calls == max_epochs * train_data_length // steps
