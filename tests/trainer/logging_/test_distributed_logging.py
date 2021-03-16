b169fb570496fd5bfdbc8632e20fbaade9ea6fb9
SeanNaren <sean@grid.ai>
2021-03-16 10:43:06 +0000

    Add missing imports & stage
    Add missing stage

--- tests/trainer/logging_/test_distributed_logging.py
+++ tests/trainer/logging_/test_distributed_logging.py
@@ -11,14 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
 import platform
 from unittest import mock
+from unittest.mock import Mock
 
 import pytest
 import torch
 
-from pytorch_lightning import Trainer
+from pytorch_lightning import Callback, Trainer
 from tests.helpers import BoringModel
 
 
