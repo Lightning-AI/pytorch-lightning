cad06717f25acfd93d50a8fe2fe9b20369e9d97e
SeanNaren <sean@grid.ai>
2021-03-16 17:20:13 +0000

    Reverse hook

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -428,9 +428,10 @@ class Trainer(
         # ----------------------------
         # SET UP TRAINING
         # ----------------------------
+        self.call_setup_hook(model)
         self.call_hook("on_before_accelerator_backend_setup", model)
         self.accelerator.setup(self, model)  # note: this sets up self.lightning_module
-        self.call_setup_hook(model)
+
 
         # ----------------------------
         # INSPECT THE CORE LOOPS
