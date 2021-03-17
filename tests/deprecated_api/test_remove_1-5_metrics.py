9f319600cc2027e89ce704e7f3eec26bc0e5e74b
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-17 23:23:22 +0100

    class: AUC AUROC

--- tests/deprecated_api/test_remove_1-5_metrics.py
+++ tests/deprecated_api/test_remove_1-5_metrics.py
@@ -16,7 +16,7 @@
 import pytest
 import torch
 
-from pytorch_lightning.metrics import Accuracy, MetricCollection
+from pytorch_lightning.metrics import Accuracy, MetricCollection, AUC, AUROC
 from pytorch_lightning.metrics.functional.accuracy import accuracy
 from pytorch_lightning.metrics.utils import get_num_classes, select_topk, to_categorical, to_onehot
 
@@ -38,16 +38,6 @@ def test_v1_5_metrics_utils():
         assert torch.equal(to_categorical(x), torch.Tensor([1, 0]).to(int))
 
 
-def test_v1_5_metric_accuracy():
-    accuracy.warned = False
-    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
-        assert accuracy(preds=torch.tensor([0, 1]), target=torch.tensor([0, 1])) == torch.tensor(1.)
-
-    Accuracy.__init__.warned = False
-    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
-        Accuracy()
-
-
 def test_v1_5_metrics_collection():
     target = torch.tensor([0, 2, 0, 2, 0, 1, 0, 2])
     preds = torch.tensor([2, 1, 2, 0, 1, 2, 2, 2])
@@ -59,3 +49,27 @@ def test_v1_5_metrics_collection():
     ):
         metrics = MetricCollection([Accuracy()])
     assert metrics(preds, target) == {'Accuracy': torch.tensor(0.1250)}
+
+
+def test_v1_5_metric_accuracy():
+    accuracy.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        assert accuracy(preds=torch.tensor([0, 1]), target=torch.tensor([0, 1])) == torch.tensor(1.)
+
+    Accuracy.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        Accuracy()
+
+
+def test_v1_5_metric_auc():
+    AUC.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        AUC()
+
+    AUROC.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        AUROC()
+
+    # auc.warned = False
+    # with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+    #     assert accuracy(preds=torch.tensor([0, 1]), target=torch.tensor([0, 1])) == torch.tensor(1.)
