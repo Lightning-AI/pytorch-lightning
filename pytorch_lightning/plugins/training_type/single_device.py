bd2a53a73f1c4cf43320d4517259321190f967ef
SeanNaren <sean@grid.ai>
2021-03-17 12:00:57 +0000

    Add additional model to device hook if any additional parameters have been set

--- pytorch_lightning/plugins/training_type/single_device.py
+++ pytorch_lightning/plugins/training_type/single_device.py
@@ -65,10 +65,12 @@ class SingleDevicePlugin(TrainingTypePlugin):
         self._model.to(self.root_device)
 
     def pre_dispatch(self) -> None:
+        # Ensures any additional parameters defined in setup are moved to the correct device.
         self.model_to_device()
 
     def connect(self, model: torch.nn.Module) -> torch.nn.Module:
         self._model = model
+        self.model_to_device()
         return self.model
 
     @property
