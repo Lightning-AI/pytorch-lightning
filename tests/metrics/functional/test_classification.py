38a2119359f22dd9525cb7978eb2ac230a36ab59
Jirka Borovec <Borda@users.noreply.github.com>
2021-03-18 18:21:59 +0100

    Prune metrics: precision & recall 6/n (#6573)
    * avg precision
    
    * precision
    * recall
    
    * curve
    
    * tests
    
    * chlog
    
    * isort
    
    * fix

--- tests/metrics/functional/test_classification.py
+++ tests/metrics/functional/test_classification.py
@@ -1,33 +1,7 @@
 import pytest
 import torch
 
-from pytorch_lightning import seed_everything
 from pytorch_lightning.metrics.functional.classification import dice_score
-from pytorch_lightning.metrics.functional.precision_recall_curve import _binary_clf_curve
-
-
-@pytest.mark.parametrize(['sample_weight', 'pos_label', "exp_shape"], [
-    pytest.param(1, 1., 42),
-    pytest.param(None, 1., 42),
-])
-def test_binary_clf_curve(sample_weight, pos_label, exp_shape):
-    # TODO: move back the pred and target to test func arguments
-    #  if you fix the array inside the function, you'd also have fix the shape,
-    #  because when the array changes, you also have to fix the shape
-    seed_everything(0)
-    pred = torch.randint(low=51, high=99, size=(100, ), dtype=torch.float) / 100
-    target = torch.tensor([0, 1] * 50, dtype=torch.int)
-    if sample_weight is not None:
-        sample_weight = torch.ones_like(pred) * sample_weight
-
-    fps, tps, thresh = _binary_clf_curve(preds=pred, target=target, sample_weights=sample_weight, pos_label=pos_label)
-
-    assert isinstance(tps, torch.Tensor)
-    assert isinstance(fps, torch.Tensor)
-    assert isinstance(thresh, torch.Tensor)
-    assert tps.shape == (exp_shape, )
-    assert fps.shape == (exp_shape, )
-    assert thresh.shape == (exp_shape, )
 
 
 @pytest.mark.parametrize(['pred', 'target', 'expected'], [
--- tests/metrics/test_remove_1-5_metrics.py
+++ tests/metrics/test_remove_1-5_metrics.py
@@ -16,8 +16,27 @@
 import pytest
 import torch
 
-from pytorch_lightning.metrics import Accuracy, AUC, AUROC, MetricCollection, ROC
-from pytorch_lightning.metrics.functional import auc, auroc, roc
+from pytorch_lightning.metrics import (
+    Accuracy,
+    AUC,
+    AUROC,
+    AveragePrecision,
+    MetricCollection,
+    Precision,
+    PrecisionRecallCurve,
+    Recall,
+    ROC,
+)
+from pytorch_lightning.metrics.functional import (
+    auc,
+    auroc,
+    average_precision,
+    precision,
+    precision_recall,
+    precision_recall_curve,
+    recall,
+    roc,
+)
 from pytorch_lightning.metrics.functional.accuracy import accuracy
 from pytorch_lightning.metrics.utils import get_num_classes, select_topk, to_categorical, to_onehot
 
@@ -88,13 +107,58 @@ def test_v1_5_metric_auc_auroc():
     target = torch.tensor([0, 1, 1, 1])
     roc.warned = False
     with pytest.deprecated_call(match='It will be removed in v1.5.0'):
-        fpr, tpr, thresholds = roc(preds, target, pos_label=1)
+        fpr, tpr, thrs = roc(preds, target, pos_label=1)
         assert torch.equal(fpr, torch.tensor([0., 0., 0., 0., 1.]))
         assert torch.allclose(tpr, torch.tensor([0.0000, 0.3333, 0.6667, 1.0000, 1.0000]), atol=1e-4)
-        assert torch.equal(thresholds, torch.tensor([4, 3, 2, 1, 0]))
+        assert torch.equal(thrs, torch.tensor([4, 3, 2, 1, 0]))
 
     preds = torch.tensor([0.13, 0.26, 0.08, 0.19, 0.34])
     target = torch.tensor([0, 0, 1, 1, 1])
     auroc.warned = False
     with pytest.deprecated_call(match='It will be removed in v1.5.0'):
         assert auroc(preds, target) == torch.tensor(0.5)
+
+
+def test_v1_5_metric_precision_recall():
+    AveragePrecision.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        AveragePrecision()
+
+    Precision.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        Precision()
+
+    Recall.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        Recall()
+
+    PrecisionRecallCurve.__init__.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        PrecisionRecallCurve()
+
+    pred = torch.tensor([0, 1, 2, 3])
+    target = torch.tensor([0, 1, 1, 1])
+    average_precision.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        assert average_precision(pred, target) == torch.tensor(1.)
+
+    precision.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        assert precision(pred, target) == torch.tensor(0.5)
+
+    recall.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        assert recall(pred, target) == torch.tensor(0.5)
+
+    precision_recall.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        prec, rc = precision_recall(pred, target)
+        assert prec == torch.tensor(0.5)
+        assert rc == torch.tensor(0.5)
+
+    precision_recall_curve.warned = False
+    with pytest.deprecated_call(match='It will be removed in v1.5.0'):
+        prec, rc, thrs = precision_recall_curve(pred, target)
+        assert torch.equal(prec, torch.tensor([1., 1., 1., 1.]))
+        assert torch.allclose(rc, torch.tensor([1., 0.6667, 0.3333, 0.]), atol=1e-4)
+        assert torch.equal(thrs, torch.tensor([1, 2, 3]))
