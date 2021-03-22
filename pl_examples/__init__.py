ccde3b8c24980ee812a58affd5357370e7d8d029
Jirka Borovec <Borda@users.noreply.github.com>
2021-03-22 17:28:44 +0100

    ...
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- pl_examples/__init__.py
+++ pl_examples/__init__.py
@@ -15,7 +15,7 @@ _PACKAGE_ROOT = os.path.dirname(_EXAMPLES_ROOT)
 _DATASETS_PATH = os.path.join(_PACKAGE_ROOT, 'Datasets')
 
 _TORCHVISION_AVAILABLE = _module_available("torchvision")
-_TORCHVISION_MNIST_AVAILABLE = not bool(os.environ.get("PL_USE_MOCKED_MNIST", "0"))
+_TORCHVISION_MNIST_AVAILABLE = not bool(os.environ.get("PL_USE_MOCKED_MNIST", False))
 _DALI_AVAILABLE = _module_available("nvidia.dali")
 
 if _TORCHVISION_MNIST_AVAILABLE:
