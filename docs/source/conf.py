6536613e141ff5ce45c5cce7584f6e4fa13d842d
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-23 19:35:13 +0100

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -178,11 +178,12 @@ html_theme_path = [pt_lightning_sphinx_theme.get_html_theme_path()]
 # documentation.
 
 html_theme_options = {
-    'pytorch_project': info.__homepage__,
-    'canonical_url': info.__homepage__,
+    'pytorch_project': 'https://pytorchlightning.ai',
+    'canonical_url': 'https://pytorch-lightning.readthedocs.io',
     'collapse_navigation': False,
     'display_version': True,
     'logo_only': False,
+    'gitlab_url': info.__homepage__,
 }
 
 html_logo = '_static/images/logo.svg'
