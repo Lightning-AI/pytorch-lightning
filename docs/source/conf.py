8cd75a4dd51939881da265752c2d81307cbe4d9e
Jirka Borovec <Borda@users.noreply.github.com>
2021-03-23 08:51:45 +0100

    fix comparing versions (#6434)
    * fix comparing versions
    
    * chlog
    
    * .
    
    * ...
    
    * datasets

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -334,6 +334,7 @@ PACKAGE_MAPPING = {
 }
 MOCK_PACKAGES = []
 if SPHINX_MOCK_REQUIREMENTS:
+    MOCK_PACKAGES += ['fairscale']
     # mock also base packages when we are on RTD since we don't install them there
     MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements.txt'))
     MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements', 'extra.txt'))
