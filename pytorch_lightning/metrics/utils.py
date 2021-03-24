a4d3920d878606fa0307cad8233ccf808e2f7e10
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-24 10:08:55 +0100

    .

--- pytorch_lightning/metrics/utils.py
+++ pytorch_lightning/metrics/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from functools import partial
-from typing import List, Optional
+from typing import Optional
 
 import torch
 from deprecate import deprecated
