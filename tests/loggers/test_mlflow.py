0aa8caa3ea2a012578dbbecab41523b60d5ccb67
Ethan Harris <ewah1g13@soton.ac.uk>
2021-03-25 11:34:28 +0000

    Add artifcact_location arg to MLFlow logger

--- tests/loggers/test_mlflow.py
+++ tests/loggers/test_mlflow.py
@@ -199,3 +199,21 @@ def test_mlflow_logger_with_long_param_value(client, mlflow, tmpdir):
 
     with pytest.warns(RuntimeWarning, match=f'Discard {key}={value}'):
         logger.log_hyperparams(params)
+
+
+@mock.patch('pytorch_lightning.loggers.mlflow.mlflow')
+@mock.patch('pytorch_lightning.loggers.mlflow.MlflowClient')
+def test_mlflow_logger_with_artifact_location(client, mlflow, tmpdir):
+    """
+    Test that the logger raises warning with special characters not accepted by MLFlow.
+    """
+    logger = MLFlowLogger('test', save_dir=tmpdir, artifact_location='my_artifact_location')
+    logger._mlflow_client.get_experiment_by_name.return_value = None
+
+    metrics = {'some_metric': 10}
+    logger.log_metrics(metrics)
+
+    logger._mlflow_client.create_experiment.assert_called_once_with(
+        name='test',
+        artifact_location='my_artifact_location',
+    )
