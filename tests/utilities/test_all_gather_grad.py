3390eb6f4c95a0e08d5d8547b7eb0d4dfe91c7b6
Carlos Mocholi <carlossmocholi@gmail.com>
2021-03-25 22:12:14 +0100

    Fix test

--- tests/utilities/test_all_gather_grad.py
+++ tests/utilities/test_all_gather_grad.py
@@ -55,7 +55,6 @@ def test_all_gather_collection(tmpdir):
         training_epoch_end_called = False
 
         def training_epoch_end(self, outputs) -> None:
-            self.training_epoch_end_called = True
             losses = torch.stack([x["loss"] for x in outputs])
             gathered_loss = self.all_gather({
                 "losses_tensor_int": torch.rand(2, 2).int().t(),
@@ -67,7 +66,7 @@ def test_all_gather_collection(tmpdir):
                 "losses": losses,
                 "losses_list": [losses, losses]
             })
-            assert gathered_loss["losses_tensor_int"][0].dtype == torch.int64
+            assert gathered_loss["losses_tensor_int"][0].dtype == torch.int32
             assert gathered_loss["losses_tensor_float"][0].dtype == torch.float
             assert gathered_loss["losses_np_ndarray"][0].dtype == torch.int64
             # torch.bool can't be all_gathered
@@ -76,6 +75,7 @@ def test_all_gather_collection(tmpdir):
             assert gathered_loss["losses_int"][0].dtype == torch.int
             assert gathered_loss["losses_list"][0].numel() == 2 * len(losses)
             assert gathered_loss["losses"].numel() == 2 * len(losses)
+            self.training_epoch_end_called = True
 
     seed_everything(42)
 
