29d47735376e33cb9f29bbaa993f764645e17b48
Carlos Mocholi <carlossmocholi@gmail.com>
2021-03-26 18:36:37 +0100

    Unnecessary fit

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1780,12 +1780,9 @@ def test_trainer_attach_data_pipeline_to_model(tmpdir):
 
 
 def test_exception_when_testing_or_validating_with_fast_dev_run(tmpdir):
-    model = BoringModel()
     trainer = Trainer(default_root_dir=tmpdir, fast_dev_run=True)
-    trainer.fit(model)
 
-    with pytest.raises(MisconfigurationException, match=".*when `fast_dev_run=True`*"):
+    with pytest.raises(MisconfigurationException, match=".*with `fast_dev_run=True`*"):
         trainer.validate()
-
-    with pytest.raises(MisconfigurationException, match=".*when `fast_dev_run=True`*"):
+    with pytest.raises(MisconfigurationException, match=".*with `fast_dev_run=True`*"):
         trainer.test()
