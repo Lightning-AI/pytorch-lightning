e0d8e845322a6eee2581cead817fb42968dbcf4a
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-11 00:03:57 +0200

    flake8

--- pytorch_lightning/plugins/precision/native_amp.py
+++ pytorch_lightning/plugins/precision/native_amp.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from contextlib import contextmanager
-from typing import Any, Callable, Generator, TYPE_CHECKING
+from typing import Any, Callable, Generator
 
 import torch
 from torch.optim import LBFGS, Optimizer
