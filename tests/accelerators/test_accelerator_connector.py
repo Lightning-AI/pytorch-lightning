c2744e65cf900a69da2e6ab9ba493c898a2adc39
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-10 05:27:03 +0200

    fix test

--- tests/accelerators/test_accelerator_connector.py
+++ tests/accelerators/test_accelerator_connector.py
@@ -195,7 +195,7 @@ def test_accelerator_choice_ddp_te(device_count_mock, setup_distributed_mock):
 
 
 @RunIf(min_gpus=1)
-@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "LOCAL_RANK": "10", "NODE_RANK": "0"})
+@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "RANk": "1", "LOCAL_RANK": "1", "GROUP_RANK": "0"})
 @mock.patch('torch.cuda.device_count', return_value=2)
 @mock.patch('pytorch_lightning.plugins.DDPPlugin.setup_distributed', autospec=True)
 def test_accelerator_choice_ddp2_te(device_count_mock, setup_distributed_mock):
@@ -207,8 +207,8 @@ def test_accelerator_choice_ddp2_te(device_count_mock, setup_distributed_mock):
             assert isinstance(trainer.accelerator, GPUAccelerator)
             assert isinstance(trainer.training_type_plugin, DDP2Plugin)
             assert isinstance(trainer.training_type_plugin.cluster_environment, TorchElasticEnvironment)
-            assert trainer.training_type_plugin.cluster_environment.local_rank() == 10
-            assert trainer.training_type_plugin.task_idx == 10
+            assert trainer.training_type_plugin.cluster_environment.local_rank() == 1
+            assert trainer.training_type_plugin.task_idx == 1
             raise SystemExit()
 
     model = BoringModel()
