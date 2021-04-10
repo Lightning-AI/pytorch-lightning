93a053807a3f0753aaf73d1f5a2e01e5bda113e0
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-10 05:28:27 +0200

    TYPO

--- tests/accelerators/test_accelerator_connector.py
+++ tests/accelerators/test_accelerator_connector.py
@@ -166,7 +166,7 @@ def test_accelerator_choice_ddp2_slurm(device_count_mock, setup_distributed_mock
 
 
 @RunIf(min_gpus=1)
-@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "LOCAL_RANK": "1", "NODE_RANK": "0"})
+@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "RANK": "1", "LOCAL_RANK": "1", "GROUP_RANK": "0"})
 @mock.patch('torch.cuda.device_count', return_value=2)
 @mock.patch('pytorch_lightning.plugins.DDPPlugin.setup_distributed', autospec=True)
 def test_accelerator_choice_ddp_te(device_count_mock, setup_distributed_mock):
@@ -195,7 +195,7 @@ def test_accelerator_choice_ddp_te(device_count_mock, setup_distributed_mock):
 
 
 @RunIf(min_gpus=1)
-@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "RANk": "1", "LOCAL_RANK": "1", "GROUP_RANK": "0"})
+@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1", "WORLD_SIZE": "2", "RANK": "1", "LOCAL_RANK": "1", "GROUP_RANK": "0"})
 @mock.patch('torch.cuda.device_count', return_value=2)
 @mock.patch('pytorch_lightning.plugins.DDPPlugin.setup_distributed', autospec=True)
 def test_accelerator_choice_ddp2_te(device_count_mock, setup_distributed_mock):
