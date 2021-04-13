036fc155fee9565e2faec6c05ead19a9a1433c59
Ethan Harris <ewah1g13@soton.ac.uk>
2021-04-13 12:28:55 +0100

    Remove unused import

--- tests/trainer/test_training_loop.py
+++ tests/trainer/test_training_loop.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
-
 from pytorch_lightning import Trainer
 from tests.helpers import BoringModel
 
