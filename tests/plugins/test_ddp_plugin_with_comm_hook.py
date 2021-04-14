9b29428dad2982e76f127d406203575930cd0abd
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-14 20:34:02 +0200

    Apply suggestions from code review

--- tests/plugins/test_ddp_plugin_with_comm_hook.py
+++ tests/plugins/test_ddp_plugin_with_comm_hook.py
@@ -45,7 +45,7 @@ def test_ddp_fp16_compress_comm_hook(tmpdir):
     trainer_comm_hook = (trainer.accelerator.training_type_plugin._model.get_ddp_logging_data().comm_hook)
     expected_comm_hook = default.fp16_compress_hook.__qualname__
     assert trainer_comm_hook == expected_comm_hook
-    assert (trainer.state == TrainerState.FINISHED), f"Training failed with {trainer.state}"
+    assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
 
 
 @RunIf(skip_windows=True, min_torch="1.9.0", min_gpus=2, special=True)
@@ -69,7 +69,7 @@ def test_ddp_sgd_comm_hook(tmpdir):
     trainer_comm_hook = (trainer.accelerator.training_type_plugin._model.get_ddp_logging_data().comm_hook)
     expected_comm_hook = powerSGD.powerSGD_hook.__qualname__
     assert trainer_comm_hook == expected_comm_hook
-    assert (trainer.state == TrainerState.FINISHED), f"Training failed with {trainer.state}"
+    assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
 
 
 @RunIf(skip_windows=True, min_torch="1.9.0", min_gpus=2, special=True)
@@ -94,7 +94,7 @@ def test_ddp_fp16_compress_wrap_sgd_comm_hook(tmpdir):
     trainer_comm_hook = (trainer.accelerator.training_type_plugin._model.get_ddp_logging_data().comm_hook)
     expected_comm_hook = default.fp16_compress_wrapper(powerSGD.powerSGD_hook).__qualname__
     assert trainer_comm_hook == expected_comm_hook
-    assert (trainer.state == TrainerState.FINISHED), f"Training failed with {trainer.state}"
+    assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
 
 
 @RunIf(skip_windows=True, min_torch="1.9.0", min_gpus=2, special=True)
@@ -114,4 +114,4 @@ def test_ddp_spawn_fp16_compress_comm_hook(tmpdir):
         fast_dev_run=True,
     )
     trainer.fit(model)
-    assert (trainer.state == TrainerState.FINISHED), f"Training failed with {trainer.state}"
+    assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
