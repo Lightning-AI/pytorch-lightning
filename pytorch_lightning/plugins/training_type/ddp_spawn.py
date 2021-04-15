9ecd7855b063f1509a67c50b23437814ca10cb3d
Carlos Mocholi <carlossmocholi@gmail.com>
2021-04-15 23:17:48 +0200

    Fix typing kwargs

--- pytorch_lightning/plugins/training_type/ddp_spawn.py
+++ pytorch_lightning/plugins/training_type/ddp_spawn.py
@@ -14,7 +14,7 @@
 import logging
 import os
 import re
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, List, Optional, Union
 
 import torch
 import torch.distributed as torch_distrib
@@ -57,7 +57,7 @@ class DDPSpawnPlugin(ParallelPlugin):
         ddp_comm_state: Optional[object] = None,
         ddp_comm_hook: Optional[callable] = None,
         ddp_comm_wrapper: Optional[callable] = None,
-        **kwargs: Union[Any, Dict[str, Any]],
+        **kwargs: Any,
     ):
         super().__init__(parallel_devices=parallel_devices, cluster_environment=cluster_environment)
         self.num_nodes = num_nodes
