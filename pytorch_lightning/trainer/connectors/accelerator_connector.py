bdb66ab8921f975c30827a992ab4870c2358c9cf
Shuying Sun <shuyingsun@fb.com>
2021-04-15 12:14:50 -0700

    fix test

--- pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -435,11 +435,11 @@ class AcceleratorConnector(object):
         if hasattr(training_type, 'cluster_environment') and getattr(training_type, 'cluster_environment') is None:
             training_type.cluster_environment = self.select_cluster_environment()
 
-        if hasattr(training_type, 'num_nodes'):
+        if hasattr(training_type, 'num_nodes') and getattr(training_type, 'num_nodes') != self.num_nodes:
             # set num_nodes for training_type from trainer setting
             training_type.num_nodes = self.num_nodes
 
-        if hasattr(training_type, 'sync_batchnorm'):
+        if hasattr(training_type, 'sync_batchnorm') and getattr(training_type, 'sync_batchnorm') != self.sync_batchnorm:
             # Set sync_batchnorm for training_type from trainer setting.
             training_type.sync_batchnorm = self.sync_batchnorm
 
