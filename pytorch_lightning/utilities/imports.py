09d3c8bcd1d220f583e5bc4eb1a015f503b94f01
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-22 22:45:46 +0200

    Fix `torchmetrics` compatibility (#7131)
    * get_num_classes
    
    * tmp
    
    * fix one test
    
    * fix deprecated tests
    
    * fix deprecate
    
    * pep8
    
    * deprecate 0.3
    
    * wip
    
    * wip
    
    * HaCK
    
    * brnch
    
    * brnch
    
    * format
    
    * Apply suggestions from code review
    
    * prune
    
    * rev
    
    * mltilabel
    
    * Apply suggestions from code review
    
    * master
    
    * rev
    
    * .
    
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>

--- pytorch_lightning/utilities/imports.py
+++ pytorch_lightning/utilities/imports.py
@@ -86,6 +86,8 @@ _RPC_AVAILABLE = not _IS_WINDOWS and _module_available('torch.distributed.rpc')
 _TORCH_QUANTIZE_AVAILABLE = bool([eg for eg in torch.backends.quantized.supported_engines if eg != 'none'])
 _TORCHTEXT_AVAILABLE = _module_available("torchtext")
 _TORCHVISION_AVAILABLE = _module_available('torchvision')
+_TORCHMETRICS_LOWER_THAN_0_3 = _compare_version("torchmetrics", operator.lt, "0.3.0")
+_TORCHMETRICS_GREATER_EQUAL_0_3 = _compare_version("torchmetrics", operator.ge, "0.3.0")
 _XLA_AVAILABLE = _module_available("torch_xla")
 
 from pytorch_lightning.utilities.xla_device import XLADeviceUtils  # noqa: E402
