f84d047104de71a8f0546dcbbdaff0094b0a45f9
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-22 15:43:38 +0200

    clean up

--- pytorch_lightning/utilities/argparse.py
+++ pytorch_lightning/utilities/argparse.py
@@ -289,13 +289,6 @@ def _gpus_allowed_type(x) -> Union[int, str]:
     return int(x)
 
 
-# def _gpus_arg_default(x) -> Union[int, str]:  # pragma: no-cover
-#     # unused, but here for backward compatibility with old checkpoints that need to be able to
-#     # unpickle the function from the checkpoint, as it was not filtered out in versions < 1.2.8
-#     # see: https://github.com/PyTorchLightning/pytorch-lightning/pull/6898
-#     pass
-
-
 def _int_or_float_type(x) -> Union[int, float]:
     if '.' in str(x):
         return float(x)
