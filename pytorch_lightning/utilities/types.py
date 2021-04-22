5de19ab3d5b1488adca764375fab7b100747b97d
ananthsub <ananth.subramaniam@gmail.com>
2021-04-22 11:26:05 -0700

    mapping?

--- pytorch_lightning/utilities/types.py
+++ pytorch_lightning/utilities/types.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterator, List, Union
+from typing import Any, Iterator, List, Mapping, Union
 
 import torch
 from torchmetrics import Metric
@@ -8,6 +8,6 @@ Convention:
  - Types used in public hooks (as those in the `LightningModule` and `Callback`) should be public (no trailing `_`)
 """
 _METRIC = Union[Metric, torch.Tensor, int, float]
-STEP_OUTPUT = Union[torch.Tensor, Dict[str, Any]]
+STEP_OUTPUT = Union[torch.Tensor, Mapping[str, Any]]
 EPOCH_OUTPUT = List[STEP_OUTPUT]
 _PARAMETERS = Iterator[torch.nn.Parameter]
