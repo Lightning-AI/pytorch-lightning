50eb86e0a9044d3eff148e0a2693c7247fae4352
ananthsub <ananth.subramaniam@gmail.com>
2021-04-23 00:08:21 -0700

    rm-trainer-logging

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -26,6 +26,7 @@ from pytorch_lightning.trainer.connectors.logger_connector.epoch_result_store im
 from pytorch_lightning.trainer.connectors.logger_connector.metrics_holder import MetricsHolder
 from pytorch_lightning.trainer.states import RunningStage, TrainerState
 from pytorch_lightning.utilities import DeviceType
+from pytorch_lightning.utilities.metrics import metrics_to_scalars
 
 
 class LoggerConnector:
@@ -210,7 +211,7 @@ class LoggerConnector:
         metrics.update(grad_norm_dic)
 
         # turn all tensors to scalars
-        scalar_metrics = self.trainer.metrics_to_scalars(metrics)
+        scalar_metrics = metrics_to_scalars(metrics)
 
         if "step" in scalar_metrics and step is None:
             step = scalar_metrics.pop("step")
