478d452c3e362de84d87edd3bc6c5bc2048d2d50
Carlos Mocholi <carlossmocholi@gmail.com>
2021-04-27 15:45:32 +0200

    flake8

--- tests/core/test_results.py
+++ tests/core/test_results.py
@@ -173,11 +173,9 @@ def test_result_obj_predictions(tmpdir, test_option: int, do_train: bool, gpus: 
     if do_train:
         trainer.fit(model, dm)
         assert trainer.state == TrainerState.FINISHED, f"Training failed with {trainer.state}"
-        result = trainer.test(datamodule=dm)
-        # TODO: add end-to-end test
-        # assert result[0]['test_loss'] < 0.6
+        trainer.test(datamodule=dm)
     else:
-        result = trainer.test(model, datamodule=dm)
+        trainer.test(model, datamodule=dm)
 
     # check prediction file now exists and is of expected length
     assert prediction_file.exists()
