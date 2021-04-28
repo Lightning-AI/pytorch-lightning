4f79a0f7c229b3eb85f478d7b095649bb474c53b
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-28 15:03:37 +0200

    rename batchsplitloop to optimizerloop and add missing helper methods

--- pytorch_lightning/loops/batch_loop.py
+++ pytorch_lightning/loops/batch_loop.py
@@ -1,4 +1,5 @@
 from pytorch_lightning.loops.base import Loop
+from pytorch_lightning.loops.optimizer_loop import OptimizerLoop
 from pytorch_lightning.utilities import AttributeDict
 
 
@@ -8,10 +9,10 @@ class BatchLoop(Loop):
     def on_run_start(self, batch, batch_idx, dataloader_idx):
         self._grad_norm_dic = {}
         self.trainer.hiddens = None
-        self._optimizers = self.prepare_optimizers()
+        # self._optimizers = self.prepare_optimizers()
         # lightning module hook
         self._splits = enumerate(self.tbptt_split_batch(batch))
-        self.tbptt_loop = BatchSplitLoop(self._optimizers)
+        self.tbptt_loop = OptimizerLoop()
 
     def on_advance_start(self):
         return super().on_advance_start()
@@ -44,72 +45,10 @@ class BatchLoop(Loop):
 
         return super().run(batch, batch_idx, dataloader_idx)
 
-
-class BatchSplitLoop(Loop):
-    """ Runs over a single split of a batch of data (TBPTT). """
-
-    def __init__(self, optimizers):
-        super().__init__()
-        self._optimizers = enumerate(optimizers)
-
-    def advance(self, split_batch, split_idx, batch_idx):
-        opt_idx, optimizer = next(self._optimizers)
-
-        # toggle model params + set info to logger_connector
-        self.run_train_split_start(split_idx, split_batch, opt_idx, optimizer)
-
-        if self.should_accumulate():
-            # For gradient accumulation
-
-            # -------------------
-            # calculate loss (train step + train step end)
-            # -------------------
-
-            # automatic_optimization=True: perform dpp sync only when performing optimizer_step
-            # automatic_optimization=False: don't block synchronization here
-            with self.block_ddp_sync_behaviour():
-                self.training_step_and_backward(split_batch, batch_idx, opt_idx, optimizer, self.trainer.hiddens)
-
-            batch_outputs = self._process_closure_result(
-                batch_outputs=batch_outputs,
-                opt_idx=opt_idx,
-            )
-
-        # ------------------------------
-        # BACKWARD PASS
-        # ------------------------------
-        # gradient update with accumulated gradients
-
-        else:
-            if self.automatic_optimization:
-
-                def train_step_and_backward_closure():
-                    result = self.training_step_and_backward(
-                        split_batch, batch_idx, opt_idx, optimizer, self.trainer.hiddens
-                    )
-                    return None if result is None else result.loss
-
-                # optimizer step
-                self.optimizer_step(optimizer, opt_idx, batch_idx, train_step_and_backward_closure)
-
-            else:
-                self._curr_step_result = self.training_step(split_batch, batch_idx, opt_idx, self.trainer.hiddens)
-
-            if self._curr_step_result is None:
-                # user decided to skip optimization
-                # make sure to zero grad.
-                # TODO add logic to skip in the outer loop
-                return
-                # continue
-
-            batch_outputs = self._process_closure_result(
-                batch_outputs=batch_outputs,
-                opt_idx=opt_idx,
-            )
-
-            # todo: Properly aggregate grad_norm accros opt_idx and split_idx
-            grad_norm_dic = self._cur_grad_norm_dict
-            self._cur_grad_norm_dict = None
-
-            # update running loss + reset accumulated loss
-            self.update_running_loss()
+    def tbptt_split_batch(self, batch):
+        splits = [batch]
+        if self.trainer.truncated_bptt_steps is not None:
+            model_ref = self.trainer.lightning_module
+            with self.trainer.profiler.profile("tbptt_split_batch"):
+                splits = model_ref.tbptt_split_batch(batch, self.trainer.truncated_bptt_steps)
+        return splits
