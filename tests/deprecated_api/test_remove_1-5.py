6da747e7757ede4df0063a865b5280c077f0c3ff
Daniel Mesejo-León <mesejoleon@gmail.com>
2021-05-04 02:01:41 +0200

    Deprecate `LightningModule.datamodule` reference in favor of the trainer one (#6929) (#7168)
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- tests/deprecated_api/test_remove_1-5.py
+++ tests/deprecated_api/test_remove_1-5.py
@@ -27,7 +27,7 @@ from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.profiler import AdvancedProfiler, BaseProfiler, PyTorchProfiler, SimpleProfiler
 from pytorch_lightning.trainer.callback_hook import warning_cache as callback_warning_cache
 from tests.deprecated_api import no_deprecated_call
-from tests.helpers import BoringModel
+from tests.helpers import BoringModel, BoringDataModule
 from tests.helpers.utils import no_warning_call
 
 
@@ -380,3 +380,12 @@ def test_v1_5_0_lighting_module_grad_norm(tmpdir):
     model = BoringModel()
     with pytest.deprecated_call(match="is deprecated in v1.3 and will be removed in v1.5"):
         model.grad_norm(2)
+
+
+def test_v1_5_0_datamodule_setter():
+    model = BoringModel()
+    datamodule = BoringDataModule()
+    with no_deprecated_call(match="The `LightningModule.datamodule`"):
+        model.datamodule = datamodule
+    with pytest.deprecated_call(match="The `LightningModule.datamodule`"):
+        _ = model.datamodule
