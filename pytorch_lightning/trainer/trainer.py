339df3bd84faba5f01c34b97a23e69cd8057c747
Leonard Lausen <leonard@lausen.nl>
2021-05-04 06:42:57 +0000

    Fix `Trainer.plugins` type declaration (#7288)
    * Fix trainer.plugins type declaration
    
    * Don't ClusterEnvironment(Plugin)
    
    * fix import error, yapf formatter
    
    * Add test
    
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -30,6 +30,7 @@ from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.core.step_result import Result
 from pytorch_lightning.loggers import LightningLoggerBase
 from pytorch_lightning.plugins import Plugin
+from pytorch_lightning.plugins.environments import ClusterEnvironment
 from pytorch_lightning.profiler import BaseProfiler
 from pytorch_lightning.trainer.callback_hook import TrainerCallbackHookMixin
 from pytorch_lightning.trainer.configuration_validator import ConfigValidator
@@ -137,7 +138,7 @@ class Trainer(
         terminate_on_nan: bool = False,
         auto_scale_batch_size: Union[str, bool] = False,
         prepare_data_per_node: bool = True,
-        plugins: Optional[Union[Plugin, str, list]] = None,
+        plugins: Optional[Union[List[Union[Plugin, ClusterEnvironment, str]], Plugin, ClusterEnvironment, str]] = None,
         amp_backend: str = 'native',
         amp_level: str = 'O2',
         distributed_backend: Optional[str] = None,
