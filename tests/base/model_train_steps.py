3ee8962ecba9c9aadb695fe4ad6c6c15e15e9ca2
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-06 14:11:01 +0200

    flake8

--- tests/base/model_train_steps.py
+++ tests/base/model_train_steps.py
@@ -11,11 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math
 from abc import ABC
 from collections import OrderedDict
-
-import torch
 
 
 class TrainingStepVariations(ABC):
