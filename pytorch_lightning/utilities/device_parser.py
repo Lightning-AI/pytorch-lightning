99811aaacfd7691c3a9ce5c3a3e6f9575113a6a9
Stancl, Daniel <daniel.stancl@firma.seznam.cz>
2021-05-06 12:46:51 +0200

    Add import Set to device_parser.py

--- pytorch_lightning/utilities/device_parser.py
+++ pytorch_lightning/utilities/device_parser.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import operator
-from typing import Any, List, MutableSequence, Optional, Tuple, Union
+from typing import Any, List, MutableSequence, Optional, Set, Tuple, Union
 
 import torch
 
