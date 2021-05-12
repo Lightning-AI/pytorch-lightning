9769afa70db015cfe8ed24f3fd62a26da5102c01
jirka <jirka.borovec@seznam.cz>
2021-05-12 14:40:48 +0200

    warning_utils

--- tests/deprecated_api/test_remove_1-4.py
+++ tests/deprecated_api/test_remove_1-4.py
@@ -45,9 +45,6 @@ def test_v1_4_0_deprecated_trainer_methods():
 
 
 def test_v1_4_0_deprecated_imports():
-    _soft_unimport_module('pytorch_lightning.utilities.warning_utils')
-    with pytest.deprecated_call(match='will be removed in v1.4'):
-        from pytorch_lightning.utilities.warning_utils import WarningCache  # noqa: F811 F401
 
     _soft_unimport_module('pytorch_lightning.utilities.xla_device_utils')
     with pytest.deprecated_call(match='will be removed in v1.4'):
