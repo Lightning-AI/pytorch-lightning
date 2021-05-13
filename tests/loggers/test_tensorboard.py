c2d6706d10f5cc413232daacc6bba594bb4a8c28
Jan-Henrik Lambrechts <janhenrik.lambrechts@outlook.be>
2021-05-13 11:10:44 +0200

    removed model init

--- tests/loggers/test_tensorboard.py
+++ tests/loggers/test_tensorboard.py
@@ -131,7 +131,7 @@ def test_tensorboard_log_sub_dir(tmpdir):
         def name(self):
             return "name"
 
-    model = BoringModel()
+
     trainer_args = dict(
         default_root_dir=tmpdir,
         max_steps=1,
