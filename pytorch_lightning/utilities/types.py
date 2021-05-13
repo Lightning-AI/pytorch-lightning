bfabb36ee50fd8fd7c08afbc5c02f9a07372b725
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-13 02:39:13 +0200

    Resolve TODO

--- pytorch_lightning/utilities/types.py
+++ pytorch_lightning/utilities/types.py
@@ -28,8 +28,9 @@ EPOCH_OUTPUT = List[STEP_OUTPUT]
 _EVALUATE_OUTPUT = List[Dict[str, float]]  # 1 dict per DataLoader
 _PREDICT_OUTPUT = Union[List[Any], List[List[Any]]]
 _PARAMETERS = Iterator[torch.nn.Parameter]
-# TODO: expand allowed train_dataloaders types once fixed.
-TRAIN_DATALOADERS = Union[DataLoader, Sequence[DataLoader], Dict[str, DataLoader],
+TRAIN_DATALOADERS = Union[DataLoader, Sequence[DataLoader], Sequence[Sequence[DataLoader]], Sequence[Dict[str,
+                                                                                                          DataLoader]],
+                          Dict[str, DataLoader], Dict[str, Dict[str, DataLoader]], Dict[str, Sequence[DataLoader]],
                           # can't import, otherwise circular imports
                           'CombinedLoader',  # noqa: F821 undefined name 'CombinedLoader'
                           ]
