7b8d2e1027879438df42c9575f8d40cba255cd81
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-14 16:00:38 +0200

    Refactor progress structure

--- pytorch_lightning/trainer/progress.py
+++ pytorch_lightning/trainer/progress.py
@@ -11,72 +11,147 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from dataclasses import dataclass
 
 
 @dataclass
 class Progress:
-    """ Basic dataclass to track event progress.
+    """
+    Basic dataclass to track event progress.
 
     Args:
-        ready: Intended to track the number of batches fetched from the dataloader.
-        started: Intended to be incremented after `on_*_batch_start` completes.
-        processed: Intended to be incremented after `*_step` runs.
-        completed: Intended to be incremented after `on_*_batch_end` runs.
+        ready: Intended to track the number of events ready to start.
+        started: Intended to be incremented after `on_*_start` completes.
+        processed: Intended to be incremented after the event is processed.
+        completed: Intended to be incremented after `on_*_end` completes.
     """
     ready: int = 0
     started: int = 0
     processed: int = 0
     completed: int = 0
 
+    def reset(self) -> None:
+        self.ready = 0
+        self.started = 0
+        self.processed = 0
+        self.completed = 0
+
+
+@dataclass
+class BaseProgress:
+    """
+    Basic dataclass to track event progress.
+
+    Args:
+        total: Intended to track the total progress of an event
+        current: Intended to track the current progress of an event
+    """
+    total: Progress = Progress()
+    current: Progress = Progress()
+
+    def increment_ready(self) -> None:
+        self.total.ready += 1
+        self.current.ready += 1
+
+    def increment_started(self) -> None:
+        self.total.started += 1
+        self.current.started += 1
+
+    def increment_processed(self) -> None:
+        self.total.processed += 1
+        self.current.processed += 1
+
+    def increment_completed(self) -> None:
+        self.total.completed += 1
+        self.current.completed += 1
+
 
 @dataclass
 class LoopProgress:
-    """Basic dataclass to track loop progress during execution.
+    """
+    Dataclass to track loop progress during execution.
 
-    The general structure is to count totals across epochs as well as intra-epoch counts.
-    These counters are local to a trainer rank.
-    By default, they are not globally synced across all ranks.
+    These counters are local to a trainer rank. By default, they are not globally synced across all ranks.
 
     Args:
-        total: Tracks the progress counters across epochs
-        epoch: Tracks the progress within the current epoch.
-        epochs_completed_total: Track the total number of passes through the dataset
+        epoch: Tracks epochs progress.
+        batch: Tracks batch progress.
     """
-    total = Progress()
-    epoch = Progress()
-    epochs_completed_total: int = 0
+    epoch: BaseProgress = BaseProgress()
+    batch: BaseProgress = BaseProgress()
 
     def increment_batch_ready(self) -> None:
-        self.total.ready += 1
-        self.epoch.ready += 1
+        self.batch.increment_ready()
 
     def increment_batch_started(self) -> None:
-        self.total.started += 1
-        self.epoch.started += 1
+        self.batch.increment_started()
 
     def increment_batch_processed(self) -> None:
-        self.total.processed += 1
-        self.epoch.processed += 1
+        self.batch.increment_processed()
 
     def increment_batch_completed(self) -> None:
-        self.total.completed += 1
-        self.epoch.completed += 1
+        self.batch.increment_completed()
 
     def increment_epoch_completed(self) -> None:
-        self.epochs_completed_total += 1
+        self.epoch.total.completed += 1
         self.reset_on_epoch()
 
     def reset_on_epoch(self) -> None:
-        self.epoch = Progress()
+        self.epoch.current.reset()
 
 
 @dataclass
-class TrainerProgress:
-    """ Basic dataclass to track loop progress across stages during trainer execution. """
+class OptimizationProgress:
+    """
+    Dataclass to track optimization progress.
 
-    train: LoopProgress
-    val: LoopProgress
-    test: LoopProgress
-    predict: LoopProgress
+    Args:
+        optimizer: Tracks optimizer progress.
+        scheduler: Tracks scheduler progress.
+    """
+    optimizer: BaseProgress = BaseProgress()
+    scheduler: BaseProgress = BaseProgress()
+
+    @property
+    def optimizer_steps(self) -> int:
+        return self.optimizer.total.completed
+
+    @property
+    def scheduler_steps(self) -> int:
+        return self.scheduler.total.completed
+
+
+@dataclass
+class TrainProgress(BaseProgress):
+    """
+    Extends the progress with training specific attributes
+
+    Args:
+        optimization: Tracks optimization progress
+    """
+    optimization: OptimizationProgress = OptimizationProgress()
+
+
+@dataclass
+class TrainLoopProgress(LoopProgress):
+    epoch: TrainProgress = TrainProgress()
+
+
+@dataclass
+class FitLoopProgress:
+    train: TrainProgress = TrainLoopProgress()
+    val: LoopProgress = LoopProgress()
+
+
+@dataclass
+class LoopState:
+    """
+    Basic dataclass to track loop progress across trainer functions during trainer execution.
+
+    This class will be removed and these attributes will live in each loop.
+    """
+
+    fit: FitLoopProgress = FitLoopProgress()
+    val: LoopProgress = LoopProgress()
+    test: LoopProgress = LoopProgress()
+    predict: LoopProgress = LoopProgress()
