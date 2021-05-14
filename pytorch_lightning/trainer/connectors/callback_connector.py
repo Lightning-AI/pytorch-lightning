c071503684551543f15557cdcd46be60f023d73c
ananthsub <ananth.subramaniam@gmail.com>
2021-05-13 22:01:13 -0700

    [feat] Add stronger validation for checkpoint_callback configuration

--- pytorch_lightning/trainer/connectors/callback_connector.py
+++ pytorch_lightning/trainer/connectors/callback_connector.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 import os
 from datetime import timedelta
+from pathlib import Path
 from typing import Dict, List, Optional, Union
 
 from pytorch_lightning.callbacks import Callback, ModelCheckpoint, ProgressBar, ProgressBarBase
@@ -29,14 +30,14 @@ class CallbackConnector:
 
     def on_trainer_init(
         self,
-        callbacks,
-        checkpoint_callback,
-        progress_bar_refresh_rate,
-        process_position,
-        default_root_dir,
-        weights_save_path,
-        resume_from_checkpoint,
-        stochastic_weight_avg,
+        callbacks: Optional[Union[List[Callback], Callback]],
+        checkpoint_callback: bool,
+        progress_bar_refresh_rate: Optional[int],
+        process_position: int,
+        default_root_dir: Optional[str],
+        weights_save_path: Optional[str],
+        resume_from_checkpoint: Optional[Union[Path, str]],
+        stochastic_weight_avg: bool,
         max_time: Optional[Union[str, timedelta, Dict[str, int]]] = None,
     ):
         self.trainer.resume_from_checkpoint = resume_from_checkpoint
@@ -69,7 +70,11 @@ class CallbackConnector:
         # it is important that these are the last callbacks to run
         self.trainer.callbacks = self._reorder_callbacks(self.trainer.callbacks)
 
-    def configure_checkpoint_callbacks(self, checkpoint_callback: Union[ModelCheckpoint, bool]):
+    def configure_checkpoint_callbacks(self, checkpoint_callback: bool) -> None:
+        if not isinstance(checkpoint_callback, bool):
+            raise MisconfigurationException(
+                f"Invalid type provided for checkpoint_callback: Expected bool but received {type(checkpoint_callback)}."
+            )
         if self._trainer_has_checkpoint_callbacks() and checkpoint_callback is False:
             raise MisconfigurationException(
                 "Trainer was configured with checkpoint_callback=False but found ModelCheckpoint"
