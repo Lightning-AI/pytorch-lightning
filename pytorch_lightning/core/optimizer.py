172031d67046e7a7974f65c29a83504bc02c5921
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-17 12:57:36 +0200

    fix pepe

--- pytorch_lightning/core/optimizer.py
+++ pytorch_lightning/core/optimizer.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import types
 from contextlib import contextmanager
 from typing import Callable, Optional
 from weakref import proxy
