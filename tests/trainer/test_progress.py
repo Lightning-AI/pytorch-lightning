97cf176dd955a9fa6b17fb9dbdbe832bc6e0dc92
ananthsub <ananth.subramaniam@gmail.com>
2021-05-17 23:34:24 -0700

    Update test_progress.py

--- tests/trainer/test_progress.py
+++ tests/trainer/test_progress.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 from pytorch_lightning.trainer.progress import LoopProgress, ProgressState
 
 
