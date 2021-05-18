430c7d660c26b1e21d052bee255677504acf867d
ananthsub <ananth.subramaniam@gmail.com>
2021-05-17 23:31:33 -0700

    address-comments

--- pytorch_lightning/trainer/progress.py
+++ pytorch_lightning/trainer/progress.py
@@ -12,71 +12,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
-class Progress:
-    """ Basic dataclass to track event progress.
+class ProgressState:
+    """
+    Basic dataclass to track event progress.
 
     Args:
-        ready: Intended to track the number of batches fetched from the dataloader.
-        started: Intended to be incremented after `on_*_batch_start` completes.
-        processed: Intended to be incremented after `*_step` runs.
-        completed: Intended to be incremented after `on_*_batch_end` runs.
+        ready: Intended to track the number of events ready to start.
+        started: Intended to be incremented after the event is started (e.g. after `on_*_start runs).
+        processed: Intended to be incremented after the event is processed.
+        completed: Intended to be incremented after the event completes (e.g. after `on_*_end` runs).
     """
     ready: int = 0
     started: int = 0
     processed: int = 0
     completed: int = 0
 
+    def reset(self) -> None:
+        self.ready = 0
+        self.started = 0
+        self.processed = 0
+        self.completed = 0
+
+
+@dataclass
+class Progress:
+    """
+    Basic dataclass to track aggregated and current progress states.
+
+    Args:
+        total: Intended to track the total progress of an event
+        current: Intended to track the current progress of an event
+    """
+    total: ProgressState = field(default_factory=ProgressState)
+    current: ProgressState = field(default_factory=ProgressState)
+
+    def increment_ready(self) -> None:
+        self.total.ready += 1
+        self.current.ready += 1
+
+    def increment_started(self) -> None:
+        self.total.started += 1
+        self.current.started += 1
+
+    def increment_processed(self) -> None:
+        self.total.processed += 1
+        self.current.processed += 1
+
+    def increment_completed(self) -> None:
+        self.total.completed += 1
+        self.current.completed += 1
+
 
 @dataclass
 class LoopProgress:
-    """Basic dataclass to track loop progress during execution.
-
-    The general structure is to count totals across epochs as well as intra-epoch counts.
-    These counters are local to a trainer rank.
-    By default, they are not globally synced across all ranks.
-
-    Args:
-        total: Tracks the progress counters across epochs
-        epoch: Tracks the progress within the current epoch.
-        epochs_completed_total: Track the total number of passes through the dataset
     """
-    total = Progress()
-    epoch = Progress()
-    epochs_completed_total: int = 0
+    Dataclass to track loop progress during execution.
 
-    def increment_batch_ready(self) -> None:
-        self.total.ready += 1
-        self.epoch.ready += 1
-
-    def increment_batch_started(self) -> None:
-        self.total.started += 1
-        self.epoch.started += 1
-
-    def increment_batch_processed(self) -> None:
-        self.total.processed += 1
-        self.epoch.processed += 1
-
-    def increment_batch_completed(self) -> None:
-        self.total.completed += 1
-        self.epoch.completed += 1
+    These counters are local to a trainer rank. By default, they are not globally synced across all ranks.
+    Args:
+        epoch: Tracks epochs progress.
+        batch: Tracks batch progress.
+    """
+    epoch: Progress = field(default_factory=Progress)
+    batch: Progress = field(default_factory=Progress)
 
     def increment_epoch_completed(self) -> None:
-        self.epochs_completed_total += 1
+        self.epoch.increment_completed()
         self.reset_on_epoch()
 
     def reset_on_epoch(self) -> None:
-        self.epoch = Progress()
-
-
-@dataclass
-class TrainerProgress:
-    """ Basic dataclass to track loop progress across stages during trainer execution. """
-
-    train: LoopProgress
-    val: LoopProgress
-    test: LoopProgress
-    predict: LoopProgress
+        self.batch.current.reset()
+        self.epoch.current.reset()
--- tests/trainer/test_progress.py
+++ tests/trainer/test_progress.py
@@ -14,114 +14,139 @@
 
 import logging
 
-from pytorch_lightning.trainer.progress import LoopProgress
+from pytorch_lightning.trainer.progress import LoopProgress, ProgressState
 
 
-def test_increment_batch_ready(tmpdir):
+def test_increment_ready(tmpdir):
     prog = LoopProgress()
-    prog.increment_batch_ready()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == prog.total.ready
+    prog.batch.increment_ready()
+    assert prog.batch.total.ready == 1
+    assert prog.batch.current.ready == prog.batch.total.ready
 
 
-def test_increment_batch_started(tmpdir):
+def test_increment_started(tmpdir):
     prog = LoopProgress()
-    prog.increment_batch_started()
-    logging.error(prog)
-    assert prog.total.ready == 0
-    assert prog.epoch.started == 1
-    assert prog.epoch.started == prog.total.started
+    prog.epoch.increment_started()
+    assert prog.batch.total.ready == 0
+    assert prog.epoch.total.ready == 0
+    assert prog.epoch.total.started == 1
+    assert prog.epoch.total.started == prog.epoch.current.started
 
 
-def test_increment_batch_processed(tmpdir):
+def test_increment_processed(tmpdir):
     prog = LoopProgress()
-    prog.increment_batch_processed()
-    assert prog.total.started == 0
-    assert prog.total.processed == 1
-    assert prog.epoch.processed == prog.total.processed
+    prog.epoch.increment_processed()
+    assert prog.batch.total.ready == 0
+    assert prog.batch.total.started == 0
+    assert prog.epoch.total.started == 0
+    assert prog.epoch.total.processed == 1
+    assert prog.epoch.total.processed == prog.epoch.current.processed
 
 
-def test_increment_batch_completed(tmpdir):
+def test_increment_completed(tmpdir):
     prog = LoopProgress()
-    prog.increment_batch_completed()
-    assert prog.total.processed == 0
-    assert prog.total.completed == 1
-    assert prog.epoch.completed == prog.total.completed
+    prog.epoch.increment_completed()
+    assert prog.batch.total.ready == 0
+    assert prog.batch.total.started == 0
+    assert prog.epoch.total.started == 0
+    assert prog.epoch.total.processed == 0
+    assert prog.epoch.total.completed == 1
+    assert prog.epoch.total.completed == prog.epoch.current.completed
 
 
 def test_increment_epoch(tmpdir):
     """ Test sequences for incrementing epochs. """
     prog = LoopProgress()
+    prog.batch.increment_completed()
+    assert prog.batch.current.completed == 1
+
     prog.increment_epoch_completed()
-    assert prog.epochs_completed_total == 1
+    prog.increment_epoch_completed()
+    assert prog.epoch.current.completed == 0
+    assert prog.epoch.total.completed == 2
+    assert prog.batch.current.completed == 0
+    assert prog.batch.total.completed == 1
+
+
+def test_reset_on_epoch(tmpdir):
+    """ Test sequences for resetting. """
+    prog = LoopProgress()
+
+    prog.batch.increment_started()
+    assert prog.batch.total.started == 1
+    assert prog.epoch.total.started == 0
+
+    prog.reset_on_epoch()
+    assert prog.batch.current.started == 0
+    assert prog.batch.total == ProgressState(started=1)
+
+    prog.batch.increment_started()
+    assert prog.batch.total == ProgressState(started=2)
+    assert prog.epoch.total.started == 0
 
 
 def test_increment_batch_ready_start_process_finish_epoch(tmpdir):
     """ Test sequences for incrementing batches reads and epochs. """
     prog = LoopProgress()
 
-    prog.increment_batch_ready()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 1
+    prog.epoch.increment_ready()
+    assert prog.epoch.total.ready == 1
+    assert prog.epoch.current.ready == 1
+    assert prog.batch.total.ready == 0
+    assert prog.batch.current.ready == 0
 
-    prog.increment_batch_started()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 1
-    assert prog.total.started == 1
-    assert prog.epoch.started == 1
+    prog.epoch.increment_started()
+    assert prog.epoch.total.ready == 1
+    assert prog.epoch.current.ready == 1
+    assert prog.epoch.total.started == 1
+    assert prog.epoch.current.started == 1
+    assert prog.batch.total.started == 0
+    assert prog.batch.current.started == 0
 
-    prog.increment_batch_processed()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 1
-    assert prog.total.started == 1
-    assert prog.epoch.started == 1
-    assert prog.total.processed == 1
-    assert prog.epoch.processed == 1
+    prog.batch.increment_ready()
+    assert prog.batch.total.ready == 1
+    assert prog.batch.current.ready == 1
+    assert prog.epoch.total.ready == 1
+    assert prog.epoch.current.ready == 1
 
-    prog.increment_batch_completed()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 1
-    assert prog.total.started == 1
-    assert prog.epoch.started == 1
-    assert prog.total.processed == 1
-    assert prog.epoch.processed == 1
-    assert prog.total.completed == 1
-    assert prog.epoch.completed == 1
+    prog.batch.increment_started()
+    assert prog.batch.total.started == 1
+    assert prog.batch.current.started == 1
+    assert prog.epoch.total.started == 1
+    assert prog.epoch.current.started == 1
 
-    prog.increment_epoch_completed()
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 0
-    assert prog.total.started == 1
-    assert prog.epoch.started == 0
-    assert prog.total.processed == 1
-    assert prog.epoch.processed == 0
-    assert prog.total.completed == 1
-    assert prog.epoch.completed == 0
-    assert prog.epochs_completed_total == 1
+    prog.batch.increment_processed()
+    assert prog.batch.total.processed == 1
+    assert prog.batch.current.processed == 1
+    assert prog.epoch.total.processed == 0
+    assert prog.epoch.current.processed == 0
 
-    prog.increment_batch_ready()
-    assert prog.total.ready == 2
-    assert prog.epoch.ready == 1
-    assert prog.epochs_completed_total == 1
+    prog.batch.increment_completed()
+    assert prog.batch.total.completed == 1
+    assert prog.batch.current.completed == 1
+    assert prog.epoch.total.completed == 0
+    assert prog.epoch.current.completed == 0
 
-    prog.reset_on_epoch()
-    assert prog.epoch.ready == 0
-
-
-def test_reset_on_epoch(tmpdir):
-    """ Test sequences for resetting. """
-    prog = LoopProgress()
-    prog.increment_batch_ready()
-    prog.reset_on_epoch()
-
-    assert prog.total.ready == 1
-    assert prog.epoch.ready == 0
-
-    prog.increment_batch_ready()
-    assert prog.total.ready == 2
-    assert prog.epoch.ready == 1
+    prog.epoch.increment_processed()
+    assert prog.batch.total.processed == 1
+    assert prog.batch.current.processed == 1
+    assert prog.epoch.total.processed == 1
+    assert prog.epoch.current.processed == 1
 
     prog.increment_epoch_completed()
-    assert prog.total.ready == 2
-    assert prog.epoch.ready == 0
-    assert prog.epochs_completed_total == 1
+    assert prog.batch.total.completed == 1
+    assert prog.batch.current.completed == 0
+    assert prog.epoch.total.completed == 1
+    assert prog.epoch.current.completed == 0
+
+    prog.epoch.increment_ready()
+    assert prog.epoch.total.ready == 2
+    assert prog.epoch.current.ready == 1
+
+    prog.batch.increment_ready()
+    assert prog.batch.total.ready == 2
+    assert prog.batch.current.ready == 1
+
+    prog.reset_on_epoch()
+    assert prog.batch.current.ready == 0
+    assert prog.epoch.current.ready == 0
