f166edc7058ecda32a7182bb716a146c5bbf2250
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-22 02:31:12 +0200

    WIP

--- pytorch_lightning/core/step_result.py
+++ pytorch_lightning/core/step_result.py
@@ -13,41 +13,39 @@
 # limitations under the License.
 import numbers
 from copy import deepcopy
-from dataclasses import dataclass, field
-from typing import Any, Callable, Dict, Iterable, List, MutableMapping, Optional, Tuple, Union
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Iterable, Optional, Union
 
 import torch
-from torch import Tensor, tensor
+from torch import Tensor
 from torchmetrics import Metric
 
 from pytorch_lightning.utilities.distributed import sync_ddp_if_available, tpu_distributed
 from pytorch_lightning.utilities.enums import LightningEnum
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from pytorch_lightning.utilities.types import _METRIC
 
 
 class DefaultMetricsKeys(LightningEnum):
+    CALLBACK = "callback"
+    PBAR = "pbar"
+    LOG = "log"
 
-    CALLBACK_METRICS = "callback_metrics"
-    PBAR_METRICS = "pbar_metrics"
-    LOG_METRICS = "log_metrics"
 
-
+# TODO: remove
 class Result:
     pass
 
 
 @dataclass
 class Metadata:
-
-    hook_name: str
+    fx: str  # TODO: distinction?
     name: str
-    on_prog_bar: bool
-    on_logger: bool = True
+    prog_bar: bool = False
+    logger: bool = True
     on_step: bool = False
     on_epoch: bool = True
     reduce_fx: Callable = torch.mean
-    tbptt_reduce_fx: Callable = torch.mean
-    tbptt_pad_token: int = 0
     dataloader_idx: Optional[int] = None
     is_tensor: bool = True
 
@@ -70,17 +68,19 @@ class Metadata:
 
 class ResultMetric(Metric):
 
-    def __init__(self, metatata: Metadata):
+    def __init__(self, metadata: Metadata) -> None:
         super().__init__()
-
-        self.meta = metatata
+        self.meta = metadata
         if self.meta.is_tensor:
+            # TODO: dist_reduce_fx?
             self.add_state("values", [])
             self.add_state("batch_sizes", [])
 
-    def update(self, value: Union[torch.Tensor, Metric], batch_size: int) -> None:
+    def update(self, value: _METRIC, batch_size: Optional[int] = None) -> None:
         if self.meta.is_tensor:
             self.values.append(value)
+            if batch_size is None:
+                batch_size = self.extract_batch_size(value)
             self.batch_sizes.append(batch_size)
         else:
             self.value = value
@@ -88,7 +88,7 @@ class ResultMetric(Metric):
     def compute(self) -> torch.Tensor:
         if self.meta.is_tensor:
             if self.reduce_fx == torch.mean:
-                return (tensor(self.values) * tensor(self.batch_sizes)).sum() / sum(self.batch_sizes)
+                return (torch.tensor(self.values) * torch.tensor(self.batch_sizes)).sum() / sum(self.batch_sizes)
             elif self.reduce_fx == torch.max:
                 return max(self.values)
             else:
@@ -96,27 +96,10 @@ class ResultMetric(Metric):
         else:
             return self.value.compute()
 
-
-class ResultCollection(dict):
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.minimize: Optional[Tensor] = None
-        self.on_epoch_end_reached: bool = False
-        self.default_metrics = {
-            DefaultMetricsKeys.CALLBACK_METRICS: {},
-            DefaultMetricsKeys.PBAR_METRICS: {},
-            DefaultMetricsKeys.LOG_METRICS: {},
-        }
-
-    @property
-    def metrics_fn(self):
-        return self.get_epoch_metrics if self.on_epoch_end_reached else self.get_batch_metrics
-
     @staticmethod
     def extract_batch_size(batch: Any) -> int:
         try:
-            return ResultCollection._extract_batch_size(batch)
+            return ResultMetric._extract_batch_size(batch)
         except RecursionError:
             return 1
 
@@ -134,13 +117,26 @@ class ResultCollection(dict):
             return len(batch)
         elif isinstance(batch, dict):
             sample = next(iter(batch.values()), 1)
-            size = ResultCollection._extract_batch_size(sample)
+            size = ResultMetric._extract_batch_size(sample)
         elif isinstance(batch, Iterable):
             sample = next(iter(batch), 1)
-            size = ResultCollection._extract_batch_size(sample)
+            size = ResultMetric._extract_batch_size(sample)
         else:
             size = 1
         return size
+
+
+class ResultCollection(dict):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.minimize: Optional[Tensor] = None
+        self.on_epoch_end_reached: bool = False
+        self.default_metrics = {k: {} for k in DefaultMetricsKeys}
+
+    @property
+    def metrics(self) -> Dict[str, Dict[str, torch.Tensor]]:
+        return self.get_epoch_metrics() if self.on_epoch_end_reached else self.get_batch_metrics()
 
     @staticmethod
     def _sync(
@@ -190,8 +186,6 @@ class ResultCollection(dict):
         on_step: bool = False,
         on_epoch: bool = True,
         reduce_fx: Callable = torch.mean,
-        tbptt_reduce_fx: Callable = torch.mean,
-        tbptt_pad_token: int = 0,
         enable_graph: bool = False,
         sync_dist: bool = False,
         sync_dist_op: Union[Any, str] = 'mean',
@@ -219,31 +213,27 @@ class ResultCollection(dict):
         if isinstance(value, torch.Tensor) and value.device.type == "xla":
             value = value.cpu()
 
-        if batch_size is None:
-            raise MisconfigurationException("`batch_size` should be provided.")
+        key = f"{hook_name}.{name}"
 
-        storage_key = f"{hook_name}.{name}"
-
-        if storage_key not in self:
+        if key not in self:
             result = ResultMetric(
                 Metadata(
-                    hook_name=hook_name,
+                    fx=hook_name,
                     name=name,
-                    on_prog_bar=prog_bar,
-                    on_logger=logger,
+                    prog_bar=prog_bar,
+                    logger=logger,
                     on_step=on_step,
                     on_epoch=on_epoch,
                     reduce_fx=reduce_fx,
-                    tbptt_reduce_fx=tbptt_reduce_fx,
-                    tbptt_pad_token=tbptt_pad_token,
                     dataloader_idx=dataloader_idx,
                 )
             )
-            self[storage_key] = result
+            self[key] = result
 
-        self[storage_key](value, batch_size)
+        self[key](value, batch_size)
 
     def get_batch_metrics(self) -> Dict[str, Dict[str, torch.Tensor]]:
+        # TODO: do we need deepcopy?
         metrics = deepcopy(self.default_metrics)
 
         for result_metric in self.values():
@@ -251,30 +241,28 @@ class ResultCollection(dict):
                 continue
 
             foward_cache: torch.Tensor = result_metric._forward_cache.detach()
-            name: str = result_metric.meta.name
-            name_forked: str = result_metric.meta.forked_step_name
 
-            if result_metric.meta.on_prog_bar:
-                metrics[DefaultMetricsKeys.PBAR_METRICS][name_forked] = foward_cache
-
-            if result_metric.meta.on_logger:
-                metrics[DefaultMetricsKeys.LOG_METRICS][name_forked] = foward_cache
-
-            metrics[DefaultMetricsKeys.CALLBACK_METRICS][name] = foward_cache
+            name_forked = result_metric.meta.forked_step_name
+            if result_metric.meta.prog_bar:
+                metrics[DefaultMetricsKeys.PBAR][name_forked] = foward_cache
+            if result_metric.meta.logger:
+                metrics[DefaultMetricsKeys.LOG][name_forked] = foward_cache
+            metrics[DefaultMetricsKeys.CALLBACK][result_metric.meta.name] = foward_cache
 
         return metrics
 
+    # TODO: add_dataloader_idx?
     def get_batch_log_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics to log at the end of the batch"""
-        return self.get_batch_metrics()[DefaultMetricsKeys.LOG_METRICS]
+        return self.get_batch_metrics()[DefaultMetricsKeys.LOG]
 
     def get_batch_pbar_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics to include in the progress_bar at the end of the batch"""
-        return self.get_batch_metrics()[DefaultMetricsKeys.PBAR_METRICS]
+        return self.get_batch_metrics()[DefaultMetricsKeys.PBAR]
 
     def get_batch_callback_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics for the callbacks at the end of the batch"""
-        return self.get_batch_metrics()[DefaultMetricsKeys.CALLBACK_METRICS]
+        return self.get_batch_metrics()[DefaultMetricsKeys.CALLBACK]
 
     def get_epoch_metrics(self) -> Dict[str, Dict[str, torch.Tensor]]:
         metrics = deepcopy(self.default_metrics)
@@ -287,39 +275,36 @@ class ResultCollection(dict):
                 result_metric.compute()
 
             computed: torch.Tensor = result_metric._computed.detach()
-            name: str = result_metric.meta.name
+
             name_forked: str = result_metric.meta.forked_epoch_name
-
-            if result_metric.meta.on_prog_bar:
-                metrics[DefaultMetricsKeys.PBAR_METRICS][name_forked] = computed
-
-            if result_metric.meta.on_logger:
-                metrics[DefaultMetricsKeys.LOG_METRICS][name_forked] = computed
-
-            metrics[DefaultMetricsKeys.CALLBACK_METRICS][name] = computed
+            if result_metric.meta.prog_bar:
+                metrics[DefaultMetricsKeys.PBAR][name_forked] = computed
+            if result_metric.meta.logger:
+                metrics[DefaultMetricsKeys.LOG][name_forked] = computed
+            metrics[DefaultMetricsKeys.CALLBACK][result_metric.meta.name] = computed
 
         return metrics
 
     def get_epoch_log_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics to log at the end of the epoch"""
-        return self.get_epoch_metrics()[DefaultMetricsKeys.LOG_METRICS]
+        return self.get_epoch_metrics()[DefaultMetricsKeys.LOG]
 
     def get_epoch_pbar_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics to include in the progress_bar at the end of the epoch"""
-        return self.get_epoch_metrics()[DefaultMetricsKeys.PBAR_METRICS]
+        return self.get_epoch_metrics()[DefaultMetricsKeys.PBAR]
 
     def get_epoch_callback_metrics(self, add_dataloader_idx: bool = False) -> Dict[str, torch.Tensor]:
         """Gets the metrics for the callbacks at the end of the epoch"""
-        return self.get_epoch_metrics()[DefaultMetricsKeys.CALLBACK_METRICS]
+        return self.get_epoch_metrics()[DefaultMetricsKeys.CALLBACK]
 
     def to(self, *args, **kwargs) -> 'ResultCollection':
         """Move all data to the given device."""
-        for item in self.values():
-            if isinstance(item, ResultMetric):
-                item.to(*args, **kwargs)
+        for k, v in self.items():
+            if isinstance(v, (torch.Tensor, Metric)):
+                self[k] = v.to(*args, **kwargs)
         return self
 
-    def cpu(self) -> 'Result':
+    def cpu(self) -> 'ResultCollection':
         """Move all data to CPU."""
         return self.to(device="cpu")
 
