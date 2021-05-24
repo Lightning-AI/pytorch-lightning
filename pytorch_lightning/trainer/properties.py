aac4ec527ab52eac4ffafa5d3f3f15d12094d968
Kaushik B <kaushikbokka@gmail.com>
2021-05-24 23:18:11 +0530

    Add property to Trainer

--- pytorch_lightning/trainer/properties.py
+++ pytorch_lightning/trainer/properties.py
@@ -98,6 +98,10 @@ class TrainerProperties(ABC):
         return getattr(self.accelerator.training_type_plugin, "world_size", 1)
 
     @property
+    def should_save_checkpoint(self) -> bool:
+        return self.accelerator.training_type_plugin.should_save_checkpoint
+
+    @property
     def _distrib_type(self) -> DistributedType:
         return self.accelerator_connector._distrib_type
 
