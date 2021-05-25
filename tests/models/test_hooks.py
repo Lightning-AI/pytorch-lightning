00ccee0349ee68aea70761bf56fa1759e6c015da
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-25 13:40:43 +0200

    Bad merge

--- tests/models/test_hooks.py
+++ tests/models/test_hooks.py
@@ -524,39 +524,6 @@ def test_trainer_model_hook_system_fit_no_val(tmpdir):
     assert model.called == expected
 
 
-def test_trainer_model_hook_system_fit_no_val(tmpdir):
-    model = HookedModel()
-    train_batches = 2
-    trainer = Trainer(
-        default_root_dir=tmpdir,
-        max_epochs=1,
-        limit_val_batches=0,
-        limit_train_batches=train_batches,
-        progress_bar_refresh_rate=0,
-        weights_summary=None,
-    )
-    assert model.called == []
-    trainer.fit(model)
-    expected = [
-        'setup_fit',
-        'on_fit_start',
-        'on_pretrain_routine_start',
-        'on_pretrain_routine_end',
-        'on_train_start',
-        'on_epoch_start',
-        'on_train_epoch_start',
-        *(model.train_batch * train_batches),
-        'training_epoch_end',
-        'on_train_epoch_end',
-        'on_epoch_end',
-        'on_save_checkpoint',  # from train epoch end
-        'on_train_end',
-        'on_fit_end',
-        'teardown_fit',
-    ]
-    assert model.called == expected
-
-
 def test_trainer_model_hook_system_validate(tmpdir):
     model = HookedModel()
     trainer = Trainer(
