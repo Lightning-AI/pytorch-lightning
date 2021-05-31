34e4ee03c533d5b4ba4183f7357404f90c852a56
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-31 13:27:08 +0200

    call time_elapsed

--- pytorch_lightning/callbacks/timer.py
+++ pytorch_lightning/callbacks/timer.py
@@ -170,4 +170,6 @@ class Timer(Callback):
         should_stop = trainer.accelerator.broadcast(should_stop)
         trainer.should_stop = trainer.should_stop or should_stop
         if should_stop and self._verbose:
-            rank_zero_info(f"Time limit reached. Elapsed time is {self.time_elapsed}. Signaling Trainer to stop.")
+            rank_zero_info(
+                f"Time limit reached. Elapsed time is {self.time_elapsed(RunningStage.TRAINING)}."
+                f" Signaling Trainer to stop.")
