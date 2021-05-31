4621a525857b38cc6e5d86621e1843f9fb55a868
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-31 13:09:28 +0200

    cosmetic change

--- pytorch_lightning/loops/training_loop.py
+++ pytorch_lightning/loops/training_loop.py
@@ -50,7 +50,7 @@ class TrainingLoop(Loop):
 
     @property
     def done(self):
-        max_steps_reached = (self.max_steps is not None and self.max_steps <= self.global_step)
+        max_steps_reached = self.max_steps is not None and self.global_step >= self.max_steps
         return max_steps_reached or self.trainer.should_stop or self._num_training_batches_reached(self.is_last_batch)
 
     def run(self, *args, **kwargs):
