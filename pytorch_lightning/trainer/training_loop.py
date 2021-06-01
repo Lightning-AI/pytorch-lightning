371c058800ed38a1064b91d3a22d63192e0ba4c5
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-06-01 07:51:48 +0200

    Update training_loop.py

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -269,14 +269,15 @@ class TrainLoop:
             if training_step_output.grad_fn is None:
                 # TODO: Find why - RuntimeError: Expected to mark a variable ready only once ...
                 raise MisconfigurationException("In manual optimization, `training_step` should not return a Tensor")
-            elif self.trainer.automatic_optimization:
-                if not (
-                    isinstance(training_step_output, torch.Tensor) or
-                    (isinstance(training_step_output, Mapping) and 'loss' in training_step_output)
-                ):
-                    raise MisconfigurationException(
-                        "In automatic optimization, `training_step` must either return a Tensor or a dict with key 'loss'"
-                    )
+        elif self.trainer.automatic_optimization:
+            if not (
+                isinstance(training_step_output, torch.Tensor) or
+                (isinstance(training_step_output, Mapping) and 'loss' in training_step_output)
+            ):
+                raise MisconfigurationException(
+                    "In automatic optimization, `training_step` must either return a Tensor, "
+                    "a dict with key 'loss' or None (where the step will be skipped)"
+                )
 
     def training_step(self, split_batch, batch_idx, opt_idx, hiddens):
         # give the PL module a result for logging
