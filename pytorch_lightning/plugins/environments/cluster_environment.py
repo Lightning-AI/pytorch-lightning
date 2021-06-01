173210d36b118285650ac11b184dd3fc7c289cb8
Shuying Sun <shuyingsun@fb.com>
2021-06-01 12:47:12 -0700

    format

--- pytorch_lightning/plugins/environments/cluster_environment.py
+++ pytorch_lightning/plugins/environments/cluster_environment.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 from abc import ABC, abstractmethod
 from typing import Dict
+
 from pytorch_lightning.utilities import rank_zero_warn
-import os
 
 
 class ClusterEnvironment(ABC):
--- tests/plugins/environments/test_cluster_environment_with_overriden_parameters.py
+++ tests/plugins/environments/test_cluster_environment_with_overriden_parameters.py
@@ -1,68 +1,82 @@
-import logging
 import os
 from unittest import mock
 
 import pytest
+from pytorch_lightning.plugins.environments import (
+    ClusterEnvironment,
+    KubeflowEnvironment,
+    SLURMEnvironment,
+    TorchElasticEnvironment,
+    LightningEnvironment,
+)
 
-from pytorch_lightning.plugins.environments import KubeflowEnvironment, SLURMEnvironment, TorchElasticEnvironment, LightningEnvironment
 
-
-@pytest.mark.parametrize(["cluster_environment_type"], [("kubeflow", ), ("lightning", ), ("slurm", ), ("torchelastic", )])
+@pytest.mark.parametrize(
+    ["cluster_environment_type"],
+    [("kubeflow",), ("lightning",), ("slurm",), ("torchelastic",)],
+)
 def test_default_environ_parameters(cluster_environment_type: str):
     """
     Test for setting default environ parameters.
     """
     if cluster_environment_type == "kubeflow":
-        env = KubeflowEnvironment()
+        KubeflowEnvironment()
     elif cluster_environment_type == "lightning":
-        env = LightningEnvironment()
+        LightningEnvironment()
     elif cluster_environment_type == "slurm":
-        env = SLURMEnvironment()
+        SLURMEnvironment()
     elif cluster_environment_type == "torchelastic":
-        env = TorchElasticEnvironment()
+        TorchElasticEnvironment()
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") is not None
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") == "4"
     assert os.environ.get("NCCL_SOCKET_NTHREADS") is not None
     assert os.environ.get("NCCL_SOCKET_NTHREADS") == "2"
 
+
 @mock.patch.dict(
-    os.environ, {
+    os.environ,
+    {
         "NCCL_NSOCKS_PERTHREA": "3",
-    }
+    },
 )
-@pytest.mark.parametrize(["cluster_environment_type"], [("kubeflow", ), ("lightning", ), ("slurm", ), ("torchelastic", )])
+@pytest.mark.parametrize(
+    ["cluster_environment_type"],
+    [("kubeflow",), ("lightning",), ("slurm",), ("torchelastic",)],
+)
 def test_with_not_overriden_default_environ_parameters(cluster_environment_type: str):
     """
     Test for not setting default environ parameters when parameter is already set in `os.environ`.
     """
     if cluster_environment_type == "kubeflow":
-        env = KubeflowEnvironment()
+        KubeflowEnvironment()
     elif cluster_environment_type == "lightning":
-        env = LightningEnvironment()
+        LightningEnvironment()
     elif cluster_environment_type == "slurm":
-        env = SLURMEnvironment()
+        SLURMEnvironment()
     elif cluster_environment_type == "torchelastic":
-        env = TorchElasticEnvironment()
+        TorchElasticEnvironment()
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") is not None
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") == "3"
     assert os.environ.get("NCCL_SOCKET_NTHREADS") is not None
     assert os.environ.get("NCCL_SOCKET_NTHREADS") == "2"
 
 
-
-@pytest.mark.parametrize(["cluster_environment_type"], [("kubeflow", ), ("lightning", ), ("slurm", ), ("torchelastic", )])
+@pytest.mark.parametrize(
+    ["cluster_environment_type"],
+    [("kubeflow",), ("lightning",), ("slurm",), ("torchelastic",)],
+)
 def test_with_user_defined_environ_parameters(cluster_environment_type: str):
     """
     Test for overriding environ parameters when user provide `envrion_settings`.
     """
     if cluster_environment_type == "kubeflow":
-        env = KubeflowEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
+        KubeflowEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
     elif cluster_environment_type == "lightning":
-        env = LightningEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
+        LightningEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
     elif cluster_environment_type == "slurm":
-        env = SLURMEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
+        SLURMEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
     elif cluster_environment_type == "torchelastic":
-        env = TorchElasticEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
+        TorchElasticEnvironment(environ_settings={"NCCL_NSOCKS_PERTHREA": "1"})
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") is not None
     assert os.environ.get("NCCL_NSOCKS_PERTHREA") == "1"
     assert os.environ.get("NCCL_SOCKET_NTHREADS") is not None
