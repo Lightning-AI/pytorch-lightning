59ebfbd92676adf6a3c8b658b68cdf3a42e2aed8
Andrew Tritt <ajtritt@lbl.gov>
2021-06-03 13:51:17 -0400

    add test for setting training_type_plugin

---
+++ tests/trainer/connectors/test_accelerator_connector.py
@@ -0,0 +1,29 @@
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.accelerators import Accelerator
+from pytorch_lightning.plugins import PrecisionPlugin, DDPPlugin, SingleDevicePlugin
+from pytorch_lightning.trainer.connectors.accelerator_connector import AcceleratorConnector
+
+def test_accelerator_training_type_plugin():
+    """Test that the training_type_plugin pulled from accelearator"""
+
+    # check that this works for different types of plugins to ensure
+    # there are no dependencies on TrainingTypePlugin class refinements
+
+    precision_plugin = PrecisionPlugin()
+    singledev_plugin = SingleDevicePlugin(torch.device('cpu'))
+    accelerator = Accelerator(
+        precision_plugin=precision_plugin,
+        training_type_plugin=singledev_plugin,
+    )
+    trainer = Trainer(accelerator=accelerator)
+    assert trainer.training_type_plugin is singledev_plugin
+
+    precision_plugin = PrecisionPlugin()
+    ddp_plugin = DDPPlugin()
+    accelerator = Accelerator(
+        precision_plugin=precision_plugin,
+        training_type_plugin=ddp_plugin,
+    )
+    trainer = Trainer(accelerator=accelerator)
+    assert trainer.training_type_plugin is ddp_plugin
