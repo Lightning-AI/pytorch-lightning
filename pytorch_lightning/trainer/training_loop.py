6a0d50369382ec3e7cce681facc9a2aad2a71d00
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-06-04 17:34:39 +0200

    Add warning to trainstep output (#7779)
    * Update training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update test_training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update test_training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update training_loop.py
    
    * Update pytorch_lightning/trainer/training_loop.py
    
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>
    
    * Update test_training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update pytorch_lightning/trainer/training_loop.py
    
    Co-authored-by: ananthsub <ananth.subramaniam@gmail.com>
    
    * Update training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update test_training_loop.py
    
    * Update training_loop.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * escape regex
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>
    Co-authored-by: ananthsub <ananth.subramaniam@gmail.com>

--- pytorch_lightning/trainer/training_loop.py
+++ pytorch_lightning/trainer/training_loop.py
@@ -16,7 +16,7 @@ from collections import OrderedDict
 from contextlib import contextmanager, suppress
 from copy import copy
 from functools import partial, update_wrapper
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -269,6 +269,16 @@ class TrainLoop:
             if training_step_output.grad_fn is None:
                 # TODO: Find why - RuntimeError: Expected to mark a variable ready only once ...
                 raise MisconfigurationException("In manual optimization, `training_step` should not return a Tensor")
+        elif self.trainer.lightning_module.automatic_optimization:
+            if not any((
+                isinstance(training_step_output, torch.Tensor),
+                (isinstance(training_step_output, Mapping)
+                 and 'loss' in training_step_output), training_step_output is None
+            )):
+                raise MisconfigurationException(
+                    "In automatic optimization, `training_step` must either return a Tensor, "
+                    "a dict with key 'loss' or None (where the step will be skipped)."
+                )
 
     def training_step(self, split_batch, batch_idx, opt_idx, hiddens):
         # give the PL module a result for logging
