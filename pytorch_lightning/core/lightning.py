fa921d85cd38082c2698a516e2e4653b997a4ad7
Carlos Mocholi <carlossmocholi@gmail.com>
2021-06-08 12:19:48 +0200

    Avoid partial

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -326,12 +326,9 @@ class LightningModule(
             )
 
         # check for invalid values
-        apply_to_collection(value, dict, partial(self.__check_not_nested, name))
+        apply_to_collection(value, dict, self.__check_not_nested, name)
         apply_to_collection(
-            value,
-            object,
-            partial(self.__check_allowed, name, value),
-            wrong_dtype=(numbers.Number, Metric, Tensor, dict)
+            value, object, self.__check_allowed, name, value, wrong_dtype=(numbers.Number, Metric, Tensor, dict)
         )
 
         # set the default depending on the fx_name
@@ -447,14 +444,14 @@ class LightningModule(
         return sync_fn(value, group=sync_dist_group, reduce_op=sync_dist_op)
 
     @staticmethod
-    def __check_not_nested(name: str, value: dict) -> None:
+    def __check_not_nested(value: dict, name: str) -> None:
         # self-imposed restriction. for simplicity
         if any(isinstance(v, dict) for v in value.values()):
             raise ValueError(f'`self.log({name}, {value})` was called, but nested dictionaries cannot be logged')
         return value
 
     @staticmethod
-    def __check_allowed(name: str, value: Any, v: Any) -> None:
+    def __check_allowed(v: Any, name: str, value: Any) -> None:
         raise ValueError(f'`self.log({name}, {value})` was called, but `{type(v).__name__}` values cannot be logged')
 
     def write_prediction(
@@ -551,8 +548,7 @@ class LightningModule(
         group = group if group is not None else torch.distributed.group.WORLD
         all_gather = self.trainer.accelerator.all_gather
         data = convert_to_tensors(data, device=self.device)
-        all_gather = partial(all_gather, group=group, sync_grads=sync_grads)
-        return apply_to_collection(data, torch.Tensor, all_gather)
+        return apply_to_collection(data, torch.Tensor, all_gather, group=group, sync_grads=sync_grads)
 
     def forward(self, *args, **kwargs) -> Any:
         r"""
