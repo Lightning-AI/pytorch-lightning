762832621d6c448e3c8d69a581a78f77fffd51ff
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-09 23:50:13 +0200

    imports

--- pytorch_lightning/loops/training_batch_loop.py
+++ pytorch_lightning/loops/training_batch_loop.py
@@ -1,6 +1,19 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from collections import OrderedDict
 from contextlib import contextmanager
-from copy import copy
 from functools import partial, update_wrapper
 from typing import Any, Callable, List, Mapping, Optional, Tuple
 
@@ -20,7 +33,6 @@ from pytorch_lightning.utilities.signature_utils import is_param_in_hook_signatu
 from pytorch_lightning.utilities.warnings import WarningCache
 
 
-# TODO: typing
 class TrainingBatchLoop(Loop):
     """ Runs over a single batch of data. """
 
