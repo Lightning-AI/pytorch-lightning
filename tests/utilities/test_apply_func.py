b53932b883c265df57da1a675337785b9e0d0168
dalek-who <839991169@qq.com>
2021-06-11 14:50:05 +0800

    remove unused type-hints

--- tests/utilities/test_apply_func.py
+++ tests/utilities/test_apply_func.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Union, List, Dict, Tuple, Optional
+from typing import List
 import numbers
 from collections import namedtuple, OrderedDict
 import dataclasses
