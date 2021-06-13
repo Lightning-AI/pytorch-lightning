3357c1a03e135a858e5d4040f921052eea419e3e
Jirka Borovec <Borda@users.noreply.github.com>
2021-06-13 18:28:04 +0200

    \s
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- requirements/adjust_versions.py
+++ requirements/adjust_versions.py
@@ -41,7 +41,7 @@ def main(path_req: str, torch_version: Optional[str] = None) -> None:
     with open(path_req, "r") as fp:
         req = fp.read()
     # remove comments
-    req = re.sub(rf"\W*#.*{os.linesep}", os.linesep, req)
+    req = re.sub(rf"\s*#.*{os.linesep}", os.linesep, req)
 
     latest = find_latest(torch_version)
     for lib, version in latest.items():
