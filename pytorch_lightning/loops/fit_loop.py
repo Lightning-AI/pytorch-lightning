70cdb144238276315412fc5b242fd1c8bfd48699
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-14 17:38:58 +0200

    carlos

--- pytorch_lightning/loops/fit_loop.py
+++ pytorch_lightning/loops/fit_loop.py
@@ -51,11 +51,8 @@ class FitLoop(Loop):
         max_steps: Optional[int] = None
     ):
         super().__init__()
-        # If neither max_epochs or max_steps is set, then use existing default of max_epochs = 1000
         self.max_epochs = 1000 if (max_epochs is None and max_steps is None) else max_epochs
-        # If neither min_epochs or min_steps is set, then use existing default of min_epochs = 1
         self.min_epochs = 1 if (min_epochs is None and min_steps is None) else min_epochs
-
         self.training_loop = TrainingEpochLoop(min_steps, max_steps)
         self.results = ResultCollection(training=True)
 
