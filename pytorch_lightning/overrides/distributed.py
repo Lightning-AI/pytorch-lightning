94c72836b20e46d95d6206352cb933f7f001a34f
SeanNaren <sean@grid.ai>
2021-06-15 10:58:44 +0100

    Simplify message

--- pytorch_lightning/overrides/distributed.py
+++ pytorch_lightning/overrides/distributed.py
@@ -63,8 +63,7 @@ def _find_tensors(obj):  # pragma: no-cover
 # Note: Keep track of Pytorch DDP and update if there is a change
 # https://github.com/pytorch/pytorch/blob/v1.7.1/torch/nn/parallel/distributed.py#L626-L638
 def prepare_for_backward(model: DistributedDataParallel, output: Any):
-    # DDP based plugin are being subclasses
-    # and `prepare_for_backward` is `DistributedDataParallel` specific.
+    # `prepare_for_backward` is `DistributedDataParallel` specific.
     if not isinstance(model, DistributedDataParallel):
         return
     if torch.is_grad_enabled() and model.require_backward_grad_sync:
