44ac6af9e0660f57aa1f5d1ece5e29289999800c
AffineParameter <austin.basye@hudl.com>
2021-06-15 09:53:17 -0500

    next attempt to satisfy the linter

--- tests/callbacks/test_lr_monitor.py
+++ tests/callbacks/test_lr_monitor.py
@@ -323,7 +323,7 @@ def test_lr_monitor_duplicate_custom_pg_names(tmpdir):
             return x
 
         def configure_optimizers(self):
-            optimizer = torch.optim.SGD([
+            param_groups = [
                 {
                     'params': [p for p in self.linear_a.parameters()],
                     'name': 'linear'
@@ -332,8 +332,8 @@ def test_lr_monitor_duplicate_custom_pg_names(tmpdir):
                     'params': [p for p in self.linear_b.parameters()],
                     'name': 'linear'
                 },
-            ],
-                                        lr=0.1)
+            ]
+            optimizer = torch.optim.SGD(param_groups, lr=0.1)
             lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
             return [optimizer], [lr_scheduler]
 
