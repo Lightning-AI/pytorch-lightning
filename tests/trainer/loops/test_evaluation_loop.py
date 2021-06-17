b7483b425cab95290eb2cbf354ccb0a77004df83
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-18 00:32:41 +0200

    check which assert is failing

--- tests/trainer/loops/test_evaluation_loop.py
+++ tests/trainer/loops/test_evaluation_loop.py
@@ -95,14 +95,16 @@ def test_memory_consumption_validation(tmpdir):
             # there is a batch and the boring model, but not two batches on gpu, assume 32 bit = 4 bytes
             lower = 101 * self.num_params * 4
             upper = 201 * self.num_params * 4
-            assert lower < torch.cuda.memory_allocated(0) < upper
+            assert lower < torch.cuda.memory_allocated(self.device)
+            assert torch.cuda.memory_allocated(self.device) < upper
             return super().training_step(batch, batch_idx)
 
         def validation_step(self, batch, batch_idx):
             # there is a batch and the boring model, but not two batches on gpu, assume 32 bit = 4 bytes
             lower = 101 * self.num_params * 4
             upper = 201 * self.num_params * 4
-            assert lower < torch.cuda.memory_allocated(0) < upper
+            assert lower < torch.cuda.memory_allocated(self.device)
+            assert torch.cuda.memory_allocated(self.device) < upper
             return super().validation_step(batch, batch_idx)
 
     torch.cuda.empty_cache()
