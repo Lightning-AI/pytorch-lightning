cab35a6251f19be266db1d3a8a9600f1098d44db
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-17 12:14:46 +0200

    remove unused type import

--- pytorch_lightning/loops/dataloader/evaluation_dataloader_loop.py
+++ pytorch_lightning/loops/dataloader/evaluation_dataloader_loop.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Sequence, Tuple, Union
+from typing import Any, List, Optional, Sequence, Union
 
 from deprecate.utils import void
 from torch.utils.data.dataloader import DataLoader
