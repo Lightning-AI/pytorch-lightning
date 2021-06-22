b60a142d0b8bb66b9e24063e43315b586c4f0568
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-22 12:40:08 +0200

    imports

--- pytorch_lightning/loops/dataloader/__init__.py
+++ pytorch_lightning/loops/dataloader/__init__.py
@@ -14,4 +14,3 @@
 
 from pytorch_lightning.loops.dataloader.dataloader_loop import DataLoaderLoop  # noqa: F401
 from pytorch_lightning.loops.dataloader.evaluation_loop import EvaluationLoop  # noqa: F401
-from pytorch_lightning.loops.dataloader.prediction_loop import PredictionLoop  # noqa: F401
