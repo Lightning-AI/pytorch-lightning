979bc23009eb8fd6289924e422d5f215f7dab085
tchaton <thomas@grid.ai>
2021-06-23 09:48:36 +0100

    resolve bug

--- pytorch_lightning/trainer/connectors/checkpoint_connector.py
+++ pytorch_lightning/trainer/connectors/checkpoint_connector.py
@@ -450,7 +450,7 @@ class CheckpointConnector:
 
     def get_result_collections_state_dict(self):
         return {
-            RunningStage.TRAINING.value: self.trainer.fit_loop.results.state_dict(),
+            RunningStage.TRAINING.value: self.trainer.fit_loop.training_loop.results.state_dict(),
             RunningStage.SANITY_CHECKING.value: self.trainer.fit_loop.validation_loop.results.state_dict(),
             RunningStage.VALIDATING.value: self.trainer.validation_loop.results.state_dict(),
             RunningStage.TESTING.value: self.trainer.evaluation_loop.results.state_dict(),
