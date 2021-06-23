ccccf15ddea983693b4306dcd08481d0c8e09a5c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-23 11:59:50 +0200

    remove unused imports

--- pytorch_lightning/loops/fit_loop.py
+++ pytorch_lightning/loops/fit_loop.py
@@ -16,9 +16,6 @@ import logging
 from contextlib import suppress
 from typing import Any, Optional
 
-from deprecate import void
-from torch.optim import Optimizer
-
 import pytorch_lightning as pl
 from pytorch_lightning.loops.base import Loop
 from pytorch_lightning.loops.dataloader.evaluation_dataloader_loop import EvaluationDataLoaderLoop
