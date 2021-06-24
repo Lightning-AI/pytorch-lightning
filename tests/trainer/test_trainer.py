f86fc8a374ea15bb79e1570390c2de3337584075
tchaton <thomas@grid.ai>
2021-06-24 17:35:42 +0100

    add grid run test

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -26,7 +26,7 @@ import pytest
 import torch
 from omegaconf import OmegaConf
 from torch.optim import SGD
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 import tests.helpers.utils as tutils
 from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
@@ -1951,8 +1951,102 @@ def test_cloud_fit_local(tmpdir):
 
 @RunIf(grid=True)
 def test_cloud_fit_on_run(tmpdir):
+
     from grid.pytorch_lightning import GridConfig
+
+    class RandomDataset(Dataset):
+
+        def __init__(self, size, length):
+            self.len = length
+            self.data = torch.randn(length, size)
+
+        def __getitem__(self, index):
+            return self.data[index]
+
+        def __len__(self):
+            return self.len
+
+    class BoringModel(LightningModule):
+
+        def __init__(self):
+            """
+            Testing PL Module
+
+            Use as follows:
+            - subclass
+            - modify the behavior for what you want
+
+            class TestModel(BaseTestModel):
+                def training_step(...):
+                    # do your own thing
+
+            or:
+
+            model = BaseTestModel()
+            model.training_epoch_end = None
+
+            """
+            super().__init__()
+            self.layer = torch.nn.Linear(32, 2)
+
+        def forward(self, x):
+            return self.layer(x)
+
+        def loss(self, batch, prediction):
+            # An arbitrary loss to have a loss that updates the model weights during `Trainer.fit` calls
+            return torch.nn.functional.mse_loss(prediction, torch.ones_like(prediction))
+
+        def step(self, x):
+            x = self(x)
+            out = torch.nn.functional.mse_loss(x, torch.ones_like(x))
+            return out
+
+        def training_step(self, batch, batch_idx):
+            output = self(batch)
+            loss = self.loss(batch, output)
+            return {"loss": loss}
+
+        def training_step_end(self, training_step_outputs):
+            return training_step_outputs
+
+        def training_epoch_end(self, outputs) -> None:
+            torch.stack([x["loss"] for x in outputs]).mean()
+
+        def validation_step(self, batch, batch_idx):
+            output = self(batch)
+            loss = self.loss(batch, output)
+            return {"x": loss}
+
+        def validation_epoch_end(self, outputs) -> None:
+            torch.stack([x['x'] for x in outputs]).mean()
+
+        def test_step(self, batch, batch_idx):
+            output = self(batch)
+            loss = self.loss(batch, output)
+            return {"y": loss}
+
+        def test_epoch_end(self, outputs) -> None:
+            torch.stack([x["y"] for x in outputs]).mean()
+
+        def configure_optimizers(self):
+            optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
+            return [optimizer], [lr_scheduler]
+
+        def train_dataloader(self):
+            return DataLoader(RandomDataset(32, 64))
+
+        def val_dataloader(self):
+            return DataLoader(RandomDataset(32, 64))
+
+        def test_dataloader(self):
+            return DataLoader(RandomDataset(32, 64))
+
+        def predict_dataloader(self):
+            return DataLoader(RandomDataset(32, 64))
+
     model = BoringModel()
+
     before_params = deepcopy(model.state_dict())
     trainer = Trainer(default_root_dir=tmpdir, max_epochs=1, limit_train_batches=2, logger=True)
     grid_config = GridConfig(
