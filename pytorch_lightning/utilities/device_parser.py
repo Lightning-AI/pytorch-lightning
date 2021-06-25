6b80f139f688d36fae8a3fea6c8ec757f9dbb368
Stancl, Daniel <daniel.stancl@firma.seznam.cz>
2021-06-25 20:56:22 +0200

    Remove unused import

--- pytorch_lightning/utilities/device_parser.py
+++ pytorch_lightning/utilities/device_parser.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import operator
-from typing import Any, List, MutableSequence, Optional, Tuple, Union, type_check_only
+from typing import Any, List, MutableSequence, Optional, Tuple, Union
 
 import torch
 
