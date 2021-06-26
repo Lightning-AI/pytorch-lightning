ce5175e9c31bee835fd59e3230f3a761a9026cf9
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2021-06-26 10:36:54 +0200

    Fix truncated_bptt_steps hiddens detach()

--- pytorch_lightning/loops/batch/training_batch_loop.py
+++ pytorch_lightning/loops/batch/training_batch_loop.py
@@ -345,8 +345,14 @@ class TrainingBatchLoop(Loop):
         if isinstance(training_step_output, dict):
             loss = training_step_output.pop("loss", None)
             hiddens = training_step_output.pop("hiddens", None)
+
             if hiddens is not None:
-                hiddens = hiddens.detach()
+                # handle cases of hiddens being a Tensor or a tuple of Tensors
+                def detach(tensor):
+                    return tensor.detatch()
+
+                hiddens = apply_to_collection(hiddens, dtype=Tensor, function=detach)
+
             results.extra = training_step_output
 
         # handle scalar return
