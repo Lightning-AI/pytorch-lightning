8838b433b13eb2c107f52423e02c10eb1ec4fa47
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-28 00:57:55 +0200

    fix type

--- pytorch_lightning/loops/batch/training_batch_loop.py
+++ pytorch_lightning/loops/batch/training_batch_loop.py
@@ -138,7 +138,7 @@ class TrainingBatchLoop(Loop):
             if result:
                 self.batch_outputs[0].append(result.training_step_output)
 
-    def on_run_end(self) -> Any:
+    def on_run_end(self) -> None:
         # release memory
         self._remaining_splits = None
 
