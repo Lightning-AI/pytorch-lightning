14e9fdcc038f43bd9d689a2cc7544ee608c5c7a2
Jirka <jirka.borovec@seznam.cz>
2021-06-29 23:32:59 +0200

    pep8

--- pytorch_lightning/metrics/functional/f_beta.py
+++ pytorch_lightning/metrics/functional/f_beta.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Optional
 
 import torch
 from torchmetrics.functional import f1 as _f1
