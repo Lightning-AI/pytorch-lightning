af710855b0e254a696d7151392ac703d5b61fe86
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-04 19:45:07 +0200

    summarize

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -1688,6 +1688,24 @@ class LightningModule(
         return splits
 
     def summarize(self, mode: Optional[str] = "top", max_depth: Optional[int] = None) -> Optional[ModelSummary]:
+        """
+        Summarize this LightingModule.
+
+        Args:
+            mode: Can be either ``'top'`` (summarize only direct submodules) or ``'full'`` (summarize all layers).
+
+                .. deprecated:: v1.4
+                    This parameter was deprecated in v1.4 in favor of `max_depth` and will be removed in v1.6.
+
+            max_depth: The maximum depth of layer nesting that the summary will include. A value of 0 turns the
+                layer summary off. Default: 1.
+
+        Return:
+            ModelSummary: The model summary object
+
+        See Also:
+            - Model summary utility class: :class:`~pytorch_lightning.memory.ModelSummary`
+        """
         model_summary = None
 
         # temporary mapping from mode to max_depth
