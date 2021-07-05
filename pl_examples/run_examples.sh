8e49bfdbd2ad52621bbf3359ca825177f61c9be9
Carlos Mocholi <carlossmocholi@gmail.com>
2021-07-05 19:01:03 +0200

    Keep dir path

--- pl_examples/run_examples.sh
+++ pl_examples/run_examples.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 
-ARGS="--trainer.max_epochs=1 --data.batch_size=32 --trainer.limit_train_batches=2 --trainer.limit_val_batches=2"
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+args="--trainer.max_epochs=1 --data.batch_size=32 --trainer.limit_train_batches=2 --trainer.limit_val_batches=2"
 
-python pl_examples/basic_examples/simple_image_classifier.py $@ ${ARGS}
-python pl_examples/basic_examples/backbone_image_classifier.py $@ ${ARGS}
-python pl_examples/basic_examples/autoencoder.py $@ ${ARGS}
+python ${dir_path}/basic_examples/simple_image_classifier.py $@ ${args}
+python ${dir_path}/basic_examples/backbone_image_classifier.py $@ ${args}
+python ${dir_path}/basic_examples/autoencoder.py $@ ${args}
