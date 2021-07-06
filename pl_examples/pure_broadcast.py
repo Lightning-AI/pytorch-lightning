69c57b997d468166bf502279d90c5bd9f7c2ea2d
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-06 18:02:42 +0200

    x

--- pl_examples/pure_broadcast.py
+++ pl_examples/pure_broadcast.py
@@ -16,24 +16,24 @@ def run(args: Namespace):
     message_str = f"this is local rank {local_rank}"
 
     torch.distributed.init_process_group(backend="nccl", world_size=2, rank=local_rank)
-    print("init successful")
+    print(f"[{local_rank}] init successful")
 
     torch.distributed.broadcast(message_tensor, src=0)
-    print("the tensor message is:", message_tensor)
+    print(f"[{local_rank}] the tensor message is:", message_tensor)
 
     # UNCOMMENT TO REPRODUCE HANG
-    torch.distributed.broadcast_object_list(message_str, src=0)
-    print("the string message is:", message_str)
+    # torch.distributed.broadcast_object_list(message_str, src=0)
+    # print(f"[{local_rank}] the string message is:", message_str)
 
-    print("before wrapping")
+    print(f"[{local_rank}] before wrapping")
     model = nn.Linear(2, 2)
     model.to(device)
     ddp_model = DistributedDataParallel(model, device_ids=[local_rank])
 
-    print("after wrapping")
+    print(f"[{local_rank}] barrier")
     torch.distributed.barrier()
 
-    print("before forward")
+    print(f"[{local_rank}] before forward")
     ddp_model(torch.rand(5, 2).to(device))
 
 
