aa85ce4022396ab9d299820accfd12ba2a69a006
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-07 22:50:44 +0200

    conflict

--- pytorch_lightning/loops/epoch/training_epoch_loop.py
+++ pytorch_lightning/loops/epoch/training_epoch_loop.py
@@ -72,22 +72,20 @@ class TrainingEpochLoop(loops.Loop):
     def connect(
         self,
         trainer: 'pl.Trainer',
+        batch_loop,
+        val_loop,
         *args: Any,
         progress: Optional[TrainingEpochProgress] = None,
         **kwargs: Any
     ) -> None:
         """Connects the loop with necessary arguments like the trainer"""
         super().connect(trainer, *args, **kwargs)
+        self.batch_loop = batch_loop  # or TrainingBatchLoop()
+        self.val_loop = val_loop  # or loops.EvaluationLoop()
         if progress is not None:
             self.progress = progress
         self.batch_loop.connect(trainer, progress=self.progress.batch, optim_progress=self.progress.optim)
         self.val_loop.connect(trainer, progress=self.progress.val)
-    def connect(self, trainer: 'pl.Trainer', batch_loop, val_loop) -> None:
-        """Connects the loop with all necessary parts like trainer and accelerators"""
-        super().connect(trainer)
-        self.batch_loop = batch_loop  #  or TrainingBatchLoop()
-        self.val_loop = val_loop  #or loops.EvaluationLoop()
-        # self.val_loop.connect(trainer)
 
     def reset(self) -> None:
         """Resets the internal state of the loop for a new run"""
