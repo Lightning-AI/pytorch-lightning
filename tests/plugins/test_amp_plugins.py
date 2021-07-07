87ebbb05ba54d25c50068db8ecd2a3010d4d2d0a
Carlos Mocholi <carlossmocholi@gmail.com>
2021-07-07 20:09:38 +0200

    Refactor plugins backward

--- tests/plugins/test_amp_plugins.py
+++ tests/plugins/test_amp_plugins.py
@@ -72,6 +72,11 @@ def test_amp_apex_ddp(
 class GradientUnscaleBoringModel(BoringModel):
 
     def on_after_backward(self):
+        # TODO: replace with `on_before_optimizer_step` so we don't need to check accumulate and unscale manually
+        if self.trainer.fit_loop.should_accumulate():
+            return
+        opt = self.optimizers()
+        self.trainer.accelerator.precision_plugin.scaler.unscale_(opt)
         norm = torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
         if not (torch.isinf(norm) or torch.isnan(norm)):
             assert norm.item() < 15.
