d2f477a79df479044b64074009d875d7ce386f1e
Kaushik B <kaushikbokka@gmail.com>
2021-07-09 02:25:33 +0530

    Fix torchscript issue

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -78,6 +78,7 @@ class LightningModule(
         "model_size",
         "automatic_optimization",
         "truncated_bptt_steps",
+        "loaded_optimizer_states_dict",
     ] + DeviceDtypeModuleMixin.__jit_unused_properties__ + HyperparametersMixin.__jit_unused_properties__
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
