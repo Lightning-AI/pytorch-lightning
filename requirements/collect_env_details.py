da63082b27faef71b9538219dc396fde15425f1e
Jirka <jirka.borovec@seznam.cz>
2021-07-09 20:26:04 +0200

    fixes

--- requirements/collect_env_details.py
+++ requirements/collect_env_details.py
@@ -26,7 +26,7 @@ import numpy
 import torch
 import tqdm
 
-sys.path += [os.path.abspath('..'), os.path.abspath('../tests')]
+sys.path += [os.path.abspath('..'), os.path.abspath('.')]
 import pytorch_lightning  # noqa: E402
 
 LEVEL_OFFSET = '\t'
