452b5f36b341b1e7cdabd240b3f9ea8c2617443a
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-09 15:28:23 +0200

    update references in docs

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -1931,10 +1931,10 @@ class LightningModule(
 
         Args:
             file_path: The path of the file the onnx model should be saved to.
-            input_sample: An input for tracing. Default: None (Use self.example_input_array)
+            input_sample: An input for tracing. Default: None (uses :attr:`example_input_array`)
             model_check_fn: A custom function that conducts onnx model checks. The default function here
                 conducts tests using the onnxruntime.
-            **kwargs: Will be passed to torch.onnx.export function.
+            **kwargs: Will be passed to :func:`torch.onnx.export` function.
 
         Example::
 
