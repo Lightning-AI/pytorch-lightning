53baf920d172ae4882afacf31d7f5239e29c1eb2
SeanNaren <sean@grid.ai>
2021-07-09 14:57:23 +0100

    Fix IPU tests

--- pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -184,7 +184,7 @@ class AcceleratorConnector(object):
                 msg = "TPUs are not available" if not _TPU_AVAILABLE else "you didn't pass `tpu_cores` to `Trainer`"
                 raise MisconfigurationException(f"You passed `accelerator='tpu'`, but {msg}")
             self._accelerator_type = DeviceType.TPU
-        elif self.distributed_backend == DeviceType.IPU:
+        elif self.distributed_backend == DeviceType.IPU or isinstance(self._training_type_plugin, IPUPlugin):
             if not self.has_ipu:
                 msg = "IPUs are not available" if not _IPU_AVAILABLE else "you didn't pass `ipus` to `Trainer`"
                 raise MisconfigurationException(f"You passed `accelerator='ipu'`, but {msg}")
@@ -332,7 +332,7 @@ class AcceleratorConnector(object):
 
     @property
     def use_ipu(self) -> bool:
-        return self._accelerator_type == DeviceType.IPU and self.has_ipu
+        return self._accelerator_type == DeviceType.IPU or self.has_ipu
 
     @property
     def use_dp(self) -> bool:
@@ -406,8 +406,7 @@ class AcceleratorConnector(object):
             if isinstance(self.tpu_cores, int):
                 devices = list(range(self.tpu_cores))
         elif self.use_ipu:
-            if isinstance(self.ipus, int):
-                devices = list(range(self.ipus))
+            devices = list(range(self.num_ipus))
         else:
             devices = [torch.device("cpu")] * self.num_processes
         return devices
