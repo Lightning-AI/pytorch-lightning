ee8d9b80368eacd48d1f10ec99df635efdb9ea44
tchaton <thomas@grid.ai>
2021-07-10 13:20:57 +0200

    update

--- pytorch_lightning/loops/base.py
+++ pytorch_lightning/loops/base.py
@@ -67,7 +67,7 @@ class Loop(ABC):
 
     @trainer.setter
     def trainer(self, trainer: 'pl.Trainer'):
-        """Connect the Trainer to itself and all sub-children loops"""
+        """Connect the Trainer to itself and all its children loops"""
         if not isinstance(trainer, pl.Trainer):
             raise MisconfigurationException(
                 f"Loop {self.__class__.__name__} should be connected to a `Trainer`, found: {trainer}."
