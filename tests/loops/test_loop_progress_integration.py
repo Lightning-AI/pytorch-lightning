0838d7a7fa957f1f20cbd5afe9128400a94101b0
tchaton <thomas@grid.ai>
2021-07-12 20:21:39 +0200

    update

--- tests/loops/test_loop_progress_integration.py
+++ tests/loops/test_loop_progress_integration.py
@@ -9,11 +9,8 @@ def test_loop_progress_integration():
     assert trainer.test_loop.progress is not trainer.predict_loop.progress
     # check the validation progresses are not shared
     assert trainer.fit_loop.epoch_loop.val_loop.progress is not trainer.validate_loop.progress
-    expected = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]["progress"]
-    assert expected == trainer.fit_loop.epoch_loop.progress
-    expected = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]["batch_loop"]["progress"]
-    assert expected == trainer.fit_loop.epoch_loop.batch_loop.progress
-    expected = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]["val_loop"]["progress"]
-    assert expected == trainer.fit_loop.epoch_loop.val_loop.progress
-    expected = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]["val_loop"]["epoch_loop"]["progress"]
-    assert expected == trainer.fit_loop.epoch_loop.val_loop.epoch_loop.progress
+    generated = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]
+    assert generated["progress"] is trainer.fit_loop.epoch_loop.progress
+    assert generated["batch_loop"]["progress"] is trainer.fit_loop.epoch_loop.batch_loop.progress
+    assert generated["val_loop"]["progress"] is trainer.fit_loop.epoch_loop.val_loop.progress
+    assert generated["val_loop"]["epoch_loop"]["progress"] is trainer.fit_loop.epoch_loop.val_loop.epoch_loop.progress
