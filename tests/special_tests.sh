bb5964f263448cd00c9eac8967815bb61d147f07
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-14 12:33:32 +0200

    fix typo

--- tests/special_tests.sh
+++ tests/special_tests.sh
@@ -79,7 +79,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # test that a user can manually launch individual processes
-args="--trainer.gpus 2 --trainer.accelerator ddp --trainer.fast_dev_run=True
+args="--trainer.gpus 2 --trainer.accelerator ddp --trainer.fast_dev_run 1"
 MASTER_ADDR="localhost" MASTER_PORT=1234 LOCAL_RANK=1 python pl_examples/basic_examples/simple_image_classifier.py ${args} &
 MASTER_ADDR="localhost" MASTER_PORT=1234 LOCAL_RANK=0 python pl_examples/basic_examples/simple_image_classifier.py ${args}
 report+="Ran\tmanual ddp launch test\n"
