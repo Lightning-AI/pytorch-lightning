01f87145ce30bc63c06ffc9927d40cf56a4bdb70
Carlos Mocholi <carlossmocholi@gmail.com>
2021-07-14 17:20:40 +0200

    Fix test

--- tests/loops/test_loop_progress_integration.py
+++ tests/loops/test_loop_progress_integration.py
@@ -5,12 +5,16 @@ from tests.loops.test_loops import _collect_loop_progress
 def test_loop_progress_integration():
     trainer = Trainer()
     # check no progresses are shared
-    assert trainer.validate_loop.progress is not trainer.test_loop.progress
-    assert trainer.test_loop.progress is not trainer.predict_loop.progress
+    assert trainer.fit_loop.epoch_progress is not trainer.validate_loop.dataloader_progress
+    assert trainer.validate_loop.dataloader_progress is not trainer.test_loop.dataloader_progress
+    assert trainer.test_loop.dataloader_progress is not trainer.predict_loop.dataloader_progress
     # check the validation progresses are not shared
-    assert trainer.fit_loop.epoch_loop.val_loop.progress is not trainer.validate_loop.progress
-    generated = _collect_loop_progress(trainer.fit_loop)["epoch_loop"]
-    assert generated["progress"] is trainer.fit_loop.epoch_loop.progress
-    assert generated["batch_loop"]["progress"] is trainer.fit_loop.epoch_loop.batch_loop.progress
-    assert generated["val_loop"]["progress"] is trainer.fit_loop.epoch_loop.val_loop.progress
-    assert generated["val_loop"]["epoch_loop"]["progress"] is trainer.fit_loop.epoch_loop.val_loop.epoch_loop.progress
+    assert trainer.fit_loop.epoch_loop.val_loop.dataloader_progress is not trainer.validate_loop.dataloader_progress
+    # check recursive collection of progresses
+    progresses = _collect_loop_progress(trainer.fit_loop)
+    assert progresses["epoch_progress"] is trainer.fit_loop.epoch_progress
+    assert progresses["epoch_loop"]["batch_progress"] is trainer.fit_loop.epoch_loop.batch_progress
+    assert progresses["epoch_loop"]["val_loop"]["dataloader_progress"
+                                                ] is trainer.fit_loop.epoch_loop.val_loop.dataloader_progress
+    assert progresses["epoch_loop"]["val_loop"]["epoch_loop"][
+        "batch_progress"] is trainer.fit_loop.epoch_loop.val_loop.epoch_loop.batch_progress
