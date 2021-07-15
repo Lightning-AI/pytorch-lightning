4a6997e74364ff76f8e30c48961df18e201757c5
Jirka <jirka.borovec@seznam.cz>
2021-07-15 12:40:02 +0200

    test

--- tests/utilities/test_enums.py
+++ tests/utilities/test_enums.py
@@ -2,5 +2,7 @@ from pytorch_lightning.utilities import DeviceType
 
 
 def test_consistency():
+    assert DeviceType.TPU not in ('GPU', 'CPU')
+    assert DeviceType.TPU in ('TPU', 'CPU')
     assert DeviceType.TPU in ('tpu', 'CPU')
     assert DeviceType.TPU in ('tpu', 'CPU')
