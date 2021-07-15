da15bcdbb8004b1f517a17144c3241e7005e798e
Carlos Mocholí <carlossmocholi@gmail.com>
2021-07-15 13:39:52 +0200

    Update tests/utilities/test_enums.py

--- tests/utilities/test_enums.py
+++ tests/utilities/test_enums.py
@@ -5,4 +5,6 @@ def test_consistency():
     assert DeviceType.TPU not in ('GPU', 'CPU')
     assert DeviceType.TPU in ('TPU', 'CPU')
     assert DeviceType.TPU in ('tpu', 'CPU')
-    assert DeviceType.TPU in ('tpu', 'CPU')
+    assert DeviceType.TPU not in {'GPU', 'CPU'}
+    assert DeviceType.TPU in {'TPU', 'CPU'}
+    assert DeviceType.TPU in {'tpu', 'CPU'}
