4b26cd20ae3bfbc7283632a9ebf3fef935c4c936
Carlos Mocholí <carlossmocholi@gmail.com>
2021-07-19 14:24:46 +0200

    Update tests/utilities/test_enums.py
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>

--- tests/utilities/test_enums.py
+++ tests/utilities/test_enums.py
@@ -6,5 +6,6 @@ def test_consistency():
     assert DeviceType.TPU in ('TPU', 'CPU')
     assert DeviceType.TPU in ('tpu', 'CPU')
     assert DeviceType.TPU not in {'GPU', 'CPU'}
-    assert DeviceType.TPU in {'TPU', 'CPU'}
+    # hash cannot be case invariant
+    assert DeviceType.TPU not in {'TPU', 'CPU'}
     assert DeviceType.TPU in {'tpu', 'CPU'}
