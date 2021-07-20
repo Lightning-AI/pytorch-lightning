9bf4ceb9671327ab170616811a0e6292bcc88e5f
Kaushik B <kaushikbokka@gmail.com>
2021-07-20 10:41:33 +0530

    Update test

--- tests/deprecated_api/test_remove_1-6.py
+++ tests/deprecated_api/test_remove_1-6.py
@@ -21,6 +21,7 @@ from pytorch_lightning.core.memory import ModelSummary
 from pytorch_lightning.plugins.training_type import DDPPlugin, DDPSpawnPlugin
 from pytorch_lightning.utilities.distributed import rank_zero_deprecation, rank_zero_warn
 from pytorch_lightning.utilities.model_helpers import is_overridden
+from tests.deprecated_api import _soft_unimport_module
 from tests.helpers import BoringDataModule, BoringModel
 
 
@@ -326,5 +327,6 @@ def test_v1_6_0_deprecated_hpc_load(tmpdir):
 
 def test_v1_6_0_deprecated_device_dtype_mixin_import():
 
+    _soft_unimport_module('pytorch_lightning.utilities.device_dtype_mixin')
     with pytest.deprecated_call(match='will be removed in v1.6'):
         from pytorch_lightning.utilities.device_dtype_mixin import DeviceDtypeModuleMixin  # noqa: F811 F401
