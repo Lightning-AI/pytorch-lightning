4e13cf870b6a4117f681e914fc71d778513e15e9
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-20 11:24:57 +0200

    unused import

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from pprint import pprint
 from typing import Any, Dict, Iterable, Mapping, Optional, Union
 
