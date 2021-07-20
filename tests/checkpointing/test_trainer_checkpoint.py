c05b88ec7fb8e8376bc326f8281f07fe9eeb14ea
tchaton <thomas@grid.ai>
2021-07-20 20:32:41 +0200

    optimize test

--- tests/checkpointing/test_trainer_checkpoint.py
+++ tests/checkpointing/test_trainer_checkpoint.py
@@ -89,7 +89,7 @@ def test_finetuning_with_resume_from_checkpoint(tmpdir):
 
 def test_accumulated_gradient_batches_with_resume_from_checkpoint(tmpdir):
     """
-    This test validates that accumulated gradient properly restart
+    This test validates that accumulated gradient properly is recomputed and reset on the trainer.
     """
 
     train_data = DataLoader(RandomDataset(32, 64), num_workers=0)
@@ -97,11 +97,13 @@ def test_accumulated_gradient_batches_with_resume_from_checkpoint(tmpdir):
 
     cb = ModelCheckpoint(dirpath=tmpdir, save_last=True)
     model = BoringModel()
-    trainer_kwargs = dict(max_epochs=2, accumulate_grad_batches={0: 2}, callbacks=cb)
+    trainer_kwargs = dict(
+        max_epochs=1, accumulate_grad_batches={0: 2}, callbacks=cb, limit_train_batches=1, limit_val_batches=0
+    )
     trainer = Trainer(**trainer_kwargs)
     trainer.fit(model, train_data, val_data)
 
-    trainer_kwargs['max_epochs'] = 3
+    trainer_kwargs['max_epochs'] = 2
     trainer_kwargs['resume_from_checkpoint'] = cb.last_model_path
     trainer = Trainer(**trainer_kwargs)
     trainer.fit(model, train_data, val_data)
