6d951deb93f9d59fc1d13e67b7cf4e343bac6641
manipopopo <pwmutantbread@gmail.com>
2021-07-28 04:19:03 +0000

    Change variable names

--- pytorch_lightning/callbacks/quantization.py
+++ pytorch_lightning/callbacks/quantization.py
@@ -154,7 +154,7 @@ class QuantizationAwareTraining(Callback):
     """
 
     OBSERVER_TYPES = ("histogram", "average")
-    DISABLE_OBSERVER_STAGES = ("train", "validate", "test", "predict")
+    OBSERVER_STAGES = ("train", "validate", "test", "predict")
 
     def __init__(
         self,
@@ -190,10 +190,10 @@ class QuantizationAwareTraining(Callback):
         self._convert_on_fit_end = quantize_on_fit_end
 
         disable_observers = set(disable_observers)
-        unsupported_stages = disable_observers - set(self.DISABLE_OBSERVER_STAGES)
+        unsupported_stages = disable_observers - set(self.OBSERVER_STAGES)
         if unsupported_stages:
             raise MisconfigurationException(
-                f'Unsupported stages "{tuple(sorted(unsupported_stages))}", allowed are {self.DISABLE_OBSERVER_STAGES}.'
+                f'Unsupported stages "{tuple(sorted(unsupported_stages))}", allowed are {self.OBSERVER_STAGES}.'
             )
         self._disable_observer_stages = disable_observers
 
