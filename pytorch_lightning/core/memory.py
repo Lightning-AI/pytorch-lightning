ea27e983f914453b98695b371c1319d943c56ba2
samlurye <samlurye@users.noreply.github.com>
2021-07-30 11:35:47 -0400

    Update pytorch_lightning/core/memory.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pytorch_lightning/core/memory.py
+++ pytorch_lightning/core/memory.py
@@ -14,10 +14,9 @@
 from pytorch_lightning.utilities import rank_zero_deprecation
 
 rank_zero_deprecation(
-    "pytorch_lightning.core.memory.get_memory_profile and "
-    "pytorch_lightning.core.memory.get_gpu_memory_map have been moved "
-    "to pytorch_lightning.utilities.memory since v1.5 and will be "
-    "removed in v1.7."
+    "`pytorch_lightning.core.memory.get_memory_profile` and"
+    " `pytorch_lightning.core.memory.get_gpu_memory_map` have been moved"
+    " to `pytorch_lightning.utilities.memory` since v1.5 and will be removed in v1.7."
 )
 
 # To support backward compatibility as get_memory_profile and get_gpu_memory_map have been moved
