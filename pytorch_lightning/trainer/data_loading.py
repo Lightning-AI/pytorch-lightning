d81b341101b1a589f6a0f271393b6a93035eabff
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-30 14:40:41 +0200

    temporarily remove injecting PL global seed when wrapping iterable ds

--- pytorch_lightning/trainer/data_loading.py
+++ pytorch_lightning/trainer/data_loading.py
@@ -248,12 +248,6 @@ class TrainerDataLoadingMixin(ABC):
 
         # wrap the ``IterableDataset`` into a ``CaptureIterableDataset`` to record sampler states.
         if _fault_tolerant_enabled() and isinstance(dl_kwargs["dataset"], IterableDataset):
-            # force the seed in the ``Dataset``
-            seed = int(os.getenv("PL_GLOBAL_SEED", 0)) + self.current_epoch + self.global_rank
-
-            if dl_kwargs.get("generator") is None:
-                dl_kwargs["generator"] = torch.Generator().manual_seed(seed)
-
             dl_kwargs["dataset"] = CaptureIterableDataset(
                 dataset=dl_kwargs["dataset"], initial_seed=dl_kwargs["generator"].initial_seed()
             )
