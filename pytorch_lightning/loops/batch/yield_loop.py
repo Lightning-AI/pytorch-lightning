140c9252142a4d8cfc6e94506fb3c9db410349de
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-30 13:04:35 +0200

    clean up

--- pytorch_lightning/loops/batch/yield_loop.py
+++ pytorch_lightning/loops/batch/yield_loop.py
@@ -1,20 +1,14 @@
 import inspect
-from functools import update_wrapper, partial
-from typing import Any, Optional, Generator, Callable, Mapping
+from typing import Any, Optional, Generator
 
-import torch
 from torch import Tensor
-from torch.optim import Optimizer
 from pytorch_lightning.loops import Loop
 
 from pytorch_lightning.loops import TrainingBatchLoop
 from pytorch_lightning.utilities import AttributeDict
-from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from pytorch_lightning.utilities.types import STEP_OUTPUT
 
 
 class YieldLoop(TrainingBatchLoop):
-
     def connect(self, **kwargs: "Loop") -> None:
         raise NotImplementedError(f"{self.__class__.__name__} does not connect any child loops.")
 
@@ -31,53 +25,25 @@ class YieldLoop(TrainingBatchLoop):
         # let logger connector extract current batch size
         self.trainer.logger_connector.on_train_split_start(batch_idx, split_idx, split_batch)
 
-        training_step_generator = self._get_training_step_generator(split_batch, batch_idx, opt_idx=0, hiddens=self._hiddens)
+        self._training_step_generator = self._get_training_step_generator(
+            split_batch, batch_idx, opt_idx=0, hiddens=self._hiddens
+        )
 
         for opt_idx, optimizer in enumerate(self.trainer.optimizers):
             self.optim_progress.optimizer_idx = opt_idx
-            result = self._run_optimization(training_step_generator, batch_idx, opt_idx, optimizer)
+            result = self._run_optimization(batch_idx, split_batch, opt_idx, optimizer)
             if result:
-                self.batch_outputs[opt_idx].append(result.training_step_output)
+                self.batch_outputs[opt_idx].append(result.result_collection)
 
-    def _run_optimization(
-        self, training_step_generator: Generator, batch_idx: int, opt_idx: int, optimizer: torch.optim.Optimizer,
-    ):
+    def _make_step_fn(self, split_batch, batch_idx, opt_idx, hiddens):
+        def step_fn():
+            return self._training_step(self._training_step_generator)
 
-        # toggle model params
-        self._run_optimization_start(opt_idx, optimizer)
+        return step_fn
 
-        result = AttributeDict()
-        closure = self._make_closure(training_step_generator, batch_idx, opt_idx, optimizer, result)
-
-        if self.should_accumulate():
-            # For gradient accumulation
-
-            # -------------------
-            # calculate loss (train step + train step end)
-            # -------------------
-            # automatic_optimization=True: perform ddp sync only when performing optimizer_step
-            # automatic_optimization=False: don't block synchronization here
-            with self.block_ddp_sync_behaviour():
-                closure()
-
-        # ------------------------------
-        # BACKWARD PASS
-        # ------------------------------
-        # gradient update with accumulated gradients
-        else:
-            self._optimizer_step(optimizer, opt_idx, batch_idx, closure)
-
-        if result:
-            # if no result, user decided to skip optimization
-            # otherwise update running loss + reset accumulated loss
-            self._update_running_loss(result.loss)
-            self._process_closure_result(result)
-
-        # untoggle model params
-        self._run_optimization_end(opt_idx)
-        return result
-
-    def _get_training_step_generator(self, split_batch: Any, batch_idx: int, opt_idx: int, hiddens: Tensor) -> Generator:
+    def _get_training_step_generator(
+        self, split_batch: Any, batch_idx: int, opt_idx: int, hiddens: Tensor
+    ) -> Generator:
         step_kwargs = self._build_kwargs(split_batch, batch_idx, opt_idx, hiddens)
         generator = self.trainer.accelerator.training_step(step_kwargs)
 
@@ -100,66 +66,13 @@ class YieldLoop(TrainingBatchLoop):
 
             self._check_training_step_output(training_step_output)
 
-            training_step_output = self._process_training_step_output(training_step_output)
-            if training_step_output is None:
+            result_collection = self._process_training_step_output(training_step_output)
+            if result_collection is None:
                 return
 
         closure_loss = None
-        loss = None
         if self.trainer.lightning_module.automatic_optimization:
             # accumulate loss. if accumulate_grad_batches==1, no effect
-            closure_loss = training_step_output.minimize / self.trainer.accumulate_grad_batches
+            closure_loss = result_collection.minimize / self.trainer.accumulate_grad_batches
             # the loss will get scaled for amp. avoid any modifications to it
-            loss = closure_loss.detach().clone()
-        return AttributeDict(closure_loss=closure_loss, loss=loss, training_step_output=training_step_output)
-
-    def _training_step_and_backward_closure(
-        self,
-        training_step_generator: Generator,
-        batch_idx: int,
-        opt_idx: int,
-        optimizer: Optimizer,
-        return_result: AttributeDict,
-    ) -> Optional[Tensor]:
-        result = self.training_step_and_backward(training_step_generator, batch_idx, opt_idx, optimizer)
-        if result is not None:
-            return_result.update(result)
-            return return_result.loss
-
-    def training_step_and_backward(
-        self,
-        training_step_generator: Generator,
-        batch_idx: int,
-        opt_idx: int,
-        optimizer: torch.optim.Optimizer,
-    ) -> STEP_OUTPUT:
-        """Wrap forward, zero_grad and backward in a closure so second order methods work"""
-        with self.trainer.profiler.profile("training_step_and_backward"):
-            # lightning module hook
-            result = self._training_step(training_step_generator)
-
-            if not self._skip_backward and self.trainer.lightning_module.automatic_optimization:
-                is_first_batch_to_accumulate = batch_idx % self.trainer.accumulate_grad_batches == 0
-
-                if is_first_batch_to_accumulate:
-                    self._on_before_zero_grad(optimizer)
-                    self._optimizer_zero_grad(batch_idx, optimizer, opt_idx)
-
-                # backward pass
-                if result is not None:
-                    with self.trainer.profiler.profile("backward"):
-                        self.backward(result, optimizer, opt_idx)
-
-                    # when in dev debugging track the losses
-                    self.trainer.dev_debugger.track_train_loss_history(batch_idx, result.loss)
-
-                    # check if loss or model weights are nan
-                    if self.trainer.terminate_on_nan:
-                        self._check_finite(result.loss)
-
-                else:
-                    self._warning_cache.warn(
-                        "training_step returned None. If this was on purpose, ignore this warning..."
-                    )
-
-        return result
+        return AttributeDict(closure_loss=closure_loss, result_collection=result_collection)
