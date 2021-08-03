61a1746ea29bd7e308cce1ab24aa9e01c3d299da
Carlos Mocholi <carlossmocholi@gmail.com>
2021-08-03 15:06:02 +0200

    Remove support for the deprecateed `on_{save,load}_checkpoint` signature

--- tests/deprecated_api/test_remove_1-5.py
+++ tests/deprecated_api/test_remove_1-5.py
@@ -14,13 +14,12 @@
 """Test deprecated functionality which will be removed in v1.5.0"""
 import operator
 import os
-from typing import Any, Dict
 from unittest import mock
 
 import pytest
 import torch
 
-from pytorch_lightning import Callback, Trainer
+from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.core.decorators import auto_move_data
 from pytorch_lightning.loggers import WandbLogger
@@ -46,104 +45,6 @@ def test_v1_5_0_model_checkpoint_save_checkpoint():
 def test_v1_5_0_wandb_unused_sync_step(_):
     with pytest.deprecated_call(match=r"v1.2.1 and will be removed in v1.5"):
         WandbLogger(sync_step=True)
-
-
-def test_v1_5_0_old_callback_on_save_checkpoint(tmpdir):
-    class OldSignature(Callback):
-        def on_save_checkpoint(self, trainer, pl_module):
-            ...
-
-    model = BoringModel()
-    trainer_kwargs = {"default_root_dir": tmpdir, "checkpoint_callback": False, "max_epochs": 1}
-    filepath = tmpdir / "test.ckpt"
-
-    trainer = Trainer(**trainer_kwargs, callbacks=[OldSignature()])
-    trainer.fit(model)
-
-    with pytest.deprecated_call(match="old signature will be removed in v1.5"):
-        trainer.save_checkpoint(filepath)
-
-    class NewSignature(Callback):
-        def on_save_checkpoint(self, trainer, pl_module, checkpoint):
-            ...
-
-    class ValidSignature1(Callback):
-        def on_save_checkpoint(self, trainer, *args):
-            ...
-
-    class ValidSignature2(Callback):
-        def on_save_checkpoint(self, *args):
-            ...
-
-    trainer.callbacks = [NewSignature(), ValidSignature1(), ValidSignature2()]
-    with no_warning_call(DeprecationWarning):
-        trainer.save_checkpoint(filepath)
-
-
-class BaseSignatureOnLoadCheckpoint(Callback):
-    def __init__(self):
-        self.on_load_checkpoint_called = False
-
-
-class OldSignatureOnLoadCheckpoint(BaseSignatureOnLoadCheckpoint):
-    def on_save_checkpoint(self, *args) -> Dict[str, Any]:
-        return {"a": 0}
-
-    def on_load_checkpoint(self, callback_state) -> None:
-        assert callback_state == {"a": 0}
-        self.on_load_checkpoint_called = True
-
-
-class NewSignatureOnLoadCheckpoint(BaseSignatureOnLoadCheckpoint):
-    def on_save_checkpoint(self, trainer, pl_module, checkpoint) -> dict:
-        return {"something": "something"}
-
-    def on_load_checkpoint(self, trainer, pl_module, checkpoint):
-        assert checkpoint == {"something": "something"}
-        self.on_load_checkpoint_called = True
-
-
-class ValidSignature2OnLoadCheckpoint(BaseSignatureOnLoadCheckpoint):
-    def on_save_checkpoint(self, trainer, pl_module, checkpoint) -> dict:
-        return {"something": "something"}
-
-    def on_load_checkpoint(self, *args):
-        assert len(args) == 3
-        self.on_load_checkpoint_called = True
-
-
-def test_v1_5_0_old_callback_on_load_checkpoint(tmpdir):
-
-    model = BoringModel()
-    trainer_kwargs = {"default_root_dir": tmpdir, "max_steps": 1}
-    chk = ModelCheckpoint(save_last=True)
-    trainer = Trainer(**trainer_kwargs, callbacks=[OldSignatureOnLoadCheckpoint(), chk])
-    trainer.fit(model)
-
-    with pytest.deprecated_call(match="old signature will be removed in v1.5"):
-        trainer_kwargs["max_steps"] = 2
-        cb = OldSignatureOnLoadCheckpoint()
-        trainer = Trainer(**trainer_kwargs, callbacks=cb, resume_from_checkpoint=chk.last_model_path)
-        trainer.fit(model)
-        assert cb.on_load_checkpoint_called
-
-    class ValidSignature1(BaseSignatureOnLoadCheckpoint):
-        def on_load_checkpoint(self, trainer, *args):
-            assert len(args) == 2
-            self.on_load_checkpoint_called = True
-
-    model = BoringModel()
-    chk = ModelCheckpoint(save_last=True)
-    trainer = Trainer(
-        **trainer_kwargs,
-        callbacks=[NewSignatureOnLoadCheckpoint(), ValidSignature1(), ValidSignature2OnLoadCheckpoint(), chk]
-    )
-    with no_deprecated_call(match="old signature will be removed in v1.5"):
-        trainer.fit(model)
-
-    trainer = Trainer(**trainer_kwargs, resume_from_checkpoint=chk.last_model_path)
-    with no_deprecated_call(match="old signature will be removed in v1.5"):
-        trainer.fit(model)
 
 
 def test_v1_5_0_legacy_profiler_argument():
