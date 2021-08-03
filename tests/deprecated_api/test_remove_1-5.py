c2e0bd35ce1e873f4256e597b7201e6c778d295d
Carlos Mocholi <carlossmocholi@gmail.com>
2021-08-03 03:23:27 +0200

    Remove deprecated `on_save_checkpoint` argument

--- tests/deprecated_api/test_remove_1-5.py
+++ tests/deprecated_api/test_remove_1-5.py
@@ -34,14 +34,6 @@ from tests.helpers.runif import RunIf
 from tests.helpers.utils import no_warning_call
 
 
-def test_v1_5_0_model_checkpoint_save_checkpoint():
-    model_ckpt = ModelCheckpoint()
-    trainer = Trainer()
-    trainer.save_checkpoint = lambda *_, **__: None
-    with pytest.deprecated_call(match="ModelCheckpoint.save_checkpoint` signature has changed"):
-        model_ckpt.save_checkpoint(trainer, object())
-
-
 @mock.patch("pytorch_lightning.loggers.wandb.wandb")
 def test_v1_5_0_wandb_unused_sync_step(_):
     with pytest.deprecated_call(match=r"v1.2.1 and will be removed in v1.5"):
