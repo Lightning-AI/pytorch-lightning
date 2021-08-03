9dc913a590adc1d6d76fd010099f36e1d6ca8d5e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-03 23:28:30 +0200

    fix ticks in warning message test

--- tests/deprecated_api/test_remove_1-7.py
+++ tests/deprecated_api/test_remove_1-7.py
@@ -30,13 +30,13 @@ def test_v1_7_0_deprecated_lightning_module_summarize(tmpdir):
 
 def test_v1_7_0_moved_model_summary_and_layer_summary(tmpdir):
     _soft_unimport_module("pytorch_lightning.core.memory")
-    with pytest.deprecated_call(match="to pytorch_lightning.utilities.model_summary since v1.5"):
+    with pytest.deprecated_call(match="to `pytorch_lightning.utilities.model_summary` since v1.5"):
         from pytorch_lightning.core.memory import LayerSummary, ModelSummary  # noqa: F811 F401
 
 
 def test_v1_7_0_moved_get_memory_profile_and_get_gpu_memory_map(tmpdir):
     _soft_unimport_module("pytorch_lightning.core.memory")
-    with pytest.deprecated_call(match="to pytorch_lightning.utilities.memory since v1.5"):
+    with pytest.deprecated_call(match="to `pytorch_lightning.utilities.memory` since v1.5"):
         from pytorch_lightning.core.memory import get_gpu_memory_map, get_memory_profile  # noqa: F811 F401
 
 
