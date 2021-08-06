4d37d5b1da2297ae9954af93b4713fa571a7fb7d
Rio H <hoshrio@gmail.com>
2021-08-06 15:16:31 -0700

    Add test_has_iterable_dataset

--- tests/utilities/test_data.py
+++ tests/utilities/test_data.py
@@ -1,6 +1,9 @@
 import torch
 
-from pytorch_lightning.utilities.data import extract_batch_size
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset, IterableDataset
+
+from pytorch_lightning.utilities.data import extract_batch_size, has_iterable_dataset
 
 
 def test_extract_batch_size():
@@ -19,3 +22,21 @@ def test_extract_batch_size():
 
     batch = {"test": [{"test": [torch.zeros(11, 10)]}]}
     assert extract_batch_size(batch) == 11
+
+
+def test_has_iterable_dataset():
+    class MockIterableDataset(IterableDataset):
+        def __iter__(self):
+            yield 1
+            return self
+
+    assert has_iterable_dataset(DataLoader(MockIterableDataset()))
+
+    assert not has_iterable_dataset(DataLoader(Dataset()))
+
+    class MockDataset(Dataset):
+        def __iter__(self):
+            yield 1
+            return self
+
+    assert not has_iterable_dataset(DataLoader(MockDataset()))
