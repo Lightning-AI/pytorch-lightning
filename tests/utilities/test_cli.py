99134387a3ad12d717396e68c57ac9602afaf9cf
Danielle Pintz <daniellepintz@gmail.com>
2021-08-16 12:29:06 -0700

    fix cli test

--- tests/utilities/test_cli.py
+++ tests/utilities/test_cli.py
@@ -36,7 +36,6 @@ from pytorch_lightning.utilities.cli import instantiate_class, LightningArgument
 from pytorch_lightning.utilities.imports import _TORCHVISION_AVAILABLE
 from tests.helpers import BoringDataModule, BoringModel
 from tests.helpers.runif import RunIf
-from tests.helpers.utils import DummyException
 
 torchvision_version = version.parse("0")
 if _TORCHVISION_AVAILABLE:
@@ -551,7 +550,7 @@ def test_lightning_cli_link_arguments(tmpdir):
 
 class EarlyExitTestModel(BoringModel):
     def on_fit_start(self):
-        raise DummyException()
+        raise Exception("Error on fit start")
 
 
 @pytest.mark.parametrize("logger", (False, True))
@@ -564,7 +563,7 @@ class EarlyExitTestModel(BoringModel):
     ),
 )
 def test_cli_ddp_spawn_save_config_callback(tmpdir, logger, trainer_kwargs):
-    with mock.patch("sys.argv", ["any.py"]), pytest.raises(DummyException):
+    with mock.patch("sys.argv", ["any.py"]), pytest.raises(Exception, match=r"Error on fit start"):
         LightningCLI(
             EarlyExitTestModel,
             trainer_defaults={
