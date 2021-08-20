1693e63fc3b7f6aef54f283532ac539fec31ed5d
Ning <liningblack@hotmail.com>
2021-08-20 10:29:39 -0700

    Update comment pytorch_lightning/trainer/trainer.py
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -245,7 +245,7 @@ class Trainer(
 
                 .. deprecated:: v1.5
                     Deprecated in v1.5.0 and will be removed in v1.7.0
-                    Please set `prepare_data_per_node` in LightningDataModule or LightningModule directly instead.
+                    Please set ``prepare_data_per_node`` in LightningDataModule or LightningModule directly instead.
 
             process_position: orders the progress bar when running multiple models on same machine.
 
