ea1827daed795babc2f85aa93f921127d1660e63
Carlos Mocholi <carlossmocholi@gmail.com>
2021-08-23 14:28:05 +0200

    Fix comment

--- tests/utilities/test_fetching.py
+++ tests/utilities/test_fetching.py
@@ -95,11 +95,16 @@ def test_misconfiguration_error():
 
 def get_cycles_per_ms() -> float:
     """
-    Measure and return approximate number of cycles per millisecond for torch.cuda._sleep
-    Copied from: github.com/pytorch/pytorch/blob/master/test/test_cuda.py
+    Get 10 values and remove the 2 max and 2 min and return the avg.
+    This is to avoid system disturbance that skew the results, e.g. the very first cuda call likely does a bunch of
+    init, which takes much longer than subsequent calls.
     """
 
     def measure() -> float:
+        """
+        Measure and return approximate number of cycles per millisecond for `torch.cuda._sleep`
+        Copied from: https://github.com/pytorch/pytorch/blob/v1.9.0/test/test_cuda.py#L81
+        """
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
         start.record()
@@ -109,14 +114,6 @@ def get_cycles_per_ms() -> float:
         cycles_per_ms = 1000000 / start.elapsed_time(end)
         return cycles_per_ms
 
-    # Get 10 values and remove the 2 max and 2 min and return the avg.
-    # This is to avoid system disturbance that skew the results, e.g.
-    # the very first cuda call likely does a bunch of init, which takes
-    # much longer than subsequent calls.
-    #
-    # Tested on both Tesla V100, Quadro GP100, Titan RTX, RTX 3090 GPUs
-    # and seems to return stable values. Therefore, we enable caching
-    # using lru_cache decorator above.
     num = 10
     vals = []
     for _ in range(num):
