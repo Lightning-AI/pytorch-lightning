cd1c8c16e74a3513bb10dd27bbc873065943e6f9
tchaton <thomas@grid.ai>
2021-08-24 17:26:16 +0100

    resolve typing

--- pytorch_lightning/loops/dataloader/evaluation_loop.py
+++ pytorch_lightning/loops/dataloader/evaluation_loop.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Iterator, List, Optional, Sequence, Union
+from typing import Any, List, Optional, Sequence, Union
 
 from deprecate.utils import void
 from torch.utils.data.dataloader import DataLoader
