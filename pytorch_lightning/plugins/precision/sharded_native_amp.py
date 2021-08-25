bac8b1be81eeede3d12190ab0d820597df77fc3b
Sean Naren <sean@grid.ai>
2021-08-25 13:18:00 +0100

    Add support for CPU AMP autocast (#9084)

--- pytorch_lightning/plugins/precision/sharded_native_amp.py
+++ pytorch_lightning/plugins/precision/sharded_native_amp.py
@@ -24,9 +24,10 @@ if _NATIVE_AMP_AVAILABLE and _FAIRSCALE_AVAILABLE:
 class ShardedNativeMixedPrecisionPlugin(NativeMixedPrecisionPlugin):
     """Mixed Precision for Sharded Training"""
 
-    def __init__(self, precision: Union[int, str] = 16) -> None:
-        super().__init__(precision)
-        self.scaler = ShardedGradScaler()
+    def __init__(self, precision: Union[int, str] = 16, use_cpu: bool = False) -> None:
+        super().__init__(precision, use_cpu=use_cpu)
+        if not self.use_cpu:
+            self.scaler = ShardedGradScaler()
 
     def clip_grad_by_norm(
         self, optimizer: "OSS", clip_val: Union[int, float], norm_type: float = 2.0, eps: float = 1e-6
