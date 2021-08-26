03427384d93133574d242c0e53872579c71ed687
tchaton <thomas@grid.ai>
2021-08-26 11:45:26 +0100

    add fixme

--- pytorch_lightning/loops/fit_loop.py
+++ pytorch_lightning/loops/fit_loop.py
@@ -243,6 +243,7 @@ class FitLoop(Loop):
 
     def on_save_checkpoint(self) -> Dict:
         state_dict = super().on_save_checkpoint()
+        # FIXME(@tchaton) Should pass has_completed=True when iterator is exhausted ?
         state_dict["dataloader_state_dict"] = self.trainer.train_dataloader.state_dict(has_completed=False)
         return state_dict
 
