1b706379b6eee305589c99a73dc3765ba4f9d9af
Eric Wiener <ewiener@zoox.com>
2021-08-28 20:29:51 -0400

    Now checking that max vals are int (vs. not float)

--- pytorch_lightning/trainer/configuration_validator.py
+++ pytorch_lightning/trainer/configuration_validator.py
@@ -91,12 +91,12 @@ class ConfigValidator:
             )
 
         # Allow max_epochs or max_steps to be zero, since this will be handled by fit_loop.done
-        if trainer.max_epochs and (trainer.max_epochs < -1 or isinstance(trainer.max_epochs, float)):
+        if trainer.max_epochs and (trainer.max_epochs < -1 or not isinstance(trainer.max_epochs, int)):
             raise MisconfigurationException(
                 f"`max_epochs` must be a positive integer or -1. You passed in {trainer.max_epochs}."
             )
 
-        if trainer.max_steps and (trainer.max_steps < -1 or isinstance(trainer.max_steps, float)):
+        if trainer.max_steps and (trainer.max_steps < -1 or not isinstance(trainer.max_steps, int)):
             raise MisconfigurationException(
                 f"`max_steps` must be a positive integer or -1. You passed in {trainer.max_steps}."
             )
