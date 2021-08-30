ab3850750cc005c18ca53cf0f46706e206f536eb
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-30 12:31:24 +0200

    extract build_kwargs function from training loop

--- pytorch_lightning/loops/utilities.py
+++ pytorch_lightning/loops/utilities.py
@@ -11,10 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from collections import OrderedDict
+from contextlib import contextmanager
+from typing import Any, Generator, Iterator, Mapping, Optional, Tuple, Sequence, Dict
 
 from typing import Any, Iterator, Mapping, Optional, Tuple
 
 import torch
+from torch import Tensor
+from torch.optim import Optimizer
 
 import pytorch_lightning as pl
 from pytorch_lightning.trainer.connectors.logger_connector.result import ResultCollection
@@ -22,6 +27,7 @@ from pytorch_lightning.utilities.apply_func import apply_to_collection
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.fetching import AbstractDataFetcher, DataLoaderIterDataFetcher
 from pytorch_lightning.utilities.finite_checks import detect_nan_parameters
+from pytorch_lightning.utilities.signature_utils import is_param_in_hook_signature
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 
 
@@ -103,6 +109,58 @@ def _process_training_step_output(
     if trainer.move_metrics_to_cpu:
         results.cpu()
     return results, hiddens
+
+
+def _build_training_step_kwargs(
+    lightning_module: "pl.LightningModule",
+    optimizers: Sequence[Optimizer],
+    batch: Any,
+    batch_idx: int,
+    opt_idx: int,
+    hiddens: Optional[Tensor],
+) -> Dict[str, Any]:
+    """Builds the keyword arguments for training_step
+
+    Args:
+        lightning_module: the LightningModule with a `training_step` hook implementation
+        optimizers: the list of optimizers from the Trainer
+        batch: the batch to train on
+        batch_idx: the index of the current batch
+        opt_idx: the index of the current optimizer
+        hiddens: the hidden state of the previous RNN iteration
+
+    Returns:
+        the keyword arguments for the training step
+    """
+    # enable not needing to add opt_idx to training_step
+    step_kwargs = OrderedDict([("batch", batch)])
+
+    training_step_fx = getattr(lightning_module, "training_step")
+
+    if is_param_in_hook_signature(training_step_fx, "batch_idx", min_args=2):
+        step_kwargs["batch_idx"] = batch_idx
+
+    if len(optimizers) > 1:
+        has_opt_idx_in_train_step = is_param_in_hook_signature(training_step_fx, "optimizer_idx")
+        if has_opt_idx_in_train_step:
+            if not lightning_module.automatic_optimization:
+                raise ValueError(
+                    "Your `LightningModule.training_step` signature contains an `optimizer_idx` argument but"
+                    " in manual optimization optimizers must be handled by the user. Remove the optimizer_idx"
+                    " argument or set `self.automatic_optimization = True`."
+                )
+            step_kwargs["optimizer_idx"] = opt_idx
+        elif not has_opt_idx_in_train_step and lightning_module.automatic_optimization:
+            raise ValueError(
+                f"Your LightningModule defines {len(optimizers)} optimizers but"
+                " `training_step` is missing the `optimizer_idx` argument."
+            )
+
+    # pass hiddens if using tbptt
+    if lightning_module.truncated_bptt_steps > 0:
+        step_kwargs["hiddens"] = hiddens
+
+    return step_kwargs
 
 
 def _prepare_dataloader_iter(data_fetcher: AbstractDataFetcher, batch_idx: int) -> Iterator:
