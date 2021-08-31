ff7305f74d4e097a45b6a4d8c0fff6d4f5aaa386
thomas chaton <thomas@grid.ai>
2021-08-31 22:02:33 +0100

    [bugfix] Prevent a DDP failure using copy (#9239)

--- pytorch_lightning/loops/batch/training_batch_loop.py
+++ pytorch_lightning/loops/batch/training_batch_loop.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from copy import copy
+from copy import deepcopy
 from functools import partial
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
@@ -142,12 +142,12 @@ class TrainingBatchLoop(Loop):
 
                 result = self._run_optimization(batch_idx, split_batch, opt_idx, optimizer)
                 if result:
-                    self.batch_outputs[opt_idx].append(copy(result.result_collection))
+                    self.batch_outputs[opt_idx].append(deepcopy(result.result_collection))
         else:
             # in manual optimization, there is no looping over optimizers
             result = self._run_optimization(batch_idx, split_batch)
             if result:
-                self.batch_outputs[0].append(copy(result.result_collection))
+                self.batch_outputs[0].append(deepcopy(result.result_collection))
 
     def teardown(self) -> None:
         # release memory
