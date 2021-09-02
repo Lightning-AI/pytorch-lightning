7982d8675e6fa3099556ff747c0a2c86084ee170
Carlos Mocholi <carlossmocholi@gmail.com>
2021-09-02 05:55:21 +0200

    Type code

--- pytorch_lightning/utilities/memory.py
+++ pytorch_lightning/utilities/memory.py
@@ -99,7 +99,7 @@ def garbage_collection_cuda() -> None:
             raise
 
 
-def get_memory_profile(mode: str) -> Union[Dict[str, float], Dict[int, float]]:
+def get_memory_profile(mode: str) -> Union[Dict[str, float]]:
     """Get a profile of the current memory usage.
 
     Args:
