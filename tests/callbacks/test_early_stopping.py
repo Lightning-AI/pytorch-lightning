b130961a4317b582a5542238feea1ff78c9bc8ca
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-05 13:04:44 +0200

    remove unused imports

--- tests/callbacks/test_early_stopping.py
+++ tests/callbacks/test_early_stopping.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-import os
 import pickle
 from typing import List, Optional
 from unittest import mock
--- tests/trainer/flags/test_fast_dev_run.py
+++ tests/trainer/flags/test_fast_dev_run.py
@@ -1,5 +1,4 @@
 import os
-from unittest import mock
 from unittest.mock import Mock
 
 import pytest
