8da7599413dc4889bea10b766a455a03f5b47a31
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-11 19:48:50 +0200

    unused imports

--- pytorch_lightning/callbacks/model_summary.py
+++ pytorch_lightning/callbacks/model_summary.py
@@ -22,7 +22,7 @@ the name, type and number of parameters for each layer.
 
 """
 import logging
-from typing import List, Optional, Union, Tuple
+from typing import List, Tuple
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.base import Callback
