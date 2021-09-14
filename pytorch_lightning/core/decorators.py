dac83cf5744e9663466d2b7449c29f9bfee0fd4f
Kaushik B <kaushikbokka@gmail.com>
2021-09-15 00:31:14 +0530

    Enable auto parameters tying for TPUs

--- pytorch_lightning/core/decorators.py
+++ pytorch_lightning/core/decorators.py
@@ -14,8 +14,9 @@
 """Decorator for LightningModule methods."""
 
 from functools import wraps
-from typing import Callable
+from typing import Callable, Dict, Optional
 
+from pytorch_lightning.overrides import LightningDistributedModule
 from pytorch_lightning.utilities import rank_zero_warn
 
 
@@ -51,6 +52,62 @@ def parameter_validation(fn: Callable) -> Callable:
                 f"Layer count: [Before: {pre_layer_count} After: {post_layer_count}]"
             )
 
+        return module
+
+    return inner_fn
+
+
+def find_shared_parameters(module, tied_parameters: Optional[Dict] = None, prefix: str = ""):
+    if tied_parameters is None:
+        first_call = True
+        tied_parameters = {}
+    else:
+        first_call = False
+    for name, param in module._parameters.items():
+        param_prefix = prefix + ("." if prefix else "") + name
+        if param is None:
+            continue
+        if param not in tied_parameters:
+            tied_parameters[param] = []
+        tied_parameters[param].append(param_prefix)
+    for name, m in module._modules.items():
+        if m is None:
+            continue
+        submodule_prefix = prefix + ("." if prefix else "") + name
+        find_shared_parameters(m, tied_parameters, submodule_prefix)
+    if first_call:
+        return [x for x in tied_parameters.values() if len(x) > 1]
+
+
+def apply_weight_tying(module, shared_params):
+    for shared_param in shared_params:
+        ref = _get_module_by_path(module, shared_param[0])
+        for path in shared_param[1:]:
+            _set_module_by_path(module, path, ref)
+    return module
+
+
+def _get_module_by_path(module, path):
+    path = path.split(".")
+    for name in path:
+        module = getattr(module, name)
+    return module
+
+
+def _set_module_by_path(module, path, value):
+    path = path.split(".")
+    for name in path[:-1]:
+        module = getattr(module, name)
+    setattr(module, path[-1], value)
+
+
+def auto_weight_tying(model_to_device: Callable) -> Callable:
+    @wraps(model_to_device)
+    def inner_fn(self, *args, **kwargs):
+        shared_params = find_shared_parameters(self.model)
+        model_to_device(self, *args, **kwargs)
+        module = self.model.module if isinstance(self.model, LightningDistributedModule) else self.model
+        module = apply_weight_tying(module, shared_params)
         return module
 
     return inner_fn
