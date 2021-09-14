e619ce191ee5b2e95f703310f83278e6876b7f7f
edward-io <53842584+edward-io@users.noreply.github.com>
2021-09-13 20:16:45 -0700

    address comments

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -55,7 +55,8 @@ class LoggerConnector:
                 f"Setting `Trainer(flush_logs_every_n_steps={flush_logs_every_n_steps})` is deprecated in v1.5 "
                 "and will be removed in v1.7. Please configure flushing in the logger instead."
             )
-        flush_logs_every_n_steps = 100  # original default parameter
+        else:
+            flush_logs_every_n_steps = 100  # original default parameter
         self.trainer.flush_logs_every_n_steps = flush_logs_every_n_steps
         self.trainer.log_every_n_steps = log_every_n_steps
         self.trainer.move_metrics_to_cpu = move_metrics_to_cpu
