f9b414727a4ace4a870fc0b1443f12f52e556042
Puneet Dabral <puneetdabral2000@gmail.com>
2021-09-15 20:43:18 +0530

    remove abc from lightning module

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -19,7 +19,6 @@ import logging
 import numbers
 import os
 import tempfile
-from abc import ABC
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
