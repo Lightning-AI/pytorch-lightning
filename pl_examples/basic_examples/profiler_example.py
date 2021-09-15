e2eb8324613605f921bd951e439f2f2bb54a18ca
thomas chaton <thomas@grid.ai>
2021-09-15 08:01:41 +0100

    [bugfix] Resolve example after LightningCLI update (#9520)

--- pl_examples/basic_examples/profiler_example.py
+++ pl_examples/basic_examples/profiler_example.py
@@ -35,6 +35,7 @@ from pytorch_lightning.profiler.pytorch import PyTorchProfiler
 from pytorch_lightning.utilities.cli import LightningCLI
 
 DEFAULT_CMD_LINE = (
+    "fit",
     "--trainer.max_epochs=1",
     "--trainer.limit_train_batches=15",
     "--trainer.limit_val_batches=15",
