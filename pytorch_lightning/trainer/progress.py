800ab6b8494345dc7a156150a61fad0d53b85b3c
Carlos Mocholi <carlossmocholi@gmail.com>
2021-09-23 03:28:23 +0200

    Fix mypy

--- pytorch_lightning/trainer/progress.py
+++ pytorch_lightning/trainer/progress.py
@@ -225,7 +225,7 @@ class OptimizerProgress(BaseProgress):
         self.step.reset_on_epoch()
         self.zero_grad.reset_on_epoch()
 
-    def reset_on_restart(self):
+    def reset_on_restart(self) -> None:
         self.step.reset_on_restart()
         self.zero_grad.reset_on_restart()
 
