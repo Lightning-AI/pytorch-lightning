dba2c65d1a82d57b30c92b9c0b2edd40b0cefcd6
Jirka <jirka.borovec@seznam.cz>
2021-09-26 22:08:05 +0200

    docs

--- pytorch_lightning/callbacks/finetuning.py
+++ pytorch_lightning/callbacks/finetuning.py
@@ -53,32 +53,29 @@ class BaseFinetuning(Callback):
 
     Example::
 
-        class MyModel(LightningModule)
-
-            ...
-
-            def configure_optimizer(self):
-                # Make sure to filter the parameters based on `requires_grad`
-                return Adam(filter(lambda p: p.requires_grad, self.parameters))
-
-        class FeatureExtractorFreezeUnfreeze(BaseFinetuning):
-
-            def __init__(self, unfreeze_at_epoch=10):
-                self._unfreeze_at_epoch = unfreeze_at_epoch
-
-            def freeze_before_training(self, pl_module):
-                # freeze any module you want
-                # Here, we are freezing `feature_extractor`
-                self.freeze(pl_module.feature_extractor)
-
-            def finetune_function(self, pl_module, current_epoch, optimizer, optimizer_idx):
-                # When `current_epoch` is 10, feature_extractor will start training.
-                if current_epoch == self._unfreeze_at_epoch:
-                    self.unfreeze_and_add_param_group(
-                        modules=pl_module.feature_extractor,
-                        optimizer=optimizer,
-                        train_bn=True,
-                    )
+        >>> class MyModel(pl.LightningModule):
+        ...     def configure_optimizer(self):
+        ...         # Make sure to filter the parameters based on `requires_grad`
+        ...         return Adam(filter(lambda p: p.requires_grad, self.parameters))
+        ...
+        >>> class FeatureExtractorFreezeUnfreeze(BaseFinetuning):
+        ...
+        ...     def __init__(self, unfreeze_at_epoch=10):
+        ...         self._unfreeze_at_epoch = unfreeze_at_epoch
+        ...
+        ...     def freeze_before_training(self, pl_module):
+        ...         # freeze any module you want
+        ...         # Here, we are freezing `feature_extractor`
+        ...         self.freeze(pl_module.feature_extractor)
+        ...
+        ...     def finetune_function(self, pl_module, current_epoch, optimizer, optimizer_idx):
+        ...         # When `current_epoch` is 10, feature_extractor will start training.
+        ...         if current_epoch == self._unfreeze_at_epoch:
+        ...             self.unfreeze_and_add_param_group(
+        ...                 modules=pl_module.feature_extractor,
+        ...                 optimizer=optimizer,
+        ...                 train_bn=True,
+        ...             )
     """
 
     def __init__(self):
