afa4195f88921517bdbf348eb97f3a2dd77e947a
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-27 14:40:17 +0200

    fix  unused import

--- pytorch_lightning/loops/batch/training_batch_loop.py
+++ pytorch_lightning/loops/batch/training_batch_loop.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, List, Optional, Union
 
 from deprecate import void
 from torch import Tensor
