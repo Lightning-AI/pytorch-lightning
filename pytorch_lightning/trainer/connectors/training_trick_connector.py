fb9d34e274eb5fe0d32bc3d4bde4b555d81a12ab
rohitgr7 <rohitgr1998@gmail.com>
2021-09-28 17:47:24 +0530

    pre-commit

--- pytorch_lightning/trainer/connectors/training_trick_connector.py
+++ pytorch_lightning/trainer/connectors/training_trick_connector.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Dict, Optional, Union
+from typing import Union
 
-from pytorch_lightning.callbacks import GradientAccumulationScheduler
-from pytorch_lightning.utilities import GradClipAlgorithmType, rank_zero_deprecation
+from pytorch_lightning.utilities import GradClipAlgorithmType
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
