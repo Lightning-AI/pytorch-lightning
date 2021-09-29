8ad4f6e37e54c108ba235255e911cd901281c741
SeanNaren <sean@grid.ai>
2021-09-29 12:43:56 +0100

    Revert

--- pytorch_lightning/callbacks/rich_model_summary.py
+++ pytorch_lightning/callbacks/rich_model_summary.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 from pytorch_lightning.callbacks import ModelSummary
 from pytorch_lightning.utilities.imports import _RICH_AVAILABLE
@@ -67,7 +67,7 @@ class RichModelSummary(ModelSummary):
 
     @staticmethod
     def summarize(
-        summary_data: List[Tuple[str, List[Union[str, Tuple]]]],
+        summary_data: List[Tuple[str, List[str]]],
         total_parameters: int,
         trainable_parameters: int,
         model_size: float,
