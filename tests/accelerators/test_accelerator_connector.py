cd94e021f9b830bb89cc3d64fed0ad74060443ea
Carlos Mocholi <carlossmocholi@gmail.com>
2021-09-29 17:02:53 +0200

    Undo change

--- tests/accelerators/test_accelerator_connector.py
+++ tests/accelerators/test_accelerator_connector.py
@@ -631,7 +631,7 @@ def test_accelerator_ddp_for_cpu(tmpdir):
     assert isinstance(trainer.training_type_plugin, DDPPlugin)
 
 
-@pytest.mark.parametrize("precision", [1, 12, "'invalid'"])
+@pytest.mark.parametrize("precision", [1, 12, "invalid"])
 def test_validate_precision_type(tmpdir, precision):
 
     with pytest.raises(MisconfigurationException, match=f"Precision {repr(precision)} is invalid"):
