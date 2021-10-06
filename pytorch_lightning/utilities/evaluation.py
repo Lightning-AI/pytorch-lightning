864b4005204626b32d4d6fa4b70317fb274fda6f
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-06 23:23:14 +0530

    Create evaluation.py

---
+++ pytorch_lightning/utilities/evaluation.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+
+def get_loss(model,X,y,criterion):
+    preds = model(X)
+    loss = criterion(preds,y)
+    return loss.item()
+  
+def get_accuracy(model,X,y):
+    preds = model(X)
+    correct = 0
+    total = 0
+    for pred,yb in zip(preds,y):
+        pred = int(torch.argmax(pred))
+        yb = int(torch.argmax(yb))
+        if pred == yb:
+            correct += 1
+        total += 1
+    acc = round(correct/total,3)*100
+    return acc
