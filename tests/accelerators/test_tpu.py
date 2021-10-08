8af00269e219c52d96192788775c3a69c6c1d2ce
Danielle Pintz <daniellepintz@gmail.com>
2021-10-08 20:45:12 +0000

    fix test

--- tests/accelerators/test_tpu.py
+++ tests/accelerators/test_tpu.py
@@ -1,15 +1,14 @@
 from pytorch_lightning.accelerators import TPUAccelerator
-from pytorch_lightning.plugins import SingleTPUPlugin
-from pytorch_lightning.plugins.training_type import TPUSpawnPlugin
+from pytorch_lightning.plugins import SingleTPUPlugin, PrecisionPlugin
 from tests.helpers.runif import RunIf
 
 
 @RunIf(tpu=True)
 def test_device_stats_tpu(tmpdir):
     """Test TPU get_device_stats."""
-    plugin = SingleTPUPlugin(1)
-    TPUAccel = TPUAccelerator(training_type_plugin=TPUSpawnPlugin(), precision_plugin=plugin)
-    tpu_stats = TPUAccel.get_device_stats("1")
+    TPUAccel = TPUAccelerator(training_type_plugin=SingleTPUPlugin(1), precision_plugin=PrecisionPlugin())
+    tpu_stats = TPUAccel.get_device_stats("xla:1")
+
     fields = ["avg. free memory (MB)", "avg. peak memory (MB)"]
 
     for f in fields:
