ad6259dac0081dc3a8637734752c24359f40c6c3
Rohit Gupta <rohitgr1998@gmail.com>
2021-10-11 17:28:41 +0530

    update

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -429,10 +429,10 @@ class ModelCheckpoint(Callback):
                 )
 
             if self.save_top_k == -1 and self.save_last:
-                 rank_zero_info(
-                     "ModelCheckpoint(save_last=True, save_top_k=-1, monitor=None)"
-                     " will duplicate the last checkpoint saved."
-                 )
+                rank_zero_info(
+                    "ModelCheckpoint(save_last=True, save_top_k=-1, monitor=None)"
+                    " will duplicate the last checkpoint saved."
+                )
 
     def __init_ckpt_dir(self, dirpath: Optional[_PATH], filename: Optional[str]) -> None:
         self._fs = get_filesystem(dirpath if dirpath else "")
