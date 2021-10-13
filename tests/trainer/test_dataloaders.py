d170976f58ae55f904cdf4cba0fcc3a07d6b8f07
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-13 18:42:13 +0200

    fix import

--- tests/trainer/test_dataloaders.py
+++ tests/trainer/test_dataloaders.py
@@ -27,7 +27,6 @@ import tests.helpers.pipelines as tpipes
 from pytorch_lightning import Callback, seed_everything, Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.utilities.data import has_iterable_dataset, has_len
-from pytorch_lightning.utilities.distributed import replace_sampler
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from tests.base import EvalModelTemplate
 from tests.helpers.boring_model import BoringModel, RandomDataset, RandomIterableDataset, RandomIterableDatasetWithLen
