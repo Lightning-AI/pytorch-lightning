e542da05c48f8795a239e577c8e2341e5d09d85f
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-18 10:54:37 +0200

    update

--- pl_examples/loops/yield_loop.py
+++ pl_examples/loops/yield_loop.py
@@ -1,22 +1,16 @@
-import torch
-
-from pytorch_lightning import Trainer
-
-
 import inspect
 from functools import partial
-from typing import Any, Generator, List, Optional, Tuple
+from typing import Generator
 
-from torch.optim import Optimizer
-
-from pytorch_lightning.loops import Loop, OptimizerLoop
-from pytorch_lightning.loops.optimization.optimizer_loop import ClosureResult
-from pytorch_lightning.loops.utilities import _build_training_step_kwargs
-from pytorch_lightning.utilities import AttributeDict
-from pytorch_lightning.utilities.exceptions import MisconfigurationException
+import torch
 
 from pl_examples.domain_templates.generative_adversarial_net import GAN as GANTemplate
 from pl_examples.domain_templates.generative_adversarial_net import MNISTDataModule
+from pytorch_lightning import Trainer
+from pytorch_lightning.loops import OptimizerLoop
+from pytorch_lightning.loops.optimization.optimizer_loop import ClosureResult
+from pytorch_lightning.loops.utilities import _build_training_step_kwargs
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
 class Yield:
@@ -24,22 +18,18 @@ class Yield:
     method yields losses for each optimizer instead of returning them."""
 
     def training_step(self, batch, batch_idx, optimizer_idx=0) -> Generator:
-        # the optimizer_idx is just here to shortcut the implementation for this POC
-        # TODO: generalize and override the build_kwargs function in YieldLoop
         pass
 
 
 class YieldLoop(OptimizerLoop):
     def __init__(self):
         super().__init__()
-        self._training_step_generator: Generator = ...
+        self._generator = None
 
-    def connect(self, **kwargs: "Loop") -> None:
+    def connect(self, **kwargs):
         raise NotImplementedError(f"{self.__class__.__name__} does not connect any child loops.")
 
-    def on_run_start(  # type: ignore[override]
-        self, batch: Any, optimizers: List[Tuple[int, Optimizer]], batch_idx: int
-    ) -> None:
+    def on_run_start(self, batch, optimizers, batch_idx):
         super().on_run_start(batch, optimizers, batch_idx)
         if not isinstance(self.trainer.lightning_module, Yield):
             raise MisconfigurationException(
@@ -49,31 +39,34 @@ class YieldLoop(OptimizerLoop):
         assert inspect.isgeneratorfunction(self.trainer.lightning_module.training_step)
         assert self.trainer.lightning_module.automatic_optimization
 
-        self._training_step_generator = self._get_training_step_generator(batch, batch_idx, opt_idx=0)
+        # We request the generator once and save it for later
+        # so we can call next() on it.
+        self._generator = self._get_generator(batch, batch_idx, opt_idx=0)
 
-    def _make_step_fn(self, split_batch: Any, batch_idx: int, opt_idx: int):
-        return partial(self._training_step, self._training_step_generator)
+    def _make_step_fn(self, split_batch, batch_idx, opt_idx):
+        return partial(self._training_step, self._generator)
 
-    def _get_training_step_generator(self, split_batch: Any, batch_idx: int, opt_idx: int) -> Generator:
+    def _get_generator(self, split_batch, batch_idx, opt_idx):
         step_kwargs = _build_training_step_kwargs(
             self.trainer.lightning_module, self.trainer.optimizers, split_batch, batch_idx, opt_idx, hiddens=None
         )
-        generator = self.trainer.accelerator.training_step(step_kwargs)
-        return generator
 
-    def _training_step(self, training_step_generator: Generator) -> Optional[AttributeDict]:
-        # give the PL module a result for logging
-        lightning_module = self.trainer.lightning_module
+        # Here we are basically calling lightning_module.training_step()
+        # and this returns a generator! The training_step is handled by the
+        # accelerator to enable distributed training.
+        return self.trainer.accelerator.training_step(step_kwargs)
 
-        with self.trainer.profiler.profile("model_forward"):
-            # manually capture logged metrics
-            lightning_module._current_fx_name = "training_step"
-            with self.trainer.profiler.profile("training_step"):
-                training_step_output = next(training_step_generator)
-                self.trainer.accelerator.post_training_step()
+    def _training_step(self, generator):
+        # required for logging
+        self.trainer.lightning_module._current_fx_name = "training_step"
 
-            training_step_output = self.trainer.call_hook("training_step_end", training_step_output)
-            result = ClosureResult.from_training_step_output(training_step_output, self.trainer.accumulate_grad_batches)
+        # Here, instead of calling lightning_module.training_step()
+        # we call next() on the generator!
+        training_step_output = next(generator)
+        self.trainer.accelerator.post_training_step()
+
+        training_step_output = self.trainer.call_hook("training_step_end", training_step_output)
+        result = ClosureResult.from_training_step_output(training_step_output, self.trainer.accumulate_grad_batches)
         return result
 
 
@@ -113,13 +106,14 @@ class GAN(Yield, GANTemplate):
         yield d_loss
 
 
-def run():
+if __name__ == "__main__":
     model = GAN()
     dm = MNISTDataModule()
     trainer = Trainer()
+
+    # Connect the new loop
+    # YieldLoop now replaces the previous optimizer loop
     trainer.fit_loop.epoch_loop.batch_loop.connect(optimizer_loop=YieldLoop())
+
+    # fit() will now use the new loop!
     trainer.fit(model, dm)
-
-
-if __name__ == "__main__":
-    run()
