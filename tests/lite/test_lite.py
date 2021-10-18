8a5447d58abdcd14205df5d72037c4c316afa3ba
Thomas Chaton <thomas@grid.ai>
2021-10-18 13:32:47 -0400

    update

--- tests/lite/test_lite.py
+++ tests/lite/test_lite.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from contextlib import contextmanager
 from copy import deepcopy
-from typing import Generator
+from typing import Callable, Generator
 
 import pytest
 import torch
@@ -22,7 +22,9 @@ from torch.utils.data import DataLoader
 
 from pytorch_lightning import seed_everything
 from pytorch_lightning.lite import LightningLite
+from pytorch_lightning.utilities.imports import _TORCH_BFLOAT_AVAILABLE
 from tests.helpers.boring_model import RandomDataset
+from tests.helpers.runif import RunIf
 
 
 class BoringModel(nn.Module):
@@ -39,7 +41,14 @@ def configure_optimizers(module: nn.Module):
     return torch.optim.SGD(module.parameters(), lr=0.001)
 
 
-def main(model: nn.Module, train_dataloader: DataLoader, val_dataloader: DataLoader, num_epochs: int = 10):
+def main(
+    move_to_device: Callable,
+    model: nn.Module,
+    train_dataloader: DataLoader,
+    val_dataloader: DataLoader,
+    num_epochs: int = 10,
+):
+    model = move_to_device(model)
     optimizer = configure_optimizers(model)
 
     for epoch in range(num_epochs):
@@ -48,6 +57,7 @@ def main(model: nn.Module, train_dataloader: DataLoader, val_dataloader: DataLoa
 
         model.train()
         for batch in train_dataloader:
+            batch = move_to_device(batch)
             optimizer.zero_grad()
             loss = model(batch)
             train_losses.append(loss)
@@ -57,6 +67,7 @@ def main(model: nn.Module, train_dataloader: DataLoader, val_dataloader: DataLoa
         model.eval()
         with torch.no_grad():
             for batch in val_dataloader:
+                batch = move_to_device(batch)
                 val_losses.append(model(batch))
 
         train_epoch_loss = torch.stack(train_losses).mean()
@@ -81,6 +92,7 @@ class LiteTrainer(LightningLite):
 
             model.train()
             for batch in train_dataloader:
+                batch = self.to_device(batch)
                 optimizer.zero_grad()
                 loss = model(batch)
                 train_losses.append(loss)
@@ -90,45 +102,57 @@ class LiteTrainer(LightningLite):
             model.eval()
             with torch.no_grad():
                 for batch in val_dataloader:
+                    batch = self.to_device(batch)
                     val_losses.append(model(batch))
 
 
 @contextmanager
-def precision_context(precision, use_cpu, dype) -> Generator[None, None, None]:
+def precision_context(precision, accelerator) -> Generator[None, None, None]:
     if precision == 32:
         yield
         return
-    if dype == torch.bfloat16 and not use_cpu:
-        with torch.cuda.amp.autocast(dtype=dype):
+    if precision == 16 and accelerator == "gpu":
+        with torch.cuda.amp.autocast():
             yield
-    elif use_cpu:
-        with torch.cpu.amp.autocast(dtype=dype):
+    elif accelerator == "cpu":
+        with torch.cpu.amp.autocast(dtype=torch.float16 if precision == 16 else torch.bfloat16):
             yield
     else:
         with torch.cuda.amp.autocast():
             yield
 
 
+@RunIf(min_gpus=2)
 @pytest.mark.parametrize(
-    "dype,use_cpu,precision",
-    [pytest.param(torch.bfloat16, True, "bf16"), pytest.param(torch.float32, True, 32)],
+    "precision, strategy, devices, accelerator",
+    [
+        pytest.param(32, None, 1, "gpu"),
+        pytest.param(16, None, 1, "gpu"),
+        #pytest.param(
+        #    "bf16",
+        #    None,
+        #    1,
+        #    "gpu",
+        #    marks=pytest.mark.skipif(not _TORCH_BFLOAT_AVAILABLE, reason="bfloat16 isn't available."),
+        #),
+        pytest.param(32, None, 1, "cpu"),
+    ],
 )
-def test_boring_lite_model_cpu(dype, use_cpu, precision, tmpdir):
-
+def test_boring_lite_model_single_device(precision, strategy, devices, accelerator, tmpdir):
     seed_everything(42)
     train_dataloader = DataLoader(RandomDataset(32, 64))
     val_dataloader = DataLoader(RandomDataset(32, 64))
     model = BoringModel()
-    num_epochs = 10
-
+    num_epochs = 1
     state_dict = deepcopy(model.state_dict())
 
-    with precision_context(precision, use_cpu, dype):
-        pure_state_dict = main(model, train_dataloader, val_dataloader, num_epochs=num_epochs)
-
-    model.load_state_dict(state_dict)
-    lite = LiteTrainer(precision=precision)
+    lite = LiteTrainer(precision=precision, strategy=strategy, devices=devices, accelerator=accelerator)
     lite.run(model, train_dataloader, val_dataloader, num_epochs=num_epochs)
+    lite_state_dict = model.state_dict()
 
-    for w_pure, w_lite in zip(pure_state_dict.values(), model.state_dict().values()):
+    with precision_context(precision, accelerator):
+        model.load_state_dict(state_dict)
+        pure_state_dict = main(lite.to_device, model, train_dataloader, val_dataloader, num_epochs=num_epochs)
+
+    for w_pure, w_lite in zip(pure_state_dict.values(), lite_state_dict.values()):
         assert torch.equal(w_pure, w_lite)
