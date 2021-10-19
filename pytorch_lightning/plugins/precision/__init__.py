be9b0141351ab6b2922ecfecc5be383ffd39caf1
Carlos Mocholi <carlossmocholi@gmail.com>
2021-10-19 16:52:36 +0200

    Missing import

--- pytorch_lightning/plugins/precision/__init__.py
+++ pytorch_lightning/plugins/precision/__init__.py
@@ -4,6 +4,7 @@ from pytorch_lightning.plugins.precision.double import DoublePrecisionPlugin  # 
 from pytorch_lightning.plugins.precision.fully_sharded_native_amp import (  # noqa: F401
     FullyShardedNativeMixedPrecisionPlugin,
 )
+from pytorch_lightning.plugins.precision.ipu_precision import IPUPrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.mixed import MixedPrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.precision_plugin import PrecisionPlugin  # noqa: F401
