00a7e8e057326a24d8d77784fe56db09053a0c29
Rohit Gupta <rohitgr1998@gmail.com>
2021-10-20 00:51:46 +0530

    fn -> method

--- pytorch_lightning/callbacks/pruning.py
+++ pytorch_lightning/callbacks/pruning.py
@@ -166,7 +166,7 @@ class ModelPruning(Callback):
         self._parameter_names = parameter_names or self.PARAMETER_NAMES
         self._global_kwargs: Dict[str, Any] = {}
         self._original_layers: Optional[Dict[int, _LayerRef]] = None
-        self._pruning_fn_name: Optional[str] = None
+        self._pruning_method_name: Optional[str] = None
 
         for name in self._parameter_names:
             if name not in self.PARAMETER_NAMES:
@@ -250,7 +250,7 @@ class ModelPruning(Callback):
             self._global_kwargs = kwargs
         # save the function __name__ now because partial does not include it
         # and there are issues setting the attribute manually in ddp.
-        self._pruning_fn_name = pruning_meth.__name__
+        self._pruning_method_name = pruning_meth.__name__
         if self._use_global_unstructured:
             return pruning_meth
         return ModelPruning._wrap_pruning_fn(pruning_meth, **kwargs)
@@ -348,7 +348,7 @@ class ModelPruning(Callback):
         prev_total_zeros = sum(zeros for zeros, _ in prev)
         curr_total_zeros = sum(zeros for zeros, _ in curr)
         log.info(
-            f"Applied `{self._pruning_fn_name}`. Pruned:"
+            f"Applied `{self._pruning_method_name}`. Pruned:"
             f" {prev_total_zeros}/{total_params} ({prev_total_zeros / total_params:.2%}) ->"
             f" {curr_total_zeros}/{total_params} ({curr_total_zeros / total_params:.2%})"
         )
@@ -357,7 +357,7 @@ class ModelPruning(Callback):
                 prev_mask_zeros, prev_mask_size = prev[i]
                 curr_mask_zeros, curr_mask_size = curr[i]
                 log.info(
-                    f"Applied `{self._pruning_fn_name}` to `{module!r}.{name}` with amount={amount}. Pruned:"
+                    f"Applied `{self._pruning_method_name}` to `{module!r}.{name}` with amount={amount}. Pruned:"
                     f" {prev_mask_zeros} ({prev_mask_zeros / prev_mask_size:.2%}) ->"
                     f" {curr_mask_zeros} ({curr_mask_zeros / curr_mask_size:.2%})"
                 )
