dd81b696d9793b5db3019617048f93aa1d75634a
Carlos Mocholi <carlossmocholi@gmail.com>
2021-10-19 16:09:33 +0200

    Add `TPUPrecisionPlugin`

--- pytorch_lightning/plugins/precision/__init__.py
+++ pytorch_lightning/plugins/precision/__init__.py
@@ -8,4 +8,5 @@ from pytorch_lightning.plugins.precision.mixed import MixedPrecisionPlugin  # no
 from pytorch_lightning.plugins.precision.native_amp import NativeMixedPrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.precision_plugin import PrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.sharded_native_amp import ShardedNativeMixedPrecisionPlugin  # noqa: F401
+from pytorch_lightning.plugins.precision.tpu import TPUPrecisionPlugin  # noqa: F401
 from pytorch_lightning.plugins.precision.tpu_bfloat import TPUHalfPrecisionPlugin  # noqa: F401
