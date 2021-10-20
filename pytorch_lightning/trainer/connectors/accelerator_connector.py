c376daf94277050a92ad9a3dae609ae6035eb5b7
Carlos Mocholi <carlossmocholi@gmail.com>
2021-10-20 02:42:04 +0200

    .

--- pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -640,7 +640,7 @@ class AcceleratorConnector:
             self.precision = "bf16"
 
         if self.precision == 16:
-            rank_zero_info(f"Using 16bit {self.amp_type.value} mixed precision")
+            rank_zero_info(f"Using 16bit {self.amp_type.value} Automatic Mixed Precision (AMP)")
 
             if self.amp_type == AMPType.NATIVE:
                 if self._is_sharded_training_type:
@@ -664,7 +664,7 @@ class AcceleratorConnector:
                     "You passed `Trainer(amp_type='apex', precision='bf16')` but it's not supported."
                     " Try using `amp_type='native'` instead."
                 )
-            rank_zero_info("Using bfloat16 native mixed precision")
+            rank_zero_info("Using bfloat16 Automatic Mixed Precision (AMP)")
             if self._is_sharded_training_type:
                 return ShardedNativeMixedPrecisionPlugin(self.precision, use_cpu=self.use_cpu)
             if self._is_fully_sharded_training_type:
