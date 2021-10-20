18a4dd1b0fca33c26bbeb6a7bda66da02b002dc0
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-20 14:22:42 +0200

    update todo note

--- tests/lite/test_wrappers.py
+++ tests/lite/test_wrappers.py
@@ -41,6 +41,7 @@ def test_lite_module_wraps():
         (32, torch.float64, torch.float32),
         (16, torch.float32, torch.float16),
         (16, torch.float64, torch.float16),
+        # ("mixed", torch.float32, torch.float16),  # TODO: support precision="mixed"
     ],
 )
 def test_lite_module_forward_conversion(precision, input_type, expected_type):
