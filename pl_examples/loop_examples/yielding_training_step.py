6a8117499abf74aa468ad96fbb1719b69627874d
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-21 11:13:56 +0200

    remove unused imports

--- pl_examples/loop_examples/yielding_training_step.py
+++ pl_examples/loop_examples/yielding_training_step.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import inspect
-from abc import ABC, abstractmethod
 from functools import partial
 from typing import Generator
 
