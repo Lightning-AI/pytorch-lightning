4c81c78e21fe521271468f5a34fcaf255383c386
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-21 03:53:03 +0200

    add tests for distributed sampler

--- tests/lite/test_lite.py
+++ tests/lite/test_lite.py
@@ -12,14 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from unittest import mock
-from unittest.mock import Mock, patch, PropertyMock
+from unittest.mock import Mock, PropertyMock
 
 import pytest
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Sampler, DistributedSampler
 
 from pytorch_lightning.lite import LightningLite
 from pytorch_lightning.lite.wrappers import _LiteDataLoader
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
 class EmptyLite(LightningLite):
@@ -28,6 +29,7 @@ class EmptyLite(LightningLite):
 
 
 def test_setup_dataloaders_return_type():
+    """Test that the setup method returns the dataloaders wrapped as LiteDataLoader and in the right order."""
     lite = EmptyLite()
 
     # single dataloader
@@ -35,8 +37,8 @@ def test_setup_dataloaders_return_type():
     assert isinstance(lite_dataloader, _LiteDataLoader)
 
     # multiple dataloaders
-    dataset0 = range(2)
-    dataset1 = range(3)
+    dataset0 = Mock()
+    dataset1 = Mock()
     dataloader0 = DataLoader(dataset0)
     dataloader1 = DataLoader(dataset1)
     lite_dataloader0, lite_dataloader1 = lite.setup_dataloaders(dataloader0, dataloader1)
@@ -52,6 +54,7 @@ def test_setup_dataloaders_return_type():
     return_value=torch.device("cuda", 1),
 )
 def test_setup_dataloaders_move_to_device(lite_device_mock):
+    """Test that the setup configures LiteDataLoader to move the data to the device automatically."""
     lite = EmptyLite()
     lite_dataloaders = lite.setup_dataloaders(DataLoader(Mock()), DataLoader(Mock()), move_to_device=False)
     assert all(dl.device is None for dl in lite_dataloaders)
@@ -61,3 +64,41 @@ def test_setup_dataloaders_move_to_device(lite_device_mock):
     lite_dataloaders = lite.setup_dataloaders(DataLoader(Mock()), DataLoader(Mock()), move_to_device=True)
     assert all(dl.device == torch.device("cuda", 1) for dl in lite_dataloaders)
     lite_device_mock.assert_called()
+
+
+def test_setup_dataloaders_distributed_sampler_not_needed():
+    """Test that replace_sampler option has no effect when no distributed sampler is needed."""
+    custom_sampler = Mock(spec=Sampler)
+    dataloader = DataLoader(Mock(), sampler=custom_sampler)
+
+    # keep the custom sampler when not needed to replace
+    lite = EmptyLite()
+    lite_dataloader = lite.setup_dataloaders(dataloader, replace_sampler=True)
+    assert lite_dataloader.sampler is custom_sampler
+
+
+@pytest.mark.parametrize("strategy", LightningLite._supported_strategy_types())
+def test_setup_dataloaders_replace_custom_sampler(strategy):
+    """Test that asking to replace a custom sampler results in an error when a distributed sampler would be needed."""
+    custom_sampler = Mock(spec=Sampler)
+    dataloader = DataLoader(Mock(), sampler=custom_sampler)
+
+    # explicitly asking to replace when a custom sampler is already configured raises an exception
+    lite = EmptyLite(accelerator="cpu", strategy=strategy, devices=2)
+    if getattr(lite._strategy, "is_distributed", False):
+        with pytest.raises(MisconfigurationException, match="You seem to have configured a sampler in your DataLoader"):
+            lite.setup_dataloaders(dataloader, replace_sampler=True)
+
+    # setting `replace_sampler=False` leaves the sampler untouched
+    lite_dataloader = lite.setup_dataloaders(dataloader, replace_sampler=False)
+    assert lite_dataloader.sampler is custom_sampler
+
+
+@pytest.mark.parametrize("strategy", LightningLite._supported_strategy_types())
+@pytest.mark.parametrize("shuffle", [True, False])
+def test_setup_dataloaders_replace_standard_sampler(shuffle, strategy):
+    """Test that Lite replaces the default samplers with DistributedSampler automatically."""
+    lite = EmptyLite(accelerator="cpu", strategy=strategy, devices=2)
+    is_distributed = getattr(lite._strategy, "is_distributed", False)
+    lite_dataloader = lite.setup_dataloaders(DataLoader(range(3), shuffle=shuffle))
+    assert not is_distributed or isinstance(lite_dataloader.sampler, DistributedSampler)
