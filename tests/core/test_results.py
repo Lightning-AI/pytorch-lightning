7eb2edf4216f95420eee2cd4a43a519c61572a5c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-25 14:09:05 +0200

    rename set_random_master_port (#10104)
    Co-authored-by: tchaton <thomas@grid.ai>

--- tests/core/test_results.py
+++ tests/core/test_results.py
@@ -41,6 +41,6 @@ def _ddp_test_fn(rank, worldsize):
 @RunIf(skip_windows=True)
 def test_result_reduce_ddp():
     """Make sure result logging works with DDP."""
-    tutils.set_random_master_port()
+    tutils.set_random_main_port()
     worldsize = 2
     mp.spawn(_ddp_test_fn, args=(worldsize,), nprocs=worldsize)
