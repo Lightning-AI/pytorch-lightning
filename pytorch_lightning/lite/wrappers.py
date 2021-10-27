782c70f46996af99b71679b72ad7bb00254e23f8
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-27 09:25:36 +0200

    lightning lite package and tests

---
+++ pytorch_lightning/lite/wrappers.py
@@ -0,0 +1,151 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Callable, Dict, Generator, Iterator, List, Optional, Union
+
+import torch
+from torch import nn as nn
+from torch import Tensor
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+from pytorch_lightning.accelerators import Accelerator
+from pytorch_lightning.utilities.apply_func import apply_to_collection, move_data_to_device
+
+
+def _do_nothing_closure() -> None:
+    return None
+
+
+class _LiteOptimizer:
+    def __init__(self, optimizer: Optimizer, accelerator: Accelerator) -> None:
+        """LiteOptimizer is a thin wrapper around the :class:`~torch.optim.Optimizer` that delegates the optimizer
+        step calls to the accelerator/strategy plugin.
+
+        The underlying wrapped optimizer object can be accessed via the property :attr:`optimizer`.
+
+        Args:
+            optimizer: The optimizer to wrap
+            accelerator: Reference to the accelerator for handling the optimizer step
+        """
+        self.__dict__ = {k: v for k, v in optimizer.__dict__.items() if k not in ("step", "__del__")}
+        self.__class__ = type("Lite" + optimizer.__class__.__name__, (self.__class__, optimizer.__class__), {})
+        self._optimizer = optimizer
+        self._accelerator = accelerator
+
+    @property
+    def optimizer(self) -> Optimizer:
+        return self._optimizer
+
+    @property
+    def state(self) -> Dict[str, torch.Tensor]:
+        return self._optimizer.state
+
+    @state.setter
+    def state(self, state: Dict[str, torch.Tensor]) -> None:
+        self._optimizer.state = state
+
+    @property
+    def defaults(self) -> Dict[str, Any]:
+        return self._optimizer.defaults
+
+    @defaults.setter
+    def defaults(self, defaults: Dict[str, Any]) -> None:
+        self._optimizer.defaults = defaults
+
+    @property
+    def param_groups(self) -> List[Dict[str, torch.Tensor]]:
+        return self._optimizer.param_groups
+
+    @param_groups.setter
+    def param_groups(self, param_groups: List[Dict[str, torch.Tensor]]) -> None:
+        self._optimizer.param_groups = param_groups
+
+    def step(self, closure: Optional[Callable] = None) -> None:
+        closure = closure or _do_nothing_closure
+        self._accelerator.optimizer_step(
+            self._optimizer,
+            opt_idx=0,
+            lambda_closure=closure,
+            model=self._accelerator.model,
+        )
+
+    def zero_grad(self, *args: Any, **kwargs: Any) -> None:
+        self._optimizer.zero_grad(*args, **kwargs)
+
+
+class _LiteModule(nn.Module):
+    # TODO: Pass in the precision plugin instead of accelerator
+    def __init__(self, module: nn.Module, accelerator: Accelerator) -> None:
+        """The LiteModule is a thin wrapper around the :class:`torch.nn.Module` and handles precision / autocast
+        automatically for the forward pass.
+
+        The underlying wrapped module can be accessed via the property :attr:`module`.
+
+        Args:
+            module: The module to wrap
+            accelerator: Reference to the accelerator for handling precision context
+        """
+        super().__init__()
+        self._module = module
+        self._accelerator = accelerator
+
+    @property
+    def module(self) -> nn.Module:
+        return self._module
+
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
+        """Casts all inputs to the right precision and handles autocast for operations in the module forward
+        method."""
+        precision = self._accelerator.precision_plugin.precision
+        precision_to_type = {
+            "mixed": torch.float16,
+            16: torch.float16,
+            32: torch.float32,
+            64: torch.float64,
+        }
+        # TODO (@awaelchli): let the precision plugin handle the conversion
+        to_type = precision_to_type[precision]
+        args, kwargs = apply_to_collection([args, kwargs], function=lambda t: t.to(to_type), dtype=Tensor)
+
+        with self._accelerator.precision_plugin.forward_context():
+            output = self.module(*args, **kwargs)
+
+        output = apply_to_collection(output, function=lambda t: t.to(torch.get_default_dtype()), dtype=Tensor)
+        return output
+
+
+class _LiteDataLoader(DataLoader):
+    def __init__(self, device: Optional[torch.device] = None, **dl_kwargs: Any) -> None:
+        """The LiteDataLoader is an extension of the PyTorch :class:`~torch.utils.data.DataLoader` that adds
+        additional features such as moving the data to the device automatically.
+
+        Args:
+            device: The device to which the data should be moved. By default the device is `None` and no data
+                transfers will be made (identical behavior as :class:`~torch.utils.data.DataLoader`).
+            **dl_kwargs: Accepts all arguments that the PyTorch :class:`~torch.utils.data.DataLoader` accepts.
+        """
+        super().__init__(**dl_kwargs)
+        self._device = device
+
+    @property
+    def device(self) -> Optional[torch.device]:
+        return self._device
+
+    def __iter__(self) -> Union[Iterator[Any], Generator[Any, None, None]]:
+        iterator = super().__iter__()
+        if self._device is None:
+            return iterator
+
+        for item in iterator:
+            yield move_data_to_device(item, self._device)
---
+++ tests/lite/test_wrappers.py
@@ -0,0 +1,106 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest.mock import ANY, Mock
+
+import pytest
+import torch
+
+from pytorch_lightning.lite import LightningLite
+from pytorch_lightning.lite.wrappers import _LiteDataLoader, _LiteModule, _LiteOptimizer
+from tests.helpers.runif import RunIf
+
+
+class EmptyLite(LightningLite):
+    def run(self):
+        pass
+
+
+def test_lite_module_wraps():
+    """Test that the wrapped module is accessible via the property."""
+    module = Mock()
+    assert _LiteModule(module, Mock()).module is module
+
+
+@RunIf(min_gpus=1)
+@pytest.mark.parametrize(
+    "precision, input_type, expected_type",
+    [
+        (32, torch.float16, torch.float32),
+        (32, torch.float32, torch.float32),
+        (32, torch.float64, torch.float32),
+        (16, torch.float32, torch.float16),
+        (16, torch.float64, torch.float16),
+        # ("mixed", torch.float32, torch.float16),  # TODO: support precision="mixed"
+    ],
+)
+def test_lite_module_forward_conversion(precision, input_type, expected_type):
+    """Test that the LiteModule performs autocasting on the input tensors and during forward()."""
+    lite = EmptyLite(precision=precision, accelerator="gpu", devices=1)
+    device = torch.device("cuda", 0)
+
+    def check_autocast(forward_input):
+        assert precision not in (16, "mixed") or torch.is_autocast_enabled()
+        return forward_input
+
+    module = Mock(wraps=torch.nn.Linear(1, 1), side_effect=check_autocast)
+    lite_module = _LiteModule(module, lite._accelerator).to(device)
+    out = lite_module(torch.rand(1, dtype=input_type, device=device))
+    assert module.call_args[0][0].dtype == expected_type
+    assert out.dtype == torch.get_default_dtype()
+
+
+@pytest.mark.parametrize(
+    "src_device, dest_device",
+    [
+        (torch.device("cpu"), torch.device("cpu")),
+        pytest.param(torch.device("cpu"), torch.device("cuda", 0), marks=RunIf(min_gpus=1)),
+        pytest.param(torch.device("cuda", 0), torch.device("cpu"), marks=RunIf(min_gpus=1)),
+    ],
+)
+def test_lite_dataloader_device_placement(src_device, dest_device):
+    """Test that the LiteDataLoader moves data to the device in its iterator."""
+    sample0 = torch.tensor(0, device=src_device)
+    sample1 = torch.tensor(1, device=src_device)
+    sample2 = {"data": torch.tensor(2, device=src_device)}
+    sample3 = {"data": torch.tensor(3, device=src_device)}
+    data = [sample0, sample1, sample2, sample3]
+    lite_dataloader = _LiteDataLoader(device=dest_device, dataset=data, batch_size=2)
+    iterator = iter(lite_dataloader)
+
+    batch0 = next(iterator)
+    assert torch.equal(batch0, torch.tensor([0, 1], device=dest_device))
+
+    batch1 = next(iterator)
+    assert torch.equal(batch1["data"], torch.tensor([2, 3], device=dest_device))
+
+
+def test_lite_optimizer_wraps():
+    """Test that the LiteOptimizer fully wraps the optimizer."""
+    optimizer_cls = torch.optim.SGD
+    optimizer = Mock(spec=optimizer_cls)
+    lite_optimizer = _LiteOptimizer(optimizer, Mock())
+    assert lite_optimizer.optimizer is optimizer
+    assert isinstance(lite_optimizer, optimizer_cls)
+
+
+def test_lite_optimizer_steps():
+    """Test that the LiteOptimizer forwards the step() and zero_grad() calls to the wrapped optimizer."""
+    optimizer = Mock()
+    accelerator = Mock()
+    lite_optimizer = _LiteOptimizer(optimizer=optimizer, accelerator=accelerator)
+    lite_optimizer.step()
+    accelerator.optimizer_step.assert_called_once()
+    accelerator.optimizer_step.assert_called_with(optimizer, opt_idx=0, lambda_closure=ANY, model=accelerator.model)
+    lite_optimizer.zero_grad()
+    optimizer.zero_grad.assert_called_once()
