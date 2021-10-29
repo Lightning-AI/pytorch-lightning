d7b430fa20e1c113f307b31d5b48a2d6f54aca83
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-10-29 10:41:46 +0200

    Update pl_examples/basic_examples/mnist_examples/image_classifier_2_lite.py
    Co-authored-by: Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>

--- pl_examples/basic_examples/mnist_examples/image_classifier_2_lite.py
+++ pl_examples/basic_examples/mnist_examples/image_classifier_2_lite.py
@@ -88,7 +88,7 @@ class Lite(LightningLite):
             test_loss = 0
             with torch.no_grad():
                 for data, target in test_loader:
-                    # NOTE: no need to call .to(device) on the data, target"
+                    # NOTE: no need to call .to(device) on the data, target
                     output = model(data)
                     test_loss += F.nll_loss(output, target, reduction="sum").item()
 
