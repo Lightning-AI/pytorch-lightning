cfdc53d0eda90c5c51de2b1c55f8dcd72024254d
tchaton <thomas@grid.ai>
2021-10-30 18:17:22 +0100

    update

--- pytorch_lightning/core/system.py
+++ pytorch_lightning/core/system.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The LightningSystem - an LightningModule """
+import functools
 from collections import OrderedDict
-from typing import Optional, Union
+from typing import Callable, Dict, IO, Optional, Type, Union
 
 import torch
 from torch import Tensor
@@ -21,15 +22,72 @@ from torch.nn import Module, Parameter
 from torchmetrics import Metric
 
 from pytorch_lightning import LightningModule
+from pytorch_lightning.utilities.cloud_io import load as pl_load
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
-__all__ = ["LightningSystem"]
+
+# https://stackoverflow.com/a/63851681/9201239
+def _get_all_subclasses(cls):
+    subclass_list = []
+
+    def recurse(cl):
+        for subclass in cl.__subclasses__():
+            subclass_list.append(subclass)
+            recurse(subclass)
+
+    recurse(cls)
+
+    return set(subclass_list)
+
+
+def _wrap_init(f, system=None):
+    @functools.wraps(f)
+    def wrapper(module, *args, **kwargs):
+        nonlocal system
+        f(module, *args, **kwargs)
+        system._module_map_to_arguments[id(module)] = (args, kwargs)
+
+    return wrapper
+
+
+def _enable_class(cls, system: "LightningSystem"):
+    cls._old_init = cls.__init__
+    cls.__init__ = _wrap_init(cls.__init__, system=system)
+
+
+def _disable_class(cls):
+    cls.__init__ = cls._old_init
+
+
+def from_checkpoint(
+    module_cls: Type[Module],
+    checkpoint_path: Union[str, IO],
+    map_location: Optional[Union[Dict[str, str], str, torch.device, int, Callable]] = None,
+    **kwargs,
+) -> Module:
+    if map_location is not None:
+        checkpoint = pl_load(checkpoint_path, map_location=map_location)
+    else:
+        checkpoint = pl_load(checkpoint_path, map_location=lambda storage, loc: storage)
+
+    module, module_class_name = checkpoint.pop("module_info")
+    assert module_cls.__module__ == module
+    assert module_cls.__name__ == module_class_name
+    checkpoint.pop("module_name")
+    module = module_cls(*checkpoint.pop("args"), **checkpoint.pop("kwargs"))
+    module.load_state_dict(checkpoint)
+    return module
 
 
 class LightningSystem(LightningModule):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._metric_modules = OrderedDict()
+        self._module_map_to_arguments = {}
+        self._args = None
+        self._kwargs = None
+        for subclass in _get_all_subclasses(Module):
+            _enable_class(subclass, self)
 
     def __setattr__(self, name: str, value: Union[Tensor, "Module"]) -> None:
         def remove_from(*dicts_or_sets):
@@ -55,6 +113,11 @@ class LightningSystem(LightningModule):
                         "A `LightningSystem` supports only a single nn.Module expects `torchmetrics.Metric`."
                     )
                 modules[name] = value
+                if not isinstance(value, Metric):
+                    for subclass in _get_all_subclasses(Module):
+                        _disable_class(subclass)
+                    self._args, self._kwargs = self._module_map_to_arguments[id(value)]
+                    self._module_map_to_arguments = None
             elif modules is not None and name in modules:
                 if value is not None:
                     raise TypeError(
@@ -79,6 +142,13 @@ class LightningSystem(LightningModule):
             modules: OrderedDict = self.__dict__.get("_modules")
             return list(modules.keys())[0]
 
+    @property
+    def module_info(self) -> Optional[str]:
+        if self.has_module:
+            modules: OrderedDict = self.__dict__.get("_modules")
+            module = list(modules.values())[0]
+            return (module.__module__, module.__class__.__name__)
+
     def state_dict(self, destination=None, prefix="", keep_vars=False):
         state_dict = super().state_dict()
         if self.has_module:
@@ -89,7 +159,12 @@ class LightningSystem(LightningModule):
                     return k.replace(module_name + ".", "")
                 return k
 
-            return {prune_name(k): v for k, v in state_dict.items()}
+            state_dict = {prune_name(k): v for k, v in state_dict.items()}
+            state_dict["args"] = self._args
+            state_dict["kwargs"] = self._kwargs
+            state_dict["module_name"] = self.module_name
+            state_dict["module_info"] = self.module_info
+            return state_dict
         return state_dict
 
     def load_state_dict(self, state_dict: "OrderedDict[str, Tensor]", strict: bool = True):
@@ -104,3 +179,6 @@ class LightningSystem(LightningModule):
 
         state_dict = OrderedDict({add_name(k): v for k, v in state_dict.items()})
         return super().load_state_dict(state_dict, strict=strict)
+
+
+__all__ = ["LightningSystem", "from_checkpoint"]
--- tests/core/test_lightning_system.py
+++ tests/core/test_lightning_system.py
@@ -11,27 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 from copy import deepcopy
 
 import pytest
 import torch
 from torch import nn
 
-from pytorch_lightning.core.system import LightningSystem
+from pytorch_lightning.core.system import from_checkpoint, LightningSystem
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
 def test_lightning_system(tmpdir):
-    class System(LightningSystem):
-        def ___init__(self):
-            super().__init__()
 
-    system = System()
+    system = LightningSystem()
     assert not system.has_module
     assert system.module_name is None
 
-    linear = nn.Linear(1, 1)
+    linear = nn.Linear(in_features=1, out_features=1)
     system.model = linear
+    assert system._args == ()
+    assert system._kwargs == dict(in_features=1, out_features=1)
     assert system.has_module
     assert system.module_name == "model"
 
@@ -39,7 +39,7 @@ def test_lightning_system(tmpdir):
         MisconfigurationException,
         match="A `LightningSystem` supports only a single nn.Module expects `torchmetrics.Metric`",
     ):
-        setattr(system, "model_1", nn.Linear(1, 1))
+        setattr(system, "model_1", nn.Linear(in_features=1, out_features=1))
 
     with pytest.raises(
         MisconfigurationException,
@@ -48,7 +48,25 @@ def test_lightning_system(tmpdir):
         setattr(system, "weight", nn.Parameter(torch.Tensor(1, 1)))
 
     state_dict = deepcopy(system.state_dict())
-    assert state_dict == linear.state_dict()
+    assert state_dict["weight"] == linear.state_dict()["weight"]
 
-    system.load_state_dict(nn.Linear(1, 1).state_dict())
+    system.load_state_dict(nn.Linear(in_features=1, out_features=1).state_dict())
     assert system.state_dict()["weight"] != state_dict["weight"]
+
+    class DummyModel(nn.Module):
+        def __init__(self, name: str, in_features, out_features):
+            super().__init__()
+            self.linear = nn.Linear(in_features, out_features)
+
+    class System(LightningSystem):
+        def __init__(self, name: str, in_features, out_features, random):
+            super().__init__()
+            self.save_hyperparameters()
+            self.module = DummyModel(name, in_features, out_features + 1)
+
+    system = System("dummy", 1, 1, "random")
+
+    p = os.path.join(tmpdir, "model.pt")
+    torch.save(state_dict, p)
+    linear = from_checkpoint(nn.Linear, p)
+    assert linear.state_dict()["weight"] == state_dict["weight"]
