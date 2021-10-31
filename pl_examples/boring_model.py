7be1e968205ae942fabe090ceafd797f3d085fcc
tchaton <thomas@grid.ai>
2021-10-31 09:40:29 +0000

    update

---
+++ pl_examples/boring_model.py
@@ -0,0 +1,43 @@
+import os
+import tempfile
+
+import torch
+
+from pytorch_lightning import LightningModule
+
+
+class BoringModel(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.layer = torch.nn.Linear(32, 2)
+
+    def forward(self, x):
+        return self.layer(x)
+
+    def training_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("train_loss", loss)
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("valid_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("test_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
+
+
+def run():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        checkpoint_path = os.path.join(tmpdir, "model.pt")
+        state_dict = {"state_dict": BoringModel().state_dict()}
+        torch.save(state_dict, checkpoint_path)
+        BoringModel.load_from_checkpoint(checkpoint_path)
+
+
+if __name__ == "__main__":
+    run()
