009a02e3a9ac69b466d18fc76d16e347a730ca77
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2021-11-01 23:18:16 +0530

    Update pytorch_lightning/lite/wrappers.py

--- pytorch_lightning/lite/wrappers.py
+++ pytorch_lightning/lite/wrappers.py
@@ -151,7 +151,7 @@ def _replace_dataloader_init_method() -> Generator:
 
 class _LiteDataLoader:
     def __init__(self, dataloader: Iterable, device: Optional[torch.device] = None) -> None:
-        """The LiteDataLoader is an extension of an Iterator. It would move move the data to the device
+        """The LiteDataLoader is an extension of an Iterator. It would move the data to the device
         automatically if the device is specified.
 
         Args:
