e3eb9353302c6301707bca0ceedf6583dddac49e
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-04 18:45:58 +0100

    Drop torch 1.6 support

--- requirements/adjust_versions.py
+++ requirements/adjust_versions.py
@@ -14,7 +14,6 @@ VERSIONS = [
     dict(torch="1.8.0", torchvision="0.9.0", torchtext="0.9.0"),
     dict(torch="1.7.1", torchvision="0.8.2", torchtext="0.8.1"),
     dict(torch="1.7.0", torchvision="0.8.1", torchtext="0.8.0"),
-    dict(torch="1.6.0", torchvision="0.7.0", torchtext="0.7"),
 ]
 
 
