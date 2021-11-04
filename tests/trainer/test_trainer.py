21f7e799f16b517ddfeed596e67f7a89c287db04
Siyu Wang <siyuw@fb.com>
2021-11-03 23:51:52 -0700

    Add more trainer config and ttp register tests

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -2163,12 +2163,13 @@ def test_detect_anomaly_nan(tmpdir):
             dict(_distrib_type=None, _device_type=DeviceType.CPU, num_gpus=0, num_processes=1),
         ),
         (
-            dict(strategy="fsdp", gpus=2),
-            dict(_distrib_type=DistributedType.DDP_FULLY_SHARDED, _device_type=DeviceType.GPU, num_gpus=2, num_processes=1),
-        ),
-        (
             dict(strategy="ddp_fully_sharded", gpus=1),
-            dict(_distrib_type=DistributedType.DDP_FULLY_SHARDED, _device_type=DeviceType.GPU, num_gpus=1, num_processes=1),
+            dict(
+                _distrib_type=DistributedType.DDP_FULLY_SHARDED,
+                _device_type=DeviceType.GPU,
+                num_gpus=1,
+                num_processes=1,
+            ),
         ),
         (
             dict(strategy=DDPSpawnPlugin(), num_processes=2, gpus=None),
