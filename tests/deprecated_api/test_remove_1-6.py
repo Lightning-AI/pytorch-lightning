f54ae6dcbd705f84d98cc3a4a22e2bbae0163abe
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-11-05 10:14:33 +0100

    apply removal to spawn plugin

--- tests/deprecated_api/test_remove_1-6.py
+++ tests/deprecated_api/test_remove_1-6.py
@@ -46,16 +46,6 @@ def test_old_transfer_batch_to_device_hook(tmpdir):
         trainer.fit(OldModel())
 
 
-def test_v1_6_0_ddp_spawn_num_nodes():
-    with pytest.deprecated_call(match="Argument `num_nodes` in `DDPSpawnPlugin` is deprecated in v1.4"):
-        DDPSpawnPlugin(num_nodes=1)
-
-
-def test_v1_6_0_ddp_spawn_sync_batchnorm():
-    with pytest.deprecated_call(match="Argument `sync_batchnorm` in `DDPSpawnPlugin` is deprecated in v1.4"):
-        DDPSpawnPlugin(sync_batchnorm=False)
-
-
 def test_v1_6_0_reload_dataloaders_every_epoch(tmpdir):
     model = BoringModel()
 
