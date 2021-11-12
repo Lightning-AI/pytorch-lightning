f950cd29d06bc8e00de369b84329469374ad20fe
Jirka <jirka.borovec@seznam.cz>
2021-11-12 22:57:24 +0100

    fixing saving hparams in yaml

--- pytorch_lightning/core/saving.py
+++ pytorch_lightning/core/saving.py
@@ -387,7 +387,7 @@ def save_hparams_to_yaml(config_yaml, hparams: Union[dict, Namespace], use_omega
         try:
             v = v.name if isinstance(v, Enum) else v
             yaml.dump(v)
-        except TypeError:
+        except (AttributeError, TypeError):
             warn(f"Skipping '{k}' parameter because it is not possible to safely dump to YAML.")
             hparams[k] = type(v).__name__
         else:
