8b0cb47cc03bc29a69e84a5637f8caf23ef364a7
puhuk <wetr235@gmail.com>
2021-11-15 20:54:47 +0900

    Remove deprecated `hpc_load` in `CheckpointConnector` (#10525)
    Co-authored-by: Aki Nitta <nitta@akihironitta.com>

--- tests/deprecated_api/test_remove_1-6.py
+++ tests/deprecated_api/test_remove_1-6.py
@@ -62,13 +62,3 @@ def test_v1_6_0_deprecated_disable_validation():
     trainer = Trainer()
     with pytest.deprecated_call(match="disable_validation` is deprecated in v1.4"):
         _ = trainer.disable_validation
-
-
-def test_v1_6_0_deprecated_hpc_load(tmpdir):
-    model = BoringModel()
-    trainer = Trainer(default_root_dir=tmpdir, max_steps=1)
-    trainer.fit(model)
-    trainer.checkpoint_connector.hpc_save(tmpdir, trainer.logger)
-    checkpoint_path = trainer.checkpoint_connector.get_max_ckpt_path_from_folder(str(tmpdir))
-    with pytest.deprecated_call(match=r"`CheckpointConnector.hpc_load\(\)` was deprecated in v1.4"):
-        trainer.checkpoint_connector.hpc_load(checkpoint_path)
