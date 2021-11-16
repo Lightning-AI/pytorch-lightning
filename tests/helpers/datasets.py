2b7dc18ff28f9ac492c5361a88baf4eb768ba75d
tchaton <thomas@grid.ai>
2021-11-16 17:05:34 +0000

    update

--- tests/helpers/datasets.py
+++ tests/helpers/datasets.py
@@ -16,12 +16,11 @@ import os
 import random
 import time
 import urllib.request
-from typing import List, Optional, Sequence, Tuple
+from typing import Optional, Sequence, Tuple
 
-import numpy as np
 import torch
 from torch import Tensor
-from torch.utils.data import Dataset, Subset
+from torch.utils.data import Dataset
 
 
 class MNIST(Dataset):
@@ -214,24 +213,3 @@ class SklearnDataset(Dataset):
 
     def __len__(self):
         return len(self.y)
-
-
-def make_unbalanced(dataset: Dataset, weights: List[float]):
-    assert np.sum(weights) == 1
-
-    indices = []
-
-    total = np.bincount(dataset.targets).min() * np.array(weights)
-    count = np.zeros(len(weights))
-
-    new_targets = []
-
-    for idx, t in enumerate(dataset.targets):
-        if count[t] < total[t]:
-            indices.append(idx)
-            new_targets.append(t)
-            count[t] += 1
-
-    subset = Subset(dataset, indices)
-    subset.targets = np.asarray(new_targets)
-    return subset
