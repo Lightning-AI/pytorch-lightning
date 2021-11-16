b31ab514aa5abddeed796a59c200bf283b5f3133
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-11-16 20:23:19 +0100

    update version

--- pytorch_lightning/__about__.py
+++ pytorch_lightning/__about__.py
@@ -1,7 +1,7 @@
 import time
 
 _this_year = time.strftime("%Y")
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 __author__ = "William Falcon et al."
 __author_email__ = "waf2107@columbia.edu"
 __license__ = "Apache-2.0"
