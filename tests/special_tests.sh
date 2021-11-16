982f8a67389d732d9df578ffc66bb60cbc9511d6
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-16 19:52:02 +0100

    Revert "Debug"
    This reverts commit e18e4872ff2525f08758aa347c650a53b3704918.

--- tests/special_tests.sh
+++ tests/special_tests.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -ex
+set -e
 
 # this environment variable allows special tests to run
 export PL_RUNNING_SPECIAL_TESTS=1
