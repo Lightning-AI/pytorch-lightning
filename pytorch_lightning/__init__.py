59a1473465d70eefc1e321de772d04cad4967526
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-17 01:17:51 +0100

    Add workaround

--- pytorch_lightning/__init__.py
+++ pytorch_lightning/__init__.py
@@ -1,7 +1,6 @@
 """Root package info."""
 
 import logging
-import os
 
 from pytorch_lightning.__about__ import *  # noqa: F401, F403
 
@@ -12,9 +11,7 @@ _logger.setLevel(logging.INFO)
 # if root logger has handlers, propagate messages up and let root logger process them
 if not _root_logger.hasHandlers():
     _logger.addHandler(logging.StreamHandler())
-
-_PACKAGE_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+    _logger.propagate = False
 
 from pytorch_lightning.callbacks import Callback  # noqa: E402
 from pytorch_lightning.core import LightningDataModule, LightningModule  # noqa: E402
--- tests/conftest.py
+++ tests/conftest.py
@@ -99,6 +99,18 @@ def reset_deterministic_algorithm():
         torch.set_deterministic(False)
 
 
+@pytest.yield_fixture
+def caplog(caplog):
+    """Workaround for https://github.com/pytest-dev/pytest/issues/3697."""
+    import logging
+
+    lightning_logger = logging.getLogger("pytorch_lightning")
+    propagate = lightning_logger.propagate
+    lightning_logger.propagate = True
+    yield caplog
+    lightning_logger.propagate = propagate
+
+
 @pytest.fixture
 def tmpdir_server(tmpdir):
     if sys.version_info >= (3, 7):
