4d4dbdb981dc6eaee1faab87cb567fab0de37b0e
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-17 23:12:30 +0100

    skip

--- tests/accelerators/test_accelerator_connector.py
+++ tests/accelerators/test_accelerator_connector.py
@@ -343,7 +343,7 @@ def test_accelerator_choice_ddp_cpu_and_strategy(tmpdir):
     _test_accelerator_choice_ddp_cpu_and_strategy(tmpdir, ddp_strategy_class=DDPPlugin)
 
 
-@RunIf(skip_windows=True)
+@RunIf(skip_windows=True, skip_49370=True)
 def test_accelerator_choice_ddp_cpu_and_strategy_spawn(tmpdir):
     """Test that accelerator="ddp_cpu" can work together with an instance of DDPPSpawnPlugin."""
     _test_accelerator_choice_ddp_cpu_and_strategy(tmpdir, ddp_strategy_class=DDPSpawnPlugin)
