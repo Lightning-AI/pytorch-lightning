26dfd56358be0f35226b601b068a4da19d42dea5
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-17 16:28:30 +0100

    Bad merge

--- tests/conftest.py
+++ tests/conftest.py
@@ -99,18 +99,6 @@ def reset_deterministic_algorithm():
         torch.set_deterministic(False)
 
 
-@pytest.yield_fixture
-def caplog(caplog):
-    """Workaround for https://github.com/pytest-dev/pytest/issues/3697."""
-    import logging
-
-    lightning_logger = logging.getLogger("pytorch_lightning")
-    propagate = lightning_logger.propagate
-    lightning_logger.propagate = True
-    yield caplog
-    lightning_logger.propagate = propagate
-
-
 @pytest.fixture
 def caplog(caplog):
     """Workaround for https://github.com/pytest-dev/pytest/issues/3697.
