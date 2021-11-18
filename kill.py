0547dce4e640c9e52a0d07241156f443bdc2c79c
tchaton <thomas@grid.ai>
2021-11-18 19:18:15 +0000

    update

--- kill.py
+++
@@ -1,7 +0,0 @@
-import os
-import signal
-
-# ps -aef | grep -i 'test.py' | grep -v 'grep' | awk '{ print $2 }'
-os.kill(1774059, signal.SIGUSR1)
-os.kill(1775127, signal.SIGUSR1)
-os.kill(1775383, signal.SIGUSR1)
