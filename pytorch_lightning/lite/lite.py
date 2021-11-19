b9bb5c320d90c115b86a3dca9e6ad834953ef3be
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-11-19 22:43:34 +0100

    drop unused import

--- pytorch_lightning/lite/lite.py
+++ pytorch_lightning/lite/lite.py
@@ -16,7 +16,7 @@ from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from functools import partial
 from pathlib import Path
-from typing import Any, Callable, Dict, Generator, Iterable, List, Optional, Sequence, Tuple, Union, cast
+from typing import Any, Callable, Dict, Generator, List, Optional, Sequence, Tuple, Union, cast
 
 import torch
 import torch.nn as nn
