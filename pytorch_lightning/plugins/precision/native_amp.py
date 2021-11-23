bb98ac4422d8527a00ee132c5cf6bf0c56a805e5
four4fish <88516121+four4fish@users.noreply.github.com>
2021-11-22 22:47:31 -0800

    Update native_amp.py

--- pytorch_lightning/plugins/precision/native_amp.py
+++ pytorch_lightning/plugins/precision/native_amp.py
@@ -65,7 +65,7 @@ class NativeMixedPrecisionPlugin(MixedPrecisionPlugin):
 
     @property
     def amp_backend(self) -> Optional[LightningEnum]:
-        return backend
+        return self.backend
 
     def pre_backward(self, model: "pl.LightningModule", closure_loss: torch.Tensor) -> torch.Tensor:
         if self.scaler is not None:
