4d046c6827fa0b5c6af96b696d6ae6a20ae41b51
tchaton <thomas@grid.ai>
2021-11-24 19:50:10 +0000

    update

--- tests/trainer/connectors/test_signal_connector.py
+++ tests/trainer/connectors/test_signal_connector.py
@@ -28,7 +28,7 @@ from tests.helpers.runif import RunIf
 
 
 @pytest.mark.parametrize("register_handler", [False, True])
-@pytest.mark.parametrize("terminate_gracefully", [True])
+@pytest.mark.parametrize("terminate_gracefully", [False, True])
 @RunIf(skip_windows=True)
 def test_fault_tolerant_sig_handler(register_handler, terminate_gracefully, tmpdir):
 
