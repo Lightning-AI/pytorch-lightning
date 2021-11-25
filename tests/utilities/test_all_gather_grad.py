91800e66309df5e37476dc2bd83dc5cbd68ee990
puhuk <wetr235@gmail.com>
2021-11-25 23:52:03 +0900

    Updated

--- tests/utilities/test_all_gather_grad.py
+++ tests/utilities/test_all_gather_grad.py
@@ -6,7 +6,7 @@ import torch
 
 from pytorch_lightning import seed_everything, Trainer
 
-# from pytorch_lightning.utilities import AllGatherGrad
+from pytorch_lightning.utilities import AllGatherGrad
 from tests.helpers.boring_model import BoringModel
 from tests.helpers.runif import RunIf
 
@@ -18,6 +18,33 @@ def setup_ddp(rank, world_size):
 
     if torch.distributed.is_available() and sys.platform not in ("win32", "cygwin"):
         torch.distributed.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def _test_all_gather_ddp(rank, world_size):
+
+    '''
+        _test_all_gather_ddp will be removed as deprecating AllGatherGrad.
+    '''
+
+    setup_ddp(rank, world_size)
+
+    tensor1 = torch.ones(8, requires_grad=True)
+    tensor2 = torch.ones((8, 16, 32), requires_grad=True)
+
+    tensor1_gathered = AllGatherGrad.apply(tensor1)
+    tensor2_gathered = AllGatherGrad.apply(tensor2)
+
+    tensor1_gathered = tensor1_gathered * rank
+    tensor2_gathered = tensor2_gathered * rank
+
+    tensor1_gathered.sum().backward()
+    tensor2_gathered.sum().backward()
+
+    grad1 = torch.zeros_like(tensor1.grad).fill_(torch.arange(world_size).sum().float())
+    grad2 = torch.zeros_like(tensor2.grad).fill_(torch.arange(world_size).sum().float())
+
+    assert torch.allclose(grad1, tensor1.grad)
+    assert torch.allclose(grad2, tensor2.grad)
 
 
 @RunIf(min_gpus=2, skip_windows=True, special=True)
