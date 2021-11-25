31b4d3f6830070092d477519f40e10c002a189fc
Rajath Bharadwaj <rajathrajath940@gmail.com>
2021-11-25 15:33:15 +0530

    changed to rank_zero_warn

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -444,11 +444,13 @@ class Trainer(
         self.signal_connector = SignalConnector(self)
         self.tuner = Tuner(self)
 
-        # UserWarning for max_epochs if not set
-
+        #rank_zero_warning of type UserWarning for max_epochs if not set
         if self.max_epochs is None and max_steps == -1 and max_time is None:
-            warnings.warn("`max_epochs` is not set. By default it will run for 1000 epochs.")
-
+            rank_zero_warn(
+                "`max_epochs` is not set. By default it will run for 1000 epochs.",
+                UserWarning,
+            )
+            
         fit_loop = FitLoop(
             min_epochs=(1 if (min_epochs is None and min_steps is None and max_time is None) else min_epochs),
             max_epochs=(
