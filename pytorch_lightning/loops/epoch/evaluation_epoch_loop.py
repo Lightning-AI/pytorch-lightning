a5fe96dc75c62eb8d1af520039b680c0669bf995
Danielle Pintz <daniellepintz@gmail.com>
2021-11-25 13:18:45 -0800

    fix

--- pytorch_lightning/loops/epoch/evaluation_epoch_loop.py
+++ pytorch_lightning/loops/epoch/evaluation_epoch_loop.py
@@ -209,10 +209,10 @@ class EvaluationEpochLoop(Loop):
 
         if self.trainer.testing:
             self.trainer.lightning_module._current_fx_name = "test_step"
-            self.trainer._call_accelerator_hook("test_step", step_kwargs)
+            output = self.trainer._call_accelerator_hook("test_step", step_kwargs)
         else:
             self.trainer.lightning_module._current_fx_name = "validation_step"
-            self.trainer._call_accelerator_hook("validation_step", step_kwargs)
+            output = self.trainer._call_accelerator_hook("validation_step", step_kwargs)
 
         return output
 
