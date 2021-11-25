80876668ec8784687e39cc44f55c72023d94a8d5
tchaton <thomas@grid.ai>
2021-11-25 19:30:53 +0000

    update

--- pl_examples/run_examples.sh
+++ pl_examples/run_examples.sh
@@ -35,3 +35,6 @@ args="
 
 python "${dir_path}/basic_examples/mnist_examples/image_classifier_4_lightning_module.py" ${args} "$@"
 python "${dir_path}/basic_examples/mnist_examples/image_classifier_5_lightning_datamodule.py" ${args} "$@"
+
+args="--should_fail 0"
+python "${dir_path}/fault_tolerant/automatic.py" ${args}
