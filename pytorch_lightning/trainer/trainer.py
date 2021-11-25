aef58f83779803f2bec6045152fd74e943fc0b06
tchaton <thomas@grid.ai>
2021-11-25 16:40:54 +0000

    update

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -2096,12 +2096,17 @@ class Trainer(
     def _exit_gracefully_on_signal(self) -> None:
         if _fault_tolerant_training():
             # the signal should be sent to rank 0
-            should_terminate_gracefully = self.training_type_plugin.broadcast(self._terminate_gracefully)
-            if not should_terminate_gracefully:
+            if not self._has_terminated_gracefully():
                 return
             caller = inspect.stack()[1]
             class_name = caller[0].f_locals["self"].__class__.__name__
             raise ExitGracefullyException(f"Exiting gracefully on {class_name}:{caller.function}")
+
+    def _has_terminated_gracefully(self) -> bool:
+        value = torch.tensor(
+            self._terminate_gracefully, device=self.training_type_plugin.root_device, dtype=torch.uint8
+        )
+        return self.training_type_plugin.reduce(value, reduce_op="sum") > 0
 
     @property
     def weights_summary(self) -> Optional[str]:
