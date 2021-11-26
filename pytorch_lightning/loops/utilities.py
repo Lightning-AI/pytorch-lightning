4421b5a9dcc8fa9da4c28568e69777dda5da4290
Rajath Bharadwaj <rajathrajath940@gmail.com>
2021-11-26 19:19:29 +0530

    added typing
    added typing to utility function

--- pytorch_lightning/loops/utilities.py
+++ pytorch_lightning/loops/utilities.py
@@ -14,15 +14,15 @@
 from collections import OrderedDict
 from contextlib import contextmanager
 from functools import lru_cache
-from typing import Any, Dict, Generator, Iterator, List, Optional, Sequence, Tuple
-
+from typing import Any, Dict, Generator, Iterator, List, Optional, Sequence, Tuple, Union
+from datetime import timedelta
+from pytorch_lightning.utilities import rank_zero_warn
 import numpy as np
 import torch
 from torch.optim import Optimizer
 
 import pytorch_lightning as pl
 from pytorch_lightning.plugins import ParallelPlugin
-from pytorch_lightning.utilities import rank_zero_warn
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.fetching import AbstractDataFetcher, DataLoaderIterDataFetcher
 from pytorch_lightning.utilities.memory import recursive_detach
@@ -62,17 +62,17 @@ def _extract_hiddens(training_step_output: STEP_OUTPUT, truncated_bptt_steps: in
     hiddens = recursive_detach(training_step_output["hiddens"])
     return hiddens
 
-
 # rank_zero_warning of type UserWarning for max_epochs if not set
-def _parse_max_epochs_and_steps(max_steps, max_epochs, max_time, min_steps, min_epochs):
+def _parse_max_epochs_and_steps(max_steps: int, max_epochs: int, max_time: Union[str, timedelta, Dict[str, int]], min_steps: int, min_epochs: int) -> Tuple[int, int]:
     if max_epochs is None and max_steps == -1 and max_time is None:
         rank_zero_warn(
             "`max_epochs` is not set. By default it will run for 1000 epochs.",
             UserWarning,
         )
-    min_epochs = 1 if (min_epochs is None and min_steps is None and max_time is None) else min_epochs
+    min_epochs = (1 if (min_epochs is None and min_steps is None and max_time is None) else min_epochs)
     max_epochs = max_epochs if max_epochs is not None else (1000 if (max_steps == -1 and max_time is None) else -1)
     return min_epochs, max_epochs
+
 
 
 def _build_training_step_kwargs(
@@ -181,6 +181,7 @@ def _get_active_optimizers(
     # find optimizer index by looking for the first {item > current_place} in the cumsum list
     opt_idx = np.searchsorted(freq_cumsum, current_place_in_loop, side="right")
     return [(opt_idx, optimizers[opt_idx])]
+
 
 
 def _is_max_limit_reached(current: int, maximum: int = -1) -> bool:
