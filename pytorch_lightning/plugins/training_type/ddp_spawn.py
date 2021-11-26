12dc0ead6f5e32e820ce31929ea0e5fd6c092128
tchaton <thomas@grid.ai>
2021-11-26 16:24:56 +0000

    update

--- pytorch_lightning/plugins/training_type/ddp_spawn.py
+++ pytorch_lightning/plugins/training_type/ddp_spawn.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import gc
 import logging
 import os
 import re
@@ -431,4 +430,3 @@ class DDPSpawnPlugin(ParallelPlugin):
                 )
                 del logger._experiment
                 logger._experiment = None
-                gc.collect()
