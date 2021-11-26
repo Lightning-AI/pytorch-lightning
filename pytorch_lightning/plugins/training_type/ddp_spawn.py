cd1f316a472844cc3237405af58730b0b208ec9c
tchaton <thomas@grid.ai>
2021-11-26 13:54:43 +0000

    update

--- pytorch_lightning/plugins/training_type/ddp_spawn.py
+++ pytorch_lightning/plugins/training_type/ddp_spawn.py
@@ -172,6 +172,8 @@ class DDPSpawnPlugin(ParallelPlugin):
         Return:
             The output of the function of process 0.
         """
+        if isinstance(args[0].logger, pl.loggers.TensorBoardLogger):
+            args[0].logger.finalize("")
         os.environ["MASTER_PORT"] = str(self.cluster_environment.main_port)
         context = mp.get_context("spawn")
         return_queue = context.SimpleQueue() if return_result else None
