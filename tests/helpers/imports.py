6b814fabc975394b33b4fde79896a365e324eef3
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-30 01:13:25 +0100

    Avoid required torchtext import

--- tests/helpers/imports.py
+++ tests/helpers/imports.py
@@ -1,6 +1,14 @@
-from pytorch_lightning.utilities.imports import _TORCH_GREATER_EQUAL_1_8
+from pytorch_lightning.utilities.imports import _TORCH_GREATER_EQUAL_1_8, _TORCHTEXT_AVAILABLE
 
-if _TORCH_GREATER_EQUAL_1_8:
-    from torchtext.legacy.data import Batch, Dataset, Example, Field, Iterator, LabelField
+if _TORCHTEXT_AVAILABLE:
+    if _TORCH_GREATER_EQUAL_1_8:
+        from torchtext.legacy.data import Batch, Dataset, Example, Field, Iterator, LabelField
+    else:
+        from torchtext.data import Batch, Dataset, Example, Field, Iterator, LabelField
 else:
-    from torchtext.data import Batch, Dataset, Example, Field, Iterator, LabelField  # noqa: F401
+    Batch = type(None)
+    Dataset = type(None)
+    Example = type(None)
+    Field = type(None)
+    Iterator = type(None)
+    LabelField = type(None)
