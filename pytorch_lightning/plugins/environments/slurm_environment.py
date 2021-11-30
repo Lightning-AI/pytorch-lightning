af0c0d8507ba105522a0abe835e73f9973e9b6e4
AndresAlgaba <andresalgaba@gmail.com>
2021-11-30 21:24:02 +0100

    change order

--- pytorch_lightning/plugins/environments/slurm_environment.py
+++ pytorch_lightning/plugins/environments/slurm_environment.py
@@ -91,18 +91,22 @@ class SLURMEnvironment(ClusterEnvironment):
 
     @staticmethod
     def job_id() -> Optional[int]:
-        job_id = os.environ.get("SLURM_JOB_ID")
-
-        if job_id:
-            try:
-                job_id = int(job_id)
-            except ValueError:
-                job_id = None
 
         # in interactive mode, don't make logs use the same job id
         in_slurm_interactive_mode = SLURMEnvironment.job_name() == "bash"
         if in_slurm_interactive_mode:
+            return None
+
+        job_id = os.environ.get("SLURM_JOB_ID")
+
+        if job_id is None:
+            return None
+
+        try:
+            job_id = int(job_id)
+        except ValueError:
             job_id = None
+
         return job_id
 
     def world_size(self) -> int:
