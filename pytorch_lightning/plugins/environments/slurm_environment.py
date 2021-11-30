5e0b9bd5707bf256ad3426cb4f3312b8a8534920
AndresAlgaba <andresalgaba@gmail.com>
2021-11-30 20:10:33 +0100

    reverse if job_id

--- pytorch_lightning/plugins/environments/slurm_environment.py
+++ pytorch_lightning/plugins/environments/slurm_environment.py
@@ -92,10 +92,12 @@ class SLURMEnvironment(ClusterEnvironment):
     @staticmethod
     def job_id() -> Optional[int]:
         job_id = os.environ.get("SLURM_JOB_ID")
-        try:
-            job_id = int(job_id)
-        except ValueError:
-            job_id = None
+
+        if job_id:
+            try:
+                job_id = int(job_id)
+            except ValueError:
+                job_id = None
 
         # in interactive mode, don't make logs use the same job id
         in_slurm_interactive_mode = SLURMEnvironment.job_name() == "bash"
