6e49f996ec0b4f4b7bb5fc2782fc940634a2aa7e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-12-02 04:20:40 +0100

    reduce diff to prev. version

--- pytorch_lightning/loops/epoch/prediction_epoch_loop.py
+++ pytorch_lightning/loops/epoch/prediction_epoch_loop.py
@@ -121,9 +121,11 @@ class PredictionEpochLoop(Loop):
         """
         # configure step_kwargs
         step_kwargs = self._build_kwargs(batch, batch_idx, dataloader_idx)
-        model_ref = self.trainer.lightning_module
 
+        # extract batch_indices and store them
         self.current_batch_indices = self._seen_batch_indices[batch_idx] if self._seen_batch_indices else []
+
+        model_ref = self.trainer.lightning_module
 
         self.trainer.call_hook("on_predict_batch_start", batch, batch_idx, dataloader_idx)
 
