a61854d6c5b799d8eab2a762ae23c2d39d284cbc
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-12-06 14:46:14 +0100

    call post dispatch

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -1226,6 +1226,7 @@ class Trainer(
     def _post_dispatch(self):
         # these `teardown` calls are here instead of in `_call_teardown_hook` since they are internal teardowns
         # which need to happen before.
+        self.training_type_plugin.post_dispatch(self)
         self.accelerator.teardown()
         self._data_connector.teardown()
         self._active_loop.teardown()
