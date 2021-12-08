c30bc32aaa065142974444aab144620ab1b6dd76
four4fish <88516121+four4fish@users.noreply.github.com>
2021-12-08 10:36:40 -0800

    Update sharded_spawn.py

--- pytorch_lightning/plugins/training_type/sharded_spawn.py
+++ pytorch_lightning/plugins/training_type/sharded_spawn.py
@@ -19,7 +19,6 @@ from torch.nn import Module
 from torch.optim import Optimizer
 
 import pytorch_lightning as pl
-from pytorch_lightning.plugins.precision.sharded_native_amp import ShardedNativeMixedPrecisionPlugin
 from pytorch_lightning.plugins.training_type.ddp_spawn import DDPSpawnPlugin
 from pytorch_lightning.trainer.states import TrainerFn
 from pytorch_lightning.utilities import _FAIRSCALE_AVAILABLE, rank_zero_only
@@ -29,7 +28,6 @@ from pytorch_lightning.utilities.exceptions import MisconfigurationException
 if _FAIRSCALE_AVAILABLE:
     from fairscale.nn.data_parallel.sharded_ddp import ShardedDataParallel
     from fairscale.optim import OSS
-    from fairscale.optim.grad_scaler import ShardedGradScaler
 
     from pytorch_lightning.overrides.fairscale import LightningShardedDataParallel, unwrap_lightning_module_sharded
 
