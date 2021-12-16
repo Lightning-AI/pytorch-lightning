448b5244a89cc2324be6bf08e059b34d1f59d1aa
four4fish <88516121+four4fish@users.noreply.github.com>
2021-12-15 18:38:21 -0800

    Apply suggestions from code review
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -451,7 +451,7 @@ class Trainer(
             amp_level,
             plugins,
         )
-        self.logger_connector: LoggerConnector = LoggerConnector(self, log_gpu_memory)
+        self.logger_connector = LoggerConnector(self, log_gpu_memory)
         self._callback_connector = CallbackConnector(self)
         self.checkpoint_connector = CheckpointConnector(self, resume_from_checkpoint)
         self.signal_connector = SignalConnector(self)
