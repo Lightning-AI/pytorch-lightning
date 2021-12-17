51c2888983c857e4fef7ddb7b927f5e31eabba6a
quancs <1017241746@qq.com>
2021-12-17 15:13:55 +0800

    Update pytorch_lightning/callbacks/progress/rich_progress.py

--- pytorch_lightning/callbacks/progress/rich_progress.py
+++ pytorch_lightning/callbacks/progress/rich_progress.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import copy
 import math
 from dataclasses import dataclass
 from datetime import timedelta
