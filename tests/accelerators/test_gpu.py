a3e2ef2be05b8275cd9a73f748a893b0c3bede63
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-12-18 23:38:40 +0100

    Refactor plugin tests whose assertions don't need to run in `on_fit_start` hook (#11149)

--- tests/accelerators/test_gpu.py
+++ tests/accelerators/test_gpu.py
@@ -1,6 +1,23 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest import mock
+
 import torch
 
+from pytorch_lightning import Trainer
 from pytorch_lightning.accelerators import GPUAccelerator
+from tests.helpers import BoringModel
 from tests.helpers.runif import RunIf
 
 
@@ -26,3 +43,20 @@ def test_get_nvidia_gpu_stats(tmpdir):
 
     for f in fields:
         assert any(f in h for h in gpu_stats.keys())
+
+
+@RunIf(min_gpus=1)
+@mock.patch("torch.cuda.set_device")
+def test_set_cuda_device(set_device_mock, tmpdir):
+    model = BoringModel()
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        fast_dev_run=True,
+        accelerator="gpu",
+        devices=1,
+        enable_checkpointing=False,
+        enable_model_summary=False,
+        enable_progress_bar=False,
+    )
+    trainer.fit(model)
+    set_device_mock.assert_called_once()
