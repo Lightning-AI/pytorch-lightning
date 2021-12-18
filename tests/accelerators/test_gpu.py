1fcf150ee21d5c30cd1fabf9312ed62944ece25c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-12-18 02:45:40 +0100

    add test for set_device

--- tests/accelerators/test_gpu.py
+++ tests/accelerators/test_gpu.py
@@ -1,6 +1,10 @@
+from unittest import mock
+
 import torch
 
+from pytorch_lightning import Trainer
 from pytorch_lightning.accelerators import GPUAccelerator
+from tests.helpers import BoringModel
 from tests.helpers.runif import RunIf
 
 
@@ -26,3 +30,12 @@ def test_get_nvidia_gpu_stats(tmpdir):
 
     for f in fields:
         assert any(f in h for h in gpu_stats.keys())
+
+
+@RunIf(gpus=1)
+@mock.patch("torch.cuda.set_device")
+def test_set_cuda_device(set_device_mock, tmpdir):
+    model = BoringModel()
+    trainer = Trainer(default_root_dir=tmpdir, fast_dev_run=True, accelerator="gpu", devices=1, enable_checkpointing=False, enable_model_summary=False, enable_progress_bar=False)
+    trainer.fit(model)
+    set_device_mock.assert_called_once()
