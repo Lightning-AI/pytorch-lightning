b442255a293fa4b6cc192f8388d3875bc285c2c5
Kaushik B <kaushikbokka@gmail.com>
2021-12-20 17:39:13 +0530

    Collect and run all ipu tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -208,3 +208,11 @@ def pytest_collection_modifyitems(items):
             # has `@RunIf(slow=True)`
             if marker.name == "skipif" and marker.kwargs.get("slow")
         ]
+    elif os.getenv("PL_RUN_IPU_TESTS", "0") == "1":
+        items[:] = [
+            item
+            for item in items
+            for marker in item.own_markers
+            # has `@RunIf(tpu=True)`
+            if marker.name == "skipif" and marker.kwargs.get("ipu")
+        ]
