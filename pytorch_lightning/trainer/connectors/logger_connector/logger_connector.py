285de043c2cefe7ff0614a8320da9b53f7a4e4e3
rohitgr7 <rohitgr1998@gmail.com>
2021-12-28 02:36:02 +0530

    try fix mypy

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Dict, Iterable, Optional, Union
+from typing import Any, cast, Dict, Iterable, Optional, Union
 
 import torch
 
@@ -102,8 +102,8 @@ class LoggerConnector:
         if self.trainer.logger is None or not metrics:
             return
 
-        if step is None:
-            step = metrics.pop("step", None)
+        if step is None and "step" in metrics:
+            step = cast(int, metrics.pop("step"))
 
         # this is done prior to make sure logged_metrics always contain tensors
         self._logged_metrics.update(metrics)
