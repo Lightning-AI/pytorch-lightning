a34a47c3d47aa8933060cd6fb64528d3aa24e3fd
Carlos Mocholi <carlossmocholi@gmail.com>
2022-01-13 16:11:35 +0100

    Fix error

--- pytorch_lightning/strategies/strategy.py
+++ pytorch_lightning/strategies/strategy.py
@@ -52,7 +52,7 @@ class Strategy(ABC):
         self.checkpoint_io = checkpoint_io
         self.precision_plugin = precision_plugin
         self.optimizers: List[Optimizer] = []
-        # FIXME: rename to _config?
+        # TODO: rename to `lr_scheduler_configs` to match the property in the `Trainer`
         self.lr_schedulers: List[LRSchedulerConfig] = []
         self.optimizer_frequencies: List[int] = []
         if is_overridden("post_dispatch", self, parent=Strategy):
