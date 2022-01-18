3815fc7f2e3056ebda90bc61c83617a2ccecc32a
ritaw <wangraying@gmail.com>
2022-01-18 22:03:20 +0800

    add test

--- tests/strategies/test_bagua_strategy.py
+++ tests/strategies/test_bagua_strategy.py
@@ -11,9 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import pytest
 import torch
 
 from pytorch_lightning import Trainer
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.strategies import BaguaStrategy
 from tests.helpers.boring_model import BoringModel
 from tests.helpers.runif import RunIf
@@ -40,3 +42,10 @@ def test_bagua_algorithm(tmpdir):
 
     for param in model.parameters():
         assert torch.norm(param) < 3
+
+
+@RunIf(bagua=False, min_gpus=1)
+def test_bagua_not_available():
+
+    with pytest.raises(MisconfigurationException, match="you must have `Bagua` installed"):
+        trainer = Trainer(strategy="bagua", gpus=1)
