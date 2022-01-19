03e2d01f72807fd2e8001c0f8ccc30ebcd6c2339
Kyle <s.t.o.r.m.detective@gmail.com>
2022-01-19 00:19:02 -0500

    flake8

--- tests/tuner/test_auto_gpu_select.py
+++ tests/tuner/test_auto_gpu_select.py
@@ -16,7 +16,6 @@ import re
 import pytest
 import torch
 
-from pytorch_lightning import Trainer
 from pytorch_lightning.tuner.auto_gpu_select import pick_multiple_gpus
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from tests.helpers.runif import RunIf
