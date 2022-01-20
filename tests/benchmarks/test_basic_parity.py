df3d25f9efa8ff59d17cd9980cc51fb7c96fa549
Akihiro Nitta <nitta@akihironitta.com>
2022-01-20 11:37:44 +0000

    Undo unrelated change

--- tests/benchmarks/test_basic_parity.py
+++ tests/benchmarks/test_basic_parity.py
@@ -79,7 +79,7 @@ def test_pytorch_parity(
 def _hook_memory():
     if torch.cuda.is_available():
         torch.cuda.synchronize()
-        used_memory = torch.cuda.reset_peak_memory_stats()
+        used_memory = torch.cuda.max_memory_allocated()
     else:
         used_memory = np.nan
     return used_memory
