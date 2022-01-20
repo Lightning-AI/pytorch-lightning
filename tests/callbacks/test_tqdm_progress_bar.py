39453a4a3d85ddfa55d3292cfa978868d3725731
Danielle Pintz <daniellepintz@gmail.com>
2022-01-20 12:18:37 -0800

    remove standalone

--- tests/callbacks/test_tqdm_progress_bar.py
+++ tests/callbacks/test_tqdm_progress_bar.py
@@ -619,7 +619,7 @@ def test_tqdm_progress_bar_correct_value_epoch_end(tmpdir):
     trainer.test(model, verbose=False)
     assert pbar.calls["test"] == []
 
-@RunIf(standalone=True)
+
 @mock.patch("pytorch_lightning.trainer.trainer.Trainer.is_global_zero", new_callable=PropertyMock, return_value=False)
 def test_tqdm_progress_bar_disabled_when_not_rank_zero(is_global_zero, tmpdir):
     """Test that the progress bar is disabled when not in global rank zero."""
