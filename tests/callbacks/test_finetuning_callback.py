5952bb6382026c0e29cb03e527bebb768914dd6a
krishnakalyan3 <krishnakalyan3@gmail.com>
2022-01-23 23:28:58 +0100

    remove type from testing

--- tests/callbacks/test_finetuning_callback.py
+++ tests/callbacks/test_finetuning_callback.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from collections import OrderedDict
-from typing import Optional
 
 import pytest
 import torch
@@ -269,7 +268,7 @@ def test_on_before_accelerator_backend_setup(tmpdir):
     configure_optimizers function call."""
 
     class TestCallback(Callback):
-        def setup(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", stage: Optional[str] = None) -> None:
+        def setup(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
             pl_module.setup_called = True
 
     class TestModel(BoringModel):
