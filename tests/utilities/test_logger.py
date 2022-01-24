04c283f05a90f5fe4891eb7ec8f859852f9d2389
Akash Kwatra <akashkw@gmail.com>
2022-01-24 15:04:46 -0800

    flatten_dict unit test

--- tests/utilities/test_logger.py
+++ tests/utilities/test_logger.py
@@ -24,6 +24,41 @@ from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.logger import _convert_params, _flatten_dict, _sanitize_callable_params, _add_prefix
 from tests.helpers import BoringModel
 
+def test_flatten_dict():
+    """Validate flatten_dict can handle nested dictionaries and argparse Namespace"""
+
+    # Test basic dict flattening with custom delimiter
+    params = {"a": {"b" : "c"}}
+    params = _flatten_dict(params, "--")
+
+    assert("a" not in params)
+    assert(params["a--b"] == "c")
+
+    # Test complex nested dict flattening
+    params = {"a": {5: {"foo": "bar"}}, "b": 6, "c": {7: [1,2,3,4], 8: "foo", 9: {10: "bar"}}}
+    params = _flatten_dict(params)
+
+    assert("a" not in params)
+    assert(params["a/5/foo"] == "bar")
+    assert(params["b"] == 6)
+    assert(params["c/7"] == [1,2,3,4])
+    assert(params["c/8"] == "foo")
+    assert(params["c/9/10"] == "bar")
+
+    # Test flattening of argparse Namespace
+    opt = "--max_epochs 1".split(" ")
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parent_parser=parser)
+    params = parser.parse_args(opt)
+    wrapping_dict = {"params": params}
+    params = _flatten_dict(wrapping_dict)
+
+    assert(type(params) == dict)
+    assert(params["params/logger"] == True)
+    assert(params["params/gpus"] == "None")
+    assert("logger" not in params)
+    assert("gpus" not in params)
+
 
 def test_sanitize_callable_params():
     """Callback function are not serializiable.
@@ -55,7 +90,7 @@ def test_sanitize_callable_params():
     assert params["wrapper_something_wo_name"] == "<lambda>"
 
 def test_add_prefix():
-    """ Make sure add_prefix modifies the dict keys correctly."""
+    """Verify add_prefix modifies the dict keys correctly."""
 
     metrics = {"metric1" : 1, "metric2" : 2}
     metrics = _add_prefix(metrics, "prefix", "-")
