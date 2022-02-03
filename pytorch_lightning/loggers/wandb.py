b730af4ae352c008af074e9e236513385f582bae
NathanGodey <38216711+NathanGodey@users.noreply.github.com>
2022-02-03 11:03:13 +0100

    make step an actual argument

--- pytorch_lightning/loggers/wandb.py
+++ pytorch_lightning/loggers/wandb.py
@@ -406,14 +406,13 @@ class WandbLogger(LightningLoggerBase):
         self.log_table(key, columns, data, dataframe, step)
 
     @rank_zero_only
-    def log_image(self, key: str, images: List[Any], **kwargs: str) -> None:
+    def log_image(self, key: str, images: List[Any], step: Optional[int]=None, **kwargs: str) -> None:
         """Log images (tensors, numpy arrays, PIL Images or file paths).
 
         Optional kwargs are lists passed to each image (ex: caption, masks, boxes).
         """
         if not isinstance(images, list):
             raise TypeError(f'Expected a list as "images", found {type(images)}')
-        step = kwargs.pop("step", None)
         n = len(images)
         for k, v in kwargs.items():
             if len(v) != n:
