955a8547d71e06eee868389f3afddd9e039f6ada
ananthsub <ananth.subramaniam@gmail.com>
2022-02-03 23:40:39 -0800

    Apply suggestions from code review

--- pytorch_lightning/utilities/distributed.py
+++ pytorch_lightning/utilities/distributed.py
@@ -374,13 +374,13 @@ def init_dist_connection(
     if not torch.distributed.is_available():
         raise RuntimeError("torch.distributed is not available. Cannot initialize distributed process group")
     if torch.distributed.is_initialized():
-        log.debug("torch distributed is already initialized. exiting early")
+        log.debug("torch.distributed is already initialized. Exiting early")
         return
     global_rank = global_rank if global_rank is not None else cluster_environment.global_rank()
     world_size = world_size if world_size is not None else cluster_environment.world_size()
     os.environ["MASTER_ADDR"] = cluster_environment.main_address
     os.environ["MASTER_PORT"] = str(cluster_environment.main_port)
-    log.info(f"initializing distributed: GLOBAL_RANK: {global_rank}, MEMBER: {global_rank + 1}/{world_size}")
+    log.info(f"Initializing distributed: GLOBAL_RANK: {global_rank}, MEMBER: {global_rank + 1}/{world_size}")
     torch.distributed.init_process_group(torch_distributed_backend, rank=global_rank, world_size=world_size, **kwargs)
 
     # on rank=0 let everyone know training is starting
