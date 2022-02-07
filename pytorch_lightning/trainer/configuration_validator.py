88ce0425c2046c1d7021417fb2c1c1cc435ff643
krishnakalyan3 <krishnakalyan3@gmail.com>
2022-02-07 09:49:12 +0100

    update validator

--- pytorch_lightning/trainer/configuration_validator.py
+++ pytorch_lightning/trainer/configuration_validator.py
@@ -61,6 +61,8 @@ def verify_loop_configurations(trainer: "pl.Trainer") -> None:
     _check_on_batch_start_end(trainer, model)
     # TODO: Remove this in v1.8
     _check_on_configure_sharded_model(trainer)
+    # TODO: Remove this in v1.8
+    _check_on_before_accelerator_backend_setup(trainer, model)
 
 
 def __verify_train_val_loop_configuration(trainer: "pl.Trainer", model: "pl.LightningModule") -> None:
@@ -346,5 +348,13 @@ def _check_on_configure_sharded_model(trainer: "pl.Trainer") -> None:
         if is_overridden(method_name="on_configure_sharded_model", instance=callback):
             rank_zero_deprecation(
                 "The `on_configure_sharded_model` callback hook was deprecated in"
+                " v1.6 and will be removed in v1.8. Use `setup()` instead."
+            )
+
+def _check_on_before_accelerator_backend_setup(trainer: "pl.Trainer", model: "pl.LightningModule") -> None:
+    for callback in trainer.callbacks:
+        if is_overridden(method_name="on_before_accelerator_backend_setup", instance=callback):
+            rank_zero_deprecation(
+                "The `on_before_accelerator_backend_setup` callback hook was deprecated in"
                 " v1.6 and will be removed in v1.8. Use `setup()` instead."
             )
