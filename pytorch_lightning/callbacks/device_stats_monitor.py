efd5d3bfe6e765675d980c1ce7f52e2f5b00f549
Eric Wiener <ewiener@zoox.com>
2022-02-07 10:06:36 -0500

    Add cpu metrics to device stats monitor

--- pytorch_lightning/callbacks/device_stats_monitor.py
+++ pytorch_lightning/callbacks/device_stats_monitor.py
@@ -21,6 +21,7 @@ Monitors and logs device stats during training.
 from typing import Any, Dict, Optional
 
 import pytorch_lightning as pl
+from pytorch_lightning.accelerators.cpu import get_cpu_process_metrics
 from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.types import STEP_OUTPUT
@@ -31,6 +32,9 @@ class DeviceStatsMonitor(Callback):
     r"""
     Automatically monitors and logs device stats during training stage. ``DeviceStatsMonitor``
     is a special callback as it requires a ``logger`` to passed as argument to the ``Trainer``.
+
+    Args:
+        cpu_stats: if True, will also record CPU stats in addition to accelerator stats (default: True).
 
     Raises:
         MisconfigurationException:
@@ -43,17 +47,26 @@ class DeviceStatsMonitor(Callback):
         >>> trainer = Trainer(callbacks=[device_stats]) # doctest: +SKIP
     """
 
-    def setup(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", stage: Optional[str] = None) -> None:
+    def __init__(self, cpu_stats: bool = True) -> None:
+        super().__init__()
+        self.cpu_stats = cpu_stats
+
+    def setup(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        stage: Optional[str] = None,
+    ) -> None:
         if not trainer.loggers:
             raise MisconfigurationException("Cannot use DeviceStatsMonitor callback with Trainer that has no logger.")
 
-    def on_train_batch_start(
+    def _on_train_batch_common(
         self,
         trainer: "pl.Trainer",
         pl_module: "pl.LightningModule",
         batch: Any,
         batch_idx: int,
-        unused: int = 0,
+        key: str,
     ) -> None:
         if not trainer.loggers:
             raise MisconfigurationException("Cannot use `DeviceStatsMonitor` callback with `Trainer(logger=False)`.")
@@ -63,10 +76,24 @@ class DeviceStatsMonitor(Callback):
 
         device = trainer.strategy.root_device
         device_stats = trainer.accelerator.get_device_stats(device)
+
+        if self.cpu_stats:
+            device_stats.update(get_cpu_process_metrics())
+
         for logger in trainer.loggers:
             separator = logger.group_separator
-            prefixed_device_stats = _prefix_metric_keys(device_stats, "on_train_batch_start", separator)
-            logger.log_metrics(prefixed_device_stats, step=trainer.fit_loop.epoch_loop._batches_that_stepped)
+            prefixed_device_stats = _prefix_metric_keys(device_stats, key, separator)
+            logger.log_metrics(prefixed_device_stats, step=trainer.global_step)
+
+    def on_train_batch_start(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        batch: Any,
+        batch_idx: int,
+        unused: Optional[int] = 0,
+    ) -> None:
+        self._on_train_batch_common(trainer, pl_module, batch, batch_idx, "on_train_batch_start")
 
     def on_train_batch_end(
         self,
@@ -77,18 +104,7 @@ class DeviceStatsMonitor(Callback):
         batch_idx: int,
         unused: int = 0,
     ) -> None:
-        if not trainer.loggers:
-            raise MisconfigurationException("Cannot use `DeviceStatsMonitor` callback with `Trainer(logger=False)`.")
-
-        if not trainer._logger_connector.should_update_logs:
-            return
-
-        device = trainer.strategy.root_device
-        device_stats = trainer.accelerator.get_device_stats(device)
-        for logger in trainer.loggers:
-            separator = logger.group_separator
-            prefixed_device_stats = _prefix_metric_keys(device_stats, "on_train_batch_end", separator)
-            logger.log_metrics(prefixed_device_stats, step=trainer.fit_loop.epoch_loop._batches_that_stepped)
+        self._on_train_batch_common(trainer, pl_module, batch, batch_idx, "on_train_batch_end")
 
 
 def _prefix_metric_keys(metrics_dict: Dict[str, float], prefix: str, separator: str) -> Dict[str, float]:
