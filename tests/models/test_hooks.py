6f3b41740c65e6d3736951133870f3b05ca5d143
Akihiro Nitta <akihiro@pytorchlightning.ai>
2022-02-07 00:02:20 -0500

    Add FIXME

--- tests/models/test_hooks.py
+++ tests/models/test_hooks.py
@@ -434,6 +434,11 @@ class HookedModel(BoringModel):
         return out
 
 
+"""
+FIXME: not sure why this case sometimes raises the warning.
+tests/models/test_hooks.py::test_trainer_model_hook_system_fit[True-kwargs1]
+"""
+
 @pytest.mark.parametrize(
     "kwargs",
     [
