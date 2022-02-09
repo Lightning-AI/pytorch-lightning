ef6757e11fa1b976402ed4479729fa96f35fbab2
Jirka <jirka.borovec@seznam.cz>
2022-02-09 17:56:39 +0100

    move

--- requirements/_check_avail_strategies.py
+++ requirements/_check_avail_strategies.py
@@ -1,8 +1,9 @@
 import bagua
 import deepspeed
 import fairscale
-import horovod.torch  # noqa: F401
+import horovod.torch
 
 print(fairscale.__version__)
 print(deepspeed.__version__)
 print(bagua.__version__)
+print(horovod.torch.nccl_built())
