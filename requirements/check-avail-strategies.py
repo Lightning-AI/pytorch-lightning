90ff8df364d74071d5d8fbf5a1f6c782e9387b20
Jirka Borovec <Borda@users.noreply.github.com>
2022-02-10 22:42:15 +0100

    prune prints
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- requirements/check-avail-strategies.py
+++ requirements/check-avail-strategies.py
@@ -2,8 +2,3 @@ import bagua
 import deepspeed
 import fairscale
 import horovod.torch
-
-print(fairscale.__version__)
-print(deepspeed.__version__)
-print(bagua.__version__)
-print(horovod.torch.nccl_built())
