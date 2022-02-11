ee1f7496f770f0299fe83637e27f1b117943cb59
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-02-11 03:04:05 +0100

    modify test

--- tests/loops/test_training_loop.py
+++ tests/loops/test_training_loop.py
@@ -58,7 +58,8 @@ def test_outputs_format(tmpdir):
     trainer.fit(model)
 
 
-def test_training_starts_with_seed(tmpdir):
+@pytest.mark.parametrize("seed_once", (True, False))
+def test_training_starts_with_seed(tmpdir, seed_once):
     """Test that the training always starts with the same random state (when using seed_everything)."""
 
     class SeededModel(BoringModel):
@@ -72,14 +73,21 @@ def test_training_starts_with_seed(tmpdir):
 
     def run_training(**trainer_kwargs):
         model = SeededModel()
-        seed_everything(123)
         trainer = Trainer(**trainer_kwargs)
         trainer.fit(model)
         return torch.cat(model.seen_batches)
 
-    sequence0 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=0)
-    sequence1 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=2)
-    assert torch.allclose(sequence0, sequence1)
+    if seed_once:
+        seed_everything(123)
+        sequence0 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=0)
+        sequence1 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=2)
+        assert not torch.allclose(sequence0, sequence1)
+    else:
+        seed_everything(123)
+        sequence0 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=0)
+        seed_everything(123)
+        sequence1 = run_training(default_root_dir=tmpdir, max_steps=2, num_sanity_val_steps=2)
+        assert torch.allclose(sequence0, sequence1)
 
 
 @pytest.mark.parametrize(["max_epochs", "batch_idx_"], [(2, 5), (3, 8), (4, 12)])
