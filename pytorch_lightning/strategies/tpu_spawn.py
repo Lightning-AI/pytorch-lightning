1131bf72524ce3eb05c4d92ce201b9fafa6e6446
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-02-14 04:58:08 +0100

    reset

--- pytorch_lightning/strategies/tpu_spawn.py
+++ pytorch_lightning/strategies/tpu_spawn.py
@@ -142,6 +142,7 @@ class TPUSpawnStrategy(DDPSpawnStrategy):
 
     @property
     def is_distributed(self) -> bool:
+        # HOST_WORLD_SIZE is None outside the xmp.spawn process
         return os.getenv(xenv.HOST_WORLD_SIZE, None) and self.world_size != 1
 
     def process_dataloader(self, dataloader: DataLoader) -> MpDeviceLoader:
