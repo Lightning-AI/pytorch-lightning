856165cabdab32f98f52784692df12f158b5435c
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-02-17 22:40:47 +0100

    refactor trainer laucher argument

--- pytorch_lightning/strategies/launchers/xla_spawn.py
+++ pytorch_lightning/strategies/launchers/xla_spawn.py
@@ -44,7 +44,7 @@ class _XLASpawnLauncher(_SpawnLauncher):
         - It is important that the entry point to the program/script is guarded by ``if __name__ == "__main__"``.
     """
 
-    def launch(self, function: Callable, *args: Any, **kwargs: Any) -> Any:
+    def launch(self, function: Callable, *args: Any, trainer: Optional["pl.Trainer"] = None, **kwargs: Any) -> Any:
         """Spawns processes that run the given function in parallel.
 
         The function is allowed to have a return value. However, when all processes join, only the return value
@@ -53,13 +53,10 @@ class _XLASpawnLauncher(_SpawnLauncher):
         Arguments:
             function: The entry point for all spawned processes.
             *args: Optional positional arguments to be passed to the given function.
+            trainer: Optional reference to the :class:`~pytorch_lightning.trainer.trainer.Trainer` for which
+                a selected set of attributes get restored in the main process after processes join.
             **kwargs: Optional keyword arguments to be passed to the given function.
-                If a keyword argument named `trainer` is present and is an instance of
-                :class:`~pytorch_lightning.trainer.trainer.Trainer`, a selected set of attributes from the trainer get
-                restored in the main process after processes join. The `trainer` keyword argument will NOT be passed
-                into the function.
         """
-        trainer = kwargs.pop("trainer", None)
         context = mp.get_context(self._strategy.start_method or "fork")
         return_queue = context.SimpleQueue()
         xmp.spawn(
