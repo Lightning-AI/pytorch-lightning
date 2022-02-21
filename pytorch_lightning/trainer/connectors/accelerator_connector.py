c83717ef51d5c493a1ac4969a588bb2facc10712
Kaushik B <kaushikbokka@gmail.com>
2022-02-21 18:17:04 +0530

    Fix devices auto

--- pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -469,8 +469,8 @@ class AcceleratorConnector:
             accelerator_class = ACCELERATORS[self._accelerator_flag]
             self.accelerator = accelerator_class()
 
-        self._devices_flag = self.accelerator.parse_devices(self._devices_flag)
         self._set_devices_flag_if_auto_passed()
+        self._devices_flag = self.accelerator.parse_devices(self._devices_flag)
         self._parallel_devices = self.accelerator.get_parallel_devices(self._devices_flag)
 
         self._gpus = self._devices_flag if not self._gpus else self._gpus
