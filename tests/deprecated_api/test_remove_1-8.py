85a45fbdf3c1da32429f686de2f9c0799a109c30
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2022-02-24 13:02:14 +0530

    Update tests/deprecated_api/test_remove_1-8.py

--- tests/deprecated_api/test_remove_1-8.py
+++ tests/deprecated_api/test_remove_1-8.py
@@ -574,7 +574,7 @@ def test_v1_8_0_deprecated_agg_and_log_metrics_override(tmpdir):
     Trainer(logger=[logger2, logger3])
 
 
-def test_v1_8_0_weights_save_path():
+def test_v1_8_0_weights_save_path(tmpdir):
     with pytest.deprecated_call(
         match=r"Setting `Trainer\(weights_save_path=\)` is deprecated in v1.6 and will be removed in v1.8."
     ):
