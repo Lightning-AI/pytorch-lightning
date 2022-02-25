b75cf38d100060d11550124b2837be9d8e6d885d
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2022-02-25 14:59:32 +0530

    Update pytorch_lightning/accelerators/cpu.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pytorch_lightning/accelerators/cpu.py
+++ pytorch_lightning/accelerators/cpu.py
@@ -41,7 +41,7 @@ class CPUAccelerator(Accelerator):
         return {}
 
     @staticmethod
-    def parse_devices(devices: int | str | list[int]) -> int | str | list[int]:
+    def parse_devices(devices: Union[int, str, List[int]]) -> Union[int, str, List[int]]:
         """Accelerator Parsing logic."""
         return devices
 
