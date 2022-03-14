88e9752d9c549a87e6e6ff17443c787d0b6de98b
Kyle <s.t.o.r.m.detective@gmail.com>
2022-03-14 15:36:02 -0400

    devices always returns an int or List[int]

--- tests/models/test_tpu.py
+++ tests/models/test_tpu.py
@@ -306,9 +306,9 @@ def test_broadcast_on_tpu():
         (8, None, False),
         ([1], 1, False),
         ([8], 8, False),
-        ("1,", 1, False),
-        ("1", None, False),
-        ("9, ", 9, True),
+        (1, 1, False),
+        (1, None, False),
+        (9, 9, True),
         ([9], 9, True),
         ([0], 0, True),
         (2, None, True),
@@ -328,7 +328,7 @@ def test_tpu_choice(tmpdir, devices, expected_tpu_id, error_expected):
 
 @pytest.mark.parametrize(
     ["cli_args", "expected"],
-    [("--accelerator=tpu --devices=8", {"devices": 8}), ("--accelerator=tpu --devices=1,", {"devices": "1,"})],
+    [("--accelerator=tpu --devices=8", {"devices": 8}), ("--accelerator=tpu --devices=1,", {"devices": 1})],
 )
 @RunIf(tpu=True)
 @pl_multi_process_test
