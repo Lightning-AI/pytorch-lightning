68a13c66383ccc13e98591e8bf90ccc0afa54cb3
Akihiro Nitta <nitta@akihironitta.com>
2022-03-18 06:35:44 +0900

    Skip test

--- tests/callbacks/test_quantization.py
+++ tests/callbacks/test_quantization.py
@@ -35,6 +35,7 @@ else:
     from torch.quantization import FakeQuantize as FakeQuantizeBase
 
 
+@pytest.mark.skip(reason="TODO: Possible cause of segfaults in CI")
 @pytest.mark.parametrize("observe", ["average", "histogram"])
 @pytest.mark.parametrize("fuse", [True, False])
 @pytest.mark.parametrize("convert", [True, False])
