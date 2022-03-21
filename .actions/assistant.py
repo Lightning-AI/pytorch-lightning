d617b9ca875af609d131c23a41f67d285737b2a7
Jirka <jirka.borovec@seznam.cz>
2022-03-21 05:12:02 +0100

    create

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -1,10 +1,14 @@
 import datetime
+import json
 import os
 import re
+from pprint import pprint
 from typing import Dict, Sequence
+from urllib import request
 
 import fire
 import yaml
+from pkg_resources import parse_version
 
 REQUIREMENT_FILES = (
     "requirements.txt",
@@ -35,6 +39,12 @@ class Requirements:
         self.lines = [f"{ln}{os.linesep}" for ln in self.lines]
         with open(self._fname, "w") as fp:
             fp.writelines(self.lines)
+
+
+def versions(pkg_name):
+    url = f"https://pypi.python.org/pypi/{pkg_name}/json"
+    releases = json.loads(request.urlopen(url).read())["releases"]
+    return sorted(releases, key=parse_version, reverse=True)
 
 
 class AssistantCLI:
@@ -78,7 +88,7 @@ class AssistantCLI:
             req = req[: req.index(comment_char)].strip()
         if not req:  # if requirement is not empty
             return req
-        sep_idx = [req.index(c) for c in "<=>" if c in req]
+        sep_idx = [req.index(c) for c in "<=>[]" if c in req]
         name = (req[: min(sep_idx)] if sep_idx else req).strip()
         if name not in locked:
             return req
@@ -97,6 +107,31 @@ class AssistantCLI:
         for fname in req_files:
             with Requirements(fname, show_final=True) as req:
                 req.lines = [AssistantCLI._lock_pkg_version(ln, locked) for ln in req.lines]
+
+    @staticmethod
+    def _latest_pkg_version(req: str, comment_char: str = "#") -> tuple:
+        sep_idx = [req.index(c) for c in "<=>[]" + comment_char if c in req]
+        name = (req[: min(sep_idx)] if sep_idx else req).strip()
+        if not name:
+            return None, None
+        ver = versions(name)[0]
+        return name, ver
+
+    @staticmethod
+    def create_locked_versions(req_files: Sequence[str] = REQUIREMENT_FILES, lock_file: str = REQUIREMENT_LOCK) -> None:
+        """Replace the min package version by fixed one."""
+        if isinstance(req_files, str):
+            req_files = [req_files]
+
+        locked = {}
+        for fname in req_files:
+            with Requirements(fname) as req:
+                pkg_versions = [AssistantCLI._latest_pkg_version(ln) for ln in req.lines]
+                locked.update({pkg: str(ver) for pkg, ver in pkg_versions if pkg})
+
+        pprint(locked)
+        with open(lock_file, "w") as fp:
+            yaml.safe_dump(locked, fp)
 
 
 if __name__ == "__main__":
