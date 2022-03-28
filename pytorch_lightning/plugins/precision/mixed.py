2dcc7c7632fa5b41126616b8b09bc857ec00d6e7
Justus Schock <justus.schock@posteo.de>
2022-03-28 15:40:56 +0200

    mport

--- pytorch_lightning/plugins/precision/mixed.py
+++ pytorch_lightning/plugins/precision/mixed.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from abc import abstractmethod
 from typing import Union
 
 from pytorch_lightning.plugins.precision.precision_plugin import PrecisionPlugin
