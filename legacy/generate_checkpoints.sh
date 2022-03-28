bf0f07397a66deef181636082a9753967b4fa561
Akihiro Nitta <nitta@akihironitta.com>
2022-03-28 21:35:18 +0900

    Update generate_checkpoints.sh

--- legacy/generate_checkpoints.sh
+++ legacy/generate_checkpoints.sh
@@ -1,44 +1,34 @@
 #!/bin/bash
 # Sample call:
 #  bash generate_checkpoints.sh 1.0.2 1.0.3 1.0.4
-
 set -e
 
 LEGACY_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-FROZEN_MIN_PT_VERSION="1.6"
-
-echo $LEGACY_PATH
-# install some PT version here so it does not need to reinstalled for each env
-pip install virtualenv "torch==1.6" --quiet
-
 ENV_PATH="$LEGACY_PATH/vEnv"
 
 # iterate over all arguments assuming that each argument is version
-for ver in "$@"
+for pl_ver in "$@"
 do
-	echo "processing version: $ver"
-	# mkdir "$LEGACY_PATH/$ver"
+	echo processing version: $pl_ver
 
-  # create local env
-  echo $ENV_PATH
-  virtualenv $ENV_PATH --system-site-packages
-  # activate and install PL version
-  source "$ENV_PATH/bin/activate"
+  # Don't install/update anything before activating venv
+  # to avoid breaking any existing environment.
+  python -m venv $ENV_PATH
+  source $ENV_PATH/bin/activate
+
   # there are problem to load ckpt in older versions since they are saved the newer versions
-  pip install "pytorch_lightning==$ver" "torch==$FROZEN_MIN_PT_VERSION" "torchmetrics" "scikit-learn" --quiet
+  python -m pip install "pytorch_lightning==$pl_ver" torch "torchmetrics" "scikit-learn" --progress-bar off
 
   python --version
-  pip --version
-  pip list | grep torch
+  python -m pip --version
+  python -m pip list | grep -e torch -e learn
 
   python "$LEGACY_PATH/simple_classif_training.py"
-  cp "$LEGACY_PATH/simple_classif_training.py" ${LEGACY_PATH}/checkpoints/${ver}
+  cp "$LEGACY_PATH/simple_classif_training.py" ${LEGACY_PATH}/checkpoints/$pl_ver
 
-  mv ${LEGACY_PATH}/checkpoints/${ver}/lightning_logs/version_0/checkpoints/*.ckpt ${LEGACY_PATH}/checkpoints/${ver}/
-  rm -rf ${LEGACY_PATH}/checkpoints/${ver}/lightning_logs
+  mv ${LEGACY_PATH}/checkpoints/$pl_ver/lightning_logs/version_0/checkpoints/*.ckpt $LEGACY_PATH/checkpoints/$pl_ver/
+  rm -rf ${LEGACY_PATH}/checkpoints/$pl_ver/lightning_logs
 
   deactivate
-  # clear env
   rm -rf $ENV_PATH
-
 done
