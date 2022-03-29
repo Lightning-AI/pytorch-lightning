e62479fc3220a039098634546dc9c4689f670965
Akihiro Nitta <nitta@akihironitta.com>
2022-03-29 12:11:19 +0900

    .

--- legacy/generate_checkpoints.sh
+++ legacy/generate_checkpoints.sh
@@ -3,8 +3,8 @@
 #  bash generate_checkpoints.sh 1.0.2 1.0.3 1.0.4
 set -e
 
-LEGACY_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-ENV_PATH="$LEGACY_PATH/vEnv"
+LEGACY_PATH=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
+ENV_PATH=$LEGACY_PATH/vEnv
 
 # iterate over all arguments assuming that each argument is version
 for pl_ver in "$@"
@@ -17,15 +17,15 @@ do
   source $ENV_PATH/bin/activate
 
   # there are problem to load ckpt in older versions since they are saved the newer versions
-  python -m pip install "pytorch_lightning==$pl_ver" torch "torchmetrics" "scikit-learn" --quiet
+  python -m pip install pytorch_lightning==$pl_ver torch torchmetrics scikit-learn --quiet
 
   python --version
   python -m pip --version
   python -m pip list | grep -e torch -e learn
 
-  python "$LEGACY_PATH/simple_classif_training.py" > /dev/null 2>&1
-  cp "$LEGACY_PATH/simple_classif_training.py" ${LEGACY_PATH}/checkpoints/$pl_ver
+  python $LEGACY_PATH/simple_classif_training.py > /dev/null 2>&1
 
+  cp $LEGACY_PATH/simple_classif_training.py ${LEGACY_PATH}/checkpoints/$pl_ver
   mv ${LEGACY_PATH}/checkpoints/$pl_ver/lightning_logs/version_0/checkpoints/*.ckpt $LEGACY_PATH/checkpoints/$pl_ver/
   rm -rf ${LEGACY_PATH}/checkpoints/$pl_ver/lightning_logs
 
