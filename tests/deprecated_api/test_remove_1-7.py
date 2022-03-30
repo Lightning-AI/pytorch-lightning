e789d2cb606356d51531a026a5935a2d7e87b615
yhl48 <limjohnyh@gmail.com>
2022-03-30 14:53:31 +0000

    remove deprecated test for prepare_data_per_node

--- tests/deprecated_api/test_remove_1-7.py
+++ tests/deprecated_api/test_remove_1-7.py
@@ -126,11 +126,6 @@ def test_v1_7_0_moved_get_progress_bar_dict(tmpdir):
         _ = trainer.progress_bar_dict
 
 
-def test_v1_7_0_trainer_prepare_data_per_node(tmpdir):
-    with pytest.deprecated_call(match="Setting `prepare_data_per_node` with the trainer flag is deprecated in v1.5.0"):
-        _ = Trainer(prepare_data_per_node=False)
-
-
 def test_v1_7_0_stochastic_weight_avg_trainer_constructor(tmpdir):
     with pytest.deprecated_call(match=r"Setting `Trainer\(stochastic_weight_avg=True\)` is deprecated in v1.5"):
         _ = Trainer(stochastic_weight_avg=True)
