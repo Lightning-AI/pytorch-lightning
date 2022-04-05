bba36506527a6e6286f8e5b4b0f7fdcc14871a90
talregev <talregev@users.noreply.github.com>
2022-04-05 11:43:17 +0300

    [Feature] Replace slash in metric name to underscore
    Replace slash `/` in metric name to underscore `_`.
    Filename cannot contain '/' slash by definition.

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -520,6 +520,8 @@ class ModelCheckpoint(Callback):
                 if name not in metrics:
                     metrics[name] = 0
             filename = filename.format(**metrics)
+            # filename cannot contain '/' slash by definition
+            filename = filename.replace('/','_')
 
         if prefix:
             filename = cls.CHECKPOINT_JOIN_CHAR.join([prefix, filename])
