15ac92bcc80a92a1d30217573ea06ec38569cda7
puhuk <wetr235@gmail.com>
2022-04-07 22:42:19 +0900

    Update logger_connector.py

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -16,12 +16,10 @@ from typing import Any, Dict, Iterable, Optional, Union
 import torch
 
 import pytorch_lightning as pl
-from pytorch_lightning.accelerators import GPUAccelerator
 from pytorch_lightning.loggers import LightningLoggerBase, TensorBoardLogger
 from pytorch_lightning.plugins.environments.slurm_environment import SLURMEnvironment
 from pytorch_lightning.trainer.connectors.logger_connector.result import _METRICS, _OUT_DICT, _PBAR_DICT
 from pytorch_lightning.trainer.states import RunningStage
-from pytorch_lightning.utilities import memory
 from pytorch_lightning.utilities.apply_func import apply_to_collection, move_data_to_device
 from pytorch_lightning.utilities.metrics import metrics_to_scalars
 from pytorch_lightning.utilities.model_helpers import is_overridden
