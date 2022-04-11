79aa3ec9a285ec5221177384ed971d0edcec8d13
Kaushik B <kaushikbokka@gmail.com>
2022-04-11 14:07:01 +0530

    Fix tests

--- tests/trainer/properties/test_auto_gpu_select.py
+++ tests/trainer/properties/test_auto_gpu_select.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 import re
 from unittest import mock
 
@@ -26,26 +25,6 @@ from tests.helpers.runif import RunIf
 
 @RunIf(min_gpus=2)
 @pytest.mark.parametrize(
-    ["auto_select_gpus", "gpus", "expected_error"],
-    [(True, 0, MisconfigurationException), (True, -1, None), (False, 0, None), (False, -1, None)],
-)
-def test_trainer_with_gpus_and_auto_select_gpus(auto_select_gpus, gpus, expected_error):
-    if expected_error:
-        with pytest.raises(
-            expected_error,
-            match=re.escape(
-                "auto_select_gpus=True, gpus=0 is not a valid configuration."
-                " Please select a valid number of GPU resources when using auto_select_gpus."
-            ),
-        ), pytest.deprecated_call(match=r"is deprecated in v1.7 and will be removed in v2.0."):
-            Trainer(auto_select_gpus=auto_select_gpus, gpus=gpus)
-    else:
-        with pytest.deprecated_call(match=r"is deprecated in v1.7 and will be removed in v2.0."):
-            Trainer(auto_select_gpus=auto_select_gpus, gpus=gpus)
-
-
-@RunIf(min_gpus=2)
-@pytest.mark.parametrize(
     ["auto_select_gpus", "devices", "expected_error"],
     [(True, 0, MisconfigurationException), (True, -1, None), (False, 0, None), (False, -1, None)],
 )
@@ -54,7 +33,7 @@ def test_trainer_with_devices_and_auto_select_gpus(auto_select_gpus, devices, ex
         with pytest.raises(
             expected_error,
             match=re.escape(
-                "auto_select_gpus=True, gpus=0 is not a valid configuration."
+                "auto_select_gpus=True, gpus/devices=0 is not a valid configuration."
                 " Please select a valid number of GPU resources when using auto_select_gpus."
             ),
         ):
@@ -73,7 +52,7 @@ def test_pick_multiple_gpus(nb, expected_gpu_idxs, expected_error):
         with pytest.raises(
             expected_error,
             match=re.escape(
-                "auto_select_gpus=True, gpus=0 is not a valid configuration."
+                "auto_select_gpus=True, gpus/devices=0 is not a valid configuration."
                 " Please select a valid number of GPU resources when using auto_select_gpus."
             ),
         ):
@@ -82,19 +61,21 @@ def test_pick_multiple_gpus(nb, expected_gpu_idxs, expected_error):
         assert expected_gpu_idxs == pick_multiple_gpus(nb)
 
 
-@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "1"})
 @RunIf(min_gpus=2)
-def test_auto_select_gpus_with_devices_flag():
+@mock.patch("pytorch_lightning.trainer.connectors.accelerator_connector.pick_multiple_gpus", return_value=[1])
+def test_auto_select_gpus_with_devices_flag(pick_gpus_mock):
 
     trainer = Trainer(auto_select_gpus=True, accelerator="gpu", devices=1)
     assert trainer.num_devices == 1
     assert trainer.device_ids == [1]
 
 
-@mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "1"})
 @RunIf(min_gpus=2)
-def test_auto_select_gpus_with_gpus_flag():
+@mock.patch("pytorch_lightning.trainer.connectors.accelerator_connector.pick_multiple_gpus", return_value=[1])
+def test_auto_select_gpus_with_gpus_flag(pick_gpus_mock):
 
-    trainer = Trainer(auto_select_gpus=True, gpus=1)
+    with pytest.deprecated_call(match=r"is deprecated in v1.7 and will be removed in v2.0."):
+        trainer = Trainer(auto_select_gpus=True, gpus=1)
+
     assert trainer.num_devices == 1
     assert trainer.device_ids == [1]
