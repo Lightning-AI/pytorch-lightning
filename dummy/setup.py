8735d4cf104fd564669170c7bba3e013ce7ae6d5
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-04-14 09:38:53 +0200

    wip

---
+++ dummy/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="my-package",
+    version="0.0.1",
+    # entry_points={
+    #     "pytorch_lightning.callbacks_factory": [
+    #         # The format here must be [any name]=[module path]:[function name]
+    #         "monitor_callbacks=factories:my_callbacks_factory"
+    #     ]
+    # },
+)
