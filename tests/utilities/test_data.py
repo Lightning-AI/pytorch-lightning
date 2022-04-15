b8d4b812212970113b0951a77e8b76985f85c0d1
otaj <6065855+otaj@users.noreply.github.com>
2022-04-15 13:58:28 +0200

    Inspect correct function in wrap_init (#12716)
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>
    Co-authored-by: Rohit Gupta <rohitgr1998@gmail.com>

--- tests/utilities/test_data.py
+++ tests/utilities/test_data.py
@@ -161,19 +161,38 @@ def test_replace_dataloader_init_method():
             super().__init__(*args, **kwargs)
 
     class DataLoaderSubclass2(DataLoaderSubclass1):
-        def __init__(self, attribute1, attribute2, *args, **kwargs):
+        def __init__(self, attribute2, *args, **kwargs):
             # intentionally not setting this attribute, calling super with different args
             # self.attribute2 = attribute2
-            super().__init__(attribute1, *args, **kwargs)
+            super().__init__(attribute2 + "-2", *args, **kwargs)
 
     with _replace_dataloader_init_method():
         dataloader = DataLoaderSubclass1("attribute1", dataset=range(4), batch_size=2)
-        assert dataloader.attribute1 == "attribute1"
+
+    assert dataloader.attribute1 == "attribute1"
 
     with _replace_dataloader_init_method():
-        dataloader = DataLoaderSubclass2("attribute1", "attribute2", dataset=range(4), batch_size=2)
-        assert dataloader.attribute1 == "attribute1"
-        assert dataloader.attribute2 == "attribute2"
+        dataloader = DataLoaderSubclass2("attribute2", dataset=range(4), batch_size=2)
+
+    assert dataloader.attribute1 == "attribute2-2"
+    assert dataloader.attribute2 == "attribute2"
+
+    # Failing test case from issue 12564
+    class MyBaseDataLoader(DataLoader):
+        pass
+
+    class MyDataLoader(MyBaseDataLoader):
+        def __init__(self, data: torch.Tensor, *args, **kwargs):
+            self.data = data
+            super().__init__(range(data.size(0)), *args, **kwargs)
+
+    data = torch.randn((10, 20))
+
+    with _replace_dataloader_init_method():
+        dataloader = MyDataLoader(data, batch_size=2)
+
+    assert dataloader.data is data
+    assert dataloader.dataset == range(10)
 
     # `poptorch.DataLoader` uses this pattern, simulate it
     class PoptorchDataLoader(DataLoader):
@@ -188,9 +207,11 @@ def test_replace_dataloader_init_method():
     # †his read-only property pattern is fine
     dataloader = PoptorchDataLoader(123, [1])
     assert dataloader.options == 123
+
     # still works with the init replacement
     with _replace_dataloader_init_method():
         dataloader = PoptorchDataLoader(123, [1])
+
     assert dataloader.options == 123
 
 
