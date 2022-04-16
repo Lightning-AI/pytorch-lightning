1a0ba480da866c95cefc63fab17375206ba7fa6f
William Falcon <waf2107@columbia.edu>
2022-04-16 18:24:31 -0400

    added demos folder

---
+++ pytorch_lightning/demos/__init__.py
@@ -0,0 +1,8 @@
+from pytorch_lightning.demos.boring_classes import (  # noqa: F401
+    BoringDataModule,
+    BoringModel,
+    ManualOptimBoringModel,
+    RandomDataset,
+)
+
+__all__ = ["BoringDataModule", "BoringModel", "ManualOptimBoringModel", "RandomDataset"]
