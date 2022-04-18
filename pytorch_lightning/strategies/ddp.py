77fc17c384a68aa09b42249fbca257efdf9eb43a
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-04-18 22:23:31 +0530

    Update pytorch_lightning/strategies/ddp.py
    Co-authored-by: Rohit Gupta <rohitgr1998@gmail.com>

--- pytorch_lightning/strategies/ddp.py
+++ pytorch_lightning/strategies/ddp.py
@@ -63,8 +63,7 @@ from pytorch_lightning.utilities.types import STEP_OUTPUT
 if _FAIRSCALE_AVAILABLE:
     from fairscale.optim import OSS
 if _TORCH_GREATER_EQUAL_1_10 and torch.distributed.is_available():
-    if torch.distributed.is_available():
-        from torch.distributed.algorithms.model_averaging.averagers import ModelAverager
+    from torch.distributed.algorithms.model_averaging.averagers import ModelAverager
 
 
 log = logging.getLogger(__name__)
