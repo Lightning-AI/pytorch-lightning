9b9448f9075aecaa678021d2c308a8971dfca28e
Jirka <jirka.borovec@seznam.cz>
2022-04-18 15:42:12 +0200

    debug & cli

--- pytorch_lightning/utilities/cli.py
+++ pytorch_lightning/utilities/cli.py
@@ -487,7 +487,7 @@ class LightningCLI:
         A full configuration yaml would be parsed from ``PL_CONFIG`` if set.
         Individual settings are so parsed from variables named for example ``PL_TRAINER__MAX_EPOCHS``.
 
-        For more info, read :ref:`the CLI docs <common/lightning_cli:LightningCLI>`.
+        For more info, read :ref:`the CLI docs <common/cli:LightningCLI>`.
 
         .. warning:: ``LightningCLI`` is in beta and subject to change.
 
@@ -507,7 +507,7 @@ class LightningCLI:
             trainer_defaults: Set to override Trainer defaults or add persistent callbacks. The callbacks added through
                 this argument will not be configurable from a configuration file and will always be present for
                 this particular CLI. Alternatively, configurable callbacks can be added as explained in
-                :ref:`the CLI docs <common/lightning_cli:Configurable callbacks>`.
+                :ref:`the CLI docs <common/cli:Configurable callbacks>`.
             seed_everything_default: Default value for the :func:`~pytorch_lightning.utilities.seed.seed_everything`
                 seed argument.
             description: Description of the tool shown when running ``--help``.
