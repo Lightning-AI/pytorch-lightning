ecd3df7c0d2e7afae457bebfbf25e8c89e4157ea
Kaushik B <kaushikbokka@gmail.com>
2022-04-19 18:23:02 +0530

    Ignore demos for mypy

--- pytorch_lightning/demos/boring_classes.py
+++ pytorch_lightning/demos/boring_classes.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# type: ignore
-# TODO: Resolve typing for demo
 from typing import Optional
 
 import torch
