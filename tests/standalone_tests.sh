41cc00014f86ad5859249fbf5c60d29aa57a9e48
Jirka <jirka.borovec@seznam.cz>
2022-04-25 18:53:09 +0200

    tests/utilities/test_warnings.py

--- tests/standalone_tests.sh
+++ tests/standalone_tests.sh
@@ -36,7 +36,7 @@ fi
 parametrizations_arr=($parametrizations)
 
 # TODO: tests to skip - space separated
-blocklist='tests/profiler/test_profiler.py::test_pytorch_profiler_nested_emit_nvtx'
+blocklist='tests/profiler/test_profiler.py::test_pytorch_profiler_nested_emit_nvtx tests/utilities/test_warnings.py'
 report=''
 
 for i in "${!parametrizations_arr[@]}"; do
