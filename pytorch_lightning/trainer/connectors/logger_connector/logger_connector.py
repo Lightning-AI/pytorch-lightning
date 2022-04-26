46604d9c1680c82e905c8bfe606cbd87a44d36d2
Rohit Gupta <rohitgr1998@gmail.com>
2022-04-26 15:58:50 +0530

    bad merge

--- pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -19,7 +19,6 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import Logger, TensorBoardLogger
 from pytorch_lightning.plugins.environments.slurm_environment import SLURMEnvironment
 from pytorch_lightning.trainer.connectors.logger_connector.result import _METRICS, _OUT_DICT, _PBAR_DICT
-from pytorch_lightning.utilities import memory
 from pytorch_lightning.utilities.apply_func import apply_to_collection, move_data_to_device
 from pytorch_lightning.utilities.metrics import metrics_to_scalars
 from pytorch_lightning.utilities.model_helpers import is_overridden
