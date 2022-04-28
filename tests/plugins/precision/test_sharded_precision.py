c0007c5394b79d8f2d84080554f86cc5092cf546
SeanNaren <sean@grid.ai>
2022-04-28 11:00:01 +0100

    Only use sharded scaler when in fp16

---
+++ tests/plugins/precision/test_sharded_precision.py
@@ -0,0 +1,33 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from pytorch_lightning.plugins import ShardedNativeMixedPrecisionPlugin
+from pytorch_lightning.utilities import _FAIRSCALE_AVAILABLE
+from tests.helpers.runif import RunIf
+
+ShardedGradScaler = None
+if _FAIRSCALE_AVAILABLE:
+    from fairscale.optim.grad_scaler import ShardedGradScaler
+
+
+@RunIf(fairscale=True)
+@pytest.mark.parametrize("precision,expected", [(16, ShardedGradScaler), ("bf16", None), (32, None)])
+def test_sharded_precision_scaler(precision, expected):
+    plugin = ShardedNativeMixedPrecisionPlugin(precision=precision, device="cuda")
+    if expected:
+        assert isinstance(plugin.scaler, expected)
+    else:
+        assert not plugin.scaler
