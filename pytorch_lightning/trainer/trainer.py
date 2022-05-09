03248c947f6a156aeeb1932051729307201f8890
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2022-05-09 10:31:05 +0530

    Update pytorch_lightning/trainer/trainer.py

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -2807,7 +2807,7 @@ def _evaluation_context() -> Generator:
         torch.inference_mode
         if _TORCH_GREATER_EQUAL_1_9
         and not (dist.is_initialized() and dist.get_backend() == "gloo")
-        and not (_HPU_AVAILABLE)
+        and not isinstance(self.accelerator, HPUAccelerator)
         else torch.no_grad
     )
     with context_manager_class():
