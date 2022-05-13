0771babaa028c65898b0bb21707bbfcda0172a49
Akihiro Nitta <nitta@akihironitta.com>
2022-05-13 17:32:24 +0900

    Update test

--- tests/utilities/test_meta.py
+++ tests/utilities/test_meta.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import pytest
 from torch import nn
 
 from pytorch_lightning.core.lightning import LightningModule
@@ -73,14 +74,12 @@ def test_init_meta_context():
 
 
 @RunIf(min_torch="1.10.0", standalone=True)
-def test_fit_with_init_meta_context():
-    """Test fitting a model initialized on meta device."""
+def test_materialize_module_recursive_child():
+    """Test materialize_module doesn't set a child recursively to a model instantiated within init_meta_context."""
     with init_meta_context():
         model = BoringModel()
-    trainer = Trainer(
-        fast_dev_run=True,
-        enable_progress_bar=False,
-        enable_model_summary=False,
-        enable_checkpointing=False,
-    )
-    trainer.fit(model)
+
+    materialize_module(model)
+
+    with pytest.raises(AttributeError, match="'Linear' object has no attribute 'layer'"):
+        model.layer.layer
