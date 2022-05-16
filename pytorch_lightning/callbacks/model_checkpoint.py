898feaf5e471a88d29a6d029129a355d0413f5b6
otaj <6065855+otaj@users.noreply.github.com>
2022-05-16 09:20:04 +0200

    Update pytorch_lightning/callbacks/model_checkpoint.py
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -49,7 +49,7 @@ class BaseModelCheckpoint(Callback):
     r"""
     This is the base class for Model checkpointing. Expert users may want to subclass it in case of writing
     custom :class:`~pytorch_lightning.callbacks.model_checkpoint.ModelCheckpoint` callback, so that
-    the trainer recognizes the custom class as a checkpointing callback
+    the trainer recognizes the custom class as a checkpointing callback.
     """
 
 
