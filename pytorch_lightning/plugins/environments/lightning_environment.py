7b422609504f046dcd2e85a666e74a91833bb71b
awaelchli <aedu.waelchli@gmail.com>
2022-05-20 00:05:38 -0400

    Avoid annoying firewall message from find_free_network_port

--- pytorch_lightning/plugins/environments/lightning_environment.py
+++ pytorch_lightning/plugins/environments/lightning_environment.py
@@ -96,7 +96,6 @@ def find_free_network_port() -> int:
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 0))
-    s.listen(1)
     port = s.getsockname()[1]
     s.close()
     return port
