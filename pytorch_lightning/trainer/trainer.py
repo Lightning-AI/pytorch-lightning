db2a36c9469b4fb4a1ebf64ae5137ebde89fee3f
mads-oestergaard <104391876+mads-oestergaard@users.noreply.github.com>
2022-05-23 12:09:47 +0200

    Update trainer profiler typehint to use `Profiler` instead of the deprecated `BaseProfiler` (#13084)
    * Fix trainer profiler typehint
    
    * Remove unused import of deprecated BaseProfiler
    
    * Update CHANGELOG.md
    
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -52,7 +52,6 @@ from pytorch_lightning.plugins import (
 from pytorch_lightning.plugins.environments.slurm_environment import SLURMEnvironment
 from pytorch_lightning.profiler import (
     AdvancedProfiler,
-    BaseProfiler,
     PassThroughProfiler,
     Profiler,
     PyTorchProfiler,
@@ -173,7 +172,7 @@ class Trainer(
         weights_save_path: Optional[str] = None,  # TODO: Remove in 1.8
         num_sanity_val_steps: int = 2,
         resume_from_checkpoint: Optional[Union[Path, str]] = None,
-        profiler: Optional[Union[BaseProfiler, str]] = None,
+        profiler: Optional[Union[Profiler, str]] = None,
         benchmark: Optional[bool] = None,
         deterministic: bool = False,
         reload_dataloaders_every_n_epochs: int = 0,
