b93d1e4ec380834beb2b5687c88dd6fd812dbc5c
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-05-24 10:01:45 +0200

    update callback tests

--- tests/callbacks/test_stochastic_weight_avg.py
+++ tests/callbacks/test_stochastic_weight_avg.py
@@ -160,10 +160,12 @@ def test_swa_callback_ddp_spawn(tmpdir):
 def test_swa_callback_ddp_cpu(tmpdir):
     train_with_swa(tmpdir, strategy="ddp_spawn", accelerator="cpu", devices=2)
 
-
-@RunIf(min_cuda_gpus=1)
-def test_swa_callback_1_gpu(tmpdir):
-    train_with_swa(tmpdir, accelerator="gpu", devices=1)
+@pytest.mark.parametrize('accelerator', [
+    pytest.param("gpu", marks=RunIf(min_cuda_gpus=1)),
+    pytest.param("mps", marks=RunIf(mps=True))
+])
+def test_swa_callback_1_gpu(tmpdir, accelerator):
+    train_with_swa(tmpdir, accelerator=accelerator, devices=1)
 
 
 @pytest.mark.parametrize("batchnorm", (True, False))
