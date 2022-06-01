d5b37e9e33b59098db615189b48c0dd5fd52244a
Jirka <jirka.borovec@seznam.cz>
2022-06-01 15:05:16 +0200

    rename unittests

---
+++ test/unittests/helpers/__init__.py
@@ -0,0 +1,7 @@
+from unittests.helpers.boring_model import (  # noqa: F401
+    BoringDataModule,
+    BoringModel,
+    ManualOptimBoringModel,
+    RandomDataset,
+)
+from unittests.helpers.datasets import TrialMNIST  # noqa: F401
