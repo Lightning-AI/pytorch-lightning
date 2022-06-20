80678438e2f07fa6a43c2f02709e5a44d4d9de64
Adam J. Stewart <ajstewart426@gmail.com>
2022-06-20 10:21:32 -0700

    Update import name

--- tests/tests_pytorch/trainer/flags/test_min_max_epochs.py
+++ tests/tests_pytorch/trainer/flags/test_min_max_epochs.py
@@ -1,5 +1,5 @@
 import pytest
-from tests.helpers.utils import no_warning_call
+from tests_pytorch.helpers.utils import no_warning_call
 
 from pytorch_lightning import Trainer
 from pytorch_lightning.demos.boring_classes import BoringModel
