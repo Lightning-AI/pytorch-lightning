7090ba94bf3eb2c441e62d6b876dd97ab7f44514
awaelchli <aedu.waelchli@gmail.com>
2022-06-22 16:57:19 +0200

    Reroute profiler to profilers (#12308)
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>

---
+++ src/pytorch_lightning/profiler/advanced.py
@@ -0,0 +1,24 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pytorch_lightning.profilers.advanced import AdvancedProfiler as NewAdvancedProfiler
+from pytorch_lightning.utilities import rank_zero_deprecation
+
+
+class AdvancedProfiler(NewAdvancedProfiler):
+    def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        rank_zero_deprecation(
+            "`pytorch_lightning.profiler.AdvancedProfiler` is deprecated in v1.7 and will be removed in v1.9."
+            " Use the equivalent `pytorch_lightning.profilers.AdvancedProfiler` class instead."
+        )
+        super().__init__(*args, **kwargs)
