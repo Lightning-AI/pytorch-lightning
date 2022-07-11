dd9c9866d30979b3dca8887ff783128323b22f4e
Rohit Gupta <rohitgr1998@gmail.com>
2022-07-11 23:17:33 +0530

    update

--- src/pytorch_lightning/loggers/base.py
+++ src/pytorch_lightning/loggers/base.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, Mapping, Optional, Sequence
+from typing import Any, Callable, Dict, Mapping, Optional, Sequence
 
 import numpy as np
 
