80e8ab050bf341174bff790321442e54d41af602
donlapark <10988155+donlapark@users.noreply.github.com>
2022-07-14 01:24:34 +0700

    Clean up code

--- src/pytorch_lightning/tuner/tuning.py
+++ src/pytorch_lightning/tuner/tuning.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Any, Dict, Optional, Union
-
 from typing_extensions import NotRequired, TypedDict
 
 import pytorch_lightning as pl
