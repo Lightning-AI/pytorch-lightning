393f797bd6e1b6ff1623ebdc22d9c415f2be87ba
thomas chaton <thomas@grid.ai>
2022-07-14 11:29:01 +0100

    update

--- .actions/job_skipper.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--changed_files", type=list, action="store", required=True)
-hparams = parser.parse_args()
-print(hparams.changed_files)
