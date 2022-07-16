8737df166d86db06d557d31b9e26988ad5784ce5
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-07-16 11:41:30 -0400

    pre-commit & mypy conflict

--- src/pytorch_lightning/core/module.py
+++ src/pytorch_lightning/core/module.py
@@ -1997,5 +1997,5 @@ class LightningModule(
         else:
             # We need to make sure the self inside the method is a weakref proxy
             self.__class__._register_load_state_dict_pre_hook(
-                weakref.proxy(self), pre_load_state_dict_hook, True
-            )  # type: ignore[arg-type]
+                weakref.proxy(self), pre_load_state_dict_hook, True  # type: ignore[arg-type]
+            )
