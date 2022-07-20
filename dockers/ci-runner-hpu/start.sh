64e8e8eb4b93ec0895a7f624e519978dff35fab8
Jirka Borovec <Borda@users.noreply.github.com>
2022-07-20 12:35:01 +0200

    CI: debug HPU flow (#13419)
    * Update the hpu-tests.yml to pull docker from vault
    * fire & sudo
    * habana-gaudi-hpus
    * Check the driver status on gaudi server (#13718)
    
    Co-authored-by: arao <arao@habana.ai>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Akarsha Rao <94624926+raoakarsha@users.noreply.github.com>

--- dockers/ci-runner-hpu/start.sh
+++ dockers/ci-runner-hpu/start.sh
@@ -93,4 +93,4 @@ trap 'cleanup; exit 143' TERM
 
 # To be aware of TERM and INT signals call run.sh
 # Running it with the --once flag at the end will shut down the agent after the build is executed
-./run.sh --once & wait $!
+./run.sh & wait $!
