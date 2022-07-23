aa548c94497f728cec43c585e619e15ac070461a
thomas chaton <thomas@grid.ai>
2022-07-23 21:10:03 +0200

    update

--- examples/app_multi_node/train.py
+++ examples/app_multi_node/train.py
@@ -3,5 +3,5 @@ from lightning.pytorch.demos.boring_classes import BoringModel
 
 if __name__ == "__main__":
     model = BoringModel()
-    trainer = Trainer(max_epochs=1, devices=2, strategy="ddp")
+    trainer = Trainer(max_epochs=1, strategy="ddp")
     trainer.fit(model)
