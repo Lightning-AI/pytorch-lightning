ee161b33e6669b1227a14e4c3ace39e0ddb6ae85
thomas chaton <thomas@grid.ai>
2022-07-23 14:16:41 +0200

    update

--- examples/app_multi_node/app.py
+++ examples/app_multi_node/app.py
@@ -2,4 +2,10 @@ from lightning import LightningApp
 from lightning.app.components.training import LightningTrainingComponent
 from lightning_app.utilities.packaging.cloud_compute import CloudCompute
 
-app = LightningApp(LightningTrainingComponent("train.py", num_nodes=2, cloud_compute=CloudCompute("cpu")))
+app = LightningApp(
+    LightningTrainingComponent(
+        "train.py",
+        num_nodes=2,
+        cloud_compute=CloudCompute("gpu"),
+    ),
+)
