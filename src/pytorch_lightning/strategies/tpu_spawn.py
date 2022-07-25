bec228a3de1c59df92fc271e060ff28fc88b0d47
BongYang <douvlecircle@gmail.com>
2022-07-25 13:35:22 +0900

    fix is_distribued check world_size

--- src/pytorch_lightning/strategies/tpu_spawn.py
+++ src/pytorch_lightning/strategies/tpu_spawn.py
@@ -153,7 +153,7 @@ class TPUSpawnStrategy(DDPSpawnStrategy):
     @property
     def is_distributed(self) -> bool:
         # HOST_WORLD_SIZE is None outside the xmp.spawn process
-        return os.getenv(xenv.HOST_WORLD_SIZE, None) is not None and self.world_size != 1
+        return (xenv.HOST_WORLD_SIZE in os.environ) and self.world_size != 1
 
     def process_dataloader(self, dataloader: DataLoader) -> MpDeviceLoader:
         TPUSpawnStrategy._validate_dataloader(dataloader)
