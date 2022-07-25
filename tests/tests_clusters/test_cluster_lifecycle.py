fc5a1e6ec8e63e414ad2597faa93d9d497eb1724
Raphael Randschau <raphael@lightning.ai>
2022-07-25 15:11:30 -0700

    adjust e2e tests

--- tests/tests_clusters/test_cluster_lifecycle.py
+++ tests/tests_clusters/test_cluster_lifecycle.py
@@ -1,15 +1,25 @@
+import os
+import uuid
+
 import pytest
 
 from src.lightning_app.testing.testing import run_cli
 
 
 @pytest.mark.cloud
+@pytest.mark.skipif(
+    os.environ.get("LIGHTNING_BYOC_ROLE_ARN") is None, reason="missing LIGHTNING_BYOC_ROLE_ARN environment variable"
+)
+@pytest.mark.skipif(
+    os.environ.get("LIGHTNING_BYOC_EXTERNAL_ID") is None,
+    reason="missing LIGHTNING_BYOC_EXTERNAL_ID environment variable",
+)
 def test_cluster_lifecycle() -> None:
-    role_arn = "TODO"
-    external_id = "TODO"
+    role_arn = os.environ.get("LIGHTNING_BYOC_ROLE_ARN", None)
+    external_id = os.environ.get("LIGHTNING_BYOC_EXTERNAL_ID", None)
     region = "us-west-2"
     instance_types = "t2.small,t3.small"
-    cluster_name = "TODO-randomize"
+    cluster_name = "byoc-%s" % (uuid.uuid4())
     with run_cli(
         [
             "create",
@@ -39,5 +49,5 @@ def test_cluster_lifecycle() -> None:
 
 @pytest.mark.cloud
 def test_cluster_list() -> None:
-    with run_cli(["clusters", "list"]) as (stdout, stderr):
-        assert "grid-cloud" in stdout, f"stdout: {stdout}\nstderr: {stderr}"
+    with run_cli(["list", "clusters"]) as (stdout, stderr):
+        assert "lightning-cloud" in stdout, f"stdout: {stdout}\nstderr: {stderr}"
