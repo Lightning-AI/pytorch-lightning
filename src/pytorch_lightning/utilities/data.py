a0bb6ff86c3794b3864ba0a1fb6d991972046507
otaj <ota@lightning.ai>
2022-07-26 14:01:04 +0200

    test ipu failing test

--- src/pytorch_lightning/utilities/data.py
+++ src/pytorch_lightning/utilities/data.py
@@ -379,7 +379,11 @@ def _dataloader_init_kwargs_resolve_sampler(
             fast_forward_sampler.setup(dataloader_batch_size=1)
 
         return {
+            "sampler": None,
+            "shuffle": False,
             "batch_sampler": batch_sampler,
+            "batch_size": 1,
+            "drop_last": False,
         }
 
     if fault_tolerant_mode.is_automatic:
