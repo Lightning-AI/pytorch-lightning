cef9e919e3f29ea430643c8b74b6d1ef2bdb4c21
donlapark <10988155+donlapark@users.noreply.github.com>
2022-07-26 17:59:20 +0700

    Add `assert` that `lightning_module` can be `None`

--- src/pytorch_lightning/strategies/deepspeed.py
+++ src/pytorch_lightning/strategies/deepspeed.py
@@ -613,7 +613,7 @@ class DeepSpeedStrategy(DDPStrategy):
         # the model may not be wrapped with DeepEngine & LightningDeepSpeedModule if calling this too early
         module = getattr(self.model, "module", self.model)
         module = module.module if isinstance(module, LightningDeepSpeedModule) else module
-        assert isinstance(module, pl.LightningModule)
+        assert isinstance(module, pl.LightningModule) or module is None
         return module
 
     @property
