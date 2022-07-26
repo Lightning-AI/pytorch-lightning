d93fb7a780c4ddac591b89cdc3e4ae32f957d2ac
awaelchli <aedu.waelchli@gmail.com>
2022-07-26 07:13:40 +0200

    docs

--- src/pytorch_lightning/tuner/lr_finder.py
+++ src/pytorch_lightning/tuner/lr_finder.py
@@ -174,12 +174,12 @@ class _LRFinder:
         return fig
 
     def suggestion(self, skip_begin: int = 10, skip_end: int = 1) -> Optional[float]:
-        """This will propose a suggestion for choice of initial learning rate as the point with the steepest
-        negative gradient.
+        """This will propose a suggestion for an initial learning rate based on the point with the steepest negative
+        gradient.
 
         Args:
-            skip_begin: how many samples to skip in the beginning. Prevent too naive estimates
-            skip_end: how many samples to skip in the end. Prevent too optimistic estimates
+            skip_begin: how many samples to skip in the beginning; helps to avoid too naive estimates
+            skip_end: how many samples to skip in the end; helps to avoid too optimistic estimates
 
         Returns:
             The suggested initial learning rate to use, or `None` if a suggestion is not possible due to too few
