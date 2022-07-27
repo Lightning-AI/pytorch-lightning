fix mypy error with ignore[list-item] in broadcast
ref from broadcast() in fully_sharded_native.py

src/pytorch_lightning/strategies/ddp.py
@@ -326,7 +326,7 @@ class DDPStrategy(ParallelStrategy):
     def broadcast(self, obj: TBroadcast, src: int = 0) -> TBroadcast:
         obj = [obj]
         if self.global_rank != src:
-            obj = [None]
+            obj = [None]  # type: ignore[list-item]
         torch.distributed.broadcast_object_list(obj, src, group=_group.WORLD)
         return obj[0]
 