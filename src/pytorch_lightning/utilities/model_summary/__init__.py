835067808c2e91e70abe58bbdcdf52cfa1f9fd8a
awaelchli <aedu.waelchli@gmail.com>
2022-07-27 22:33:26 +0200

    organize model summary utilities

---
+++ src/pytorch_lightning/utilities/model_summary/__init__.py
@@ -0,0 +1,15 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pytorch_lightning.utilities.model_summary.model_summary import LayerSummary, ModelSummary, get_human_readable_count, parse_batch_shape, get_formatted_model_size, summarize  # noqa: F401
+from pytorch_lightning.utilities.model_summary.model_summary_deepspeed import DeepSpeedSummary  # noqa: F401
