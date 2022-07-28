fc90611ed0a98972cfcde79352fa8fb5d39caf04
Abhishek Kumar <abhishekiitd327@gmail.com>
2022-07-28 14:46:22 +0530

    Add HPUDataModule and example

---
+++ examples/pl_hpu/hpu_datamodule_sample.py
@@ -0,0 +1,125 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import argparse
+
+import torch
+from torch.nn import functional as F
+from torchvision import transforms, models
+
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.hpu_datamodule import HPUDataModule
+
+_DATASETS_PATH = "./data"
+
+class RN50Module(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+        backbone = models.resnet50()
+        num_filters = backbone.fc.in_features
+        layers = list(backbone.children())[:-1]
+        self.feature_extractor = torch.nn.Sequential(*layers)
+
+        num_target_classes = 10
+        self.classifier = torch.nn.Linear(num_filters, num_target_classes)
+
+    def forward(self, x):
+        representations = self.feature_extractor(x).flatten(1)
+        x = self.classifier(representations)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        loss = F.cross_entropy(self(x), y)
+        return loss
+
+    def accuracy(self, logits, y):
+        acc = torch.sum(torch.eq(torch.argmax(logits, -1), y).to(torch.float32)) / len(y)
+        return acc
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        probs = self(x)
+        acc = self.accuracy(probs, y)
+        return acc
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        acc = self.accuracy(logits, y)
+        return acc
+
+    def validation_epoch_end(self, outputs) -> None:
+        self.log("val_acc", torch.stack(outputs).mean(), prog_bar=True)
+
+    def test_epoch_end(self, outputs) -> None:
+        self.log("test_acc", torch.stack(outputs).mean())
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.02)
+
+
+def main(args):
+
+    data_path = args.data_path
+    train_dir = os.path.join(data_path, 'train')
+    val_dir = os.path.join(data_path, 'val')
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_transforms=[
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    val_transforms = [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ]
+
+    data_module = HPUDataModule(
+        train_dir,
+        val_dir,
+        train_transforms=train_transforms,
+        val_transforms=val_transforms,
+        num_workers=8,
+        batch_size=32,
+        shuffle=False,
+        pin_memory=True,
+        drop_last=True,
+        )
+
+    # Initialize a trainer
+    trainer = pl.Trainer(devices=1, accelerator="hpu", max_epochs=1, max_steps=2)
+
+    # Init our model
+    model = RN50Module()
+
+    trainer.fit(model, datamodule=data_module)
+    trainer.validate(model, datamodule=data_module)
+    trainer.test(model, datamodule=data_module)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='PyLight ImageNet Training')
+    parser.add_argument('--data-path', default=_DATASETS_PATH, help='dataset')
+    args = parser.parse_args()
+
+    main(args)
---
+++ src/pytorch_lightning/utilities/hpu_datamodule.py
@@ -0,0 +1,169 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.imports import _TORCHVISION_AVAILABLE
+from pytorch_lightning.utilities import _HPU_AVAILABLE
+
+
+if _TORCHVISION_AVAILABLE:
+    from torchvision import transforms as transform_lib
+    import torchvision.datasets
+
+if _HPU_AVAILABLE:
+    try:
+        import habana_dataloader
+    except ImportError:
+        raise ModuleNotFoundError("habana_dataloader package is not installed.")
+else:
+    raise RuntimeError("Expected device type HPU for running HPUDataModule ...")
+
+_DATASETS_PATH = "./data"
+
+def load_data(traindir, valdir, train_transforms, val_transforms):
+    # check supported transforms
+    if train_transforms != None:
+        for t in train_transforms:
+            if isinstance(t, transform_lib.RandomResizedCrop) or \
+                isinstance(t, transform_lib.RandomHorizontalFlip) or \
+                isinstance(t, transform_lib.ToTensor) or \
+                isinstance(t, transform_lib.Normalize):
+
+                continue
+            else:
+                raise ValueError("Unsupported train transform: " + str(type(t)))
+
+        train_transforms = transform_lib.Compose(train_transforms)
+
+    if val_transforms != None:
+        for t in val_transforms:
+            if isinstance(t, transform_lib.Resize) or \
+                isinstance(t, transform_lib.CenterCrop) or \
+                isinstance(t, transform_lib.ToTensor) or \
+                isinstance(t, transform_lib.Normalize):
+
+                continue
+            else:
+                raise ValueError("Unsupported val transform: " + str(type(t)))
+
+        val_transforms = transform_lib.Compose(val_transforms)
+
+    if "imagenet" not in traindir.lower() and "ilsvrc2012" not in traindir.lower():
+        raise ValueError("Habana dataloader only supports Imagenet dataset")
+        
+    # Data loading code
+    dataset_train = torchvision.datasets.ImageFolder(
+        traindir,
+        train_transforms)
+
+    dataset_val = torchvision.datasets.ImageFolder(
+        valdir,
+        val_transforms)
+
+    return dataset_train, dataset_val
+
+
+class HPUDataModule(pl.LightningDataModule):
+
+    name = "hpu-dataset"
+
+    def __init__(
+        self,
+        train_dir: str = _DATASETS_PATH,
+        val_dir: str = _DATASETS_PATH,
+        num_workers: int = 8,
+        normalize: bool = False,
+        seed: int = 42,
+        batch_size: int = 32,
+        train_transforms = None,
+        val_transforms = None,
+        pin_memory = True,
+        shuffle = False,
+        drop_last = True,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.train_dir = train_dir
+        self.val_dir = val_dir
+        self.num_workers = num_workers
+        self.normalize = normalize
+        self.seed = seed
+        self.batch_size = batch_size
+        self.train_transform = train_transforms
+        self.val_transform = val_transforms
+        self.pin_memory = pin_memory
+        self.shuffle = shuffle
+        self.drop_last = drop_last
+
+        self.data_loader_type = habana_dataloader.HabanaDataLoader
+
+    def setup(self, stage: Optional[str] = None):
+        if self.shuffle == True:
+            raise ValueError("HabanaDataLoader does not support shuffle=True")
+
+        if self.pin_memory == False:
+            raise ValueError("HabanaDataLoader supports only pin_memory=True")
+
+        if self.num_workers != 8:
+            raise ValueError("HabanaDataLoader only supports num_workers as 8")
+
+        self.dataset_train, self.dataset_val = load_data(
+            self.train_dir,
+            self.val_dir,
+            self.train_transform,
+            self.val_transform)
+
+        dataset = self.dataset_train if stage == 'fit' else self.dataset_val
+        if dataset == None:
+            raise TypeError("Error creating dataset")
+
+    def train_dataloader(self):
+        """train set removes a subset to use for validation."""
+        loader = self.data_loader_type(
+            self.dataset_train,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            drop_last=self.drop_last,
+        )
+        return loader
+
+    def val_dataloader(self):
+        """val set uses a subset of the training set for validation."""
+        loader = self.data_loader_type(
+            self.dataset_val,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            drop_last=self.drop_last,
+        )
+        return loader
+
+    def test_dataloader(self):
+        """test set uses the test split."""
+        loader = self.data_loader_type(
+            self.dataset_val,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            drop_last=self.drop_last,
+        )
+        return loader
