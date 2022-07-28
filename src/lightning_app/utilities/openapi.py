22868f397aa0c9eaa0e65e746ace7b986ead8923
Raphael Randschau <nicolai86@users.noreply.github.com>
2022-07-28 13:48:07 -0700

    Update src/lightning_app/utilities/openapi.py
    Co-authored-by: Laverne Henderson <laverne.henderson@coupa.com>

--- src/lightning_app/utilities/openapi.py
+++ src/lightning_app/utilities/openapi.py
@@ -7,7 +7,7 @@ def _duplicate_checker(js):
     result = {}
     for name, value in js:
         if name in result:
-            raise ValueError(f"Failed to load JSON: duplicate key {name}.")
+            raise ValueError(f"Unable to load JSON. A duplicate key {name} was detected. JSON objects must have unique keys.")
         result[name] = value
     return result
 
