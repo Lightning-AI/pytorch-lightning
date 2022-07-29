63353b54181b1a40399fece0fbe498ca5200d7c2
Adam Bobowski <adam@grid.ai>
2022-07-29 15:36:54 +0200

    typo fix

--- src/lightning_app/core/work.py
+++ src/lightning_app/core/work.py
@@ -521,7 +521,7 @@ class LightningWork(abc.ABC):
         """Override to add your own logic.
 
         Raises:
-            LightningPlatformException: If resource exceeds platform quotas or other contraints.
+            LightningPlatformException: If resource exceeds platform quotas or other constraints.
         """
         pass
 
