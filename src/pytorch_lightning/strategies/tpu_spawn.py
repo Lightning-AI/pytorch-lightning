19b84c99cc34f5df0bad335590e881232ba5e26f
awaelchli <aedu.waelchli@gmail.com>
2022-07-29 17:43:26 +0200

    remove unused import

--- src/pytorch_lightning/strategies/tpu_spawn.py
+++ src/pytorch_lightning/strategies/tpu_spawn.py
@@ -37,7 +37,6 @@ from pytorch_lightning.utilities.distributed import ReduceOp
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.optimizer import optimizers_to_device
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
-from pytorch_lightning.utilities.seed import reset_seed
 from pytorch_lightning.utilities.types import _PATH, STEP_OUTPUT
 
 if _TPU_AVAILABLE:
