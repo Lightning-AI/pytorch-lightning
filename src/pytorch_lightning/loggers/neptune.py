800cf0b3ffb1b8dc94673cf5abe8e91703a59cd7
Clément POIRET <poiret.clement@outlook.fr>
2022-08-03 16:15:21 +0200

    Fix NeptuneLogger unusable after pytorch-lightning 1.7.0 (#13988)
    Co-authored-by: rohitgr7 <rohitgr1998@gmail.com>
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- src/pytorch_lightning/loggers/neptune.py
+++ src/pytorch_lightning/loggers/neptune.py
@@ -38,7 +38,7 @@ from pytorch_lightning.utilities.logger import _add_prefix, _convert_params, _sa
 from pytorch_lightning.utilities.model_summary import ModelSummary
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
-_NEPTUNE_AVAILABLE = _RequirementAvailable("neptune")
+_NEPTUNE_AVAILABLE = _RequirementAvailable("neptune-client")
 _NEPTUNE_GREATER_EQUAL_0_9 = _RequirementAvailable("neptune-client>=0.9.0")
 
 
