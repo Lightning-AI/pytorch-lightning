a380a2b4f1843f8add5a543df007610eee81270b
awaelchli <aedu.waelchli@gmail.com>
2022-08-10 01:52:19 +0200

    try weird stuff

--- tests/tests_pytorch/run_standalone_tasks.sh
+++ tests/tests_pytorch/run_standalone_tasks.sh
@@ -42,5 +42,5 @@ MASTER_ADDR="localhost" MASTER_PORT=1234 LOCAL_RANK=0 python examples/convert_fr
 
 # test that ddp can launched as a module (-m option)
 echo "Running ddp example as module"
-export PYTHONPATH="${PYTHONPATH}:$(pwd)/examples/convert_from_pt_to_pl"
+export PYTHONPATH="${PYTHONPATH}:$(python -c "import pytorch_lightning; print(pytorch_lightning.__file__)")"
 python -m examples.convert_from_pt_to_pl.image_classifier_5_lightning_datamodule ${args}
