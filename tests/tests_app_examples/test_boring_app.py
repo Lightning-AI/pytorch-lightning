2745ae6319015ba6d773b35ee6140a922742abbf
thomas chaton <thomas@grid.ai>
2022-08-10 18:20:54 +0200

    update

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -1,8 +1,10 @@
 import os
 
 import pytest
+from click.testing import CliRunner
 from tests_app import _PROJECT_ROOT
 
+from lightning_app.cli.lightning_cli import logs
 from lightning_app.testing.testing import run_app_in_cloud, wait_for
 
 
@@ -22,3 +24,11 @@ def test_boring_app_example_cloud() -> None:
                 return True
 
         wait_for(view_page, check_hello_there)
+
+        runner = CliRunner()
+        result = runner.invoke(logs, [name])
+        lines = result.output.splitlines()
+
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert any("http://0.0.0.0:1111" in line for line in lines)
