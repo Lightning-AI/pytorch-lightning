94cc189d0f66f2a08f5666917ed83e3299107ef2
Marc Skov Madsen <masma@orsted.com>
2022-08-13 04:59:12 +0000

    remove duplicate code

--- src/lightning_app/frontend/streamlit_base.py
+++ src/lightning_app/frontend/streamlit_base.py
@@ -9,6 +9,7 @@ from typing import Callable, Union
 from lightning_app.core.flow import LightningFlow
 from lightning_app.utilities.app_helpers import StreamLitStatePlugin
 from lightning_app.utilities.state import AppState
+from lightning_app.frontend.utilities.utils import _reduce_to_flow_scope
 
 app_state = AppState(plugin=StreamLitStatePlugin())
 
@@ -20,19 +21,9 @@ def _get_render_fn_from_environment() -> Callable:
     return getattr(module, render_fn_name)
 
 
-def _app_state_to_flow_scope(state: AppState, flow: Union[str, LightningFlow]) -> AppState:
-    """Returns a new AppState with the scope reduced to the given flow, as if the given flow as the root."""
-    flow_name = flow.name if isinstance(flow, LightningFlow) else flow
-    flow_name_parts = flow_name.split(".")[1:]  # exclude root
-    flow_state = state
-    for part in flow_name_parts:
-        flow_state = getattr(flow_state, part)
-    return flow_state
-
-
 def main():
     # Fetch the information of which flow attaches to this streamlit instance
-    flow_state = _app_state_to_flow_scope(app_state, flow=os.environ["LIGHTNING_FLOW_NAME"])
+    flow_state = _reduce_to_flow_scope(app_state, flow=os.environ["LIGHTNING_FLOW_NAME"])
 
     # Call the provided render function.
     # Pass it the state, scoped to the current flow.
