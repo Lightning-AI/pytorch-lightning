1967a9810c27c382b55b00b388a076402d2841e3
awaelchli <aedu.waelchli@gmail.com>
2022-08-18 19:11:37 +0200

    simplify logic

--- src/pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ src/pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -562,23 +562,11 @@ class AcceleratorConnector:
     def _choose_and_init_cluster_environment(self) -> ClusterEnvironment:
         if isinstance(self._cluster_environment_flag, ClusterEnvironment):
             return self._cluster_environment_flag
-        if self._is_slurm_managing_tasks():
-            rank_zero_info("Multiprocessing is handled by SLURM.")
-            return SLURMEnvironment()
-        for env_type in (BaguaEnvironment, TorchElasticEnvironment, KubeflowEnvironment, LSFEnvironment):
+        for env_type in (SLURMEnvironment, BaguaEnvironment, TorchElasticEnvironment, KubeflowEnvironment, LSFEnvironment):
             if env_type.detect():
                 # Ignore type error because it is a false positive: https://github.com/python/mypy/issues/13044
                 return env_type()  # type: ignore[abstract]
         return LightningEnvironment()
-
-    def _is_slurm_managing_tasks(self) -> bool:
-        """used by choosing cluster enviroment."""
-        if not SLURMEnvironment.detect() or SLURMEnvironment.job_name() == "bash":
-            return False
-
-        total_requested_devices = len(self._parallel_devices) * self._num_nodes_flag
-        num_slurm_tasks = int(os.environ["SLURM_NTASKS"], 0)
-        return num_slurm_tasks == total_requested_devices
 
     def _choose_strategy(self) -> Union[Strategy, str]:
         if self._accelerator_flag == "ipu":
