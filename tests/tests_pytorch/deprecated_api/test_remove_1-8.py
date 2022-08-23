fc249497620393241ca32eea317c47bddf8f0d6d
Carlos Mocholí <carlossmocholi@gmail.com>
2022-08-24 00:59:33 +0200

    Remove support for the deprecated torchtext legacy

--- tests/tests_pytorch/deprecated_api/test_remove_1-8.py
+++ tests/tests_pytorch/deprecated_api/test_remove_1-8.py
@@ -35,21 +35,9 @@ from pytorch_lightning.strategies.ipu import LightningIPUModule
 from pytorch_lightning.trainer.configuration_validator import _check_datamodule_checkpoint_hooks
 from pytorch_lightning.trainer.states import RunningStage
 from pytorch_lightning.utilities import device_parser
-from pytorch_lightning.utilities.apply_func import move_data_to_device
-from pytorch_lightning.utilities.imports import _TORCHTEXT_LEGACY
 from pytorch_lightning.utilities.rank_zero import rank_zero_only, rank_zero_warn
 from tests_pytorch.deprecated_api import no_deprecated_call
 from tests_pytorch.helpers.runif import RunIf
-from tests_pytorch.helpers.torchtext_utils import get_dummy_torchtext_data_iterator
-
-
-@pytest.mark.skipif(not _TORCHTEXT_LEGACY, reason="torchtext.legacy is deprecated.")
-def test_v1_8_0_deprecated_torchtext_batch():
-
-    with pytest.deprecated_call(match="is deprecated and Lightning will remove support for it in v1.8"):
-        data_iterator, _ = get_dummy_torchtext_data_iterator(num_samples=3, batch_size=3)
-        batch = next(iter(data_iterator))
-        _ = move_data_to_device(batch=batch, device=torch.device("cpu"))
 
 
 def test_v1_8_0_on_init_start_end(tmpdir):
