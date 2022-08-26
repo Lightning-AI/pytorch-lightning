a0c6438051169de8b42bf2321b7bf0fc9f0f788f
awaelchli <aedu.waelchli@gmail.com>
2022-08-26 14:11:58 +0200

    flake8

--- tests/tests_pytorch/trainer/connectors/test_checkpoint_connector.py
+++ tests/tests_pytorch/trainer/connectors/test_checkpoint_connector.py
@@ -12,16 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-from unittest import mock
 from unittest.mock import Mock
 
-import pytest
 import torch
 
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.demos.boring_classes import BoringModel
-from pytorch_lightning.plugins.environments import SLURMEnvironment
 from pytorch_lightning.trainer.states import TrainerFn
 
 
