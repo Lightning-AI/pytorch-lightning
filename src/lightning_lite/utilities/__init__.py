4980dde9ce3250ae3566b5cb2cc54897d8309121
awaelchli <aedu.waelchli@gmail.com>
2022-09-02 19:04:18 +0200

    manually apply e0c2c3e677d141594cdd799050942b10908c9a97

--- src/lightning_lite/utilities/__init__.py
+++ src/lightning_lite/utilities/__init__.py
@@ -17,9 +17,6 @@ from lightning_lite.utilities.apply_func import move_data_to_device  # noqa: F40
 from lightning_lite.utilities.distributed import AllGatherGrad  # noqa: F401
 from lightning_lite.utilities.enums import _AcceleratorType, _StrategyType, AMPType, LightningEnum  # noqa: F401
 from lightning_lite.utilities.imports import (  # noqa: F401
-    _FAIRSCALE_FULLY_SHARDED_AVAILABLE,
-    _FAIRSCALE_OSS_FP16_BROADCAST_AVAILABLE,
-    _GROUP_AVAILABLE,
     _HIVEMIND_AVAILABLE,
     _HOROVOD_AVAILABLE,
     _HPU_AVAILABLE,
