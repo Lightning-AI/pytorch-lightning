c31b5dcb2afd44d7bec7add67d5118297e563585
awaelchli <aedu.waelchli@gmail.com>
2022-09-03 14:16:28 +0200

    Deprecate pl/core/mixins/device_dtype_mixin and update imports (#14511)

--- src/pytorch_lightning/trainer/connectors/logger_connector/result.py
+++ src/pytorch_lightning/trainer/connectors/logger_connector/result.py
@@ -20,7 +20,7 @@ from torch import Tensor
 from torchmetrics import Metric
 from typing_extensions import TypedDict
 
-from pytorch_lightning.core.mixins import DeviceDtypeModuleMixin
+from lightning_lite.utilities.device_dtype_mixin import DeviceDtypeModuleMixin
 from pytorch_lightning.utilities.apply_func import apply_to_collection, apply_to_collections, move_data_to_device
 from pytorch_lightning.utilities.data import extract_batch_size
 from pytorch_lightning.utilities.distributed import distributed_available
