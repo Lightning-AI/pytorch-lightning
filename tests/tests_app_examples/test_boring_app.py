5cab45f38e52c72902bd9819dee1bec6f1946410
manskx <ahmed.mansy156@gmail.com>
2022-09-07 15:03:42 +0200

    debug tests

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -18,8 +18,8 @@ def test_boring_app_example_cloud() -> None:
     ):
 
         def check_hello_there(*_, **__):
-            locator = view_page.frame_locator("iframe").locator('ul:has-text("Hello there!")')
-            locator.wait_for(timeout=20 * 1000)
+            locator = view_page.frame_locator("iframe").locator('text="Hello there!"')
+            locator.wait_for(timeout=30 * 1000)
             if len(locator.all_text_contents()):
                 return True
 
