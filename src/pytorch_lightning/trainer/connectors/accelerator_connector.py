375248cbaeaf318a0b31cf656354a1cca8530ba7
awaelchli <aedu.waelchli@gmail.com>
2022-09-08 12:29:13 +0200

    update

--- src/pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ src/pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -524,7 +524,9 @@ class AcceleratorConnector:
 
         if not self.accelerator.is_available():
             available_accelerator = [
-                acc_str for acc_str in self._accelerator_types if AcceleratorRegistry[acc_str].is_available()
+                acc_str
+                for acc_str in self._accelerator_types
+                if AcceleratorRegistry[acc_str]["accelerator"].is_available()
             ]
             raise MisconfigurationException(
                 f"{self.accelerator.__class__.__qualname__} can not run on your system"
