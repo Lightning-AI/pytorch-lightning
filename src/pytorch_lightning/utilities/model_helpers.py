e7c411387a73fcb271de0ffe4f79bb9fd114fe6a
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-09 14:10:13 +0200

    Integrate lightning_utilities `is_overridden`

--- src/pytorch_lightning/utilities/model_helpers.py
+++ src/pytorch_lightning/utilities/model_helpers.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from functools import partial
 from typing import Optional, Type
-from unittest.mock import Mock
 
 import pytorch_lightning as pl
 
@@ -22,7 +20,6 @@ def is_overridden(method_name: str, instance: Optional[object] = None, parent: O
     if instance is None:
         # if `self.lightning_module` was passed as instance, it can be `None`
         return False
-
     if parent is None:
         if isinstance(instance, pl.LightningModule):
             parent = pl.LightningModule
@@ -32,25 +29,6 @@ def is_overridden(method_name: str, instance: Optional[object] = None, parent: O
             parent = pl.Callback
         if parent is None:
             raise ValueError("Expected a parent")
+    from lightning_utilities.core.overrides import is_overridden
 
-    instance_attr = getattr(instance, method_name, None)
-    if instance_attr is None:
-        return False
-    # `functools.wraps()` support
-    if hasattr(instance_attr, "__wrapped__"):
-        instance_attr = instance_attr.__wrapped__
-    # `Mock(wraps=...)` support
-    if isinstance(instance_attr, Mock):
-        # access the wrapped function
-        instance_attr = instance_attr._mock_wraps
-    # `partial` support
-    elif isinstance(instance_attr, partial):
-        instance_attr = instance_attr.func
-    if instance_attr is None:
-        return False
-
-    parent_attr = getattr(parent, method_name, None)
-    if parent_attr is None:
-        raise ValueError("The parent should define the method")
-
-    return instance_attr.__code__ != parent_attr.__code__
+    return is_overridden(method_name, instance, parent)
