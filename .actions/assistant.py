985b31b0feddf5eeac645a3af841cc1176dffc7f
Jirka Borovec <Borda@users.noreply.github.com>
2022-09-09 20:38:03 +0200

    CI: hotfix last version (#14627)
    LooseVersion was not correctly evaluation RC and set it as last even though the full release is out...

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -1,6 +1,7 @@
 import datetime
 import glob
 import json
+import logging
 import os
 import re
 import shutil
@@ -128,8 +129,10 @@ class AssistantCLI:
         url = f"https://pypi.org/pypi/{PACKAGE_MAPPING[package]}/json"
         data = json.load(urlopen(Request(url)))
         if not version:
-            pypi_vers = pypi_versions(PACKAGE_MAPPING[package], drop_pre=False)
-            version = pypi_vers[-1]
+            versions = list(data["releases"].keys())
+            versions = sorted(versions, key=lambda x: version_parse(x))
+            logging.debug(f"Available versions: {versions}")
+            version = versions[-1]
         releases = list(filter(lambda r: r["packagetype"] == "sdist", data["releases"][version]))
         assert releases, f"Missing 'sdist' for this package/version aka {package}/{version}"
         release = releases[0]
@@ -137,6 +140,7 @@ class AssistantCLI:
         pkg_file = os.path.basename(pkg_url)
         pkg_path = os.path.join(folder, pkg_file)
         os.makedirs(folder, exist_ok=True)
+        print(f"downloading: {pkg_url}")
         request.urlretrieve(pkg_url, pkg_path)
 
     @staticmethod
