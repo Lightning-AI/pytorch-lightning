f917cb4bd7f36269a4a139507b0e5a064e8d7720
awaelchli <aedu.waelchli@gmail.com>
2022-09-13 00:30:51 +0200

    add spawn family

--- src/lightning_lite/strategies/__init__.py
+++ src/lightning_lite/strategies/__init__.py
@@ -11,10 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from lightning_lite.strategies.ddp_spawn import DDPSpawnStrategy  # noqa: F401
 from lightning_lite.strategies.parallel import ParallelStrategy  # noqa: F401
 from lightning_lite.strategies.registry import _StrategyRegistry, call_register_strategies
+from lightning_lite.strategies.sharded_spawn import DDPSpawnShardedStrategy  # noqa: F401
 from lightning_lite.strategies.single_device import SingleDeviceStrategy  # noqa: F401
 from lightning_lite.strategies.strategy import Strategy  # noqa: F401
+from lightning_lite.strategies.tpu_spawn import TPUSpawnStrategy  # noqa: F401
 
 _STRATEGIES_BASE_MODULE = "lightning_lite.strategies"
 STRATEGY_REGISTRY = _StrategyRegistry()
