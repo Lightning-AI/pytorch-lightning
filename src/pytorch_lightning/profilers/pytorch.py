56fe5eb751a03e8fb123959745db9804ccb8200e
otaj <ota@lightning.ai>
2022-09-13 16:10:25 +0200

    make mypy stop complaining

--- src/pytorch_lightning/profilers/pytorch.py
+++ src/pytorch_lightning/profilers/pytorch.py
@@ -524,7 +524,7 @@ class PyTorchProfiler(Profiler):
             self._register.__exit__(None, None, None)
             self._register = None
 
-    def teardown(self, stage: str) -> None:
+    def teardown(self, stage: Optional[str]) -> None:
         self._delete_profilers()
 
         for k in list(self._recording_map):
