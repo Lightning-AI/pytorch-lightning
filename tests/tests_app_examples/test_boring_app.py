aee5c02ebb012881826daf98c7aa68d1c43d9124
thomas chaton <thomas@grid.ai>
2022-09-13 16:50:36 +0200

    (hot fix) Resolve Boring App (#14684)
    * resolve_boring_app
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -13,7 +13,7 @@ def test_boring_app_example_cloud() -> None:
     with run_app_in_cloud(
         os.path.join(_PROJECT_ROOT, "examples/app_boring/"),
         app_name="app_dynamic.py",
-        debug=True,
+        debug=False,
     ) as (
         _,
         view_page,
@@ -23,14 +23,10 @@ def test_boring_app_example_cloud() -> None:
 
         def check_hello_there(*_, **__):
             locator = view_page.frame_locator("iframe").locator('ul:has-text("Hello there!")')
-            locator.wait_for(timeout=3 * 1000)
             if len(locator.all_text_contents()):
                 return True
 
         wait_for(view_page, check_hello_there)
-
-        for _ in fetch_logs():
-            pass
 
         runner = CliRunner()
         result = runner.invoke(logs, [name])
@@ -39,3 +35,4 @@ def test_boring_app_example_cloud() -> None:
         assert result.exit_code == 0
         assert result.exception is None
         assert any("http://0.0.0.0:8080" in line for line in lines)
+        print("Succeeded App!")
