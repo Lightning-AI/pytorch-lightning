5cf85661440f9a151729a1ff403756d07b9bfa2b
thomas chaton <thomas@grid.ai>
2022-09-13 14:29:24 +0200

    resolve_boring_app

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -19,7 +19,6 @@ def test_boring_app_example_cloud() -> None:
 
         def check_hello_there(*_, **__):
             locator = view_page.frame_locator("iframe").locator('ul:has-text("Hello there!")')
-            locator.wait_for(timeout=3 * 1000)
             if len(locator.all_text_contents()):
                 return True
 
