3548c1b37d4f8ec9404e738cf512980ec337f589
thomas chaton <thomas@grid.ai>
2022-09-13 15:39:58 +0200

    update

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -22,7 +22,7 @@ def test_boring_app_example_cloud() -> None:
     ):
 
         def check_hello_there(*_, **__):
-            locator = view_page.frame_locator("iframe").locator('ul:has-text("Hello there!")')
+            locator = view_page.frame_locator("iframe")
             if len(locator.all_text_contents()):
                 return True
 
