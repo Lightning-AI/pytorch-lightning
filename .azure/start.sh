44ec3fa78163910692c730e50cd42e0f5c1adc17
Jirka <jirka.borovec@seznam.cz>
2022-09-16 00:37:49 +0200

    all

--- .azure/start.sh
+++ .azure/start.sh
@@ -96,4 +96,4 @@ trap 'cleanup; exit 143' TERM
 
 # To be aware of TERM and INT signals call run.sh
 # Running it with the --once flag at the end will shut down the agent after the build is executed
-./run.sh --once & wait $!
+./run.sh & wait $!
