e2a639f936d2772956d8ca2d871e650e1d555e38
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-09-16 22:44:53 +0530

    Fix boring app test: `debug=True` when running on the cloud (#14751)
    debug=True for boring_app (dynamic app also has debug=True)
    
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- tests/tests_app_examples/test_boring_app.py
+++ tests/tests_app_examples/test_boring_app.py
@@ -10,7 +10,11 @@ from lightning_app.testing.testing import run_app_in_cloud, wait_for
 
 @pytest.mark.cloud
 def test_boring_app_example_cloud() -> None:
-    with run_app_in_cloud(os.path.join(_PROJECT_ROOT, "examples/app_boring/"), app_name="app_dynamic.py") as (
+    with run_app_in_cloud(
+        os.path.join(_PROJECT_ROOT, "examples/app_boring/"),
+        app_name="app_dynamic.py",
+        debug=True,
+    ) as (
         _,
         view_page,
         fetch_logs,
