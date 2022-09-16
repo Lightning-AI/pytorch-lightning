7de881d223b3a956ee3e7a29734e3a93afd38c96
otaj <ota@lightning.ai>
2022-09-16 14:16:13 +0200

    Use valid pypi versions for install in assistant

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -124,10 +124,8 @@ class AssistantCLI:
         url = f"https://pypi.org/pypi/{PACKAGE_MAPPING[package]}/json"
         data = json.load(urlopen(Request(url)))
         if not version:
-            versions = list(data["releases"].keys())
-            versions = sorted(versions, key=lambda x: version_parse(x))
-            logging.debug(f"Available versions: {versions}")
-            version = versions[-1]
+            pypi_vers = pypi_versions(PACKAGE_MAPPING[package])
+            version = pypi_vers[-1]
         releases = list(filter(lambda r: r["packagetype"] == "sdist", data["releases"][version]))
         assert releases, f"Missing 'sdist' for this package/version aka {package}/{version}"
         release = releases[0]
