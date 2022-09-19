e3df9ca45ceb82c808c62387c9ac4f4abc2dbbe2
awaelchli <aedu.waelchli@gmail.com>
2022-09-19 22:15:49 +0200

    remove mixed precision class

--- src/lightning_lite/plugins/__init__.py
+++ src/lightning_lite/plugins/__init__.py
@@ -18,7 +18,6 @@ from lightning_lite.plugins.io.torch_plugin import TorchCheckpointIO
 from lightning_lite.plugins.io.xla_plugin import XLACheckpointIO
 from lightning_lite.plugins.precision.deepspeed import DeepSpeedPrecision
 from lightning_lite.plugins.precision.double import DoublePrecision
-from lightning_lite.plugins.precision.mixed import MixedPrecision
 from lightning_lite.plugins.precision.native_amp import NativeMixedPrecision
 from lightning_lite.plugins.precision.precision import Precision
 from lightning_lite.plugins.precision.tpu import TPUPrecision
--- src/lightning_lite/plugins/precision/__init__.py
+++ src/lightning_lite/plugins/precision/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from lightning_lite.plugins.precision.deepspeed import DeepSpeedPrecision
-from lightning_lite.plugins.precision.mixed import MixedPrecision
 from lightning_lite.plugins.precision.native_amp import NativeMixedPrecision
 from lightning_lite.plugins.precision.precision import Precision
 from lightning_lite.plugins.precision.tpu import TPUPrecision
@@ -20,7 +19,6 @@ from lightning_lite.plugins.precision.tpu_bf16 import TPUBf16Precision
 
 __all__ = [
     "DeepSpeedPrecision",
-    "MixedPrecision",
     "NativeMixedPrecision",
     "Precision",
     "TPUPrecision",
