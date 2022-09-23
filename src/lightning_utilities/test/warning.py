88bd877506353d7329e24172634c20ca2b50ff66
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-23 22:12:18 +0200

    Add `lightning_utilities.test.warning.no_warning_call` (#55)

---
+++ src/lightning_utilities/test/warning.py
@@ -0,0 +1,14 @@
+import re
+import warnings
+from contextlib import contextmanager
+from typing import Generator, Optional, Type
+
+
+@contextmanager
+def no_warning_call(expected_warning: Type[Warning] = Warning, match: Optional[str] = None) -> Generator:
+    with warnings.catch_warnings(record=True) as record:
+        yield
+
+    for w in record:
+        if issubclass(w.category, expected_warning) and (match is None or re.compile(match).search(str(w.message))):
+            raise AssertionError(f"`{expected_warning.__name__}` was raised: {w.message!r}")
---
+++ tests/unittests/test/test_warnings.py
@@ -0,0 +1,25 @@
+import warnings
+from re import escape
+
+import pytest
+
+from lightning_utilities.test.warning import no_warning_call
+
+
+def test_no_warning_call():
+    with no_warning_call():
+        ...
+
+    with pytest.raises(AssertionError, match=escape("`Warning` was raised: UserWarning('foo')")):
+        with no_warning_call():
+            warnings.warn("foo")
+
+    with no_warning_call(DeprecationWarning):
+        warnings.warn("foo")
+
+    class MyDeprecationWarning(DeprecationWarning):
+        ...
+
+    with pytest.raises(AssertionError, match=escape("`DeprecationWarning` was raised: MyDeprecationWarning('bar')")):
+        with pytest.warns(DeprecationWarning), no_warning_call(DeprecationWarning):
+            warnings.warn("bar", category=MyDeprecationWarning)
