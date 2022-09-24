048c5ba6643492a73dd02fdcf55f580f1b1d855e
Kishan Savant <ksavant577@gmail.com>
2022-09-24 18:38:14 +0530

    Remove deprecated trainer.should_rank_save_checkpoint

--- tests/tests_pytorch/deprecated_api/test_remove_1-8.py
+++ tests/tests_pytorch/deprecated_api/test_remove_1-8.py
@@ -91,14 +91,6 @@ def test_v1_8_0_trainer_ckpt_path_attributes(fn_prefix: str):
         setattr(trainer, test_attr, "v")
 
 
-def test_v1_8_0_deprecated_trainer_should_rank_save_checkpoint(tmpdir):
-    trainer = Trainer()
-    with pytest.deprecated_call(
-        match=r"`Trainer.should_rank_save_checkpoint` is deprecated in v1.6 and will be removed in v1.8."
-    ):
-        _ = trainer.should_rank_save_checkpoint
-
-
 def test_v1_8_0_trainer_optimizers_mixin():
     trainer = Trainer()
     model = BoringModel()
