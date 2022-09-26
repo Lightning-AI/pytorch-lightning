35419b5bf90bb516c7234f5efe66a12365e71724
Kishan Savant <66986430+NeoKish@users.noreply.github.com>
2022-09-26 16:28:19 +0530

    Removed the deprecated trainer_data_loading_mixin (#14888)

--- tests/tests_pytorch/deprecated_api/test_remove_1-8.py
+++ tests/tests_pytorch/deprecated_api/test_remove_1-8.py
@@ -22,7 +22,6 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.demos.boring_classes import BoringDataModule, BoringModel
 from pytorch_lightning.strategies.ipu import LightningIPUModule
 from pytorch_lightning.trainer.configuration_validator import _check_datamodule_checkpoint_hooks
-from pytorch_lightning.trainer.states import RunningStage
 
 
 def test_v1_8_0_on_init_start_end(tmpdir):
@@ -91,22 +90,6 @@ def test_v1_8_0_trainer_optimizers_mixin():
         "v1.8."
     ):
         trainer.convert_to_lightning_optimizers()
-
-
-def test_v1_8_0_deprecate_trainer_data_loading_mixin():
-    trainer = Trainer(max_epochs=1)
-    model = BoringModel()
-    dm = BoringDataModule()
-    trainer.fit(model, datamodule=dm)
-
-    with pytest.deprecated_call(
-        match=r"`TrainerDataLoadingMixin.prepare_dataloader` was deprecated in v1.6 and will be removed in v1.8.",
-    ):
-        trainer.prepare_dataloader(dataloader=model.train_dataloader, shuffle=False)
-    with pytest.deprecated_call(
-        match=r"`TrainerDataLoadingMixin.request_dataloader` was deprecated in v1.6 and will be removed in v1.8.",
-    ):
-        trainer.request_dataloader(stage=RunningStage.TRAINING)
 
 
 def test_v_1_8_0_deprecated_device_stats_monitor_prefix_metric_keys():
