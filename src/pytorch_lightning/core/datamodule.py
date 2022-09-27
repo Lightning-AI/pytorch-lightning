1213f0b76cf6bd310b2052c66a14a1d263b35f6b
awaelchli <aedu.waelchli@gmail.com>
2022-09-27 22:45:59 +0200

    remove unused import

--- src/pytorch_lightning/core/datamodule.py
+++ src/pytorch_lightning/core/datamodule.py
@@ -20,7 +20,7 @@ from torch.utils.data import DataLoader, Dataset, IterableDataset
 
 import pytorch_lightning as pl
 from lightning_lite.utilities.types import _PATH
-from pytorch_lightning.core.hooks import CheckpointHooks, DataHooks
+from pytorch_lightning.core.hooks import DataHooks
 from pytorch_lightning.core.mixins import HyperparametersMixin
 from pytorch_lightning.core.saving import _load_from_checkpoint
 from pytorch_lightning.utilities.argparse import (
