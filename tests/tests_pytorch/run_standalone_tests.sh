905dc62507d96278ef7cb210e2833a59edec086a
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-27 19:30:06 +0200

    Revert "DEBUG - skip to standalone"
    This reverts commit 98aef0fbc1feaa32110286c560e4bfb24afdb1f4.

--- tests/tests_pytorch/run_standalone_tests.sh
+++ tests/tests_pytorch/run_standalone_tests.sh
@@ -17,13 +17,13 @@ set -e
 
 # Batch size for testing: Determines how many standalone test invocations run in parallel
 # It can be set through the env variable PL_STANDALONE_TESTS_BATCH_SIZE and defaults to 6 if not set
-test_batch_size="${PL_STANDALONE_TESTS_BATCH_SIZE:-1}"
+test_batch_size="${PL_STANDALONE_TESTS_BATCH_SIZE:-6}"
 source="${PL_STANDALONE_TESTS_SOURCE}"
 
 # this environment variable allows special tests to run
 export PL_RUN_STANDALONE_TESTS=1
 # python arguments
-defaults="-m pytest --no-header"
+defaults="-m coverage run --source $source --append -m pytest --no-header"
 
 # find tests marked as `@RunIf(standalone=True)`. done manually instead of with pytest because it is faster
 grep_output=$(grep --recursive --word-regexp . --regexp 'standalone=True' --include '*.py')
