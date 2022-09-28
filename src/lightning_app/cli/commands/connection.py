fb04f51366922fc2d86a944f12b9114f485ea5aa
Krishna Kalyan <krishnakalyan3@gmail.com>
2022-09-28 08:46:48 +0200

    Update src/lightning_app/cli/commands/connection.py
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- src/lightning_app/cli/commands/connection.py
+++ src/lightning_app/cli/commands/connection.py
@@ -33,7 +33,7 @@ def connect(app_name_or_id: str, yes: bool = False):
             else:
                 click.echo(
                     f"You are already connected to the cloud Lightning App: {app_name_or_id}."
-                    " Please run `lightning disconnect`"
+                    " HINT: Use `lightning disconnect` to return back to the main Lighting CLI."
                 )
         else:
             disconnect()
