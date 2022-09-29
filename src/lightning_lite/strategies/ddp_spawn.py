94253fe57f7abab3afdc99b1da899a260f6b2990
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-09-29 21:38:09 +0200

    debug

--- src/lightning_lite/strategies/ddp_spawn.py
+++ src/lightning_lite/strategies/ddp_spawn.py
@@ -11,11 +11,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from datetime import timedelta
+from typing import Optional, List, Literal, Any
 
+import torch
+from torch.distributed.constants import default_pg_timeout
+
+from lightning_lite.accelerators import Accelerator
+from lightning_lite.plugins import ClusterEnvironment, CheckpointIO, Precision
 from lightning_lite.strategies import DDPStrategy
 
 
 class DDPSpawnStrategy(DDPStrategy):
     """Spawns processes using the :func:`torch.multiprocessing.spawn` method and joins processes after training
     finishes."""
-    pass
+
+    def __init__(
+        self,
+        accelerator: Optional[Accelerator] = None,
+        parallel_devices: Optional[List[torch.device]] = None,
+        cluster_environment: Optional[ClusterEnvironment] = None,
+        checkpoint_io: Optional[CheckpointIO] = None,
+        precision_plugin: Optional[Precision] = None,
+        process_group_backend: Optional[str] = None,
+        timeout: Optional[timedelta] = default_pg_timeout,
+        start_method: Literal["popen", "spawn", "fork", "forkserver"] = "spawn",  # different default than in ddp
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            accelerator=accelerator,
+            parallel_devices=parallel_devices,
+            cluster_environment=cluster_environment,
+            checkpoint_io=checkpoint_io,
+            precision_plugin=precision_plugin,
+            process_group_backend=process_group_backend,
+            timeout=timeout,
+            start_method=start_method,
+        )
