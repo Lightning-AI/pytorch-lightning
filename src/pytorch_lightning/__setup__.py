27aaa9604b9a5a3e591c4328e66e17d9d86f64ee
rohitgr7 <rohitgr1998@gmail.com>
2022-09-30 15:12:36 +0530

    run gpu ci

--- src/pytorch_lightning/__setup__.py
+++ src/pytorch_lightning/__setup__.py
@@ -82,9 +82,6 @@ def _setup_args(**__: Any) -> Dict[str, Any]:
         url=_about.__homepage__,
         download_url="https://github.com/Lightning-AI/lightning",
         license=_about.__license__,
-        dependency_links=[
-            "https://release.colossalai.org",
-        ],
         packages=find_packages(
             where="src",
             include=[
