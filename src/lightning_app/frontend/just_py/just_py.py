d611fb4e6ae19e82a0ebf9643832221d6d88f81e
thomas chaton <thomas@grid.ai>
2022-10-05 13:44:18 +0200

    update

--- src/lightning_app/frontend/just_py/just_py.py
+++ src/lightning_app/frontend/just_py/just_py.py
@@ -11,6 +11,64 @@ from lightning_app.utilities.log import get_frontend_logfile
 
 
 class JustPyFrontend(Frontend):
+    """A frontend for wrapping JustPy code in your LightingFlow.
+
+    Return this in your `LightningFlow.configure_layout()` method if you wish to build the UI with ``justpyt``.
+    To use this frontend, you must first install the `justpyt` package (if running locally):
+
+    .. code-block:: bash
+
+        pip install justpy
+
+    Arguments:
+        render_fn: A function that contains your justpyt code. This function must accept exactly one argument, the
+            `AppState` object which you can use to access variables in your flow (see example below).
+
+    Example:
+
+        In your LightningFlow, override the method `configure_layout`:
+
+        .. code-block:: python
+
+            from typing import Callable
+            from lightning import LightningApp, LightningFlow
+            from lightning.app.frontend import JustPyFrontend
+
+
+            class Flow(LightningFlow):
+                def __init__(self):
+                    super().__init__()
+                    self.counter = 0
+
+                def run(self):
+                    print(self.counter)
+
+                def configure_layout(self):
+                    return JustPyFrontend(render_fn=render_fn)
+
+
+            def render_fn(get_state: Callable) -> Callable:
+                import justpy as jp
+
+                def my_click(self, *_):
+                    state = get_state()
+                    old_counter = state.counter
+                    state.counter += 1
+                    self.text = f"Click Me ! Old Counter: {old_counter} New Counter: {state.counter}"
+
+                def website():
+                    wp = jp.WebPage()
+                    d = jp.Div(text="Hello ! Click Me!")
+                    d.on("click", my_click)
+                    wp.add(d)
+                    return wp
+
+                return website
+
+
+            app = LightningApp(Flow())
+    """
+
     def __init__(self, render_fn: Callable) -> None:
         super().__init__()
 
