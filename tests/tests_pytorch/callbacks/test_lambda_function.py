7c2c526c8bfddf2419c8d029befacf225b43fe91
awaelchli <aedu.waelchli@gmail.com>
2022-10-08 20:12:24 +0200

    fix tests

--- tests/tests_pytorch/callbacks/test_lambda_function.py
+++ tests/tests_pytorch/callbacks/test_lambda_function.py
@@ -49,7 +49,7 @@ def test_lambda_call(tmpdir):
         callbacks=[LambdaCallback(**hooks_args)],
     )
     with pytest.deprecated_call(
-        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v1.8"
+        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v2.0"
     ):
         trainer.fit(model)
     ckpt_path = trainer.checkpoint_callback.best_model_path
@@ -65,15 +65,15 @@ def test_lambda_call(tmpdir):
         callbacks=[LambdaCallback(**hooks_args)],
     )
     with pytest.deprecated_call(
-        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v1.8"
+        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v2.0"
     ):
         trainer.fit(model, ckpt_path=ckpt_path)
     with pytest.deprecated_call(
-        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v1.8"
+        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v2.0"
     ):
         trainer.test(model)
     with pytest.deprecated_call(
-        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v1.8"
+        match="`LambdaCallback.on_load_checkpoint` will change its signature and behavior in v2.0"
     ):
         trainer.predict(model)
 
