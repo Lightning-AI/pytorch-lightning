2730240f88671b61a8a3e0b8d881e5a628d1f358
Carlos Mocholí <carlossmocholi@gmail.com>
2022-10-11 01:42:18 +0200

    Sort test

--- tests/tests_pytorch/graveyard/test_training_type.py
+++ tests/tests_pytorch/graveyard/test_training_type.py
@@ -41,77 +41,49 @@ def test_removed_training_type_plugin_classes(name, import_path):
 
 def test_removed_training_type_plugin_classes_inner_import():
     from pytorch_lightning.plugins.training_type.ddp import DDPPlugin
+    from pytorch_lightning.plugins.training_type.ddp2 import DDP2Plugin
+    from pytorch_lightning.plugins.training_type.ddp_spawn import DDPSpawnPlugin
+    from pytorch_lightning.plugins.training_type.deepspeed import DeepSpeedPlugin
+    from pytorch_lightning.plugins.training_type.dp import DataParallelPlugin
+    from pytorch_lightning.plugins.training_type.fully_sharded import DDPFullyShardedPlugin
+    from pytorch_lightning.plugins.training_type.horovod import HorovodPlugin
+    from pytorch_lightning.plugins.training_type.ipu import IPUPlugin
+    from pytorch_lightning.plugins.training_type.parallel import ParallelPlugin
+    from pytorch_lightning.plugins.training_type.sharded import DDPShardedPlugin
+    from pytorch_lightning.plugins.training_type.sharded_spawn import DDPSpawnShardedPlugin
+    from pytorch_lightning.plugins.training_type.single_device import SingleDevicePlugin
+    from pytorch_lightning.plugins.training_type.single_tpu import SingleTPUPlugin
+    from pytorch_lightning.plugins.training_type.tpu_spawn import TPUSpawnPlugin
+    from pytorch_lightning.plugins.training_type.training_type_plugin import TrainingTypePlugin
 
     with pytest.raises(RuntimeError, match="DDPPlugin` class was removed"):
         DDPPlugin()
-
-    from pytorch_lightning.plugins.training_type.ddp2 import DDP2Plugin
-
     with pytest.raises(RuntimeError, match="DDP2Plugin` class was removed"):
         DDP2Plugin()
-
-    from pytorch_lightning.plugins.training_type.ddp_spawn import DDP2Plugin
-
-    with pytest.raises(RuntimeError, match="DDP2Plugin` class was removed"):
-        DDP2Plugin()
-
-    from pytorch_lightning.plugins.training_type.deepspeed import DeepSpeedPlugin
-
+    with pytest.raises(RuntimeError, match="DDPSpawnPlugin` class was removed"):
+        DDPSpawnPlugin()
     with pytest.raises(RuntimeError, match="DeepSpeedPlugin` class was removed"):
         DeepSpeedPlugin()
-
-    from pytorch_lightning.plugins.training_type.dp import DataParallelPlugin
-
     with pytest.raises(RuntimeError, match="DataParallelPlugin` class was removed"):
         DataParallelPlugin()
-
-    from pytorch_lightning.plugins.training_type.fully_sharded import DDPFullyShardedPlugin
-
     with pytest.raises(RuntimeError, match="DDPFullyShardedPlugin` class was removed"):
         DDPFullyShardedPlugin()
-
-    from pytorch_lightning.plugins.training_type.horovod import HorovodPlugin
-
     with pytest.raises(RuntimeError, match="HorovodPlugin` class was removed"):
         HorovodPlugin()
-
-    from pytorch_lightning.plugins.training_type.ipu import IPUPlugin
-
     with pytest.raises(RuntimeError, match="IPUPlugin` class was removed"):
         IPUPlugin()
-
-    from pytorch_lightning.plugins.training_type.parallel import ParallelPlugin
-
     with pytest.raises(RuntimeError, match="ParallelPlugin` class was removed"):
         ParallelPlugin()
-
-    from pytorch_lightning.plugins.training_type.sharded import DDPShardedPlugin
-
     with pytest.raises(RuntimeError, match="DDPShardedPlugin` class was removed"):
         DDPShardedPlugin()
-
-    from pytorch_lightning.plugins.training_type.sharded_spawn import DDPSpawnShardedPlugin
-
     with pytest.raises(RuntimeError, match="DDPSpawnShardedPlugin` class was removed"):
         DDPSpawnShardedPlugin()
-
-    from pytorch_lightning.plugins.training_type.single_device import SingleDevicePlugin
-
     with pytest.raises(RuntimeError, match="SingleDevicePlugin` class was removed"):
         SingleDevicePlugin()
-
-    from pytorch_lightning.plugins.training_type.single_tpu import SingleTPUPlugin
-
     with pytest.raises(RuntimeError, match="SingleTPUPlugin` class was removed"):
         SingleTPUPlugin()
-
-    from pytorch_lightning.plugins.training_type.tpu_spawn import TPUSpawnPlugin
-
     with pytest.raises(RuntimeError, match="TPUSpawnPlugin` class was removed"):
         TPUSpawnPlugin()
-
-    from pytorch_lightning.plugins.training_type.training_type_plugin import TrainingTypePlugin
-
     with pytest.raises(RuntimeError, match="TrainingTypePlugin` class was removed"):
         TrainingTypePlugin()
 
