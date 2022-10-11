c7b6c6220fe769bf3a55dba387de554070354cd5
awaelchli <aedu.waelchli@gmail.com>
2022-10-11 17:14:09 +0200

    comment for borda

--- src/pytorch_lightning/_graveyard/__init__.py
+++ src/pytorch_lightning/_graveyard/__init__.py
@@ -1,2 +1,2 @@
 import pytorch_lightning._graveyard.callbacks
-import pytorch_lightning._graveyard.training_type  # noqa: F401
+import pytorch_lightning._graveyard.training_type
--- src/pytorch_lightning/_graveyard/callbacks.py
+++ src/pytorch_lightning/_graveyard/callbacks.py
@@ -18,6 +18,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 def _save_checkpoint(_: ModelCheckpoint, __: Any) -> None:
+    # Remove in v2.0.0
     raise NotImplementedError(
         f"`{ModelCheckpoint.__name__}.save_checkpoint()` was deprecated in v1.6 and is no longer supported"
         f" as of 1.8. Please use `trainer.save_checkpoint()` to manually save a checkpoint. This method will be"
