08f659b32bc4334fdfaa3e60e88f63434730aeef
awaelchli <aedu.waelchli@gmail.com>
2022-10-11 20:36:06 +0200

    stupid

--- src/pytorch_lightning/_graveyard/__init__.py
+++ src/pytorch_lightning/_graveyard/__init__.py
@@ -14,4 +14,4 @@
 
 import pytorch_lightning._graveyard.callbacks
 import pytorch_lightning._graveyard.trainer
-import pytorch_lightning._graveyard.training_type
+import pytorch_lightning._graveyard.training_type  # noqa: F401
