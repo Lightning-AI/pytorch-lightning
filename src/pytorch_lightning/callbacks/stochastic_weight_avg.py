9dd1a8c19c969239150af338ae56c51b61d11b89
awaelchli <aedu.waelchli@gmail.com>
2022-10-13 17:23:42 +0200

    updates to mypy

--- src/pytorch_lightning/callbacks/stochastic_weight_avg.py
+++ src/pytorch_lightning/callbacks/stochastic_weight_avg.py
@@ -129,7 +129,7 @@ class StochasticWeightAveraging(Callback):
         self._scheduler_state: Optional[Dict] = None
         self._init_n_averaged = 0
         self._latest_update_epoch = -1
-        self.momenta: Optional[Dict[nn.modules.batchnorm._BatchNorm, float]] = None
+        self.momenta: Dict[nn.modules.batchnorm._BatchNorm, Optional[float]] = {}
         self._max_epochs: int
 
     @property
@@ -298,15 +298,14 @@ class StochasticWeightAveraging(Callback):
                 dtype=module.running_var.dtype,
             )
             self.momenta[module] = module.momentum
-            module.momentum = None
+            module.momentum = None  # type: ignore[assignment]
             assert module.num_batches_tracked is not None
             module.num_batches_tracked *= 0
 
     def reset_momenta(self) -> None:
         """Adapted from https://github.com/pytorch/pytorch/blob/v1.7.1/torch/optim/swa_utils.py#L164-L165."""
-        assert self.momenta is not None
         for bn_module in self.momenta:
-            bn_module.momentum = self.momenta[bn_module]
+            bn_module.momentum = self.momenta[bn_module]  # type: ignore[assignment]
 
     @staticmethod
     def update_parameters(
