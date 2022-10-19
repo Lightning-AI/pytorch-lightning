dec4f9c86683c2ae3c2ef5bb7ef431a5fb362ad6
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-10-19 12:49:37 +0200

    debug

--- src/lightning_lite/strategies/fsdp.py
+++ src/lightning_lite/strategies/fsdp.py
@@ -84,7 +84,7 @@ class FSDPStrategy(ParallelStrategy):
         parallel_devices: Optional[List[torch.device]] = None,
         cluster_environment: Optional[ClusterEnvironment] = None,
         checkpoint_io: Optional[CheckpointIO] = None,
-        precision_plugin: Optional[Precision] = None,
+        precision: Optional[Precision] = None,
         process_group_backend: Optional[str] = None,
         timeout: Optional[timedelta] = default_pg_timeout,
         cpu_offload: Optional["CPUOffload"] = None,
@@ -100,7 +100,7 @@ class FSDPStrategy(ParallelStrategy):
             parallel_devices=parallel_devices,
             cluster_environment=cluster_environment,
             checkpoint_io=checkpoint_io,
-            precision_plugin=precision_plugin,
+            precision=precision,
         )
         self._num_nodes = 1
         self._process_group_backend: Optional[str] = process_group_backend
