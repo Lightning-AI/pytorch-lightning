9cfc69f9791c912498b8d78d2b71b14239487dc9
manskx <ahmed.mansy156@gmail.com>
2022-10-20 11:36:38 +0200

    fix tests

--- tests/tests_app/cli/test_run_app.py
+++ tests/tests_app/cli/test_run_app.py
@@ -70,6 +70,7 @@ def test_lightning_run_cluster_without_cloud(monkeypatch):
             no_cache=True,
             env=("FOO=bar",),
             secret=(),
+            checkpoint=None,
         )
 
 
@@ -96,6 +97,7 @@ def test_lightning_run_app_cloud(mock_dispatch: mock.MagicMock, open_ui, caplog,
             no_cache=True,
             env=("FOO=bar",),
             secret=("BAR=my-secret",),
+            checkpoint=None,
         )
     # capture logs.
     # TODO(yurij): refactor the test, check if the actual HTTP request is being sent and that the proper admin
@@ -111,6 +113,7 @@ def test_lightning_run_app_cloud(mock_dispatch: mock.MagicMock, open_ui, caplog,
         env_vars={"FOO": "bar"},
         secrets={"BAR": "my-secret"},
         cluster_id="",
+        checkpoint=None,
     )
 
 
@@ -130,6 +133,7 @@ def test_lightning_run_app_secrets(monkeypatch):
             no_cache=True,
             env=(),
             secret=("FOO=my-secret"),
+            checkpoint=None,
         )
 
 
