7a827b09bbe3457b2a7ba6ae4277ae05557edd15
Rick Izzo <rlizzo@users.noreply.github.com>
2022-10-19 23:24:27 -0400

    Separate the concept of a Drive from that of a Mount (#15120)
    * added mount class and configured it into compute config
    
    * added mount to the cloud runtime dispatcher
    
    * raise error if s3 bucket is passed to a drive telling the user to utilize mounts
    
    * added example for app
    
    * udpated tests
    
    * updated tests
    
    * addressed code review comments
    
    * fix bug
    
    * bugfix
    
    * updates'
    
    * code review comments
    
    * updates
    
    * fixed tests after rename
    
    * fix tests

--- tests/tests_app/utilities/packaging/test_cloud_compute.py
+++ tests/tests_app/utilities/packaging/test_cloud_compute.py
@@ -1,6 +1,7 @@
 import pytest
 
 from lightning_app import CloudCompute
+from lightning_app.storage import Mount
 
 
 def test_cloud_compute_unsupported_features():
@@ -17,6 +18,33 @@ def test_cloud_compute_names():
 
 
 def test_cloud_compute_shared_memory():
-
     cloud_compute = CloudCompute("gpu", shm_size=1100)
     assert cloud_compute.shm_size == 1100
+
+
+def test_cloud_compute_with_mounts():
+    mount_1 = Mount(source="s3://foo/", root_dir="./foo")
+    mount_2 = Mount(source="s3://foo/bar/", root_dir="./bar")
+
+    cloud_compute = CloudCompute("gpu", mounts=mount_1)
+    assert cloud_compute.mounts == mount_1
+
+    cloud_compute = CloudCompute("gpu", mounts=[mount_1, mount_2])
+    assert cloud_compute.mounts == [mount_1, mount_2]
+
+    cc_dict = cloud_compute.to_dict()
+    assert "mounts" in cc_dict
+    assert cc_dict["mounts"] == [
+        {"root_dir": "./foo", "source": "s3://foo/"},
+        {"root_dir": "./bar", "source": "s3://foo/bar/"},
+    ]
+
+    assert CloudCompute.from_dict(cc_dict) == cloud_compute
+
+
+def test_cloud_compute_with_non_unique_mount_root_dirs():
+    mount_1 = Mount(source="s3://foo/", root_dir="./foo")
+    mount_2 = Mount(source="s3://foo/bar/", root_dir="./foo")
+
+    with pytest.raises(ValueError, match="Every Mount attached to a work must have a unique 'root_dir' argument."):
+        CloudCompute("gpu", mounts=[mount_1, mount_2])
