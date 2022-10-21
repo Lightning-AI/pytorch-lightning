d4753e376ab665fb1959e20739a6ce9affb0e8ee
Carlos Mocholí <carlossmocholi@gmail.com>
2022-10-21 19:29:54 +0200

    Names

--- tests/tests_lite/plugins/collectives/test_collective.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from lightning_lite.plugins.collectives.collective import Collective
-
-
-def test_cannot_instantiate_abstract_class():
-    with pytest.raises(TypeError):
-        Collective()
