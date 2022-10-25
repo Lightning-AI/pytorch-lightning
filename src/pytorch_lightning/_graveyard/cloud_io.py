4aa0767fcbc1e0eb8b72a6a872561d761f2d9872
Justus Schock <justus.schock@posteo.de>
2022-10-25 18:02:03 +0200

    add cloudio pickle patching for unified package

---
+++ src/pytorch_lightning/_graveyard/cloud_io.py
@@ -0,0 +1,22 @@
+import pickle
+import warnings
+from copy import deepcopy
+
+
+class RedirectingUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module.startswith("pytorch_" + "lightning"):
+            # for the standalone package this won't do anything,
+            # for the unified mirror package it will redirect the imports
+
+            old_module = deepcopy(module)
+            module = "pytorch_lightning" + module[len("pytorch_" + "lightning") :]
+
+            # this warning won't trigger for standalone as these imports are identical
+            if module != old_module:
+                warnings.warn(f"Redirecting import of {module}.{name} to pytorch_lightning.{name}")
+
+        return super().find_class(module, name)
+
+
+pickle.Unpickler = RedirectingUnpickler
