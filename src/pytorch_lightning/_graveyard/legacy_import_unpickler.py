21a8665de22fbc56ec8f60d2debeb564dcb90599
Justus Schock <justus.schock@posteo.de>
2022-10-26 09:36:47 +0200

    incorporate reviews

--- src/pytorch_lightning/_graveyard/legacy_import_unpickler.py
+++ src/pytorch_lightning/_graveyard/legacy_import_unpickler.py
@@ -1,6 +1,5 @@
 import pickle
 import warnings
-from copy import deepcopy
 from typing import Any
 
 
@@ -10,7 +9,7 @@ class RedirectingUnpickler(pickle.Unpickler):
         if module.startswith(pl):
             # for the standalone package this won't do anything,
             # for the unified mirror package it will redirect the imports
-            old_module = deepcopy(module)
+            old_module = module
             module = "pytorch_lightning" + module[len(pl) :]
             # this warning won't trigger for standalone as these imports are identical
             if module != old_module:
