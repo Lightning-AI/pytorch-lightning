96dc2bce6ad6cf539a74f1b7b4403a3e240af0e8
awaelchli <aedu.waelchli@gmail.com>
2022-10-26 17:49:30 +0200

    parse

--- src/pytorch_lightning/utilities/upgrade_checkpoint.py
+++ src/pytorch_lightning/utilities/upgrade_checkpoint.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import argparse
 import glob
 import logging
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from shutil import copyfile
 from typing import List
@@ -51,49 +51,28 @@ def upgrade_checkpoint(filepath: _PATH) -> None:
     torch.save(checkpoint, filepath)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=(
-            "A utility to upgrade old checkpoints to the format of the current Lightning version."
-            " By default, this will also save a backup of the original file."
-        )
-    )
-    parser.add_argument("path", "--path", "--file", type=str, help="Path to a checkpoint file or a directory with checkpoints to upgrade")
-    parser.add_argument("--recursive", "-r", type=bool, action="store_true", help="If the specified path is a directory, recursively search for checkpoint files to upgrade")
-    parser.add_argument("--extension", "-e", type=str, default=".ckpt", help="The file extension to look for when searching for checkpoint files in a directory.")
-    parser.add_argument("--no-backup", type=bool, action="store_true", help="Do not save backup files. This will overwrite your existing files")
-
-    args = parser.parse_args()
+def main(args: Namespace) -> None:
     path = Path(args.path).absolute()
     extension: str = args.extension if args.extension.startswith(".") else f".{args.extension}"
     files: List[Path] = []
 
     if not path.exists():
-        raise FileNotFoundError()
+        log.error(
+            f"The path {path} does not exist. Please provide a valid path to a checkpoint file or a directory"
+            " containing checkpoints."
+        )
+        exit(1)
 
     if path.is_file():
         files = [path]
     if path.is_dir():
-        files = [Path(p) for p in glob.glob(f"{str(path)}**{extension}", recursive=True)]
+        files = [Path(p) for p in glob.glob(str(path / "**" / f"*{extension}"), recursive=True)]
     if not files:
-        raise FileNotFoundError(
-            f"No files were found in {path}."
+        log.error(
+            f"No checkpoint files with extension {extension} were found in {path}."
             f" HINT: Try setting the `--extension` option to specify the right file extension to look for."
         )
-
-    # existing_backup_files = [file.relative_to(path.parent) for file in files if file.suffix == ".bak"]
-    # existing_backup_files_str = "\n".join((str(f) for f in existing_backup_files))
-    # if existing_backup_files:
-    #     answer = input(
-    #         "It looks like there was already an upgrade on these files. The following backup files already exist:\n\n"
-    #         f"{existing_backup_files_str}\n\n"
-    #         f"Are you sure you want to continue and overwrite the file(s)? The backup files are the original"
-    #         f" checkpoints before the upgrade"
-    #     )
-
-
-    # if not args.no_backup:
-    #     input("")
+        exit(1)
 
     log.info("Creating a backup of the existing checkpoint files before overwriting in the upgrade process.")
     for file in files:
@@ -107,3 +86,28 @@ if __name__ == "__main__":
     for file in tqdm(files):
         with pl_legacy_patch():
             upgrade_checkpoint(file)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description=(
+            "A utility to upgrade old checkpoints to the format of the current Lightning version."
+            " By default, this will also save a backup of the original file."
+        )
+    )
+    parser.add_argument("path", type=str, help="Path to a checkpoint file or a directory with checkpoints to upgrade")
+    parser.add_argument(
+        "--recursive",
+        "-r",
+        action="store_true",
+        help="If the specified path is a directory, recursively search for checkpoint files to upgrade",
+    )
+    parser.add_argument(
+        "--extension",
+        "-e",
+        type=str,
+        default=".ckpt",
+        help="The file extension to look for when searching for checkpoint files in a directory.",
+    )
+    args = parser.parse_args()
+    main(args)
