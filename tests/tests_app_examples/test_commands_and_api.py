0726564c05cc0c1838e780545bcf4c4e252b559e
thomas chaton <thomas@grid.ai>
2022-10-27 13:22:56 +0100

    update

--- tests/tests_app_examples/test_commands_and_api.py
+++ tests/tests_app_examples/test_commands_and_api.py
@@ -47,5 +47,5 @@ def test_commands_and_api_example_cloud() -> None:
             sleep(1)
 
         # 7: Send a request to the Rest API directly.
-        resp = requests.post(base_url + "/pure_function")
+        resp = requests.get(base_url + "/pure_function")
         assert resp.status_code == 200, resp.json()
