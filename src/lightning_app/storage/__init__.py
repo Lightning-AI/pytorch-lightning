ca3d6a619cf5ef1379b60cba1c7768b18a3749e9
Ethan Harris <ethanwharris@gmail.com>
2022-10-27 10:41:48 +0100

    Revert storage init

--- src/lightning_app/storage/__init__.py
+++ src/lightning_app/storage/__init__.py
@@ -1,11 +1,4 @@
-from lightning_app.storage.drive import Drive
-from lightning_app.storage.mount import Mount
-from lightning_app.storage.path import Path
-from lightning_app.storage.payload import Payload
-
-__all__ = [
-    "Drive",
-    "Mount",
-    "Path",
-    "Payload",
-]
+from lightning_app.storage.drive import Drive  # noqa: F401
+from lightning_app.storage.mount import Mount  # noqa: F401
+from lightning_app.storage.path import Path  # noqa: F401
+from lightning_app.storage.payload import Payload  # noqa: F401
