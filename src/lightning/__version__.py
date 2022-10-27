5bc1d46a65dfadebdaee8aa0398c1914f677e115
Jirka <jirka.borovec@seznam.cz>
2022-10-28 00:02:45 +0200

    ver as hash

--- src/lightning/__version__.py
+++ src/lightning/__version__.py
@@ -1,1 +1,14 @@
-version = "1.9.0dev"
+# in case you have not installed the package and have only set python path to this source
+
+import subprocess
+
+
+def get_git_revision_short_hash() -> str:
+    # https://stackoverflow.com/a/21901260/4521646
+    return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
+
+
+try:
+    version: str = get_git_revision_short_hash()
+except Exception:
+    version: str = ""
