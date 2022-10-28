84deb65290a4872e64ac0c0a6af918d45c68a333
thomas chaton <thomas@grid.ai>
2022-10-28 10:55:43 +0100

    update

--- tests/tests_app/conftest.py
+++ tests/tests_app/conftest.py
@@ -39,12 +39,12 @@ def pytest_sessionfinish(session, exitstatus):
     # TODO this isn't great. We should have each tests doing it's own cleanup
     current_process = psutil.Process()
     for child in current_process.children(recursive=True):
-        params = child.as_dict() or {}
-        cmd_lines = params.get("cmdline", [])
+        # params = child.as_dict() or {}
+        # cmd_lines = params.get("cmdline", [])
         # we shouldn't kill the resource tracker from multiprocessing. If we do,
         # `atexit` will throw as it uses resource tracker to try to clean up
-        if cmd_lines and "resource_tracker" in cmd_lines[-1]:
-            continue
+        # if cmd_lines and "resource_tracker" in cmd_lines[-1]:
+        #     continue
         child.kill()
 
     main_thread = threading.current_thread()
