2762c23badb7b746e84fa3e39f0a2e3ad77aca28
Ethan Harris <ethanwharris@gmail.com>
2022-11-01 12:17:47 +0000

    Test + docs

--- src/lightning_app/utilities/cli_helpers.py
+++ src/lightning_app/utilities/cli_helpers.py
@@ -233,24 +233,26 @@ def _is_valid_release(release):
 
 
 @functools.lru_cache(maxsize=1)
-def _is_old_version() -> bool:
+def _get_newer_version() -> Optional[str]:
+    """Check PyPI for newer versions of ``lightning``, returning the newest version if different from the current
+    or ``None`` otherwise."""
     if packaging.version.parse(__version__).is_prerelease:
-        return False
+        return None
     try:
         response = requests.get(f"https://pypi.org/pypi/{__package_name__}/json")
         releases = response.json()["releases"]
         if __version__ not in releases:
-            # Always return False if not installed from PyPI (e.g. dev versions)
-            return False
+            # Always return None if not installed from PyPI (e.g. dev versions)
+            return None
         releases = {version: release for version, release in filter(_is_valid_release, releases.items())}
         sorted_releases = sorted(
             releases.items(), key=lambda release: release[1][0]["upload_time_iso_8601"], reverse=True
         )
         latest_version = sorted_releases[0][0]
-        return False if __version__ == latest_version else latest_version
+        return None if __version__ == latest_version else latest_version
     except Exception:
-        # Return False if any exception occurs
-        return False
+        # Return None if any exception occurs
+        return "err"
 
 
 def _redirect_command(executable: str):
@@ -264,7 +266,11 @@ def _redirect_command(executable: str):
 
 
 def _check_version_and_upgrade():
-    new_version = _is_old_version()
+    """Checks that the current version of ``lightning`` is the latest on PyPI.
+
+    If not, prompt the user to upgrade ``lightning`` for them and re-run the current call in the new version.
+    """
+    new_version = _get_newer_version()
     if new_version:
         prompt = f"A newer version of {__package_name__} is available ({new_version}). Would you like to upgrade?"
 
@@ -285,11 +291,17 @@ def _check_version_and_upgrade():
 
 
 def _check_environment_and_redirect():
+    """Checks that the current ``sys.executable`` is the same as the executable resolved from the current
+    environment.
+
+    If not, this utility tries to redirect the ``lightning`` call to the environment executable (prompting the user to
+    install lightning for them there if needed).
+    """
     env_executable = shutil.which("python")
 
     if env_executable != sys.executable:
         logger.info(
-            "Lightning is running from outside your current environment. " "Switching to your current environment."
+            "Lightning is running from outside your current environment. Switching to your current environment."
         )
 
         process = subprocess.run(
@@ -299,7 +311,7 @@ def _check_environment_and_redirect():
         )
 
         if "No module named lightning" in process.stderr:
-            prompt = f"The {__package_name__} package is not installed. " f"Would you like to install it? [Y/n (exit)]"
+            prompt = f"The {__package_name__} package is not installed. Would you like to install it? [Y/n (exit)]"
 
             if click.confirm(prompt, default=True, show_default=False):
                 command = f"pip install {__package_name__}"
