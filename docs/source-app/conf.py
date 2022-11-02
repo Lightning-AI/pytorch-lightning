7bdf9aad79f3eecbb948384c09b5ff33bb200f6b
Jirka <jirka.borovec@seznam.cz>
2022-11-03 00:25:14 +0100

    lai_sphinx_theme

--- docs/source-app/conf.py
+++ docs/source-app/conf.py
@@ -16,7 +16,7 @@ import os
 import shutil
 import sys
 
-import pt_lightning_sphinx_theme
+import lai_sphinx_theme
 
 import lightning_app
 
@@ -149,8 +149,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pt_lightning_sphinx_theme"
-html_theme_path = [pt_lightning_sphinx_theme.get_html_theme_path()]
+html_theme = "lai_sphinx_theme"
+html_theme_path = [lai_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
