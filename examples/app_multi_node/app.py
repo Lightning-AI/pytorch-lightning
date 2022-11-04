143b535cdcb1dc0d58a77d49a55e7d3bf080130f
thomas chaton <thomas@grid.ai>
2022-11-04 16:31:47 +0000

    update

--- examples/app_multi_node/app.py
+++ examples/app_multi_node/app.py
@@ -1,6 +1,6 @@
-from lightning import LightningApp
-from lightning.app.components import LightningTrainingComponent
-from lightning.app.utilities.packaging.cloud_compute import CloudCompute
+from lightning_app import LightningApp
+from lightning_app.components import LightningTrainingComponent
+from lightning_app.utilities.packaging.cloud_compute import CloudCompute
 
 app = LightningApp(
     LightningTrainingComponent(
