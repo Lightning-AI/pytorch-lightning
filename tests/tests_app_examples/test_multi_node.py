4154a640d08cd0b943b0b690f014d1b33ebe0604
thomas chaton <thomas@grid.ai>
2022-11-04 10:29:45 +0000

    update

--- tests/tests_app_examples/test_multi_node.py
+++ tests/tests_app_examples/test_multi_node.py
@@ -27,3 +27,27 @@ def test_multi_node_example():
     result = application_testing(LightningTestMultiNodeApp, command_line)
     assert result.exit_code == 0
     os.chdir(cwd)
+
+
+class LightningTestMultiNodeWorksApp(LightningTestApp):
+    def on_before_run_once(self):
+        res = super().on_before_run_once()
+        if all(w.has_stopped for w in self.works):
+            return True
+        return res
+
+
+def test_multi_node_example_2():
+    cwd = os.getcwd()
+    new_cwd = os.path.join(_PROJECT_ROOT, "examples/app_multi_node")
+    os.chdir(new_cwd)
+    command_line = [
+        "app_work.py",
+        "--blocking",
+        "False",
+        "--open-ui",
+        "False",
+    ]
+    result = application_testing(LightningTestMultiNodeWorksApp, command_line)
+    assert result.exit_code == 0
+    os.chdir(cwd)
