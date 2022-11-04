4885f9951baabd23be1bb270d1cbc4abdba66311
Jirka <jirka.borovec@seznam.cz>
2022-11-04 23:41:59 +0100

    checks

--- requirements/pytorch/check-avail-strategies.py
+++ requirements/pytorch/check-avail-strategies.py
@@ -1,7 +1,17 @@
-import bagua  # noqa: F401
-import deepspeed  # noqa: F401
-import fairscale  # noqa: F401
-import horovod.torch
+import sys
 
-# returns an error code
-assert horovod.torch.nccl_built()
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    print(args)
+
+    if not args or "bagua" in args:
+        import bagua  # noqa: F401
+    if not args or "deepspeed" in args:
+        import deepspeed  # noqa: F401
+    if not args or "fairscale" in args:
+        import fairscale  # noqa: F401
+    if not args or "horovod" in args:
+        import horovod.torch
+
+        # returns an error code
+        assert horovod.torch.nccl_built()
