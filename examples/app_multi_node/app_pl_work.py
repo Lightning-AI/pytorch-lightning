d221d358cee1f12294919d59d0f077da5b07d40b
thomas chaton <thomas@grid.ai>
2022-11-06 15:06:42 +0000

    update

--- examples/app_multi_node/app_pl_work.py
+++ examples/app_multi_node/app_pl_work.py
@@ -2,15 +2,7 @@ import os
 
 import lightning as L
 from lightning.app.components import MultiNode
-from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.demos.boring_classes import BoringModel
-
-
-class LoggingBoringModel(BoringModel):
-    def training_step(self, batch, batch_idx: int):
-        loss = super().training_step(batch, batch_idx)
-        self.log("loss", loss["loss"])
-        return loss
 
 
 class PyTorchLightningMultiNode(L.LightningWork):
@@ -25,14 +17,13 @@ class PyTorchLightningMultiNode(L.LightningWork):
         os.environ["MASTER_PORT"] = str(main_port)
         os.environ["NODE_RANK"] = str(node_rank)
 
-        model = LoggingBoringModel()
+        model = BoringModel()
         trainer = L.Trainer(
             max_epochs=10,
             devices="auto",
             accelerator="auto",
             num_nodes=nodes,
             strategy="ddp_spawn",  # Only spawn based strategies are supported for now.
-            callbacks=[ModelCheckpoint(monitor="loss")],
         )
         trainer.fit(model)
 
