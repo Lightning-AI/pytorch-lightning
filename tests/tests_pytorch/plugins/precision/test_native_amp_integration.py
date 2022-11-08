a8c6f1a742e769f1f6cda6174bd443a1b233efe4
awaelchli <aedu.waelchli@gmail.com>
2022-11-08 10:27:18 +0100

    unused import

--- tests/tests_pytorch/plugins/precision/test_native_amp_integration.py
+++ tests/tests_pytorch/plugins/precision/test_native_amp_integration.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-from unittest import mock
-
 import torch
 
 from lightning_lite import seed_everything
