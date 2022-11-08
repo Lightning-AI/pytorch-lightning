a5e62e4d3bffa4cafaf2dbd92a21ad6d58c97f09
Rick Izzo <rick@grid.ai>
2022-11-08 00:15:44 -0500

    added example test

---
+++ tests/tests_app_examples/test_installation_commands_app.py
@@ -0,0 +1,22 @@
+import os
+from datetime import time
+
+import pytest
+from tests_app import _PROJECT_ROOT
+
+from lightning_app.testing.testing import run_app_in_cloud
+
+
+@pytest.mark.cloud
+def test_installation_commands_app_example_cloud() -> None:
+    with run_app_in_cloud(
+        os.path.join(_PROJECT_ROOT, "examples/app_installation_commands"),
+        app_name="app.py",
+        debug=True,
+    ) as (_, _, fetch_logs, _):
+        has_logs = False
+        while not has_logs:
+            for log in fetch_logs(["work"]):
+                if "lmdb successfully installed" in log:
+                    has_logs = True
+            time.sleep(1)
