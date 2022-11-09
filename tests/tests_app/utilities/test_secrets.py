e5f1d452d43f27931ea0ccd1c33ce1c17e45a2f5
awaelchli <aedu.waelchli@gmail.com>
2022-11-09 14:49:10 +0100

    debug

--- tests/tests_app/utilities/test_secrets.py
+++ tests/tests_app/utilities/test_secrets.py
@@ -1,10 +1,11 @@
 from typing import Dict, List
 from unittest import mock
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 from lightning_cloud.openapi import V1ListMembershipsResponse, V1ListSecretsResponse, V1Membership, V1Secret
 
+import lightning_app
 from lightning_app.utilities.secrets import _names_to_ids
 
 
@@ -29,18 +30,26 @@ from lightning_app.utilities.secrets import _names_to_ids
     ],
 )
 @mock.patch("lightning_cloud.login.Auth.authenticate", MagicMock())
-@mock.patch("lightning_app.utilities.secrets.LightningClient.secret_service_list_secrets")
-@mock.patch("lightning_app.utilities.secrets.LightningClient.projects_service_list_memberships")
 def test_names_to_ids(
-    list_memberships: MagicMock,
-    list_secrets: MagicMock,
     secret_names: List[str],
     secrets: List[V1Secret],
     expected: Dict[str, str],
     expected_exception: bool,
+    monkeypatch,
 ):
+
+
+
+    list_memberships = Mock()
     list_memberships.return_value = V1ListMembershipsResponse(memberships=[V1Membership(project_id="default-project")])
+    list_secrets = Mock()
     list_secrets.return_value = V1ListSecretsResponse(secrets=secrets)
+    monkeypatch.setattr(lightning_app.utilities.secrets.LightningClient, "projects_service_list_memberships", list_memberships)
+    monkeypatch.setattr(lightning_app.utilities.secrets.LightningClient, "secret_service_list_secrets", list_secrets)
+
+    from lightning.app.utilities import secrets as _secrets
+    monkeypatch.setattr(_secrets.LightningClient, "projects_service_list_memberships", list_memberships)
+    monkeypatch.setattr(_secrets.LightningClient, "secret_service_list_secrets", list_secrets)
 
     if expected_exception:
         with pytest.raises(ValueError):
