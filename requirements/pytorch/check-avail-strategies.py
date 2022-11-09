e1ca8b2a1b284833b3b1113c53f7189abc2dfb2f
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2022-11-09 23:26:13 +0100

    Apply suggestions from code review
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- requirements/pytorch/check-avail-strategies.py
+++ requirements/pytorch/check-avail-strategies.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     print(args)
 
-    if not args or "bagua" in args:
+    if "bagua" in args:
         import bagua  # noqa: F401
-    if not args or "deepspeed" in args:
+    if "deepspeed" in args:
         import deepspeed  # noqa: F401
-    if not args or "fairscale" in args:
+    if "fairscale" in args:
         import fairscale  # noqa: F401
-    if not args or "horovod" in args:
+    if "horovod" in args:
         import horovod.torch
 
         # returns an error code
