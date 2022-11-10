b2e92bf5639171cae0db92e390fc551051d0ef51
awaelchli <aedu.waelchli@gmail.com>
2022-11-11 00:21:25 +0100

    x

--- src/pytorch_lightning/trainer/connectors/callback_connector.py
+++ src/pytorch_lightning/trainer/connectors/callback_connector.py
@@ -299,11 +299,10 @@ def _validate_callbacks_list(callbacks: List[Callback]) -> None:
     seen_callbacks = defaultdict(list)
     for callback in stateful_callbacks:
         if callback.state_key in seen_callbacks:
-            duplicates = seen_callbacks[callback.state_key]
             raise RuntimeError(
-                f"Found {len(duplicates)} stateful callbacks of type `{type(callback).__name__}`. In the current"
+                f"Found more than one stateful callback of type `{type(callback).__name__}`. In the current"
                 " configuration, this callback does not support being saved alongside other instances of the same type."
                 f" Please consult the documentation of `{type(callback).__name__}` regarding valid settings for"
-                " for the callback to be checkpointable."
+                " the callback state to be checkpointable."
             )
         seen_callbacks[callback.state_key].append(callback)
