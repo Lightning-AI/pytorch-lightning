000d1126e020a5e17ead6c3c2e0fa26ff8932add
Carlos Mocholí <carlossmocholi@gmail.com>
2022-11-10 02:42:01 +0100

    Revert "DEBUG"
    This reverts commit 1414e8590285d62962a44590ae70cf9635c0d4e7.

--- src/lightning_lite/lite.py
+++ src/lightning_lite/lite.py
@@ -5,7 +5,6 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-# hello :)
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
