2ddd196e19ac862b2ce2f4651ae716eadc0968d7
Rick Izzo <rick@grid.ai>
2022-11-09 22:38:42 -0500

    fixed condition in which app command comments would not execute before the flow is dispatched

--- tests/tests_app_examples/test_installation_commands_app.py
+++ tests/tests_app_examples/test_installation_commands_app.py
@@ -7,18 +7,40 @@ from tests_app import _PROJECT_ROOT
 from lightning_app.testing.testing import run_app_in_cloud
 
 
-@pytest.mark.skip(reason="temporarily disabled until backend release")
 @pytest.mark.cloud
 def test_installation_commands_app_example_cloud() -> None:
+    # This is expected to fail since it is missing the "setup" flag
+    with run_app_in_cloud(
+        os.path.join(_PROJECT_ROOT, "examples/app_installation_commands"),
+        app_name="app.py",
+        debug=True,
+    ) as (_, _, fetch_logs, _):
+        has_logs = False
+        while not has_logs:
+            for log in fetch_logs(["flow"]):
+                if "ModuleNotFoundError: No module named 'lmdb'" in log:
+                    has_logs = True
+            time.sleep(1)
+
+    # This is expected to pass, since the "setup" flag is passed
     with run_app_in_cloud(
         os.path.join(_PROJECT_ROOT, "examples/app_installation_commands"),
         app_name="app.py",
         extra_args=["--setup"],
         debug=True,
     ) as (_, _, fetch_logs, _):
-        has_logs = False
-        while not has_logs:
+        # check installation in flow
+        has_flow_logs = False
+        while not has_flow_logs:
+            for log in fetch_logs(["flow"]):
+                if "lmdb successfully installed" in log:
+                    has_flow_logs = True
+            time.sleep(1)
+
+        # check installation in work
+        has_work_logs = False
+        while not has_work_logs:
             for log in fetch_logs(["work"]):
                 if "lmdb successfully installed" in log:
-                    has_logs = True
+                    has_work_logs = True
             time.sleep(1)
