5a6b3bdecb271d90f71fdbcfeb71360cc970128f
awaelchli <aedu.waelchli@gmail.com>
2022-11-10 22:11:02 +0100

    guard bagua import

--- requirements/pytorch/check-avail-strategies.py
+++ requirements/pytorch/check-avail-strategies.py
@@ -1,4 +1,9 @@
-import bagua  # noqa: F401
+from pytorch_lightning.utilities.imports import _TORCH_GREATER_EQUAL_1_12
+
+if _TORCH_GREATER_EQUAL_1_12:
+    # TODO: Remove guard once we support testing bagua on >= 1.13
+    import bagua  # noqa: F401
+
 import deepspeed  # noqa: F401
 import fairscale  # noqa: F401
 import horovod.torch
