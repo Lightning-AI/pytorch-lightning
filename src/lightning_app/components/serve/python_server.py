7665a1ec0e0bc730e9ddbc8889b8dc3073f88903
thomas chaton <thomas@grid.ai>
2022-11-11 11:46:41 +0000

    update

--- src/lightning_app/components/serve/python_server.py
+++ src/lightning_app/components/serve/python_server.py
@@ -14,7 +14,7 @@ from lightning_app.utilities.app_helpers import Logger
 logger = Logger(__name__)
 
 
-def image_to_base64(image_path):
+def _image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     return encoded_string.decode("UTF-8")
