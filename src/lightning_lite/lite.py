98379121a8820e0fa154b76d17b265f7ff422115
Carlos Mocholí <carlossmocholi@gmail.com>
2022-11-11 12:22:52 +0100

    Debug

--- src/lightning_lite/lite.py
+++ src/lightning_lite/lite.py
@@ -5,7 +5,7 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# sdfsdf
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
