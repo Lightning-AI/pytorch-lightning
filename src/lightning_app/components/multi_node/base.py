afdda533f77fd77151a901363b9f4068da66eae7
thomas chaton <thomas@grid.ai>
2022-11-11 16:09:06 +0000

    update

--- src/lightning_app/components/multi_node/base.py
+++ src/lightning_app/components/multi_node/base.py
@@ -52,31 +52,26 @@ class MultiNode(LightningFlow):
             work_kwargs: Keywords arguments to be provided to the work on instantiation.
         """
         super().__init__()
-        self.ws = structures.List()
-        self._work_cls = work_cls
+        self.ws = structures.List(
+            *[
+                work_cls(
+                    *work_args,
+                    cloud_compute=cloud_compute,
+                    **work_kwargs,
+                    parallel=True,
+                )
+                for _ in range(num_nodes)
+            ]
+        )
         self.num_nodes = num_nodes
-        self._cloud_compute = cloud_compute
-        self._work_args = work_args
-        self._work_kwargs = work_kwargs
         self.has_started = False
 
     def run(self) -> None:
         if not self.has_started:
 
-            # 1. Create & start the works
-            if not self.ws:
-                for node_rank in range(self.num_nodes):
-                    self.ws.append(
-                        self._work_cls(
-                            *self._work_args,
-                            cloud_compute=self._cloud_compute,
-                            **self._work_kwargs,
-                            parallel=True,
-                        )
-                    )
-
-                    # Starting node `node_rank`` ...
-                    self.ws[-1].start()
+            # 1. Start all the works.
+            for w in self.ws:
+                w.start()
 
             # 2. Wait for all machines to be started !
             if not all(w.status.stage == WorkStageStatus.STARTED for w in self.ws):
