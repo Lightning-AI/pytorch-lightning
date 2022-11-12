6a52596599015b3ed3d0edabe990864d5b6b9b41
Carlos Mocholí <carlossmocholi@gmail.com>
2022-11-12 16:05:03 +0100

    Unused changes

--- requirements/pytorch/check-avail-strategies.py
+++ requirements/pytorch/check-avail-strategies.py
@@ -1,17 +1,8 @@
-import sys
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    print(args)
+    import bagua  # noqa: F401
+    import deepspeed  # noqa: F401
+    import fairscale  # noqa: F401
+    import horovod.torch
 
-    if "bagua" in args:
-        import bagua  # noqa: F401
-    if "deepspeed" in args:
-        import deepspeed  # noqa: F401
-    if "fairscale" in args:
-        import fairscale  # noqa: F401
-    if "horovod" in args:
-        import horovod.torch
-
-        # returns an error code
-        assert horovod.torch.nccl_built()
+    # returns an error code
+    assert horovod.torch.nccl_built()
