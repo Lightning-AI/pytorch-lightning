fb3bc0dc8927b7aefc3cdc1f13eafd7016141a53
Carlos Mocholí <carlossmocholi@gmail.com>
2022-11-18 10:28:00 +0100

    pip install all reqs

--- docs/source-app/levels/basic/hello_components/blue.py
+++ docs/source-app/levels/basic/hello_components/blue.py
@@ -1,6 +1,5 @@
 # app.py
-# ! pip install pandas
-# ! pip install scikit-learn
+# ! pip install https://github.com/Lightning-AI/lightning/archive/refs/heads/master.zip numpy pandas sentencepiece 'torch>=1.7.0,!=1.8.0' scikit-learn 'transformers==4.16.2'
 # ! curl https://gist.githubusercontent.com/awaelchli/49ad15ddebb3449f859a184bd90cea6f/raw/9b4257c938ddd35ddf26009c1c587876445a4c4c/model.py -o model.py
 import lightning as L
 from lightning.app.components import LightningTrainerMultiNode
@@ -9,8 +8,7 @@ from transformers import T5TokenizerFast as T5Tokenizer
 from model import TextSummarization, TextSummarizationDataModule, predict
 
 sample_text = """
-ML Ops platforms come in many flavors from platforms that train models to platforms that label data and auto-retrain models. To build an ML Ops platform requires dozens of engineers, multiple years and 10+ million in funding. The majority of that work will go into infrastructure, multi-cloud, user management, consumption models, billing, and much more.
-Build your platform with Lightning and launch in weeks not months. Focus on the workflow you want to enable (label data then train models), Lightning will handle all the infrastructure, billing, user management, and the other operational headaches.
+copy paste your text here
 """
 
 
