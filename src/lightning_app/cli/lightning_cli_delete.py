de5c0386fd3f3a48089fe2e58bdc53c8d9ff36bc
Rick Izzo <rick@grid.ai>
2022-11-22 17:17:24 -0500

    after PR review

--- src/lightning_app/cli/lightning_cli_delete.py
+++ src/lightning_app/cli/lightning_cli_delete.py
@@ -1,4 +1,8 @@
 import click
+import inquirer
+from inquirer.themes import GreenPassion
+from lightning_cloud.openapi import V1ClusterType
+from rich.console import Console
 
 from lightning_app.cli.cmd_apps import _AppManager
 from lightning_app.cli.cmd_clusters import AWSClusterManager
@@ -67,8 +71,74 @@ def delete_app(app_name: str, cluster_id: str) -> None:
     Deleting an app also deletes all app websites, works, artifacts, and logs. This permanently removes not only the
     record of the app, but all resources associated with the app.
     """
-    manager = _AppManager()
-    manager.delete(cluster_id=cluster_id, app_id=app_name)
+    console = Console()
+    cluster_manager = AWSClusterManager()
+    app_manager = _AppManager()
+
+    default_cluster, valid_clusters = None, []
+    for cluster in cluster_manager.get_clusters().clusters:
+        valid_clusters.append(cluster.id)
+        if cluster.spec.cluster_type == V1ClusterType.GLOBAL and default_cluster is None:
+            default_cluster = cluster.id
+
+    if cluster_id is None:
+        cluster_id = default_cluster
+
+    else:
+        if cluster_id not in valid_clusters:
+            console.print(f"[warning][b][yellow]You don't have access to cluster: {cluster_id}[/yellow][/b][/warning]")
+            if len(valid_clusters) == 1:
+                confirm_cluster = [
+                    inquirer.Confirm(
+                        "confirm_cluster",
+                        message=f"Did you mean to specify the default Lightning Cloud cluster: {default_cluster}?",
+                        default=True,
+                    ),
+                ]
+                if not inquirer.prompt(confirm_cluster, theme=GreenPassion())["confirm_cluster"]:
+                    console.print("[b][yellow]Exiting![/b][/yellow]")
+                    return
+                cluster_id = default_cluster
+            else:
+                available_clusters = [
+                    inquirer.List(
+                        "cluster_id",
+                        message="Please select which cluster the app should be deleted from",
+                        choices=valid_clusters,
+                        default=default_cluster,
+                    ),
+                ]
+                cluster_id = inquirer.prompt(available_clusters, theme=GreenPassion())["cluster_id"]
+
+    selected_app_instance_id = None
+    all_app_names_and_ids = {}
+    for app in app_manager.list_apps(cluster_id=cluster_id):
+        all_app_names_and_ids[app.name] = app.id
+        if app_name == app.name:
+            selected_app_instance_id = app.id
+            break
+        if app_name == app.id:
+            selected_app_instance_id = app.id
+            break
+
+    if selected_app_instance_id is None:
+        console.print(
+            f"[warning][b][yellow]Cluster {cluster_id} does not have an app named {app_name}[/yellow][/b][/warning]"
+        )
+        available_apps = [
+            inquirer.List(
+                "app_name",
+                message="Select the app name to delete",
+                choices=list(all_app_names_and_ids.keys()),
+            ),
+        ]
+        app_name = inquirer.prompt(available_apps, theme=GreenPassion())["app_name"]
+        selected_app_instance_id = all_app_names_and_ids[app_name]
+
+    console.print(
+        f"App {app_name} (debug id {selected_app_instance_id}) has been successfully deleted from cluster {cluster_id}"
+    )
+    return
 
 
 @delete.command("ssh-key")
