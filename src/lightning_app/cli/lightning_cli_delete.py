f9de2683a9e0244e0b46f4601a2b682fbfc028e0
Rick Izzo <rick@grid.ai>
2022-11-22 22:51:39 -0500

    delete CLI working

--- src/lightning_app/cli/lightning_cli_delete.py
+++ src/lightning_app/cli/lightning_cli_delete.py
@@ -65,11 +65,20 @@ def delete_cluster(cluster: str, force: bool = False, wait: bool = False) -> Non
     default=None,
     help="Delete the Lighting App from a specific Lightning AI BYOC compute cluster",
 )
-def delete_app(app_name: str, cluster_id: str) -> None:
-    """Delete a Lightning AI app and associated data and resources.
+@click.option(
+    "skip_user_confirm_prompt",
+    "--yes",
+    "-y",
+    is_flag=True,
+    default=False,
+    help="Do not prompt for confirmation.",
+)
+def delete_app(app_name: str, cluster_id: str, skip_user_confirm_prompt: bool) -> None:
+    """Delete a Lightning app.
 
-    Deleting an app also deletes all app websites, works, artifacts, and logs. This permanently removes not only the
-    record of the app, but all resources associated with the app.
+    Deleting an app also deletes all app websites, works, artifacts, and logs. This permanently removes any record of
+    the app as well as all any of its associated resources and data. This does not affect any resources and data
+    associated with other Lightning apps on your account.
     """
     console = Console()
     cluster_manager = AWSClusterManager()
@@ -81,39 +90,52 @@ def delete_app(app_name: str, cluster_id: str) -> None:
         if cluster.spec.cluster_type == V1ClusterType.GLOBAL and default_cluster is None:
             default_cluster = cluster.id
 
+    # when no cluster-id is passed in, use the default (Lightning Cloud) cluster
     if cluster_id is None:
         cluster_id = default_cluster
 
-    else:
-        if cluster_id not in valid_clusters:
-            console.print(f"[warning][b][yellow]You don't have access to cluster: {cluster_id}[/yellow][/b][/warning]")
-            if len(valid_clusters) == 1:
-                confirm_cluster = [
+    if cluster_id not in valid_clusters:
+        console.print(f'[b][yellow]You don\'t have access to cluster "{cluster_id}"[/yellow][/b]')
+        if len(valid_clusters) == 1:
+            # if there are no BYOC clusters, then confirm that
+            # the user wants to fall back to the Lightning Cloud.
+            try:
+                ask = [
                     inquirer.Confirm(
-                        "confirm_cluster",
-                        message=f"Did you mean to specify the default Lightning Cloud cluster: {default_cluster}?",
+                        "confirm",
+                        message=f'Did you mean to specify the default Lightning Cloud cluster "{default_cluster}"?',
                         default=True,
                     ),
                 ]
-                if not inquirer.prompt(confirm_cluster, theme=GreenPassion())["confirm_cluster"]:
-                    console.print("[b][yellow]Exiting![/b][/yellow]")
+                if inquirer.prompt(ask, theme=GreenPassion(), raise_keyboard_interrupt=True)["confirm"] is False:
+                    console.print("[b][red]Aborted![/b][/red]")
                     return
-                cluster_id = default_cluster
-            else:
-                available_clusters = [
+            except KeyboardInterrupt:
+                console.print("[b][red]Cancelled by user![/b][/red]")
+                return
+            cluster_id = default_cluster
+        else:
+            # When there are BYOC clusters, have them select which cluster to use from all available.
+            try:
+                ask = [
                     inquirer.List(
-                        "cluster_id",
-                        message="Please select which cluster the app should be deleted from",
+                        "cluster",
+                        message=f'Please select which cluster app "{app_name}" should be deleted from',
                         choices=valid_clusters,
                         default=default_cluster,
                     ),
                 ]
-                cluster_id = inquirer.prompt(available_clusters, theme=GreenPassion())["cluster_id"]
+                cluster_id = inquirer.prompt(ask, theme=GreenPassion(), raise_keyboard_interrupt=True)["cluster"]
+            except KeyboardInterrupt:
+                console.print("[b][red]Cancelled by user![/b][/red]")
+                return
 
-    selected_app_instance_id = None
     all_app_names_and_ids = {}
+    selected_app_instance_id = None
+
     for app in app_manager.list_apps(cluster_id=cluster_id):
         all_app_names_and_ids[app.name] = app.id
+        # figure out the ID of some app_name on the cluster.
         if app_name == app.name:
             selected_app_instance_id = app.id
             break
@@ -122,22 +144,52 @@ def delete_app(app_name: str, cluster_id: str) -> None:
             break
 
     if selected_app_instance_id is None:
-        console.print(
-            f"[warning][b][yellow]Cluster {cluster_id} does not have an app named {app_name}[/yellow][/b][/warning]"
-        )
-        available_apps = [
-            inquirer.List(
-                "app_name",
-                message="Select the app name to delete",
-                choices=list(all_app_names_and_ids.keys()),
-            ),
-        ]
-        app_name = inquirer.prompt(available_apps, theme=GreenPassion())["app_name"]
-        selected_app_instance_id = all_app_names_and_ids[app_name]
+        # when there is no app with the given app_name on the cluster,
+        # ask the user which app they would like to delete.
+        console.print(f'[b][yellow]Cluster "{cluster_id}" does not have an app named "{app_name}"[/yellow][/b]')
+        try:
+            ask = [
+                inquirer.List(
+                    "app_name",
+                    message="Select the app name to delete",
+                    choices=list(all_app_names_and_ids.keys()),
+                ),
+            ]
+            app_name = inquirer.prompt(ask, theme=GreenPassion(), raise_keyboard_interrupt=True)["app_name"]
+            selected_app_instance_id = all_app_names_and_ids[app_name]
+        except KeyboardInterrupt:
+            console.print("[b][red]Cancelled by user![/b][/red]")
+            return
 
-    console.print(
-        f"App {app_name} (debug id {selected_app_instance_id}) has been successfully deleted from cluster {cluster_id}"
-    )
+    if not skip_user_confirm_prompt:
+        # when the --yes / -y flags were not passed, do a final
+        # confirmation that the user wants to delete the app.
+        try:
+            ask = [
+                inquirer.Confirm(
+                    "confirm",
+                    message=f'Are you sure you want to delete app "{app_name}" on cluster "{default_cluster}"?',
+                    default=False,
+                ),
+            ]
+            if inquirer.prompt(ask, theme=GreenPassion(), raise_keyboard_interrupt=True)["confirm"] is False:
+                console.print("[b][red]Aborted![/b][/red]")
+                return
+        except KeyboardInterrupt:
+            console.print("[b][red]Cancelled by user![/b][/red]")
+            return
+
+    try:
+        # Delete the app!
+        app_manager.delete(app_id=selected_app_instance_id)
+    except Exception as e:
+        console.print(
+            f'[b][red]An issue occurred while deleting app "{app_name}. If the issue persists, please '
+            "reach out to us at [link=mailto:support@lightning.ai]support@lightning.ai[/link][/b][/red]."
+        )
+        raise click.ClickException(str(e))
+
+    console.print(f'[b][green]App "{app_name}" has been successfully deleted from cluster "{cluster_id}"![/green][/b]')
     return
 
 
