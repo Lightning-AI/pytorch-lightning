bc02f12b76adac0156f907b2a595c7b737fd1812
awaelchli <aedu.waelchli@gmail.com>
2022-11-24 15:10:59 +0100

    update test

--- tests/tests_examples_app/public/test_multi_node.py
+++ tests/tests_examples_app/public/test_multi_node.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from unittest import mock
 
 import pytest
 from tests_examples_app.public import _PATH_EXAMPLES
@@ -11,13 +12,14 @@ class LightningTestMultiNodeApp(LightningTestApp):
     def on_before_run_once(self):
         res = super().on_before_run_once()
         if self.works and all(w.has_stopped for w in self.works):
-            assert len([w for w in self.works]) == 1
+            assert len([w for w in self.works]) == 2
             return True
         return res
 
 
 @pytest.mark.skip(reason="flaky")
-def test_multi_node_example(monkeypatch):
+@mock.patch("lightning_app.components.multi_node.base.is_running_in_cloud", return_value=True)
+def test_multi_node_example(_, monkeypatch):
     monkeypatch.chdir(os.path.join(_PATH_EXAMPLES, "app_multi_node"))
     command_line = [
         "app.py",
@@ -34,7 +36,7 @@ class LightningTestMultiNodeWorksApp(LightningTestApp):
     def on_before_run_once(self):
         res = super().on_before_run_once()
         if self.works and all(w.has_stopped for w in self.works):
-            assert len([w for w in self.works]) == 1
+            assert len([w for w in self.works]) == 2
             return True
         return res
 
@@ -50,7 +52,8 @@ class LightningTestMultiNodeWorksApp(LightningTestApp):
     ],
 )
 @pytest.mark.skipif(sys.platform == "win32", reason="flaky")
-def test_multi_node_examples(app_name, monkeypatch):
+@mock.patch("lightning_app.components.multi_node.base.is_running_in_cloud", return_value=True)
+def test_multi_node_examples(_, app_name, monkeypatch):
     monkeypatch.chdir(os.path.join(_PATH_EXAMPLES, "app_multi_node"))
     command_line = [
         app_name,
