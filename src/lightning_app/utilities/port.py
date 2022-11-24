04daf575af34307c294022b3542a694ad432bbb2
thomas chaton <thomas@grid.ai>
2022-11-24 11:28:48 +0000

    Update src/lightning_app/utilities/port.py
    Co-authored-by: Ethan Harris <ethanwharris@gmail.com>

--- src/lightning_app/utilities/port.py
+++ src/lightning_app/utilities/port.py
@@ -62,8 +62,6 @@ def enable_port() -> V1NetworkConfig:
     if not app_id or not project_id:
         raise Exception("The app_id and project_id should be defined.")
 
-    assert project_id
-
     client = LightningClient()
     list_apps_resp = client.lightningapp_instance_service_list_lightningapp_instances(project_id=project_id)
     lit_app: Optional[Externalv1LightningappInstance] = None
