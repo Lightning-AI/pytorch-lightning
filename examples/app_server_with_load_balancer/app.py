9cc237f3198346716012347c43d8dc7c22dabc84
Akihiro Nitta <nitta@akihironitta.com>
2022-11-24 21:23:16 +0900

    Update example

--- examples/app_server_with_load_balancer/app.py
+++ examples/app_server_with_load_balancer/app.py
@@ -1,33 +1,44 @@
-# !pip install torchvision pydantic
 import base64
 import io
+import logging
+from typing import Any, List
 
 import torch
 import torchvision
-from PIL import Image
+from PIL import Image as PILImage
 from pydantic import BaseModel
 
 import lightning as L
-from lightning.app.components.serve import Image as InputImage
+from lightning.app.components import AutoScaler
 from lightning.app.components.serve import PythonServer
+from lightning.app.utilities.network import find_free_network_port
+
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
+    level=logging.INFO,
+)
+
+
+class RequestModel(BaseModel):
+    image: str
+
+
+class BatchRequestModel(BaseModel):
+    inputs: List[RequestModel]
+
+
+class BatchResponse(BaseModel):
+    outputs: List[Any]
 
 
 class PyTorchServer(PythonServer):
-    def __init__(self):
-        super().__init__(
-            input_type=InputImage,
-            output_type=OutputData,
-            cloud_compute=L.CloudCompute("gpu"),
-        )
-
     def setup(self):
         self._model = torchvision.models.resnet18(pretrained=True)
         self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self._model.to(self._device)
 
-    def predict(self, request):
-        image = base64.b64decode(request.image.encode("utf-8"))
-        image = Image.open(io.BytesIO(image))
+    def predict(self, requests: BatchRequestModel):
         transforms = torchvision.transforms.Compose(
             [
                 torchvision.transforms.Resize(224),
@@ -35,25 +46,34 @@ class PyTorchServer(PythonServer):
                 torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         )
-        image = transforms(image)
-        image = image.to(self._device)
-        prediction = self._model(image.unsqueeze(0))
-        return {"prediction": prediction.argmax().item()}
+        images = []
+        for request in requests.inputs:
+            image = base64.b64decode(request.image.encode("utf-8"))
+            image = PILImage.open(io.BytesIO(image))
+            image = transforms(image).unsqueeze(0)
+            images.append(image)
+        images = torch.cat(images)
+        images = images.to(self._device)
+        predictions = self._model(images)
+        results = predictions.argmax(1).cpu().numpy().tolist()
+        return BatchResponse(outputs=[{"prediction": e} for e in results])
 
 
-class OutputData(BaseModel):
-    prediction: int
+class RootFlow(AutoScaler):
+    def create_worker(self, *args, **kwargs) -> L.LightningWork:
+        return PyTorchServer(
+            port=find_free_network_port(),
+            input_type=BatchRequestModel,
+            output_type=BatchResponse,
+            cloud_compute=L.CloudCompute("gpu"),
+        )
 
 
-app = L.LightningApp(PyTorchServer())
-
-
-# TODO: name confusion LoadBalancer vs. AutoScaler
-# from lightning.app.components import LoadBalancer
-# component = LoadBalancer(
-#     PyTorchServer,
-#     num_replicas=4,
-#     balance_function="predict",
-#     auto_scale=False,
-# )
-# app = L.LightningApp(component)
+app = L.LightningApp(
+    RootFlow(
+        input_schema=RequestModel,
+        output_schema=Any,
+        batch_timeout_secs=0.1,
+        worker_url="predict",
+    )
+)
