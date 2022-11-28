5723995368c2c2860aecec21a3bb7274953e5855
hhsecond <sherin@grid.ai>
2022-11-28 15:44:33 +0530

    trying to fix ubuntu test

--- tests/tests_app/utilities/test_safe_pickle.py
+++ tests/tests_app/utilities/test_safe_pickle.py
@@ -8,4 +8,4 @@ def test_safe_pickle_app():
         ["lightning", "run", "app", "safe_pickle_app.py", "--open-ui", "false"], stdout=subprocess.PIPE, cwd=test_dir
     )
     stdout, _ = proc.communicate()
-    assert "Exiting the pickling app successfully!!" in stdout.decode("UTF-8")
+    assert "Exiting the pickling app successfully" in stdout.decode("UTF-8")
