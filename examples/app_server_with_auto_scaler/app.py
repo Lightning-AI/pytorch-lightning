15dca215c205627511165590870110d891917255
Akihiro Nitta <nitta@akihironitta.com>
2022-11-29 21:17:26 +0900

    Update example

--- examples/app_server_with_auto_scaler/app.py
+++ examples/app_server_with_auto_scaler/app.py
@@ -1,3 +1,4 @@
+import warnings  # FIXME: remove before merge
 from typing import Any, List
 
 import torch
@@ -5,6 +6,9 @@ import torchvision
 from pydantic import BaseModel
 
 import lightning as L
+
+warnings.filterwarnings("ignore", "Arguments other than a weight enum", UserWarning)  # FIXME: remove before merge
+warnings.filterwarnings("ignore", "The parameter 'pretrained' is deprecated", UserWarning)  # FIXME: remove before merge
 
 
 class RequestModel(BaseModel):
@@ -53,8 +57,23 @@ class PyTorchServer(L.app.components.PythonServer):
         return BatchResponse(outputs=[{"prediction": e} for e in results])
 
 
+class MyAutoScaler(L.app.components.AutoScaler):
+    def scale(self, replicas, metrics):
+        """The default replication logic that users can override."""
+
+        # upscale
+        if metrics["pending_requests"] > self.upscale_threshold * metrics["pending_works"]:
+            return replicas + 1
+
+        # downscale
+        if metrics["pending_requests"] < self.downscale_threshold:
+            return replicas - 1
+
+        return replicas
+
+
 app = L.LightningApp(
-    L.app.components.AutoScaler(
+    MyAutoScaler(
         PyTorchServer,
         min_replicas=2,
         max_replicas=4,
