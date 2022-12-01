1bc3a97e95c9013a8ab86076437558a3428354d8
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-01 12:41:22 -0800

    Remove checks for torch greater than 1.10 (#15846)

--- src/lightning_lite/utilities/imports.py
+++ src/lightning_lite/utilities/imports.py
@@ -27,8 +27,6 @@ _IS_INTERACTIVE = hasattr(sys, "ps1") or bool(sys.flags.interactive)
 
 _PYTHON_GREATER_EQUAL_3_8_0 = (sys.version_info.major, sys.version_info.minor) >= (3, 8)
 _PYTHON_GREATER_EQUAL_3_10_0 = (sys.version_info.major, sys.version_info.minor) >= (3, 10)
-_TORCH_GREATER_EQUAL_1_10 = compare_version("torch", operator.ge, "1.10.0")
-_TORCH_LESSER_EQUAL_1_10_2 = compare_version("torch", operator.le, "1.10.2")
 _TORCH_GREATER_EQUAL_1_11 = compare_version("torch", operator.ge, "1.11.0")
 _TORCH_GREATER_EQUAL_1_12 = compare_version("torch", operator.ge, "1.12.0")
 _TORCH_GREATER_EQUAL_1_13 = compare_version("torch", operator.ge, "1.13.0")
