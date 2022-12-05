93ea43e2155dedb4e29a5c52ec108368c84fc23a
Rick Izzo <rick@grid.ai>
2022-12-05 15:09:51 -0500

    fix windows cli

--- tests/tests_app/cli/test_cmd_cli_delete.py
+++ tests/tests_app/cli/test_cmd_cli_delete.py
@@ -1,10 +1,13 @@
+import sys
 from unittest import mock
 
+import pytest
 from lightning_cloud.openapi import Externalv1Cluster, Externalv1LightningappInstance, V1ClusterSpec, V1ClusterType
 
 from lightning_app.cli.lightning_cli_delete import _find_cluster_for_user, _find_selected_app_instance_id
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="currently not supported for windows.")
 @mock.patch("lightning_app.cli.lightning_cli_delete.AWSClusterManager.list_clusters")
 def test_find_cluster_for_user_when_provided_valid_cluster_id(list_clusters_mock: mock.MagicMock):
     list_clusters_mock.return_value = [
@@ -25,6 +28,7 @@ def test_find_cluster_for_user_when_provided_valid_cluster_id(list_clusters_mock
     assert returned_cluster_id == "custom"
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="currently not supported for windows.")
 @mock.patch("lightning_app.cli.lightning_cli_delete.AWSClusterManager.list_clusters")
 def test_find_cluster_for_user_without_cluster_id_uses_default(list_clusters_mock: mock.MagicMock):
     list_clusters_mock.return_value = [
@@ -39,6 +43,7 @@ def test_find_cluster_for_user_without_cluster_id_uses_default(list_clusters_moc
     assert returned_cluster_id == "default"
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="currently not supported for windows.")
 @mock.patch("lightning_app.cli.lightning_cli_delete.AWSClusterManager.list_clusters")
 @mock.patch("lightning_app.cli.lightning_cli_delete.inquirer")
 def test_find_cluster_for_user_without_valid_cluster_id_asks_if_they_meant_to_use_valid(
@@ -57,6 +62,7 @@ def test_find_cluster_for_user_without_valid_cluster_id_asks_if_they_meant_to_us
     inquirer_mock.assert_called()
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="currently not supported for windows.")
 @mock.patch("lightning_app.cli.lightning_cli_delete._AppManager.list_apps")
 def test_app_find_selected_app_instance_id_when_app_name_exists(list_apps_mock: mock.MagicMock):
     list_apps_mock.return_value = [
@@ -67,6 +73,7 @@ def test_app_find_selected_app_instance_id_when_app_name_exists(list_apps_mock: 
     list_apps_mock.assert_called_once_with(cluster_id="default-cluster")
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="currently not supported for windows.")
 @mock.patch("lightning_app.cli.lightning_cli_delete._AppManager.list_apps")
 def test_app_find_selected_app_instance_id_when_app_id_exists(list_apps_mock: mock.MagicMock):
     list_apps_mock.return_value = [
