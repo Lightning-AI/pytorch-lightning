852089e0567e6dc56489ff96b6d5ca59326bec7b
thomas chaton <thomas@grid.ai>
2022-12-06 17:25:42 +0000

    [App] Add ready property to the flow (#15921)

--- src/lightning_app/core/flow.py
+++ src/lightning_app/core/flow.py
@@ -231,6 +231,12 @@ class LightningFlow:
         return self.__getattribute__(item)
 
     @property
+    def ready(self) -> bool:
+        """Override to customize when your App should be ready."""
+        flows = self.flows
+        return all(flow.ready for flow in flows.values()) if flows else True
+
+    @property
     def changes(self):
         return self._changes.copy()
 
