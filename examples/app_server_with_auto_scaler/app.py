1d4e85d33e650ae6db65a85c6eafe173b394021a
Akihiro Nitta <nitta@akihironitta.com>
2022-12-09 03:35:41 +0900

    Update example

--- examples/app_server_with_auto_scaler/app.py
+++ examples/app_server_with_auto_scaler/app.py
@@ -36,9 +36,6 @@ class PyTorchServer(L.app.components.PythonServer):
         self._model = torchvision.models.resnet18(pretrained=True).to(self._device)
 
     def predict(self, requests: BatchRequestModel):
-        import time
-
-        time.sleep(60)
         transforms = torchvision.transforms.Compose(
             [
                 torchvision.transforms.Resize(224),
@@ -63,16 +60,14 @@ class MyAutoScaler(L.app.components.AutoScaler):
         """The default scaling logic that users can override."""
         # scale out if the number of pending requests exceeds max batch size.
         max_requests_per_work = self.max_batch_size
-        pending_requests_per_running_or_pending_work = metrics["pending_requests"] / (
-            replicas + metrics["pending_works"]
-        )
-        if pending_requests_per_running_or_pending_work >= max_requests_per_work:
+        pending_requests_per_work = metrics["pending_requests"] / (replicas + metrics["pending_works"])
+        if pending_requests_per_work >= max_requests_per_work:
             return replicas + 1
 
         # scale in if the number of pending requests is below 25% of max_requests_per_work
         min_requests_per_work = max_requests_per_work * 0.25
-        pending_requests_per_running_work = metrics["pending_requests"] / replicas
-        if pending_requests_per_running_work < min_requests_per_work:
+        pending_requests_per_work = metrics["pending_requests"] / replicas
+        if pending_requests_per_work < min_requests_per_work:
             return replicas - 1
 
         return replicas
@@ -80,17 +75,17 @@ class MyAutoScaler(L.app.components.AutoScaler):
 
 app = L.LightningApp(
     MyAutoScaler(
-        # server class and args
+        # work class and args
         PyTorchServer,
-        cloud_compute=L.CloudCompute("default"),
+        cloud_compute=L.CloudCompute("gpu"),
         # autoscaler specific args
         min_replicas=1,
         max_replicas=4,
-        autoscale_interval=1,
+        autoscale_interval=10,
         endpoint="predict",
         input_type=RequestModel,
         output_type=Any,
         timeout_batching=1,
-        max_batch_size=1,
+        max_batch_size=8,
     )
 )
