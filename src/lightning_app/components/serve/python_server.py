e0ef90c567cddfd00a0027fb8794736f97851fdc
hhsecond <sherin@grid.ai>
2022-12-09 17:29:04 +0000

    import fix

--- src/lightning_app/components/serve/python_server.py
+++ src/lightning_app/components/serve/python_server.py
@@ -72,7 +72,7 @@ class PythonServer(LightningWork, abc.ABC):
 
     _start_method = "spawn"
 
-    @requires(["torch", "lightning_api_access"])
+    @requires(["torch"])
     def __init__(  # type: ignore
         self,
         input_type: type = _DefaultInputData,
@@ -193,29 +193,27 @@ class PythonServer(LightningWork, abc.ABC):
         fastapi_app.post("/predict", response_model=output_type)(predict_fn)
 
     def configure_layout(self) -> None:
-        if module_available("lightning_api_access"):
-            from lightning_api_access import APIAccessFrontend
+        from lightning_api_access import APIAccessFrontend
+        class_name = self.__class__.__name__
+        url = f"{self.url}/predict"
 
-            class_name = self.__class__.__name__
-            url = f"{self.url}/predict"
+        try:
+            request = self._get_sample_dict_from_datatype(self.configure_input_type())
+            response = self._get_sample_dict_from_datatype(self.configure_output_type())
+        except TypeError:
+            return None
 
-            try:
-                request = self._get_sample_dict_from_datatype(self.configure_input_type())
-                response = self._get_sample_dict_from_datatype(self.configure_output_type())
-            except TypeError:
-                return None
-
-            return APIAccessFrontend(
-                apis=[
-                    {
-                        "name": class_name,
-                        "url": url,
-                        "method": "POST",
-                        "request": request,
-                        "response": response,
-                    }
-                ]
-            )
+        return APIAccessFrontend(
+            apis=[
+                {
+                    "name": class_name,
+                    "url": url,
+                    "method": "POST",
+                    "request": request,
+                    "response": response,
+                }
+            ]
+        )
 
     def run(self, *args: Any, **kwargs: Any) -> Any:
         """Run method takes care of configuring and setting up a FastAPI server behind the scenes.
