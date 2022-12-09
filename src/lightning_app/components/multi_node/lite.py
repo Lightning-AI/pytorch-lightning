8f61e36c860e4d2cd13f909a63888a28131a0c76
awaelchli <aedu.waelchli@gmail.com>
2022-12-09 16:23:16 +0100

    more fixes

--- src/lightning_app/components/multi_node/lite.py
+++ src/lightning_app/components/multi_node/lite.py
@@ -39,7 +39,7 @@ class _LiteRunExecutor(_PyTorchSpawnRunExecutor):
         for pkg_name in ("lightning.fabric", "lightning_" + "fabric"):
             try:
                 pkg = importlib.import_module(pkg_name)
-                lites.append(pkg.LightningFabric)
+                lites.append(pkg.Fabric)
                 strategies.append(pkg.strategies.DDPSpawnShardedStrategy)
                 strategies.append(pkg.strategies.DDPSpawnStrategy)
                 mps_accelerators.append(pkg.accelerators.MPSAccelerator)
