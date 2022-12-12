4b33c35bf3be4a5b39edf2223f9ed1278fd7d62b
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-12 18:57:42 +0100

    Revert broken change

--- src/lightning_app/runners/cloud.py
+++ src/lightning_app/runners/cloud.py
@@ -529,7 +529,7 @@ class CloudRuntime(Runtime):
                 f"Your application folder '{root.absolute()}' is more than {CLOUD_UPLOAD_WARNING} MB. "
                 f"The total size is {round(app_folder_size_in_mb, 2)} MB. {len(files)} files were uploaded.\n"
                 + largest_paths_msg
-                + " Perhaps you should try running the app in an empty directory."
+                + "Perhaps you should try running the app in an empty directory."
             )
             if not (root / DOT_IGNORE_FILENAME).is_file():
                 warning_msg += (
