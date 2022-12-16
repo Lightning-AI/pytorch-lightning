bdeb1f54bde2f08f317df0fe3b20c6bf5db283c7
Sherin Thomas <sherin@lightning.ai>
2022-12-16 17:19:00 +0000

    Update src/lightning_app/components/serve/__init__.py

--- src/lightning_app/components/serve/__init__.py
+++ src/lightning_app/components/serve/__init__.py
@@ -1,4 +1,5 @@
 from lightning_app.components.serve.python_server import Category, Image, Number, Text, PythonServer, AutoScaler
+from lightning_app.components.serve.gradio import ServeGradio
 from lightning_app.components.serve.streamlit import ServeStreamlit
 
 __all__ = ["ServeGradio", "ServeStreamlit", "PythonServer", "Image", "Number", "Category", "Text", "AutoScaler"]
