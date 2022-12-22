d2a21f70578d38c1ca4bb7f0563f245f855d9168
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2022-12-22 10:34:11 +0100

    Apply suggestions from code review

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -117,7 +117,7 @@ def load_requirements(
     path_dir: str,
     file_name: str = "base.txt",
     comment_char: str = "#",
-    unfreeze: str = "all",
+    unfreeze: str = "all"
 ) -> List[str]:
     """Loading requirements from a file.
 
