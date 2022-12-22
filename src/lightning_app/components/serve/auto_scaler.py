04e6e4e0d7588943c9fb1ff8dd893f5fbf73a5d8
Dmitry Frolov <dmitsf@gmail.com>
2022-12-22 13:12:54 -0500

    Apply suggestions from code review
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- src/lightning_app/components/serve/auto_scaler.py
+++ src/lightning_app/components/serve/auto_scaler.py
@@ -430,9 +430,9 @@ class _LoadBalancer(LightningWork):
         """
         old_servers = set(self._servers)
         server_urls: List[str] = [
-            f"http://{server._internal_ip}:{server._port}"
+            f"http://{server._internal_ip}:{server.port}"
             for server in server_works
-            if server._internal_ip and server._port
+            if server._internal_ip
         ]
         new_servers = set(server_urls)
 
