177895aa63497e4a144cabb092c92e651afe5af4
awaelchli <aedu.waelchli@gmail.com>
2022-12-22 22:11:51 +0100

    fixes

--- src/pytorch_lightning/trainer/connectors/accelerator_connector.py
+++ src/pytorch_lightning/trainer/connectors/accelerator_connector.py
@@ -75,7 +75,6 @@ from pytorch_lightning.strategies import (
     TPUSpawnStrategy,
 )
 from pytorch_lightning.strategies.ddp_spawn import _DDP_FORK_ALIASES
-from pytorch_lightning.tuner.auto_gpu_select import pick_multiple_gpus
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from pytorch_lightning.utilities.imports import _IPU_AVAILABLE
 from pytorch_lightning.utilities.rank_zero import rank_zero_deprecation, rank_zero_info, rank_zero_warn
