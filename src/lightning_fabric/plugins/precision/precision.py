c03b41f077a0f8593ae1a2ea560653627ace69ac
awaelchli <aedu.waelchli@gmail.com>
2023-01-06 17:11:00 +0100

    typing why..

--- src/lightning_fabric/plugins/precision/precision.py
+++ src/lightning_fabric/plugins/precision/precision.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import contextlib
-from typing import Any, Dict, Generator, Optional
+from typing import Any, Dict, Generator, Optional, Union
 
 import torch
 from torch import Tensor
