9cf55aff3d2d5d0eed0baf408564cc8f6befa2e2
lightningforever <adrian@lightning.ai>
2023-01-10 02:11:29 +0100

    test

--- tests/tests_pytorch/core/test_lightning_module.py
+++ tests/tests_pytorch/core/test_lightning_module.py
@@ -512,13 +512,41 @@ def test_fabric_attributes():
 
     fabric = Fabric()
     wrapped_module, wrapped_optimizer = fabric.setup(module, optimizer)
-    assert module.fabric is fabric
-    assert module._fabric_optimizers == [wrapped_optimizer]
+    assert wrapped_module.fabric is fabric
+    assert wrapped_module._fabric_optimizers == [wrapped_optimizer]
 
     # Attribute access on LightningModule.trainer gets redirected to Fabric
-    assert isinstance(module.trainer, _TrainerFabricShim)
-    assert module.trainer.global_rank == 0
+    assert isinstance(wrapped_module.trainer, _TrainerFabricShim)
+    assert wrapped_module.trainer.global_rank == 0
     with pytest.raises(AttributeError, match="Your LightningModule code tried to access `self.trainer.current_epoch`"):
-        _ = module.trainer.current_epoch
+        _ = wrapped_module.trainer.current_epoch
 
-    assert module.optimizers() == wrapped_optimizer
+    assert wrapped_module.optimizers() == wrapped_optimizer
+
+
+def test_fabric_logger_access():
+    """Test that the logger attribute can be accessed when the LightningModule is used together with Fabric."""
+    # No logger
+    module = BoringModel()
+    fabric = Fabric()
+    wrapped_module = fabric.setup(module)
+    assert wrapped_module.loggers == []
+    with pytest.raises(IndexError):
+        _ = wrapped_module.logger
+
+    # Single Logger
+    logger = Mock()
+    module = BoringModel()
+    fabric = Fabric(loggers=logger)
+    wrapped_module = fabric.setup(module)
+    assert wrapped_module.logger == logger
+    assert wrapped_module.loggers == [logger]
+
+    # Multiple loggers
+    logger1 = Mock()
+    logger2 = Mock()
+    module = BoringModel()
+    fabric = Fabric(loggers=[logger1, logger2])
+    wrapped_module = fabric.setup(module)
+    assert wrapped_module.logger == logger1
+    assert wrapped_module.loggers == [logger1, logger2]
