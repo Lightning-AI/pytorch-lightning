464c847c41189eafea2173e6c90962ae5c89a82a
Carlos Mocholí <carlossmocholi@gmail.com>
2023-01-11 18:07:03 +0100

    Remove the deprecated `resume_from_checkpoint` Trainer argument (#16167)

--- tests/tests_pytorch/deprecated_api/test_remove_2-0.py
+++ tests/tests_pytorch/deprecated_api/test_remove_2-0.py
@@ -68,48 +68,7 @@ from pytorch_lightning.utilities.distributed import (
 from pytorch_lightning.utilities.optimizer import optimizer_to_device, optimizers_to_device
 from pytorch_lightning.utilities.seed import pl_worker_init_function, reset_seed, seed_everything
 from pytorch_lightning.utilities.xla_device import inner_f, pl_multi_process, XLADeviceUtils
-from tests_pytorch.callbacks.test_callbacks import OldStatefulCallback
 from tests_pytorch.helpers.runif import RunIf
-
-
-def test_v2_0_0_resume_from_checkpoint_trainer_constructor(tmpdir):
-    # test resume_from_checkpoint still works until v2.0 deprecation
-    model = BoringModel()
-    callback = OldStatefulCallback(state=111)
-    trainer = Trainer(default_root_dir=tmpdir, max_steps=1, callbacks=[callback])
-    trainer.fit(model)
-    ckpt_path = trainer.checkpoint_callback.best_model_path
-
-    callback = OldStatefulCallback(state=222)
-    with pytest.deprecated_call(match=r"Setting `Trainer\(resume_from_checkpoint=\)` is deprecated in v1.5"):
-        trainer = Trainer(default_root_dir=tmpdir, max_steps=2, callbacks=[callback], resume_from_checkpoint=ckpt_path)
-    with pytest.deprecated_call(match=r"trainer.resume_from_checkpoint` is deprecated in v1.5"):
-        _ = trainer.resume_from_checkpoint
-    assert trainer._checkpoint_connector.resume_checkpoint_path is None
-    assert trainer._checkpoint_connector.resume_from_checkpoint_fit_path == ckpt_path
-    trainer.validate(model=model, ckpt_path=ckpt_path)
-    assert callback.state == 222
-    assert trainer._checkpoint_connector.resume_checkpoint_path is None
-    assert trainer._checkpoint_connector.resume_from_checkpoint_fit_path == ckpt_path
-    with pytest.deprecated_call(match=r"trainer.resume_from_checkpoint` is deprecated in v1.5"):
-        trainer.fit(model)
-    ckpt_path = trainer.checkpoint_callback.best_model_path  # last `fit` replaced the `best_model_path`
-    assert callback.state == 111
-    assert trainer._checkpoint_connector.resume_checkpoint_path is None
-    assert trainer._checkpoint_connector.resume_from_checkpoint_fit_path is None
-    trainer.predict(model=model, ckpt_path=ckpt_path)
-    assert trainer._checkpoint_connector.resume_checkpoint_path is None
-    assert trainer._checkpoint_connector.resume_from_checkpoint_fit_path is None
-    trainer.fit(model)
-    assert trainer._checkpoint_connector.resume_checkpoint_path is None
-    assert trainer._checkpoint_connector.resume_from_checkpoint_fit_path is None
-
-    # test fit(ckpt_path=) precedence over Trainer(resume_from_checkpoint=) path
-    model = BoringModel()
-    with pytest.deprecated_call(match=r"Setting `Trainer\(resume_from_checkpoint=\)` is deprecated in v1.5"):
-        trainer = Trainer(resume_from_checkpoint="trainer_arg_path")
-    with pytest.raises(FileNotFoundError, match="Checkpoint at fit_arg_ckpt_path not found. Aborting training."):
-        trainer.fit(model, ckpt_path="fit_arg_ckpt_path")
 
 
 def test_v2_0_0_callback_on_load_checkpoint_hook(tmpdir):
