0876a6412d448c8903cf05a6ca9058023d6e4e5d
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-01-12 20:28:09 +0900

    fix import removed `DDPShardedStrategy` (#16341)

--- tests/tests_app/components/multi_node/test_fabric.py
+++ tests/tests_app/components/multi_node/test_fabric.py
@@ -98,6 +98,3 @@ def test_trainer_run_executor_arguments_choices(args_given: dict, args_expected:
 def test_run_executor_invalid_strategy_instances():
     with pytest.raises(ValueError, match="DDP Spawned strategies aren't supported yet."):
         _, _ = _get_args_after_tracer_injection(strategy=lf.strategies.DDPStrategy(start_method="spawn"))
-
-    with pytest.raises(ValueError, match="DDP Spawned strategies aren't supported yet."):
-        _, _ = _get_args_after_tracer_injection(strategy=lf.strategies.DDPShardedStrategy(start_method="spawn"))
