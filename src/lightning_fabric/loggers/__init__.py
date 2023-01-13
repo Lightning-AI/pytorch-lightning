8f1269283f3ae8365d746b335a47c2766180b933
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-01-13 14:09:44 +0100

    Add CSVLogger for Lightning Lite (#16346)
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>

--- src/lightning_fabric/loggers/__init__.py
+++ src/lightning_fabric/loggers/__init__.py
@@ -10,5 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from lightning_fabric.loggers.csv_logs import CSVLogger  # noqa: F401
 from lightning_fabric.loggers.logger import Logger  # noqa: F401
 from lightning_fabric.loggers.tensorboard import TensorBoardLogger  # noqa: F401
