68aba0b55854574cb8e8ec29d919f6f64e94f20c
awaelchli <aedu.waelchli@gmail.com>
2023-01-15 13:39:24 +0100

    unused import

--- tests/tests_pytorch/strategies/test_ddp_spawn_strategy.py
+++ tests/tests_pytorch/strategies/test_ddp_spawn_strategy.py
@@ -11,10 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from datetime import timedelta
 from unittest import mock
-from unittest.mock import Mock
 
 import pytest
 import torch
