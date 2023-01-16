850e498a346e2ee5425b68acb88ce3b1abd02a89
Carlos Mocholí <carlossmocholi@gmail.com>
2023-01-16 16:31:30 +0100

    Remove the deprecated `AllGatherGrad` class (#16360)

--- src/pytorch_lightning/utilities/__init__.py
+++ src/pytorch_lightning/utilities/__init__.py
@@ -17,7 +17,6 @@ import numpy
 
 from lightning_fabric.utilities import LightningEnum  # noqa: F401
 from lightning_fabric.utilities import move_data_to_device  # noqa: F401
-from pytorch_lightning.utilities.distributed import AllGatherGrad  # noqa: F401
 from pytorch_lightning.utilities.enums import GradClipAlgorithmType  # noqa: F401
 from pytorch_lightning.utilities.grads import grad_norm  # noqa: F401
 from pytorch_lightning.utilities.imports import (  # noqa: F401
