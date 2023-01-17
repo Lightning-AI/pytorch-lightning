ecc8c27620bce7bc2f1bf83e27511fb93671565b
Carlos Mocholí <carlossmocholi@gmail.com>
2023-01-17 17:42:37 +0100

    Fix logger import

--- src/pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
+++ src/pytorch_lightning/trainer/connectors/logger_connector/logger_connector.py
@@ -18,12 +18,11 @@ from lightning_utilities.core.rank_zero import WarningCache
 from torch import Tensor
 
 import pytorch_lightning as pl
-from lightning_fabric.loggers import CSVLogger
 from lightning_fabric.loggers.tensorboard import _TENSORBOARD_AVAILABLE, _TENSORBOARDX_AVAILABLE
 from lightning_fabric.plugins.environments import SLURMEnvironment
 from lightning_fabric.utilities import move_data_to_device
 from lightning_fabric.utilities.apply_func import convert_tensors_to_scalars
-from pytorch_lightning.loggers import Logger, TensorBoardLogger
+from pytorch_lightning.loggers import CSVLogger, Logger, TensorBoardLogger
 from pytorch_lightning.trainer.connectors.logger_connector.result import _METRICS, _OUT_DICT, _PBAR_DICT
 
 warning_cache = WarningCache()
