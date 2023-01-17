957d884cb1d60fd33d880c84ee89a1921b7cc7c4
Carlos Mocholí <carlossmocholi@gmail.com>
2023-01-17 17:41:50 +0100

    Revert "fix `dirpath` in `log_dir` for CSVLogger (#16401)"
    This reverts commit d46091c32d3a09bc4300b933636a77f5ec55c2ba.

--- src/pytorch_lightning/trainer/trainer.py
+++ src/pytorch_lightning/trainer/trainer.py
@@ -53,6 +53,7 @@ from pytorch_lightning.callbacks import Callback, Checkpoint, EarlyStopping, Pro
 from pytorch_lightning.callbacks.prediction_writer import BasePredictionWriter
 from pytorch_lightning.core.datamodule import LightningDataModule
 from pytorch_lightning.loggers import Logger
+from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 from pytorch_lightning.loops import PredictionLoop, TrainingEpochLoop
 from pytorch_lightning.loops.dataloader.evaluation_loop import EvaluationLoop
 from pytorch_lightning.loops.fit_loop import FitLoop
@@ -1806,8 +1807,10 @@ class Trainer:
     @property
     def log_dir(self) -> Optional[str]:
         if len(self.loggers) > 0:
-            logger_ = self.loggers[0]
-            dirpath = getattr(logger_, "log_dir", None) or getattr(logger_, "save_dir", None)
+            if not isinstance(self.loggers[0], TensorBoardLogger):
+                dirpath = self.loggers[0].save_dir
+            else:
+                dirpath = self.loggers[0].log_dir
         else:
             dirpath = self.default_root_dir
 
