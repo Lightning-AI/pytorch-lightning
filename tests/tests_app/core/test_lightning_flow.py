60f9406dffe624528a52358ec238ce3b2c5f6201
Justus Schock <12886177+justusschock@users.noreply.github.com>
2023-01-17 15:13:05 +0100

    rename app `_exit` 2/2 (#16398)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>

--- tests/tests_app/core/test_lightning_flow.py
+++ tests/tests_app/core/test_lightning_flow.py
@@ -954,3 +954,9 @@ def test_structures_register_work_cloudcompute():
         assert len(v.component_names) == 1
         assert v.component_names[0][:-1] in ("root.w_list.", "root.w_dict.")
         assert v.component_names[0][-1].isdigit()
+
+
+def test_deprecation_warning_exit():
+    with pytest.raises(ExitAppException):
+        with pytest.warns(DeprecationWarning, match="*Use LightningFlow.stop instead"):
+            RootFlowReady()._exit()
