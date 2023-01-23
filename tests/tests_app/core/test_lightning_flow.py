e72d7a2069a1bd4d898783ca3b94539e45d0df43
thomas <thomas@thomass-MacBook-Pro.local>
2023-01-23 09:35:39 +0000

    update

--- tests/tests_app/core/test_lightning_flow.py
+++ tests/tests_app/core/test_lightning_flow.py
@@ -648,7 +648,7 @@ class FlowSchedule(LightningFlow):
             if len(self._last_times) < 3:
                 self._last_times.append(time())
             else:
-                # TODO: Resolve
+                # TODO: Resolve scheduling
                 assert abs((time() - self._last_times[-1]) - self.target) < 20
                 self.stop()
 
