1f003b8c5d2132106f2223c7c2112d25367297d4
Jirka <jirka.borovec@seznam.cz>
2023-01-24 17:23:28 +0100

    debug

--- tests/tests_cloud/test_requirements.py
+++ tests/tests_cloud/test_requirements.py
@@ -24,8 +24,8 @@ def test_requirements_as_a_file(version: str = "latest", model_name: str = "bori
     download_from_lightning_cloud(f"{_USERNAME}/{model_name}")
 
     req_folder_path = os.path.join(STORAGE_DIR, _USERNAME, model_name, version)
-    assert os.path.isdir(req_folder_path)
-    assert "requirements.txt" in os.listdir(req_folder_path)
+    assert os.path.isdir(req_folder_path), "missing: %s" % req_folder_path
+    assert "requirements.txt" in os.listdir(req_folder_path), "among files: %r" % os.listdir(req_folder_path)
 
 
 def test_requirements_as_a_list(version: str = "1.0.0", model_name: str = "boring_model"):
@@ -45,8 +45,8 @@ def test_requirements_as_a_list(version: str = "1.0.0", model_name: str = "borin
     download_from_lightning_cloud(f"{_USERNAME}/{model_name}", version=version)
 
     req_folder_path = os.path.join(STORAGE_DIR, _USERNAME, model_name, version)
-    assert os.path.isdir(req_folder_path)
-    assert "requirements.txt" in os.listdir(req_folder_path)
+    assert os.path.isdir(req_folder_path), "missing: %s" % req_folder_path
+    assert "requirements.txt" in os.listdir(req_folder_path), "among files: %r" % os.listdir(req_folder_path)
 
     with open(f"{req_folder_path}/requirements.txt") as req_file:
         reqs = req_file.readlines()
