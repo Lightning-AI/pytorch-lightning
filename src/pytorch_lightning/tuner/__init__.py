ec6cbdf57f00cc5f09953283e1907cfb60968db1
awaelchli <aedu.waelchli@gmail.com>
2023-01-27 03:47:35 +0100

    remove resolved todo for circular import

--- src/pytorch_lightning/tuner/__init__.py
+++ src/pytorch_lightning/tuner/__init__.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: This is causing a circular import
 from pytorch_lightning.tuner.tuning import Tuner
