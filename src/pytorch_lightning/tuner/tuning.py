32af540f694ba35b770dd79eabbf6836daddf652
awaelchli <aedu.waelchli@gmail.com>
2023-01-27 02:03:13 +0100

    docstring

--- src/pytorch_lightning/tuner/tuning.py
+++ src/pytorch_lightning/tuner/tuning.py
@@ -49,20 +49,14 @@ class Tuner:
 
         Args:
             model: Model to tune.
-
             train_dataloaders: A collection of :class:`torch.utils.data.DataLoader` or a
                 :class:`~pytorch_lightning.core.datamodule.LightningDataModule` specifying training samples.
                 In the case of multiple dataloaders, please see this :ref:`section <multiple-dataloaders>`.
-
             val_dataloaders: A :class:`torch.utils.data.DataLoader` or a sequence of them specifying validation samples.
-
             dataloaders: A :class:`torch.utils.data.DataLoader` or a sequence of them specifying val/test/predict
                 samples used for running tuner on validation/testing/prediction.
-
             datamodule: An instance of :class:`~pytorch_lightning.core.datamodule.LightningDataModule`.
-
             method: Method to run tuner on. It can be any of ``("fit", "validate", "test", "predict")``.
-
             mode: Search strategy to update the batch size:
 
                 - ``'power'``: Keep multiplying the batch size by 2, until we get an OOM error.
@@ -72,12 +66,9 @@ class Tuner:
             steps_per_trial: number of steps to run with a given batch size.
                 Ideally 1 should be enough to test if an OOM error occurs,
                 however in practise a few are needed
-
             init_val: initial batch size to start the search with
-
             max_trials: max number of increases in batch size done before
                algorithm is terminated
-
             batch_arg_name: name of the attribute that stores the batch size.
                 It is expected that the user has provided a model or datamodule that has a hyperparameter
                 with that name. We will look for this attribute name in the following places
@@ -134,24 +125,17 @@ class Tuner:
 
         Args:
             model: Model to tune.
-
             train_dataloaders: A collection of :class:`torch.utils.data.DataLoader` or a
                 :class:`~pytorch_lightning.core.datamodule.LightningDataModule` specifying training samples.
                 In the case of multiple dataloaders, please see this :ref:`section <multiple-dataloaders>`.
-
             val_dataloaders: A :class:`torch.utils.data.DataLoader` or a sequence of them specifying validation samples.
-
             dataloaders: A :class:`torch.utils.data.DataLoader` or a sequence of them specifying val/test/predict
                 samples used for running tuner on validation/testing/prediction.
-
             datamodule: An instance of :class:`~pytorch_lightning.core.datamodule.LightningDataModule`.
-
+            method: Method to run tuner on. It can be any of ``("fit", "validate", "test", "predict")``.
             min_lr: minimum learning rate to investigate
-
             max_lr: maximum learning rate to investigate
-
             num_training: number of learning rates to test
-
             mode: Search strategy to update learning rate after each batch:
 
                 - ``'exponential'``: Increases the learning rate exponentially.
@@ -160,12 +144,13 @@ class Tuner:
             early_stop_threshold: Threshold for stopping the search. If the
                 loss at any point is larger than early_stop_threshold*best_loss
                 then the search is stopped. To disable, set to None.
-
             update_attr: Whether to update the learning rate attribute or not.
+            attr_name: Name of the attribute which stores the learning rate. The names 'learning_rate' or 'lr' get
+                automatically detected. Otherwise, set the name here.
 
         Raises:
             MisconfigurationException:
-                If learning rate/lr in ``model`` or ``model.hparams`` isn't overridden when ``auto_lr_find=True``,
+                If learning rate/lr in ``model`` or ``model.hparams`` isn't overridden,
                 or if you are using more than one optimizer.
         """
         if method != "fit":
