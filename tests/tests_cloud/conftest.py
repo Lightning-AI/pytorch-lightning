c0c68616dad904895e75682474d8a16f768cb5c7
Jirka <jirka.borovec@seznam.cz>
2023-01-27 19:54:54 +0100

    conftest

---
+++ tests/tests_cloud/conftest.py
@@ -0,0 +1,16 @@
+import sys
+import tempfile
+import importlib
+
+import pytest
+
+import lightning
+import lightning.store.save
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clean_home(monkeypatch):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        monkeypatch.setattr(lightning.store.save, '_LIGHTNING_STORAGE_DIR', tmpdirname)
+        importlib.reload(sys.modules['lightning.store.save'])
+        yield tmpdirname
