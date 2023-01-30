a2ab59dcf5fc18a6513999dbd5a408799084c718
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-01-31 02:12:39 +0900

    update version for Fabric CLI (#16556)
    (cherry picked from commit 86d31eb8d28bc4a252c3881746c076475c2a06f3)

--- src/lightning_app/cli/lightning_cli.py
+++ src/lightning_app/cli/lightning_cli.py
@@ -389,8 +389,8 @@ def run_app(
     )
 
 
-if RequirementCache("lightning-fabric>=1.9.0.dev0") or RequirementCache("lightning>=1.9.0.dev0"):
-    # lightning.fabric.cli may not be available when installing only standalone lightning-app package
+if RequirementCache("lightning-fabric>=1.9.0") or RequirementCache("lightning>=1.9.0"):
+    # note it is automatically replaced to `from lightning.fabric.cli` when building monolithic/mirror package
     from lightning_fabric.cli import _run_model
 
     run.add_command(_run_model)
