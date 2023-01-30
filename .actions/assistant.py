53ea8cfebfa6e874b2f7dc7430d6fcbb09c151d5
Jirka <jirka.borovec@seznam.cz>
2023-01-30 15:40:28 +0100

    ci + assistant

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -256,7 +256,7 @@ def _retrieve_files(directory: str, *ext: str) -> List[str]:
     return all_files
 
 
-def _replace_imports(lines: List[str], mapping: List[Tuple[str, str]]) -> List[str]:
+def _replace_imports(lines: List[str], mapping: List[Tuple[str, str]], lightning_by: str = "") -> List[str]:
     """Replace imports of standalone package to lightning.
 
     >>> lns = [
@@ -268,27 +268,36 @@ def _replace_imports(lines: List[str], mapping: List[Tuple[str, str]]) -> List[s
     ...     "lightning_apps = []",
     ...     "lightning_app and pytorch_lightning are ours",
     ...     "def _lightning_app():",
-    ...     ":class:`~lightning_app.core.flow.LightningFlow`"
+    ...     ":class:`~lightning_app.core.flow.LightningFlow`",
+    ...     "from lightning import __version__",
+    ...     "@lightning.ai"
     ... ]
     >>> mapping = [("lightning_app", "lightning.app"), ("pytorch_lightning", "lightning.pytorch")]
-    >>> _replace_imports(lns, mapping)  # doctest: +NORMALIZE_WHITESPACE
+    >>> _replace_imports(lns, mapping, lightning_by="lightning_fabric")  # doctest: +NORMALIZE_WHITESPACE
     ['"lightning_app"', 'lightning.app', 'lightning_app/', 'delete_cloud_lightning_apps', 'from lightning.app import', \
      'lightning_apps = []', 'lightning.app and lightning.pytorch are ours', 'def _lightning_app():', \
-     ':class:`~lightning.app.core.flow.LightningFlow`']
+     ':class:`~lightning.app.core.flow.LightningFlow`', 'from lightning_fabric import __version__', '@lightning.ai']
     """
     out = lines[:]
     for source_import, target_import in mapping:
         for i, ln in enumerate(out):
             out[i] = re.sub(
-                rf"([^_]|^){source_import}([^_\w\"/]|$)",
+                rf"([^_/@]|^){source_import}([^_\w\"/]|$)",
                 rf"\1{target_import}\2",
                 ln,
             )
+            if lightning_by:  # in addition, replace base package
+                out[i] = out[i].replace("from lightning import ", f"from {lightning_by} import ")
+                out[i] = out[i].replace("import lightning ", f"import {lightning_by} ")
     return out
 
 
 def copy_replace_imports(
-    source_dir: str, source_imports: List[str], target_imports: List[str], target_dir: Optional[str] = None
+    source_dir: str,
+    source_imports: List[str],
+    target_imports: List[str],
+    target_dir: Optional[str] = None,
+    lightning_by: str = "",
 ) -> None:
     """Copy package content with import adjustments."""
     print(f"Replacing imports: {locals()}")
@@ -318,7 +327,7 @@ def copy_replace_imports(
                 # a binary file, skip
                 print(f"Skipped replacing imports for {fp}")
                 continue
-        lines = _replace_imports(lines, list(zip(source_imports, target_imports)))
+        lines = _replace_imports(lines, list(zip(source_imports, target_imports)), lightning_by=lightning_by)
         os.makedirs(os.path.dirname(fp_new), exist_ok=True)
         with open(fp_new, "w", encoding="utf-8") as fo:
             fo.writelines(lines)
@@ -337,11 +346,12 @@ def create_mirror_package(source_dir: str, package_mapping: Dict[str, str], reve
 
     for pkg_to, pkg_from in mapping.items():
         if pkg_to.split(".")[-1] in reverse:
-            imports_src_ = imports_tgt + ["lightning"]
-            imports_tgt_ = imports_src + [pkg_from]
+            imports_src_, imports_tgt_ = imports_tgt, imports_src
             pkg_to, pkg_from = pkg_from, pkg_to
+            lightning_by = pkg_from
         else:
             imports_src_, imports_tgt_ = imports_src, imports_tgt
+            lightning_by = ""
 
         copy_replace_imports(
             source_dir=os.path.join(source_dir, pkg_from.replace(".", os.sep)),
@@ -349,6 +359,7 @@ def create_mirror_package(source_dir: str, package_mapping: Dict[str, str], reve
             source_imports=imports_src_,
             target_imports=imports_tgt_,
             target_dir=os.path.join(source_dir, pkg_to.replace(".", os.sep)),
+            lightning_by=lightning_by,
         )
 
 
@@ -393,12 +404,18 @@ class AssistantCLI:
 
     @staticmethod
     def copy_replace_imports(
-        source_dir: str, source_import: str, target_import: str, target_dir: Optional[str] = None
+        source_dir: str,
+        source_import: str,
+        target_import: str,
+        target_dir: Optional[str] = None,
+        lightning_by: str = "",
     ) -> None:
         """Copy package content with import adjustments."""
         source_imports = source_import.strip().split(",")
         target_imports = target_import.strip().split(",")
-        copy_replace_imports(source_dir, source_imports, target_imports, target_dir=target_dir)
+        copy_replace_imports(
+            source_dir, source_imports, target_imports, target_dir=target_dir, lightning_by=lightning_by
+        )
 
 
 if __name__ == "__main__":
