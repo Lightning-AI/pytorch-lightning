d521f2bc99544bf1e4dce83634b6d17c157d29d9
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-01-30 20:02:15 +0900

    store: mock/fixture home (#16536)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/tests_cloud/test_versioning.py
+++ tests/tests_cloud/test_versioning.py
@@ -3,15 +3,14 @@ import platform
 
 import pytest
 from tests_cloud import _API_KEY, _PROJECT_ID, _USERNAME
-from tests_cloud.helpers import cleanup
 
 from lightning.store.cloud_api import download_from_cloud, upload_to_cloud
-from lightning.store.save import _LIGHTNING_STORAGE_DIR
+from lightning.store.save import __STORAGE_DIR_NAME
 from pytorch_lightning.demos.boring_classes import BoringModel
 
 
-def assert_download_successful(username, model_name, version):
-    folder_name = os.path.join(_LIGHTNING_STORAGE_DIR, username, model_name, version)
+def assert_download_successful(lit_home, username, model_name, version):
+    folder_name = os.path.join(lit_home, __STORAGE_DIR_NAME, username, model_name, version)
     assert os.path.isdir(folder_name), f"Folder name: {folder_name} doesn't exist."
     assert len(os.listdir(folder_name)) != 0
 
@@ -30,12 +29,10 @@ def assert_download_successful(username, model_name, version):
         ]
     ),
 )
-def test_versioning_valid_case(case, expected_case, model_name: str = "boring_model_versioning"):
-    cleanup()
-
+def test_versioning_valid_case(lit_home, case, expected_case, model_name: str = "boring_model_versioning"):
     upload_to_cloud(model_name, version=case, model=BoringModel(), api_key=_API_KEY, project_id=_PROJECT_ID)
     download_from_cloud(f"{_USERNAME}/{model_name}", version=case)
-    assert_download_successful(_USERNAME, model_name, expected_case)
+    assert_download_successful(lit_home, _USERNAME, model_name, expected_case)
 
 
 @pytest.mark.parametrize(
@@ -50,9 +47,7 @@ def test_versioning_valid_case(case, expected_case, model_name: str = "boring_mo
         ]
     ),
 )
-def test_versioning_invalid_case(case, model_name: str = "boring_model_versioning"):
-    cleanup()
-
+def test_versioning_invalid_case(lit_home, case, model_name: str = "boring_model_versioning"):
     with pytest.raises(ConnectionRefusedError):
         upload_to_cloud(model_name, version=case, model=BoringModel(), api_key=_API_KEY, project_id=_PROJECT_ID)
 
