dc38663a030348a106c7a5fb89ef022e29d00b10
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-01-30 20:19:57 +0900

    store: update API in messages (#16535)

--- tests/tests_cloud/test_requirements.py
+++ tests/tests_cloud/test_requirements.py
@@ -2,7 +2,7 @@ import os
 
 from tests_cloud import _API_KEY, _PROJECT_ID, _USERNAME
 
-from lightning.store import download_from_cloud, upload_to_cloud
+from lightning.store import download_model, upload_model
 from lightning.store.save import __STORAGE_DIR_NAME
 from pytorch_lightning.demos.boring_classes import BoringModel
 
@@ -10,7 +10,7 @@ from pytorch_lightning.demos.boring_classes import BoringModel
 def test_requirements(lit_home, version: str = "1.0.0", model_name: str = "boring_model"):
     requirements_list = ["pytorch_lightning==1.7.7", "lightning"]
 
-    upload_to_cloud(
+    upload_model(
         model_name,
         version=version,
         model=BoringModel(),
@@ -19,7 +19,7 @@ def test_requirements(lit_home, version: str = "1.0.0", model_name: str = "borin
         project_id=_PROJECT_ID,
     )
 
-    download_from_cloud(f"{_USERNAME}/{model_name}", version=version)
+    download_model(f"{_USERNAME}/{model_name}", version=version)
 
     req_folder_path = os.path.join(lit_home, __STORAGE_DIR_NAME, _USERNAME, model_name, version)
     assert os.path.isdir(req_folder_path), "missing: %s" % req_folder_path
