23c948f1dd05d861f259b208a2ff1eae0328761d
Ethan Harris <ethanwharris@gmail.com>
2023-02-01 19:23:31 +0000

    Import

--- src/lightning_app/core/__init__.py
+++ src/lightning_app/core/__init__.py
@@ -1,5 +1,6 @@
 from lightning_app.core.app import LightningApp
 from lightning_app.core.flow import LightningFlow
+from lightning_app.core.plugin import Plugin
 from lightning_app.core.work import LightningWork
 
-__all__ = ["LightningApp", "LightningFlow", "LightningWork"]
+__all__ = ["LightningApp", "LightningFlow", "LightningWork", "Plugin"]
