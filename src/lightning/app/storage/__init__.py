7a43a9adc0005421e0063e68953f13e9eb73e86f
thomas <thomas@thomass-MacBook-Pro.local>
2023-02-01 08:57:50 +0000

    update

--- src/lightning/app/storage/__init__.py
+++ src/lightning/app/storage/__init__.py
@@ -3,3 +3,5 @@ from lightning.app.storage.mount import Mount  # noqa: F401
 from lightning.app.storage.orchestrator import StorageOrchestrator  # noqa: F401
 from lightning.app.storage.path import Path  # noqa: F401
 from lightning.app.storage.payload import Payload  # noqa: F401
+from lightning.app.storage.filesystem import FileSystem  # noqa: F401
+
