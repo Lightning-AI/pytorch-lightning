b053e273b3fb007f282eb195c88f4cd5f65ca5d7
thomas <thomas@thomass-MacBook-Pro.local>
2023-02-02 09:10:55 +0000

    update

--- src/lightning/app/cli/commands/pwd.py
+++ src/lightning/app/cli/commands/pwd.py
@@ -6,26 +6,25 @@ from rich.text import Text
 
 from lightning.app.cli.commands.connection import _LIGHTNING_CONNECTION_FOLDER
 from lightning.app.utilities.app_helpers import Logger
+from lightning.app.cli.commands.cd import _CD_FILE
 
 logger = Logger(__name__)
 
 
-def pwd() -> None:
+def pwd() -> str:
 
     with Live(Spinner("point", text=Text("pending...", style="white")), transient=True):
 
-        cd_file = os.path.join(_LIGHTNING_CONNECTION_FOLDER, "cd.txt")
-        root = "."
+        root = "/"
 
-        if not os.path.exists(cd_file):
-            if path.startswith(".."):
-                path = root
-
-            with open(cd_file, "w") as f:
-                f.write(path + "\n")
+        if not os.path.exists(_CD_FILE):
+            with open(_CD_FILE, "w") as f:
+                f.write(root + "\n")
         else:
-            with open(cd_file) as f:
+            with open(_CD_FILE) as f:
                 lines = f.readlines()
                 root = lines[0].replace("\n", "")
 
     print(root)
+
+    return root
--- tests/tests_app/cli/test_cd.py
+++ tests/tests_app/cli/test_cd.py
@@ -1,19 +1,30 @@
 import os
 
 from lightning_app.cli.commands.cd import _CD_FILE, cd
+from lightning_app.cli.commands.pwd import pwd
 
 
 def test_cd():
     """This test validates cd behaves as expected."""
     assert "/" == cd("/")
+    assert "/" == pwd()
     assert "/hero" == cd("hero")
+    assert "/hero" == pwd()
     assert "/hero/something_else" == cd("something_else")
+    assert "/hero/something_else" == pwd()
     assert "/hero/something_else/hello/a" == cd("hello/a")
+    assert "/hero/something_else/hello/a" == pwd()
     assert "/hero/something_else" == cd("../..")
+    assert "/hero/something_else" == pwd()
     assert "/hero" == cd("..")
+    assert "/hero" == pwd()
     assert "/" == cd("/")
+    assert "/" == pwd()
     assert "/a" == cd("../a")
+    assert "/a" == pwd()
     assert "/a/thomas/hello" == cd("thomas/hello")
+    assert "/a/thomas/hello" == pwd()
     assert "/thomas/hello" == cd("/thomas/hello")
+    assert "/thomas/hello" == pwd()
 
     os.remove(_CD_FILE)
