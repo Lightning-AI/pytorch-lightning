3e576f0b89fd737e04c0c40bd7d2225233e086f3
Justus Schock <12886177+justusschock@users.noreply.github.com>
2023-02-03 12:31:28 +0100

    temporarily remove force-killing session procs

--- tests/tests_app/conftest.py
+++ tests/tests_app/conftest.py
@@ -29,31 +29,31 @@ def fn(self, *args, timeout=None, **kwargs):
 Thread._wait_for_tstate_lock = fn
 
 
-def pytest_sessionfinish(session, exitstatus):
-    """Pytest hook that get called after whole test run finished, right before returning the exit status to the
-    system."""
-    # kill all the processes and threads created by parent
-    # TODO this isn't great. We should have each tests doing it's own cleanup
-    current_process = psutil.Process()
-    for child in current_process.children(recursive=True):
-        try:
-            params = child.as_dict() or {}
-            cmd_lines = params.get("cmdline", [])
-            # we shouldn't kill the resource tracker from multiprocessing. If we do,
-            # `atexit` will throw as it uses resource tracker to try to clean up
-            if cmd_lines and "resource_tracker" in cmd_lines[-1]:
-                continue
-            child.kill()
-        except psutil.NoSuchProcess:
-            pass
+# def pytest_sessionfinish(session, exitstatus):
+#     """Pytest hook that get called after whole test run finished, right before returning the exit status to the
+#     system."""
+#     # kill all the processes and threads created by parent
+#     # TODO this isn't great. We should have each tests doing it's own cleanup
+#     current_process = psutil.Process()
+#     for child in current_process.children(recursive=True):
+#         try:
+#             params = child.as_dict() or {}
+#             cmd_lines = params.get("cmdline", [])
+#             # we shouldn't kill the resource tracker from multiprocessing. If we do,
+#             # `atexit` will throw as it uses resource tracker to try to clean up
+#             if cmd_lines and "resource_tracker" in cmd_lines[-1]:
+#                 continue
+#             child.kill()
+#         except psutil.NoSuchProcess:
+#             pass
 
-    main_thread = threading.current_thread()
-    for t in threading.enumerate():
-        if t is not main_thread:
-            t.join(0)
+#     main_thread = threading.current_thread()
+#     for t in threading.enumerate():
+#         if t is not main_thread:
+#             t.join(0)
 
-    for child_pid in _collect_child_process_pids(os.getpid()):
-        os.kill(child_pid, signal.SIGTERM)
+#     for child_pid in _collect_child_process_pids(os.getpid()):
+#         os.kill(child_pid, signal.SIGTERM)
 
 
 @pytest.fixture(scope="function", autouse=True)
