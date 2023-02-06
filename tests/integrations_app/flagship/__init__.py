cca7698722bd71586c4674a804a16391c8c8afad
Justus Schock <justus.schock@posteo.de>
2023-02-06 16:44:04 +0100

    add integration test tldr

--- tests/integrations_app/flagship/__init__.py
+++ tests/integrations_app/flagship/__init__.py
@@ -2,4 +2,4 @@ import os.path
 
 from integrations_app import _PATH_TESTS_DIR
 
-_PATH_INTEGRATIONS_DIR = os.path.join(_PATH_TESTS_DIR, "_flagship-app")
+_PATH_INTEGRATIONS_DIR = os.path.join(_PATH_TESTS_DIR, "_flagship_app")
