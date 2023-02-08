ec3995d407a886272cce1d74ac8d4d3189f4b9fe
Justus Schock <justus.schock@posteo.de>
2023-02-08 16:35:12 +0100

    add integration test tldr

--- tests/integrations_app/flagship/test_tldr.py
+++ tests/integrations_app/flagship/test_tldr.py
@@ -6,22 +6,28 @@ from integrations_app.flagship import _PATH_INTEGRATIONS_DIR
 from lightning.app.testing.testing import run_app_in_cloud
 from lightning.app.utilities.imports import _is_playwright_available
 
-if _is_playwright_available():
-    import playwright
+from textwrap import dedent
 
 
 def test_app_in_cloud():
 
     with open(os.path.join(_PATH_INTEGRATIONS_DIR, "test_app.py"), "w") as f:
-        app_string = """
+        app_string = dedent(
+            dedent(
+        """
+        # !pip install git+https://github.com/Lightning-AI/LAI-TLDR-Component git+https://github.com/Lightning-AI/lightning-LLMs
+        # !curl https://raw.githubusercontent.com/Shivanandroy/T5-Finetuning-PyTorch/main/data/news_summary.csv --create-dirs -o ${HOME}/data/summary/news.csv -C -
         import lightning
         from tests.test_app import DummyTLDR
-        app = lightning.app.LightningApp(
+        from lit_llms.tensorboard import MultiNodeLightningTrainerWithTensorboard
+        app = lightning.LightningApp(
             MultiNodeLightningTrainerWithTensorboard(
                 DummyTLDR, num_nodes=2, cloud_compute=lightning.CloudCompute("gpu-fast-multi", disk_size=50),
             )
         )
         """
+            )
+        )
         f.write(app_string)
 
         with run_app_in_cloud(_PATH_INTEGRATIONS_DIR, "test_app.py") as (
@@ -31,27 +37,14 @@ def test_app_in_cloud():
             name,
         ):
 
-            # def check_training_finished(*_, **__):
-            #     locator = view_page.frame_locator("iframe").locator(
-            #         'ul:has-text("`Trainer.fit` stopped: `max_epochs=2` reached.")'
-            #     )
-            #     if len(locator.all_text_contents()):
-            #         return True
-            #
-            # wait_for(view_page, check_training_finished)
-
-            logs = []
-            while True:
-                sleep(10)
-                try:
-                    logs = list(fetch_logs("multinode.ws.0"))
-                    if any(["`Trainer.fit` stopped: `max_epochs=2` reached." in line for line in logs]):
-                        break
-                except (
-                    playwright._impl._api_types.Error,
-                    playwright._impl._api_types.TimeoutError,
-                ):
-                    pass
+            # Validate the logs.
+            has_logs = False
+            while not has_logs:
+                logs = list(fetch_logs)
+                for log in logs:
+                    if "`Trainer.fit` stopped: `max_epochs=2` reached." in log:
+                        has_logs = True
+                sleep(1)
 
         expected_strings = [
             # don't include values for actual hardware availability as this may depend on environment.
