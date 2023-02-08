2edc907cd2f523d05270dfb3df47c07e9cb43960
Justus Schock <justus.schock@posteo.de>
2023-02-08 17:14:36 +0100

    add integration test tldr

--- tests/integrations_app/flagship/test_tldr.py
+++ tests/integrations_app/flagship/test_tldr.py
@@ -1,5 +1,4 @@
 import os
-from textwrap import dedent
 from time import sleep
 
 from integrations_app.flagship import _PATH_INTEGRATIONS_DIR
@@ -9,26 +8,7 @@ from lightning.app.testing.testing import run_app_in_cloud
 
 def test_app_in_cloud():
 
-    with open(os.path.join(_PATH_INTEGRATIONS_DIR, "test_app.py"), "w") as f:
-        app_string = dedent(
-            dedent(
-                """
-        # !pip install git+https://github.com/Lightning-AI/LAI-TLDR-Component git+https://github.com/Lightning-AI/lightning-LLMs
-        # !curl https://raw.githubusercontent.com/Shivanandroy/T5-Finetuning-PyTorch/main/data/news_summary.csv --create-dirs -o ${HOME}/data/summary/news.csv -C -
-        import lightning
-        from tests.test_app import DummyTLDR
-        from lit_llms.tensorboard import MultiNodeLightningTrainerWithTensorboard
-        app = lightning.LightningApp(
-            MultiNodeLightningTrainerWithTensorboard(
-                DummyTLDR, num_nodes=2, cloud_compute=lightning.CloudCompute("gpu-fast-multi", disk_size=50),
-            )
-        )
-        """
-            )
-        )
-        f.write(app_string)
-
-        with run_app_in_cloud(_PATH_INTEGRATIONS_DIR, "test_app.py") as (
+        with run_app_in_cloud(_PATH_INTEGRATIONS_DIR, "dummy_test_app.py") as (
             _,
             view_page,
             fetch_logs,
