44557b9de8b8d2583623f35e7786bf4907866e9d
Sherin Thomas <sherin@lightning.ai>
2023-02-09 19:21:51 +0530

    [App] Connect and Disconnect node (#16700)
    Connect and Disconnect node

--- src/lightning/app/cli/lightning_cli.py
+++ src/lightning/app/cli/lightning_cli.py
@@ -45,6 +45,7 @@ from lightning.app.cli.connect.app import (
     disconnect_app,
 )
 from lightning.app.cli.connect.data import connect_data
+from lightning.app.cli.connect.node import connect_node, disconnect_node
 from lightning.app.cli.lightning_cli_create import create
 from lightning.app.cli.lightning_cli_delete import delete
 from lightning.app.cli.lightning_cli_list import get_list
@@ -136,11 +137,13 @@ def disconnect() -> None:
 
 connect.command("app")(connect_app)
 disconnect.command("app")(disconnect_app)
-connect.command("data")(connect_data)
-_main.command()(ls)
-_main.command()(cd)
-_main.command()(cp)
-_main.command()(pwd)
+connect.command("node", hidden=True)(connect_node)
+disconnect.command("node", hidden=True)(disconnect_node)
+connect.command("data", hidden=True)(connect_data)
+_main.command(hidden=True)(ls)
+_main.command(hidden=True)(cd)
+_main.command(hidden=True)(cp)
+_main.command(hidden=True)(pwd)
 show.command()(logs)
 
 
