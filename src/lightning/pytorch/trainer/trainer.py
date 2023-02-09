327e341bf0e645c1ce6432e5cbd7366f29a096eb
Carlos Mocholí <carlossmocholi@gmail.com>
2023-02-09 18:01:02 +0100

    Resolve TODO

--- src/lightning/pytorch/trainer/trainer.py
+++ src/lightning/pytorch/trainer/trainer.py
@@ -1216,14 +1216,7 @@ class Trainer:
 
         for callback in self.callbacks:
             with self.profiler.profile(f"[Callback]{callback.state_key}.on_save_checkpoint"):
-                state = callback.on_save_checkpoint(self, self.lightning_module, checkpoint)
-            if state is not None:
-                # TODO: Remove this error message in v2.0
-                raise ValueError(
-                    f"Returning a value from `{callback.__class__.__name__}.on_save_checkpoint` was deprecated in v1.6"
-                    f" and is no longer supported as of v1.8. Please override `Callback.state_dict` to return state"
-                    f" to be saved."
-                )
+                callback.on_save_checkpoint(self, self.lightning_module, checkpoint)
 
         if pl_module:
             # restore current_fx when nested context
