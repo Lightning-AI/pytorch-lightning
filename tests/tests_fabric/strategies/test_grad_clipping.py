186b6a7e17af618ae4989e292f6cd255e6e40d0c
Justus Schock <justus.schock@posteo.de>
2023-02-10 13:30:50 +0100

    update tests for nonfinite grads

--- tests/tests_fabric/strategies/test_grad_clipping.py
+++ tests/tests_fabric/strategies/test_grad_clipping.py
@@ -10,10 +10,19 @@ from lightning.fabric.strategies import DeepSpeedStrategy, FSDPStrategy
 
 class _MyFabricGradNorm(BoringFabric):
     def after_backward(self, model, optimizer):
-        self.strategy.clip_gradients_norm(optimizer, max_norm=0.05, error_if_nonfinite=False)
+        self.strategy.clip_gradients_norm(optimizer, max_norm=0.05, error_if_nonfinite=True)
         parameters = model.parameters()
         grad_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), 2) for p in parameters]), 2)
         torch.testing.assert_close(grad_norm, torch.tensor(0.05, device=self.device))
+
+    def run(self):
+        while True:
+            try:
+                super().run()
+                break
+            except RuntimeError:  # nonfinite grads -> skip and continue
+                pass
+
 
 
 class _MyFabricGradVal(BoringFabric):
