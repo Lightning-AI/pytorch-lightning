930b19749011341819a42b232acc92a922e6d212
Dingu Sagar <dingu.sagar@engati.com>
2023-02-11 11:35:11 +0530

    adding more docs for the TypeError exception scenarios - adding minor version

--- src/lightning/pytorch/trainer/trainer.py
+++ src/lightning/pytorch/trainer/trainer.py
@@ -475,9 +475,9 @@ class Trainer:
 
         Raises:
             TypeError:
-                If ``model`` is not `LightningModule` for torch version less than 2.0
+                If ``model`` is not `LightningModule` for torch version less than 2.0.0
                 and if ``model`` is not `LightningModule` or `torch._dynamo.OptimizedModule` for torch versions
-                greater than or equal to 2.0.
+                greater than or equal to 2.0.0 .
         """
         model = _maybe_unwrap_optimized(model)
         self.strategy._lightning_module = model
