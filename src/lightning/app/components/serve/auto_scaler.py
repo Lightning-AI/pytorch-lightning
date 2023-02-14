a766f9d81772e16b4ee3d9430c3b0fb3ea6ba511
Noha Alon <nohalon@gmail.com>
2023-02-14 16:19:31 +0200

    fix warning in the autoscaler

--- src/lightning/app/components/serve/auto_scaler.py
+++ src/lightning/app/components/serve/auto_scaler.py
@@ -453,7 +453,7 @@ class _LoadBalancer(LightningWork):
         try:
             from lightning_api_access import APIAccessFrontend
         except ModuleNotFoundError:
-            logger.warn("APIAccessFrontend not found. Please install lightning-api-access to enable the UI")
+            logger.warn("APIAccessFrontend not found. To enable the UI run: pip install lightning-api-access")
             return
 
         if is_running_in_cloud():
