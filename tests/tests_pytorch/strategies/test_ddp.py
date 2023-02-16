72fa8264c46eee22fa35c3367975ff59c281dd16
awaelchli <aedu.waelchli@gmail.com>
2023-02-16 07:01:26 +0100

    strategies

--- tests/tests_pytorch/strategies/test_ddp.py
+++ tests/tests_pytorch/strategies/test_ddp.py
@@ -75,7 +75,7 @@ def test_torch_distributed_backend_invalid(cuda_count_2, tmpdir):
 @RunIf(skip_windows=True)
 @mock.patch("torch.cuda.set_device")
 @mock.patch("lightning.pytorch.accelerators.cuda._check_cuda_matmul_precision")
-def test_ddp_torch_dist_is_available_in_setup(_, __, cuda_count_1, tmpdir):
+def test_ddp_torch_dist_is_available_in_setup(_, __, cuda_count_1, mps_count_0, tmpdir):
     """Test to ensure torch distributed is available within the setup hook using ddp."""
 
     class TestModel(BoringModel):
