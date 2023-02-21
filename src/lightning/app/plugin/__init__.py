724cb00ea64ba530a7082ae212624f70048a9bb9
Ethan Harris <ethanwharris@gmail.com>
2023-02-21 22:12:47 +0000

    Imports and typing

--- src/lightning/app/plugin/__init__.py
+++ src/lightning/app/plugin/__init__.py
@@ -1,4 +1,5 @@
 import lightning.app.plugin.actions as actions
+from lightning.app.plugin.actions import NavigateTo, Toast, ToastSeverity
 from lightning.app.plugin.plugin import LightningPlugin
 
-__all__ = ["LightningPlugin", "actions"]
+__all__ = ["LightningPlugin", "actions", "Toast", "ToastSeverity", "NavigateTo"]
