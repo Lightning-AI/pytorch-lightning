34dff1a2432fcf6294c5ad3de23618387e63f5a5
Jirka <jirka.borovec@seznam.cz>
2023-02-23 22:23:34 +0100

    exclude

--- src/lightning_fabric/__about__.py
+++ src/lightning_fabric/__about__.py
@@ -6,11 +6,7 @@ __license__ = "Apache-2.0"
 __copyright__ = f"Copyright (c) 2022-{time.strftime('%Y')}, {__author__}."
 __homepage__ = "https://github.com/Lightning-AI/lightning"
 __docs_url__ = "https://pytorch-lightning.readthedocs.io/en/stable/"
-# TODO
 __docs__ = ""
-__long_docs__ = """
-
-"""
 
 __all__ = [
     "__author__",
