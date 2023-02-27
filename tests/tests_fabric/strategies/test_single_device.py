de9980f14065ebb0c66bdbb2c2e2a13455cbf4a2
Justus Schock <justus.schock@posteo.de>
2023-02-27 23:42:19 +0100

    manually update scaler if necessary

--- tests/tests_fabric/strategies/test_single_device.py
+++ tests/tests_fabric/strategies/test_single_device.py
@@ -75,9 +75,21 @@ class _MyFabricGradNorm(BoringFabric):
             try:
                 super().run()
                 break
-            except RuntimeError as e:  # nonfinite grads -> skip and continue
-                if i > 10:
+            except RuntimeError as e:
+                # nonfinite grads -> skip and continue
+                # this may repeat until the scaler finds a factor where overflow is avoided,
+                # so the while loop should eventually break
+                # stop after a max of 10 tries
+                if i > 10 or not str(e).startswith('The total norm'):
                     raise e
+
+                # unscale was already called by last attempt,
+                # but no update afterwards since optimizer step was missing.
+                # Manually update here -> Need to update inf stats first.
+                scaler = getattr(self._precision, 'scaler', None)
+                if scaler is not None:
+                    scaler._check_inf_per_device(self.optimizer)
+                    scaler.update()
             finally:
                 i += 1
 
@@ -88,37 +100,38 @@ class _MyFabricGradVal(BoringFabric):
             if p.grad is not None and torch.isnan(p.grad).any().item() or torch.isinf(p.grad).any().item():
                 raise RuntimeError("Nonfinite grads")
 
-        self.clip_gradients(model, optimizer, clip_val=1e-10)
+        self.clip_gradients(model, optimizer, clip_val=1e-3)
 
         parameters = model.parameters()
         grad_max_list = [torch.max(p.grad.detach().abs()) for p in parameters]
         grad_max = torch.max(torch.stack(grad_max_list))
-        torch.testing.assert_close(grad_max.abs(), torch.tensor(1e-10, device=self.device))
+        torch.testing.assert_close(grad_max.abs(), torch.tensor(1e-3, device=self.device))
+        print('done')
 
     def run(self):
-        return
         # 10 retries
         i = 0
         while True:
             try:
                 super().run()
                 break
-            except RuntimeError as e:  # nonfinite grads -> skip and continue
-                if i > 10:
+            except RuntimeError as e:
+                # nonfinite grads -> skip and continue
+                # this may repeat until the scaler finds a factor where overflow is avoided,
+                # so the while loop should eventually break
+                # stop after a max of 10 tries
+                if i > 10 or not str(e).startswith('Nonfinite grads'):
                     raise e
+
+                # unscale was already called by last attempt,
+                # but no update afterwards since optimizer step was missing.
+                # Manually update here -> Need to update inf stats first.
+                scaler = getattr(self._precision, 'scaler', None)
+                if scaler is not None:
+                    scaler._check_inf_per_device(self.optimizer)
+                    scaler.update()
             finally:
                 i += 1
-
-    # def run(self):
-    #     while True:
-    #         try:
-    #             super().run()
-    #             break
-    #         except RuntimeError:
-    #             # nonfinite grads -> skip and continue
-    #             # this may repeat until the scaler finds a factor where overflow is avoided,
-    #             # so the while loop should eventually break
-    #             pass
 
 
 @pytest.mark.parametrize(
