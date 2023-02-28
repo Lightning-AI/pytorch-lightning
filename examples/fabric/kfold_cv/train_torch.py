8d4f566a53b2d505f99cca94f46db54f1b1dea1a
Nikhil Shenoy <nikhilshenoy@dhcp-128-189-229-146.ubcsecure.wireless.ubc.ca>
2023-02-28 12:22:35 -0800

    Added torch example

--- examples/fabric/kfold_cv/train_torch.py
+++ examples/fabric/kfold_cv/train_torch.py
@@ -22,11 +22,7 @@ import torch.optim as optim
 import torchvision.transforms as T
 from sklearn import model_selection
 from torch.optim.lr_scheduler import StepLR
-from torchmetrics.classification import Accuracy
 from torchvision.datasets import MNIST
-
-from lightning.fabric import Fabric  # import Fabric
-from lightning.fabric import seed_everything
 
 DATASETS_PATH = path.join(path.dirname(__file__), "..", "..", "..", "Datasets")
 
@@ -57,7 +53,7 @@ class Net(nn.Module):
         return output
 
 
-def train_dataloader(model, data_loader, optimizer, fabric, epoch, hparams, fold):
+def train_dataloader(model, data_loader, optimizer, epoch, hparams, fold):
     # TRAINING LOOP
     model.train()
     for batch_idx, (data, target) in enumerate(data_loader):
@@ -65,7 +61,7 @@ def train_dataloader(model, data_loader, optimizer, fabric, epoch, hparams, fold
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
-        fabric.backward(loss)  # instead of loss.backward()
+        loss.backward()
 
         optimizer.step()
         if (batch_idx == 0) or ((batch_idx + 1) % hparams.log_interval == 0):
@@ -84,47 +80,39 @@ def train_dataloader(model, data_loader, optimizer, fabric, epoch, hparams, fold
             break
 
 
-def validate_dataloader(model, data_loader, fabric, hparams, metric_fn, fold):
+def validate_dataloader(model, data_loader, hparams, fold):
     model.eval()
     loss = 0
+    correct = 0
+    len_ = 0
     with torch.no_grad():
         for data, target in data_loader:
             # NOTE: no need to call `.to(device)` on the data, target
             output = model(data)
             loss += F.nll_loss(output, target, reduction="sum").item()
 
-            # WITHOUT TorchMetrics
-            # pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            # correct += pred.eq(target.view_as(pred)).sum().item()
-
-            # WITH TorchMetrics
-            metric_fn(output, target)
+            # compute accuracy
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            len_ += pred.shape[0]
 
             if hparams.dry_run:
                 break
 
-    # all_gather is used to aggregated the value across processes
-    loss = fabric.all_gather(loss).sum() / len(data_loader.dataset)
+    # compute average loss
+    loss /= len_
 
-    # val acc
-    acc = metric_fn.compute()
-
-    print(f"\nFor fold: {fold} Validation set: Average loss: {loss:.4f}, Accuracy: ({100 * acc:.0f}%)\n")
-    metric_fn.reset()
+    # compute acc
+    acc = 100.0 * correct / len_
+    print(f"\nFor fold: {fold} Validation set: Average loss: {loss:.4f}, Accuracy: ({acc:.0f}%)\n")
 
 
 def run(hparams):
-    # Create the Lightning Fabric object. The parameters like accelerator, strategy, devices etc. will be proided
-    # by the command line. See all options: `lightning run model --help`
-    fabric = Fabric()
+    torch.manual_seed(hparams.seed)
 
-    seed_everything(hparams.seed)  # instead of torch.manual_seed(...)
-
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
     transform = T.Compose([T.ToTensor(), T.Normalize((0.1307,), (0.3081,))])
-    # This is meant to ensure the data are download only by 1 process.
-    if fabric.is_global_zero:
-        MNIST(DATASETS_PATH, download=True)
-    fabric.barrier()
 
     # initialize dataset
     dataset = MNIST(DATASETS_PATH, train=True, transform=transform)
@@ -142,46 +130,31 @@ def run(hparams):
         train_loader = torch.utils.data.DataLoader(dataset, batch_size=hparams.batch_size, sampler=train_sampler)
         val_loader = torch.utils.data.DataLoader(dataset, batch_size=hparams.batch_size, sampler=val_sampler)
 
-        # don't forget to call `setup_dataloaders` to prepare for dataloaders for distributed training.
-        train_loader, val_loader = fabric.setup_dataloaders(train_loader, val_loader)
-
-        model = Net()  # remove call to .to(device)
+        model = Net().to(device)
         optimizer = optim.Adadelta(model.parameters(), lr=hparams.lr)
 
-        # don't forget to call `setup` to prepare for model / optimizer for distributed training.
-        # the model is moved automatically to the right device.
-        model, optimizer = fabric.setup(model, optimizer)
-
         scheduler = StepLR(optimizer, step_size=1, gamma=hparams.gamma)
-
-        # use torchmetrics instead of manually computing the accuracy
-        test_acc = Accuracy(task="multiclass", num_classes=10).to(fabric.device)
 
         # EPOCH LOOP
         for epoch in range(1, hparams.epochs + 1):
 
             # TRAINING LOOP
-            train_dataloader(model, train_loader, optimizer, fabric, epoch, hparams, fold)
+            train_dataloader(model, train_loader, optimizer, epoch, hparams, fold)
 
             scheduler.step()
 
             # VALIDATION LOOP
-            validate_dataloader(model, val_loader, fabric, hparams, test_acc, fold)
+            validate_dataloader(model, val_loader, hparams, fold)
 
         if hparams.dry_run:
             break
 
-    # When using distributed training, use `fabric.save`
-    # to ensure the current process is allowed to save a checkpoint
     if hparams.save_model:
-        fabric.save(model.state_dict(), "mnist_cnn.pt")
+        torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == "__main__":
-    # Arguments can be passed in through the CLI as normal and will be parsed here
-    # Example:
-    # lightning run model image_classifier.py accelerator=cuda --epochs=3
-    parser = argparse.ArgumentParser(description="Fabric MNIST K-Fold Cross Validation Example")
+    parser = argparse.ArgumentParser(description="Pytorch MNIST K-Fold Cross Validation Example")
     parser.add_argument(
         "--batch-size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
     )
