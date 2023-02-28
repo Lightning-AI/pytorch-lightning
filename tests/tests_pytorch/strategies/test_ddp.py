647c9b3dbb45b7241fc1d8c5dc55a7e7a6fd33b1
Carlos Mocholí <carlossmocholi@gmail.com>
2023-02-28 15:13:49 +0100

    Revert "Expected not required?"
    This reverts commit 6547e840edb67ee90ab748d03c82d0defa567e42.

--- tests/tests_pytorch/strategies/test_ddp.py
+++ tests/tests_pytorch/strategies/test_ddp.py
@@ -18,6 +18,7 @@ import torch
 from torch.nn.parallel.distributed import DistributedDataParallel
 
 import lightning.pytorch as pl
+from lightning.fabric.utilities.imports import _TORCH_GREATER_EQUAL_2_0
 from lightning.pytorch import seed_everything, Trainer
 from lightning.pytorch.callbacks import Callback
 from lightning.pytorch.demos.boring_classes import BoringModel
@@ -99,8 +100,11 @@ def test_ddp_wrapper(tmpdir, precision):
     class CustomCallback(Callback):
         def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
             assert isinstance(trainer.strategy.model, DistributedDataParallel)
-            assert trainer.strategy.model.parameters_to_ignore == ["module.something"]
-            assert trainer.strategy.model.module._ddp_params_and_buffers_to_ignore == ["module.something"]
+            expected = ["module.something"]
+            assert (
+                trainer.strategy.model.parameters_to_ignore == set(expected) if _TORCH_GREATER_EQUAL_2_0 else expected
+            )
+            assert trainer.strategy.model.module._ddp_params_and_buffers_to_ignore == expected
 
     model = CustomModel()
     trainer = Trainer(
