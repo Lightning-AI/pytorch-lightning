7bb867860174d4071fa9276b91275945582a78df
Justus Schock <justus.schock@posteo.de>
2023-03-03 12:35:06 +0100

    update trainer

--- examples/fabric/byot/trainer.py
+++ examples/fabric/byot/trainer.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import Mapping
 from functools import partial
 from typing import Any, cast, List, Literal, Optional, Tuple, Union
@@ -41,6 +42,8 @@ class Trainer:
         val_sanity_check: int = 2,
         validation_frequency: int = 1,
         use_distributed_sampler: bool = True,
+        checkpoint_dir: str = "./checkpoints",
+        checkpoint_frequency: int = 1,
     ) -> None:
 
         self.fabric = Fabric(
@@ -56,12 +59,17 @@ class Trainer:
         self.grad_accum_steps: int = grad_accum_steps
         self.current_epoch = 0
 
-        # TODO: Expose as arguments
         self.max_epochs = max_epochs
         self.max_steps = max_steps
         self.should_stop = False
 
-        # TODO:validation that int or float('inf')
+        # ensures limit_X_batches is either int or inf
+        if not isinstance(limit_train_batches, int):
+            assert limit_train_batches == float('inf')
+
+        if not isinstance(limit_val_batches, int):
+            assert limit_val_batches == float('inf')
+
         self.limit_train_batches = limit_train_batches
         self.limit_val_batches = limit_val_batches
         self.val_sanity_check = val_sanity_check
@@ -69,6 +77,9 @@ class Trainer:
         self.use_distributed_sampler = use_distributed_sampler
         self._current_train_return: Union[torch.Tensor, Mapping[str, Any]] = {}
         self._current_val_return: Optional[Union[torch.Tensor, Mapping[str, Any]]] = {}
+
+        self.checkpoint_dir = checkpoint_dir
+        self.checkpoint_frequency = checkpoint_frequency
 
     def fit(
         self, model: LightningModule, train_loader: DataLoader, val_loader: DataLoader, ckpt_path: Optional[str] = None
@@ -79,25 +90,39 @@ class Trainer:
         # TODO: Have fabric launch the loop function directly?
         self.fabric.launch()
 
-        # TODO: load checkpoint if exists
-
+        # setup dataloaders
         train_loader = self.fabric.setup_dataloaders(train_loader, use_distributed_sampler=self.use_distributed_sampler)
         if val_loader is not None:
             val_loader = self.fabric.setup_dataloaders(val_loader, use_distributed_sampler=self.use_distributed_sampler)
 
-        # TODO: raise error if multiple optimizers
-        # TODO: support lr scheduler
-        optimizer, scheduler = self._parse_optimizers_schedulers(model.configure_optimizers())
 
+        # setup model and optimizer
         if isinstance(self.fabric.strategy, FSDPStrategy):
             model = self.fabric.setup_module(model)
-            # FIXME: optimizer setup with wrapped model not supported through fabric with configure_optimizers
-            raise NotImplementedError("BYOT currently does not support fsdp")
+            # FIXME: currently, there is no way to support fsdp with model.configure_optimizers in fabric
+            # as it would require fabric to hold a reference to the model, which we don't want to.
+            raise NotImplementedError("BYOT currently does not support FSDP")
+            optimizer, scheduler_cfg = self._parse_optimizers_schedulers(model.configure_optimizers())
             optimizer = self.fabric.setup_optimizers(optimizer)
         else:
+            optimizer, scheduler_cfg = self._parse_optimizers_schedulers(model.configure_optimizers())
+            assert optimizer is not None
             model, optimizer = self.fabric.setup(model, optimizer)
 
-        if self.val_sanity_check and val_loader is not None:
+        # assemble state (current epoch and global step will be added in save)
+        state = {"model": model, "optim": optimizer, "scheduler": scheduler_cfg}
+
+        # load last checkpoint if available
+        latest_checkpoint_path = self.get_latest_checkpoint(self.checkpoint_dir)
+        if latest_checkpoint_path is not None:
+            self.load(state, latest_checkpoint_path)
+
+            # check if we even need to train here
+            if self.max_epochs is not None and self.current_epoch >= self.max_epochs:
+                self.should_stop = True
+
+        # Run validation sanity check
+        if self.val_sanity_check and val_loader is not None and not self.should_stop:
             self.val_loop(model, val_loader, limit_batches=self.val_sanity_check)
 
         # TODO: should this be a for loop?
@@ -106,15 +131,18 @@ class Trainer:
                 model, optimizer, train_loader, limit_batches=self.limit_train_batches, scheduler_cfg=scheduler_cfg
             )
 
-            if self.should_eval:
+            if self.should_validate:
                 self.val_loop(model, val_loader, limit_batches=self.limit_val_batches)
 
             self.step_scheduler(model, scheduler_cfg, level="epoch", current_value=self.current_epoch)
-            # TODO: Checkpointing
 
             self.current_epoch += 1
-            if self.max_epochs is not None and self.current_epoch > self.max_epochs:
+            
+            # stopping condition on epoch level
+            if self.max_epochs is not None and self.current_epoch >= self.max_epochs:
                 self.should_stop = True
+
+            self.save(state)
 
     def train_loop(
         self,
@@ -125,12 +153,11 @@ class Trainer:
         scheduler_cfg: Optional[Mapping[str, Union[LRScheduler, bool, str, int]]] = None,
     ):
         self.fabric.call("on_train_epoch_start")
+        iterable = self.pbar_wrapper(
+            train_loader, total=min(len(train_loader), limit_batches), desc=f"Epoch {self.current_epoch}"
+        )
 
-        for batch_idx, batch in enumerate(
-            self.pbar_wrapper(
-                train_loader, total=min(len(train_loader), limit_batches), desc=f"Epoch {self.current_epoch}"
-            )
-        ):
+        for batch_idx, batch in enumerate(iterable):
             # end epoch if stopping training completely or max batches for this epoch reached
             if self.should_stop or batch_idx >= limit_batches:
                 self.fabric.call("on_train_epoch_end")
@@ -138,37 +165,53 @@ class Trainer:
 
             self.fabric.call("on_train_batch_start", batch, batch_idx)
 
-            # TODO: transfer_batch_to_device_hooks don't make sense due to fabric wrapping DataLoader
-
             # check if optimizer should step in gradient accumulation
-            should_optim_step = self.grad_accum_steps % self.global_step == 0
+            should_optim_step = self.global_step % self.grad_accum_steps == 0
             if should_optim_step:
                 # currently only supports a single optimizer
                 self.fabric.call("on_before_optimizer_step", optimizer, 0)
                 # TODO: (lower priority) support optimizer without closure?
+                
+                # optimizer step runs train step internally through closure
                 optimizer.step(
                     partial(self.training_step, model=model, optimizer=optimizer, batch=batch, batch_idx=batch_idx)
                 )
+                self.fabric.call("on_before_zero_grad", optimizer)
+
+                optimizer.zero_grad()
+
             else:
+                # gradient accumulation -> no optimizer step
                 self.training_step(model=model, optimizer=optimizer, batch=batch, batch_idx=batch_idx)
 
             self.fabric.call("on_train_batch_end", self._current_train_return, batch, batch_idx)
 
-            self.step_scheduler(model, scheduler_cfg, level="step", current_value=self.global_step)
+            # this guard ensures, we only step the scheduler once per global step
+            if should_optim_step:
+                self.step_scheduler(model, scheduler_cfg, level="step", current_value=self.global_step)
+
+            # add output values to progress bar
+            self._format_iterable(iterable, self._current_train_return, "train")
 
             # only increase global step if optimizer stepped
             self.global_step += int(should_optim_step)
+
+            # stopping criterion on step level
             if self.max_steps is not None and self.global_step >= self.max_steps:
                 self.should_stop = True
+                break
 
         self.fabric.call("on_train_epoch_end")
 
     def val_loop(
         self, model: LightningModule, val_loader: Optional[DataLoader], limit_batches: Union[int, float] = float("inf")
     ):
+        # no validation if val_loader wasn't passed
         if val_loader is None:
             return
-        elif val_loader is not None and is_overridden("validation_step", _unwrap_objects(model), LightningModule):
+
+        # no validation but warning if val_loader was passed, but validation_step not implemented
+        elif val_loader is not None and not is_overridden("validation_step", _unwrap_objects(model), LightningModule):
             rank_zero_warn(
                 "Your LightningModule does not have a validation_step implemented, "
                 "but you passed a validation dataloder. Skipping Validation."
@@ -182,9 +225,9 @@ class Trainer:
 
         self.fabric.call("on_validation_epoch_start")
 
-        for batch_idx, batch in enumerate(
-            self.pbar_wrapper(val_loader, total=min(len(val_loader), limit_batches), desc="Validation")
-        ):
+        iterable = self.pbar_wrapper(val_loader, total=min(len(val_loader), limit_batches), desc="Validation Sanity Check" if self.global_step == 0 else "Validation")
+
+        for batch_idx, batch in enumerate(iterable):
 
             # end epoch if stopping training completely or max batches for this epoch reached
             if self.should_stop or batch_idx >= limit_batches:
@@ -194,10 +237,13 @@ class Trainer:
             self.fabric.call("on_validation_batch_start", batch, batch_idx)
 
             out = model.validation_step(batch, batch_idx)
+            # avoid gradients in stored/accumulated values -> prevents potential OOM
             out = apply_to_collection(out, torch.Tensor, lambda x: x.detach())
 
             self.fabric.call("on_validation_batch_end", out, batch, batch_idx)
             self._current_val_return = out
+
+            self._format_iterable(iterable, self._current_val_return, "val")
 
         self.fabric.call("on_validation_epoch_end")
 
@@ -210,15 +256,13 @@ class Trainer:
 
         loss = outputs if isinstance(outputs, torch.Tensor) else outputs["loss"]
 
-        self.fabric.call("on_before_zero_grad", optimizer)
-
-        optimizer.zero_grad()
-
         self.fabric.call("on_before_backward", loss)
         self.fabric.backward(loss)
         self.fabric.call("on_after_backward")
         # TODO: reroute configure_gradient_clipping in LM to fabric.gradient clipping
         # self.fabric.call('configure_gradient_clipping')
+        
+        # avoid gradients in stored/accumulated values -> prevents potential OOM
         self._current_train_return = apply_to_collection(outputs, dtype=torch.Tensor, function=lambda x: x.detach())
 
         return loss
@@ -230,28 +274,33 @@ class Trainer:
         level: Literal["step", "epoch"],
         current_value: int,
     ):
+        # no scheduler
         if scheduler_cfg is None:
             return
 
+        # wrong interval (step vs. epoch)
         if scheduler_cfg["interval"] != level:
             return
 
-        if cast(int, scheduler_cfg["frequency"]) % current_value != 0:
+        # right interval, but wrong step wrt frequency
+        if current_value % cast(int, scheduler_cfg['frequency']) != 0:
             return
 
+        # assemble potential monitored values
         possible_monitor_vals = {None: None}
         if isinstance(self._current_train_return, torch.Tensor):
             possible_monitor_vals.update("train_loss", self._current_train_return)
         elif isinstance(self._current_train_return, Mapping):
-            possible_monitor_vals.update(self._current_train_return)
+            possible_monitor_vals.update({"train_" + k: v for k, v in self._current_train_return.items()})
 
         if isinstance(self._current_val_return, torch.Tensor):
             possible_monitor_vals.update("val_loss", self._current_val_return)
         elif isinstance(self._current_val_return, Mapping):
-            possible_monitor_vals.update(self._current_val_return)
+            possible_monitor_vals.update({"val_" + k: v for k, v in self._current_val_return.items()})
 
-        monitor = {**self._current_train_return, **self._current_val_return}.get(scheduler_cfg["monitor"], None)
+        monitor = {**self._current_train_return, **self._current_val_return}[scheduler_cfg["monitor"]]
 
+        # rely on model hook for actual step
         model.lr_scheduler_step(scheduler_cfg["scheduler"], monitor)
 
     @property
@@ -260,8 +309,35 @@ class Trainer:
 
     def pbar_wrapper(self, iterable, total, **kwargs):
         if self.fabric.is_global_zero:
-            return tqdm(iterable, total, **kwargs)
+            return tqdm(iterable, total=total, **kwargs)
         return iterable
+
+    def load(self, state, path):
+        if state is None:
+            state = {}
+
+        remainder = self.fabric.load(path, state)
+        self.global_step = remainder.pop("global_step")
+        self.current_epoch = remainder.pop("current_epoch")
+
+        if remainder:
+            raise RuntimeError(f"Unused Checkpoint Values: {remainder}")
+
+    def save(self, state):
+        if state is None:
+            state = {}
+
+        state.update(global_step=self.global_step, current_epoch=self.current_epoch)
+
+        self.fabric.save(os.path.join(self.checkpoint_dir, f"epoch-{self.current_epoch:04d}.ckpt"), state)
+
+    @staticmethod
+    def get_latest_checkpoint(checkpoint_dir):
+        if not os.path.isdir(checkpoint_dir):
+            return None
+
+        items = sorted(os.listdir(checkpoint_dir))
+        return os.path.join(checkpoint_dir, items[-1])
 
     def _parse_optimizers_schedulers(
         self, configure_optim_output
@@ -282,6 +358,7 @@ class Trainer:
             _lr_sched_defaults.update(configure_optim_output)
             return None, _lr_sched_defaults
 
+        # list or tuple
         elif isinstance(configure_optim_output, (list, tuple)):
             if all([isinstance(_opt_cand, Optimizable) for _opt_cand in configure_optim_output]):
                 # single optimizer in list
@@ -305,3 +382,19 @@ class Trainer:
                 return opt_cands, lr_cands
 
         return None, None
+
+    @staticmethod
+    def _format_iterable(
+        prog_bar, candidates: Optional[Union[torch.Tensor, Mapping[str, Union[torch.Tensor, float, int]]]], prefix: str
+    ):
+        if isinstance(prog_bar, tqdm) and candidates is not None:
+            postfix_str = ""
+            float_candidates = apply_to_collection(candidates, torch.Tensor, lambda x: x.item())
+            if isinstance(candidates, torch.Tensor):
+                postfix_str += f" {prefix}_loss: {float_candidates:.3f}"
+            elif isinstance(candidates, Mapping):
+                for k, v in float_candidates.items():
+                    postfix_str += f" {prefix}_{k}: {v:.3f}"
+
+            if postfix_str:
+                prog_bar.set_postfix_str(postfix_str)
