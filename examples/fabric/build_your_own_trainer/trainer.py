fbdce76d29ff75cf9cf3eaa8e88a9f80073fb22d
Justus Schock <12886177+justusschock@users.noreply.github.com>
2023-03-06 09:43:50 +0100

    Update examples/fabric/build_your_own_trainer/trainer.py
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- examples/fabric/build_your_own_trainer/trainer.py
+++ examples/fabric/build_your_own_trainer/trainer.py
@@ -4,8 +4,7 @@ from functools import partial
 from typing import Any, cast, Iterable, List, Literal, Optional, Tuple, Union
 
 import torch
-from lightning_utilities.core import is_overridden
-from lightning_utilities.core.apply_func import apply_to_collection
+from lightning_utilities.core import is_overridden, apply_to_collection
 from tqdm import tqdm
 
 import lightning as L
