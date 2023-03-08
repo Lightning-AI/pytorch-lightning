ca500fd558c7bd7bcce18aacee95a816f84a4329
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-08 13:13:08 +0100

    Make `apply_to_collection` importable from top level (#120)

--- src/lightning_utilities/__init__.py
+++ src/lightning_utilities/__init__.py
@@ -3,6 +3,7 @@
 import os
 
 from lightning_utilities.__about__ import *  # noqa: F401, F403
+from lightning_utilities.core.apply_func import apply_to_collection
 from lightning_utilities.core.enums import StrEnum
 from lightning_utilities.core.imports import compare_version, module_available
 from lightning_utilities.core.overrides import is_overridden
@@ -13,6 +14,7 @@ _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
 
 
 __all__ = [
+    "apply_to_collection",
     "StrEnum",
     "module_available",
     "compare_version",
--- src/lightning_utilities/core/__init__.py
+++ src/lightning_utilities/core/__init__.py
@@ -1,10 +1,12 @@
 """Core utilities."""
+from lightning_utilities.core.apply_func import apply_to_collection
 from lightning_utilities.core.enums import StrEnum
 from lightning_utilities.core.imports import compare_version, module_available
 from lightning_utilities.core.overrides import is_overridden
 from lightning_utilities.core.rank_zero import WarningCache
 
 __all__ = [
+    "apply_to_collection",
     "StrEnum",
     "module_available",
     "compare_version",
