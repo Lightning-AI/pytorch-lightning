5bd3aa571eb9e873dbbef0daf688036f98d4271e
Jirka <jirka.borovec@seznam.cz>
2023-03-15 19:25:45 +0100

    cleaning

--- docs/rtfd-build.sh
+++ docs/rtfd-build.sh
@@ -1,8 +1,12 @@
+# building for PRs and skip stable and latest states
+
 if ! [ $READTHEDOCS_VERSION == "latest" -o $READTHEDOCS_VERSION == "stable" ];
 then
     cd ./docs/source-pytorch ;
-    PL_FAST_DOCS_DEV=1 make html --jobs 2 ;
+    export PL_FAST_DOCS_DEV=1 ;
+    make html --jobs  $(nproc) ;
     ls -lh ../build
 else
+    echo "Void build... :-]" ;
     mkdir -p ./docs/build/html
 fi
