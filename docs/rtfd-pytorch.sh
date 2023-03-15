ac404a59edcca285672e8d14fbab75128b227932
Jirka <jirka.borovec@seznam.cz>
2023-03-15 08:47:00 +0100

    sudo

--- docs/rtfd-pytorch.sh
+++ docs/rtfd-pytorch.sh
@@ -5,7 +5,8 @@ python --version
 pip --version
 ls -lh .
 export PACKAGE_NAME=pytorch
-pip install -e . -r requirements/pytorch/docs.txt -f "https://download.pytorch.org/whl/cpu/torch_stable.html"
+sudo pip install -q -r requirements/pytorch/docs.txt
+pip install -e . -q -f "https://download.pytorch.org/whl/cpu/torch_stable.html"
 pip list
 
 cd ./docs/source-pytorch
