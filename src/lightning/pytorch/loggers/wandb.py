9adb78a1f67b73963a85d9b04b2083c44cf434bf
Bharat Ramanathan <ramanathan.parameshwaran@gohuddl.com>
2023-03-16 16:50:28 +0530

    feat(integrations): add audio logging feature to wandb logger

--- src/lightning/pytorch/loggers/wandb.py
+++ src/lightning/pytorch/loggers/wandb.py
@@ -483,6 +483,22 @@ class WandbLogger(Logger):
         metrics = {key: [wandb.Image(img, **kwarg) for img, kwarg in zip(images, kwarg_list)]}
         self.log_metrics(metrics, step)
 
+    @rank_zero_only
+    def log_audio(self, key: str, audios: List[Any], step: Optional[int] = None, **kwargs: Any) -> None:
+        """Log audios (numpy arrays, or file paths).
+
+        Optional kwargs are lists passed to each audio (ex: caption, sample_rate).
+        """
+        if not isinstance(audios, list):
+            raise TypeError(f'Expected a list as "audios", found {type(audios)}')
+        n = len(audios)
+        for k, v in kwargs.items():
+            if len(v) != n:
+                raise ValueError(f"Expected {n} items but only found {len(v)} for {k}")
+        kwarg_list = [{k: kwargs[k][i] for k in kwargs.keys()} for i in range(n)]
+        metrics = {key: [wandb.Audio(img, **kwarg) for img, kwarg in zip(audios, kwarg_list)]}
+        self.log_metrics(metrics, step)
+
     @property
     def save_dir(self) -> Optional[str]:
         """Gets the save directory.
