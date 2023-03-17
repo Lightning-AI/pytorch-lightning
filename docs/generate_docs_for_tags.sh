2c96881ab02d67184aae759a64b6e365083e1bb3
Jirka <jirka.borovec@seznam.cz>
2023-03-17 17:27:47 +0100

    path

--- docs/generate_docs_for_tags.sh
+++ docs/generate_docs_for_tags.sh
@@ -2,18 +2,18 @@
 # Usage:
 # 1. Generate docs with one or more specified versions:
 #    export PACKAGE_NAME=app
-#    bash generate_docs_from_tags.sh 1.9.3 1.9.2 1.9.1 1.9.0
+#    bash docs/generate_docs_from_tags.sh 1.9.3 1.9.2 1.9.1 1.9.0
 set -e
 
-PATH_HERE=$(cd $(dirname $0); pwd -P)
-PATH_ROOT=$PATH_HERE/..
-PATH_ENV=$PATH_HERE/venv-docs
+PATH_ROOT=$(cd $(dirname $0); pwd -P)
+PATH_DOCS=$PATH_ROOT/docs
+PATH_ENV=$PATH_ROOT/venv-docs
 # export PACKAGE_NAME=app
 export FREEZE_REQUIREMENTS=1
-export PYTHONPATH=$(dirname $PATH_HERE)  # for `import tests_pytorch`
+export PYTHONPATH=$(dirname $PATH_DOCS)  # for `import tests_pytorch`
 
-echo PATH_HERE: $PATH_HERE
 echo PATH_ROOT: $PATH_ROOT
+echo PATH_DOCS: $PATH_DOCS
 echo PATH_ENV: $PATH_ENV
 echo PYTHONPATH: $PYTHONPATH
 
@@ -26,10 +26,10 @@ function build_docs {
 	pip install -e . --quiet -r requirements/$PACKAGE_NAME/docs.txt -f pypi -f https://download.pytorch.org/whl/cpu/torch_stable.html
   pip list
 
-  cd $PATH_HERE
+  cd $PATH_DOCS
 	cd /source-$PACKAGE_NAME && make html --jobs $(nproc)
 
-  mv $PATH_HERE/build/html $PATH_HERE/builds-$PACKAGE_NAME/$tag
+  mv $PATH_DOCS/build/html $PATH_DOCS/builds-$PACKAGE_NAME/$tag
 }
 
 # iterate over all arguments assuming that each argument is version
