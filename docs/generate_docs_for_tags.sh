258031abf7acada5c74384dc184d34ed70e366d5
Jirka <jirka.borovec@seznam.cz>
2023-03-17 18:18:15 +0100

    cloning

--- docs/generate_docs_for_tags.sh
+++ docs/generate_docs_for_tags.sh
@@ -5,15 +5,12 @@
 #    bash docs/generate_docs_for_tags.sh 1.9.3 1.9.2 1.9.1 1.9.0
 set -e
 
-PATH_ROOT=$(cd $(dirname $0); pwd -P)
-PATH_DOCS=$PATH_ROOT/docs
+PATH_ROOT=~/Desktop/builds
 PATH_ENV=$PATH_ROOT/venv-docs
 # export PACKAGE_NAME=app
 export FREEZE_REQUIREMENTS=1
-export PYTHONPATH=$(dirname $PATH_DOCS)  # for `import tests_pytorch`
 
 echo PATH_ROOT: $PATH_ROOT
-echo PATH_DOCS: $PATH_DOCS
 echo PATH_ENV: $PATH_ENV
 echo PYTHONPATH: $PYTHONPATH
 
@@ -21,15 +18,16 @@ function build_docs {
   python --version
   pip --version
 
-  cd $PATH_ROOT
   pip install -q setuptools wheel python-multipart
-	pip install -e . --quiet -r requirements/$PACKAGE_NAME/docs.txt -f pypi -f https://download.pytorch.org/whl/cpu/torch_stable.html
+	pip install -e . -q -r requirements/$PACKAGE_NAME/docs.txt -f ../pypi -f https://download.pytorch.org/whl/cpu/torch_stable.html
   pip list
 
-  cd $PATH_DOCS
-	cd /source-$PACKAGE_NAME && make html --jobs $(nproc)
+	cd docs/source-$PACKAGE_NAME
+	make html --jobs $(nproc)
+	cd ../..
 
-  mv $PATH_DOCS/build/html $PATH_DOCS/builds-$PACKAGE_NAME/$tag
+  mkdir -p $PATH_ROOT/docs-$PACKAGE_NAME
+  mv docs/build/html $PATH_ROOT/docs-$PACKAGE_NAME/$tag
 }
 
 # iterate over all arguments assuming that each argument is version
@@ -42,10 +40,15 @@ do
   python -m venv $PATH_ENV
   source $PATH_ENV/bin/activate
 
-  git checkout $tag
-  # git pull --recurse-submodules
+  cd $PATH_ROOT
+  git clone --depth 1 --single-branch --branch $tag https://github.com/Lightning-AI/lightning.git
+  cd lightning
+  git pull --recurse-submodules
 
   build_docs > "building-$PACKAGE_NAME_${tag}.log"
+
+  cd ..
+  rm -rf lightning
 
   deactivate
   rm -rf $PATH_ENV
