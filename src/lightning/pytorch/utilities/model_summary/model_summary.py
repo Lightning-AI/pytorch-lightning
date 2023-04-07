6afd390e1defd02e790a6d5a44c73ab575762d33
ishandutta0098 <ishandutta0098@gmail.com>
2023-04-07 18:16:40 +0530

    Remove manual int() in num_parameters()

--- src/lightning/pytorch/utilities/model_summary/model_summary.py
+++ src/lightning/pytorch/utilities/model_summary/model_summary.py
@@ -121,10 +121,8 @@ class LayerSummary:
     @property
     def num_parameters(self) -> int:
         """Returns the number of parameters in this module."""
-        return int(
-            sum(
-                cast(int, math.prod(p.shape)) if not _is_lazy_weight_tensor(p) else 0 for p in self._module.parameters()
-            )
+        return sum(
+            cast(int, math.prod(p.shape)) if not _is_lazy_weight_tensor(p) else 0 for p in self._module.parameters()
         )
 
 
