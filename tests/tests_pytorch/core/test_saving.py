45242d44bf2ecbfb7f7f9d716116f08d231d5dac
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-04-09 18:10:20 +0100

    use tmpdir argument for location to save ckpt

--- tests/tests_pytorch/core/test_saving.py
+++ tests/tests_pytorch/core/test_saving.py
@@ -6,17 +6,17 @@ from lightning.pytorch.demos.boring_classes import BoringModel
 from tests_pytorch.helpers.runif import RunIf
 
 
-def create_boring_checkpoint():
+def create_boring_checkpoint(tmpdir):
     model = BoringModel()
     trainer = pl.Trainer(accelerator="auto", max_epochs=1, enable_model_summary=False, enable_progress_bar=False)
     trainer.fit(model)
-    trainer.save_checkpoint("./boring.ckpt")
+    trainer.save_checkpoint(f"{tmpdir}/boring.ckpt")
 
 
 @pytest.mark.parametrize("map_location", ("cpu", torch.device("cpu"), lambda storage, loc: storage, {"cpu": "cpu"}))
-def test_load_from_checkpoint_map_location_cpu(map_location):
-    create_boring_checkpoint()
-    model = BoringModel.load_from_checkpoint("./boring.ckpt", map_location=map_location)
+def test_load_from_checkpoint_map_location_cpu(tmpdir, map_location):
+    create_boring_checkpoint(tmpdir)
+    model = BoringModel.load_from_checkpoint(f"{tmpdir}/boring.ckpt", map_location=map_location)
     assert model.device.type == "cpu"
 
 
@@ -24,7 +24,7 @@ def test_load_from_checkpoint_map_location_cpu(map_location):
 @pytest.mark.parametrize(
     "map_location", ("cuda", torch.device("cuda"), lambda storage, loc: storage.cuda(), {"cpu": "cuda"})
 )
-def test_load_from_checkpoint_map_location_gpu(map_location):
-    create_boring_checkpoint()
-    model = BoringModel.load_from_checkpoint("./boring.ckpt", map_location=map_location)
+def test_load_from_checkpoint_map_location_gpu(tmpdir, map_location):
+    create_boring_checkpoint(tmpdir)
+    model = BoringModel.load_from_checkpoint(f"{tmpdir}/boring.ckpt", map_location=map_location)
     assert model.device.type == "cuda"
