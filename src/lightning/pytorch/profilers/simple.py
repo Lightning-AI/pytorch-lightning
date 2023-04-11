d32e4e72688f5385b32249471e9073eecccc6b79
ishandutta0098 <ishandutta0098@gmail.com>
2023-04-11 11:53:06 +0530

    Change list comprehension to loops for _make_report_extended and _make_report

--- src/lightning/pytorch/profilers/simple.py
+++ src/lightning/pytorch/profilers/simple.py
@@ -79,25 +79,30 @@ class SimpleProfiler(Profiler):
 
     def _make_report_extended(self) -> Tuple[_TABLE_DATA_EXTENDED, float, float]:
         total_duration = time.monotonic() - self.start_time
-        report = [
-            (
-                a,
-                torch.mean(torch.tensor(d)).item(),
-                len(d),
-                torch.sum(torch.tensor(d)).item(),
-                100.0 * torch.sum(torch.tensor(d)).item() / total_duration,
-            )
-            for a, d in self.recorded_durations.items()
-        ]
+        report = []
+
+        for a, d in self.recorded_durations.items():
+            d_tensor = torch.tensor(d)
+            mean_d = torch.mean(d_tensor).item()
+            len_d = len(d)
+            sum_d = torch.sum(d_tensor).item()
+            percentage_d = 100.0 * sum_d / total_duration
+
+            report.append((a, mean_d, len_d, sum_d, percentage_d))
+
         report.sort(key=lambda x: x[4], reverse=True)
         total_calls = sum(x[2] for x in report)
         return report, total_calls, total_duration
 
     def _make_report(self) -> _TABLE_DATA:
-        report = [
-            (action, torch.mean(torch.tensor(d)).item(), torch.sum(torch.tensor(d)).item())
-            for action, d in self.recorded_durations.items()
-        ]
+        report = []
+        for action, d in self.recorded_durations.items():
+            d_tensor = torch.tensor(d)
+            mean_d = torch.mean(d_tensor).item()
+            sum_d = torch.sum(d_tensor).item()
+
+            report.append((action, mean_d, sum_d))
+
         report.sort(key=lambda x: x[1], reverse=True)
         return report
 
