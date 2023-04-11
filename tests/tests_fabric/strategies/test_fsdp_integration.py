60476d55a32f24e504c409c5f689cbff2d866a39
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-11 08:17:22 -0400

    test

--- tests/tests_fabric/strategies/test_fsdp_integration.py
+++ tests/tests_fabric/strategies/test_fsdp_integration.py
@@ -34,135 +34,45 @@ if _TORCH_GREATER_EQUAL_1_12:
     from torch.distributed.fsdp.wrap import wrap, always_wrap_policy
 
 
-def _get_model():
-    return torch.nn.Sequential(torch.nn.Linear(32, 32), torch.nn.ReLU(), torch.nn.Linear(32, 2))
-
-
-class _MyFabricManualWrapping(BoringFabric):
+class _MyFabric(BoringFabric):
     def get_model(self):
-        model = _get_model()
+        return torch.nn.Sequential(torch.nn.Linear(32, 32), torch.nn.ReLU(), torch.nn.Linear(32, 2))
+
+    def step(self, model, batch):
+        forward_module = model._forward_module
+        original_module = model.module
+        assert isinstance(forward_module, FullyShardedDataParallel)
+        assert isinstance(self._precision, FSDPPrecision)
+
+        precision = torch.float16 if self._precision.precision == "16-mixed" else torch.bfloat16
+        assert forward_module.mixed_precision.param_dtype == precision
+        assert forward_module.mixed_precision.reduce_dtype == precision
+        assert forward_module.mixed_precision.buffer_dtype == precision
+
+        for layer_num in [0, 2]:
+            assert isinstance(original_module[layer_num], FullyShardedDataParallel)
+            assert original_module[layer_num].mixed_precision.param_dtype == precision
+            assert original_module[layer_num].mixed_precision.reduce_dtype == precision
+            assert original_module[layer_num].mixed_precision.buffer_dtype == precision
+
+        output = model(batch)
+        loss = torch.nn.functional.mse_loss(output, torch.ones_like(output))
+        return loss
+
+
+class _MyFabricManualWrapping(_MyFabric):
+    def get_model(self):
+        model = super().get_model()
         for i, layer in enumerate(model):
             if i % 2 == 0:
                 model[i] = wrap(layer)
         return model
 
 
-class _MyFabric(BoringFabric):
-    def get_model(self):
-        return _get_model()
-
-
-def _step(fabric, model, batch):
-    forward_module = model._forward_module
-    original_module = model.module
-    assert isinstance(forward_module, FullyShardedDataParallel)
-    assert isinstance(fabric._precision, FSDPPrecision)
-
-    precision = torch.float16 if fabric._precision.precision == "16" else torch.bfloat16
-    assert forward_module.mixed_precision.param_dtype == precision
-    assert forward_module.mixed_precision.reduce_dtype == precision
-    assert forward_module.mixed_precision.buffer_dtype == precision
-
-    for layer_num in [0, 2]:
-        assert isinstance(original_module[layer_num], FullyShardedDataParallel)
-        assert original_module[layer_num].mixed_precision.param_dtype == precision
-        assert original_module[layer_num].mixed_precision.reduce_dtype == precision
-        assert original_module[layer_num].mixed_precision.buffer_dtype == precision
-
-    output = model(batch)
-    loss = torch.nn.functional.mse_loss(output, torch.ones_like(output))
-    return loss
-
-
-# def _assert_save_equality(fabric, model, ckpt_path):
-#     current_state_dict = fabric._strategy.get_module_state_dict(model)
-
-#     checkpoint = fabric.load(ckpt_path)
-#     loaded_model = _get_model()
-#     loaded_model.load_state_dict(checkpoint)
-
-#     # model parameters are identical after loading
-#     for current_param, loaded_param in zip(current_state_dict.values(), loaded_model.state_dict().values()):
-#         assert torch.allclose(current_param.float().cpu(), loaded_param.cpu())
-
-
-if _TORCH_GREATER_EQUAL_2_0:
-
-    def _custom_auto_wrap_policy(
-        module,
-        recurse,
-        nonwrapped_numel: int,
-    ) -> bool:
-        return nonwrapped_numel >= 2
-
-else:
-
-    def _custom_auto_wrap_policy(
-        module,
-        recurse,
-        unwrapped_params: int,
-    ) -> bool:
-        return unwrapped_params >= 2
-
-
-@RunIf(min_cuda_gpus=2, skip_windows=True, standalone=True, min_torch="1.13")
-@pytest.mark.parametrize("precision", ("16-mixed", pytest.param("bf16-mixed", marks=RunIf(bf16_cuda=True))))
-@pytest.mark.parametrize("manual_wrapping", [True, False])
-def test_fsdp_train_save_load(manual_wrapping, precision):
-    """Test FSDP training, saving and loading with different wrapping and precision settings."""
-    strategy = FSDPStrategy(
-        auto_wrap_policy=_custom_auto_wrap_policy,
-        activation_checkpointing=[torch.nn.Linear],
-    )
-    fabric_cls = _MyFabricManualWrapping if manual_wrapping else _MyFabric
-    fabric = fabric_cls(accelerator="cuda", strategy=strategy, devices=2, precision=precision)
-    fabric.run()
-
-    with tempfile.TemporaryFile() as ckpt_path:
-        ckpt_path = fabric.broadcast(str(ckpt_path))
-        state = {"model": fabric.model, "optimizer": fabric.optimizer}
-        fabric.save(ckpt_path, state)
-
-        params_before = deepcopy(list(fabric.model.parameters()))
-
-        fabric = fabric_cls(accelerator="cuda", strategy=strategy, devices=2, precision=precision)
-        state = {"model": fabric.model, "optimizer": fabric.optimizer}
-        fabric.load(ckpt_path, state)
-
-        params_after = deepcopy(list(fabric.model.parameters()))
-        for p0, p1 in zip(params_before, params_after):
-            assert torch.equal(p0, p1)
-
-    # _assert_save_equality(fabric, fabric.model, ckpt_path)
-
-
-@RunIf(min_cuda_gpus=2, skip_windows=True, standalone=True, min_torch="1.12")
-@pytest.mark.parametrize("move_to_device", [True, False])
-@mock.patch("lightning.fabric.wrappers._FabricModule")
-def test_setup_module_move_to_device(fabric_module_mock, move_to_device):
-    """Test that `move_to_device` does nothing, FSDP decides which device parameters get moved to which device
-    (sharding)."""
-    strategy = FSDPStrategy(auto_wrap_policy=_custom_auto_wrap_policy)
-    fabric = Fabric(accelerator="cuda", devices=2, strategy=strategy)
-    fabric.launch()
-
-    model = torch.nn.Linear(10, 10, bias=False)  # total params: 10 * 10 = 100
-    fabric_model = fabric.setup_module(model, move_to_device=move_to_device)
-    fabric_module_mock.assert_not_called()
-
-    assert len(list(fabric_model.parameters())) == 1
-    # the linear layer got sharded and each part is on the expected device
-    assert next(fabric_model.parameters()).device == torch.device("cuda", fabric.local_rank)
-    assert next(fabric_model.parameters()).numel() == 50
-
-    # The _DeviceDtypeModuleMixin currently can't represent the device in a meaningful way for sharded models
-    assert fabric_model.device == torch.device("cpu")
-    assert fabric.device == torch.device("cuda", fabric.local_rank)
-
-
-
+# TODO: Refactor to use the MyFabric classes above
 @RunIf(min_cuda_gpus=2, standalone=True, min_torch="2.0.0")
-def test_save_load(tmp_path):
+def test_fsdp_train_save_load(tmp_path):
+    """Test FSDP training, saving and loading with different wrapping and precision settings."""
     fabric = Fabric(accelerator="cuda", devices=2, strategy=FSDPStrategy(auto_wrap_policy=always_wrap_policy))
     fabric.launch()
 
@@ -204,9 +114,33 @@ def test_save_load(tmp_path):
 
     metadata = fabric.load(checkpoint_path, state)
 
-    weights_after = deepcopy(list(model.parameters()))
-    assert all(torch.equal(p0, p1) for p0, p1 in zip(weights_before, weights_after))
+    params_after = deepcopy(list(model.parameters()))
+    assert all(torch.equal(p0, p1) for p0, p1 in zip(weights_before, params_after))
 
     # check user data in state reloaded
     assert state["steps"] == 1
     assert not metadata
+
+
+@RunIf(min_cuda_gpus=2, skip_windows=True, standalone=True, min_torch="1.12")
+@pytest.mark.parametrize("move_to_device", [True, False])
+@mock.patch("lightning.fabric.wrappers._FabricModule")
+def test_setup_module_move_to_device(fabric_module_mock, move_to_device):
+    """Test that `move_to_device` does nothing, FSDP decides which device parameters get moved to which device
+    (sharding)."""
+    strategy = FSDPStrategy(auto_wrap_policy=always_wrap_policy)
+    fabric = Fabric(accelerator="cuda", devices=2, strategy=strategy)
+    fabric.launch()
+
+    model = torch.nn.Linear(10, 10, bias=False)  # total params: 10 * 10 = 100
+    fabric_model = fabric.setup_module(model, move_to_device=move_to_device)
+    fabric_module_mock.assert_not_called()
+
+    assert len(list(fabric_model.parameters())) == 1
+    # the linear layer got sharded and each part is on the expected device
+    assert next(fabric_model.parameters()).device == torch.device("cuda", fabric.local_rank)
+    assert next(fabric_model.parameters()).numel() == 50
+
+    # The _DeviceDtypeModuleMixin currently can't represent the device in a meaningful way for sharded models
+    assert fabric_model.device == torch.device("cpu")
+    assert fabric.device == torch.device("cuda", fabric.local_rank)
