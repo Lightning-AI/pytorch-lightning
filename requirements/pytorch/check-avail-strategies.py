9ef616f8cc88b59695f5b31c00814d3620bc8210
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-12 18:14:37 +0200

    Prune colossalai

--- requirements/pytorch/check-avail-strategies.py
+++ requirements/pytorch/check-avail-strategies.py
@@ -1,4 +1,6 @@
 if __name__ == "__main__":
     import deepspeed  # noqa: F401
     import lightning_bagua  # noqa: F401
-    import lightning_colossalai  # noqa: F401
+
+    # colossalai does not support torch 2.0
+    # import lightning_colossalai
