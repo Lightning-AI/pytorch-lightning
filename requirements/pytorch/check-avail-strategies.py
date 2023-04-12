28fb64aad6fbd7be03b6c5ab99370695d1908332
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-12 18:31:47 +0200

    Strategy issues

--- requirements/pytorch/check-avail-strategies.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    import deepspeed  # noqa: F401
-    import lightning_bagua  # noqa: F401
-
-    # colossalai does not support torch 2.0
-    # import lightning_colossalai
