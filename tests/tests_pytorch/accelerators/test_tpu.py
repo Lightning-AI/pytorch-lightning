bf93f6648379a01b9621841dbf32381f92428515
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-14 02:12:28 +0200

    Parametrize CPU test that checks for the device offset

--- tests/tests_pytorch/accelerators/test_tpu.py
+++ tests/tests_pytorch/accelerators/test_tpu.py
@@ -312,8 +312,13 @@ def test_warning_if_tpus_not_used(tpu_available):
         ("2,", [2]),
     ],
 )
+@pytest.mark.parametrize("runtime", ("xrt", "pjrt"))
 @RunIf(min_python="3.9")  # mocking issue
-def test_trainer_config_device_ids(devices, expected_device_ids, tpu_available, monkeypatch):
+def test_trainer_config_device_ids(devices, expected_device_ids, runtime, tpu_available, monkeypatch):
+    from torch_xla.experimental import pjrt
+
+    monkeypatch.setattr(pjrt, "using_pjrt", lambda: runtime == "pjrt")
+
     mock = DeviceMock()
     monkeypatch.setattr(torch, "device", mock)
     if _IS_WINDOWS:
@@ -321,6 +326,7 @@ def test_trainer_config_device_ids(devices, expected_device_ids, tpu_available, 
         monkeypatch.setattr(torch.multiprocessing, "get_all_start_methods", lambda: ["fork", "spawn"])
 
     trainer = Trainer(accelerator="tpu", devices=devices)
-    assert mock.mock_calls == [call("xla", i + 1) for i in expected_device_ids]
+    device_offset = int(runtime == "xrt")
+    assert mock.mock_calls == [call("xla", i + device_offset) for i in expected_device_ids]
     assert len(trainer.device_ids) == len(expected_device_ids)
     assert trainer.num_devices == len(expected_device_ids)
