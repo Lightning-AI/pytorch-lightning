2efc70c93b8d23ac4ed240c2763ef39a8f8b6d70
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-13 21:23:09 -0400

    optimizers

--- tests/tests_fabric/strategies/test_fsdp_integration.py
+++ tests/tests_fabric/strategies/test_fsdp_integration.py
@@ -68,9 +68,9 @@ class _MyFabricManualWrapping(_MyFabric):
 
 
 @RunIf(min_cuda_gpus=2, standalone=True, min_torch="2.0.0")
-# @pytest.mark.parametrize("precision", ("16-mixed", pytest.param("bf16-mixed", marks=RunIf(bf16_cuda=True))))
-# @pytest.mark.parametrize("manual_wrapping", [True, False])
-def test_fsdp_train_save_load(tmp_path, manual_wrapping=False, precision="bf16-mixed"):
+@pytest.mark.parametrize("precision", ("16-mixed", pytest.param("bf16-mixed", marks=RunIf(bf16_cuda=True))))
+@pytest.mark.parametrize("manual_wrapping", [True, False])
+def test_fsdp_train_save_load(tmp_path, manual_wrapping, precision):
     """Test FSDP training, saving and loading with different wrapping and precision settings."""
     fabric_cls = _MyFabricManualWrapping if manual_wrapping else _MyFabric
     fabric = fabric_cls(accelerator="cuda", strategy=FSDPStrategy(auto_wrap_policy=always_wrap_policy), devices=2, precision=precision)
