0442dc0278952d17b34042d26cda7b76a23e413e
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-14 19:26:21 +0200

    Missed this in the commit above

--- src/lightning/pytorch/trainer/connectors/accelerator_connector.py
+++ src/lightning/pytorch/trainer/connectors/accelerator_connector.py
@@ -630,10 +630,8 @@ class _AcceleratorConnector:
 
     @property
     def is_distributed(self) -> bool:
-        # TODO: deprecate this property
-        # Used for custom plugins.
-        # Custom plugins should implement is_distributed property.
-        if hasattr(self.strategy, "is_distributed") and not isinstance(self.accelerator, TPUAccelerator):
+        if hasattr(self.strategy, "is_distributed"):
+            # Used for custom plugins. They should implement this property
             return self.strategy.is_distributed
         distributed_strategy = [
             DDPStrategy,
@@ -645,10 +643,7 @@ class _AcceleratorConnector:
             from lightning_habana import HPUParallelStrategy
 
             distributed_strategy.append(HPUParallelStrategy)
-        is_distributed = isinstance(self.strategy, tuple(distributed_strategy))
-        if isinstance(self.accelerator, TPUAccelerator):
-            is_distributed |= self.strategy.is_distributed
-        return is_distributed
+        return isinstance(self.strategy, tuple(distributed_strategy))
 
 
 def _set_torch_flags(
