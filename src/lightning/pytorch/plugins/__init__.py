e2629f296b731c75b9f4ae0f627f9fe43b53c3e4
Jirka <jirka.borovec@seznam.cz>
2023-04-14 14:32:52 +0200

    init

--- src/lightning/pytorch/plugins/__init__.py
+++ src/lightning/pytorch/plugins/__init__.py
@@ -2,13 +2,11 @@ from typing import Union
 
 from lightning.fabric.plugins import CheckpointIO, ClusterEnvironment, TorchCheckpointIO, XLACheckpointIO
 from lightning.pytorch.plugins.io.async_plugin import AsyncCheckpointIO
-from lightning.pytorch.plugins.io.hpu_plugin import HPUCheckpointIO  # noqa: F401
 from lightning.pytorch.plugins.layer_sync import LayerSync, TorchSyncBatchNorm
 from lightning.pytorch.plugins.precision.amp import MixedPrecisionPlugin
 from lightning.pytorch.plugins.precision.deepspeed import DeepSpeedPrecisionPlugin
 from lightning.pytorch.plugins.precision.double import DoublePrecisionPlugin
 from lightning.pytorch.plugins.precision.fsdp import FSDPMixedPrecisionPlugin
-from lightning.pytorch.plugins.precision.hpu import HPUPrecisionPlugin  # noqa: F401
 from lightning.pytorch.plugins.precision.ipu import IPUPrecisionPlugin
 from lightning.pytorch.plugins.precision.precision_plugin import PrecisionPlugin
 from lightning.pytorch.plugins.precision.tpu import TPUPrecisionPlugin
--- src/lightning/pytorch/strategies/__init__.py
+++ src/lightning/pytorch/strategies/__init__.py
@@ -15,11 +15,9 @@ from lightning.fabric.strategies.registry import _StrategyRegistry
 from lightning.pytorch.strategies.ddp import DDPStrategy
 from lightning.pytorch.strategies.deepspeed import DeepSpeedStrategy
 from lightning.pytorch.strategies.fsdp import FSDPStrategy
-from lightning.pytorch.strategies.hpu_parallel import HPUParallelStrategy  # noqa: F401
 from lightning.pytorch.strategies.ipu import IPUStrategy  # noqa: F401
 from lightning.pytorch.strategies.parallel import ParallelStrategy
 from lightning.pytorch.strategies.single_device import SingleDeviceStrategy
-from lightning.pytorch.strategies.single_hpu import SingleHPUStrategy  # noqa: F401
 from lightning.pytorch.strategies.single_tpu import SingleTPUStrategy  # noqa: F401
 from lightning.pytorch.strategies.strategy import Strategy
 from lightning.pytorch.strategies.utils import _call_register_strategies
