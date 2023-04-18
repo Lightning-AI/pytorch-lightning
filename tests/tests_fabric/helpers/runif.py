99f44795385727ea5ce056f3449ed199c278822c
awaelchli <aedu.waelchli@gmail.com>
2023-04-18 23:54:59 +0200

    logical or

--- tests/tests_fabric/helpers/runif.py
+++ tests/tests_fabric/helpers/runif.py
@@ -147,7 +147,7 @@ class RunIf:
                 cond = sys.platform == "win32" or sys.version_info >= (3, 11)
 
             # set use_base_version for nightly support
-            cond &= compare_version("torch", operator.lt, "2.0.0", use_base_version=True)
+            cond |= compare_version("torch", operator.lt, "2.0.0", use_base_version=True)
             conditions.append(cond)
             reasons.append("torch.dynamo")
 
