604f5f7ecc83b39341c5318b541af33e4787b214
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-19 17:43:37 +0200

    [TPU] Support io with `torch_xla.utils.serialization`

--- src/lightning/fabric/plugins/io/xla.py
+++ src/lightning/fabric/plugins/io/xla.py
@@ -11,16 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-from typing import Any, Dict, Optional
+from typing import Any, Callable, Dict, Optional
 
-import torch
 from lightning_utilities.core.apply_func import apply_to_collection
 from lightning_utilities.core.imports import RequirementCache
 
 from lightning.fabric.accelerators.tpu import _XLA_AVAILABLE
 from lightning.fabric.plugins.io.torch_io import TorchCheckpointIO
 from lightning.fabric.utilities.cloud_io import get_filesystem
+from lightning.fabric.utilities.rank_zero import rank_zero_info
 from lightning.fabric.utilities.types import _PATH
 
 
@@ -30,10 +29,11 @@ class XLACheckpointIO(TorchCheckpointIO):
     .. warning::  This is an :ref:`experimental <versioning:Experimental API>` feature.
     """
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, *args: Any, xser: bool = True, **kwargs: Any) -> None:
         if not _XLA_AVAILABLE:
             raise ModuleNotFoundError(str(_XLA_AVAILABLE))
         super().__init__(*args, **kwargs)
+        self.xser = xser
 
     def save_checkpoint(self, checkpoint: Dict[str, Any], path: _PATH, storage_options: Optional[Any] = None) -> None:
         """Save model/training states as a checkpoint file through state-dump and file-write.
@@ -47,20 +47,35 @@ class XLACheckpointIO(TorchCheckpointIO):
             TypeError:
                 If ``storage_options`` arg is passed in
         """
-        if storage_options is not None:
-            raise TypeError(
-                "`Trainer.save_checkpoint(..., storage_options=...)` with `storage_options` arg"
-                f" is not supported for `{self.__class__.__name__}`. Please implement your custom `CheckpointIO`"
-                " to define how you'd like to use `storage_options`."
-            )
-        fs = get_filesystem(path)
-        fs.makedirs(os.path.dirname(path), exist_ok=True)
         if RequirementCache("omegaconf"):
             # workaround for https://github.com/pytorch/xla/issues/2773
             from omegaconf import DictConfig, ListConfig, OmegaConf
 
             checkpoint = apply_to_collection(checkpoint, (DictConfig, ListConfig), OmegaConf.to_container)
-        import torch_xla.core.xla_model as xm
 
-        cpu_data = xm._maybe_convert_to_cpu(checkpoint, convert=True)
-        torch.save(cpu_data, path)
+        if self.xser:
+            import torch_xla.utils.serialization as xser
+
+            checkpoint = xser._rewrite_data(xser._get_tensors_folder(path), checkpoint, save_tensors=True)
+        else:
+            import torch_xla.core.xla_model as xm
+
+            checkpoint = xm._maybe_convert_to_cpu(checkpoint, convert=True)
+        super().save_checkpoint(checkpoint, path, storage_options=storage_options)
+
+    def load_checkpoint(
+        self, path: _PATH, map_location: Optional[Callable] = lambda storage, loc: storage
+    ) -> Dict[str, Any]:
+        if not self.xser:
+            return super().load_checkpoint(path, map_location=map_location)
+        elif map_location is not None:
+            # requires https://github.com/pytorch/xla/pull/4899
+            rank_zero_info(f"Passing `map_location={map_location}` is not supported with `XLACheckpointIO(xser=True)`")
+
+        fs = get_filesystem(path)
+        if not fs.exists(path):
+            raise FileNotFoundError(f"Checkpoint at {path} not found. Aborting training.")
+
+        import torch_xla.utils.serialization as xser
+
+        return xser.load(path)
