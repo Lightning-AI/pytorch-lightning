274b85e4214e70c5be00fb5b418f2eb81ba43a1e
Irina Kogay <kogayie1995@gmail.com>
2023-04-22 15:27:44 +0400

    make DummyDefault for test

--- src/lightning/app/components/serve/theme.py
+++ src/lightning/app/components/serve/theme.py
@@ -7,13 +7,15 @@ if _is_gradio_available():
 else:
     gradio = ModuleType("gradio")
     gradio.themes = ModuleType("gradio.themes")
-    gradio.themes.Default = ModuleType("gradio.themes.Default")
     class DummyColor:
         def __init__(self, *args, **kwargs):
             pass
-
+    class DummyDefault:
+        def __init__(self, *args, **kwargs):
+            pass
+    gradio.themes.Default = DummyDefault
     gradio.themes.Color = DummyColor
-    
+
 theme = gradio.themes.Default(
     primary_hue=gradio.themes.Color(
         "#ffffff",
