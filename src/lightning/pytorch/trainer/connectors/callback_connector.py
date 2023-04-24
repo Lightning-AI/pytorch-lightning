0af20c1156723158a16472758b2fc204e0aacd17
Jirka <jirka.borovec@seznam.cz>
2023-04-24 23:25:44 +0200

    mypy

--- src/lightning/pytorch/trainer/connectors/callback_connector.py
+++ src/lightning/pytorch/trainer/connectors/callback_connector.py
@@ -226,8 +226,10 @@ def _configure_external_callbacks() -> List[Callback]:
     if _PYTHON_GREATER_EQUAL_3_8_0:
         from importlib.metadata import entry_points
 
-        factories = (  # type: ignore[arg-type]
-            entry_points(group=group) if _PYTHON_GREATER_EQUAL_3_10_0 else entry_points().get(group, {})
+        factories = (
+            entry_points(group=group)
+            if _PYTHON_GREATER_EQUAL_3_10_0
+            else entry_points().get(group, {})  # type: ignore[arg-type]
         )
     else:
         from pkg_resources import iter_entry_points
