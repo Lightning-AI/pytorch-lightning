77da5a4644196edd193b7562bc35a9d1979955bd
Carlos Mocholí <carlossmocholi@gmail.com>
2023-04-25 17:46:28 +0200

    Fallback to module available check for mlflow (#17467)
    (cherry picked from commit b0af0eede27531a3d7d9940b8e6ed205b03e6218)

--- src/lightning/pytorch/loggers/mlflow.py
+++ src/lightning/pytorch/loggers/mlflow.py
@@ -25,7 +25,7 @@ from time import time
 from typing import Any, Dict, List, Literal, Mapping, Optional, Union
 
 import yaml
-from lightning_utilities.core.imports import RequirementCache
+from lightning_utilities.core.imports import module_available, RequirementCache
 from torch import Tensor
 
 from lightning.fabric.utilities.logger import _add_prefix, _convert_params, _flatten_dict
@@ -36,9 +36,8 @@ from lightning.pytorch.utilities.rank_zero import rank_zero_only, rank_zero_warn
 
 log = logging.getLogger(__name__)
 LOCAL_FILE_URI_PREFIX = "file:"
-_MLFLOW_FULL_AVAILABLE = RequirementCache("mlflow>=1.0.0")
-_MLFLOW_SKINNY_AVAILABLE = RequirementCache("mlflow-skinny>=1.0.0")
-_MLFLOW_AVAILABLE = _MLFLOW_FULL_AVAILABLE or _MLFLOW_SKINNY_AVAILABLE
+_MLFLOW_AVAILABLE = RequirementCache("mlflow>=1.0.0") or module_available("mlflow")
+
 if _MLFLOW_AVAILABLE:
     from mlflow.entities import Metric, Param
     from mlflow.tracking import context, MlflowClient
@@ -149,9 +148,7 @@ class MLFlowLogger(Logger):
         run_id: Optional[str] = None,
     ):
         if not _MLFLOW_AVAILABLE:
-            raise ModuleNotFoundError(
-                f"{_MLFLOW_FULL_AVAILABLE!s}. You can also try {_MLFLOW_SKINNY_AVAILABLE.requirement!r}"
-            )
+            raise ModuleNotFoundError(str(_MLFLOW_AVAILABLE))
         super().__init__()
         if not tracking_uri:
             tracking_uri = f"{LOCAL_FILE_URI_PREFIX}{save_dir}"
