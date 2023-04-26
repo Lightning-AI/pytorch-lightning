76586ffa492a18ad9d7a9d4a9035f5ddf9615dbf
Jirka <jirka.borovec@seznam.cz>
2023-04-26 23:46:57 +0200

    data

--- src/lightning/pytorch/trainer/connectors/data_connector.py
+++ src/lightning/pytorch/trainer/connectors/data_connector.py
@@ -34,6 +34,7 @@ from lightning.pytorch.trainer.states import RunningStage, TrainerFn
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
 from lightning.pytorch.utilities.data import _is_dataloader_shuffled, _update_dataloader
 from lightning.pytorch.utilities.exceptions import MisconfigurationException
+from lightning.pytorch.utilities.imports import _LIGHTNING_GRAPHCORE_AVAILABLE
 from lightning.pytorch.utilities.model_helpers import is_overridden
 from lightning.pytorch.utilities.rank_zero import rank_zero_warn, WarningCache
 from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
@@ -164,11 +165,19 @@ class _DataConnector:
         datamodule.trainer = trainer
 
     def _requires_distributed_sampler(self, dataloader: DataLoader) -> bool:
+        if _LIGHTNING_GRAPHCORE_AVAILABLE:
+            from lightning_graphcore import IPUAccelerator
+
+            # `DistributedSampler` is never used with `poptorch.DataLoader`
+            is_ipu = isinstance(self.trainer.accelerator, IPUAccelerator)
+        else:
+            is_ipu = False
         return (
             self.trainer._accelerator_connector.use_distributed_sampler
             and self.trainer._accelerator_connector.is_distributed
             and not isinstance(dataloader.sampler, DistributedSampler)
             and not has_iterable_dataset(dataloader)
+            and not is_ipu
         )
 
     def _prepare_dataloader(self, dataloader: object, shuffle: bool, mode: RunningStage) -> object:
@@ -181,9 +190,18 @@ class _DataConnector:
         if not isinstance(dataloader, DataLoader):
             return dataloader
 
+        if _LIGHTNING_GRAPHCORE_AVAILABLE:
+            from lightning_graphcore import IPUAccelerator
+
+            # `DistributedSampler` is never used with `poptorch.DataLoader`
+            is_ipu = isinstance(self.trainer.accelerator, IPUAccelerator)
+        else:
+            is_ipu = False
         if (
             self._requires_distributed_sampler(dataloader)  # sets the distributed sampler
             or mode == RunningStage.PREDICTING  # to track indices for the predictions
+            # IPUs use a custom `poptorch.DataLoader` which we might need to convert to
+            or is_ipu
         ):
             sampler = self._resolve_sampler(dataloader, shuffle=shuffle, mode=mode)
             dataloader = _update_dataloader(dataloader, sampler, mode=mode)
