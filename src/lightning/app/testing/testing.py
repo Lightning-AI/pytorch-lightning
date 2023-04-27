befc1899f1b34088212bccdc54eee690fc1aef3b
thomas <thomas@thomass-MacBook-Pro.local>
2023-04-27 16:46:53 +0100

    update

--- src/lightning/app/testing/testing.py
+++ src/lightning/app/testing/testing.py
@@ -297,9 +297,6 @@ def run_app_in_cloud(
 
         cmd_extra_args = []
 
-        if not os.getenv("LIGHTNING_CLOUD_URL", "").startswith("https://lightning.ai"):
-            cmd_extra_args = ["--cluster-id", "staging"]
-
         with open(stdout_path, "w") as stdout:
             cmd = [
                 sys.executable,
