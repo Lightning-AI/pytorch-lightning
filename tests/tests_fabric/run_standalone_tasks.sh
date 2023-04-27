fee960771caf34c6799673b11dc183320e58f2bc
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-27 21:18:17 +0200

    update

---
+++ tests/tests_fabric/run_standalone_tasks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright The Lightning AI team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+# THIS FILE ASSUMES IT IS RUN INSIDE THE tests/tests_fabric DIRECTORY
+
+# this environment variable allows special tests to run
+export PL_RUN_STANDALONE_TESTS=1
+
+echo "Run parity tests manually"
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+py.test -v parity/test_parity_ddp::test_parity_ddp
+#py.test -v parity/test_parity_simple::test_parity_single_device
