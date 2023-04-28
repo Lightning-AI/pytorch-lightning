58722a805eb7445f80ddc13c173133e498fd6abc
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-27 21:03:15 -0400

    init empty

--- repro.py
+++
@@ -1,7 +0,0 @@
-# import pytorch_lightning  # <--- Comment or uncomment to test
-
-import os
-import multiprocessing
-
-pool = multiprocessing.Pool(8)
-print(pool.map(os.sched_getaffinity, [0] * 8))
