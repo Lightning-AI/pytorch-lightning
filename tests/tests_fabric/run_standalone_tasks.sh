b3c6bc6e5a3c107d30f8f199bb8d9b772c94101d
awaelchli <aedu.waelchli@gmail.com>
2023-04-29 07:58:48 +0200

    update

--- tests/tests_fabric/run_standalone_tasks.sh
+++ tests/tests_fabric/run_standalone_tasks.sh
@@ -27,11 +27,12 @@ do
   python -m parity.test_parity_ddp --accelerator="cpu" --devices=2 --tolerance=0.02
   if [ $? -eq 0 ]; then break; fi
 done
+if [ $? -ne 0 ]; then exit; fi
 
 
-for i in {1..3}
-do
-  echo "Run attempt: $i"
-  python -m parity.test_parity_ddp --accelerator="cuda" --devices=2 --tolerance=0.01
-  if [ $? -eq 0 ]; then break; fi
-done
+#for i in {1..3}
+#do
+#  echo "Run attempt: $i"
+#  python -m parity.test_parity_ddp --accelerator="cuda" --devices=2 --tolerance=0.01
+#  if [ $? -eq 0 ]; then break; fi
+#done
