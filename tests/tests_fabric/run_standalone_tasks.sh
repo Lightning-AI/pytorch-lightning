8ad63ff98a87f6f6ad6b01eb71bd79f0f18ab644
awaelchli <aedu.waelchli@gmail.com>
2023-04-29 07:47:25 +0200

    remove set -e

--- tests/tests_fabric/run_standalone_tasks.sh
+++ tests/tests_fabric/run_standalone_tasks.sh
@@ -12,9 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -e
-# THIS FILE ASSUMES IT IS RUN INSIDE THE tests/tests_fabric DIRECTORY
 
+# THIS FILE ASSUMES IT IS RUN INSIDE THE tests/tests_fabric DIRECTORY
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/.."
 
