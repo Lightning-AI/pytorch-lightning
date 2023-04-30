f67827c7701bea3a090b98315d2bd42f8cfaccc6
ashikshafi0 <ashikshafi0@gmail.com>
2023-04-30 00:56:15 -0500

    updated the RichProgressBarTheme class for different terminals

--- testFile/test_rich_progress.py
+++ testFile/test_rich_progress.py
@@ -3,32 +3,36 @@ from time import sleep
 from rich.progress import Progress
 
 # Parameters for the simulation
-num_epochs = 5
-num_batches = 100
+num_epochs = 1
+num_batches = 860
 batch_size = 32
 
 with Progress() as progress:
     for epoch in range(num_epochs):
         # Add a task for the current epoch
-        task_id = progress.add_task(f"[cyan]Epoch {epoch + 1}", total=num_batches)
+        task_id = progress.add_task(f"Epoch {epoch + 1}/{num_epochs}", total=num_batches)
 
         # Simulate the training process for each batch
-        for _ in range(num_batches):
-            sleep(0.02)  # Simulate time taken to process one batch
+        for batch in range(num_batches):
+            sleep(0.01)  # Simulate time taken to process one batch
             progress.update(task_id, advance=1)
 
-            # Randomly generate loss and accuracy for the current batch
-            loss = random.uniform(0.1, 1.5)
-            accuracy = random.uniform(0.5, 1.0)
+            # Calculate elapsed time and remaining time (for illustration purposes)
+            elapsed_time = progress.tasks[task_id].elapsed_time
+            remaining_time = (num_batches - batch - 1) * elapsed_time / (batch + 1)
 
-            # Update the description with the current batch's loss and accuracy
+            # Randomly generate processing speed (items per second)
+            speed = random.uniform(75, 85)
+
+            # Update the description with the current batch's information
             progress.update(
                 task_id,
-                description=f"[cyan]Epoch {epoch + 1} [bold red]Loss: {loss:.2f} [bold green]Accuracy: {accuracy:.2f}",
+                description=(
+                    f"Epoch {epoch + 1}/{num_epochs} "
+                    f"{batch + 1}/{num_batches} "
+                    f"{elapsed_time:.0f}s . {remaining_time:.0f}s "
+                    f"{speed:.2f}it/s"
+                ),
             )
-
-        # Mark the epoch task as completed
-        progress.stop()
-        progress.remove_task(task_id)
 
     print("[bold green]Training complete.")
