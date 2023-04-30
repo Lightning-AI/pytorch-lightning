a980032c7ed333e18bb7e9ccfac9cff3671d816a
ashikshafi0 <ashikshafi0@gmail.com>
2023-04-30 01:00:59 -0500

    updated the RichProgressBarTheme class for different terminals

--- testFile/test_rich_progress.py
+++ testFile/test_rich_progress.py
@@ -2,30 +2,34 @@ import random
 import time
 from time import sleep
 from rich.progress import Progress
+from src.lightning.pytorch.callbacks.progress import RichProgressBarTheme
+
+custom_theme = RichProgressBarTheme(
+    description="yellow",
+    progress_bar="green",
+    batch_progress="red",
+    time="blue",
+    processing_speed="magenta",
+    metrics="cyan"
+)
 
 # Parameters for the simulation
 num_epochs = 1
 num_batches = 860
 batch_size = 32
 
-with Progress() as progress:
+with Progress(theme=custom_theme) as progress:
     for epoch in range(num_epochs):
-        # Add a task for the current epoch
         task_id = progress.add_task(f"Epoch {epoch + 1}/{num_epochs}", total=num_batches)
 
-        # Simulate the training process for each batch
         for batch in range(num_batches):
-            sleep(0.01)  # Simulate time taken to process one batch
+            sleep(0.01)
             progress.update(task_id, advance=1)
 
-            # Calculate elapsed time and remaining time (for illustration purposes)
             elapsed_time = time.time() - progress.tasks[task_id].start_time
             remaining_time = (num_batches - batch - 1) * elapsed_time / (batch + 1)
-
-            # Randomly generate processing speed (items per second)
             speed = random.uniform(75, 85)
 
-            # Update the description with the current batch's information
             progress.update(
                 task_id,
                 description=(
