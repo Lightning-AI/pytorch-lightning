fc063a276582e3186a3d3dec3397d73f8953d7a4
Carlos Mocholí <carlossmocholi@gmail.com>
2023-05-03 03:58:03 +0200

    Duplicated arguments, no **kwargs

--- src/lightning/pytorch/utilities/testing/_runif.py
+++ src/lightning/pytorch/utilities/testing/_runif.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from lightning_utilities.core.imports import RequirementCache
 
@@ -27,15 +27,38 @@ _SKLEARN_AVAILABLE = RequirementCache("scikit-learn")
 
 def _RunIf(
     *,
+    min_cuda_gpus: int = 0,
+    min_torch: Optional[str] = None,
+    max_torch: Optional[str] = None,
+    min_python: Optional[str] = None,
+    bf16_cuda: bool = False,
+    tpu: bool = False,
+    mps: Optional[bool] = None,
+    skip_windows: bool = False,
+    standalone: bool = False,
+    deepspeed: bool = False,
+    dynamo: bool = False,
     rich: bool = False,
     omegaconf: bool = False,
     psutil: bool = False,
     sklearn: bool = False,
     onnx: bool = False,
-    **kwargs: Any
 ) -> Tuple[List[str], Dict[str, bool]]:
     """
     Args:
+        min_cuda_gpus: Require this number of gpus and that the ``PL_RUN_CUDA_TESTS=1`` environment variable is set.
+        min_torch: Require that PyTorch is greater or equal than this version.
+        max_torch: Require that PyTorch is less than this version.
+        min_python: Require that Python is greater or equal than this version.
+        bf16_cuda: Require that CUDA device supports bf16.
+        tpu: Require that TPU is available.
+        mps: If True: Require that MPS (Apple Silicon) is available,
+            if False: Explicitly Require that MPS is not available
+        skip_windows: Skip for Windows platform.
+        standalone: Mark the test as standalone, our CI will run it in a separate process.
+            This requires that the ``PL_RUN_STANDALONE_TESTS=1`` environment variable is set.
+        deepspeed: Require that microsoft/DeepSpeed is installed.
+        dynamo: Require that `torch.dynamo` is supported.
         rich: Require that willmcgugan/rich is installed.
         omegaconf: Require that omry/omegaconf is installed.
         psutil: Require that psutil is installed.
@@ -43,7 +66,19 @@ def _RunIf(
         onnx: Require that onnx is installed.
     """
 
-    reasons, kwargs = FabricRunIf(**kwargs)
+    reasons, kwargs = FabricRunIf(
+        min_cuda_gpus=min_cuda_gpus,
+        min_torch=min_torch,
+        max_torch=max_torch,
+        min_python=min_python,
+        bf16_cuda=bf16_cuda,
+        tpu=tpu,
+        mps=mps,
+        skip_windows=skip_windows,
+        standalone=standalone,
+        deepspeed=deepspeed,
+        dynamo=dynamo,
+    )
 
     if rich and not _RICH_AVAILABLE:
         reasons.append("Rich")
