12a07a8feaaf5a883b3bab2b180374ed4e78e2a3
Boon <49676590+bkiat1123@users.noreply.github.com>
2023-05-13 01:13:58 +0200

    Fix ModelCheckpoint misformats filename (#17610)
    Co-authored-by: awaelchli <aedu.waelchli@gmail.com>
    
    (cherry picked from commit cbc536a26763689f715d710627594aa18712c14d)

--- src/lightning/pytorch/callbacks/model_checkpoint.py
+++ src/lightning/pytorch/callbacks/model_checkpoint.py
@@ -507,19 +507,23 @@ class ModelCheckpoint(Checkpoint):
 
         # check and parse user passed keys in the string
         groups = re.findall(r"(\{.*?)[:\}]", filename)
-        if len(groups) >= 0:
-            for group in groups:
-                name = group[1:]
 
-                if auto_insert_metric_name:
-                    filename = filename.replace(group, name + "={" + name)
+        # sort keys from longest to shortest to avoid replacing substring
+        # eg: if keys are "epoch" and "epoch_test", the latter must be replaced first
+        groups = sorted(groups, key=lambda x: len(x), reverse=True)
 
-                # support for dots: https://stackoverflow.com/a/7934969
-                filename = filename.replace(group, f"{{0[{name}]")
+        for group in groups:
+            name = group[1:]
 
-                if name not in metrics:
-                    metrics[name] = torch.tensor(0)
-            filename = filename.format(metrics)
+            if auto_insert_metric_name:
+                filename = filename.replace(group, name + "={" + name)
+
+            # support for dots: https://stackoverflow.com/a/7934969
+            filename = filename.replace(group, f"{{0[{name}]")
+
+            if name not in metrics:
+                metrics[name] = torch.tensor(0)
+        filename = filename.format(metrics)
 
         if prefix:
             filename = cls.CHECKPOINT_JOIN_CHAR.join([prefix, filename])
