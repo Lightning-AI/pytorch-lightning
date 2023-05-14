a3a9cd55b605957dfbddbf79d0e3d6f67f60ad50
awaelchli <aedu.waelchli@gmail.com>
2023-05-14 19:37:22 +0200

    test

---
+++ tests/tests_fabric/utilities/test_init.py
@@ -0,0 +1,31 @@
+# Copyright The Lightning AI team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import torch.nn
+
+from lightning.fabric.utilities import _EmptyInit
+from tests_fabric.helpers.runif import RunIf
+
+
+@RunIf(min_cuda_gpus=1)
+def test_empty_init_memory_allocation():
+    """Test that no memory gets allocated when using the `_EmptyInit()` context manager."""
+    torch.cuda.reset_max_memory_allocated()
+    with _EmptyInit(enabled=False):
+        torch.nn.Linear(100, 100, device="cuda")
+    assert torch.cuda.max_memory_allocated() > 0
+
+    torch.cuda.reset_max_memory_allocated()
+    with _EmptyInit(enabled=True):
+        torch.nn.Linear(100, 100, device="cuda")
+    assert torch.cuda.max_memory_allocated() == 0
