866a8d8c1ee900fbd001a64391da5fa2ce0dc4ed
boon <bkiat1123@gmail.com>
2023-05-31 20:05:54 +0800

    remove commented line

--- src/lightning/fabric/utilities/device_dtype_mixin.py
+++ src/lightning/fabric/utilities/device_dtype_mixin.py
@@ -26,8 +26,6 @@ class _DeviceDtypeModuleMixin(Module):
         super().__init__()
         super().__setattr__("_dtype", torch.get_default_dtype())
         super().__setattr__("_device", torch.device("cpu"))
-        # self._dtype: Union[str, torch.dtype] = torch.get_default_dtype()
-        # self._device = torch.device("cpu")
 
     @property
     def dtype(self) -> Union[str, torch.dtype]:
