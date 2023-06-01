dbb537dde4659969764ba4c73f6374cc01d83903
awaelchli <aedu.waelchli@gmail.com>
2023-06-02 01:14:33 +0200

    Revert "update"
    This reverts commit 614841c89c3b9a5e2b1d3e5ebf301bf8cf564297.

--- src/lightning/fabric/strategies/deepspeed.py
+++ src/lightning/fabric/strategies/deepspeed.py
@@ -341,6 +341,11 @@ class DeepSpeedStrategy(DDPStrategy, _Sharded):
 
     @contextmanager
     def module_init_context(self) -> Generator[None, None, None]:
+        with super().module_init_context(), self.module_sharded_context():
+            yield
+
+    @contextmanager
+    def module_sharded_context(self) -> Generator[None, None, None]:
         # Current limitation in Fabric: The config needs to be fully determined at the time of calling the context
         # manager, which happens at the start of `Fabric.run()`. Later modifications through e.g. `Fabric.setup()`
         # won't have an effect here.
@@ -366,12 +371,6 @@ class DeepSpeedStrategy(DDPStrategy, _Sharded):
             ):
                 yield
         else:
-            yield
-
-    @contextmanager
-    def module_sharded_context(self) -> Generator[None, None, None]:
-        """Deprecated. Please use :meth:`module_init_context` instead."""
-        with self.module_init_context():
             yield
 
     def save_checkpoint(
