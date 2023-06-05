0c35887a8e78079727dbdf7b9006f279505bedde
lightningforever <adrian@lightning.ai>
2023-06-05 14:02:29 +0200

    move callback registration to fabric

--- src/lightning/pytorch/trainer/connectors/callback_connector.py
+++ src/lightning/pytorch/trainer/connectors/callback_connector.py
@@ -18,6 +18,7 @@ from datetime import timedelta
 from typing import Dict, List, Optional, Sequence, Union
 
 import lightning.pytorch as pl
+from lightning.fabric.utilities.registry import _load_external_callbacks
 from lightning.pytorch.callbacks import (
     Callback,
     Checkpoint,
@@ -75,7 +76,7 @@ class _CallbackConnector:
         # configure the ModelSummary callback
         self._configure_model_summary_callback(enable_model_summary)
 
-        self.trainer.callbacks.extend(_configure_external_callbacks())
+        self.trainer.callbacks.extend(_load_external_callbacks("lightning.pytorch.callbacks_factory"))
         _validate_callbacks_list(self.trainer.callbacks)
 
         # push all model checkpoint callbacks to the end
@@ -211,42 +212,6 @@ class _CallbackConnector:
                 other_callbacks.append(cb)
 
         return tuner_callbacks + other_callbacks + checkpoint_callbacks
-
-
-def _configure_external_callbacks() -> List[Callback]:
-    """Collect external callbacks registered through entry points.
-
-    The entry points are expected to be functions returning a list of callbacks.
-
-    Return:
-        A list of all callbacks collected from external factories.
-    """
-    group = "lightning.pytorch.callbacks_factory"
-
-    if _PYTHON_GREATER_EQUAL_3_8_0:
-        from importlib.metadata import entry_points
-
-        factories = (
-            entry_points(group=group)
-            if _PYTHON_GREATER_EQUAL_3_10_0
-            else entry_points().get(group, {})  # type: ignore[arg-type]
-        )
-    else:
-        from pkg_resources import iter_entry_points
-
-        factories = iter_entry_points(group)  # type: ignore[assignment]
-
-    external_callbacks: List[Callback] = []
-    for factory in factories:
-        callback_factory = factory.load()
-        callbacks_list: Union[List[Callback], Callback] = callback_factory()
-        callbacks_list = [callbacks_list] if isinstance(callbacks_list, Callback) else callbacks_list
-        _log.info(
-            f"Adding {len(callbacks_list)} callbacks from entry point '{factory.name}':"
-            f" {', '.join(type(cb).__name__ for cb in callbacks_list)}"
-        )
-        external_callbacks.extend(callbacks_list)
-    return external_callbacks
 
 
 def _validate_callbacks_list(callbacks: List[Callback]) -> None:
