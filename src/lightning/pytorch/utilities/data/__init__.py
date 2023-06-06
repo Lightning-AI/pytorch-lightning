ec49b6d60636743edafc40fea6f47a1ac10f8ec4
Noha Alon <nohaalon@Nohas-MacBook-Air.local>
2023-06-06 06:06:11 +0300

    remove unused private methods from init

--- src/lightning/pytorch/utilities/data/__init__.py
+++ src/lightning/pytorch/utilities/data/__init__.py
@@ -3,9 +3,7 @@ from lightning.pytorch.utilities.data.dataset import LightningDataset, S3Lightni
 # This is done for backwards compatibility
 from lightning.pytorch.utilities.data.extract_batch_size import (
     _dataloader_init_kwargs_resolve_sampler,
-    _extract_batch_size,
     _get_dataloader_init_args_and_kwargs,
-    _is_dataloader_shuffled,
     _update_dataloader,
     extract_batch_size,
     has_len_all_ranks,
@@ -14,10 +12,8 @@ from lightning.pytorch.utilities.data.extract_batch_size import (
 
 __all__ = [
     "extract_batch_size",
-    "_extract_batch_size",
     "has_len_all_ranks",
     "_update_dataloader",
-    "_is_dataloader_shuffled",
     "warning_cache",
     "_dataloader_init_kwargs_resolve_sampler",
     "_get_dataloader_init_args_and_kwargs",
