e1077e9e1a9a44bc4ef9efb8139919fa925b5156
Noha Alon <nohaalon@Nohas-MacBook-Air.local>
2023-06-06 20:26:07 +0300

    tests

--- tests/tests_data/test_dataset.py
+++ tests/tests_data/test_dataset.py
@@ -1,6 +1,8 @@
 import os
 import socket
+from types import GeneratorType
 
+import numpy as np
 import pytest
 from lightning_utilities.core.imports import package_available
 
@@ -17,21 +19,56 @@ def isConnectedWithInternet():
     return False
 
 
+@pytest.fixture(scope="session")
+def image_set(tmp_path_factory):
+    from PIL import Image
+
+    file_nums = [
+        0,
+        1,
+        10,
+        100,
+        1000,
+        10000,
+        100000,
+        1000000,
+        1000001,
+        1000002,
+        1000003,
+        1000004,
+        1000005,
+        1000006,
+        1000007,
+        1000008,
+        1000009,
+    ]
+
+    img = np.random.randint(255, size=(800, 800))
+    img = img.astype(np.uint8)
+    im = Image.fromarray(img)
+
+    for i in file_nums:
+        fn = tmp_path_factory.mktemp("test_data") / f"img-{i}.jpeg"
+        im.save(fn)
+
+    return tmp_path_factory.getbasetemp()._str
+
+
 @pytest.mark.skipif(not isConnectedWithInternet(), reason="Not connected to internet")
 @pytest.mark.skipif(not package_available("lightning"), reason="Supported only with mono-package")
-def test_lightning_dataset(tmpdir):
+def test_lightning_dataset(tmpdir, image_set):
     index_path = os.path.join(tmpdir, "index.txt")
     # TODO: adapt this once the fallback and tests for get_index are ready!
-    dset = LightningDataset("s3://imagenet-tiny", path_to_index_file=index_path)
+    dset = LightningDataset(image_set, path_to_index_file=index_path)
     tuple_of_files = dset.get_index()
-    assert isinstance(tuple_of_files, tuple)
-    assert all(map(lambda x: isinstance(x, str)))
+    assert isinstance(tuple_of_files, GeneratorType)
+    files_list = list(tuple_of_files)
 
     assert os.path.isfile(index_path)
     with open(index_path) as f:
         file_content = f.readlines()
 
-    assert len(file_content) == len(tuple_of_files)
+    assert len(file_content) == len(files_list)
 
     for file_entry, tuple_entry in zip(file_content, tuple_of_files):
         assert file_content == tuple_entry + "\n"
@@ -51,10 +88,6 @@ def test_lightning_dataset(tmpdir):
     with open(foo_path) as f:
         assert f.read() == "not bar anymore!"
 
-    with open(foo_path, "w") as f:
-        f.write("bar again!")
-
     file_obj = dset.open(foo_path, "w")
-    assert file_obj.read() == "bar again!"
     file_obj.close()
     assert file_obj._stream.closed
