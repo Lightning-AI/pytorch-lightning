5ceb1aebb790c95f1a96298502902d00181d8800
Jirka <jirka.borovec@seznam.cz>
2023-06-13 21:14:20 +0200

    6.2

--- docs/source-pytorch/conf.py
+++ docs/source-pytorch/conf.py
@@ -109,7 +109,7 @@ release = lightning.__version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = "7.0"
+needs_sphinx = "6.2"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
