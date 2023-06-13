14fc48f45a52354c5e0ddd1d25d0845e13156eaf
Jirka <jirka.borovec@seznam.cz>
2023-06-13 20:25:15 +0200

    docs: bump sphinx ver.

--- docs/source-pytorch/conf.py
+++ docs/source-pytorch/conf.py
@@ -109,7 +109,7 @@ release = lightning.__version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = "4.0"
+needs_sphinx = "7.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
