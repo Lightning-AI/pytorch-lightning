455ddb2cf30e4d03f094ba088b6635d6f0ca15be
Nicki Skafte Detlefsen <skaftenicki@gmail.com>
2023-06-19 08:38:57 +0200

    Add `torchaudio` to adjust script (#139)

--- scripts/adjust-torch-versions.py
+++ scripts/adjust-torch-versions.py
@@ -9,22 +9,22 @@ import sys
 from typing import Dict, Optional
 
 VERSIONS = [
-    {"torch": "2.0.1", "torchvision": "0.15.2", "torchtext": "0.15.2"},  # stable
-    {"torch": "2.0.0", "torchvision": "0.15.1", "torchtext": "0.15.1"},
-    {"torch": "1.14.0", "torchvision": "0.15.0", "torchtext": "0.15.0"},  # nightly
-    {"torch": "1.13.1", "torchvision": "0.14.1", "torchtext": "0.14.1"},  # stable
-    {"torch": "1.13.0", "torchvision": "0.14.0", "torchtext": "0.14.0"},
-    {"torch": "1.12.1", "torchvision": "0.13.1", "torchtext": "0.13.1"},
-    {"torch": "1.12.0", "torchvision": "0.13.0", "torchtext": "0.13.0"},
-    {"torch": "1.11.0", "torchvision": "0.12.0", "torchtext": "0.12.0"},
-    {"torch": "1.10.2", "torchvision": "0.11.3", "torchtext": "0.11.2"},
-    {"torch": "1.10.1", "torchvision": "0.11.2", "torchtext": "0.11.1"},
-    {"torch": "1.10.0", "torchvision": "0.11.1", "torchtext": "0.11.0"},
-    {"torch": "1.9.1", "torchvision": "0.10.1", "torchtext": "0.10.1"},
-    {"torch": "1.9.0", "torchvision": "0.10.0", "torchtext": "0.10.0"},
-    {"torch": "1.8.2", "torchvision": "0.9.1", "torchtext": "0.9.1"},
-    {"torch": "1.8.1", "torchvision": "0.9.1", "torchtext": "0.9.1"},
-    {"torch": "1.8.0", "torchvision": "0.9.0", "torchtext": "0.9.0"},
+    {"torch": "2.0.1", "torchvision": "0.15.2", "torchtext": "0.15.2", "torchaudio": "2.0.1"},  # stable
+    {"torch": "2.0.0", "torchvision": "0.15.1", "torchtext": "0.15.1", "torchaudio": "2.0.0"},
+    {"torch": "1.14.0", "torchvision": "0.15.0", "torchtext": "0.15.0", "torchaudio": "0.13.1"},  # nightly
+    {"torch": "1.13.1", "torchvision": "0.14.1", "torchtext": "0.14.1", "torchaudio": "0.13.1"},  # stable
+    {"torch": "1.13.0", "torchvision": "0.14.0", "torchtext": "0.14.0", "torchaudio": "0.13.0"},
+    {"torch": "1.12.1", "torchvision": "0.13.1", "torchtext": "0.13.1", "torchaudio": "0.12.1"},
+    {"torch": "1.12.0", "torchvision": "0.13.0", "torchtext": "0.13.0", "torchaudio": "0.12.0"},
+    {"torch": "1.11.0", "torchvision": "0.12.0", "torchtext": "0.12.0", "torchaudio": "0.11.0"},
+    {"torch": "1.10.2", "torchvision": "0.11.3", "torchtext": "0.11.2", "torchaudio": "0.10.2"},
+    {"torch": "1.10.1", "torchvision": "0.11.2", "torchtext": "0.11.1", "torchaudio": "0.10.1"},
+    {"torch": "1.10.0", "torchvision": "0.11.1", "torchtext": "0.11.0", "torchaudio": "0.10.0"},
+    {"torch": "1.9.1", "torchvision": "0.10.1", "torchtext": "0.10.1", "torchaudio": "0.9.1"},
+    {"torch": "1.9.0", "torchvision": "0.10.0", "torchtext": "0.10.0", "torchaudio": "0.9.0"},
+    {"torch": "1.8.2", "torchvision": "0.9.1", "torchtext": "0.9.1", "torchaudio": "0.8.1"},
+    {"torch": "1.8.1", "torchvision": "0.9.1", "torchtext": "0.9.1", "torchaudio": "0.8.1"},
+    {"torch": "1.8.0", "torchvision": "0.9.0", "torchtext": "0.9.0", "torchaudio": "0.8.0"},
 ]
 
 
