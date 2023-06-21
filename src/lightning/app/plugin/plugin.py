01cc776775e76d7b9bcf5dcbd7a3462b8aa4a971
Yurij Mikhalevich <yurij@lightning.ai>
2023-06-21 23:38:33 +0300

    feat: plugin update

--- src/lightning/app/plugin/plugin.py
+++ src/lightning/app/plugin/plugin.py
@@ -39,6 +39,7 @@ class LightningPlugin:
     actions."""
 
     def __init__(self) -> None:
+        self.plugin_id = ""
         self.project_id = ""
         self.cloudspace_id = ""
         self.cluster_id = ""
@@ -85,22 +86,26 @@ class LightningPlugin:
             cloudspace_id=self.cloudspace_id,
             name=name,
             cluster_id=self.cluster_id,
+            source_app=self.plugin_id,
         )
         # Return a relative URL so it can be used with the NavigateTo action.
         return url.replace(constants.get_lightning_cloud_url(), "")
 
     def _setup(
         self,
+        plugin_id: str,
         project_id: str,
         cloudspace_id: str,
         cluster_id: str,
     ) -> None:
+        self.plugin_id = plugin_id
         self.project_id = project_id
         self.cloudspace_id = cloudspace_id
         self.cluster_id = cluster_id
 
 
 class _Run(BaseModel):
+    plugin_id: str
     plugin_entrypoint: str
     source_code_url: str
     project_id: str
@@ -159,6 +164,7 @@ def _run_plugin(run: _Run) -> Dict[str, Any]:
         # Setup and run the plugin
         try:
             plugin._setup(
+                plugin_id=run.plugin_id,
                 project_id=run.project_id,
                 cloudspace_id=run.cloudspace_id,
                 cluster_id=run.cluster_id,
