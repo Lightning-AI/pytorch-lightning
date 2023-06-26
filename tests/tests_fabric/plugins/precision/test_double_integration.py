ae7cea2a3cd5cacaccf92201f1ef56db9a7dcb95
awaelchli <aedu.waelchli@gmail.com>
2023-06-25 18:25:37 -0700

    double

--- tests/tests_fabric/plugins/precision/test_double_integration.py
+++ tests/tests_fabric/plugins/precision/test_double_integration.py
@@ -16,8 +16,9 @@
 import torch
 import torch.nn as nn
 
-from tests_fabric.helpers.models import BoringFabric
 from tests_fabric.helpers.runif import RunIf
+
+from lightning.fabric import Fabric
 
 
 class BoringDoubleModule(nn.Module):
@@ -33,24 +34,23 @@ class BoringDoubleModule(nn.Module):
         return self.layer(x)
 
 
-class DoublePrecisionBoringFabric(BoringFabric):
-    def get_model(self):
-        return BoringDoubleModule()
-
-    def step(self, model, batch):
-        assert model.layer.weight.dtype == model.layer.bias.dtype == torch.float64
-        assert model.complex_buffer.dtype == torch.complex128
-
-        assert batch.dtype == torch.float32
-        output = model(batch)
-        assert output.dtype == torch.float32
-        return torch.nn.functional.mse_loss(output, torch.ones_like(output))
-
-    def after_backward(self, model, optimizer):
-        assert model.layer.weight.grad.dtype == torch.float64
-
-
 @RunIf(mps=False)  # MPS doesn't support float64
 def test_double_precision():
-    fabric = DoublePrecisionBoringFabric(devices=1, precision="64-true")
-    fabric.run()
+    fabric = Fabric(devices=1, precision="64-true")
+    model = BoringDoubleModule()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    model, optimizer = fabric.setup(model, optimizer)
+
+    batch = torch.rand(2, 32)
+    assert model.layer.weight.dtype == model.layer.bias.dtype == torch.float64
+    assert model.complex_buffer.dtype == torch.complex128
+
+    assert batch.dtype == torch.float32
+    output = model(batch)
+    assert output.dtype == torch.float32
+    loss = torch.nn.functional.mse_loss(output, torch.ones_like(output))
+    fabric.backward(loss)
+    assert model.layer.weight.grad.dtype == torch.float64
+    optimizer.step()
+    optimizer.zero_grad()
+
