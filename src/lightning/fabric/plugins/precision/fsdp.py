4f05d9c8593def06aa72e1e348d14a78b6147f9e
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-07-01 15:56:51 -0400

    mypy

--- src/lightning/fabric/plugins/precision/fsdp.py
+++ src/lightning/fabric/plugins/precision/fsdp.py
@@ -144,7 +144,7 @@ class FSDPPrecision(Precision):
             # skip scaler logic, as bfloat16 does not require scaler
             return super().optimizer_step(optimizer, **kwargs)
         # note: the scaler will skip the `optimizer.step` if nonfinite gradients are found
-        step_output = self.scaler.step(optimizer, **kwargs)
+        step_output = self.scaler.step(optimizer, **kwargs)  # type: ignore[arg-type]
         self.scaler.update()
         return step_output
 
