eed36c0707dea88fce17c37fe96ef21db3117c4b
Justus Schock <12886177+justusschock@users.noreply.github.com>
2023-07-07 11:27:01 +0200

    stupid typo

--- src/lightning/pytorch/callbacks/__init__.py
+++ src/lightning/pytorch/callbacks/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from lightning.pytorch.cakkbacks.spike import SpikeDetection
+from lightning.pytorch.callbacks.spike import SpikeDetection
 from lightning.pytorch.callbacks.batch_size_finder import BatchSizeFinder
 from lightning.pytorch.callbacks.callback import Callback
 from lightning.pytorch.callbacks.checkpoint import Checkpoint
