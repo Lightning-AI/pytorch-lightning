d53ad0bc148d9a31b3a26811e85481c9e6252976
Carlos Mocholí <carlossmocholi@gmail.com>
2023-07-14 05:53:54 +0200

    Various miscellaneous fixes

--- tests/tests_fabric/strategies/test_fsdp_integration.py
+++ tests/tests_fabric/strategies/test_fsdp_integration.py
@@ -321,7 +321,13 @@ def test_setup_with_orig_params_and_multiple_param_groups():
 
 @RunIf(min_cuda_gpus=2, skip_windows=True, standalone=True, dynamo=True)
 @mock.patch.dict(os.environ, {})
-@pytest.mark.parametrize("compile_after_setup", [False, True])
+@pytest.mark.parametrize(
+    "compile_after_setup",
+    [
+        False,
+        pytest.param(True, marks=RunIf(min_python="3.9", reason="https://github.com/pytorch/pytorch/issues/97811")),
+    ],
+)
 def test_compile(compile_after_setup):
     """Test that the model can be compiled before and after the model is wrapped in FSDP."""
     model = BoringModel()
