58568c4da5e8fc1b09473edd360a80a21f2b8565
Carlos Mocholí <carlossmocholi@gmail.com>
2023-07-22 04:19:57 +0200

    Uninstall typing extensions

--- tests/tests_pytorch/run_tpu_tests.sh
+++ tests/tests_pytorch/run_tpu_tests.sh
@@ -12,14 +12,18 @@ if [ "{PR_NUMBER}" != "master" ]; then  # if PR number is set
 fi
 
 echo "--- Install packages ---"
-pip list  # show what's already installed
+# show what's already installed
+pip list
+# typing-extensions==4.5.0 comes pre-installed in the environment, and pydantic doesnt support that, however,
+# pip is not smart enough to upgrade it
+pip uninstall -y typing-extensions
 # set particular PyTorch version
 pip install -q wget packaging
 python3 -m wget https://raw.githubusercontent.com/Lightning-AI/utilities/main/scripts/adjust-torch-versions.py
 for fpath in `ls requirements/**/*.txt`; do
   python3 adjust-torch-versions.py $fpath {PYTORCH_VERSION};
 done
-pip install -U .[pytorch-extra,pytorch-test] pytest-timeout
+pip install .[pytorch-extra,pytorch-test] pytest-timeout
 pip list
 
 # https://cloud.google.com/tpu/docs/v4-users-guide#train_ml_workloads_with_pytorch_xla
