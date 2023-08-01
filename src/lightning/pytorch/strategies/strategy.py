8569fe61ebafba0345e2b546782b853af7d31d4f
awaelchli <aedu.waelchli@gmail.com>
2023-08-01 21:12:40 +0200

    mypy

--- src/lightning/pytorch/strategies/strategy.py
+++ src/lightning/pytorch/strategies/strategy.py
@@ -14,7 +14,7 @@
 import logging
 from abc import ABC, abstractmethod
 from contextlib import contextmanager, nullcontext
-from typing import Any, Callable, Dict, Generator, List, Mapping, Optional, Tuple, TypeVar, Union
+from typing import Any, Callable, Dict, Generator, List, Mapping, Optional, Tuple, TypeVar, Union, cast
 
 import torch
 from torch import Tensor
@@ -108,7 +108,7 @@ class Strategy(ABC):
 
     def connect(self, model: "pl.LightningModule") -> None:
         """Called by the accelerator to connect the accelerator and the model with this plugin."""
-        model = self.precision_plugin.convert_module(model)
+        model = cast(pl.LightningModule, self.precision_plugin.convert_module(model))
         self._lightning_module = model
         self.model = model
 
