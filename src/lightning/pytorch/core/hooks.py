37732407723b82d81afdde4abd98f970a64ec365
awaelchli <aedu.waelchli@gmail.com>
2023-08-02 17:03:17 +0200

    typo fix

--- src/lightning/pytorch/core/hooks.py
+++ src/lightning/pytorch/core/hooks.py
@@ -547,9 +547,6 @@ class DataHooks:
 
         Raises:
             MisconfigurationException:
-                If using data-parallel, ``Trainer(strategy='dp')``.
-
-            MisconfigurationException:
                 If using IPUs, ``Trainer(accelerator='ipu')``.
 
         See Also:
