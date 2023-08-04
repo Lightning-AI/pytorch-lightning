e8ba67e9baad1c92b1238e26cec093080b06827b
awaelchli <aedu.waelchli@gmail.com>
2023-08-04 11:57:40 +0200

    include xlafsdp

--- src/lightning/fabric/connector.py
+++ src/lightning/fabric/connector.py
@@ -429,7 +429,7 @@ class _Connector:
         self._validate_precision_choice()
         if isinstance(self._precision_instance, Precision):
             return self._precision_instance
-        if isinstance(self.strategy, (SingleDeviceXLAStrategy, XLAStrategy)):
+        if isinstance(self.strategy, (SingleDeviceXLAStrategy, XLAStrategy, XLAFSDPStrategy)):
             return XLAPrecision(self._precision_input)
         if isinstance(self.strategy, DeepSpeedStrategy):
             return DeepSpeedPrecision(self._precision_input)  # type: ignore
