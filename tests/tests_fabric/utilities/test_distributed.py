0b73f7c0110cdc3d53a2687afac7b29523a30fdf
awaelchli <aedu.waelchli@gmail.com>
2023-08-05 20:07:23 +0200

    fix

--- tests/tests_fabric/utilities/test_distributed.py
+++ tests/tests_fabric/utilities/test_distributed.py
@@ -108,7 +108,7 @@ def _test_all_reduce(strategy):
     "devices",
     [
         pytest.param([torch.device("cuda:0"), torch.device("cuda:1")], marks=RunIf(min_cuda_gpus=2)),
-        [torch.device("cpu")] * 2,
+        [torch.device("cpu"), torch.device("cpu")],
     ],
 )
 def test_collective_operations(devices, process):
