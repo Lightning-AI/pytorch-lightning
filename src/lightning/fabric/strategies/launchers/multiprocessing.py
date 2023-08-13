1a5f402e741b53b731d8bd80037ab00b53c8a1da
awaelchli <aedu.waelchli@gmail.com>
2023-08-13 17:02:30 +0200

    simplify

--- src/lightning/fabric/strategies/launchers/multiprocessing.py
+++ src/lightning/fabric/strategies/launchers/multiprocessing.py
@@ -209,10 +209,10 @@ def _disable_module_memory_sharing(data: Any) -> Any:
     # For model weights and buffers, this is undesired and can lead to race conditions between processes.
     # Hence, we copy the tensors in the entire module to ensure it doesn't share memory with other processes.
 
+    @torch.no_grad()
     def _unshare(module: Module) -> Module:
         for tensor in itertools.chain(module.parameters(), module.buffers()):
-            with torch.no_grad():
-                tensor.data = tensor.data.clone()
+            tensor.data = tensor.data.clone()
         return module
 
     return apply_to_collection(data, function=_unshare, dtype=Module)
