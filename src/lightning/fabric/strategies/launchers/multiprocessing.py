007c27402e2736a8aec354004ab91fc3a6937e54
awaelchli <aedu.waelchli@gmail.com>
2023-08-18 11:47:58 +0200

    dedent

--- src/lightning/fabric/strategies/launchers/multiprocessing.py
+++ src/lightning/fabric/strategies/launchers/multiprocessing.py
@@ -15,6 +15,7 @@ import itertools
 import os
 from dataclasses import dataclass
 from multiprocessing.queues import SimpleQueue
+from textwrap import dedent
 from typing import Any, Callable, Dict, Literal, Optional, TYPE_CHECKING
 
 import torch
@@ -224,17 +225,19 @@ def _check_missing_main_guard() -> None:
     """Raises an exception if the ``__name__ == "__main__"`` guard is missing."""
     if not getattr(mp.current_process(), "_inheriting", False):
         return
-    message = """
-    Launching multiple processes with the 'spawn' start method requires that your script guards the main
-    function with an `if __name__ == \"__main__\"` clause. For example:
-
-    def main():
-        # Put your code here
-        ...
-
-    if __name__ == "__main__":
-        main()
-
-    Alternatively, you can run with `strategy="ddp"` to avoid this error.
-    """
+    message = dedent(
+        """
+        Launching multiple processes with the 'spawn' start method requires that your script guards the main
+        function with an `if __name__ == \"__main__\"` clause. For example:
+    
+        def main():
+            # Put your code here
+            ...
+    
+        if __name__ == "__main__":
+            main()
+    
+        Alternatively, you can run with `strategy="ddp"` to avoid this error.
+        """
+    )
     raise RuntimeError(message)
