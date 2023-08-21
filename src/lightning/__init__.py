d895176e6dc625906cf6f1d854a043c7dd0f5404
Jirka <jirka.borovec@seznam.cz>
2023-08-21 14:30:21 +0200

    ignore

--- src/lightning/__init__.py
+++ src/lightning/__init__.py
@@ -29,7 +29,7 @@ from lightning.pytorch.core import LightningDataModule, LightningModule  # noqa:
 from lightning.pytorch.trainer import Trainer  # noqa: E402
 
 import lightning.app  # isort: skip # noqa: E402
-import lightning.store  # isort: skip # noqa: E402
+import lightning.store  # isort: skip # noqa: E402, F401
 
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(os.path.dirname(_PACKAGE_ROOT))
