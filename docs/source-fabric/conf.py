6c4e5478d7cd823ea4cb2f7d41b8787c31d1a1bc
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-08-22 22:49:37 +0200

    typo
    Co-authored-by: Adrian Wälchli <aedu.waelchli@gmail.com>

--- docs/source-fabric/conf.py
+++ docs/source-fabric/conf.py
@@ -254,7 +254,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
     "pytorch_lightning": ("https://lightning.ai/docs/pytorch/stable/", None),
-    "lightning": ("https://lightning.ai/docs/app/stable//", None),
+    "lightning": ("https://lightning.ai/docs/app/stable/", None),
     "tensorboardX": ("https://tensorboardx.readthedocs.io/en/stable/", None),
 }
 nitpicky = True
