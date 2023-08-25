c771d95287f217b1844f8810e9425208d9c4bf99
Jirka <jirka.borovec@seznam.cz>
2023-08-25 13:23:10 +0200

    warnings

--- src/lightning/__init__.py
+++ src/lightning/__init__.py
@@ -1,5 +1,6 @@
 """Root package info."""
 import logging
+import warnings
 
 from lightning_utilities import module_available
 
@@ -14,7 +15,7 @@ formatter = logging.Formatter("%(levelname)s: %(message)s")
 _console.setFormatter(formatter)
 _logger.addHandler(_console)
 
-from lightning.__about__ import *  # noqa: E402, F401, F403
+from lightning.__about__ import *  # noqa: E402, F403
 from lightning.__version__ import version as __version__  # noqa: E402, F401
 from lightning.fabric.fabric import Fabric  # noqa: E402
 from lightning.fabric.utilities.seed import seed_everything  # noqa: E402
@@ -33,7 +34,6 @@ __all__ = [
 
 # todo: try to parse if the foiling is because of dependency or just wrong import
 
-
 if module_available("lightning.app"):
     from lightning.app import storage
     from lightning.app.core.app import LightningApp
@@ -43,9 +43,18 @@ if module_available("lightning.app"):
     from lightning.app.utilities.packaging.cloud_compute import CloudCompute
 
     __all__ += ["LightningApp", "LightningFlow", "LightningWork", "BuildConfig", "CloudCompute", "storage"]
-
+else:
+    warnings.warn(
+        "Importing App's sub-pckage failed."
+        " If you intend to use them, please install required dependencies `pip install lighting[app]`."
+    )
 
 if module_available("lightning.data"):
     from lightning.data import LightningDataset, LightningIterableDataset
 
     __all__ += ["LightningDataset", "LightningIterableDataset"]
+else:
+    warnings.warn(
+        "Importing Data's sub-pckage failed."
+        " If you intend to use them, please install required dependencies `pip install lighting[data]`."
+    )
