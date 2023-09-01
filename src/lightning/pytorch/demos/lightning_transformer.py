3d29d5b6305055fe30085740d5f4cfbf620ffde3
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-09-01 19:23:42 -0400

    update demo typehints

--- src/lightning/pytorch/demos/lightning_transformer.py
+++ src/lightning/pytorch/demos/lightning_transformer.py
@@ -1,26 +1,27 @@
 import torch
+from torch import Tensor
 
 import lightning.pytorch as pl
 from lightning.pytorch.demos.transformer import Transformer
 
 
 class LightningTransformer(pl.LightningModule):
-    def __init__(self, vocab_size):
+    def __init__(self, vocab_size: int) -> None:
         super().__init__()
         self.model = Transformer(vocab_size=vocab_size)
 
-    def forward(self, batch):
+    def forward(self, batch) -> Tensor:
         input, target = batch
         return self.model(input.view(1, -1), target.view(1, -1))
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx) -> Tensor:
         input, target = batch
         output = self.model(input.view(1, -1), target.view(1, -1))
         loss = torch.nn.functional.nll_loss(output, target.view(-1))
         return loss
 
-    def predict_step(self, batch):
+    def predict_step(self, batch) -> Tensor:
         return self(batch)
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> None:
         return torch.optim.SGD(self.model.parameters(), lr=0.1)
