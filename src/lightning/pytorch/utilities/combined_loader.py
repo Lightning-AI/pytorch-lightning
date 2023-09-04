34b95da409013094dd18176461ea80c464976508
awaelchli <aedu.waelchli@gmail.com>
2023-09-04 16:22:10 +0200

    update

--- src/lightning/pytorch/utilities/combined_loader.py
+++ src/lightning/pytorch/utilities/combined_loader.py
@@ -326,9 +326,9 @@ class CombinedLoader(Iterable):
 
     def __len__(self) -> int:
         """Compute the number of batches."""
-        for dl in self.flattened:
-            if sized_len(dl) is None:
-                raise NotImplementedError(f"`{type(dl).__name__}` does not define `__len__`")
+        # for dl in self.flattened:
+        #     if sized_len(dl) is None:
+        #         raise NotImplementedError(f"`{type(dl).__name__}` does not define `__len__`")
         return len(self._iterator)
 
     def reset(self) -> None:
