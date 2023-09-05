4dd975e44301b89b645f4827e5eda0d628fa6625
awaelchli <aedu.waelchli@gmail.com>
2023-09-05 15:46:54 +0200

    reset bug_report_model.py

--- examples/pytorch/bug_report/bug_report_model.py
+++ examples/pytorch/bug_report/bug_report_model.py
@@ -1,17 +1,15 @@
+import os
+
 import torch
 from torch.utils.data import DataLoader, Dataset
 
 from lightning.pytorch import LightningModule, Trainer
 
-global_batch_size = 4
-micro_batch_size = 2
-assert global_batch_size % micro_batch_size == 0
-
 
 class RandomDataset(Dataset):
-    def __init__(self, length):
+    def __init__(self, size, length):
         self.len = length
-        self.data = torch.randn(length, 32)
+        self.data = torch.randn(length, size)
 
     def __getitem__(self, index):
         return self.data[index]
@@ -24,63 +22,45 @@ class BoringModel(LightningModule):
     def __init__(self):
         super().__init__()
         self.layer = torch.nn.Linear(32, 2)
-        self.val_fetched = 0
-        self.val_iter_raised = False
-        self.val_iter_done = False
-        self.val_step_entered = 0
 
-        self.train_fetched = 0
-        self.train_iter_raised = False
-        self.train_iter_done = False
-        self.train_step_entered = 0
+    def forward(self, x):
+        return self.layer(x)
 
-    def training_step(self, dataloader_iter, batch_idx):
-        self.train_step_entered += 1
-        self.train_iter_done = dataloader_iter.done
-        for i in range(global_batch_size // micro_batch_size):
-            try:
-                batch = next(dataloader_iter)
-            except StopIteration:
-                self.train_iter_raised = True
-                return None
-            self.train_fetched += 1
-        return self.layer(batch).sum()
+    def training_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("train_loss", loss)
+        return {"loss": loss}
 
-    def validation_step(self, dataloader_iter, batch_idx):
-        self.val_step_entered += 1
-        self.val_iter_done = dataloader_iter.done
-        for i in range(global_batch_size // micro_batch_size):
-            try:
-                batch = next(dataloader_iter)
-            except StopIteration:
-                self.val_iter_raised = True
-                return
-            self.val_fetched += 1
-            self.layer(batch).sum()
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("valid_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("test_loss", loss)
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.layer.parameters(), lr=0.1)
 
 
-train_data = DataLoader(RandomDataset(length=16), batch_size=micro_batch_size)
-val_data = DataLoader(RandomDataset(length=16), batch_size=micro_batch_size)
+def run():
+    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
 
-model = BoringModel()
-trainer = Trainer(
-    # limit_train_batches=3,
-    # limit_val_batches=4,
-    num_sanity_val_steps=2,
-    max_steps=2,
-    max_epochs=1,
-    accelerator="cpu",
-)
-trainer.fit(model, train_data, val_data)
-# trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
+    model = BoringModel()
+    trainer = Trainer(
+        default_root_dir=os.getcwd(),
+        limit_train_batches=1,
+        limit_val_batches=1,
+        limit_test_batches=1,
+        num_sanity_val_steps=0,
+        max_epochs=1,
+        enable_model_summary=False,
+    )
+    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
+    trainer.test(model, dataloaders=test_data)
 
-print("train fetched", model.train_fetched)
-print("train step entered", model.train_step_entered)
-print("train iter exhausted", model.train_iter_raised)
 
-print("val fetched", model.val_fetched)
-print("val step entered", model.val_step_entered)
-print("val iter exhausted", model.val_iter_raised)
+if __name__ == "__main__":
+    run()
