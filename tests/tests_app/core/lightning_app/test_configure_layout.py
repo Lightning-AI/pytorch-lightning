e88ef926ba3b6b11073000c11bc6a4b40e4cc276
Jirka <jirka.borovec@seznam.cz>
2023-09-11 23:05:04 +0200

    skipif

--- tests/tests_app/core/lightning_app/test_configure_layout.py
+++ tests/tests_app/core/lightning_app/test_configure_layout.py
@@ -216,6 +216,7 @@ class DynamicContentComponent(EmptyFlow):
         )
 
 
+@pytest.mark.skipif(_IS_WINDOWS, reason="strange TimeOut exception")
 @pytest.mark.xfail(strict=False, reason="hanging... need to be fixed")  # fixme
 def test_dynamic_content_layout_update():
     """Test that the `configure_layout()` gets called as part of the loop and can return new layouts."""
