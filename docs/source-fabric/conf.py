a3ce7fb1fe09edec80f70569e0faaca63777293b
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-09-15 20:50:47 +0200

    Revert "try Jirka's suggestion"
    This reverts commit 838411f3fc32afbcfd1ae0d02785d28caa84489e.

--- docs/source-fabric/conf.py
+++ docs/source-fabric/conf.py
@@ -274,6 +274,8 @@ nitpick_ignore_regex = [
     ("py:class", "torch.distributed.fsdp.wrap.ModuleWrapPolicy"),
     ("py:class", "torch.distributed.fsdp.sharded_grad_scaler.ShardedGradScaler"),
     # Mocked optional packages
+    ("py:class", "deepspeed.*"),
+    ("py:.*", "torch_xla.*"),
     ("py:class", "transformer_engine.*"),
 ]
 
