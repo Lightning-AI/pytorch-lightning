457d112ee5d9520aa5360fd304e25dc96e85f5ee
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-09-20 17:42:24 -0400

    remove snippet file

--- quantization.py
+++
@@ -1,6 +0,0 @@
-from lightning_fabric import Fabric
-from lightning_fabric.plugins.precision import BitsandbytesQuantization
-
-quantization_plugin = BitsandbytesQuantization(mode="bnb.int8")
-
-fabric = Fabric(plugins=quantization_plugin)
