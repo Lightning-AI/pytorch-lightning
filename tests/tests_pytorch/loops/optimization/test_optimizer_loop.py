5c23ce7ebe69b7659154a5c139be00c74ff20b07
nik777 <nikttt@gmail.com>
2023-09-27 13:20:35 -0700

    Fixed expected exception message in test

--- tests/tests_pytorch/loops/optimization/test_optimizer_loop.py
+++ tests/tests_pytorch/loops/optimization/test_optimizer_loop.py
@@ -65,7 +65,7 @@ class OutputMapping(Generic[T], Mapping[str, T]):
 @pytest.mark.parametrize(
     "case",
     [
-        (5.0, "must return a Tensor, a dict, or None"),
+        (5.0, "must return a Tensor, a Mapping, or None"),
         ({"a": 5}, "the 'loss' key needs to be present"),
         (OutputMapping[str, int]({"a": 5}), "the 'loss' key needs to be present"),
     ],
