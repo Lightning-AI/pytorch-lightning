be43aa1ef9a22a61b506b6576a9bd40617386e06
Jirka <jirka.borovec@seznam.cz>
2023-09-27 11:57:45 +0200

    precommit

--- tests/tests_cli/conftest.py
+++ tests/tests_cli/conftest.py
@@ -1,22 +1,7 @@
-import contextlib
 import os
-import shutil
-import signal
-import threading
-from datetime import datetime
-from pathlib import Path
 from threading import Thread
 
-import psutil
-import py
 import pytest
-from lightning.app.core import constants
-from lightning.app.storage.path import _storage_root_dir
-from lightning.app.utilities.app_helpers import _collect_child_process_pids
-from lightning.app.utilities.component import _set_context
-from lightning.app.utilities.packaging import cloud_compute
-from lightning.app.utilities.packaging.app_config import _APP_CONFIG_FILENAME
-from lightning.app.utilities.state import AppState
 
 os.environ["LIGHTNING_DISPATCHED"] = "1"
 
