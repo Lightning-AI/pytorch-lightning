bcc29cdd6db227aabacc4c8f6b4c52b75f37177d
Jirka <jirka.borovec@seznam.cz>
2023-10-03 00:47:40 +0200

    docs: prune ignored links

--- docs/source-pytorch/conf.py
+++ docs/source-pytorch/conf.py
@@ -596,8 +596,5 @@ linkcheck_exclude_documents = [r"^(.*\/)*CHANGELOG.*$"]
 
 # ignore the following relative links (false positive errors during linkcheck)
 linkcheck_ignore = [
-    r"^starter/installation.html$",
     r"^installation.html$",
-    r"^../cli/lightning_cli.html$",
-    r"^../common/trainer.html#trainer-flags$",
 ]
