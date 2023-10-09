19e28c7b915a8cf3c654f08c0402b65f99a3e05c
awaelchli <aedu.waelchli@gmail.com>
2023-10-10 01:43:11 +0200

    typo

--- src/lightning/fabric/utilities/warnings.py
+++ src/lightning/fabric/utilities/warnings.py
@@ -24,7 +24,7 @@ _default_format_warning = warnings.formatwarning
 
 
 def disable_possible_user_warnings(module: str = "") -> None:
-    """Ignore warnings of the category :class:`PossibleUserWarnings` from Lightning.
+    """Ignore warnings of the category :class:`PossibleUserWarning` from Lightning.
 
     For more granular control over which warnings to ignore, use :func:`warnings.filterwarnings` directly.
 
