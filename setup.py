8237cb540a4a38389532602c009e67f307715ed1
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-10-11 02:08:35 +0900

    ci: testing with py3.12 (#182)

--- setup.py
+++ setup.py
@@ -78,5 +78,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
