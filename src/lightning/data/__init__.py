1fc422221130f784951eb4a63ddc411f3a97e7f6
thomas <thomas@thomass-MacBook-Pro.local>
2023-10-13 17:28:22 +0100

    update

--- src/lightning/data/__init__.py
+++ src/lightning/data/__init__.py
@@ -1,3 +1,4 @@
 from lightning.data.datasets import LightningDataset, LightningIterableDataset
+from lightning.data.streaming.dataset import StreamingDataset
 
-__all__ = ["LightningDataset", "LightningIterableDataset"]
+__all__ = ["LightningDataset", "StreamingDataset", "LightningIterableDataset"]
