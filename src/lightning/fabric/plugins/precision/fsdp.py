5f3a29fd0e708524b152e4d10c803fd054283254
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-10-18 16:14:42 -0400

    update

--- src/lightning/fabric/plugins/precision/fsdp.py
+++ src/lightning/fabric/plugins/precision/fsdp.py
@@ -83,10 +83,10 @@ class FSDPPrecision(Precision):
         # `torch.float32` here with PyTorch < 2.0.
         if self.precision == "16-mixed":
             param_dtype = None if not _TORCH_GREATER_EQUAL_2_0 else torch.float32
-            reduce_dtype = buffer_dtype = torch.float16
+            reduce_dtype = buffer_dtype = None
         elif self.precision == "bf16-mixed":
             param_dtype = None if not _TORCH_GREATER_EQUAL_2_0 else torch.float32
-            reduce_dtype = buffer_dtype = torch.bfloat16
+            reduce_dtype = buffer_dtype = None
         elif self.precision == "16-true":
             param_dtype = reduce_dtype = buffer_dtype = torch.float16
         elif self.precision == "bf16-true":
