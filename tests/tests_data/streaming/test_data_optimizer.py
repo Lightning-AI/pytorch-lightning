9f5009bc75011b3282912071f75e0aa2239dba46
thomas <thomas@thomass-MacBook-Pro.local>
2023-10-19 05:13:47 -0700

    update

--- tests/tests_data/streaming/test_data_optimizer.py
+++ tests/tests_data/streaming/test_data_optimizer.py
@@ -217,7 +217,9 @@ def test_data_optimizer(fast_dev_run, delete_cached_files, tmpdir, monkeypatch):
 
 @pytest.mark.parametrize("delete_cached_files", [False])
 @pytest.mark.parametrize("fast_dev_run", [False])
-@pytest.mark.skipif(condition=not _PIL_AVAILABLE or sys.platform == "win32", reason="Requires: ['pil']")
+@pytest.mark.skipif(
+    condition=not _PIL_AVAILABLE or sys.platform == "win32" or sys.platform == "darwin", reason="Requires: ['pil']"
+)
 def test_data_optimizer_distributed(fast_dev_run, delete_cached_files, tmpdir, monkeypatch):
     """This test ensures the data optimizer works in a fully distributed settings."""
 
