8e937476ffcf3ec914296dee0d3529968af7b513
thomas <thomas@thomass-MacBook-Pro.local>
2023-10-19 05:13:02 -0700

    update

--- tests/tests_data/streaming/test_cache.py
+++ tests/tests_data/streaming/test_cache.py
@@ -182,7 +182,6 @@ def test_cache_with_auto_wrapping(tmpdir):
     assert sorted(os.listdir(os.path.join(tmpdir, "cache_1"))) == [
         "chunk-0-0.bin",
         "chunk-0-1.bin",
-        "chunk-0-2.bin",
         "index.json",
     ]
     # Your dataset is optimised for the cloud
