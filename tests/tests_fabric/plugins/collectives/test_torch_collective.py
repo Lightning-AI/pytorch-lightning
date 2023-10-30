73e429c47ccd2853c3852980dfe88d128b167189
awaelchli <aedu.waelchli@gmail.com>
2023-10-30 22:11:05 +0100

    flaky

--- tests/tests_fabric/plugins/collectives/test_torch_collective.py
+++ tests/tests_fabric/plugins/collectives/test_torch_collective.py
@@ -286,6 +286,7 @@ def _test_default_process_group(strategy, *collectives):
 
 
 @skip_distributed_unavailable
+@pytest.mark.flaky(reruns=5)
 @RunIf(skip_windows=True)  # unhandled timeouts
 def test_default_process_group():
     collective_launch(_test_default_process_group, [torch.device("cpu")] * 3, num_groups=2)
