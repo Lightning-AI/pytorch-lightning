e6f6754442c2ecdd80fa1fca7a8c899cd6b2977a
thomas <thomas@thomass-MacBook-Pro.local>
2023-11-01 14:58:33 +0000

    update

--- src/lightning/data/streaming/data_processor.py
+++ src/lightning/data/streaming/data_processor.py
@@ -802,6 +802,8 @@ class DataProcessor:
                         continue
                     self.workers_tracker[index] = counter
                     new_total = sum(self.workers_tracker.values())
+                if all(not w.is_alive() for w in self.workers):
+                    break
                 pbar.update(new_total - current_total)
                 current_total = new_total
                 if current_total == num_items:
