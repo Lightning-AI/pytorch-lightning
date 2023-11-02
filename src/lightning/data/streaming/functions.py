529f00af3144f60305ee1732b90c84287c652f96
thomas <thomas@thomass-MacBook-Pro.local>
2023-11-02 20:06:28 +0000

    update

--- src/lightning/data/streaming/functions.py
+++ src/lightning/data/streaming/functions.py
@@ -84,10 +84,10 @@ class LambdaDataChunkRecipe(DataChunkRecipe):
             else:
                 yield self._fn(item_metadata)
         elif inspect.isclass(self._fn) and callable(self._fn):
-            if inspect.isgeneratorfunction(self._fn.__call__):  # type: ignore
-                yield from self._fn.__call__(item_metadata)  # type: ignore
+            if inspect.isgeneratorfunction(self._fn.__call__):
+                yield from self._fn.__call__(item_metadata)
             else:
-                yield self._fn.__call__(item_metadata)  # type: ignore
+                yield self._fn.__call__(item_metadata)
         else:
             raise ValueError(f"The provided {self._fn} isn't supported.")
 
