71208324277371433d0a62a5cc3cae5577b3dd04
Jirka <jirka.borovec@seznam.cz>
2023-11-02 12:34:47 +0100

    intersphinx_map

--- docs/source-app/conf.py
+++ docs/source-app/conf.py
@@ -254,7 +254,8 @@ epub_exclude_files = ["search.html"]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
-    # "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "lightning": ("https://lightning.ai/docs/pytorch/stable/", None),
 }
 
 nitpicky = True
