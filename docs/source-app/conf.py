372fec816c62108243e25cb20ac529cad016dc48
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-11-03 01:45:00 +0900

    ci/docs: fix copy integration sub-docs (#18921)
    (cherry picked from commit e9dcf4840e7c8785d2abf0d4da659c42b2cf5c87)

--- docs/source-app/conf.py
+++ docs/source-app/conf.py
@@ -254,7 +254,7 @@ epub_exclude_files = ["search.html"]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
-    # "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
 }
 
 nitpicky = True
