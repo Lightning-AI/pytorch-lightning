cb4ef47daa3712b9676022a97a73f1d4a1f07b13
awaelchli <aedu.waelchli@gmail.com>
2023-11-03 19:43:25 +0100

    no need to go this far

--- src/lightning/data/utilities/format.py
+++ src/lightning/data/utilities/format.py
@@ -1,6 +1,6 @@
 def _human_readable_bytes(num_bytes: int) -> str:
-    for unit in ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"):
+    for unit in ("B", "KB", "MB", "GB", "TB"):
         if abs(num_bytes) < 1000.0:
             return f"{num_bytes:3.1f} {unit}"
         num_bytes /= 1000.0
-    return f"{num_bytes:.1f} YB"
+    return f"{num_bytes:.1f} PB"
