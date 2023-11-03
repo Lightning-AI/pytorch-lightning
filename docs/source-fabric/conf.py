1fe9f14f1162a29092d8538f6f81f892d6a751e4
thomas chaton <thomas@grid.ai>
2023-11-03 11:28:55 +0000

    Improve map, optimize and StreamingDataset (#18912)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: thomas <thomas@thomass-MacBook-Pro.local>
    (cherry picked from commit f5f4d0a26471400975fdb6ea59337eaf5c51b62f)

--- docs/source-fabric/conf.py
+++ docs/source-fabric/conf.py
@@ -408,7 +408,7 @@ coverage_skip_undoc_in_source = True
 linkcheck_anchors = False
 
 # A timeout value, in seconds, for the linkcheck builder.
-linkcheck_timeout = 10
+linkcheck_timeout = 60
 
 # ignore all links in any CHANGELOG file
 linkcheck_exclude_documents = [r"^(.*\/)*CHANGELOG.*$"]
