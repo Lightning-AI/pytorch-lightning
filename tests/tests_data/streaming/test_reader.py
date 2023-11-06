e1b8073a2a7be835d7910d1ea0c2e2ca5a0578f2
thomas <thomas@thomass-MacBook-Pro.local>
2023-11-06 19:49:01 +0000

    update

--- tests/tests_data/streaming/test_reader.py
+++ tests/tests_data/streaming/test_reader.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+from unittest import mock
 
 from lightning.data.streaming import reader
 from lightning.data.streaming.cache import Cache
+from lightning.data.utilities import disk
 from lightning_cloud.resolver import Dir
 
 
@@ -38,3 +40,12 @@ def test_reader_chunk_removal(tmpdir, monkeypatch):
         assert cache[i] == i
 
     assert len(os.listdir(cache_dir)) == 3
+
+
+def test_disk_get_available_amount_of_bytes(monkeypatch):
+    result = b"Filesystem     1024-blocks      Used Available Capacity iused      ifree %iused  Mounted on\n/dev/disk3s1s1   971350180   9662852 397071612     3%  390143 3970716120    0%   /\n"  # noqa: E501
+    subprocess_mock = mock.MagicMock()
+    subprocess_mock.check_output.return_value = result
+    monkeypatch.setattr(disk, "subprocess", subprocess_mock)
+    assert disk._get_available_amount_of_bytes() == 397071612
+    subprocess_mock.check_output.assert_called()
