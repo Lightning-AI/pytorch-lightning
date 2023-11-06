4f94e55d5337399b8eedcba55f1047d0442862a7
awaelchli <aedu.waelchli@gmail.com>
2023-11-06 22:07:44 +0100

    test

--- tests/tests_data/streaming/test_dataset.py
+++ tests/tests_data/streaming/test_dataset.py
@@ -20,7 +20,7 @@ from lightning import seed_everything
 from lightning.data.datasets.env import _DistributedEnv
 from lightning.data.streaming import Cache
 from lightning.data.streaming.dataloader import StreamingDataLoader
-from lightning.data.streaming.dataset import StreamingDataset
+from lightning.data.streaming.dataset import StreamingDataset, _try_create_cache_dir
 from lightning.data.streaming.item_loader import TokensLoader
 from lightning.data.streaming.shuffle import FullShuffle, NoShuffle
 from lightning.pytorch.demos.boring_classes import RandomDataset
@@ -251,3 +251,13 @@ def test_streaming_dataset_deepcopy(tmpdir, monkeypatch):
         batches.append(batch)
 
     assert len(batches) == 10
+
+
+def test_try_create_cache_dir():
+    with mock.patch.dict(os.environ, {}, clear=True):
+        assert _try_create_cache_dir() is None
+
+    # the cache dir creating at /cache requires root privileges, so we need to mock `os.makedirs()`
+    with mock.patch.dict("os.environ", {"LIGHTNING_CLUSTER_ID": "abc", "LIGHTNING_CLOUD_PROJECT_ID": "123"}), mock.patch("lightning.data.streaming.dataset.os.makedirs"):
+        assert _try_create_cache_dir() == "/cache/0/chunks"
+        assert _try_create_cache_dir(shard_rank=3) == "/cache/3/chunks"
