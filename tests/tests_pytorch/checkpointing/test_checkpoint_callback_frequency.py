8ac765b95e8747a9a878a05ebd89e2b3d7dea47d
awaelchli <aedu.waelchli@gmail.com>
2023-11-23 02:49:15 +0100

    test

--- tests/tests_pytorch/checkpointing/test_checkpoint_callback_frequency.py
+++ tests/tests_pytorch/checkpointing/test_checkpoint_callback_frequency.py
@@ -16,10 +16,12 @@ from unittest import mock
 
 import pytest
 import torch
-from lightning.pytorch import Trainer, callbacks
+from lightning.pytorch import Trainer
 from lightning.pytorch.demos.boring_classes import BoringModel
 
 from tests_pytorch.helpers.runif import RunIf
+
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 
 def test_disabled_checkpointing(tmpdir):
@@ -71,7 +73,7 @@ def test_top_k(save_mock, tmpdir, k: int, epochs: int, val_check_interval: float
 
     model = TestModel()
     trainer = Trainer(
-        callbacks=[callbacks.ModelCheckpoint(dirpath=tmpdir, monitor="my_loss", save_top_k=k, save_last=save_last)],
+        callbacks=[ModelCheckpoint(dirpath=tmpdir, monitor="my_loss", save_top_k=k, save_last=save_last)],
         default_root_dir=tmpdir,
         max_epochs=epochs,
         enable_model_summary=False,
@@ -86,18 +88,20 @@ def test_top_k(save_mock, tmpdir, k: int, epochs: int, val_check_interval: float
 
 @mock.patch("torch.save")
 @RunIf(min_cuda_gpus=2, standalone=True)
-@pytest.mark.parametrize(("k", "epochs", "val_check_interval", "expected"), [(1, 1, 1.0, 1), (2, 2, 0.3, 4)])
+@pytest.mark.parametrize(("k", "epochs", "val_check_interval", "expected"), [
+    (1, 1, 1.0, 1),
+    (2, 2, 0.3, 4),
+    (4, 2, 0.1, 4),
+])
 def test_top_k_ddp(save_mock, tmpdir, k, epochs, val_check_interval, expected):
     class TestModel(BoringModel):
         def training_step(self, batch, batch_idx):
-            local_rank = int(os.getenv("LOCAL_RANK"))
-            self.log("my_loss", batch_idx * (1 + local_rank), on_epoch=True)
+            self.log("my_loss", batch_idx * (1 + self.trainer.local_rank), on_epoch=True)
             return super().training_step(batch, batch_idx)
 
         def on_train_epoch_end(self):
-            local_rank = int(os.getenv("LOCAL_RANK"))
             if self.trainer.is_global_zero:
-                self.log("my_loss_2", (1 + local_rank), on_epoch=True, rank_zero_only=True)
+                self.log("rank_zero_only_loss", (1 + self.trainer.local_rank), on_epoch=True, rank_zero_only=True)
             data = str(self.global_rank)
             obj = [[data], (data,), set(data)]
             out = self.trainer.strategy.broadcast(obj)
@@ -106,7 +110,10 @@ def test_top_k_ddp(save_mock, tmpdir, k, epochs, val_check_interval, expected):
 
     model = TestModel()
     trainer = Trainer(
-        callbacks=[callbacks.ModelCheckpoint(dirpath=tmpdir, monitor="my_loss_step", save_top_k=k, mode="max")],
+        callbacks=[
+            ModelCheckpoint(dirpath=tmpdir, monitor="my_loss_step", save_top_k=k, mode="max"),
+            ModelCheckpoint(dirpath=tmpdir, monitor="rank_zero_only_loss", save_top_k=k, mode="max")
+        ],
         default_root_dir=tmpdir,
         enable_progress_bar=False,
         max_epochs=epochs,
@@ -119,5 +126,7 @@ def test_top_k_ddp(save_mock, tmpdir, k, epochs, val_check_interval, expected):
         limit_val_batches=32,
     )
     trainer.fit(model)
-    if os.getenv("LOCAL_RANK") == "0":
+    if trainer.local_rank == 0:
         assert save_mock.call_count == expected
+    else:
+        assert save_mock.call_count == 0
