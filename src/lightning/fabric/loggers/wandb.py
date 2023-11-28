58561e50b69c0fcf8a0754fa0b2dc3eaad689089
Anish Shah <anish@wandb.com>
2023-11-28 14:26:36 -0500

    Update wandb.py
    Format to match other fabric loggers better

--- src/lightning/fabric/loggers/wandb.py
+++ src/lightning/fabric/loggers/wandb.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Weights and Biases Logger
--------------------------
-"""
+
 import os
 from argparse import Namespace
 from pathlib import Path
