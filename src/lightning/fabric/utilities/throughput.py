f2af669c18cdbdd386284016c677ac2a6a7a8e20
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-11-28 10:45:58 -0500

    fix

--- src/lightning/fabric/utilities/throughput.py
+++ src/lightning/fabric/utilities/throughput.py
@@ -183,16 +183,15 @@ class Throughput:
                     {"batches_per_sec": samples_per_sec, "samples_per_sec": dev_samples_per_sec * self.world_size}
                 )
 
-        if len(self._lengths) == self._lengths.maxlen:
-            elapsed_time = self._time[-1] - self._time[0]
-            elapsed_lengths = self._lengths[-1] - self._lengths[0]
-            dev_items_per_sec = elapsed_lengths / elapsed_time
-            metrics[f"device{self.separator}items_per_sec"] = dev_items_per_sec
-            if add_global_metrics:
-                items_per_sec = dev_items_per_sec * self.world_size
-                metrics["items_per_sec"] = items_per_sec
+            if len(self._lengths) == self._lengths.maxlen:
+                elapsed_lengths = self._lengths[-1] - self._lengths[0]
+                dev_items_per_sec = elapsed_lengths / elapsed_time
+                metrics[f"device{self.separator}items_per_sec"] = dev_items_per_sec
+                if add_global_metrics:
+                    items_per_sec = dev_items_per_sec * self.world_size
+                    metrics["items_per_sec"] = items_per_sec
 
-        print(metrics)
+            print(metrics)
 
         if len(self._flops) == self._flops.maxlen:
             elapsed_flops = sum(self._flops) - self._flops[0]
