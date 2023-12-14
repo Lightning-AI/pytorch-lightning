a317786566c655ce031ed0af27145fcdd31566b4
awaelchli <aedu.waelchli@gmail.com>
2023-12-14 22:50:40 +0100

    update

---
+++ tests/tests_fabric/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+from pytest import PytestWarning
+
+
+# Ignore cleanup warnings from pytest (rarely happens due to a race condition when executing pytest in parallel)
+warnings.filterwarnings("ignore", category=PytestWarning, message=r".*\(rm_rf\) error removing.*")
