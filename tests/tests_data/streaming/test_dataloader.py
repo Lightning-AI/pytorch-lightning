72fd2a0922e3f0ecd1cd728ff17e5cc66cb4b846
thomas <thomas@thomass-MacBook-Pro.local>
2023-12-21 11:26:48 +0000

    update

--- tests/tests_data/streaming/test_dataloader.py
+++ tests/tests_data/streaming/test_dataloader.py
@@ -2,7 +2,32 @@ import torch
 from lightning.data.streaming import CombinedStreamingDataset, StreamingDataLoader
 from torch import tensor
 
-from tests.tests_data.streaming.test_combined import TestStatefulDataset
+
+class TestStatefulDataset:
+    def __init__(self, size, step):
+        self.size = size
+        self.step = step
+        self.counter = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        self.counter = 0
+        return self
+
+    def __next__(self):
+        if self.counter == self.size:
+            raise StopIteration
+        value = self.step * self.counter
+        self.counter += 1
+        return value
+
+    def state_dict(self, counter):
+        return {"counter": counter}
+
+    def load_state_dict(self, state_dict):
+        self.counter = state_dict["counter"]
 
 
 def test_streaming_dataloader():
