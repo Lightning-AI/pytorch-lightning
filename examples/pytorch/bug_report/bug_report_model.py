023bd896719da0311bc40eba8e85d11a5d8eaeac
awaelchli <aedu.waelchli@gmail.com>
2023-12-28 22:12:52 +0100

    refactor

--- examples/pytorch/bug_report/bug_report_model.py
+++ examples/pytorch/bug_report/bug_report_model.py
@@ -2,64 +2,130 @@ import os
 
 import torch
 from lightning.pytorch import LightningModule, Trainer
-from torch.utils.data import DataLoader, Dataset
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchmetrics import Accuracy
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+PATH_DATASETS = os.environ.get("PATH_DATASETS", "./data")
+AVAIL_GPUS = min(1, torch.cuda.device_count())
+BATCH_SIZE = 256 if AVAIL_GPUS else 64
 
 
-class RandomDataset(Dataset):
-    def __init__(self, size, length):
-        self.len = length
-        self.data = torch.randn(length, size)
+class LitMNIST(LightningModule):
+    def __init__(self, data_dir=PATH_DATASETS, hidden_size=64, learning_rate=2e-4):
 
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return self.len
-
-
-class BoringModel(LightningModule):
-    def __init__(self):
         super().__init__()
-        self.layer = torch.nn.Linear(32, 2)
+
+        # Set our init args as class attributes
+        self.data_dir = data_dir
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+
+        # Hardcode some dataset specific attributes
+        self.num_classes = 10
+        self.dims = (1, 28, 28)
+        channels, width, height = self.dims
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+
+        # Define PyTorch model
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(channels * width * height, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, self.num_classes),
+        )
+
+        self.accuracy = Accuracy()
 
     def forward(self, x):
-        return self.layer(x)
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
 
     def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("valid_loss", loss)
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.accuracy(preds, y)
+
+        # Calling self.log will surface up scalars for you in TensorBoard
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.accuracy, prog_bar=True)
+        return loss
 
     def test_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("test_loss", loss)
+        # Here we just reuse the validation_step for testing
+        return self.validation_step(batch, batch_idx)
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    ####################
+    # DATA RELATED HOOKS
+    ####################
+
+    def prepare_data(self):
+        # download
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=BATCH_SIZE)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=BATCH_SIZE)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=BATCH_SIZE)
 
 
-def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+model = LitMNIST(learning_rate=2e-4)
+from lightning.pytorch.callbacks import RichProgressBar, ModelCheckpoint
+from lightning.pytorch.callbacks.progress.rich_progress import RichProgressBarTheme
 
-    model = BoringModel()
-    trainer = Trainer(
-        default_root_dir=os.getcwd(),
-        limit_train_batches=1,
-        limit_val_batches=1,
-        limit_test_batches=1,
-        num_sanity_val_steps=0,
-        max_epochs=1,
-        enable_model_summary=False,
-    )
-    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
-
-
-if __name__ == "__main__":
-    run()
+pbar_callback = RichProgressBar(
+    refresh_rate=1,
+    leave=False,
+    theme=RichProgressBarTheme(
+        metrics="grey7",
+        metrics_text_delimiter="\t",
+        metrics_format=".3f",
+    ),
+)
+callbacks = [pbar_callback]
+trainer = Trainer(
+    accelerator="cpu",
+    max_epochs=200,
+)
+trainer.fit(model)
--- src/lightning/fabric/utilities/checkpoint.py
+++ src/lightning/fabric/utilities/checkpoint.py
@@ -1,32 +1,26 @@
 import pickle
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import Optional
 
 import torch
 
-from lightning.fabric.utilities.types import _PATH
+from lightning.fabric.utilities.imports import _TORCH_GREATER_EQUAL_2_1
 
 
-def unshard_checkpoint(checkpoint_folder: _PATH, output_file: Optional[_PATH] = None) -> None:
+def unshard_checkpoint(checkpoint_folder: Path, output_file: Path) -> None:
     """Converts a sharded checkpoint saved with the `torch.distributed.checkpoint` API into a regular checkpoint that
     can be loaded with `torch.load()`.
 
     The current implementation assumes that the entire checkpoint fits in CPU memory.
 
     """
-    # TODO: PyTorch version check
+    if not _TORCH_GREATER_EQUAL_2_1:
+        raise ImportError("Processing distributed checkpoints requires PyTorch >= 2.1.")
 
     from torch.distributed.checkpoint import FileSystemReader, load_state_dict
-    from torch.distributed.checkpoint.metadata import BytesStorageMetadata, Metadata
+    from torch.distributed.checkpoint.metadata import BytesStorageMetadata, Metadata, TensorStorageMetadata
 
-    # TODO: Check if these paths exist
-    checkpoint_folder = Path(checkpoint_folder)
-    output_file = Path(
-        output_file if output_file is not None else checkpoint_folder.with_suffix(checkpoint_folder.suffix + ".merged")
-    )
     metadata_file = checkpoint_folder / ".metadata"
-
     with open(metadata_file, "rb") as file:
         metadata: Metadata = pickle.load(file)
 
@@ -35,17 +29,25 @@ def unshard_checkpoint(checkpoint_folder: _PATH, output_file: Optional[_PATH] = 
     for tensor_name, metadata in metadata.state_dict_metadata.items():
         if isinstance(metadata, BytesStorageMetadata):  # TODO: What does this represent?
             continue
-        state_dict[tensor_name] = torch.empty(
-            size=metadata.size, dtype=metadata.properties.dtype, device=torch.device("cpu")
-        )
+        elif isinstance(metadata, TensorStorageMetadata):
+            state_dict[tensor_name] = torch.empty(
+                size=metadata.size,
+                dtype=metadata.properties.dtype,
+                device=torch.device("cpu"),
+                memory_format=metadata.properties.memory_format,
+                layout=metadata.properties.layout,
+                requires_grad=metadata.properties.requires_grad,
+                pin_memory=metadata.properties.pin_memory,
+            )
 
-    reader = FileSystemReader(checkpoint_folder)
-    load_state_dict(state_dict=state_dict, storage_reader=reader, no_dist=True)
+    load_state_dict(state_dict=state_dict, storage_reader=FileSystemReader(checkpoint_folder), no_dist=True)
     torch.save(state_dict, output_file)
 
 
 def main() -> None:
-    parser = ArgumentParser(description=("Merges a sharded checkpoint into a single file."))
+    parser = ArgumentParser(
+        description="Merges a distributed/sharded checkpoint into a single file that can be loaded with `torch.load()`."
+    )
     parser.add_argument(
         "checkpoint_folder",
         type=str,
@@ -64,10 +66,25 @@ def main() -> None:
         ),
     )
     args = parser.parse_args()
-    unshard_checkpoint(
-        checkpoint_folder=args.checkpoint_folder,
-        output_file=args.output_file,
-    )
+
+    checkpoint_folder = Path(args.checkpoint_folder)
+    if not checkpoint_folder.exists():
+        raise FileNotFoundError(f"The provided checkpoint folder does not exist: {checkpoint_folder}")
+    if not checkpoint_folder.is_dir():
+        raise FileNotFoundError(
+            f"The provided checkpoint path must be a folder, containing the checkpoint shards: {checkpoint_folder}"
+        )
+    if args.output_file is None:
+        output_file = checkpoint_folder.with_suffix(checkpoint_folder.suffix + ".merged")
+    else:
+        output_file = Path(args.output_file)
+    if output_file.exists():
+        raise FileExistsError(
+            "The path for the merged checkpoint already exists. Choose a different path by providing"
+            f" `--output_file` or move/delete the file first: {output_file}"
+        )
+
+    unshard_checkpoint(checkpoint_folder=checkpoint_folder, output_file=output_file)
 
 
 if __name__ == "__main__":
