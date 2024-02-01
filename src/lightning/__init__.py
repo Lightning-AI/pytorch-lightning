fb9240f57769ac7fbd5cb7ccc31ac4767a0b895d
thomas <thomas@thomass-MacBook-Pro.local>
2024-02-01 15:19:00 +0000

    update

--- src/lightning/__init__.py
+++ src/lightning/__init__.py
@@ -1,5 +1,6 @@
 """Root package info."""
 import logging
+import os
 import sys
 
 # explicitly don't set root logger's propagation and leave this to subpackages to manage
@@ -13,23 +14,28 @@ formatter = logging.Formatter("%(levelname)s: %(message)s")
 _console.setFormatter(formatter)
 _logger.addHandler(_console)
 
-from lightning.__about__ import *  # noqa: E402, F403
-from lightning.__version__ import version as __version__  # noqa: E402
-from lightning.fabric.fabric import Fabric  # noqa: E402
-from lightning.fabric.utilities.seed import seed_everything  # noqa: E402
-from lightning.pytorch.callbacks import Callback  # noqa: E402
-from lightning.pytorch.core import LightningDataModule, LightningModule  # noqa: E402
-from lightning.pytorch.trainer import Trainer  # noqa: E402
+# This enables us to control imports in a more granular way for performance reasons
+lazy_import = bool(int(os.getenv("LIGHTNING_LAZY_IMPORTS", "0")))
 
-__all__ = [
-    "Trainer",
-    "LightningDataModule",
-    "LightningModule",
-    "Callback",
-    "seed_everything",
-    "Fabric",
-    "__version__",
-]
+if not lazy_import:
+
+    from lightning.__about__ import *  # noqa: F403
+    from lightning.__version__ import version as __version__
+    from lightning.fabric.fabric import Fabric
+    from lightning.fabric.utilities.seed import seed_everything
+    from lightning.pytorch.callbacks import Callback
+    from lightning.pytorch.core import LightningDataModule, LightningModule
+    from lightning.pytorch.trainer import Trainer
+
+    __all__ = [
+        "Trainer",
+        "LightningDataModule",
+        "LightningModule",
+        "Callback",
+        "seed_everything",
+        "Fabric",
+        "__version__",
+    ]
 
 
 def _cli_entry_point() -> None:
