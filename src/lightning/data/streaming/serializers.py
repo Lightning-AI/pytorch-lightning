1b3b723fb103e6657007afd68525c8ef50942a30
thomas chaton <thomas@grid.ai>
2024-02-06 12:47:04 +0000

    update

--- src/lightning/data/streaming/serializers.py
+++ src/lightning/data/streaming/serializers.py
@@ -28,7 +28,6 @@ from lightning.data.streaming.constants import _NUMPY_DTYPES_MAPPING, _TORCH_DTY
 _PIL_AVAILABLE = RequirementCache("PIL")
 _TORCH_VISION_AVAILABLE = RequirementCache("torchvision")
 _AV_AVAILABLE = RequirementCache("av")
-_RASTERIO_AVAILABLE = RequirementCache("rasterio")
 
 if _PIL_AVAILABLE:
     from PIL import Image
@@ -288,7 +287,7 @@ class FileSerializer(Serializer):
             return f.read(), file_extension.replace(".", "").lower()
 
     def deserialize(self, data: bytes) -> Any:
-        pass
+        return data
 
     def can_serialize(self, data: Any) -> bool:
         return isinstance(data, str) and os.path.exists(data)
@@ -323,19 +322,10 @@ class VideoSerializer(Serializer):
         return isinstance(data, str) and os.path.exists(data) and any(data.endswith(ext) for ext in self._EXTENSIONS)
 
 
-class TifSerializer(FileSerializer):
-    def deserialize(self, data: bytes) -> Any:
-        if not _RASTERIO_AVAILABLE:
-            raise ModuleNotFoundError("rasterio is required. Run: `pip install rasterio`")
-
-        from rasterio.io import MemoryFile
-
-        return MemoryFile(data)
-
 _SERIALIZERS = OrderedDict(
     **{
         "video": VideoSerializer(),
-        "tif": TifSerializer(),
+        "tif": FileSerializer(),
         "file": FileSerializer(),
         "pil": PILSerializer(),
         "int": IntSerializer(),
