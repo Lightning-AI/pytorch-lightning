a90fec9fc5f89809727260f84f37e42d46751605
Xinyu Yang <cauyxy@163.com>
2024-02-09 23:21:28 +0800

    bugfix: correct node rank (#19437)
    (cherry picked from commit 7b867c7d91ad8f154e9c5b4656271a6329a8b059)

--- tests/tests_data/utilities/test_shuffle.py
+++ tests/tests_data/utilities/test_shuffle.py
@@ -12,6 +12,11 @@ def test_intra_node_chunk_shuffle():
     assert shuffled_indexes == [3, 2, 1, 0, 7, 6, 5, 4]
 
 
+    chunks_per_ranks = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [10, 11], [12, 13], [14, 15]]
+    shuffled_indexes = _intra_node_chunk_shuffle(_DistributedEnv(8, 7, 2), chunks_per_ranks, 42, 2)
+    assert shuffled_indexes == [5, 2, 0, 7, 6, 1, 3, 4, 13, 10, 8, 15, 14, 9, 11, 12]
+
+
 def test_associate_chunks_and_internals_to_ranks():
     indexes = [0, 1, 2, 3, 4, 5, 6, 7]
     chunk_intervals = [[0, 50], [0, 50], [0, 50], [0, 50], [0, 50], [0, 50], [0, 50], [0, 50]]
