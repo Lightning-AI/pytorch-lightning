074e01b38b0639f2ee9a25ba155d9cdf08caf0cb
karthik venkataramani <karthikvt@vt.edu>
2024-02-13 12:53:04 -0800

    Revert "Update train.py"
    This reverts commit 19347600564c0ab4fbda2e80263fc337f2297508.

--- docs/source-app/code_samples/convert_pl_to_app/train.py
+++ docs/source-app/code_samples/convert_pl_to_app/train.py
@@ -29,7 +29,7 @@ class LitAutoEncoder(pl.LightningModule):
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, y)
+        loss = F.mse_loss(x_hat, x)
         self.log("train_loss", loss)
         return loss
 
