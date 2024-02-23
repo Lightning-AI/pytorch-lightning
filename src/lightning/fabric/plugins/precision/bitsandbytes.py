d2df6f0d8c3aaae9713ab9160ab7998809353c33
Kashif Rasul <kashif.rasul@gmail.com>
2024-02-23 21:10:21 +0100

    require bitsandbytes>0.41.0

--- src/lightning/fabric/plugins/precision/bitsandbytes.py
+++ src/lightning/fabric/plugins/precision/bitsandbytes.py
@@ -39,7 +39,7 @@ from lightning.fabric.utilities.types import _DEVICE
 
 log = logging.getLogger(__name__)
 
-_BITSANDBYTES_AVAILABLE = RequirementCache("bitsandbytes>=0.41.0")
+_BITSANDBYTES_AVAILABLE = RequirementCache("bitsandbytes>0.41.0")
 
 
 class BitsandbytesPrecision(Precision):
@@ -343,12 +343,7 @@ def _import_bitsandbytes() -> ModuleType:
         def to_empty(self, *, device: _DEVICE, recurse: bool = True) -> Self:
             if self.weight.dtype == torch.uint8:  # was quantized
                 # cannot init the quantized params directly
-                shape = (
-                    self.weight.quant_state.shape
-                    if hasattr(self.weight.quant_state, "shape")
-                    else self.weight.quant_state[1]
-                )
-                weight = torch.empty(shape, device=device, dtype=torch.half)
+                weight = torch.empty(self.weight.quant_state.shape, device=device, dtype=torch.half)
             else:
                 weight = torch.empty_like(self.weight.data, device=device)
             device = torch.device(device)
@@ -370,12 +365,7 @@ def _import_bitsandbytes() -> ModuleType:
             linear_init_finished = isinstance(self.weight, bnb.nn.Params4bit)
             if linear_init_finished and self.weight.dtype == torch.uint8:  # was quantized
                 # cannot init the quantized params directly
-                shape = (
-                    self.weight.quant_state.shape
-                    if hasattr(self.weight.quant_state, "shape")
-                    else self.weight.quant_state[1]
-                )
-                weight = torch.empty(shape, device=self.weight.device, dtype=torch.half)
+                weight = torch.empty(self.weight.quant_state.shape, device=self.weight.device, dtype=torch.half)
             else:
                 weight = self.weight.data
             torch.nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
