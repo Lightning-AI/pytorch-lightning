ce6fd57cfae36e6912a378f6a2186f743a286cbc
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-04-03 22:22:53 +0200

    skip test

--- tests/tests_pytorch/utilities/test_compile.py
+++ tests/tests_pytorch/utilities/test_compile.py
@@ -19,6 +19,7 @@ import torch
 from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.demos.boring_classes import BoringModel
 from lightning.pytorch.utilities.compile import from_compiled, to_uncompiled
+from lightning.fabric.utilities.imports import _TORCH_GREATER_EQUAL_2_2
 from lightning_utilities.core import module_available
 
 from tests_pytorch.conftest import mock_cuda_count
@@ -114,6 +115,9 @@ def test_compile_uncompile():
 
 # https://github.com/pytorch/pytorch/issues/95708
 @pytest.mark.skipif(sys.platform == "darwin", reason="fatal error: 'omp.h' file not found")
+@pytest.mark.xfail(
+    sys.platform == "win32" and _TORCH_GREATER_EQUAL_2_2, strict=False, reason="RuntimeError: Failed to import"
+)
 @RunIf(dynamo=True)
 def test_trainer_compiled_model_that_logs(tmp_path):
     class MyModel(BoringModel):
