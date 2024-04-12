55d067460beaf2a04c919b85f16dfba742cf566f
Douwe den Blanken <douwedb@gmail.com>
2024-04-12 23:16:32 +0200

    Put original import statement back in wandb.py

--- src/lightning/pytorch/loggers/wandb.py
+++ src/lightning/pytorch/loggers/wandb.py
@@ -26,11 +26,7 @@ from lightning_utilities.core.imports import RequirementCache
 from torch import Tensor
 from typing_extensions import override
 
-from lightning.fabric.utilities.logger import (
-    _add_prefix,
-    _convert_params,
-    _sanitize_callable_params
-)
+from lightning.fabric.utilities.logger import _add_prefix, _convert_params, _sanitize_callable_params
 from lightning.fabric.utilities.types import _PATH
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from lightning.pytorch.loggers.logger import Logger, rank_zero_experiment
