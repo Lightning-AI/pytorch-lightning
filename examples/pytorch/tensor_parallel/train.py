5d9d3f68a2510992b72950e5ea60f26b38d2e3f3
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-05-20 06:17:12 -0400

    update pytorch example

--- examples/pytorch/tensor_parallel/train.py
+++ examples/pytorch/tensor_parallel/train.py
@@ -1,6 +1,7 @@
 import lightning as L
 import torch
 import torch.nn.functional as F
+from torch.distributed.tensor.parallel import loss_parallel
 from data import RandomTokenDataset
 from lightning.pytorch.strategies import ModelParallelStrategy
 from model import ModelArgs, Transformer
@@ -26,7 +27,13 @@ class Llama2(L.LightningModule):
         inputs = batch[:, :-1]
         labels = batch[:, 1:]
         output = self.model(inputs)
-        return F.cross_entropy(output.reshape(-1, output.size(-1)), labels.reshape(-1))
+        # Optional: Parallelize loss computation across class dimension (see parallelism.py)
+        with loss_parallel():
+            return F.cross_entropy(output.reshape(-1, output.size(-1)), labels.reshape(-1))
+
+    def backward(self, *args, **kwargs):
+        with loss_parallel():
+            super().backward(*args, **kwargs)
 
     def configure_optimizers(self):
         return torch.optim.AdamW(self.model.parameters(), lr=3e-3, foreach=True)
