8cf1065acddd988c6e3e91f5a52730f2c9b88f42
awaelchli <aedu.waelchli@gmail.com>
2024-06-25 20:17:26 +0200

    x

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,6 @@
 import subprocess
 
 with open("test_list_new.txt", "r") as file:
-    tests = file.readlines()
+    tests = [line.strip() for line in file.readlines()]
 
-
-subprocess.run(["pytest", "-v"] + tests)
+subprocess.run(["pytest", "-v"] + tests[-100:])
