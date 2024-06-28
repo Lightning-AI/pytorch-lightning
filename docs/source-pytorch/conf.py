4f432da034196bf06b273e50af7e1dae1f07813d
Jirka <jirka.borovec@seznam.cz>
2024-06-28 14:05:03 +0200

    the HPU also need some images

--- docs/source-pytorch/conf.py
+++ docs/source-pytorch/conf.py
@@ -14,6 +14,7 @@
 import glob
 import os
 import shutil
+import urllib
 import warnings
 from importlib.util import module_from_spec, spec_from_file_location
 from types import ModuleType
@@ -93,6 +94,12 @@ assist_local.AssistantCLI.pull_docs_files(
     target_dir="docs/source-pytorch/integrations/hpu",
     checkout="refs/tags/1.5.0",
 )
+# the HPU also need some images
+URL_RAW_DOCS_GRAPHCORE = "https://raw.githubusercontent.com/Lightning-AI/lightning-Habana/1.5.0/docs/source"
+for img in ["_images/HPUProfiler.png", "_images/IGP.png"]:
+    img_ = os.path.join(_PATH_HERE, "integrations", "hpu", img)
+    os.makedirs(os.path.dirname(img_), exist_ok=True)
+    urllib.request.urlretrieve(f"{URL_RAW_DOCS_GRAPHCORE}/{img}", img_)
 
 # Copy strategies docs as single pages
 assist_local.AssistantCLI.pull_docs_files(
