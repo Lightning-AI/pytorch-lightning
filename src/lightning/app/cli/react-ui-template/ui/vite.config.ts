de3fb57d8f41f136a558a52bff02fc5a1d12f2e7
awaelchli <aedu.waelchli@gmail.com>
2024-07-05 14:51:50 +0200

    Remove the lightning app code (#20039)

--- src/lightning/app/cli/react-ui-template/ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // NOTE: Component UI's are served under `/{componentName}/` subpath, so the app needs to be configured for relative base path.
-  base: "./",
-});
