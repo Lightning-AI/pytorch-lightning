db9a9e37af039c16ce9402ac5e77b38795c8a5d7
awaelchli <aedu.waelchli@gmail.com>
2024-07-05 14:51:50 +0200

    Remove the lightning app code (#20039)

--- src/lightning/store/__init__.py
+++
@@ -1,3 +0,0 @@
-from lightning.store.store import download_model, list_models, upload_model
-
-__all__ = ["download_model", "upload_model", "list_models"]
--- tests/tests_app/utilities/packaging/projects/no_req/comp/a/a.py
+++
@@ -1,8 +0,0 @@
-import pandas  # noqa F401
-
-from lightning.app import LightningWork
-
-
-class AA(LightningWork):
-    def run(self):
-        pass
