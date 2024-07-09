17bded89d868f9d8de3ac3909876bd2b3548f274
Corwin Joy <corwin.joy@gmail.com>
2024-07-08 18:55:03 -0700

    Add tests showing GPU to CPU copies

--- src/lightning/fabric/utilities/optimizer.py
+++ src/lightning/fabric/utilities/optimizer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import contextlib
 from typing import Iterable
 
 from lightning_utilities.core.apply_func import apply_to_collection
@@ -28,7 +29,23 @@ def _optimizers_to_device(optimizers: Iterable[Optimizer], device: _DEVICE) -> N
         _optimizer_to_device(opt, device)
 
 
-def _optimizer_to_device(optimizer: Optimizer, device: _DEVICE) -> None:
+def _optimizer_to_device_old(optimizer: Optimizer, device: _DEVICE) -> None:
     """Moves the state of a single optimizer to the device."""
     for p, v in optimizer.state.items():
         optimizer.state[p] = apply_to_collection(v, Tensor, move_data_to_device, device, allow_frozen=True)
+
+
+def _optimizer_to_device(optimizer: Optimizer, device: _DEVICE) -> None:
+    """Moves the state of a single optimizer to the device."""
+    # Note special logic for 'step' parameter
+    # The 'step' parameter needs to remain unmoved (possibly on the CPU) since that is where the optimizer needs it.
+    # See https://github.com/pytorch/pytorch/issues/74424 and
+    # _process_value_according_to_param_policy in torch/optim/optimizer.py:618
+    fused = False
+    with contextlib.suppress(Exception):
+        fused = optimizer.param_groups[0]["fused"]
+
+    for p, v in optimizer.state.items():
+        for key, val in v.items():
+            if key != "step" or fused:
+                v[key] = move_data_to_device(val, device)
