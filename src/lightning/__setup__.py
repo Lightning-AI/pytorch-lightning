8b0d34410bfeab4caefe89c288acca8af9e35843
awaelchli <aedu.waelchli@gmail.com>
2024-07-12 00:13:14 +0200

    remove meta data

--- src/lightning/__setup__.py
+++ src/lightning/__setup__.py
@@ -124,7 +124,6 @@ def _setup_args() -> Dict[str, Any]:
             "Operating System :: OS Independent",
             # Specify the Python versions you support here.
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
