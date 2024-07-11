bf25167bbf64f50ba335aa759318946b21775cd2
awaelchli <aedu.waelchli@gmail.com>
2024-07-11 12:52:56 +0200

    Add testing for PyTorch 2.4 (Trainer) (#20010)

--- tests/tests_pytorch/trainer/connectors/test_accelerator_connector.py
+++ tests/tests_pytorch/trainer/connectors/test_accelerator_connector.py
@@ -1067,7 +1067,7 @@ def test_bitsandbytes_precision_cuda_required(monkeypatch):
         _AcceleratorConnector(accelerator="cpu", plugins=BitsandbytesPrecision(mode="int8"))
 
 
-@RunIf(min_torch="2.3")
+@RunIf(min_torch="2.4")
 @pytest.mark.parametrize(
     ("precision", "raises"),
     [("32-true", False), ("16-true", False), ("bf16-true", False), ("16-mixed", True), ("bf16-mixed", False)],
