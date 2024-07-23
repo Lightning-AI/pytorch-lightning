3fe10dc50b53b75d0037e709b86223012c4b89d3
Adrian Wälchli <aedu.waelchli@gmail.com>
2024-07-23 10:33:53 -0400

    fix

--- src/lightning/fabric/utilities/load.py
+++ src/lightning/fabric/utilities/load.py
@@ -160,7 +160,7 @@ class _NotYetLoadedTensor:
             return getattr(self.metatensor, name)
 
         # materializing these is needed for quantization (see lit-gpt)
-        if name in {"contiguous", "cuda", "half"}:
+        if name in {"contiguous", "cuda", "half", "data"}:
             return getattr(self._load_tensor(), name)
 
         raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
