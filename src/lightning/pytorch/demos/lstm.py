a00ceaad53c4e1774290b4f55ed0df854b2f6f9f
awaelchli <aedu.waelchli@gmail.com>
2024-07-31 13:50:31 +0200

    update types

--- src/lightning/pytorch/demos/lstm.py
+++ src/lightning/pytorch/demos/lstm.py
@@ -3,18 +3,22 @@
 Code is adapted from the PyTorch examples at
 https://github.com/pytorch/examples/blob/main/word_language_model
 """
-from typing import List
+from typing import List, Tuple, Sized, Iterator, Optional
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import Tensor
+from torch.optim import Optimizer
 from torch.utils.data import DataLoader, Sampler
 from lightning.pytorch.demos import WikiText2
 import lightning as L
 
 
 class LSTMModel(nn.Module):
-    def __init__(self, vocab_size=33278, ninp=512, nhid=512, nlayers=4, dropout=0.2):
+    def __init__(
+        self, vocab_size: int = 33278, ninp: int = 512, nhid: int = 512, nlayers: int = 4, dropout: float = 0.2
+    ):
         super().__init__()
         self.vocab_size = vocab_size
         self.drop = nn.Dropout(dropout)
@@ -25,12 +29,12 @@ class LSTMModel(nn.Module):
         self.nhid = nhid
         self.init_weights()
 
-    def init_weights(self):
+    def init_weights(self) -> None:
         nn.init.uniform_(self.encoder.weight, -0.1, 0.1)
         nn.init.zeros_(self.decoder.bias)
         nn.init.uniform_(self.decoder.weight, -0.1, 0.1)
 
-    def forward(self, input, hidden):
+    def forward(self, input: Tensor, hidden: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tensor]:
         emb = self.drop(self.encoder(input))
         output, hidden = self.rnn(emb, hidden)
         output = self.drop(output)
@@ -38,27 +42,27 @@ class LSTMModel(nn.Module):
         decoded = decoded.view(-1, self.vocab_size)
         return F.log_softmax(decoded, dim=1), hidden
 
-    def init_hidden(self, bsz):
+    def init_hidden(self, batch_size: int) -> Tuple[Tensor, Tensor]:
         weight = next(self.parameters())
         return (
-            weight.new_zeros(self.nlayers, bsz, self.nhid),
-            weight.new_zeros(self.nlayers, bsz, self.nhid),
+            weight.new_zeros(self.nlayers, batch_size, self.nhid),
+            weight.new_zeros(self.nlayers, batch_size, self.nhid),
         )
 
 
 class SequenceSampler(Sampler[List[int]]):
-    def __init__(self, dataset, batch_size):
+    def __init__(self, dataset: Sized, batch_size: int) -> None:
         super().__init__()
         self.dataset = dataset
         self.batch_size = batch_size
         self.chunk_size = len(self.dataset) // self.batch_size
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[List[int]]:
         n = len(self.dataset)
         for i in range(self.chunk_size):
             yield list(range(i, n - (n % self.batch_size), self.chunk_size))
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.chunk_size
 
 
@@ -66,12 +70,12 @@ class LightningLSTM(L.LightningModule):
     def __init__(self, vocab_size: int = 33278):
         super().__init__()
         self.model = LSTMModel(vocab_size=vocab_size)
+        self.hidden: Optional[Tuple[Tensor, Tensor]] = None
+
+    def on_train_epoch_end(self) -> None:
         self.hidden = None
 
-    def on_train_epoch_end(self):
-        self.hidden = None
-
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
         input, target = batch
         if self.hidden is None:
             self.hidden = self.model.init_hidden(input.size(0))
@@ -88,5 +92,5 @@ class LightningLSTM(L.LightningModule):
         dataset = WikiText2()
         return DataLoader(dataset, batch_sampler=SequenceSampler(dataset, batch_size=20))
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> Optimizer:
         return torch.optim.SGD(self.parameters(), lr=20.0)
