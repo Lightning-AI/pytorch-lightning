781ea264497fd1d41fcc32b79e5bd2b9f73b8972
awaelchli <aedu.waelchli@gmail.com>
2024-07-31 13:46:03 +0200

    imports

--- src/lightning/pytorch/demos/__init__.py
+++ src/lightning/pytorch/demos/__init__.py
@@ -1,2 +1,2 @@
 from lightning.pytorch.demos.transformer import LightningTransformer, Transformer, WikiText2  # noqa: F401
-from lightning.pytorch.demos.lstm import LSTMModel  # noqa: F401
+from lightning.pytorch.demos.lstm import LightningLSTM, LSTMModel, SequenceSampler  # noqa: F401
