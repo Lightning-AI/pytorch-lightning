4ebbb53f5f3013d9d21e095b11b3b189cca02614
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-11-12 16:28:06 +0100

    Apply suggestions from code review

--- tests/tests_fabric/utilities/test_logger.py
+++ tests/tests_fabric/utilities/test_logger.py
@@ -67,12 +67,13 @@ def test_flatten_dict():
     params = {"dl": [{"a": 1, "c": 3}, {"b": 2, "d": 5}], "l": [1, 2, 3, 4]}
     params = _flatten_dict(params)
 
-    assert "dl" not in params
-    assert params["dl/0/a"] == 1
-    assert params["dl/0/c"] == 3
-    assert params["dl/1/b"] == 2
-    assert params["dl/1/d"] == 5
-    assert params["l"] == [1, 2, 3, 4]
+    assert params == {
+    "dl/0/a": 1,
+    "dl/0/c": 3,
+    "dl/1/b": 2,
+    "dl/1/d": 5,
+    "l": [1, 2, 3, 4]
+    }
 
     # Test flattening of argparse Namespace
     params = Namespace(a=1, b=2)
