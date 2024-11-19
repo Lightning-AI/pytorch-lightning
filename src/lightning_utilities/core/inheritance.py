4bce8276cc81067e9c456f5c5d7f96db7730df99
Jirka B <j.borovec+github@gmail.com>
2024-11-19 12:22:40 +0100

    update copyright

--- src/lightning_utilities/core/inheritance.py
+++ src/lightning_utilities/core/inheritance.py
@@ -1,4 +1,4 @@
-# Copyright The PyTorch Lightning team.
+# Copyright The Lightning AI team.
 # Licensed under the Apache License, Version 2.0 (the "License");
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
