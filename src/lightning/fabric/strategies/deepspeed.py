737162d6f3bc7369a6ca95309f6ed22be557d34c
Luca Antiga <luca@lightning.ai>
2024-11-25 11:35:00 +0100

    Update src/lightning/fabric/strategies/deepspeed.py

--- src/lightning/fabric/strategies/deepspeed.py
+++ src/lightning/fabric/strategies/deepspeed.py
@@ -314,7 +314,7 @@ class DeepSpeedStrategy(DDPStrategy, _Sharded):
     @override
     def setup_module_and_optimizers(
         self, module: Module, optimizers: list[Optimizer], scheduler: Optional[_LRScheduler] = None
-    ) -> Tuple["DeepSpeedEngine", list[Optimizer], Optional[_LRScheduler]]:
+    ) -> tuple["DeepSpeedEngine", list[Optimizer], Optional[_LRScheduler]]:
         """Set up a model and multiple optimizers together, along with an optional learning rate scheduler. Currently,
         only a single optimizer is supported.
 
