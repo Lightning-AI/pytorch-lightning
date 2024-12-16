e428402ee51e0d1289d5b9f0ba5ab6b60164ac78
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-12-16 18:28:35 +0900

    update: add testing with Python 3.13 (#346)

--- setup.py
+++ setup.py
@@ -88,5 +88,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
