3a066811f5e45dcd0ea4971ec40186864ec5eaf4
Jirka B <j.borovec+github@gmail.com>
2025-01-08 20:01:10 +0900

    bitsandbytes

--- requirements/pytorch/check-avail-extras.py
+++ requirements/pytorch/check-avail-extras.py
@@ -4,7 +4,3 @@ if __name__ == "__main__":
     import matplotlib  # noqa: F401
     import omegaconf  # noqa: F401
     import rich  # noqa: F401
-    import torch
-
-    if torch.cuda.is_available():
-        import bitsandbytes  # noqa: F401
