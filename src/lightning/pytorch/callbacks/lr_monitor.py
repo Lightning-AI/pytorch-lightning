209d15ffeee7880c86ab0000e614002c13bfc6a4
Jirka B <j.borovec+github@gmail.com>
2025-04-28 10:18:30 +0200

    typing

--- src/lightning/pytorch/callbacks/lr_monitor.py
+++ src/lightning/pytorch/callbacks/lr_monitor.py
@@ -255,7 +255,8 @@ class LearningRateMonitor(Callback):
         if not self.log_momentum:
             return {}
 
-        momentum = param_group["betas"][0] if use_betas else param_group.get("momentum", 0)
+        momentum = param_group["betas"][0] if use_betas else param_group.get("momentum")
+        momentum = momentum if momentum is not None else 0
         self.last_momentum_values[name] = momentum
         return {name: momentum}
 
