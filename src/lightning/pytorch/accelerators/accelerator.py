3a3949a1d2ad98fa6b743b1e510e1182113a0073
GdoongMathew <gdoongmathew@gmail.com>
2025-08-24 21:48:16 +0800

    fix: fix type annotation.

--- src/lightning/pytorch/accelerators/accelerator.py
+++ src/lightning/pytorch/accelerators/accelerator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from abc import ABC
-from typing import Any
+from typing import Any, Optional
 
 import lightning.pytorch as pl
 from lightning.fabric.accelerators.accelerator import Accelerator as _Accelerator
@@ -47,6 +47,6 @@ class Accelerator(_Accelerator, ABC):
         raise NotImplementedError
 
     @classmethod
-    def device_name(cls, device: _DEVICE = None) -> str:
+    def device_name(cls, device: Optional[_DEVICE] = None) -> str:
         """Get the device name for a given device."""
         return str(cls.is_available())
