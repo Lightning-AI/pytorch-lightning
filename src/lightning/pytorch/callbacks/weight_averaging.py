3d3323da802a4f60bd7835d74e1e246a607c7523
Alex Morehead <alex.morehead@gmail.com>
2025-10-18 13:29:21 -0700

    Update weight_averaging.py

--- src/lightning/pytorch/callbacks/weight_averaging.py
+++ src/lightning/pytorch/callbacks/weight_averaging.py
@@ -387,7 +387,7 @@ class EMAWeightAveraging(WeightAveraging):
         self.update_starting_at_step = update_starting_at_step
         self.update_starting_at_epoch = update_starting_at_epoch
 
-    def should_update(self, step_idx: Optional[int] = None, epoch_idx: Optional[int] = None) -> False:
+    def should_update(self, step_idx: Optional[int] = None, epoch_idx: Optional[int] = None) -> bool:
         """Decide when to update the model weights.
 
         Args:
