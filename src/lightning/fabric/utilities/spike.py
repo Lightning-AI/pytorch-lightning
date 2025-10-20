3c6c84aa6628e444f178b3fb69c2cec4ba29d240
bhimrazy <bhimrajyadav977@gmail.com>
2025-10-20 14:39:03 +0545

    add ignore

--- src/lightning/fabric/utilities/spike.py
+++ src/lightning/fabric/utilities/spike.py
@@ -126,16 +126,16 @@ class SpikeDetection:
         raise TrainingSpikeException(batch_idx=batch_idx)
 
     def _check_atol(self, val_a: Union[float, torch.Tensor], val_b: Union[float, torch.Tensor]) -> bool:
-        return (self.atol is None) or bool(abs(val_a - val_b) >= abs(self.atol))
+        return (self.atol is None) or bool(abs(val_a - val_b) >= abs(self.atol))  # type: ignore
 
     def _check_rtol(self, val_a: Union[float, torch.Tensor], val_b: Union[float, torch.Tensor]) -> bool:
-        return (self.rtol is None) or bool(abs(val_a - val_b) >= abs(self.rtol * val_b))
+        return (self.rtol is None) or bool(abs(val_a - val_b) >= abs(self.rtol * val_b))  # type: ignore
 
     def _is_better(self, diff_val: torch.Tensor) -> bool:
         if self.mode == "min":
-            return bool((diff_val <= 0.0).all())
+            return bool((diff_val <= 0.0).all())  # type: ignore[operator]
         if self.mode == "max":
-            return bool((diff_val >= 0).all())
+            return bool((diff_val >= 0).all())  # type: ignore[operator]
 
         raise ValueError(f"Invalid mode. Has to be min or max, found {self.mode}")
 
