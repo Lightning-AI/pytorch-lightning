1eb83ace9b17c139aea8d1b7c0e700f0ce0b049c
Justus Schock <justus.schock@posteo.de>
2025-11-17 15:47:22 +0000

    add tpu mocks

--- tests/tests_fabric/graveyard/test_tpu.py
+++ tests/tests_fabric/graveyard/test_tpu.py
@@ -11,7 +11,7 @@ import torch
         ("lightning.fabric.strategies.single_tpu", "SingleTPUStrategy"),
     ],
 )
-def test_graveyard_single_tpu(import_path, name):
+def test_graveyard_single_tpu(import_path, name, tpu_available):
     module = import_module(import_path)
     cls = getattr(module, name)
     device = torch.device("cpu")
@@ -34,7 +34,7 @@ def test_graveyard_single_tpu(import_path, name):
         ("lightning.fabric.plugins.precision.xlabf16", "XLABf16Precision"),
     ],
 )
-def test_graveyard_no_device(import_path, name):
+def test_graveyard_no_device(import_path, name, tpu_available):
     module = import_module(import_path)
     cls = getattr(module, name)
     with pytest.deprecated_call(match="is deprecated"):
