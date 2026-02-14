3271035fa3f200f4083a956f1bab4c30cd5e954f
deependujha <deependujha21@gmail.com>
2026-02-14 21:00:29 +0530

    try fixing pkg_resources

--- requirements/collect_env_details.py
+++ requirements/collect_env_details.py
@@ -20,8 +20,8 @@ This server mainly to get detail info for better bug reporting.
 import os
 import platform
 import sys
+from importlib.metadata import distributions
 
-import pkg_resources
 import torch
 
 sys.path += [os.path.abspath(".."), os.path.abspath("")]
@@ -50,13 +50,9 @@ def info_cuda() -> dict:
     }
 
 
-def info_packages() -> dict:
+def info_packages() -> dict[str, str]:
     """Get name and version of all installed packages."""
-    packages = {}
-    for dist in pkg_resources.working_set:
-        package = dist.as_requirement()
-        packages[package.key] = package.specs[0][1]
-    return packages
+    return {dist.metadata["Name"].lower(): dist.version for dist in distributions()}
 
 
 def nice_print(details: dict, level: int = 0) -> list:
