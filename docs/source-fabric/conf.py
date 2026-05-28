7ca58a994f7d5c760991294b480d3794a32c7e5c
deependujha <deependujha21@gmail.com>
2026-05-28 16:03:42 +0530

    update

--- docs/source-fabric/conf.py
+++ docs/source-fabric/conf.py
@@ -393,3 +393,8 @@ linkcheck_timeout = 60
 
 # ignore all links in any CHANGELOG file
 linkcheck_exclude_documents = [r"^(.*\/)*CHANGELOG.*$"]
+
+# ignore the following relative links (false positive errors during linkcheck)
+linkcheck_ignore = [
+    "https://www.mpich.org/*",
+]
